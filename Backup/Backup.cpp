3bdb3728346b4efe22babd69797f3ac072dffa40
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-04-07 12:27:49 +0000

    Code review (general improvements performed + ToDo's).

--- Backup/Backup.cpp
+++ Backup/Backup.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 #include "Path_Loader.hpp"
 #include "Backup.hpp"
@@ -13,16 +14,24 @@
  
 
 int main(const int argc, const char* const argv[]) {
+
+  // ToDo: Using Messages
   
-  const std::string version_no("0.2");
-  const std::string banner = "OKSystem Backup Version " + version_no;
+  const std::string banner = "OKSystem Backup";
+  // ToDo: compilation time and date
+  // ToDo: current date and time
+  // ToDo: Output identifier
   std::cout << banner << '\n';
      
+  // ToDo: string variable "program identification"
+  // Every output prefixed by it.
+
   if (argc != 3) {
     const std::string error_no_input = "Please specify source and destination directories!";
+    // ToDo: Output the parameters
     const std::string info_usage = "Backup [source] [destination]";
-    std::cout << error_no_input << '\n' << info_usage << '\n';
-    return 1;
+    std::cerr << error_no_input << '\n' << info_usage << '\n';
+    return EXIT_FAILURE;
   }
 
   InfoHolder::DirectoryContainer<std::string> backup_dir;
@@ -31,6 +40,7 @@ int main(const int argc, const char* const argv[]) {
   const std::string dest = argv[2];
 
   try {
+    // ToDO: "verify"
     PathLoader::varify_source(source, backup_dir);
     PathLoader::varify_dest(dest, backup_dir);
 
@@ -41,23 +51,26 @@ int main(const int argc, const char* const argv[]) {
     std::cout << "Successfully Copied Backup Archive " << backup_dir.archive() << " To " << backup_dir.destination() << '\n';
     
     std::cout << CommandCaller::remove(backup_dir);
+    // ToDo: make "store" and "remove" follow the same principle
     
   }
   catch(const Backup::Error::Directory_Error& e){
-    std::cerr << e.what() << std::endl;
-    return 1;
+    // ToDo: more information (using general library)
+    std::cerr << e.what() << "\n";
+    return EXIT_FAILURE;
   }
   catch(const Backup::Error::Command_Error& e){
-    std::cerr << e.what() << std::endl;
-    return 1;
+    // ToDo: more information (using general library)
+    std::cerr << e.what() << "\n";
+    return EXIT_FAILURE;
   }
   catch(...){
-    std::cerr << "Program Aborted or Unknown Error Occured!" << std::endl;
-    return 1;
+    std::cerr << "Program Aborted or Unknown Error Occured!" << "\n";
+    return EXIT_FAILURE;
   }
   
   const std::string acknowledge = "Backup Done!";
-  std::cout << acknowledge << '\n';
-  return 0;
+  std::cout << acknowledge << '\n\n';
+  // ToDo: time and date
   
 }
