4b76267ee26ed1677b435ef24f74b80c5d0d3c76
Tony Bao <cstony@Swansea.ac.uk>
2005-04-13 18:40:01 +0000

    Top level header to be included by programs wish to use the IOTools module but only declarations headers are included in this file.

--- Messages/Messages_Declarations.hpp
+++ Messages/Messages_Declarations.hpp
@@ -1,1 +1,10 @@
- 
+#ifndef MESSAGESDECLARATIONS_jhgUIT98GVLLH1
+
+#define MESSAGESDECLARATIONS_jhgUIT98GVLLH1
+
+#include "Messages_Concept_Syntax_Declarations.hpp"
+
+#include "Messages_Models_Declarations.hpp"
+
+
+#endif
