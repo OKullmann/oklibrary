48342669d9f640bdef3d2246dffdd6673be9b196
Tony Bao <cstony@Swansea.ac.uk>
2005-04-13 18:11:52 +0000

    Contains Syntax definitions of the concepts of messages module

---
+++ Messages/Messages_Concept_Syntax_Definitions.hpp
@@ -0,0 +1,17 @@
+// To Do: LICENCE
+
+#ifndef MESSAGESCONCEPTSYNTAXDEFINITIONS_64gADSgb298
+
+#define MESSAGESCONCEPTSYNTAXDEFINITIONS_64gADSgb298
+
+namespace OKlib {
+  
+  namespace Messages {
+    
+    namespace Concept_Syntax {
+  
+    }
+  }
+}
+
+#endif
