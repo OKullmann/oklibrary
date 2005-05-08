42b3b25d77ce4e20be08eff01f9ac0fe12757a99
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-08 20:41:04 +0000

    Nearly all header files in the directory included (to insure at least some syntax test; two are not included which need the PostgreSQL resp. the Xerces library).

--- General/TestBibliothek.cpp
+++ General/TestBibliothek.cpp
@@ -7,4 +7,19 @@
 #include "NumberTheory_Testobjects.hpp"
 #include "Algebra_Testobjects.hpp"
 
+#include "AutomatedCompilation.hpp"
+// #include "CgiHandling.hpp" // needs library cgicc
+// #include "DatabaseHandler01.hpp" // needs libpq
+#include "emailHandling.hpp"
+#include "EndlAuto.hpp"
+#include "ErrorHandling.hpp"
+#include "Kommandozeile.hpp"
+#include "Matrices.hpp"
+#include "MessageHandling.hpp"
+#include "NumericalLimits.hpp"
+#include "StreamHandling.hpp"
+#include "TimeHandling.hpp"
+// #include "XercesTools.hpp" // needs library xercesc
+#include "XMLHandling.hpp"
+
 int main() {}
