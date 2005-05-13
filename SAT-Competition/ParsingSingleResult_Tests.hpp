a0fab65b131c37804d1a6c855163bdbc38b0547b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-13 20:32:44 +0000

    The constructor of Test_ParserResultElement_SuperSeries with the insert-call was missing. Also now changed the asserts into throws.

--- SAT-Competition/ParsingSingleResult_Tests.hpp
+++ SAT-Competition/ParsingSingleResult_Tests.hpp
@@ -6,11 +6,11 @@
 
 #include <sstream>
 #include <string>
-#include <cassert> // #####################
 
 #include <boost/spirit/core.hpp>
 
 #include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
 
 #include "SingleResult.hpp"
 #include "ParsingSingleResult.hpp"
@@ -20,14 +20,23 @@ namespace OKlib {
   namespace SATCompetition {
 
     class Test_ParserResultElement_SuperSeries : ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_ParserResultElement_SuperSeries test_type;
+      Test_ParserResultElement_SuperSeries() {
+	insert(this);
+      }
+    private :
       void perform_test_trivial() {
         SuperSeries s;
         ParserResultElement<SuperSeries> p(s);
         std::stringstream str;
         const std::string test1 = "abc123ABC";
         str << test1;
-        assert(boost::spirit::parse(str.str().c_str(), p.parser()).full);
-        assert(s.name() == test1);
+	if (not boost::spirit::parse(str.str().c_str(), p.parser()).full)
+	  throw ::OKlib::TestSystem::TestException("String " + test1 + " was not accepted.").add(OKLIB_TESTDESCRIPTION);
+	// ToDo: Using module Messages
+        if(s.name() != test1)
+	  throw ::OKlib::TestSystem::TestException("Resulting name is " + s.name() + ", and not " + test1).add(OKLIB_TESTDESCRIPTION);
       }
     };
 
