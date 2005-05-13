9a10e7d04cb5dafcb81e8ee7a68f920224cf1523
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-13 16:18:57 +0000

    Now uses TestBase::run_tests_default.

--- SAT-Competition/TestSAT-Competition.cpp
+++ SAT-Competition/TestSAT-Competition.cpp
@@ -1,3 +1,9 @@
-#include "SingleResult.hpp"
+#include <iostream>
 
-int main() {} 
+#include "TestBaseClass.hpp"
+
+#include "ParsingSingleResult_Testobjects.hpp"
+
+int main() {
+  OKlib::TestSystem::TestBase::run_tests_default(std::cout);
+} 
