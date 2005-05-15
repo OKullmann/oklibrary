e70b06d48b14e0cb4ac29c2759c58b35deaf2388
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-15 21:12:28 +0000

    *** empty log message ***

---
+++ HypergraphColouring/TestHypergraphColouring.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "Van_der_Waerden_hypergraph_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cout);
+}
