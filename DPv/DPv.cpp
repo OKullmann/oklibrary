ee4136a53a56394b6cd2fd8df9b4eee52910cc41
Tony Bao <cstony@Swansea.ac.uk>
2005-05-19 23:08:48 +0000

    Tested out the constructors for Variable, Literal and Clause classes.

--- DPv/DPv.cpp
+++ DPv/DPv.cpp
@@ -5,5 +5,9 @@
 #include "Statistics.hpp"
 
 int main(const int argc, const char* argv[]) {
+
+  OKlib::DPv::Variable v;
+  OKlib::DPv::Literal l(v);
+  OKlib::DPv::Clause C();
   return EXIT_SUCCESS;
 }
