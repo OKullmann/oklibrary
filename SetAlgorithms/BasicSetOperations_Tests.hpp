32c424d51d29493b6410cd9c1781f2174288149b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-20 12:34:29 +0000

    Now basic checks are there, but partially commented out due to an internal compiler error with g++ 3.4.3.

--- SetAlgorithms/BasicSetOperations_Tests.hpp
+++ SetAlgorithms/BasicSetOperations_Tests.hpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <utility>
 
+#include <boost/lexical_cast.hpp>
+
 #include "TestBaseClass.hpp"
 #include "TestExceptions.hpp"
 
@@ -15,14 +17,15 @@ namespace OKlib {
 
   namespace SetAlgorithms {
 
-    template <template <typename InputIterator_sets, typename InputIterator_elements, typename OutputIterator> class Intersection>
-    class Test_Intersection : public ::OKlib::TestSystem::TestBase {
+    template <template <typename InputIterator_sets, typename InputIterator_elements, typename OutputIterator> class Union>
+    class Test_Union : public ::OKlib::TestSystem::TestBase {
     public :
-      typedef Test_Intersection test_type;
-      Test_Intersection() {
+      typedef Test_Union test_type;
+      Test_Union() {
         insert(this);
       }
     private :
+
       void perform_test_trivial() {
         {
           typedef int value_type;
@@ -33,17 +36,41 @@ namespace OKlib {
           typedef VectorSets::iterator vector_iterator;
           typedef std::vector<value_type> VectorValues;
           typedef VectorValues::iterator value_iterator;
-          typedef Intersection<vector_iterator, set_iterator, value_iterator> intersection_type;
+          typedef Union<vector_iterator, set_iterator, value_iterator> union_type;
 
-          intersection_type intersection;
+          union_type union_f;
+
+          struct check_union : public ::OKlib::TestSystem::TestBase {
+            typedef check_union test_type;
+            VectorSets& sets;
+            VectorValues& output;
+            Set& reference;
+            check_union(VectorSets& sets, VectorValues& output, Set& reference) : sets(sets), output(output), reference(reference) {}
+            void perform_test_trivial() {
+              if (sets.size() > output.size()) output.resize(sets.size());
+//               const value_iterator end = union_f(sets.begin(), sets.end(), output.begin());
+//               const VectorValues::size_type size = end - output.begin();
+//               if (size != reference.size())
+//                 throw ::OKlib::TestSystem::TestException("Size is " + boost::lexical_cast<std::string>(size) + ", and not " + boost::lexical_cast<std::string>(reference.size())).add(OKLIB_TESTDESCRIPTION);
+            }
+          };
 
           VectorSets sets;
           VectorValues output;
+          Set reference;
 
-          if (sets.size() > output.size())
-            output.resize(sets.size());
-          const value_iterator end = intersection(sets.begin(), sets.end(), output.begin());
-          // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+          check_union check(sets, output, reference);            
+
+          OKLIB_TESTTRIVIAL_RETHROW(check);
+
+          Set s1; s1.insert(2); s1.insert(4); s1.insert(6);
+          sets.push_back(Range(s1.begin(), s1.end()));
+          
+
+          Set s2; s2.insert(2); s2.insert(-4); s2.insert(6);
+          Set s3; s3.insert(3); s3.insert(-4); s3.insert(5);
+           sets.push_back(Range(s2.begin(), s2.end())); sets.push_back(Range(s3.begin(), s3.end()));
+
         }
       }
     };
