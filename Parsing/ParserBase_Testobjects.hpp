1685cdcd7c16a471cd874a30c83321d0f6dc5aa8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-24 21:09:00 +0000

    Yet only including files.

---
+++ Parsing/ParserBase_Testobjects.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 24.5.2005 (Swansea)
+
+#ifndef PARSERBASETESTOBJECTS_74719nHap
+
+#define PARSERBASETESTOBJECTS_74719nHap
+
+#include "ParserBase.hpp"
+#include "ParserBase_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Parser {
+
+  }
+
+}
+#endif
