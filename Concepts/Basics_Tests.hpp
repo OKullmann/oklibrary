9317a8c8616c4fd7c9f11e6374a0ed3b6de3a37f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-24 15:46:22 +0000

    Testing archetypes.

---
+++ Concepts/Basics_Tests.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 24.5.2005 (Swansea)
+
+#ifndef BASICSTESTS_oLzW151
+
+#define BASICSTESTS_oLzW151
+
+#include "Basics.hpp"
+
+namespace OKlib {
+
+  namespace Concepts {
+
+    namespace Archetype_tests {
+
+      template class FullyEqualityComparable<FullyEqualityComparable_Archetype>;
+
+      template class FullyLessThanComparable<FullyLessThanComparable_Archetype>;
+
+      template class LinearOrder<LinearOrder_Archetype>;
+
+      template class EqualitySubstitutable<EqualitySubstitutable_Archetype>;
+    }
+
+  }
+
+}
+
+#endif
