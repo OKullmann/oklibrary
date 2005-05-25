1aa4083d595687f707f638d46a9c5b2e2c3a5dfa
Tony Bao <cstony@Swansea.ac.uk>
2005-05-25 11:25:57 +0000

    Clean up and correction.

--- DPv/Statistics.hpp
+++ DPv/Statistics.hpp
@@ -30,26 +30,17 @@ namespace OKlib {
       return clause_count(DP_reduction(v,cls));
     }
     
-    /* 
-    template <class charT, class traits>
-    std::basic_ostream<charT,traits>& operator << (std::basic_ostream<charT,traits>& os, const Clause_set& obj){
-      VariableSet vs(obj);
-      os << "c(F) = " << clause_count(obj) << '\n';
-      for (VariableSet::const_iterator i = vs.vars.begin(); i != vs.vars.end(); ++i) {
-	os << "vdg_" << (*i).v << "(F) = " << vdg(*i, obj) << '\t' << "ldg_" << (*i).v << "(F) = " << ldg(Literal((*i).v)) << '\t' << "ldg_" << ((*i).v * -1)  << "(F) = " << ldg(literal_compliment(Literal((*i).v))) << '\n';
-	os << "c(DP_" << (*i).v << "(F)) = " << clause_left(DP_reduction(*i, obj)) << '\n';
-      }
-    }
-    */
     
-    
-    void stat(const Clause_set& obj){
-      VariableSet vs(obj);
-      std::cout << "c(F) = " << clause_count(obj) << '\n';
+    template <typename T>
+    T& operator <<(T& os, const Clause_set& cls){
+      VariableSet vs(cls);
+      os << "c(F) = " <<clause_count(cls) << '\n';
       for (VariableSet::const_iterator i = vs.vars.begin(); i != vs.vars.end(); ++i) {
-	std::cout << "vdg_" << (*i).v << "(F) = " << vdg(*i, obj) << '\t' << "ldg_" << (*i).v << "(F) = " << ldg(Literal((*i).v),obj) << '\t' << "ldg_" << ((*i).v * -1)  << "(F) = " << ldg(literal_compliment(Literal((*i).v)), obj) << '\t' << "c(DP_" << (*i).v << "(F)) = " << clause_left(*i, obj) << '\n';
+	os << "vdg_" << (*i).v << "(F) = " << vdg(*i, cls) << '\t' << "ldg_" << (*i).v << "(F) = " << ldg(Literal((*i).v), cls) << '\t' << "ldg_" << ((*i).v * -1)  << "(F) = " << ldg(literal_compliment(Literal((*i).v)), cls) << '\t' << "c(DP_" << (*i).v << "(F)) = " << clause_left(*i, cls) << '\n';
       }
+      return os;
     }
+
   }
 }
 #endif
