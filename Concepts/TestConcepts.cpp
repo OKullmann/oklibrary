033fd16416aa5a6ced30e34808d62bcce928d5c5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-25 14:22:41 +0000

    Now also handling testobjects (actually, should handle only testobjects).

--- Concepts/TestConcepts.cpp
+++ Concepts/TestConcepts.cpp
@@ -1,8 +1,17 @@
 // Oliver Kullmann, 15.5.2005
 
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
 #include "std_Iterators_Tests.hpp"
 #include "Iterators_Tests.hpp"
 #include "std_Basics_Tests.hpp"
 #include "Basics_Tests.hpp"
 
-int main() {}
+#include "ConceptsMetafunctions_Testobjects.hpp"
+#include "LibraryBasics_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+}
