f7aa397b1b738f77d28278afb13fb24fda98d3b1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:36:55 +0000

    Comments added, and two new macros OKLIB_BASIC_CONCEPT_TEST_TAG and OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE defined: Only by those macros shall concept definitions be tested.

--- Concepts/LibraryBasics_Tests.hpp
+++ Concepts/LibraryBasics_Tests.hpp
@@ -4,9 +4,19 @@
 
 #define LIBRARYBASICSTESTS_rttjkN00
 
+#include <boost/static_assert.hpp>
+#include <boost/type_traits.hpp>
+// This file is guaranteed to include the last two files, so that OKLIB_BASIC_CONCEPT_TEST always works.
+
+#include "ConceptsMetafunctions.hpp"
+
 namespace OKlib {
 
   namespace Concepts {
+
+#define OKLIB_BASIC_CONCEPT_TEST_TAG(concept) BOOST_STATIC_ASSERT(IsConceptTag< concept ## _tag >::value);
+
+#define OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(concept) template class concept< concept ## _Archetype >;
 
   }
 
