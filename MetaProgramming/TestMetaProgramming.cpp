fb7446a1ac6cc0dfd5f508e27ae8e6fb90b34bfb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:25:21 +0000

    Standard testprogram.

---
+++ MetaProgramming/TestMetaProgramming.cpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 26.5.2005
+
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "Logical_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+}
