72ab5007b798ed1db081e08d1a353343abc45537
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:39:41 +0000

    Some editing, and further metafunctions for the handling of concepts and concept tags.

--- Concepts/ConceptsMetafunctions.hpp
+++ Concepts/ConceptsMetafunctions.hpp
@@ -19,21 +19,38 @@ namespace OKlib {
       typedef typename T::concept_tag type;
     };
 
+    // -------------------------------------------------------------------------------------------------------------------
+
     template <class T, class concept_tag>
-    class IsModel {
+    class IsTagModel {
       typedef typename ConceptTag<T>::type t_concept_tag;
     public :
       typedef typename 
       ::boost::mpl::or_< ::boost::is_same<t_concept_tag, concept_tag>, ::boost::is_base_and_derived<concept_tag, t_concept_tag> >::type type;
+      typedef typename type::value_type value_type;
+      static const value_type value = type::value;
+    };
+
+     // -------------------------------------------------------------------------------------------------------------------
+
+    template <class Tag>
+    struct IsConceptTag {
+      typedef typename
+      ::boost::is_base_and_derived< ::OKlib::Concepts::ConceptsBase_tag, Tag>::type type;
+      typedef typename type::value_type value_type;
+      static const value_type value = type::value;
     };
 
     template <class T>
-    class HasConcept {
+    class HasConceptTag {
       typedef typename ConceptTag<T>::type t_concept_tag;
     public :
       typedef typename
-      ::boost::is_base_and_derived< ::OKlib::Concepts::ConceptsBase, t_concept_tag>::type type;
+      IsConceptTag<t_concept_tag>::type type;
+      typedef typename type::value_type value_type;
+      static const value_type value = type::value;
     };
+
   }
 
 };
