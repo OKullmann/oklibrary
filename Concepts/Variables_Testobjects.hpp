acc0baf9d698b72466c28c115dbae86642d266d4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:48:15 +0000

    Concept tests for concept Variables.

---
+++ Concepts/Variables_Testobjects.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 26.5.2005
+
+#ifndef VARIABLESTESTOBJECTS_olKmNb56
+
+#define VARIABLESTESTOBJECTS_olKmNb56
+
+#include "LibraryBasics_Tests.hpp"
+
+#include "Variables.hpp"
+#include "Variables_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Concepts {
+
+    OKLIB_BASIC_CONCEPT_TEST_TAG(Variables);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(Variables);
+
+
+  }
+
+}
+
+#endif
