955f71accf09989b4522f762dbb0e399521488e9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:38:06 +0000

    New -testobjects-files.

--- Concepts/TestConcepts.cpp
+++ Concepts/TestConcepts.cpp
@@ -4,13 +4,13 @@
 
 #include "TestBaseClass.hpp"
 
-#include "std_Iterators_Tests.hpp"
-#include "Iterators_Tests.hpp"
-#include "std_Basics_Tests.hpp"
-#include "Basics_Tests.hpp"
-
+#include "std_Iterators_Testobjects.hpp"
+#include "Iterators_Testobjects.hpp"
+#include "std_Basics_Testobjects.hpp"
+#include "Basics_Testobjects.hpp"
 #include "ConceptsMetafunctions_Testobjects.hpp"
 #include "LibraryBasics_Testobjects.hpp"
+#include "Variables_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
