5763da6e1eb799f777f796f4e7ba6c62f3a96112
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:27:42 +0000

    Testobjects for the constant true and false metafunctions.

---
+++ MetaProgramming/Logical_Testobjects.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 26.5.2005 (Swansea)
+
+#ifndef LOGICALTESTOBJECTS_847ncVb
+
+#define LOGICALTESTOBJECTS_847ncVb
+
+#include "Logical.hpp"
+#include "Logical_Tests.hpp"
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+    namespace Metafunction_tests {
+
+      template class Test_True_T<True>;
+
+      template class Test_False_T<False>;
+    }
+  }
+
+}
+
+#endif
