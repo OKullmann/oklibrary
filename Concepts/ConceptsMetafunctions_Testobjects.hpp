d1c5a71df6bf29a2561fff096ae90e95fe4bf2df
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-05-26 15:41:18 +0000

    Standard tests for metafunctions.

--- Concepts/ConceptsMetafunctions_Testobjects.hpp
+++ Concepts/ConceptsMetafunctions_Testobjects.hpp
@@ -11,6 +11,18 @@ namespace OKlib {
 
   namespace Concepts {
 
+    namespace Metafunction_tests {
+
+      template class Test_ConceptTag_T<ConceptTag>;
+
+      template class Test_IsTagModel_T_concept_tag<IsTagModel>;
+
+      template class Test_IsConceptTag_Tag<IsConceptTag>;
+
+      template class Test_HasConceptTag_T<HasConceptTag>;
+
+    }
+
   }
 
 };
