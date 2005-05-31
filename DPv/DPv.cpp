17bb2334947a251e27f8b004fd80e15f20e210cd
Tony Bao <cstony@Swansea.ac.uk>
2005-05-31 17:23:50 +0000

    Application program which takes exactly on cnf formula in DIAMCS format and computes the statistics relevant to the DP-procedure and allows the user to choose a reduction sequence. This version still have formatting issues and needs further improvements.

--- DPv/DPv.cpp
+++ DPv/DPv.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 #include "DPv.hpp"
 #include "Statistics.hpp"
@@ -7,5 +8,43 @@
 #include "BasicDataStructure.hpp"
 
 int main(const int argc, const char* const argv[]){
+  
+  if (argc != 2) { 
+    std::cout << "[DPv] Error: Program requires exact one cnf input \n";
+    return EXIT_FAILURE;
+  }
+  
+  const std::string filename = argv[1];
+  
+  
+  OKlib::DPv::Input::FStream input(filename.c_str());
+  
+  OKlib::DPv::Dimacs data;
+  input.filestream >> data; 
+  
+  std::cout << "F = "; 
+  data.formula.show(std::cout); 
+  std::cout<<"\n";
+  
+  OKlib::DPv::stat_type s1;
+  OKlib::DPv::VariableSet vs1(data.formula);
+  OKlib::DPv::Best_order(OKlib::DPv::DPv_opt_stats(vs1, data.formula,s1));
 
+  int response = 9;
+  int empty = 9;
+  
+  OKlib::DPv::Clause_set result;
+  result.cs = data.formula.cs;
+  OKlib::DPv::VariableSet vs(result);
+  empty = vs.vars.size();
+  while (empty != 0 and response != 0) {
+    std::cout << "Variables: "; vs.show(std::cout); std::cout<< '\n';
+    std::cout << result;
+    std::cout << "Pick a Variable: ";
+    std::cin >> response;
+    result.cs = DP_reduction(OKlib::DPv::Variable(response), result).cs;
+    vs.vars = OKlib::DPv::VariableSet(result).vars;
+    empty = vs.vars.size();
+    std::cout << "F = "; result.show(std::cout); std::cout<<"\n";
+  }
 }
