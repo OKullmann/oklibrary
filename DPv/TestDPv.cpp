d63017c0cb40a193236f3471b2b8e3c101a5d028
Tony Bao <cstony@Swansea.ac.uk>
2005-06-01 15:26:05 +0000

    Now include standard testing files for sub-module BasicDataStructure and Statistics.

--- DPv/TestDPv.cpp
+++ DPv/TestDPv.cpp
@@ -3,6 +3,8 @@
 #include "TestBaseClass.hpp"
 
 #include "Input_output_Testobjects.hpp"
+#include "BasicDataStructure_Testobjects.hpp"
+#include "Statistics_Testobjects.hpp"
 
 int main(const int argc, const char* const argv[]) {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
