3b68b16f0b203f0d9bead86b08de704276d9d098
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-02 10:26:45 +0000

    Missing include's added, and usage of OKLIB_THROW now.

--- Parsing/ParserBase_Tests.hpp
+++ Parsing/ParserBase_Tests.hpp
@@ -4,6 +4,9 @@
 
 #define PARSERBASETESTS_jfbNb5y
 
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
 namespace OKlib {
 
   namespace Parser {
@@ -23,12 +26,12 @@ namespace OKlib {
         switch (match) {
         case match_full:
           if (not boost::spirit::parse(test_string.c_str(), parser.parser()).full)
-            throw ::OKlib::TestSystem::TestException("String " + test_string + " was not accepted.").add(OKLIB_TESTDESCRIPTION);
+            OKLIB_THROW("String " + test_string + " was not accepted.");
             // ToDo: Using Messages
           break;
         case (match_not_full) :
           if (boost::spirit::parse(test_string.c_str(), parser.parser()).full) {
-            throw ::OKlib::TestSystem::TestException("String " + test_string + " was accepted.").add(OKLIB_TESTDESCRIPTION);
+            OKLIB_THROW("String " + test_string + " was accepted.");
             // ToDo: Using Messages
             break;
           }
