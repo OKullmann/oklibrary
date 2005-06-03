16a6a1081d9bfe8b9a1323433b81f7cae0f13a9a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-03 16:51:52 +0000

    Formatting.

--- SAT-Competition/ResultProcessing_Tests.hpp
+++ SAT-Competition/ResultProcessing_Tests.hpp
@@ -104,60 +104,62 @@ namespace OKlib {
         typedef ParserResult<Result> Parser;
         typedef std::vector<TupleResult> VectorTupleResult;
         VectorTupleResult pre_test_vector;
-        add_positive_results(pre_test_vector);
-        const VectorTupleResult::size_type pre_test_vector_size = pre_test_vector.size();
+        {
+          add_positive_results(pre_test_vector);
+          const VectorTupleResult::size_type pre_test_vector_size = pre_test_vector.size();
 
-        std::stringstream input;
-        std::copy(pre_test_vector.begin(), pre_test_vector.end(), std::ostream_iterator<TupleResult>(input, "\n"));
+          std::stringstream input;
+          std::copy(pre_test_vector.begin(), pre_test_vector.end(), std::ostream_iterator<TupleResult>(input, "\n"));
 
-        typedef std::vector<Result> VectorResult;
-        VectorResult test_vector;
-        assert(copy_results<Parser>(input.str().c_str(), std::back_inserter(test_vector)).full);
-        const VectorResult::size_type test_vector_size = test_vector.size();
-        assert(test_vector_size == pre_test_vector_size);
+          typedef std::vector<Result> VectorResult;
+          VectorResult test_vector;
+          assert(copy_results<Parser>(input.str().c_str(), std::back_inserter(test_vector)).full);
+          const VectorResult::size_type test_vector_size = test_vector.size();
+          assert(test_vector_size == pre_test_vector_size);
 
-        std::set<SuperSeries> ref_set_super_series;
-        std::set<Series> ref_set_series;
-        std::set<Benchmark> ref_set_benchmark;
-        std::set<Solver> ref_set_solver;
-        std::set<SATStatus> ref_set_sat_status;
-        std::set<TimeOut> ref_set_time_out;
+          std::set<SuperSeries> ref_set_super_series;
+          std::set<Series> ref_set_series;
+          std::set<Benchmark> ref_set_benchmark;
+          std::set<Solver> ref_set_solver;
+          std::set<SATStatus> ref_set_sat_status;
+          std::set<TimeOut> ref_set_time_out;
 
-        for (VectorResult::const_iterator i = test_vector.begin(); i != test_vector.end(); ++i) {
-          ref_set_super_series.insert(i -> super_series());
-          ref_set_series.insert(i -> series());
-          ref_set_benchmark.insert(i -> benchmark());
-          ref_set_solver.insert(i -> solver());
-          ref_set_sat_status.insert(i -> sat_status());
-          ref_set_time_out.insert(i -> time_out());
+          for (VectorResult::const_iterator i = test_vector.begin(); i != test_vector.end(); ++i) {
+            ref_set_super_series.insert(i -> super_series());
+            ref_set_series.insert(i -> series());
+            ref_set_benchmark.insert(i -> benchmark());
+            ref_set_solver.insert(i -> solver());
+            ref_set_sat_status.insert(i -> sat_status());
+            ref_set_time_out.insert(i -> time_out());
+          }
+
+          typedef ResultDatabase<VectorResult::const_iterator> ResultDatabase_vector;
+          ResultDatabase_vector db(test_vector.begin(), test_vector.end());
+
+          if (not db.number_results() == test_vector_size)
+            OKLIB_THROW("Element count is " + boost::lexical_cast<std::string>(db.number_results()) + ", and not " + boost::lexical_cast<std::string>(test_vector_size));
+
+          const MapSuperSeries& super_series(db.super_series());
+          const MapSeries& series(db.series());
+          const MapBenchmark& benchmark(db.benchmark());
+          const MapSolver& solver(db.solver());
+          const MapSATStatus& sat_status(db.sat_status());
+          const MapTimeOut& time_out(db.time_out());
+
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(super_series, ref_set_super_series)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(series, ref_set_series)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(benchmark, ref_set_benchmark)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(solver, ref_set_solver)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(sat_status, ref_set_sat_status)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_set(time_out, ref_set_time_out)));
+
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(super_series, test_vector_size)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(series, test_vector_size)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(benchmark, test_vector_size)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(solver, test_vector_size)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(sat_status, test_vector_size)));
+          OKLIB_TESTTRIVIAL_RETHROW((check_sizes(time_out, test_vector_size)));
         }
-
-        typedef ResultDatabase<VectorResult::const_iterator> ResultDatabase_vector;
-        ResultDatabase_vector db(test_vector.begin(), test_vector.end());
-
-        if (not db.number_results() == test_vector_size)
-          OKLIB_THROW("Element count is " + boost::lexical_cast<std::string>(db.number_results()) + ", and not " + boost::lexical_cast<std::string>(test_vector_size));
-
-        const MapSuperSeries& super_series(db.super_series());
-        const MapSeries& series(db.series());
-        const MapBenchmark& benchmark(db.benchmark());
-        const MapSolver& solver(db.solver());
-        const MapSATStatus& sat_status(db.sat_status());
-        const MapTimeOut& time_out(db.time_out());
-
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(super_series, ref_set_super_series)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(series, ref_set_series)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(benchmark, ref_set_benchmark)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(solver, ref_set_solver)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(sat_status, ref_set_sat_status)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_set(time_out, ref_set_time_out)));
-
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(super_series, test_vector_size)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(series, test_vector_size)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(benchmark, test_vector_size)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(solver, test_vector_size)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(sat_status, test_vector_size)));
-        OKLIB_TESTTRIVIAL_RETHROW((check_sizes(time_out, test_vector_size)));
       }
 
     };
