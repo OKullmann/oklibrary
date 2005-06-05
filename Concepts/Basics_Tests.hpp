271879dd198f0f229dd59e5f97c1ecc34c47a4a9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:54:08 +0000

    Axioms and basic_test's for the concepts.

--- Concepts/Basics_Tests.hpp
+++ Concepts/Basics_Tests.hpp
@@ -4,12 +4,197 @@
 
 #define BASICSTESTS_oLzW151
 
-#include "Basics.hpp"
+#include <cassert>
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
 
 namespace OKlib {
 
   namespace Concepts {
 
+    template <typename T>
+    struct FullyEqualityComparable_Axiom_positive : OKlib::TestSystem::Test {
+      typedef FullyEqualityComparable_Axiom_positive test_type;
+      FullyEqualityComparable_Axiom_positive(const T& a, const T& b) : a(a), b(b) {
+        assert(a == b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (a != b)
+          OKLIB_THROW("a != b");
+      }
+    };
+    
+    template <typename T>
+    struct FullyEqualityComparable_Axiom_negative : OKlib::TestSystem::Test {
+      typedef FullyEqualityComparable_Axiom_negative test_type;
+      FullyEqualityComparable_Axiom_negative(const T& a, const T& b) : a(a), b(b) {
+        assert(a != b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (a == b))
+          OKLIB_THROW("a == b");
+      }
+    };
+
+    template <typename T>
+    struct FullyEqualityComparable_basic_test_one_object : OKlib::TestSystem::Test {
+      typedef FullyEqualityComparable_basic_test_one_object test_type;
+      FullyEqualityComparable_basic_test_one_object(const T& x) : x(x) {}
+    private :
+      const T& x;
+      void perform_test_trivial() {
+        OKLIB_TESTTRIVIAL_RETHROW(FullyEqualityComparable_Axiom_positive<T>(x, x));
+      }
+    };
+
+    // ----------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_greater_positive : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_greater_positive test_type;
+      FullyLessThanComparable_Axiom_greater_positive(const T& a, const T& b) : a(a), b(b) {
+        assert(b < a);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (a > b))
+          OKLIB_THROW("not a > b");
+      }
+    };
+
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_greater_negative : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_greater_negative test_type;
+      FullyLessThanComparable_Axiom_greater_negative(const T& a, const T& b) : a(a), b(b) {
+        assert(a > b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (b < a))
+          OKLIB_THROW("not b < a");
+      }
+    };
+  
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_less_equal_positive : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_less_equal_positive test_type;
+      FullyLessThanComparable_Axiom_less_equal_positive(const T& a, const T& b) : a(a), b(b) {
+        assert(not (a > b));
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (a <= b))
+          OKLIB_THROW("not a <= b");
+      }
+    };
+
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_less_equal_negative : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_less_equal_negative test_type;
+      FullyLessThanComparable_Axiom_less_equal_negative(const T& a, const T& b) : a(a), b(b) {
+        assert(a <= b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (a > b)
+          OKLIB_THROW("not not a > b");
+      }
+    };
+
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_greater_equal_positive : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_greater_equal_positive test_type;
+      FullyLessThanComparable_Axiom_greater_equal_positive(const T& a, const T& b) : a(a), b(b) {
+        assert(b <= a);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (a >= b))
+          OKLIB_THROW("not a >= b");
+      }
+    };
+
+    template <typename T>
+    struct FullyLessThanComparable_Axiom_greater_equal_negative : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_Axiom_greater_equal_negative test_type;
+      FullyLessThanComparable_Axiom_greater_equal_negative(const T& a, const T& b) : a(a), b(b) {
+        assert(a >= b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (b <= a))
+          OKLIB_THROW("not b <= a");
+      }
+    };
+
+    template <typename T>
+    struct FullyLessThanComparable_basic_test_one_object : OKlib::TestSystem::Test {
+      typedef FullyLessThanComparable_basic_test_one_object test_type;
+      FullyLessThanComparable_basic_test_one_object(const T& x) : x(x) {}
+    private :
+      const T& x;
+      void perform_test_trivial() {
+        OKLIB_TESTTRIVIAL_RETHROW(FullyLessThanComparable_Axiom_less_equal_positive<T>(x, x));
+        OKLIB_TESTTRIVIAL_RETHROW(FullyLessThanComparable_Axiom_less_equal_negative<T>(x, x));
+        OKLIB_TESTTRIVIAL_RETHROW(FullyLessThanComparable_Axiom_greater_equal_positive<T>(x, x));
+        OKLIB_TESTTRIVIAL_RETHROW(FullyLessThanComparable_Axiom_greater_equal_negative<T>(x, x));
+      }
+    };
+
+    // ----------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct LinearOrder_Axiom_positive : OKlib::TestSystem::Test {
+      typedef LinearOrder_Axiom_positive test_type;
+      LinearOrder_Axiom_positive(const T& a, const T& b) : a(a), b(b) {
+        assert(not (a < b) and not (b < a));
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (not (a == b))
+          OKLIB_THROW("not a == b");
+      }
+    };
+
+    template <typename T>
+    struct LinearOrder_Axiom_negative : OKlib::TestSystem::Test {
+      typedef LinearOrder_Axiom_negative test_type;
+      LinearOrder_Axiom_negative(const T& a, const T& b) : a(a), b(b) {
+        assert(a == b);
+      }
+    private :
+      const T& a, b;
+      void perform_test_trivial() {
+        if (a < b or b < a)
+          OKLIB_THROW("a < b or b < a");
+      }
+    };
+
+    template <typename T>
+    struct LinearOrder_basic_test_one_object : OKlib::TestSystem::Test {
+      typedef LinearOrder_basic_test_one_object test_type;
+      LinearOrder_basic_test_one_object(const T& x) : x(x) {}
+    private :
+      const T& x;
+      void perform_test_trivial() {
+        OKLIB_TESTTRIVIAL_RETHROW(FullyLessThanComparable_basic_test_one_object<T>(x));
+        OKLIB_TESTTRIVIAL_RETHROW(FullyEqualityComparable_basic_test_one_object<T>(x));
+        OKLIB_TESTTRIVIAL_RETHROW(LinearOrder_Axiom_negative<T>(x, x));
+      }
+    };
+    
   }
 
 }
--- Concepts/Variables_Tests.hpp
+++ Concepts/Variables_Tests.hpp
@@ -4,9 +4,168 @@
 
 #define VARIABLESTESTS_kkgFre3
 
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+#include "LibraryBasics.hpp"
+#include "Basics_Tests.hpp"
+#include "Traits.hpp"
+
+#include "Variables.hpp"
+
 namespace OKlib {
 
   namespace Concepts {
+
+    template <typename Var>
+    struct Variables_Axiom_singular_variables_identity : OKlib::TestSystem::Test {
+      typedef Variables_Axiom_singular_variables_identity test_type;
+      Variables_Axiom_singular_variables_identity(const Var& v, const Var& w) : v(v), w(w) {
+        assert(not v);
+        assert(not w);
+      }
+    private :
+      const Var& v; const Var& w;
+      void perform_test_trivial() {
+        if (v != w)
+          OKLIB_THROW("v != w");
+      }
+    };
+
+    template <typename Var>
+    struct Variables_Axiom_singular_variables_false : OKlib::TestSystem::Test {
+      typedef Variables_Axiom_singular_variables_false test_type;
+    private :
+      void perform_test_trivial() {
+        if (Var())
+          OKLIB_THROW("not not bool(Var())");
+      }
+    };
+
+    template <typename Var>
+    struct Variables_basic_test : OKlib::TestSystem::TestBase {
+      typedef Variables_basic_test test_type;
+      Variables_basic_test() {
+        insert(this);
+      }
+    private :
+      void perform_test_trivial() {
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, Variables);
+        OKLIB_TESTTRIVIAL_RETHROW(LinearOrder_basic_test_one_object<Var>(Var()));
+        OKLIB_TESTTRIVIAL_RETHROW(Variables_Axiom_singular_variables_false<Var>());
+      }
+    };
+
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    template <typename Var>
+    struct VariablesWithIndex_Axiom_index_nonnegative : OKlib::TestSystem::Test {
+      typedef VariablesWithIndex_Axiom_index_nonnegative test_type;
+      VariablesWithIndex_Axiom_index_nonnegative(const Var& v) : v(v) {}
+    private :
+      const Var& v;
+      typedef typename index_type<Var>::type index_type;
+      void perform_test_trivial() {
+        if (index_type(v) < 0)
+          OKLIB_THROW("index(v) < 0");
+      }
+    };
+  
+    template <typename Var>
+    struct VariablesWithIndex_Axiom_index_zero_positive : OKlib::TestSystem::Test {
+      typedef VariablesWithIndex_Axiom_index_zero_positive test_type;
+      VariablesWithIndex_Axiom_index_zero_positive(const Var& v) : v(v) {
+        assert(not v);
+      }
+    private :
+      const Var& v;
+      typedef typename index_type<Var>::type index_type;
+      void perform_test_trivial() {
+        if (index_type(v) != 0)
+          OKLIB_THROW("index(v) != 0");
+      }
+    };
+
+    template <typename Var>
+    struct VariablesWithIndex_Axiom_index_zero_negative : OKlib::TestSystem::Test {
+      typedef VariablesWithIndex_Axiom_index_zero_negative test_type;
+      VariablesWithIndex_Axiom_index_zero_negative(const Var& v) : v(v) {
+        typedef typename index_type<Var>::type index_type;
+        assert(index_type(v) == 0);
+      }
+    private :
+      const Var& v;
+      void perform_test_trivial() {
+        if (bool(v))
+          OKLIB_THROW("not not bool(v)");
+      }
+    };
+
+    template <typename Var>
+    struct VariablesWithIndex_Axiom_index_identity : OKlib::TestSystem::Test {
+      typedef VariablesWithIndex_Axiom_index_identity test_type;
+      VariablesWithIndex_Axiom_index_identity(const Var& v, const Var& w) : v(v), w(w) {
+        typedef typename index_type<Var>::type index_type;
+        assert(index_type(v) == index_type(w));
+      }
+    private :
+      const Var& v; const Var& w;
+      void perform_test_trivial() {
+        if (v != w)
+          OKLIB_THROW("v != w");
+      }
+    };
+
+    template <typename Var>
+    struct VariablesWithIndex_basic_test : OKlib::TestSystem::TestBase {
+      typedef VariablesWithIndex_basic_test test_type;
+      VariablesWithIndex_basic_test() {
+        insert(this);
+      }
+    private :
+      void perform_test_trivial() {
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, VariablesWithIndex);
+        OKLIB_TESTTRIVIAL_RETHROW(Variables_basic_test<Var>());
+        OKLIB_TESTTRIVIAL_RETHROW(VariablesWithIndex_Axiom_index_zero_positive<Var>(Var()));
+      }
+    };
+
+    // ---------------------------------------------------------------------------------------------------------------------
+
+    template <typename Var>
+    struct VariablesAsIndex_Axiom : OKlib::TestSystem::Test {
+      typedef VariablesAsIndex_Axiom test_type;
+      typedef typename index_type<Var>::type index_type;
+      VariablesAsIndex_Axiom(const index_type& i) : i(i) {
+        assert(i >= 0);
+      }
+    private :
+      const index_type& i;
+      void perform_test_trivial() {
+        if (index_type(Var(i)) != i)
+          OKLIB_THROW("index(Var(i)) != i");
+      }
+    };
+
+    template <typename Var>
+    struct VariablesAsIndex_basic_test : OKlib::TestSystem::TestBase {
+      typedef VariablesAsIndex_basic_test test_type;
+      VariablesAsIndex_basic_test() {
+        insert(this);
+      }
+    private :
+      typedef typename index_type<Var>::type index_type;
+      void perform_test_trivial() {
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, VariablesAsIndex);
+        OKLIB_TESTTRIVIAL_RETHROW(VariablesWithIndex_basic_test<Var>());
+        {
+          const index_type max_index = 2;
+          for (index_type i = 0; i <= max_index; ++i)
+            OKLIB_TESTTRIVIAL_RETHROW(VariablesAsIndex_Axiom<Var>(i));
+        }
+        // ToDo: With the creation power now also testing all other axioms.
+      }
+    };
 
   }
 
