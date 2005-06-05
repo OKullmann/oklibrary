2b48a8c0e8bcfd7214860680fecaac40190a8086
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:27:03 +0000

    Standard.

---
+++ SetAlgorithms/BasicMapOperations_Testobjects.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 4.6.2005 (Swansea)
+
+#ifndef BASICMAPOPERATIONSTESTOBJECTS_alakah66
+
+#define BASICMAPOPERATIONSTESTOBJECTS_alakah66
+
+#include "BasicMapOperations.hpp"
+#include "BasicMapOperations_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SetAlgorithms {
+
+    namespace Testobjects {
+
+      Test_MapValue<MapValue> test_map_value;
+
+    }
+  }
+
+}
+
+#endif
