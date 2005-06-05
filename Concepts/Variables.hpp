b3260814fd367099fa9381cd6e0432b90da20afc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:53:23 +0000

    Corrections and editions for concepts Variables, and new concepts VariablesWithIndex and VariablesAsIndex.

--- Concepts/Variables.hpp
+++ Concepts/Variables.hpp
@@ -2,15 +2,19 @@
 
 #ifndef VARIABLES_095tYb
 
-#define VARIABLES _095tYb
+#define VARIABLES_095tYb
+
+#include "TypeTraits.hpp"
 
 #include "LibraryBasics.hpp"
+#include "Traits.hpp"
 
 namespace OKlib {
 
   namespace Concepts {
 
-    struct Variables_tag : virtual BasicRequirements_tag, FullyConstructible_tag {};
+    struct Variables_tag : virtual BasicRequirements_tag, virtual FullyConstructible_tag,  virtual EqualitySubstitutable_tag, virtual LinearOrder_tag {};
+
     template <typename Var>
     struct Variables {
       void constraints() {
@@ -21,9 +25,15 @@ namespace OKlib {
         OKLIB_MODELS_CONCEPT_REQUIRES(Var, FullyConstructible);
         OKLIB_MODELS_CONCEPT_TAG(Var, FullyConstructible);
         OKLIB_MODELS_CONCEPT_REQUIRES(Var, EqualitySubstitutable);
-        OKLIB_MODELS_CONCEPT_TAG(Var, FullyConstructible);
-        // ToDo: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        OKLIB_MODELS_CONCEPT_TAG(Var, EqualitySubstitutable);
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, LinearOrder);
+        OKLIB_MODELS_CONCEPT_TAG(Var, LinearOrder);
+
+        static_cast<bool>(v); static_cast<bool>(vc);
       }
+
+      Var v;
+      const Var vc;
     };
 
     class Variables_Archetype {
@@ -34,8 +44,71 @@ namespace OKlib {
     public :
       typedef Variables_tag concept_tag;
       convertible_to_bool operator ==(const Variables_Archetype&) const {}
+      convertible_to_bool operator !=(const Variables_Archetype&) const {}
+      convertible_to_bool operator <(const Variables_Archetype&) const {}
+      convertible_to_bool operator >(const Variables_Archetype&) const {}
+      convertible_to_bool operator <=(const Variables_Archetype&) const {}
+      convertible_to_bool operator >=(const Variables_Archetype&) const {}
+      operator bool() const {}
     };
     
+    // --------------------------------------------------------------------------------------------------------------------------------------
+
+    struct VariablesWithIndex_tag : virtual Variables_tag {};
+    
+    template <typename Var>
+    struct VariablesWithIndex {
+
+      typedef typename index_type<Var>::type index_type;
+      BOOST_STATIC_ASSERT(::OKlib::MetaProgramming::is_unqualified_signedunsigned_integral<index_type>::value);
+
+      void constraints() {
+        OKLIB_MODELS_CONCEPT_TAG(Var, VariablesWithIndex);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, Variables);
+        OKLIB_MODELS_CONCEPT_TAG(Var, Variables);
+
+        static_cast<index_type>(v);
+        static_cast<index_type>(vc);
+      }
+
+      Var v;
+      const Var vc;
+    };
+
+    struct VariablesWithIndex_Archetype : Variables_Archetype {
+      typedef VariablesWithIndex_tag concept_tag;
+      typedef int index_type; // ToDo: Can this be made more generic?
+      operator index_type() const {}
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------------------------
+
+    struct VariablesAsIndex_tag : virtual VariablesWithIndex_tag {};
+    
+    template <typename Var>
+    struct VariablesAsIndex {
+
+      typedef typename index_type<Var>::type index_type;
+
+      void constraints() {
+        OKLIB_MODELS_CONCEPT_TAG(Var, VariablesAsIndex);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(Var, VariablesWithIndex);
+        OKLIB_MODELS_CONCEPT_TAG(Var, VariablesWithIndex);
+
+        Var(i);
+        Var(ic);
+      }
+
+      index_type i; const index_type ic;
+    };
+
+    struct VariablesAsIndex_Archetype : VariablesWithIndex_Archetype {
+      typedef VariablesAsIndex_tag concept_tag;
+      VariablesAsIndex_Archetype() {}
+      explicit VariablesAsIndex_Archetype(index_type) {}
+    };
   }
 
 }
