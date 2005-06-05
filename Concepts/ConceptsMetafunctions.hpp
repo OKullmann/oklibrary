27d32fb2790308ed677363cb8501cc45e7f8e38d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:55:32 +0000

    Traits classes moved out (to Traits.hpp), and use of new macros.

--- Concepts/ConceptsMetafunctions.hpp
+++ Concepts/ConceptsMetafunctions.hpp
@@ -8,27 +8,22 @@
 #include <boost/mpl/or.hpp>
 #include <boost/type_traits.hpp>
 
+#include "Logical.hpp"
+
 #include "ConceptsBase.hpp"
+#include "Traits.hpp"
 
 namespace OKlib {
 
   namespace Concepts {
 
-    template <class T>
-    struct ConceptTag {
-      typedef typename T::concept_tag type;
-    };
-
-    // -------------------------------------------------------------------------------------------------------------------
-
     template <class T, class concept_tag>
     class IsTagModel {
-      typedef typename ConceptTag<T>::type t_concept_tag;
+      typedef typename ::OKlib::Concepts::concept_tag<T>::type t_concept_tag;
     public :
       typedef typename 
       ::boost::mpl::or_< ::boost::is_same<t_concept_tag, concept_tag>, ::boost::is_base_and_derived<concept_tag, t_concept_tag> >::type type;
-      typedef typename type::value_type value_type;
-      static const value_type value = type::value;
+      OKLIB_META_VALUE_T;
     };
 
      // -------------------------------------------------------------------------------------------------------------------
@@ -37,18 +32,16 @@ namespace OKlib {
     struct IsConceptTag {
       typedef typename
       ::boost::is_base_and_derived< ::OKlib::Concepts::ConceptsBase_tag, Tag>::type type;
-      typedef typename type::value_type value_type;
-      static const value_type value = type::value;
+      OKLIB_META_VALUE_T;
     };
 
     template <class T>
     class HasConceptTag {
-      typedef typename ConceptTag<T>::type t_concept_tag;
+      typedef typename concept_tag<T>::type t_concept_tag;
     public :
       typedef typename
       IsConceptTag<t_concept_tag>::type type;
-      typedef typename type::value_type value_type;
-      static const value_type value = type::value;
+      OKLIB_META_VALUE_T;
     };
 
   }
