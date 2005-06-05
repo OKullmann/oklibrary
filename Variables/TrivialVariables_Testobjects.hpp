55ee65b4c4a21ce4a8d3c3fa2e1739e7566136a6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:17:17 +0000

    Applying ::OKlib::Concepts::VariablesAsIndex_basic_test to the four models.

---
+++ Variables/TrivialVariables_Testobjects.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 5.6.2005 (Swansea)
+
+#ifndef TRIVIALVARIABLESTESTOBJECTS_90845TgH
+
+#define TRIVIALVARIABLESTESTOBJECTS_90845TgH
+
+#include "Variables_Tests.hpp"
+
+#include "TrivialVariables.hpp"
+#include "TrivialVariables_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Variables {
+
+    ::OKlib::Concepts::VariablesAsIndex_basic_test<Variables_signed_char> test_Variables_signed_char;
+    ::OKlib::Concepts::VariablesAsIndex_basic_test<Variables_short_int> test_Variables_short_int;
+    ::OKlib::Concepts::VariablesAsIndex_basic_test<Variables_int> test_Variables_int;
+    ::OKlib::Concepts::VariablesAsIndex_basic_test<Variables_long_int> test_Variables_long_int;
+
+  }
+
+}
+
+#endif
