1592af9013aa30126eb023ce698e5d546e44640d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:50:36 +0000

    Testobjects for TypeTraits added.

--- MetaProgramming/TestMetaProgramming.cpp
+++ MetaProgramming/TestMetaProgramming.cpp
@@ -5,6 +5,7 @@
 #include "TestBaseClass.hpp"
 
 #include "Logical_Testobjects.hpp"
+#include "TypeTraits_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
