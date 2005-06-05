db1841db7fb1ca4aa7fa7419fabb355a79049fb5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:50:05 +0000

    Application of the tests in the standard way (for metafunctions).

---
+++ MetaProgramming/TypeTraits_Testobjects.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 5.6.2005 (Swansea)
+
+#ifndef TYPETRAITSTESTOBJECTS_78UiJh0
+
+#define TYPETRAITSTESTOBJECTS_78UiJh0
+
+#include "TypeTraits.hpp"
+#include "TypeTraits_Tests.hpp"
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+    template class Test_is_unqualified_signed_integral_T<is_unqualified_signed_integral>;
+
+    template class Test_is_unqualified_unsigned_integral_T<is_unqualified_unsigned_integral>;
+
+    template class Test_is_unqualified_signedunsigned_integral_T<is_unqualified_signedunsigned_integral>;
+
+  }
+
+}
+
+#endif
