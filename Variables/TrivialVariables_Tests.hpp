49100f93f70f8fc6a1f8ca670075892a661791cd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-05 20:16:37 +0000

    Yet empty.

---
+++ Variables/TrivialVariables_Tests.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 5.6.2005 (Swansea)
+
+#ifndef TRIVIALVARIABLESTESTS_90ioKl
+
+#define TRIVIALVARIABLESTESTS_90ioKl
+
+namespace OKlib {
+
+  namespace Variables {
+
+  }
+
+}
+
+#endif
