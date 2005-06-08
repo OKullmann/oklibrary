8a3926441386f5552438bdb192f923d1e2f8c5ed
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-08 11:11:22 +0000

    Standard testobjects.

---
+++ SAT-Competition/AnalysisTools_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 7.6.2005 (Swansea)
+
+#ifndef ANALYSISTOOLSTESTOBJECTS_010uJn
+
+#define ANALYSISTOOLSTESTOBJECTS_010uJn
+
+#include "AnalysisTools.hpp"
+#include "AnalysisTools_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SATCompetition {
+
+    Test_ElementaryAnalysis<ElementaryAnalysis> test_elementary_analysis;
+
+  }
+
+}
+
+#endif
