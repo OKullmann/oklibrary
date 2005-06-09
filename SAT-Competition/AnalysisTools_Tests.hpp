c2a1fd784f4325557bf46b1a6baf9c0527d5c7bf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-09 11:45:49 +0000

    Work in progress.

--- SAT-Competition/AnalysisTools_Tests.hpp
+++ SAT-Competition/AnalysisTools_Tests.hpp
@@ -4,8 +4,12 @@
 
 #define ANALYSISTOOLSTESTS_37yh6fR4
 
+#include <cassert>
+
 #include "SingleResult.hpp"
 #include "ParsingSingleResult.hpp"
+#include "ParsingResultSequences_Tests.hpp"
+#include "ResultProcessing.hpp"
 
 namespace OKlib {
 
@@ -19,9 +23,27 @@ namespace OKlib {
         insert(this);
       }
     private :
+
       void perform_test_trivial() {
+
+        test_result<Result>(filename_large_industrial, line_count_large_industrial);
+
         // ToDo: Test for series_in_superseries()
       }
+
+      template <class result_type>
+      void test_result(const std::string& filename, const int line_count) {
+        typedef  Result_database_from_file<ParserResult, result_type> result_database;
+        result_database rdb(filename);
+        assert(rdb.result_sequence.size() == line_count);
+        typedef typename result_database::database_type database;
+        typedef ElementaryAnalysis<database> elementary_analysis;
+        elementary_analysis ea(rdb.db);
+
+        OKLIB_TEST_EQUAL(ea.series_in_superseries().size(), rdb.db.super_series().size());
+        
+      }
+
     };
 
   }
