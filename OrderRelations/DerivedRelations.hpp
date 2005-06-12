918b088b15356a7a8587098a441c99ef215cc534
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:42:23 +0000

    Four functor templates for the relations derived from <, and macros OKLIB_DERIVED_UNEQUAL and OKLIB_DERIVED_ORDERRELATIONS for the definition of derived relations.

---
+++ OrderRelations/DerivedRelations.hpp
@@ -0,0 +1,65 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef DERIVEDRELATIONS_oPlK510v
+
+#define DERIVEDRELATIONS_oPlK510v
+
+#include <functional>
+#include <boost/functional.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits.hpp>
+
+namespace OKlib {
+
+  namespace OrderRelations {
+
+    template <class LessThanRelation>
+    struct GreaterThan : std::binary_function<
+      typename boost::binary_traits<LessThanRelation>::second_argument_type,
+      typename boost::binary_traits<LessThanRelation>::first_argument_type,
+      bool> {
+      bool operator() (const typename boost::binary_traits<LessThanRelation>::second_argument_type& arg1, const typename boost::binary_traits<LessThanRelation>::first_argument_type& arg2) const {
+        return LessThanRelation()(arg2, arg1);
+      }
+    };
+
+    template <class LessThanRelation>
+    struct GreaterEqualThan : std::binary_function<
+      typename boost::binary_traits<LessThanRelation>::first_argument_type,
+      typename boost::binary_traits<LessThanRelation>::second_argument_type,
+      bool> {
+      bool operator() (const typename boost::binary_traits<LessThanRelation>::first_argument_type& arg1, const typename boost::binary_traits<LessThanRelation>::second_argument_type& arg2) const {
+        return not LessThanRelation()(arg1, arg2);
+      }
+    };
+
+    template <class LessThanRelation>
+    struct LessEqualThan : std::binary_function<
+      typename boost::binary_traits<LessThanRelation>::second_argument_type,
+      typename boost::binary_traits<LessThanRelation>::first_argument_type,
+      bool> {
+      bool operator() (const typename boost::binary_traits<LessThanRelation>::second_argument_type& arg1, const typename boost::binary_traits<LessThanRelation>::first_argument_type& arg2) const {
+        return not LessThanRelation()(arg2, arg1);
+      }
+    };
+
+    template <class LessThanRelation>
+    struct Equivalence : std::binary_function<
+      typename boost::binary_traits<LessThanRelation>::first_argument_type,
+      typename boost::binary_traits<LessThanRelation>::first_argument_type,
+      bool> {
+      BOOST_STATIC_ASSERT((boost::is_same<typename boost::binary_traits<LessThanRelation>::first_argument_type, typename boost::binary_traits<LessThanRelation>::second_argument_type>::value));
+      bool operator() (const typename boost::binary_traits<LessThanRelation>::first_argument_type& arg1, const typename boost::binary_traits<LessThanRelation>::first_argument_type& arg2) const {
+        return not LessThanRelation()(arg1, arg2) and not LessThanRelation(arg2, arg1);
+      }
+    };
+  
+#define OKLIB_DERIVED_UNEQUAL(type) inline bool operator !=(const type& lhs, const type& rhs) { return not(lhs == rhs); }
+
+#define OKLIB_DERIVED_ORDERRELATIONS(type) inline bool operator >(const type& lhs, const type& rhs) { return rhs < lhs; }  inline bool operator <=(const type& lhs, const type& rhs) { return not (rhs < lhs); }  inline bool operator >=(const type& lhs, const type& rhs) { return not (lhs < rhs); }
+
+  }
+
+}
+
+#endif
