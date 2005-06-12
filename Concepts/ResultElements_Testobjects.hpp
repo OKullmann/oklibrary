58d25a80beebd88c4a1f09d7c83b34a84786c44b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:51:24 +0000

    Standard (testing concepts and archetypes).

---
+++ Concepts/ResultElements_Testobjects.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef RESULTELEMENTTESTOBJECTS_957yBhF
+
+#define RESULTELEMENTTESTOBJECTS_957yBhF
+
+#include "LibraryBasics_Tests.hpp"
+
+#include "ResultElements.hpp"
+#include "ResultElements_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Concepts {
+
+    OKLIB_BASIC_CONCEPT_TEST_TAG(ResultElement);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(ResultElement);
+
+    OKLIB_BASIC_CONCEPT_TEST_TAG(ResultElementWithOrder);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(ResultElementWithOrder);
+
+    OKLIB_BASIC_CONCEPT_TEST_TAG(ResultElementWithName);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(ResultElementWithName);
+
+  }
+
+}
+
+#endif
