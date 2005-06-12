b823d4de88e4dda51cf42b6726ac2b8da2169d4b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:43:09 +0000

    Standard (yet empty).

---
+++ OrderRelations/DerivedRelations_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef DERIVEDRELATIONSTESTOBJECTS_1016YhB
+
+#define DERIVEDRELATIONSTESTOBJECTS_1016YhB
+
+#include "DerivedRelations.hpp"
+#include "DerivedRelations_Tests.hpp"
+
+namespace OKlib {
+
+  namespace OrderRelations {
+
+  }
+
+}
+
+#endif
