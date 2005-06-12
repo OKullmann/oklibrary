0901994ff6038c26575993a13ad41a5998b464a9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:35:01 +0000

    Applying some of the basic tests from the Concept's module.

---
+++ SAT-Competition/SingleResult_Testobjects.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef SINGLERESULTTESTOBJECTS_BVcr678
+
+#define SINGLERESULTTESTOBJECTS_BVcr678
+
+#include "ResultElements_Tests.hpp"
+
+#include "SingleResult.hpp"
+#include "SingleResult_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SATCompetition {
+
+    ::OKlib::Concepts::ResultElement_basic_test<ResultElement> test_ResultElement;
+    ::OKlib::Concepts::ResultElementWithName_basic_test<ResultElement_with_name> test_ResultElement_with_name;
+
+  }
+
+}
+
+#endif
