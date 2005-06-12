84d6460420986090a87b4b47f612a12e26038079
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:48:35 +0000

    Currently concepts  - ResultElement  - ResultElementWithOrder  - ResultElementWithName.

---
+++ Concepts/ResultElements.hpp
@@ -0,0 +1,131 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef RESULTELEMENT_6Tg5Yh
+
+#define RESULTELEMENT_6Tg5Yh
+
+#include "LibraryBasics.hpp"
+#include "Traits.hpp"
+
+namespace OKlib {
+
+  namespace Concepts {
+
+    struct ResultElement_tag : virtual BasicRequirements_tag, virtual FullyConstructible_tag {};
+
+    template <typename Res>
+    struct ResultElement {
+      typedef typename string_type<Res>::type string_type;
+      typedef typename floating_point_type<Res>::type floating_point_type;
+      typedef typename natural_number_type<Res>::type natural_number_type;
+      void constraints() {
+        OKLIB_MODELS_CONCEPT_TAG(Res, ResultElement);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, BasicRequirements);
+        OKLIB_MODELS_CONCEPT_TAG(Res, BasicRequirements);
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, FullyConstructible);
+        OKLIB_MODELS_CONCEPT_TAG(Res, FullyConstructible);
+      }
+    };
+
+    class ResultElement_Archetype {
+      struct X {};
+      struct Y {};
+      struct Z {};
+    public :
+      typedef ResultElement_tag concept_tag;
+      typedef X string_type;
+      typedef Y floating_point_type;
+      typedef Z natural_number_type;
+    };
+    
+    // --------------------------------------------------------------------------------------------------------------------------------------
+
+    struct ResultElementWithOrder_tag : virtual ResultElement_tag, virtual EqualitySubstitutable_tag, virtual LinearOrder_tag {};
+
+    template <typename Res>
+    struct ResultElementWithOrder {
+      void constraints() {
+        OKLIB_MODELS_CONCEPT_TAG(Res, ResultElementWithOrder);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, ResultElement);
+        OKLIB_MODELS_CONCEPT_TAG(Res, ResultElement);
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, EqualitySubstitutable);
+        OKLIB_MODELS_CONCEPT_TAG(Res, EqualitySubstitutable);
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, LinearOrder);
+        OKLIB_MODELS_CONCEPT_TAG(Res, LinearOrder);
+      }
+    };
+
+    class ResultElementWithOrder_Archetype : public ResultElement_Archetype {
+    protected :
+      struct convertible_to_bool {
+        operator bool() {}
+      };
+    public :
+      typedef ResultElementWithOrder_tag concept_tag;
+      convertible_to_bool operator ==(const ResultElementWithOrder_Archetype&) const {}
+      convertible_to_bool operator !=(const ResultElementWithOrder_Archetype&) const {}
+      convertible_to_bool operator <(const ResultElementWithOrder_Archetype&) const {}
+      convertible_to_bool operator >(const ResultElementWithOrder_Archetype&) const {}
+      convertible_to_bool operator <=(const ResultElementWithOrder_Archetype&) const {}
+      convertible_to_bool operator >=(const ResultElementWithOrder_Archetype&) const {}
+    };
+    
+    // --------------------------------------------------------------------------------------------------------------------------------------
+
+    struct ResultElementWithName_tag : virtual ResultElementWithOrder_tag {};
+
+    template <typename Res>
+    struct ResultElementWithName {
+      typedef typename string_type<Res>::type string_type;
+      void constraints() {
+        OKLIB_MODELS_CONCEPT_TAG(Res, ResultElementWithName);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(Res, ResultElementWithOrder);
+        OKLIB_MODELS_CONCEPT_TAG(Res, ResultElementWithOrder);
+
+        OKLIB_MODELS_CONCEPT_REQUIRES(string_type, ConstCorrect);
+        OKLIB_MODELS_CONCEPT_REQUIRES(string_type, EqualitySubstitutable);
+        OKLIB_MODELS_CONCEPT_REQUIRES(string_type, LinearOrder);
+        OKLIB_MODELS_CONCEPT_REQUIRES(string_type, FullyConstructible);
+
+        Res(s);
+        Res(sc);
+        static_cast<string_type>(r.name());
+        static_cast<string_type>(rc.name());
+      }
+      string_type s; const string_type sc;
+      Res r; const Res rc;
+    };
+
+    class ResultElementWithName_Archetype : public ResultElementWithOrder_Archetype {
+    public :
+      struct string_type {
+    protected :
+        struct convertible_to_bool {
+          operator bool() {}
+        };
+      public :
+        convertible_to_bool operator ==(const string_type&) const {}
+        convertible_to_bool operator !=(const string_type&) const {}
+        convertible_to_bool operator <(const string_type&) const {}
+        convertible_to_bool operator >(const string_type&) const {}
+        convertible_to_bool operator <=(const string_type&) const {}
+        convertible_to_bool operator >=(const string_type&) const {}
+      };
+      typedef ResultElementWithName_tag concept_tag;
+      ResultElementWithName_Archetype() {}
+      ResultElementWithName_Archetype(const string_type&) {}
+      string_type name() const {}
+    };
+    
+    // --------------------------------------------------------------------------------------------------------------------------------------
+
+    // ToDo: to be completed.
+    
+  }
+
+}
+
+#endif
