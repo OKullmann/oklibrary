da41a0333b9b4bcbbb17e5f479236f9ace20d3e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:34:11 +0000

    Yet empty.

---
+++ SAT-Competition/SingleResult_Tests.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef SINGLERESULTTESTS_1019HvR
+
+#define SINGLERESULTTESTS_1019HvR
+
+namespace OKlib {
+
+  namespace SATCompetition {
+
+  }
+
+}
+
+#endif
