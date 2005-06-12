448b7d3144e70d53fe9d051175d9f86c2ceccbdf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:42:46 +0000

    Yet empty.

---
+++ OrderRelations/DerivedRelations_Tests.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 11.6.2005 (Swansea)
+
+#ifndef DERIVEDRELATIONSTESTS_aratV101
+
+#define DERIVEDRELATIONSTESTS_aratV101
+
+namespace OKlib {
+
+  namespace OrderRelations {
+
+    
+
+  }
+
+}
+
+#endif
