6edb177c8cc4d1bd2109b06582f0bdcd20dbfcbb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:47:05 +0000

    Traits meta-functions string_type, floating_point_type and natural_number_type added.

--- Concepts/Traits.hpp
+++ Concepts/Traits.hpp
@@ -20,6 +20,29 @@ namespace OKlib {
       typedef typename T::index_type type;
     };
 
+    // -------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct string_type {
+      typedef typename T::string_type type;
+    };
+
+    // -------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct floating_point_type {
+      typedef typename T::floating_point_type type;
+    };
+
+    // -------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct natural_number_type {
+      typedef typename T::natural_number_type type;
+    };
+
+    // -------------------------------------------------------------------------------------------------------------------
+
   }
 
 }
