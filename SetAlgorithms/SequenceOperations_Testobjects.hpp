89e0171b517cd0a6c344a42ed5b2464877f66c8c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:27:19 +0000

    Standard testobject.

---
+++ SetAlgorithms/SequenceOperations_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 12.6.2005 (Swansea)
+
+#ifndef SEQUENCEOPERATIONSTESTOBJECTS_1029563
+
+#define SEQUENCEOPERATIONSTESTOBJECTS_1029563
+
+#include "SequenceOperations.hpp"
+#include "SequenceOperations_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SetAlgorithms {
+
+    Test_Sum_sizes<Sum_sizes> test_sum_sizes;
+    
+  }
+
+}
+
+#endif
