1bac418e2aee31dfc2ed39dd1a83a6c10f947ff3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-12 20:39:26 +0000

    Further completed.

--- SAT-Competition/AnalysisTools.hpp
+++ SAT-Competition/AnalysisTools.hpp
@@ -9,6 +9,8 @@
 #include <algorithm>
 #include <iterator>
 
+#include "BasicMapOperations.hpp"
+
 #include "SingleResult.hpp"
 #include "ResultProcessing.hpp"
 
@@ -18,11 +20,13 @@ namespace OKlib {
 
     template <class Database>
     class ElementaryAnalysis {
-      Database& db;
+      ElementaryAnalysis(const ElementaryAnalysis&);
+      ElementaryAnalysis& operator =(const ElementaryAnalysis&);
+      const Database& db;
 
     public :
 
-      ElementaryAnalysis(Database& db) : db(db) {
+      ElementaryAnalysis(const Database& db) : db(db) {
         fill_series_in_superseries();
         fill_benchmarks_in_series();
         fill_solved_benchmarks();
@@ -31,15 +35,18 @@ namespace OKlib {
         fill_sat_status();
       }
 
+      typedef std::pair<SuperSeries, Series> SpecSeries;
+
       typedef std::vector<Series> seq_series;
+      typedef std::vector<SpecSeries> seq_spec_series;
       typedef std::vector<Benchmark> seq_benchmarks;
       typedef std::vector<Solver> seq_solvers;
       // such vectors are always sorted
 
       typedef std::map<SuperSeries, seq_series> map_superseries_series;
-      typedef std::map<Series, seq_benchmarks> map_series_benchmarks;
+      typedef std::map<SpecSeries, seq_benchmarks> map_series_benchmarks;
       typedef std::map<Solver, seq_benchmarks> map_solver_benchmarks;
-      typedef std::map<Solver, seq_series> map_solver_series;
+      typedef std::map<Solver, seq_spec_series> map_solver_series;
       typedef std::map<Benchmark, seq_solvers> map_benchmark_solvers;
       typedef std::map<Benchmark, SATStatus> map_benchmark_satstatus;
 
@@ -72,20 +79,47 @@ namespace OKlib {
         const other_iterator end_other(db.super_series().end());
         for (other_iterator i(db.super_series().begin()); i != end_other; ++i) {
           const SuperSeries super_series = i -> first;
+          assert(i -> second);
           const SetResultNodesP& result_nodes_p = *(i -> second);
           std::set<Series> set_series;
           const set_iterator end_set(result_nodes_p.end());
-          for (set_iterator j(result_nodes_p.begin()); j != end_set; ++j)
+          for (set_iterator j(result_nodes_p.begin()); j != end_set; ++j) {
+            assert((*j) -> rb);
             set_series.insert((*j) -> rb -> series()); // ToDO: Using STL
-          current = series_in_superseries_.insert(current, std::make_pair(super_series, seq_series()));
-          std::copy(set_series.begin(), set_series.end(), std::back_inserter(current -> second));
+            assert((*j) -> rb -> super_series() == super_series);
+          }
+          current = series_in_superseries_.insert(current, std::make_pair(super_series, seq_series(set_series.begin(), set_series.end())));
+          assert(current -> first == super_series);
+          assert((current -> second).size() == set_series.size());
         }
       }
       void fill_benchmarks_in_series() {
-
+        typedef map_series_benchmarks::iterator iterator;
+        typedef map_superseries_series::const_iterator iterator_superseries;
+        typedef seq_series::const_iterator iterator_series;
+        iterator current = benchmarks_in_series_.begin();
+        const iterator_superseries& end_superseries(series_in_superseries().end());
+        for (iterator_superseries i = series_in_superseries().begin(); i != end_superseries; ++i) {
+          const SuperSeries& super_series(i -> first);
+          const seq_series& vec_series(i -> second);
+          const iterator_series& end_series(vec_series.end());
+          for (iterator_series j = vec_series.begin(); j != end_series; ++j) {
+            const Series& series(*j);
+            std::set<Benchmark> set_benchmarks;
+            const SetResultNodesP& result_nodes_p = *OKlib::SetAlgorithms::map_value(db.series(), series);
+            const set_iterator& end_results(result_nodes_p.end());
+            for (set_iterator k = result_nodes_p.begin(); k != end_results; ++k) {
+              const ResultNode& node(**k);
+              const ResultBasis& result(*node.rb);
+              if (result.super_series() == super_series)
+                set_benchmarks.insert(result.benchmark());
+            }
+            current = benchmarks_in_series_.insert(current, std::make_pair(std::make_pair(super_series, series), seq_benchmarks(set_benchmarks.begin(), set_benchmarks.end())));
+          }
+        }
       }
       void fill_solved_benchmarks() {
-
+        // ToDo: to be completed
       }
       void fill_solved_series() {
 
--- SAT-Competition/AnalysisTools_Tests.hpp
+++ SAT-Competition/AnalysisTools_Tests.hpp
@@ -12,6 +12,7 @@
 
 #include "FunctionHandling.hpp"
 #include "BasicSetOperations.hpp"
+#include "SequenceOperations.hpp"
 
 #include "SingleResult.hpp"
 #include "ParsingSingleResult.hpp"
@@ -35,11 +36,10 @@ namespace OKlib {
 
         test_result<Result>(filename_large_industrial, line_count_large_industrial);
 
-        // ToDo: Test for series_in_superseries()
       }
 
       template <class result_type>
-      void test_result(const std::string& filename, const int line_count) {
+      void test_result(const std::string& filename, const unsigned int line_count) {
 
         typedef  Result_database_from_file<ParserResult, result_type> result_database;
         result_database rdb(filename);
@@ -47,17 +47,42 @@ namespace OKlib {
         typedef typename result_database::database_type database;
         typedef ElementaryAnalysis<database> elementary_analysis;
         elementary_analysis ea(rdb.db);
+        typedef typename elementary_analysis::map_superseries_series map_superseries_series;
+        typedef typename elementary_analysis::map_series_benchmarks map_series_benchmarks;
+        typedef typename elementary_analysis::seq_series seq_series;
+        typedef typename elementary_analysis::seq_benchmarks seq_benchmarks;
 
-        {
-          OKLIB_TEST_EQUAL(ea.series_in_superseries().size(), rdb.db.super_series().size());
-          typedef typename elementary_analysis::seq_series seq_series;
-          typedef typename elementary_analysis::map_superseries_series map_superseries_series;
-          typedef FunctionHandling::Second<typename map_superseries_series::value_type> second;
-          seq_series s;
-          //OKlib::SetAlgorithms::union_sets(boost::make_transform_iterator<second>(ea.series_in_superseries().begin()), boost::make_transform_iterator<second>(ea.series_in_superseries().end()), std::back_inserter(s));
-          //typedef FunctionHandling::First<MapSeries::value_type> first;
-          //OKLIB_TEST_EQUAL_RANGES(s, boost::make_iterator_range(boost::make_transform_iterator<first>(rdb.db.series().begin()), boost::make_transform_iterator<first>(rdb.db.series().end())));
+        { // testing ea.series_in_superseries()
+          {
+            typedef FunctionHandling::First<typename map_superseries_series::value_type> first1;
+            typedef FunctionHandling::First<MapSuperSeries::value_type> first2;
+            OKLIB_TEST_EQUAL_RANGES(
+                                    boost::make_iterator_range(boost::make_transform_iterator<first1>(ea.series_in_superseries().begin()), (boost::make_transform_iterator<first1>(ea.series_in_superseries().end()))),
+                                    boost::make_iterator_range(boost::make_transform_iterator<first2>(rdb.db.super_series().begin()), boost::make_transform_iterator<first2>(rdb.db.super_series().end())));
+          }
+          {
+            typedef FunctionHandling::Second<typename map_superseries_series::value_type> second;
+            seq_series s;
+            OKlib::SetAlgorithms::union_sets(boost::make_transform_iterator<second>(ea.series_in_superseries().begin()), boost::make_transform_iterator<second>(ea.series_in_superseries().end()), std::back_inserter(s));
+            typedef FunctionHandling::First<MapSeries::value_type> first;
+            OKLIB_TEST_EQUAL_RANGES(s, boost::make_iterator_range(boost::make_transform_iterator<first>(rdb.db.series().begin()), boost::make_transform_iterator<first>(rdb.db.series().end())));
+          }
         }
+
+       { // testing ea.benchmarks_in_series()
+         typedef FunctionHandling::Second<typename map_series_benchmarks::value_type> second;
+         typedef boost::transform_iterator<second, typename map_series_benchmarks::const_iterator> transform_iterator;
+         const map_series_benchmarks& map(ea.benchmarks_in_series());
+         const transform_iterator& begin(transform_iterator(map.begin()));
+         const transform_iterator& end(transform_iterator(map.end()));
+         OKLIB_TEST_EQUAL(OKlib::SetAlgorithms::sum_sizes(begin, end), rdb.db.benchmark().size());
+         seq_benchmarks seq;
+         OKlib::SetAlgorithms::union_sets(begin, end, std::back_inserter(seq));
+         typedef FunctionHandling::First<MapBenchmark::value_type> first;
+         OKLIB_TEST_EQUAL_RANGES(seq, boost::make_iterator_range(boost::make_transform_iterator<first>(rdb.db.benchmark().begin()), boost::make_transform_iterator<first>(rdb.db.benchmark().end())));
+       }
+
+       // ToDo: to be completed
       }
 
     };
