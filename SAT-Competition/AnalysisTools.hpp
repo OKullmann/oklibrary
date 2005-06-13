93fc939ef9e9e9801f1ab1705965e7548c73dd8f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-13 13:06:27 +0000

    Further completed.

--- SAT-Competition/AnalysisTools.hpp
+++ SAT-Competition/AnalysisTools.hpp
@@ -8,6 +8,11 @@
 #include <utility>
 #include <algorithm>
 #include <iterator>
+#include <stdexcept>
+#include <string>
+#include <ostream>
+
+#include <boost/lexical_cast.hpp>
 
 #include "BasicMapOperations.hpp"
 
@@ -29,6 +34,7 @@ namespace OKlib {
       ElementaryAnalysis(const Database& db) : db(db) {
         fill_series_in_superseries();
         fill_benchmarks_in_series();
+        fill_series_of_benchmark();
         fill_solved_benchmarks();
         fill_solved_series();
         fill_succesful_solvers();
@@ -36,6 +42,9 @@ namespace OKlib {
       }
 
       typedef std::pair<SuperSeries, Series> SpecSeries;
+      friend std::ostream& operator <<(std::ostream& out, const SpecSeries& series) {
+        return out << series.first << "::" << series.second;
+      }
 
       typedef std::vector<Series> seq_series;
       typedef std::vector<SpecSeries> seq_spec_series;
@@ -45,6 +54,7 @@ namespace OKlib {
 
       typedef std::map<SuperSeries, seq_series> map_superseries_series;
       typedef std::map<SpecSeries, seq_benchmarks> map_series_benchmarks;
+      typedef std::map<Benchmark, SpecSeries> map_benchmark_series;
       typedef std::map<Solver, seq_benchmarks> map_solver_benchmarks;
       typedef std::map<Solver, seq_spec_series> map_solver_series;
       typedef std::map<Benchmark, seq_solvers> map_benchmark_solvers;
@@ -52,6 +62,7 @@ namespace OKlib {
 
       const map_superseries_series& series_in_superseries() const { return series_in_superseries_; }
       const map_series_benchmarks & benchmarks_in_series() const { return benchmarks_in_series_; }
+      const map_benchmark_series& series_of_benchmark() const { return series_of_benchmark_; }
       const map_solver_benchmarks& solved_benchmarks() const { return solved_benchmarks_; }
       const map_solver_series& solved_series() const { return solved_series_; }
       const map_benchmark_solvers& succesful_solvers() const { return succesful_solvers_; }
@@ -59,10 +70,19 @@ namespace OKlib {
 
       const seq_benchmarks& inconsistent_results() const { return inconsistent_results; }
 
+      struct ErrorElementaryAnalysis : std::runtime_error {
+        ErrorElementaryAnalysis(const std::string& message) : std::runtime_error(message) {}
+      };
+      // ToDo: Should there be an exception base class for OKlib ?
+      struct AmbigueBenchmark : ErrorElementaryAnalysis {
+        AmbigueBenchmark(const Benchmark& benchmark, const SpecSeries& series1, const SpecSeries& series2) : ErrorElementaryAnalysis(boost::lexical_cast<std::string>(benchmark) + " in " + boost::lexical_cast<std::string>(series1) + " and " + boost::lexical_cast<std::string>(series2)) {}
+      };
+
     private :
 
       map_superseries_series series_in_superseries_;
       map_series_benchmarks benchmarks_in_series_;
+      map_benchmark_series series_of_benchmark_;
       map_solver_benchmarks solved_benchmarks_;
       map_solver_series solved_series_;
       map_benchmark_solvers succesful_solvers_;
@@ -118,14 +138,74 @@ namespace OKlib {
           }
         }
       }
+      void fill_series_of_benchmark() {
+        typedef map_series_benchmarks::const_iterator iterator_series;
+        typedef seq_benchmarks::const_iterator iterator_benchmarks;
+        typedef map_benchmark_series::iterator iterator;
+        const iterator_series& end_series(benchmarks_in_series().end());
+        const iterator& end_map(series_of_benchmark_.end());
+        for (iterator_series i = benchmarks_in_series().begin(); i != end_series; ++i) {
+          const SpecSeries& series(i -> first);
+          const seq_benchmarks& benchs(i -> second);
+          const iterator_benchmarks& end_benchmarks(benchs.end());
+          for (iterator_benchmarks j = benchs.begin(); j != end_benchmarks; ++j) {
+            const Benchmark& bench(*j);
+            const iterator position(series_of_benchmark_.find(bench));
+            if (position == end_map)
+              series_of_benchmark_.insert(std::make_pair(bench, series));
+            else {
+              const SpecSeries& series2(position -> second);
+              assert(series2 != series);
+              throw AmbigueBenchmark(bench, series, series2);
+            }
+              
+            series_of_benchmark_[*j] = series;
+          }
+        }
+      }
       void fill_solved_benchmarks() {
-        // ToDo: to be completed
+        typedef map_solver_benchmarks::iterator iterator;
+        iterator current = solved_benchmarks_.begin();
+        std::set<Solver> set_solvers;
+        const MapSolver& map(db.solver());
+        typedef MapSolver::const_iterator other_iterator;
+        const other_iterator end(map.end());
+        for (other_iterator i = map.begin(); i != end; ++i) {
+          const Solver& solver(i -> first);
+          const SetResultNodesP& result_nodes_p = *(i -> second);
+          std::set<Benchmark> set_benchmarks;
+          const set_iterator& end_results(result_nodes_p.end());
+          for (set_iterator j = result_nodes_p.begin(); j != end_results; ++j) {
+            const ResultNode& node(**j);
+            const ResultBasis& result(*node.rb);
+            const SolverResult& solv_res(result.sat_status().result());
+            if (solv_res == sat or solv_res == unsat)
+              set_benchmarks.insert(result.benchmark());
+          }
+          current = solved_benchmarks_.insert(current, std::make_pair(solver, seq_benchmarks(set_benchmarks.begin(), set_benchmarks.end())));
+        }
       }
       void fill_solved_series() {
-
+        typedef map_solver_series::iterator iterator;
+        iterator current = solved_series_.begin();
+        typedef map_solver_benchmarks::const_iterator iterator_solvers;
+        const iterator_solvers& end_solvers(solved_benchmarks().end());
+        for (iterator_solvers i = solved_benchmarks().begin(); i != end_solvers; ++i) {
+          std::set<SpecSeries> set_series;
+          const Solver& solver(i -> first);
+          const seq_benchmarks& benchs(i -> second);
+          typedef seq_benchmarks::const_iterator iterator_benchmarks;
+          const iterator_benchmarks end_benchs(benchs.end());
+          for (iterator_benchmarks j = benchs.begin(); j != end_benchs; ++j) {
+            const Benchmark& bench(*j);
+            const SpecSeries& series(OKlib::SetAlgorithms::map_value(series_of_benchmark(), bench));
+            set_series.insert(series);
+          }
+          current = solved_series_.insert(current, std::make_pair(solver, seq_spec_series(set_series.begin(), set_series.end())));
+        }
       }
       void fill_succesful_solvers() {
-
+         // ToDo: to be completed
       }
       void fill_sat_status() {
 
--- SAT-Competition/AnalysisTools_Tests.hpp
+++ SAT-Competition/AnalysisTools_Tests.hpp
@@ -6,6 +6,8 @@
 
 #include <cassert>
 #include <iterator>
+#include <algorithm>
+#include <iostream> // ####################
 
 #include <boost/iterator/transform_iterator.hpp>
 #include <boost/range/iterator_range.hpp>
@@ -47,8 +49,12 @@ namespace OKlib {
         typedef typename result_database::database_type database;
         typedef ElementaryAnalysis<database> elementary_analysis;
         elementary_analysis ea(rdb.db);
+
+        typedef typename elementary_analysis::SpecSeries SpecSeries;
+
         typedef typename elementary_analysis::map_superseries_series map_superseries_series;
         typedef typename elementary_analysis::map_series_benchmarks map_series_benchmarks;
+        typedef typename elementary_analysis::map_benchmark_series map_benchmark_series;
         typedef typename elementary_analysis::seq_series seq_series;
         typedef typename elementary_analysis::seq_benchmarks seq_benchmarks;
 
@@ -80,6 +86,64 @@ namespace OKlib {
          OKlib::SetAlgorithms::union_sets(begin, end, std::back_inserter(seq));
          typedef FunctionHandling::First<MapBenchmark::value_type> first;
          OKLIB_TEST_EQUAL_RANGES(seq, boost::make_iterator_range(boost::make_transform_iterator<first>(rdb.db.benchmark().begin()), boost::make_transform_iterator<first>(rdb.db.benchmark().end())));
+       }
+
+       { // testing ea.series_of_benchmark()
+         const map_benchmark_series& map1(ea.series_of_benchmark());
+         {
+           typedef FunctionHandling::First<typename map_benchmark_series::value_type> first1;
+           typedef FunctionHandling::First<MapBenchmark::value_type> first2;
+           typedef boost::transform_iterator<first1, typename map_benchmark_series::const_iterator> transform_iterator1;
+           typedef boost::transform_iterator<first2, MapBenchmark::const_iterator> transform_iterator2;
+           const MapBenchmark& map2(rdb.db.benchmark());
+           OKLIB_TEST_EQUAL_RANGES(
+                                   boost::make_iterator_range(transform_iterator1(map1.begin()), transform_iterator1(map1.end())),
+                                   boost::make_iterator_range(transform_iterator2(map2.begin()), transform_iterator2(map2.end())));
+         }
+         {
+           typedef typename map_series_benchmarks::const_iterator iterator_series;
+           typedef typename map_benchmark_series::const_iterator iterator_benchmark;
+           const map_series_benchmarks& map2(ea.benchmarks_in_series());
+           const iterator_series& end_series(map2.end());
+           const iterator_benchmark& end_benchmarks(map1.end());
+           for (iterator_series i = map2.begin(); i != end_series; ++i) {
+             const SpecSeries& series(i -> first);
+             const seq_benchmarks& benchs(i -> second);
+             typedef typename seq_benchmarks::const_iterator iterator;
+             const iterator& end(benchs.end());
+             for (iterator j =benchs.begin(); j != end; ++j) {
+               const Benchmark& bench(*j);
+               const iterator_benchmark& position(map1.find(bench));
+               OKLIB_TEST_NOTEQUAL(position, end_benchmarks);
+               OKLIB_TEST_EQUAL(position -> second, series);
+             }
+           }
+         }
+         // ToDo: Testing whether elementary_analysis::AmbigueBenchmark is thrown.
+       }
+
+       {// testing ea.solved_benchmarks()
+         typedef MapSolver::const_iterator iterator;
+         const MapSolver& map_solver(rdb.db.solver());
+         const iterator end_map(map_solver.end());
+         for (iterator i = map_solver.begin(); i != end_map; ++i) {
+           const Solver& solver(i -> first);
+           const SetResultNodesP* result_nodes_p = i -> second;
+           rdb.db.vector_of_sets.clear();
+           rdb.db.vector_of_sets.push_back(result_nodes_p);
+           rdb.db.vector_of_sets.push_back(OKlib::SetAlgorithms::map_value(rdb.db.sat_status(), SATStatus(sat)));
+           const VectorResultNodesP sat_result_nodes(rdb.db.intersection());
+           rdb.db.vector_of_sets.pop_back();
+           rdb.db.vector_of_sets.push_back(OKlib::SetAlgorithms::map_value(rdb.db.sat_status(), SATStatus(unsat)));
+           const VectorResultNodesP& unsat_result_nodes(rdb.db.intersection());
+           VectorResultNodesP sat_and_unsat; sat_and_unsat.reserve(sat_result_nodes.size() + unsat_result_nodes.size());
+           std::set_union(sat_result_nodes.begin(), sat_result_nodes.end(), unsat_result_nodes.begin(), unsat_result_nodes.end(), std::back_inserter(sat_and_unsat));
+           OKLIB_TEST_EQUAL(sat_and_unsat.size(), OKlib::SetAlgorithms::map_value(ea.solved_benchmarks(), solver).size()); 
+         }
+       }
+
+       { // testing ea.solved_series()
+         
        }
 
        // ToDo: to be completed
