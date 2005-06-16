2a2af1b24d1e20f1e295922dd5d10e638483bf9e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-16 15:00:59 +0000

    Testobjects for ExperimentalThreads.

---
+++ Parallelism/ExperimentalThreads_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 15.6.2005 (Swansea)
+
+#ifndef EXPERIMENTALTHREADSTESTOBJECTS_045rtf
+
+#define EXPERIMENTALTHREADSTESTOBJECTS_045rtf
+
+#include "ExperimentalThreads.hpp"
+#include "ExperimentalThreads_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Parallelism {
+
+    Test_Counter<Counter> test_counter;
+
+  }
+
+}
+
+#endif
