81dcec285d7cf3199873f6b80760871128b25b4d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-16 15:01:37 +0000

    Standard testprogram (linking with boost::threads).

---
+++ Parallelism/TestParallelism.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "ExperimentalThreads_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+  // ToDo: normal output to std::cout, error messages to std::cerr (thus 2 streams, as in Aeryn).
+} 
