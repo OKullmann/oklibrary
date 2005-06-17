e5425d5d0b44be16fe3f850c85588ad1a39aa094
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-06-17 23:13:19 +0000

    Standard.

---
+++ SAT-Competition/Scoring_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 17.6.2005 (Swansea)
+
+#ifndef SCORINGTESTOBJECTS_poIU71k
+
+#define SCORINGTESTOBJECTS_poIU71k
+
+#include "Scoring.hpp"
+#include "Scoring_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SATCompetition {
+
+    Test_PurseScoring<PurseScoring> test_PurseScoring;
+
+  }
+
+}
+
+#endif
