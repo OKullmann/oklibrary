5365223009d4a7acc325be9f1a7606472ed251c6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-02 19:01:00 +0000

    Standard approach.

---
+++ OrderRelations/OrderConstructions_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 2.7.2005 (Swansea)
+
+#ifndef ORDERCONSTRUCTIONSTESTOBJECTS_66TTgV
+
+#define ORDERCONSTRUCTIONSTESTOBJECTS_66TTgV
+
+#include "OrderConstructions.hpp"
+#include "OrderConstructions_Tests.hpp"
+
+namespace OKlib {
+
+  namespace OrderRelations {
+
+  }
+
+}
+
+#endif
---
+++ SetAlgorithms/Subsumption_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 2.7.2005 (Swansea)
+
+#ifndef SUBSUMPTIONTESTOBJECTS_8887yh
+
+#define SUBSUMPTIONTESTOBJECTS_8887yh
+
+#include "Subsumption.hpp"
+#include "Subsumption_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SetAlgorithms {
+
+    Test_Subsumption_elimination<Subsumption_elimination> test_subsumption_elimination;
+
+  }
+
+}
+
+#endif
