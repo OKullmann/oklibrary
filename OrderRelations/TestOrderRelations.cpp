a52bd2d64a6559cda355b0ccfd2488c6565c5cfb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-02 19:04:37 +0000

    Inclusion of testobjects for OrderConstructions.

--- OrderRelations/TestOrderRelations.cpp
+++ OrderRelations/TestOrderRelations.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "DerivedRelations_Testobjects.hpp"
+#include "OrderConstructions_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cout);
