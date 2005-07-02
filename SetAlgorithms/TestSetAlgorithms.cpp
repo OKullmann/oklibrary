507044cf6b659bff4c217b87a4bbdc50eed37543
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-02 19:01:28 +0000

    Inclusion of testobjectss for Subsumption.

--- SetAlgorithms/TestSetAlgorithms.cpp
+++ SetAlgorithms/TestSetAlgorithms.cpp
@@ -5,6 +5,7 @@
 #include "BasicSetOperations_Testobjects.hpp"
 #include "BasicMapOperations_Testobjects.hpp"
 #include "SequenceOperations_Testobjects.hpp"
+#include "Subsumption_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
