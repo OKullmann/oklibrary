dc93de40f428e9ae5b9da7bcb037819a5fcade9c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-02 19:03:58 +0000

    Yet empty.

---
+++ OrderRelations/OrderConstructions_Tests.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 2.7.2005 (Swansea)
+
+#ifndef ORDERCONSTRUCTIONSTESTS_0101tgb
+
+#define ORDERCONSTRUCTIONSTESTS_0101tgb
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace OrderRelations {
+
+  }
+
+}
+
+#endif
