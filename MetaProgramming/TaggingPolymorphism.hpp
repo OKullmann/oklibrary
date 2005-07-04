604d1809ca2357ab95d0b8cdf89676b5838d59a1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-04 12:12:55 +0000

    *** empty log message ***

---
+++ MetaProgramming/TaggingPolymorphism.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 3.7.2005 (Swansea)
+
+#ifndef TAGGINGPOLYMORPHISM_009ikm
+
+#define TAGGINGPOLYMORPHISM_009ikm
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+    struct property_tag {};
+
+  }
+
+}
+
+#endif
