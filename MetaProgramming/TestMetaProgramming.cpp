4351e48ffb073a41443e3214976a38498801cf54
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-04 12:13:49 +0000

    Included testobjects for TaggingPolymorphism.

--- MetaProgramming/TestMetaProgramming.cpp
+++ MetaProgramming/TestMetaProgramming.cpp
@@ -6,6 +6,7 @@
 
 #include "Logical_Testobjects.hpp"
 #include "TypeTraits_Testobjects.hpp"
+#include "TaggingPolymorphism_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
