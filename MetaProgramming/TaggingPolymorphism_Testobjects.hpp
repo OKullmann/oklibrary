1932e032f22cb04d8e2edc5b9660b59fda2b3c75
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-04 12:13:22 +0000

    Standard approach.

---
+++ MetaProgramming/TaggingPolymorphism_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 3.7.2005 (Swansea)
+
+#ifndef TAGGINGPOLYMORPHISMTESTOBJECTS_66TRT
+
+#define TAGGINGPOLYMORPHISMTESTOBJECTS_66TRT
+
+#include "TaggingPolymorphism.hpp"
+#include "TaggingPolymorphism_Tests.hpp"
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+  }
+
+}
+
+#endif
---
+++ MetaProgramming/TaggingPolymorphism_Tests.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 3.7.2005 (Swansea)
+
+#ifndef TAGGINGPOLYMORPHISMTESTS_alam610
+
+#define TAGGINGPOLYMORPHISMTESTS_alam610
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+  }
+
+}
+
+#endif
