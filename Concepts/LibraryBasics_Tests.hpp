547a29f7bdc3dd030239e37c02398f1a91b10399
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-04 12:17:06 +0000

    Doxygen comments.

--- Concepts/LibraryBasics_Tests.hpp
+++ Concepts/LibraryBasics_Tests.hpp
@@ -1,5 +1,10 @@
 // Oliver Kullmann, 25.5.2005 (Swansea)
 
+/*!
+  \file LibraryBasics_Tests.hpp
+  \brief Tools for testing concept definitions (and related constructions).
+*/
+
 #ifndef LIBRARYBASICSTESTS_rttjkN00
 
 #define LIBRARYBASICSTESTS_rttjkN00
@@ -14,9 +19,24 @@ namespace OKlib {
 
   namespace Concepts {
 
+    /*!
+      \namespace OKlib::Concepts::ConceptTests
+      \brief Contains macros for testing concept definitions (and related constructions).
+    */
+
     namespace ConceptTests {
 
+      /*!
+        \define OKLIB_BASIC_CONCEPT_TEST_TAG(concept)
+        \brief Tests, whether with concept a concept tag is associated.
+      */
+
 #define OKLIB_BASIC_CONCEPT_TEST_TAG(concept) BOOST_STATIC_ASSERT(IsConceptTag< concept ## _tag >::value);
+
+      /*!
+        \define OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(concept)
+        \brief Tests, whether with concept an archetype is associated which fulfills the concept.
+      */
 
 #define OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(concept) template class concept< concept ## _Archetype >;
 
