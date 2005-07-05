e21f0c35e7453e0601a4d4fa4a42dd2d1a2462c3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-05 16:45:36 +0000

    New implementation of concept_tag<T>, which is now always defined (but does not contain a member "type" if a concept tag could not be retrieved),

--- Concepts/Traits.hpp
+++ Concepts/Traits.hpp
@@ -4,14 +4,28 @@
 
 #define TRAITS_khBnBt6
 
+#include <boost/mpl/has_xxx.hpp>
+
 namespace OKlib {
 
   namespace Concepts {
 
+    /*!
+      \class concept_tag<T>
+      \brief Traits metafunction, which returns no type in case concept_tag of T is not defined.
+    */
+
+    namespace implementation_concept_tag {
+      BOOST_MPL_HAS_XXX_TRAIT_DEF(concept_tag);
+      template <bool has_concept_tag, typename T>
+      struct get_concept_tag {};
+      template <typename T>
+      struct get_concept_tag<true, T> {
+        typedef typename T::concept_tag type;
+      };
+    }
     template <class T>
-    struct concept_tag {
-      typedef typename T::concept_tag type;
-    };
+    struct concept_tag : implementation_concept_tag::get_concept_tag<implementation_concept_tag::has_concept_tag<T>::value, T> {};
 
     // -------------------------------------------------------------------------------------------------------------------
 
