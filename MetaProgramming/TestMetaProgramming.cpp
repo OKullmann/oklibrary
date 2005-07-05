f6a8cb806381b3911b3ab17e95a34da587464337
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-05 16:42:15 +0000

    Added testobjects for TypeTraitsContainer.

--- MetaProgramming/TestMetaProgramming.cpp
+++ MetaProgramming/TestMetaProgramming.cpp
@@ -7,6 +7,7 @@
 #include "Logical_Testobjects.hpp"
 #include "TypeTraits_Testobjects.hpp"
 #include "TaggingPolymorphism_Testobjects.hpp"
+#include "TypeTraitsContainer_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
