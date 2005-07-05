adc70e07a3e6d77f52556d06584595bd0d1758fc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-05 16:41:30 +0000

    Traits metafunctions is_std_container and has_size_function.

---
+++ MetaProgramming/TypeTraitsContainer.hpp
@@ -0,0 +1,44 @@
+// Oliver Kullmann, 5.6.2005 (Swansea)
+
+/*!
+  \file TypeTraits.hpp
+  \brief Extending the (general) type traits from Boost.
+*/
+
+#ifndef TYPETRAITSCONTAINER_0099io
+
+#define TYPETRAITSCONTAINER_0099io
+
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+
+#include "std_Container.hpp"
+#include "ConceptsMetafunctions.hpp"
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+    template <typename T>
+    struct is_std_container :  ::boost::mpl::and_<typename OKlib::Concepts::HasConceptTag<T>::type, typename OKlib::Concepts::IsTagModel<T, OKlib::Concepts::Container_tag>::type> {};
+    template <typename T>
+    struct is_std_container<std::vector<T> > :  ::boost::mpl::bool_<true> {};
+    template <typename T>
+    struct is_std_container<std::list<T> > :  ::boost::mpl::bool_<true> {};
+    template <typename T>
+    struct is_std_container<std::deque<T> > :  ::boost::mpl::bool_<true> {};
+    template <typename T>
+    struct is_std_container<std::set<T> > :  ::boost::mpl::bool_<true> {};
+    template <typename T>
+    struct is_std_container<std::multiset<T> > :  ::boost::mpl::bool_<true> {};
+
+    template <typename T>
+    struct has_size_function : is_std_container<T> {};
+
+  }
+
+}
+
+#endif
