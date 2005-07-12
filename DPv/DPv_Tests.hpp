70ce4fa124fd3a403fc4f32f25267d59ec27acbf
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 14:53:31 +0000

    Standard Test File.

---
+++ DPv/DPv_Tests.hpp
@@ -0,0 +1,17 @@
+// Tony H. Bao, 12.07.2005 (Swansea)
+
+#ifndef DPVTESTS_adn9tgrg099993
+
+#define DPVTESTS_adn9tgrg099993
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+  
+  namespace DPv {
+  
+  }
+}
+
+#endif
