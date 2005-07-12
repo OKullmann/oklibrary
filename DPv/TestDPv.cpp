74f5bb854882da955ff666a69d5c43640ce550b4
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 15:05:20 +0000

    Minor Corrections.

--- DPv/TestDPv.cpp
+++ DPv/TestDPv.cpp
@@ -1,7 +1,10 @@
+// Tony H. Bao, 12.07.2005 (Swansea)
+
 #include <iostream>
 
 #include "TestBaseClass.hpp"
 
+#include "DPv_Testobjects.hpp"
 #include "Input_output_Testobjects.hpp"
 #include "BasicDataStructure_Testobjects.hpp"
 #include "Statistics_Testobjects.hpp"
