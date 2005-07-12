a7352230547f3708fe41a88dee4d621f7dcd80fd
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 15:08:02 +0000

    Standard Test File.

---
+++ IOTools/IOTools_Models_Definitions_Tests.hpp
@@ -0,0 +1,17 @@
+// Tony H. Bao, 12.07.2005 (Swansea)
+
+#ifndef IOTOOLSMODELSDEFINITIONSTESTS_FSA0H8ytgggpooytd
+
+#define IOTOOLSMODELSDEFINITIONSTESTS_FSA0H8ytgggpooytd
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+  
+  namespace IOTools {
+  
+  }
+}
+
+#endif
