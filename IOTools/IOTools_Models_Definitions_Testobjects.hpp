466174f8b5a170dab64512dc2d71de89830d75eb
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 15:07:44 +0000

    Standard Testobject File.

---
+++ IOTools/IOTools_Models_Definitions_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Tony H. Bao, 12.07.2005 (Swansea)
+
+#ifndef IOTOOLSMODELSDEFINITIONSTESTOBJECTS_hht9igweetgad
+
+#define IOTOOLSMODELSDEFINITIONSTESTOBJECTS_hht9igweetgad
+
+#include "IOTools_Models_Definitions.hpp"
+#include "IOTools_Models_Definitions_Tests.hpp"
+
+namespace OKlib {
+
+  namespace IOTools {
+    
+    namespace Testobjects {
+
+    
+    }
+  }
+}
+#endif
