d6e28b5129a6df3ac12a8b664ca4cd99422238a3
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 14:52:53 +0000

    Standard Testobject File.

---
+++ DPv/DPv_Testobjects.hpp
@@ -0,0 +1,20 @@
+// Tony H. Bao, 12.07.2005 (Swansea)
+
+#ifndef DPVTESTOBJECTS_DFSSAU8777ggt3tr9
+
+#define DPVTESTOBJECTS_DFSSAU8777ggt3tr9
+
+#include "DPv.hpp"
+#include "DPv_Tests.hpp"
+
+namespace OKlib {
+
+  namespace DPv {
+    
+    namespace Testobjects {
+
+    
+    }
+  }
+}
+#endif
