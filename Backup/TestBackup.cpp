6dcbc91dcca3274dded899bb4a6c52261ef48359
Tony Bao <cstony@Swansea.ac.uk>
2005-07-12 14:09:21 +0000

    Updated to making use of the testing framework.

--- Backup/TestBackup.cpp
+++ Backup/TestBackup.cpp
@@ -2,10 +2,16 @@
 
 // Temporary solution; to be displaced by the automatic system
 
-#include "Backup.hpp"
-#include "Command_Caller.hpp"
-#include "Info_Holder.hpp"
-#include "Path_Loader.hpp"
+#include <iostream>
 
-int main() {}
+#include "TestBaseClass.hpp"
+
+#include "Backup_Testobjects.hpp"
+#include "Command_Caller_Testobjects.hpp"
+#include "Info_Holder_Testobjects.hpp"
+#include "Path_Loader_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+}
 
