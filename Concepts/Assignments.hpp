8365ff136fcf030fe14f83aa114a5cea988ab650
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-25 12:52:00 +0000

    Concepts for assignments (partial and total).

---
+++ Concepts/Assignments.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 24.7.2005 (Swansea)
+
+/*!
+  \file Assignments.hpp
+  \brief Concepts for partial and total assignments.
+  \todo Concepts for assignments as specialisations of concepts for maps (see my notes as discussed in the MSc thesis of B): If f is an assignment, then dom(f) is defined, and for v of type dom(f) the expression f(v) can be build (v is a variable with values, f(v) is of the value type). dom(f) is a set in the general sense (which can have iterators and element-decision). As a refinement iterators through all pairs (x, y) (so that std::map can easily yield models).
+*/
+
+#ifndef ASSIGNMENTS_nfgqtqp6
+
+#define ASSIGNMENTS_nfgqtqp6
+
+namespace OKlib {
+
+  namespace Concepts {
+
+  }
+
+}
+
+#endif
---
+++ Concepts/Assignments_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 24.7.2005 (Swansea)
+
+#ifndef ASSIGNMENTSTESTOBJECTS_kkhgd8
+
+#define ASSIGNMENTSTESTOBJECTS_kkhgd8
+
+#include "Assignments.hpp"
+#include "Assignments_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Concepts {
+
+  }
+
+}
+
+#endif
