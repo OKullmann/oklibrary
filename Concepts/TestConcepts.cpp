6e25f62bff1a8562afd40bc21f44e2956fd4241a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-25 12:52:23 +0000

    New testobject-files included.

--- Concepts/TestConcepts.cpp
+++ Concepts/TestConcepts.cpp
@@ -13,6 +13,11 @@
 #include "Variables_Testobjects.hpp"
 #include "ResultElements_Testobjects.hpp"
 #include "std_Container_Testobjects.hpp"
+#include "Literals_Testobjects.hpp"
+#include "Clauses_Testobjects.hpp"
+#include "ClauseSets_Testobjects.hpp"
+#include "Assignments_Testobjects.hpp"
+#include "BipartiteGraphs_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
