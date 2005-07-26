714e40c906f9a6bd0836c570013fb0373012c99e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-26 15:01:51 +0000

    More Doxygen-comments regarding the concept of a bipartite graph.

--- Concepts/BipartiteGraphs.hpp
+++ Concepts/BipartiteGraphs.hpp
@@ -8,6 +8,9 @@
   deleting vertices (from both parts), and the corresponding undo-operations.
   \todo Literal-clause graphs and vertex-hyperede graphs yield bipartite graphs; for the former
   we have special relations between the literals (via the underlying variable).
+  \todo It seems, that the concept of bipartite graphs is (yet) most important as concept ("notion"),
+  while models are provided only via hypergraphs and clause-sets, where the bipartite graphs
+  are directly "embedded" into the data structure.
 */
 
 #ifndef BIPARTITEGRAPHS_kbdgqt52
