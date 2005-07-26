3c877ccfd35715c033e083c14e273f04ec20e4da
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-07-26 15:03:06 +0000

    Filename "Literals.hpp" disambiguated (it also exists below "OKsolver").

--- Concepts/Literals.hpp
+++ Concepts/Literals.hpp
@@ -1,7 +1,7 @@
 // Oliver Kullmann, 24.7.2005 (Swansea)
 
 /*!
-  \file Literals.hpp
+  \file Concepts/Literals.hpp
   \brief Concepts for literals.
   \todo Concepts for general literals and variable-based literals.
 */
