b198b64ff81a70b74012b140228f63ba305eeca0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-10 16:28:34 +0000

    Standard test program.

---
+++ SumProduct/TestSumProduct.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "Marginalisation_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+} 
