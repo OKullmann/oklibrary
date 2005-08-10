85e7469b667e0afb8b9dcb6a001e20bf09e8da61
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-10 16:29:20 +0000

    Module for basic marginalisation (brute force; yet empty).

---
+++ SumProduct/Marginalisation_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 10.8.2005 (Swansea)
+
+#ifndef MARGINALISATIONTESTOBJECTS_9ycxz
+
+#define MARGINALISATIONTESTOBJECTS_9ycxz
+
+#include "Marginalisation.hpp"
+#include "Marginalisation_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SumProduct {
+
+  }
+
+}
+
+#endif
---
+++ SumProduct/Marginalisation_Tests.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 10.8.2005 (Swansea)
+
+/*!
+  \file Marginalisation_Tests.hpp
+  \brief Tests for generic marginalisation.
+*/
+
+#ifndef MARGINALISATIONTESTS_kkjjhy
+
+#define MARGINALISATIONTESTS_kkjjhy
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace SumProduct {
+
+  }
+
+}
+
+#endif
