f7660e661e89a648c1bc3db002648f52747667a3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-15 17:12:12 +0000

    Doxygen-comments edited.

--- Concepts/ClauseSets.hpp
+++ Concepts/ClauseSets.hpp
@@ -3,8 +3,20 @@
 /*!
   \file Concepts/ClauseSets.hpp
   \brief Concepts for clause-sets.
-  \todo Concepts for clause-sets, as clauses where the literals are clauses. Clause-sets as special hypergraphs.
-  \todo Specialising the concept of vertex-hyperedge graphs of hypergraphs: Literal-clause graphs of clause-sets.
+  \todo Concepts for clause-sets, as clauses where the literals are (again) clauses. "Clause-sets as special hypergraphs." --- see below.
+  \todo An important refinement is the concept of a "clause-set with a partial
+  assignment", which supports extending the partial assignment and reversing
+  these operations.
+  \todo Clause-sets should allow efficient access to the related hypergraphs
+  and graphs (so that we can use general hypergraph and graph algorithms;
+  examples for graphs are the resolution graph and the resolution-subsumption
+  graph). This access should happen via simple wrapping of for example clauses
+  or variables into node classes (the type of these node classes specifies
+  the type of graph).
+  \todo It is actually not clear, whether clause-sets should be regarded as
+  *special hypergraphs* (with additional structures), or whether they should
+  be regarded as *generalised* hypergraphs (from which hypergraphs and
+  graphs can be derived). Likely the second view is the better one.
 */
 
 #ifndef CLAUSESETS_0YHB5
