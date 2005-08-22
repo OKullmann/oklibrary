f32d94031f7c85990dd992eb9b3aafd20ddef6b8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-22 13:50:21 +0000

    Inclusion of testobjects for Permutations.

--- Combinatorics/TestCombinatorics.cpp
+++ Combinatorics/TestCombinatorics.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "GrayCodes_Testobjects.hpp"
+#include "Permutations_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
