a2334940968a06edd489548311df70d6958b94db
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-26 10:16:57 +0000

    Standard test program.

---
+++ HypergraphTransversals/TestHypergraphTransversals.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "DirectTransversalEnumeration_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cout);
+}
