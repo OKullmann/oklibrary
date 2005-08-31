d0033e0732975d7f47bed5aae3e04f586ce4b57c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-31 10:18:56 +0000

    Standard system.

---
+++ AutarkySearch/TestAutarkySearch.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "AnalyseTotalAssignment_Testobjects.hpp"
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+} 
