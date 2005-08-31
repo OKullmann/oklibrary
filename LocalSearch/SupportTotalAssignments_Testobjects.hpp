f4588c020f66b8943be1dd558893f5e7868deeb2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-08-31 10:19:25 +0000

    Datastructures supporting local search.

---
+++ LocalSearch/SupportTotalAssignments_Testobjects.hpp
@@ -0,0 +1,16 @@
+#ifndef SUPPORTTOTALASSIGNMENTSTESTOBJECTS_klju76
+
+#define SUPPORTTOTALASSIGNMENTSTESTOBJECTS_klju76
+
+#include "SupportTotalAssignments.hpp"
+#include "SupportTotalAssignments_Tests.hpp"
+
+namespace OKlib {
+
+  namespace LocalSearch {
+
+  }
+
+}
+
+#endif
---
+++ LocalSearch/SupportTotalAssignments_Tests.hpp
@@ -0,0 +1,21 @@
+/*!
+  \file SupportTotalAssignments_Tests.hpp
+  \brief Tests for support data structures for total assignments.
+*/
+
+#ifndef SUPPORTTOTALASSIGNMENTSTESTS_okMMnh
+
+#define SUPPORTTOTALASSIGNMENTSTESTS_okMMnh
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace LocalSearch {
+
+  }
+
+}
+
+#endif
