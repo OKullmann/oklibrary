a22e5bdcd06032e7b6da2fa7c330b5d2137091d8
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-09-07 10:29:25 +0000

    Added test class template:  Test_AssociativePrefixContainer

--- SearchDataStructures/AssociativeContainers_Tests.hpp
+++ SearchDataStructures/AssociativeContainers_Tests.hpp
@@ -14,6 +14,20 @@ namespace OKlib {
 
   namespace SearchDataStructures {
 
+    template <class PrefixContainer>
+    class Test_AssociativePrefixContainer : public ::OKlib::TestSystem::Test {
+      //PrefixContainer& container;
+      const std::string test_string;
+    public :
+      typedef Test_AssociativePrefixContainer test_type;
+      Test_AssociativePrefixContainer(const std::string& test_string) : test_string(test_string) {}
+    private :
+      void perform_test_trivial() {
+        AssociativePrefixContainer<std::string> prefix_container;
+        prefix_container.insert("Hello");
+      }
+    };
+
   }
 
 }
