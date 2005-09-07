a2f4f88e2fbd6bc726fbf68e6f935bcf2646219a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-09-07 15:09:18 +0000

    First basic test for asociative prefix containers completed.

--- SearchDataStructures/AssociativeContainers_Tests.hpp
+++ SearchDataStructures/AssociativeContainers_Tests.hpp
@@ -7,6 +7,8 @@
 
 #define ASSOCIATIVECONTAINERSTESTS_okMMnh
 
+#include <string>
+
 #include "TestBaseClass.hpp"
 #include "TestExceptions.hpp"
 
@@ -14,18 +16,55 @@ namespace OKlib {
 
   namespace SearchDataStructures {
 
-    //template <class PrefixContainer>
-    class Test_AssociativePrefixContainer : public ::OKlib::TestSystem::Test {
+    template <template <class Range> class PrefixContainer>
+    class Test_AssociativePrefixContainer : public ::OKlib::TestSystem::TestBase {
     public :
-      // typedef Test_AssociativePrefixContainer test_type;
+      typedef Test_AssociativePrefixContainer test_type;
+      Test_AssociativePrefixContainer() {
+        insert(this);
+      }
     private :
       void perform_test_trivial() {
-        AssociativePrefixContainer<std::string> prefix_container;
-        prefix_container.insert("test.hpp/dir1/dir0/");
-	prefix_container.insert("test.hpp/dir1");
-        prefix_container.end();
-        prefix_container.begin();
-        prefix_container.first_extension("test.hpp");
+        {
+          typedef PrefixContainer<std::string> APC;
+          typedef typename APC::iterator iterator;
+
+          APC prefix_container;
+
+          if (prefix_container.begin() != prefix_container.end())
+            OKLIB_THROW("prefix_container.begin() != prefix_container.end()");
+          
+          const std::string test_string("test.hpp");
+          
+          {
+            const iterator& found(prefix_container.first_extension(test_string));
+            const iterator& end(prefix_container.end());
+            if (found != end)
+              OKLIB_THROW("found != end");
+          }
+
+          const std::string word1("test.hpp/dir1/dir0/");
+          prefix_container.insert(word1);
+          if (prefix_container.begin() == prefix_container.end())
+            OKLIB_THROW("prefix_container.begin() == prefix_container.end()");
+
+          {
+            const iterator& found(prefix_container.first_extension(test_string));
+            const iterator& end(prefix_container.end());
+            OKLIB_TEST_NOTEQUAL(found, end);
+            OKLIB_TEST_EQUAL(*found, word1);
+          }
+
+          const std::string word2("test.hpp/dir1");
+          prefix_container.insert(word2);
+
+          {
+            const iterator& found(prefix_container.first_extension(test_string));
+            const iterator& end(prefix_container.end());
+            OKLIB_TEST_NOTEQUAL(found, end);
+            OKLIB_TEST_EQUAL(*found, word2);
+          }
+        }
       }
     };
 
