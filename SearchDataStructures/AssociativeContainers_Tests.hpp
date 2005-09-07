b7bb4e94595f4be48a3e2f866a0c12b73c3476a8
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-09-07 13:26:45 +0000

    Changed Test_AssociativePrefixContainer from template class to non-template class. Also added some more trivial tests to perform_test_trivial() function.

--- SearchDataStructures/AssociativeContainers_Tests.hpp
+++ SearchDataStructures/AssociativeContainers_Tests.hpp
@@ -14,17 +14,18 @@ namespace OKlib {
 
   namespace SearchDataStructures {
 
-    template <class PrefixContainer>
+    //template <class PrefixContainer>
     class Test_AssociativePrefixContainer : public ::OKlib::TestSystem::Test {
-      //PrefixContainer& container;
-      const std::string test_string;
     public :
-      typedef Test_AssociativePrefixContainer test_type;
-      Test_AssociativePrefixContainer(const std::string& test_string) : test_string(test_string) {}
+      // typedef Test_AssociativePrefixContainer test_type;
     private :
       void perform_test_trivial() {
         AssociativePrefixContainer<std::string> prefix_container;
-        prefix_container.insert("Hello");
+        prefix_container.insert("test.hpp/dir1/dir0/");
+	prefix_container.insert("test.hpp/dir1");
+        prefix_container.end();
+        prefix_container.begin();
+        prefix_container.first_extension("test.hpp");
       }
     };
 
