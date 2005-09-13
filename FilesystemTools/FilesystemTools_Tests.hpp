986908ad921fe208f06e72300ac3972ab930eef4
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-09-13 12:29:22 +0000

    Added Test_DirectoryIterator class template.

--- FilesystemTools/FilesystemTools_Tests.hpp
+++ FilesystemTools/FilesystemTools_Tests.hpp
@@ -3,16 +3,40 @@
   \brief Tests for search data structures.
 */
 
-#ifndef FILESYSTEMTOOLSTESTS_okMMnh
+#ifndef FILESYSTEMTOOLSTESTS_oui453
 
-#define FILESYSTEMTOOLSTESTS_okMMnh
+#define FILESYSTEMTOOLSTESTS_oui453
 
+#include <boost/filesystem/path.hpp> 
+#include <iostream>
+
+#include "boost/filesystem/fstream.hpp" 
 #include "TestBaseClass.hpp"
 #include "TestExceptions.hpp"
 
 namespace OKlib {
 
   namespace FilesystemTools {
+
+    template <class DirectoryIterator>
+    class Test_DirectoryIterator : public ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_DirectoryIterator test_type;
+      Test_DirectoryIterator() {
+        insert(this);
+      }
+    private :
+      void perform_test_trivial() {
+        {
+//           typedef DirectoryIterator DirIterator;
+//  	  boost::filesystem::path path("/home/csmatthew/test.hpp");
+//  	  boost::filesystem::ofstream file( path );
+//           file << "Test\n";
+//           file.close();	
+	  
+        }
+      }
+    };
 
   }
 
