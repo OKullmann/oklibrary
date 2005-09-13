14821cbca10397a512756c9b51f4c7d4d5f76a5d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-09-13 16:58:48 +0000

    What to do with that module (it's meant for OKgenerator).

---
+++ RandomGenerator/RandomGenerator_Plans.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 13.9.2005 (Swansea)
+
+/*!
+  \file RandomGenerator_Plans
+  \brief Plans for the module RandomGenerator.
+  \todo The first thing is to transfer (and upgrade) the
+  implementation of OKgenerator. This should proceed in the
+  following stages:
+   1. Creating the concept of BigIntegers in the module NumberTheory
+   together with the wrapper for GMP.
+   2. Replacing the current big-integer implementation in OKgenerator.
+   3. Creating the concept for AES in the (new) module Cryptology.
+   4. Writing a wrapper for some existing implementations for AES.
+   5. Replacing the current AES implementation in OKgenerator.
+   6. Then one needs to decide what about the input parameter handling
+      (writing our own module, or using Boost).
+*/
+
