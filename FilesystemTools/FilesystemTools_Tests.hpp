6e57f36da7a61e6275f8d91e13a110c27d32d455
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-09-19 14:14:30 +0000

    More trivial tests of DirectoryIterator.

--- FilesystemTools/FilesystemTools_Tests.hpp
+++ FilesystemTools/FilesystemTools_Tests.hpp
@@ -34,12 +34,18 @@ namespace OKlib {
       void perform_test_trivial() {
         OKLIB_MODELS_CONCEPT_REQUIRES(DirectoryIterator, OKlib::Concepts::InputIterator);
         {
-          typedef DirectoryIterator DirIterator;
+          typedef DirectoryIterator DirIt;
           typedef boost::filesystem::path path;
+          typedef boost::filesystem::directory_iterator DirIt_Boost;
           const std::string TestDirectory("TestDirectory");
           path test_path(TestDirectory);
           assert(boost::filesystem::exists(test_path));
-	  
+          const std::string TestFile("TestDirectory/Test_001.hpp");
+          path test_path_2(TestFile);
+          assert(boost::filesystem::exists(test_path_2));
+          DirIt dir_it(test_path);
+          DirIt_Boost boost_dir_it(test_path);
+	  DirIt dir_it_2(boost_dir_it);
         }
       }
     };
