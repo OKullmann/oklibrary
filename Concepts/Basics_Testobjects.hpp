fcfec289154e3a197598c0c8115e479a61e36a0b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-09-22 14:38:40 +0000

    Added new concept-checks.

--- Concepts/Basics_Testobjects.hpp
+++ Concepts/Basics_Testobjects.hpp
@@ -13,23 +13,26 @@ namespace OKlib {
 
   namespace Concepts {
 
-      OKLIB_BASIC_CONCEPT_TEST_TAG(FullyEqualityComparable);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyEqualityComparable);
+    OKLIB_BASIC_CONCEPT_TEST_TAG(FullyEqualityComparable);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyEqualityComparable);
+    
+    OKLIB_BASIC_CONCEPT_TEST_TAG(FullyLessThanComparable);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyLessThanComparable);
 
-      OKLIB_BASIC_CONCEPT_TEST_TAG(FullyLessThanComparable);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyLessThanComparable);
+    OKLIB_BASIC_CONCEPT_TEST_TAG(LinearOrder);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(LinearOrder);
 
-      OKLIB_BASIC_CONCEPT_TEST_TAG(LinearOrder);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(LinearOrder);
+    OKLIB_BASIC_CONCEPT_TEST_TAG(FullyConstructible);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyConstructible);
 
-      OKLIB_BASIC_CONCEPT_TEST_TAG(FullyConstructible);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyConstructible);
+    OKLIB_BASIC_CONCEPT_TEST_TAG(FullyConstructibleEq);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(FullyConstructibleEq);
+    
+    OKLIB_BASIC_CONCEPT_TEST_TAG(EqualitySubstitutable);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(EqualitySubstitutable);
 
-      OKLIB_BASIC_CONCEPT_TEST_TAG(EqualitySubstitutable);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(EqualitySubstitutable);
-
-      OKLIB_BASIC_CONCEPT_TEST_TAG(ConstCorrect);
-      OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(ConstCorrect);
+    OKLIB_BASIC_CONCEPT_TEST_TAG(ConstCorrect);
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(ConstCorrect);
 
   }
 
