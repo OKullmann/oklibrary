a948ee2a6825ebfb747351e70ec1d3ec34f7150a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-09-23 14:43:48 +0000

    Doxygen-comments edited, definition of concept InputIterator refined, concept definition for MultiPassInputIterator moved to here (from Iterators.hpp) and refined, and definition of concept ForwardIterator added.

--- Concepts/std_Iterators.hpp
+++ Concepts/std_Iterators.hpp
@@ -7,7 +7,14 @@
 
 /*!
   \file std_Iterators.hpp
-  \brief Iterator concepts from ISO/IEC 14882: 2003 (plus const-correctness).
+  \brief Iterator concepts from ISO/IEC 14882: 2003 (plus const-correctness). As for all concepts 
+  in "std_ ...", the concepts here are not refinements of OKlib::Concepts::BasicRequirements.
+
+  Definition of the concepts InputIterator, OutputIterator, ForwardIterator, BidirectionalIterator, RandomAccessIterator plus (additionally) MultiPassInputIterator.
+  \todo Write concepts for
+   - OutputIterator
+   - BidirectionalIterator
+   - RandomAccessIterator.
 */
 
 #ifndef STDITERATORS_pOpt5185
@@ -32,7 +39,7 @@ namespace OKlib {
       \class InputIterator
       \brief Concept InputIterator according to the standard.
 
-      Refines concepts FullyEqualityComparable, Assignable, CopyConstructible.
+      Refines concept ConstructibleCAEq.
       \todo "reference" and "pointer" are never explained in the standard, and the requirements on iterator concepts don't mention them, but they suddenly show up in the iterator traits (24.3.1) ?
       \todo Can expressions "a -> m" be expressed ?
       \todo Create in TypeTraits.hpp a metafunction "is_derived_or_same", and use it for the definition of InputIterator and elsewhere.
@@ -48,18 +55,16 @@ namespace OKlib {
       typedef typename std::iterator_traits<Iterator>::reference reference;
       typedef typename std::iterator_traits<Iterator>::pointer pointer;
       void constraints() {
-        boost::function_requires<FullyEqualityComparable<Iterator> >();
-        boost::function_requires<Assignable<Iterator> >();
-        boost::function_requires<CopyConstructible<Iterator> >();
-        static_cast<value_type>(*a);
-        static_cast<Iterator&>(++r);
-        (void) r++;
-        static_cast<value_type>(*r++);
+        boost::function_requires<ConstructibleCAEq<Iterator> >();
+        static_cast<value_type>(*a); // pre-condition: a is dereferencable
+        static_cast<Iterator&>(++r); // pre-condition: a is dereferencable
+        (void) r++; // pre-condition: a is dereferencable
+        static_cast<value_type>(*r++); // pre-condition: a is dereferencable
       }
-      const Iterator a, b;
+      const Iterator a;
       Iterator& r;
     };
-    struct InputIterator_tag : virtual ConceptsBase_tag {};
+    struct InputIterator_tag : virtual ConstructibleCAEq_tag {};
 
     class InputIterator_Archetype {
       InputIterator_Archetype();
@@ -88,6 +93,76 @@ namespace OKlib {
     };
 
     // --------------------------------------------------------------------------------------------------------------------
+
+     /*!
+      \class MultiPassInputIterator
+      \brief Concept MultiPassInputIterator according to Boost.
+
+      According to [The Boost Graph Library, ISBN 0-201-72914-8], Section 16.3 (page 291).
+      Extension of InputIterator; added here so that ForwardIterator can refine this concept.
+    */
+
+    template <typename Iterator>
+    struct MultiPassInputIterator {
+      void constraints() {
+        boost::function_requires<InputIterator<Iterator> >();
+        boost::function_requires<EqualitySubstitutable<Iterator> >();
+      }
+    };
+    struct MultiPassInputIterator_tag : virtual InputIterator_tag, virtual EqualitySubstitutable_tag {};
+
+    struct MultiPassInputIterator_Archetype : InputIterator_Archetype {};
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    template <typename Iterator>
+    struct ForwardIterator {
+      typedef typename std::iterator_traits<Iterator>::value_type value_type;
+      typedef typename std::iterator_traits<Iterator>::iterator_category iterator_category;
+      BOOST_STATIC_ASSERT((::boost::mpl::or_< ::boost::is_same<std::input_iterator_tag, iterator_category>, ::boost::is_base_and_derived<std::forward_iterator_tag, iterator_category> >::value));
+      typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+
+      typedef typename std::iterator_traits<Iterator>::reference reference;
+      typedef typename std::iterator_traits<Iterator>::pointer pointer;
+      void constraints() {
+        boost::function_requires<MultiPassInputIterator<Iterator> >();
+        boost::function_requires<DefaultConstructible<Iterator> >();
+        static_cast<value_type&>(*a);
+        static_cast<Iterator&>(++r);
+        static_cast<const Iterator&>(r++);
+        static_cast<value_type&>(*r++);
+      }
+      const Iterator a;
+      Iterator& r;
+    };
+  struct ForwardIterator_tag : virtual MultiPassInputIterator_tag, virtual DefaultConstructible_tag {};
+
+    class ForwardIterator_Archetype {
+      typedef ForwardIterator_Archetype self;
+    protected :
+      struct convertible_to_bool {
+        operator bool() {}
+      };
+    public:
+      typedef std::input_iterator_tag iterator_category;
+      struct value_type {};
+      typedef std::ptrdiff_t difference_type;
+      typedef value_type* pointer;
+      struct reference {
+        operator value_type&() {}
+      };
+      self& operator=(const self&) {}
+      convertible_to_bool operator==(const self&) const {}
+      convertible_to_bool operator!=(const self&) const {}
+      reference operator*() const {}
+      self& operator++() {}
+      struct internal {
+        value_type& operator*() {}
+        operator const self&() {}
+      };
+      internal operator++(int) {}
+    };
+
 
   }
 
