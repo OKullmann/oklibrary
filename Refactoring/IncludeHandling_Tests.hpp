4bb3a75e4960ba038ec574305f6fe561062b258e
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-10-03 11:47:13 +0000

    Adhoc testing of include directive extraction begun.

--- Refactoring/IncludeHandling_Tests.hpp
+++ Refactoring/IncludeHandling_Tests.hpp
@@ -10,6 +10,9 @@
 #define INCLUDEHANDLINGTESTS_77665r
 
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <utility>
 
 #include <boost/filesystem/fstream.hpp>
 
@@ -26,23 +29,54 @@ namespace OKlib {
     */
 
     template <template <class String> class Include_Directive>
-    class Test_IncludeHandling : public ::OKlib::TestSystem::TestBase {
+    class Test_IncludeDirective : public ::OKlib::TestSystem::TestBase {
     public :
-      typedef Test_IncludeHandling test_type;
-      Test_IncludeHandling() {
+      typedef Test_IncludeDirective test_type;
+      Test_IncludeDirective() {
         insert(this);
       }
     private :
       void perform_test_trivial() {
         {
-          typedef Include_Directive<std::string> ID;
-	  int a = 3;
-	  int b = 4;
-	  std::string c = "iostream";
-	  Include_forms d = system_header;
-          ID include_directive(c,a,b,d);
-  	  boost::filesystem::ofstream file( "/home/csmatthew/test.hpp" );
-          file << include_directive;
+          typedef Include_Directive<std::string> IncludeDirective;
+	  int spaces_after_hash = 0;
+	  int spaces_after_include = 1;
+          std::string header = "iostream";
+	  Include_forms include_form = system_header;
+          IncludeDirective include_directive(header,spaces_after_hash,spaces_after_include,include_form);
+          // \todo Test output streaming of IncludeDirective
+        }
+      }
+    };
+
+    template <template <class String> class Extract_include_directives>
+    class Test_Extract_include_directives : public ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_Extract_include_directives test_type;
+      Test_Extract_include_directives() {
+        insert(this);
+      }
+    private :
+      void perform_test_trivial() {
+        {
+          std::ifstream input("IncludeHandling.hpp");
+          Extract_include_directives<std::string> extract;
+          std::ofstream output("output.txt");
+          extract.preface() = "The preface...";
+          input >> extract;
+          output << extract;
+
+          typedef IncludeDirective<std::string> IncludeDirective;
+          int spaces_after_hash = 0;
+          int spaces_after_include = 1;
+          std::string header = "iostream";
+          Include_forms include_form = system_header;
+          IncludeDirective include_directive(header,spaces_after_hash,spaces_after_include,include_form);
+          std::string test_string("Some following text..");
+          std::pair<IncludeDirective,std::string> test_pair(include_directive,test_string);
+          extract.push_back(test_pair);
+          
+                
         }
       }
     };
