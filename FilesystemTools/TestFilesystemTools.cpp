7a67a117e01a6326eda6aca5744491e61193850a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-11 14:22:59 +0000

    Testobjects for IOStreamFilters included.

--- FilesystemTools/TestFilesystemTools.cpp
+++ FilesystemTools/TestFilesystemTools.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "FilesystemTools_Testobjects.hpp"
+#include "IOStreamFilters_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
