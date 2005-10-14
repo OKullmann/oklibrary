bac685694e5a5eaf81de064b976ec491fa14139e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-14 11:41:34 +0000

    Testobjects for Exceptions added.

--- InputOutput/TestInputOutput.cpp
+++ InputOutput/TestInputOutput.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "Dimacs_Testobjects.hpp"
+#include "Exceptions_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
