79a03dda809a5868718728873680614768ff4a7c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-14 11:40:16 +0000

    Implementation of StandardDIMACSInput completed (but not tested yet).

--- InputOutput/Dimacs.hpp
+++ InputOutput/Dimacs.hpp
@@ -25,11 +25,15 @@
 #include <cassert>
 #include <string>
 #include <set>
+#include <sstream>
 
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/ref.hpp>
+#include <boost/algorithm/string.hpp>
 
 #include "IOStreamFilters.hpp"
+
+#include "Exceptions.hpp"
 
 namespace OKlib {
   
@@ -38,12 +42,9 @@ namespace OKlib {
     /*!
       \class StandardDIMACSInput
       \brief Parsing an input stream containing a cnf formula in DIMACS format and transferring it to a CLS-adaptor.
-      \todo To be completed:
-       - Create exception class hierarchy.
-       - Create helper function for writing error messages (with stream positions).
-       - Replace the #-parts below.
       \todo Write concept for CLSAdaptor.
       \todo Write simple CLSAdaptor, which creates only statistics (but does not store the formula).
+      \todo Use Message for messages.
     */
 
     template <class CLSAdaptor, typename Int = int>
@@ -61,51 +62,71 @@ namespace OKlib {
       typedef Int int_type;
 
       StandardDIMACSInput(std::istream& in_stream, cls_adaptor_type& out) : out(out) {
-        // if (not in_stream) throw "can't open" ################
+        if (not in_stream)
+          throw IStreamError("OKlib::InputOutput::StandardDIMACSInput::StandardDIMACSInput(std::istream&, cls_adaptor_type&):\n  can't open the input stream in_stream");
         in.push(boost::ref(counter));
         in.push(in_stream);
-        // if (not in) throw "can't create" ######################
+        if (not in) throw IStreamError("OKlib::InputOutput::StandardDIMACSInput::StandardDIMACSInput(std::istream&, cls_adaptor_type&):\n  boost::iostreams::filtering_istream is defective");
         parse();
       }
+
     private :
+
+      std::string error_location() const {
+        std::stringstream s;
+        s << "line " << counter.lines() << ", column " << counter.current_characters() << ", total characters read " << counter.characters();
+        return s.str();
+      }
+
       void parse() {
         read_comments();
         read_parameter_line();
         read_clauses();
       }
+
       void read_comments() {
         assert(in);
         for (;;) {
           peek = in.get();
-          // if (not in) throw "end of input before parameter section" ########################
+          if (not in)
+            throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_comments:\n  end of input before parameter section started\n" + error_location());
           if (peek != 'c') return;
           std::string comment_line;
           std::getline(in, comment_line);
           out.comment(comment_line);
         }
       }
+
       void read_parameter_line() {
         assert(in);
-        // if (peek != 'p') throw "no parameter section"; ########################
+        if (peek != 'p')
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  no parameter section\n" + error_location());
         peek = in.get();
-        // if (not in) throw "read error" ###########################
-        // if (not peek == ' ') throw "no space as second character in parameter line" ###################
+        if (not in)
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  end of input after \"p\" in the parameter line\n" + error_location());
+        if (not peek == ' ')
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  no space as second character in parameter line\n" + error_location());
         std::string label;
         in >> label;
-        // if (not in) throw "read error" #######################
-        // if (label != "cnf") throw "syntax error parameter line" ########################
+         throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  end of input after \"p \" in the parameter line\n" + error_location());
+         if (label != "cnf")
+           throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  instead of the label \"cnf\" the string \"" + label + "\" was found\n" + error_location());
         int_type n;
         in >> n;
-        // if (not in) throw "syntax error parameter line" #########################
+        if (not in)
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  syntax error when reading the maximal index used for variables from the parameter line\n" + error_location());
         out.n(n);
         int_type c;
         in >> c;
-        // if (not in) throw "syntax error parameter line" #########################
+        if (not in)
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  syntax error when reading the number of clauses from the parameter line\n" + error_location());
         out.c(c);
         std::string rest_of_line;
         std::getline(in, rest_of_line);
-        // if (not in) throw "no clauses; unexpected end of file" #####################
-        // if (not rest_of_line only space symbols) throw "other content at end of parameter line #################
+        if (not in)
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  unexpected end of file in the parameter line\n" + error_location());
+        if (not boost::algorithm::all(rest_of_line, boost::algorithm::is_space()))
+          throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_parameter_line:\n  a trailing string \"" + rest_of_line + "\" was found in the parameter line\n" + error_location());
       }
         
       void read_clauses() {
@@ -120,9 +141,12 @@ namespace OKlib {
         
         for (int_type literal;;) {
           in >> literal;
-          // if (not in)
-          //   if (total_clause_size == 0) { out.finish(); return; }
-          //   else throw "unexpected end of file before clause finished (or perhaps syntax error)" #######################
+          if (not in)
+            if (total_clause_size == 0) {
+              out.finish(); return;
+            }
+            else
+              throw DimacsError("OKlib::InputOutput::StandardDIMACSInput::read_clauses:\n  syntax error before clause finished\n" + error_location());
           ++total_clause_size;
           if (literal == 0) {
             if (tautological) {
@@ -130,7 +154,7 @@ namespace OKlib {
               tautological = false;
             }
             else
-              out.clause(clause.begin(), clause.end(), clause.size(), total_clause_size); // perhaps better as range?
+              out.clause(clause, total_clause_size); // "clause" is a range here (this should include the size)
             clause.clear();
             total_clause_size = 0;
           }
