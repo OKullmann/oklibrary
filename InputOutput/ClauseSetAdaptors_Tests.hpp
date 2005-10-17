53bfa8947855ab52cdf08975529cb5cfb262d542
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-17 09:57:19 +0000

    Tests missing yet.

---
+++ InputOutput/ClauseSetAdaptors_Tests.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 14.10.2005 (Swansea)
+
+/*!
+  \file ClauseSetAdaptors_Tests.hpp
+  \brief Tests for adaptors to transfer clause-sets into some data structure
+  \todo Write test for CLSAdaptorStatistics.
+  \todo Write test for CLSAdaptorDIMACSOutput.
+*/
+
+#ifndef CLAUSESETADAPTORSTESTS_ooKzaq2
+
+#define CLAUSESETADAPTORSTESTS_ooKzaq2
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace InputOutput {
+
+  }
+
+}
+
+#endif
