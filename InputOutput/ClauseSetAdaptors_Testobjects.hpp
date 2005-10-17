3712a944b0be86cd9521d7f97a152f66486e3503
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-17 09:57:29 +0000

    *** empty log message ***

---
+++ InputOutput/ClauseSetAdaptors_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 14.10.2005 (Swansea)
+
+#ifndef CLAUSESETADAPTORSTESTOBJECTS_g5cmk
+
+#define CLAUSESETADAPTORSTESTOBJECTS_g5cmk
+
+#include "ClauseSetAdaptors.hpp"
+#include "ClauseSetAdaptors_Tests.hpp"
+
+namespace OKlib {
+
+  namespace InputOutput {
+
+  }
+
+}
+
+#endif
