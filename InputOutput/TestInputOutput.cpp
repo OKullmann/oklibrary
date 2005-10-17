866354a6fb4df0b328f14005c8743bcd4fc14cf4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-17 09:58:30 +0000

    Testobjects for ClauseSetAdaptors included.

--- InputOutput/TestInputOutput.cpp
+++ InputOutput/TestInputOutput.cpp
@@ -4,6 +4,7 @@
 
 #include "Dimacs_Testobjects.hpp"
 #include "Exceptions_Testobjects.hpp"
+#include "ClauseSetAdaptors_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
