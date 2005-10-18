61b117e89e4b2f07a91f466c4c147702448be116
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-18 14:53:21 +0000

    Doxygen-comments added.

--- SATAlgorithms/Enumeration.hpp
+++ SATAlgorithms/Enumeration.hpp
@@ -9,8 +9,14 @@
   of the formula (which can be implemented efficiently using literal-clause graphs
   and storing information about the assignments for each clause; this data structure
   is also fundamental for local-search algorithms (and in general any algorithm using
-  total assignments).
+  total assignments)).
   \todo Another variation is the search for autarkies.
+  \todo The trivial algorithm should at least compute the number of satisfying assignments
+  (if not stopped after finding the first one) and the maximal number of simultaneously
+  satisfiable clauses. Optionally he can also decided minimal unsatisfiability of F by the following
+  process: Each time an assignment satisfying c(F)-1 clauses is found, the falsified clause
+  is marked. F is minimally unsatisfiable iff F is unsatisfiable and at the end all clauses have
+  been marked.
 */
 
 #ifndef ENUMERATION_nncvtR9
