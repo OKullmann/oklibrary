a12b54cd97b7684c5fcfd9c88c2b80aa936b82c4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-18 15:05:45 +0000

    New module for enumeration of subsets (or "substructures").

---
+++ Combinatorics/Subsets_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 18.10.2005 (Swansea)
+
+#ifndef SUBSETSTESTOBJECTS_hHhBv
+
+#define SUBSETSTESTOBJECTS_hHhBv
+
+#include "Subsets.hpp"
+#include "Subsets_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Combinatorics {
+
+  }
+
+}
+
+#endif
