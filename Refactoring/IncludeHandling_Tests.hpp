b0a991e626f0567930b085a0bd50bc2017334616
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-10-20 15:14:20 +0000

    Testing of class ProgramRepresentationIncludes for representing the include directives within a program begun.

--- Refactoring/IncludeHandling_Tests.hpp
+++ Refactoring/IncludeHandling_Tests.hpp
@@ -56,7 +56,7 @@ namespace OKlib {
     };
 
     // ------------------------------------------------------------------
-    
+
     /*!
       \class Test_IncludeParsingGrammar
       \brief Testing grammars for parsing source code units in order to extract include directives.
@@ -118,7 +118,73 @@ namespace OKlib {
           OKLIB_TEST_EQUAL(pr, el_p);
         }
       }
-    };    
+    };
+
+    /*!
+      \class Test_ProgramRepresentationIncludes
+      \brief Testing classes for representing the include-directives within a program..
+      \todo Test all functionality.
+    */
+
+    template <template <class charT = char, class traits = std::char_traits<charT>, class Allocator = std::allocator<charT> > class Program_Representation_Includes>
+    class Test_ProgramRepresentationIncludes : public ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_ProgramRepresentationIncludes test_type;
+      Test_ProgramRepresentationIncludes() {
+        insert(this);
+      }
+    private :
+      void perform_test_trivial() {
+        { // ostream tests
+          typedef Program_Representation_Includes<> pr_type;
+          typedef std::string string_type;
+          typedef IncludeDirective<string_type> id_type;
+          typedef id_type::size_type size_type;
+          typedef std::vector<std::pair<id_type, string_type> > container_type;
+          typedef typename container_type::value_type value_type;
+
+          typedef boost::tuple<string_type, string_type, size_type, size_type, Include_forms,string_type,string_type> el_t;
+
+          typedef std::vector<el_t> test_vector_type;
+          test_vector_type test_vector;
+
+          using namespace boost::assign;
+
+          test_vector +=
+            el_t("","iostream",0,1,system_header,"","#include <iostream>"),
+            el_t("","header.hpp",0,1,source_code_header,"","#include \"header.hpp\""),
+            el_t("This is the preface\n","iostream",0,1,system_header,"This is some code after the include directive","This is the preface\n#include <iostream>This is some code after the include directive");
+
+          typedef typename test_vector_type::const_iterator iterator;
+          const iterator& end(test_vector.end());
+
+          for (iterator i = test_vector.begin(); i != end; ++i) {
+            pr_type program;
+            const el_t& el(*i);
+            string_type preface(el.template get<0>());
+            string_type header(el.template get<1>());
+            size_type spaces_after_hash(el.template get<2>());
+            size_type spaces_after_include(el.template get<3>());
+            Include_forms include_form(el.template get<4>());
+            string_type following_text(el.template get<5>());
+            id_type include(header,spaces_after_hash,spaces_after_include,include_form);
+            value_type include_with_context(std::make_pair(include,following_text));
+
+            program(preface); program(include_with_context);
+
+            std::ostringstream output;
+            output << program;
+            std::string output_string = output.str();
+
+            std::string expected_output(el.template get<6>());
+
+            if (not (output_string==expected_output))
+              OKLIB_THROW("output != expected output");
+          }
+
+        }
+      }
+    };
 
   }
 
