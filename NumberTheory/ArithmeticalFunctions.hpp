076b04ffed2dfaa353ef0aa62e2ee9b67d424149
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-21 11:44:14 +0000

    Doxygen-comment updated.

--- NumberTheory/ArithmeticalFunctions.hpp
+++ NumberTheory/ArithmeticalFunctions.hpp
@@ -1,6 +1,11 @@
 // Oliver Kullmann, 13.9.2005 (Swansea)
 
 /*!
+  \namespace OKlib::NumberTheory
+  \brief Number theoretic functions and datastructures.
+*/
+
+/*!
   \file ArithmeticalFunctions.hpp
   \brief Implementations of arithmetical functions like gcd or Euler's phi-function.
   \todo Transferring the content of General::NumberTheory.
