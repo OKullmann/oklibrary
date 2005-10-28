21a3ed9f68f1c4f1baf32ea4ef9534499cfbb461
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-28 11:13:34 +0000

    Class LexicographicalEvaluation was enhanced, so that (by using the appropriate policy) types derived from Series and SuperSeries can be used. Furthermore instead to compute for a solver the map from all "specified series" (pairs of SuperSeries and Series) to numerical evaluation, "currying" took place, and we have now a map from SuperSeries to maps of Series to numerical evaluations.

--- SAT-Competition/LexicographicalEvaluation.hpp
+++ SAT-Competition/LexicographicalEvaluation.hpp
@@ -13,13 +13,15 @@
 
 #define LEXICOGRAPHICALEVALUATION_9fsfs41917
 
-#include <vector>
 #include <map>
 #include <utility>
 
 #include "BasicMapOperations.hpp"
+#include "DerivedRelations.hpp"
 
 #include "SingleResult.hpp"
+
+#include "LexicographicalEvaluationPolicies.hpp"
 
 namespace OKlib {
 
@@ -28,10 +30,13 @@ namespace OKlib {
     /*!
       \class LexicographicalEvaluation
       \brief Given an "indexed database" of results, map each solver to the map, which
-      assigns to each solved series the pair of solved-count and average running time.
+      assigns to each solved super-series the map, which assigns to each solved series
+      the pair of solved-count and average running time.
+      \todo Write two function objects for sorting of LexicographicalEvaluation::map_series_numerics_type.
+      Or better this 
     */
 
-    template <class IndexedDatabase, typename NumberType = double>
+    template <class IndexedDatabase, class SeriesPolicy = LexicographicalSeriesPolicy<Result>, typename NumberType = double>
     class LexicographicalEvaluation {
     public :
 
@@ -47,7 +52,16 @@ namespace OKlib {
       typedef typename seq_solved_benchmarks::size_type size_type;
 
       typedef std::pair<size_type, number_type> numerics_solver_on_series_type;
-      typedef std::map<SpecSeries, numerics_solver_on_series_type> map_solver_evaluation_for_series_type;
+      friend bool operator <(const numerics_solver_on_series_type& lhs, const numerics_solver_on_series_type& rhs) {
+        return lhs.first > rhs.first or (lhs.first == rhs.first and lhs.first != 0 and lhs.second < rhs.second);
+      }
+      // a < b for a, b of type numerics_solver_on_series_type means "a is better than b"
+      OKLIB_DERIVED_RELATIONS_FRIENDS(numerics_solver_on_series_type);
+
+      typedef typename SeriesPolicy::super_series_type super_series_type;
+      typedef typename SeriesPolicy::series_type series_type;
+      typedef std::map<series_type, numerics_solver_on_series_type> map_series_numerics_type;
+      typedef std::map<super_series_type, map_series_numerics_type> map_solver_evaluation_type;
 
       const database_type& idb;
 
@@ -61,40 +75,55 @@ namespace OKlib {
           const Solver& solver(i -> first);
           typedef typename database_type::seq_solved_benchmarks seq_solved_benchmarks;
           const seq_solved_benchmarks& solved_benchmarks(i -> second);
-          map_solver_evaluation_for_series_type result_map;
-          typedef typename seq_solved_benchmarks::const_iterator iterator;
-          const iterator& end(solved_benchmarks.end());
-          for (iterator j(solved_benchmarks.begin()); j != end; ++j) { // loop over all solved benchs
-            typedef typename database_type::SolvedBenchmark solved_benchmark_type;
-            const solved_benchmark_type& solved_bench(*j);
-            const SpecSeries& series(OKlib::SetAlgorithms::map_value(idb.series_of_benchmark(), solved_bench.bench));
-            numerics_solver_on_series_type& ref(result_map[series]);
-            ++ref.first;
-            ref.second += solved_bench.node -> rb -> average().average();
+          map_solver_evaluation_type result_map;
+
+          {
+            typedef typename seq_solved_benchmarks::const_iterator iterator;
+            const iterator& end(solved_benchmarks.end());
+            for (iterator j(solved_benchmarks.begin()); j != end; ++j) { // loop over all solved benchs
+              typedef typename database_type::SolvedBenchmark solved_benchmark_type;
+              const solved_benchmark_type& solved_bench(*j);
+              const super_series_type& super_series(SeriesPolicy::super_series(solved_bench));
+              const series_type& series(SeriesPolicy::series(solved_bench));
+              numerics_solver_on_series_type& ref(result_map[super_series][series]);
+              ++ref.first;
+              ref.second += solved_bench.node -> rb -> average().average();
+            }
           }
-          typedef typename map_solver_evaluation_for_series_type::iterator iterator2;
-          const iterator2 end2(result_map.end());
-          for (iterator2 j(result_map.begin()); j != end2; ++j) {
-            numerics_solver_on_series_type& ref(j -> second);
-            assert(ref.first > 0);
-            ref.second /= ref.first;
+          {
+            typedef typename map_solver_evaluation_type::iterator iterator;
+            const iterator& end(result_map.end());
+            for (iterator j(result_map.begin()); j != end; ++j) {
+              map_series_numerics_type& ref(j -> second);
+              typedef typename map_series_numerics_type::iterator iterator2;
+              const iterator2& end2(ref.end());
+              for (iterator2 k(ref.begin()); k != end2; ++k) {
+                numerics_solver_on_series_type& ref2(k -> second);
+                assert(ref2.first > 0);
+                ref2.second /= ref2.first;
+              }
+            }
+            map.insert(std::make_pair(solver, result_map));
           }
-          map.insert(std::make_pair(solver, result_map));
 
         }
       }
       ~LexicographicalEvaluation() {}
 
-      const map_solver_evaluation_for_series_type& evaluation(const Solver& solver) const {
+      const map_solver_evaluation_type& evaluation(const Solver& solver) const {
         return OKlib::SetAlgorithms::map_value(map, solver);
       }
 
     private :
 
-      typedef std::map<Solver, map_solver_evaluation_for_series_type> map_solver_map_evaluation_type;
+      typedef std::map<Solver, map_solver_evaluation_type> map_solver_map_evaluation_type;
       map_solver_map_evaluation_type map;
       
     };
+
+    // ###########################################################
+
+    
 
   }
 
