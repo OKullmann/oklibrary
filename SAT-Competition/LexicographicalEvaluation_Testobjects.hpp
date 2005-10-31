0ec8792f0ae73889a55640034aeae0923b7d96c2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-10-31 10:47:14 +0000

    Testobjects using the new testfunctions added.

--- SAT-Competition/LexicographicalEvaluation_Testobjects.hpp
+++ SAT-Competition/LexicographicalEvaluation_Testobjects.hpp
@@ -11,8 +11,11 @@ namespace OKlib {
 
   namespace SATCompetition {
 
+    Test_RepresentationSolverSeries<LexicographicalEvaluation> test_representation_solver_series;
+
     Test_LexicographicalEvaluation<LexicographicalEvaluation> test_lexicographical_evaluation;
 
+    Test_LexicographicalEvaluationRandom<LexicographicalEvaluationRandom> test_lexicographical_evaluation_random;
   }
 
 }
