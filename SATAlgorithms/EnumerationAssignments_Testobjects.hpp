99882ca69b2e808daac2d0926464b17983a02a3e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-14 10:24:29 +0000

    Was "Enumeration" before.

---
+++ SATAlgorithms/EnumerationAssignments_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 26.7.2005 (Swansea)
+
+#ifndef ENUMERATIONASSIGNMENTSTESTOBJECTS_9ikjH
+
+#define ENUMERATIONASSIGNMENTSTESTOBJECTS_9ikjH
+
+#include "EnumerationAssignments.hpp"
+#include "EnumerationAssignments_Tests.hpp"
+
+namespace OKlib {
+
+  namespace SATAlgorithms {
+
+  }
+
+}
+
+#endif
---
+++ SATAlgorithms/EnumerationAssignments_Tests.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 26.7.2005 (Swansea)
+
+/*!
+  \file EnumerationAssignments_Tests.hpp
+  \brief Testing SAT algorithms using enumeration.
+*/
+
+#ifndef ENUMERATIONASSIGNMENTSTESTS_6tgHt
+
+#define ENUMERATIONASSIGNMENTSTESTS_6tgHt
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace SATAlgorithms {
+
+  }
+
+}
+
+#endif
