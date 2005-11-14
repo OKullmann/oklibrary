6a3db25160980fd0cd167679bff123f5479762f0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-14 10:26:39 +0000

    Inclusion of new testobject-files.

--- SATAlgorithms/TestSATAlgorithms.cpp
+++ SATAlgorithms/TestSATAlgorithms.cpp
@@ -2,7 +2,8 @@
 
 #include "TestBaseClass.hpp"
 
-#include "Enumeration_Testobjects.hpp"
+#include "EnumerationAssignments_Testobjects.hpp"
+#include "EnumerationSubclausesets_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
