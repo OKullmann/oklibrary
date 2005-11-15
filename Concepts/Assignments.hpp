3f665cfe1eff49dc4ae09a881833901b687e305e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-15 16:25:51 +0000

    \todo updated.

--- Concepts/Assignments.hpp
+++ Concepts/Assignments.hpp
@@ -3,7 +3,7 @@
 /*!
   \file Assignments.hpp
   \brief Concepts for partial and total assignments.
-  \todo Concepts for assignments as specialisations of concepts for maps (see my notes as discussed in the MSc thesis of B): If f is an assignment, then dom(f) is defined, and for v of type dom(f) the expression f(v) can be build (v is a variable with values, f(v) is of the value type). dom(f) is a set in the general sense (which can have iterators and element-decision). As a refinement iterators through all pairs (x, y) (so that std::map can easily yield models).
+  \todo Concepts for assignments as specialisations of concepts for maps (see my notes as discussed in the MSc thesis of B): If f is an assignment, then dom(f) is defined, and for v of type dom(f) the expression f(v) can be build (v is a variable with values, f(v) is of the value type). dom(f) is a set in the general sense (which can have iterators and element-decision). As a refinement iterators through all pairs (x, y) (so that std::map can easily yield models). See Clauses.hpp.
 */
 
 #ifndef ASSIGNMENTS_nfgqtqp6
