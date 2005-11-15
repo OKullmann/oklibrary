c8bf61b36035aa4c7644cda5b974c5964f28d712
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-15 16:25:23 +0000

    New todo added (regarding the relation to the assignment concept).

--- Concepts/Clauses.hpp
+++ Concepts/Clauses.hpp
@@ -4,6 +4,15 @@
   \file Concepts/Clauses.hpp
   \brief Concepts for clauses.
   \todo Concepts for and-clauses, or-clauses, etc.
+  \todo A fundamental issue here is the relation to the concept of
+  partial assignments. It should be possible to easily switch between
+  the concepts of partial assignments and clauses; perhaps the only
+  difference is in the complexity of the operations: while clauses correspond
+  to sets (using space proportionally to their size), partial assignments
+  correspond to total maps (using a special value "unassigned"; supporting
+  constant time access). As set-objects partial assignments and clause-sets
+  are identical (for standard literals), namely they are sets of pairs of variables
+  and (forbidden) values, interpreted as constituting the assignment or as literals.
 */
 
 #ifndef CLAUSES_0TRFvc
