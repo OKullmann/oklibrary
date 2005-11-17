31e8353397ff873bb579c302d72d33511b4e0455
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-17 15:47:51 +0000

    Updated \todo's.

--- Isomorphisms/CLSIsomorphismsBacktracking.hpp
+++ Isomorphisms/CLSIsomorphismsBacktracking.hpp
@@ -18,7 +18,8 @@
   SAT problem shall be implemented. Natural choice for variables are the literals in
   F_1, where each literal has the values as discussed above (also possibly using
   degree information etc.). Possibilities for clauses:
-   - excluding the clauses not in F_2, using the standard "negative literals" (for most cases this will not be feasible);
+   - excluding the clauses not in F_2, using the standard "negative literals"
+   (for most cases this will not be feasible, since there are too many clauses to be excluded);
    or
    - using a level-3 CNF with "positive literals", expressing that for each clause in F_2 the translated
      clause is in F_2.
@@ -30,6 +31,10 @@
   the conflict-multigraph) can be used to restrict the assignment of clauses. If the (original) clauses are large,
   then we get many clauses, but in nice cases we should get much less due to the restrictions on
   the literal assignments as discussed above. Here we can use standard literals.
+  Very attractive seems to be to just use these two types of variables, and binary powerset-clauses expressing
+  that if literal x maps to y, then all clauses containing x must map to clauses containing y,
+  and that if clause C maps to clause D, then every literal x in C must map to some literal y in D.
+  This is likely not complete, but perhaps a good approximation. And we avoid higher levels.
   \todo It should be useful to employ (multi-)graph isomorphism algorithms on the various (multi-)graphs
   associated with clause-sets, either as a preselection for potential isomorphisms or as a test.
   \todo We also need the determination of the automorphism group of a clause-set, either explicitely
