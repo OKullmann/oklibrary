3e2662f2b3eba7b25da5af7a0b0431057e15984f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-21 10:16:42 +0000

    Module for translating clause-set-isomorphism problems into (generalised) SAT problem (was CLSIsomorphismsBacktracking before; new design).

---
+++ Isomorphisms/CLSIsomorphisms_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 12.11.2005 (Swansea)
+
+#ifndef CLSISOMORPHISMSBACKTRACKINGTESTS_8uujjNN
+
+#define CLSISOMORPHISMSBACKTRACKINGTESTS_8uujjNN
+
+#include "CLSIsomorphisms.hpp"
+#include "CLSIsomorphisms_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Isomorphims {
+
+  }
+
+}
+
+#endif
---
+++ Isomorphisms/CLSIsomorphisms_Tests.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 12.11.2005 (Swansea)
+
+/*!
+  \file CLSIsomorphisms_Tests.hpp
+  \brief Tests for isomorphisms decision algorithms, and specialised tests
+  connected with the backtracking approach.
+  \todo The general tests should go into a dedicated test submodule.
+  (This is also a general pattern.)
+*/
+
+#ifndef CLSISOMORPHISMSTESTS_8uujjNN
+
+#define CLSISOMORPHISMSTESTS_8uujjNN
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace Isomorphims {
+
+  }
+
+}
+
+#endif
