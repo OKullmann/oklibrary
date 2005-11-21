599691cdb17a6c58521d60a225a3624d3eb1838d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-21 10:18:01 +0000

    Updated inclusion of testobjects.

--- Isomorphisms/TestIsomorphisms.cpp
+++ Isomorphisms/TestIsomorphisms.cpp
@@ -2,7 +2,8 @@
 
 #include "TestBaseClass.hpp"
 
-#include "CLSIsomorphismsBacktracking_Testobjects.hpp"
+#include "CLSIsomorphisms_Testobjects.hpp"
+#include "GraphIsomorphisms_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
