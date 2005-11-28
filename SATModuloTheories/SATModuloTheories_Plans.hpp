f6b20c75e86ce3271704aada5abcb891d7196ba1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-28 13:52:00 +0000

    New module for SAT augmented with logical theories.

---
+++ SATModuloTheories/SATModuloTheories_Plans.hpp
@@ -0,0 +1,9 @@
+// Oliver Kullmann, 20.11.2005 (Swansea)
+
+/*!
+  \file SATModuloTheories_Plans.hpp
+  \brief Plans for the module SATModuloTheories.
+  \todo First it has to be studied, how to translate this
+  problems into our domain.
+*/
+
