ef30b0ba080b18d0f0cbeeed403a652826bfa490
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-29 18:11:21 +0000

    Plans regarding (hyper)graph colouring.

---
+++ HypergraphColouring/HypergraphColouring_Plans.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 25.11.2005 (Swansea)
+
+/*!
+  \file HypergraphColouring_Plans.hpp
+  \brief Plans for the module HypergraphColouring
+  \todo Transferring (and updating) generators for
+   - OKsolver/Experimental/Transformationen/Ramsey
+   - OKsolver/Experimental/Transformationen/VanderWaerden
+   (also providing active clause-sets).
+  \todo Given a graph as input, translate the k-colouring problem.
+  \todo Given a graph as input, translate the colouring problem.
+  \todo For interesting systematically generated classes of graphs,
+  create active clause-sets translating k-colouring problems.
+  \todo Given a hypergraph as input, translate the k-colouring problem.
+  \todo Given a hypergraph as input, translate the colouring problem.
+  \todo For interesting systematically generated classes of hypergraphs,
+  create active clause-sets translating k-colouring problems.
+*/
+
