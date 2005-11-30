0df177ce80fff7c49b6a5672dd818b85a3a6d60e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-11-30 19:18:07 +0000

    Several corrections, and added function first_extension_uniqueness_checked.

--- SearchDataStructures/AssociativeContainers.hpp
+++ SearchDataStructures/AssociativeContainers.hpp
@@ -13,8 +13,10 @@
 #include <set>
 #include <functional>
 #include <algorithm>
+#include <utility>
 
 #include <boost/range/functions.hpp>
+#include <boost/utility.hpp>
 
 namespace OKlib {
 
@@ -23,7 +25,7 @@ namespace OKlib {
     /*!
       \class AssociativePrefixContainer
       \brief Class for associative container of ranges with lexicographic ordering.
-      \todo Create a concept.
+      \todo Create a concept (including providing appropriate container functionality).
     */
 
     template <class Range>
@@ -46,27 +48,48 @@ namespace OKlib {
       AssociativePrefixContainer() {};
 
       typedef typename SetRanges::const_iterator iterator;
+      typedef iterator const_iterator;
+      typedef std::pair<iterator,bool> checked_iterator_type;
+
       iterator begin() const {
 	return prefix_set.begin();
-      };
+      }
       iterator end() const {
 	return prefix_set.end();
-      };
+      }
 
       std::pair<iterator,bool> insert(const Range& x) {
 	return prefix_set.insert(x);
-      };
+      }
 
       iterator first_extension(const Range& r) const {
 	const iterator& lower_bound(prefix_set.lower_bound(r));
         const iterator& end(prefix_set.end());
         if (lower_bound == end)
           return end;
+        if (boost::size(r) > boost::size(*lower_bound))
+          return end;
         if (std::equal(boost::begin(r), boost::end(r), boost::begin(*lower_bound)))
           return lower_bound;
         else
           return end;
-      };
+      }
+
+       checked_iterator_type first_extension_uniqueness_checked(const Range& r) const {
+       const iterator& first(first_extension(r));
+       const iterator& end(prefix_set.end());
+        if (first == end)
+          return checked_iterator_type(first, true);
+        const iterator& next(boost::next(first));
+        if (next == end)
+          return checked_iterator_type(first, true);
+        if (boost::size(r) > boost::size(*next))
+          return checked_iterator_type(first, true);
+        if (std::equal(boost::begin(r), boost::end(r), boost::begin(*next)))
+          return checked_iterator_type(first, false);
+        else
+          checked_iterator_type(first, true);
+      }
     };
 
   }
