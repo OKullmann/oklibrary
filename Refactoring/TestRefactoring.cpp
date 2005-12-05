c484de4ea2c3f601af05e35dca7f78c3f0d58ca5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-05 14:53:46 +0000

    Inclusion of Renaming-testobjects.

--- Refactoring/TestRefactoring.cpp
+++ Refactoring/TestRefactoring.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "IncludeHandling_Testobjects.hpp"
+#include "Renaming_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
