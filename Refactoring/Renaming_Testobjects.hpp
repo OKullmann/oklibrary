58b2ae0ed269bc00b63f0bd19db35afebf9bf083
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-05 14:53:24 +0000

    New submodule for renaming.

---
+++ Refactoring/Renaming_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 4.12.2005 (Swansea)
+
+#ifndef RENAMINGTESTOBJECTS_6y443Ed
+
+#define RENAMINGTESTOBJECTS_6y443Ed
+
+#include "Renaming.hpp"
+#include "Renaming_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Refactoring {
+
+  }
+
+}
+
+#endif
---
+++ Refactoring/Renaming_Tests.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 4.12.2005 (Swansea)
+
+/*!
+  \file Renaming_Tests.hpp
+  \brief Tests for renaming tools.
+*/
+
+#ifndef RENAMINGTESTS_98uJJnhg
+
+#define RENAMINGTESTS_98uJJnhg
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace Refactoring {
+
+  }
+
+}
+
+#endif
