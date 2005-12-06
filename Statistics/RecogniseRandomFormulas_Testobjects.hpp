5a0ffee2472ee37d156e19862d0fcbf6c33e724d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-06 16:43:23 +0000

    Renamed versions.

---
+++ Statistics/RecogniseRandomFormulas_Testobjects.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 10.11.2005 (Swansea)
+
+#ifndef REGOCNISERANDOMFORMULASTESTOBJECTS_5664788k
+
+#define REGOCNISERANDOMFORMULASTESTOBJECTS_5664788k
+
+#include "RecogniseRandomFormulas.hpp"
+#include "RecogniseRandomFormulas_Tests.hpp"
+
+namespace OKlib {
+
+  namespace Statistics {
+
+  }
+
+}
+
+#endif
---
+++ Statistics/RecogniseRandomFormulas_Tests.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 10.11.2005 (Swansea)
+
+/*!
+  \file RecogniseRandomFormulas_Tests.hpp
+  \brief Tests for the tools on regocnition of random formulas
+*/
+
+#ifndef REGOCNISERANDOMFORMULASTESTS_61019666
+
+#define REGOCNISERANDOMFORMULASTESTS_61019666
+
+#include "TestBaseClass.hpp"
+#include "TestExceptions.hpp"
+
+namespace OKlib {
+
+  namespace Statistics {
+
+  }
+
+}
+
+#endif
