591785d263682069e29fa57cd9014ffaa8e9d416
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-12-12 22:18:59 +0000

    Naming changes.

--- Refactoring/IncludeHandling_Testobjects.hpp
+++ Refactoring/IncludeHandling_Testobjects.hpp
@@ -19,8 +19,9 @@ namespace OKlib {
     Test_ProgramRepresentationIncludes<ProgramRepresentationIncludes> test_program_representation_includes;
     Test_Parsing<ProgramRepresentationIncludes, StreamExtractor_by_istream_iterator> test_parsing_istream_iterator;
     Test_Parsing<ProgramRepresentationIncludes, StreamExtractor_by_copy> test_parsing_copy;
-    Test_Extend_include_directives<Extend_include_directives<> > test_extend_include_directives;
-    Test_Extend_include_directives_Two_directories<Extend_include_directives_Two_directories<> > test_extend_include_directives_two_directories;
+    Test_ExtendIncludeDirectives<ExtendIncludeDirectives> test_extend_include_directives;
+    Test_ExtendIncludeDirectivesTwoRanges<ExtendIncludeDirectivesTwoRanges> test_extend_include_directives_two_ranges;
+    Test_ExtendIncludeDirectivesTwoDirectories<ExtendIncludeDirectivesTwoDirectories> test_extend_include_directives_two_directories;
   }
 
 }
