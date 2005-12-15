77211cd048799d5b2338dde5a5580b91b44acc64
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2005-12-15 14:57:22 +0000

    Doxygen comments.

--- Refactoring/Transition_Plans.hpp
+++ Refactoring/Transition_Plans.hpp
@@ -1,8 +1,16 @@
 // Matthew Henderson, 5.12.2005 (Swansea)
 
 /*!
+
   \file Transition_Plans.hpp
 
   \todo There needs to be some testing of the transition.
+  \todo In Buildsystem/makefile the creation of the include directory and
+  copying of headers and makefiles to this directory has to be removed. 
+  (L22,L25,L27,L31,L41)
+  \todo In Buildsystem/makefile_generic the definition of includedir should 
+  now point to $(prefix)/OKsystem/Transitional. The copying of headers on 
+  lines 242-245 is no longer required.
+  (L103,L110,L134,L137,L165,L242-L245)
 
 */
