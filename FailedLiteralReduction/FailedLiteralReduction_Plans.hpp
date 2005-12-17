c23dd591190ce99d173e9d902a4ca14853d5b137
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-17 19:19:03 +0000

    New module for failed-literal reduction.

---
+++ FailedLiteralReduction/FailedLiteralReduction_Plans.hpp
@@ -0,0 +1,8 @@
+// Oliver Kullmann, 17.12.2005 (Swansea)
+
+/*!
+  \file FailedLiteralReduction_Plans.hpp
+  \brief Plans for the module FailedLiteralReduction
+  \todo Transferring the implementation used in OKsolver.
+*/
+
