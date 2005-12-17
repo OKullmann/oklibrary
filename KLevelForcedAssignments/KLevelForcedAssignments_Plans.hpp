17a55d24c0c70683edbfe07f65d4bb7c0e70ac7a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-17 19:19:52 +0000

    New module for the leveled generalisation of unit-clause propagation and failed-literal reduction.

---
+++ KLevelForcedAssignments/KLevelForcedAssignments_Plans.hpp
@@ -0,0 +1,8 @@
+// Oliver Kullmann, 17.12.2005 (Swansea)
+
+/*!
+  \file KLevelForcedAssignments_Plans.hpp
+  \brief Plans for the module KLevelForcedAssignments
+  \todo First the scope (the (informal) concept) has to be established.
+*/
+
