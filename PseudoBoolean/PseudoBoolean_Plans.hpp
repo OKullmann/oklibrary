fd96e412cb93fbe9872fbb77d277fad64716170f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-17 19:20:36 +0000

    New module for active clause-sets for pseudo-boolean constraints.

---
+++ PseudoBoolean/PseudoBoolean_Plans.hpp
@@ -0,0 +1,8 @@
+// Oliver Kullmann, 17.12.2005 (Swansea)
+
+/*!
+  \file PseudoBoolean_Plans.hpp
+  \brief Plans for the module PseudoBoolean
+  \todo First the scope (the (informal) concept) has to be established.
+*/
+
