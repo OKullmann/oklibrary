28cafdc4fc4b53fbcd948aa5d6e6a86afdaaf2f6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-22 17:02:42 +0000

    Extended.

--- SATModuloTheories/SATModuloTheories_Plans.hpp
+++ SATModuloTheories/SATModuloTheories_Plans.hpp
@@ -5,5 +5,9 @@
   \brief Plans for the module SATModuloTheories.
   \todo First it has to be studied, how to translate this
   problems into our domain.
+  \todo Difference constraints is a nice special case; see
+  [Armando,Castellini,Giunchiglia,Maratea; A {SAT}-Based Decision
+  Procedure for the Boolean Combination of Difference Constraints].
+  For the active clause-sets the Bellman-Ford algorithm is applied.
 */
 
