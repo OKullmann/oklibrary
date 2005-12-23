fa514ada608cd1af9f4bb19eabe75fa00392dfd0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-23 19:28:30 +0000

    Adding missing functionality to boost::filesystem::path.

---
+++ Refactoring/BoostPathCorrected.hpp
@@ -0,0 +1,36 @@
+// Oliver Kullmann, 23.12.2005 (Swansea)
+
+#ifndef BOOSTPATHCORRECTED_004ttGB
+
+#define BOOSTPATHCORRECTED_004ttGB
+
+#include <boost/filesystem/path.hpp>
+
+#include <boost/range/const_iterator.hpp> // Fix for erroneous Boost library filesystem ##################################################################
+#include <boost/range/size_type.hpp> // Fix for erroneous Boost library filesystem ##################################################################
+
+
+// Fix for erroneous Boost library filesystem ##########################################################################
+
+namespace boost {
+
+  template <>
+  struct range_const_iterator<boost::filesystem::path> {
+    typedef boost::filesystem::path::iterator type;
+  };
+  template <>
+  struct range_size<boost::filesystem::path> {
+    typedef boost::filesystem::path::iterator::difference_type type;
+  };
+
+  namespace filesystem {
+
+    inline boost::range_size<boost::filesystem::path>::type boost_range_size(const boost::filesystem::path& p) {
+      return std::distance(p.begin(),p.end());
+    }
+
+  }
+
+}
+
+#endif
