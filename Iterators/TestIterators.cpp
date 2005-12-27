a2fe38a399d82cd766155040d04057f958d3d2b5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-27 13:17:26 +0000

    New module for iterator components.

---
+++ Iterators/TestIterators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "TestBaseClass.hpp"
+
+#include "ImplicitConversionsBoost_Testobjects.hpp"
+
+int main() {
+
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+
+} 
