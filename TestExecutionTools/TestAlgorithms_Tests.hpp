e3a945ad1494fdd535d74eeabdb2e400f53ea199
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-27 13:24:43 +0000

    Basic tests completed.

--- TestExecutionTools/TestAlgorithms_Tests.hpp
+++ TestExecutionTools/TestAlgorithms_Tests.hpp
@@ -9,12 +9,92 @@
 
 #define TESTALGORITHMSTESTS_887766tg
 
+#include <boost/iterator/counting_iterator.hpp>
+#include <boost/range/iterator_range.hpp>
+
+#include "Numerical.hpp"
+
 #include "TestBaseClass.hpp"
 #include "TestExceptions.hpp"
+
+#include "SecondOrderTestTools.hpp"
 
 namespace OKlib {
 
   namespace TestExecutionTools {
+
+    /*!
+      \class Test_AllCombinations
+      \brief Tests a facility AllCombinations by executing it and counting the number of sub-tests
+      executed.
+    */
+
+    template <template <int Dim, class InputRange, class Functor> class AllCombinations>
+    class Test_AllCombinations : public ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_AllCombinations test_type;
+      Test_AllCombinations() {
+        insert(this);
+      }
+    private :
+
+      typedef TestCounter<Test_AllCombinations> test_counter_type;
+      typedef typename test_counter_type::int_type int_type;
+      typedef boost::counting_iterator<int_type> counting_iterator_type;
+      typedef boost::iterator_range<counting_iterator_type> range_type;
+
+      void perform_test_trivial() {
+        test_fixed_dimension<1>();
+        test_fixed_dimension<2>();
+        test_fixed_dimension<3>();
+      }
+
+      template <int Dim>
+      void test_fixed_dimension() {
+        typedef AllCombinations<Dim, range_type, test_counter_type> all_combinations_type;
+        const int_type number_iterations = 100;
+        test_counter_type::counter = 0;
+        OKLIB_TESTTRIVIAL_RETHROW((all_combinations_type(range_type(int_type(0), number_iterations))));
+        OKLIB_TEST_EQUAL(test_counter_type::counter, (MetaProgramming::Binomial<number_iterations, Dim>::value));
+      }
+    };
+
+    /*!
+      \class Test_AllVariations
+      \brief Tests a facility AllVariations by executing it and counting the number of sub-tests
+      executed.
+    */
+
+    template <template <int Dim, class InputRange, class Functor> class AllVariations>
+    class Test_AllVariations : public ::OKlib::TestSystem::TestBase {
+    public :
+      typedef Test_AllVariations test_type;
+      Test_AllVariations() {
+        insert(this);
+      }
+    private :
+
+      typedef TestCounter<Test_AllVariations, long> test_counter_type;
+      typedef typename test_counter_type::int_type int_type;
+      typedef boost::counting_iterator<int_type> counting_iterator_type;
+      typedef boost::iterator_range<counting_iterator_type> range_type;
+
+      void perform_test_trivial() {
+        test_fixed_dimension<1>();
+        test_fixed_dimension<2>();
+        test_fixed_dimension<3>();
+      }
+
+      template <int Dim>
+      void test_fixed_dimension() {
+        typedef AllVariations<Dim, range_type, test_counter_type> all_variations_type;
+        const int_type number_iterations = 100;
+        test_counter_type::counter = 0;
+        OKLIB_TESTTRIVIAL_RETHROW((all_variations_type(range_type(int_type(0), number_iterations))));
+        OKLIB_TEST_EQUAL(test_counter_type::counter, (MetaProgramming::Power<number_iterations, Dim>::value));
+      }
+    };
+
 
   }
 
