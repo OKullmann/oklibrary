61893adb3eb1001a328d44db5c8e57083bb1e858
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-27 13:27:14 +0000

    Testobjects included for new submodule.

--- TestExecutionTools/TestExecutionTools.cpp
+++ TestExecutionTools/TestExecutionTools.cpp
@@ -3,6 +3,7 @@
 #include "TestBaseClass.hpp"
 
 #include "TestAlgorithms_Testobjects.hpp"
+#include "SecondOrderTestTools_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
