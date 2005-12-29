46af62ad4f4362a857b37b3863d791cce27d2af6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-29 13:41:45 +0000

    New module for general traits meta-functions.

---
+++ Traits/Traits_Plans.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 29.12.2005
+
+/*!
+  \namespace OKlib::Traits
+  \brief All general-purpose traits meta-functions
+  \todo Transfer
+   - MetaProgramming/TypeTraits.hpp
+   - MetaProgramming/TypeTraitsContainer.hpp
+   - Concepts/Traits.hpp
+  to here.
+*/
+
+/*!
+  \file Traits_Plans.hpp
+  \brief Plans for the general traits module
+*/
+
