93cbb317be1e6b2e179828e9e32b0a8485b2627e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2005-12-29 13:31:12 +0000

    Description for module Concepts.

---
+++ Concepts/Concepts_Plans.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 29.12.2005 (Swansea)
+
+/*!
+  \namespace OKlib/Concepts
+  \brief Concept definitions and accompanying generic tests
+*/
+
+/*!
+  \file Concepts_Plans.hpp
+  \brief Plans for the concepts module
+  \todo Eliminate all warnings.
+  \todo What is the difference for example between
+  bool b = static_cast<bool>(e);
+  and
+  bool b = e;
+  ?! I would assume that here there is none. And in general?
+*/
+
