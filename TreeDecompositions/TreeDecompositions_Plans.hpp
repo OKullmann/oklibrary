2d96712d58ca82208900052778f3b39f43b25bc0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-02 15:29:09 +0000

    New modules for tree decompositions of graphs.

---
+++ TreeDecompositions/TreeDecompositions_Plans.hpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 2.1.2006 (Swansea)
+
+/*!
+  \file TreeDecompositions_Plans.hpp
+  \brief Plans for the module regarding tree decompositions
+  \todo First we should look out for existing implementations,
+  and, if possible, integrate them into the library.
+  \todo Furthermore we must obtain an overview on algorithms
+  from the literature and we should make a plan for our own
+  generic implementations.
+*/
+
