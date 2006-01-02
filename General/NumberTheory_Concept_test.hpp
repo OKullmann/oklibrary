73cca7ea3cb515098d04fc1ace6cb0dee9e3e1d0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-02 15:00:09 +0000

    Editorial changes, and getting rid off compiler warnings.

--- General/NumberTheory_Concept_test.hpp
+++ General/NumberTheory_Concept_test.hpp
@@ -13,36 +13,36 @@ namespace NumberTheory_Concept_test {
   struct Test_gcd {
     Test_gcd() {
       {
-	// TO IMPROVE: The concept must specify, that if the inputs are integerrs >= 0, then the output also is non-negative.
-	typedef int Int;
-	Gcd<Int> gcd;
+        // TO IMPROVE: The concept must specify, that if the inputs are integerrs >= 0, then the output also is non-negative.
+        typedef int Int;
+        Gcd<Int> gcd;
 	
-	assert(gcd(0,0) == 0);
-	assert(gcd(1,0) == 1);
-	assert(gcd(0,1) == 1);
+        assert(gcd(0,0) == 0);
+        assert(gcd(1,0) == 1);
+        assert(gcd(0,1) == 1);
 	
-	// TO IMPROVE: test levels
+        // TO IMPROVE: test levels
 	
-	const Int max_trivial_tests = 100;
-	for (Int i = 1; i <= max_trivial_tests; ++i) {
-	  assert(gcd(i,i) == i);
-	  assert(gcd(i,0) == i);
-	  assert(gcd(0,i) == i);
-	  assert(gcd(i,1) == 1);
-	  assert(gcd(1,i) == 1);
-	}
+        const Int max_trivial_tests = 100;
+        for (Int i = 1; i <= max_trivial_tests; ++i) {
+          assert(gcd(i,i) == i);
+          assert(gcd(i,0) == i);
+          assert(gcd(0,i) == i);
+          assert(gcd(i,1) == 1);
+          assert(gcd(1,i) == 1);
+        }
 	
-	const Int max_symmetry_tests = 100;
-	for (Int i = 1; i <= max_symmetry_tests; ++i)
-	  for (Int j = i+1; j <= max_symmetry_tests; ++j)
-	    assert(gcd(i,j) == gcd(j,i));
+        const Int max_symmetry_tests = 100;
+        for (Int i = 1; i <= max_symmetry_tests; ++i)
+          for (Int j = i+1; j <= max_symmetry_tests; ++j)
+            assert(gcd(i,j) == gcd(j,i));
 	
-	assert(gcd(10,3) == 1);
-	assert(gcd(19,10) == 1);
-	assert(gcd(55,10) == 5);
-	assert(gcd(112,36) == 4);
-	assert(gcd(312,262) == 2);
-	assert(gcd(1200,360) == 120);
+        assert(gcd(10,3) == 1);
+        assert(gcd(19,10) == 1);
+        assert(gcd(55,10) == 5);
+        assert(gcd(112,36) == 4);
+        assert(gcd(312,262) == 2);
+        assert(gcd(1200,360) == 120);
       }
     }
   };
@@ -51,19 +51,19 @@ namespace NumberTheory_Concept_test {
   template <typename Int, template <typename> class Gcd_extended, template <typename> class Gcd>
   struct Test_wrapper_gcd_extended {
     void operator() (const Int a, const Int b) const {
-	typedef Gcd_extended<Int> Gcd_i_e;
-	typedef Gcd<Int> Gcd_i;
-	typedef typename Gcd_i_e::result_type result_type;
+      typedef Gcd_extended<Int> Gcd_i_e;
+      typedef Gcd<Int> Gcd_i;
+      typedef typename Gcd_i_e::result_type result_type;
 
-	Gcd_i_e gcd_e;
-	Gcd_i gcd;
+      Gcd_i_e gcd_e;
+      Gcd_i gcd;
 
-	const result_type r = gcd_e(a, b);
-	assert(r.a == a);
-	assert(r.b == b);
-	assert(r.c == gcd(a,b));
-	assert(r.x * a + r.y * b == r.c);
-      }
+      const result_type r = gcd_e(a, b);
+      assert(r.a == a);
+      assert(r.b == b);
+      assert(r.c == gcd(a,b));
+      assert(r.x * a + r.y * b == r.c);
+    }
   };
 
   // OK, 6.11.2004
@@ -71,33 +71,33 @@ namespace NumberTheory_Concept_test {
   struct Test_gcd_extended {
     Test_gcd_extended() {
       {
-	typedef int Int;
-	Test_wrapper_gcd_extended<Int, Gcd_extended, Gcd> gcd_test;
+        typedef int Int;
+        Test_wrapper_gcd_extended<Int, Gcd_extended, Gcd> gcd_test;
 	
-	gcd_test(0,0);
-	gcd_test(0,1);
-	gcd_test(1,0);
+        gcd_test(0,0);
+        gcd_test(0,1);
+        gcd_test(1,0);
 
-	const Int max_trivial_tests = 100;
-	for (Int i = 1; i <= max_trivial_tests; ++i) {
-	  gcd_test(i,i);
-	  gcd_test(i,0);
-	  gcd_test(0,i);
-	  gcd_test(i,1);
-	  gcd_test(1,i);
-	}
+        const Int max_trivial_tests = 100;
+        for (Int i = 1; i <= max_trivial_tests; ++i) {
+          gcd_test(i,i);
+          gcd_test(i,0);
+          gcd_test(0,i);
+          gcd_test(i,1);
+          gcd_test(1,i);
+        }
 
-	const Int max_tests = 100;
-	for (Int i = 1; i <= max_tests; ++i)
-	  for (Int j = i+1; j <= max_tests; ++j)
-	    gcd_test(i,j);
+        const Int max_tests = 100;
+        for (Int i = 1; i <= max_tests; ++i)
+          for (Int j = i+1; j <= max_tests; ++j)
+            gcd_test(i,j);
 	
-	gcd_test(10,3);
-	gcd_test(19,10);
-	gcd_test(55,10);
-	gcd_test(112,36);
-	gcd_test(312,262);
-	gcd_test(1200,360);
+        gcd_test(10,3);
+        gcd_test(19,10);
+        gcd_test(55,10);
+        gcd_test(112,36);
+        gcd_test(312,262);
+        gcd_test(1200,360);
 
       }
     }
@@ -108,24 +108,26 @@ namespace NumberTheory_Concept_test {
   struct Test_Euler_phi {
     Test_Euler_phi() {
       {
-	typedef int Int;
-	typedef Euler_phi<Int> Euler_phi_i;
-	Euler_phi_i phi;
+        typedef int Int;
+        typedef Euler_phi<Int> Euler_phi_i;
+#ifndef NDEBUG
+        Euler_phi_i phi;
+#endif
 	
-	assert(phi(0) == 0);
-	assert(phi(1) == 1);
-	assert(phi(2) == 1);
-	assert(phi(3) == 2);
-	assert(phi(4) == 2);
-	assert(phi(5) == 4);
-	assert(phi(6) == 2);
-	assert(phi(7) == 6);
-	assert(phi(8) == 4);
-	assert(phi(9) == 6);
-	assert(phi(10) == 4);
-	assert(phi(32) == 16);
-	assert(phi(40) == 16);
-	assert(phi(55) == 40);
+        assert(phi(0) == 0);
+        assert(phi(1) == 1);
+        assert(phi(2) == 1);
+        assert(phi(3) == 2);
+        assert(phi(4) == 2);
+        assert(phi(5) == 4);
+        assert(phi(6) == 2);
+        assert(phi(7) == 6);
+        assert(phi(8) == 4);
+        assert(phi(9) == 6);
+        assert(phi(10) == 4);
+        assert(phi(32) == 16);
+        assert(phi(40) == 16);
+        assert(phi(55) == 40);
       }
     }
   };
@@ -133,5 +135,3 @@ namespace NumberTheory_Concept_test {
 }
 
 #endif
-
-
--- General/Numerics_Tests.hpp
+++ General/Numerics_Tests.hpp
@@ -14,7 +14,9 @@ namespace Numerics_Tests {
   template <template <typename base_type, typename exponent_type = int> class Integer_exponentiation>
   struct test_integer_exponentiation {
     test_integer_exponentiation() {
+#ifndef NDEBUG
       Integer_exponentiation<double> ie;
+#endif
       assert(ie(2,0) == 1);
       assert(ie(2,1) == 2);
       assert(ie(2,2) == 4);
@@ -75,7 +77,9 @@ namespace Numerics_Tests {
       typedef Nat_1_2<unsigned int> Code;
       typedef typename Code::pair_type Pair;
       typedef typename Code::number_type Nat;
-      Code code; // ?????? const Code yields error with g++ version 3.3.1
+#ifndef NDEBUG
+      Code code;
+#endif
       assert(code(0) == Pair(0,0));
       assert(code(1) == Pair(1,0));
       assert(code(2) == Pair(0,1));
@@ -88,7 +92,7 @@ namespace Numerics_Tests {
       assert(code(9) == Pair(0,3));
       const Nat max_test = 1000;
       for (Nat i = 0; i <= max_test; ++i)
-	assert(code(code(i)) == i);
+        assert(code(code(i)) == i);
     }
   };
 
@@ -99,35 +103,35 @@ namespace Numerics_Tests {
       typedef typename Edges::pair_type Pair;
       typedef typename Edges::number_type Nat;
       {
-	const Edges edges(0);
-	assert(edges.N == 0);
+        const Edges edges(0);
+        assert(edges.N == 0);
       }
       {
-	const Edges edges(1);
-	assert(edges.N == 0);
+        const Edges edges(1);
+        assert(edges.N == 0);
       }
       {
-	const Edges edges(2);
-	assert(edges.N == 1);
-	assert(edges(1) == Pair(1,2));
-	assert(edges(Pair(1,2)) == 1);
+        const Edges edges(2);
+        assert(edges.N == 1);
+        assert(edges(1) == Pair(1,2));
+        assert(edges(Pair(1,2)) == 1);
       }
       {
-	const Edges edges(3);
-	assert(edges.N == 3);
-	assert(edges(1) == Pair(1,2));
-	assert(edges(2) == Pair(1,3));
-	assert(edges(3) == Pair(2,3));
-	for (Nat i = 1; i <= edges.N; ++i)
-	  assert(edges(edges(i)) == i);
+        const Edges edges(3);
+        assert(edges.N == 3);
+        assert(edges(1) == Pair(1,2));
+        assert(edges(2) == Pair(1,3));
+        assert(edges(3) == Pair(2,3));
+        for (Nat i = 1; i <= edges.N; ++i)
+          assert(edges(edges(i)) == i);
       }
       {
-	const Nat max_number_vertices = 50;
-	for (Nat n = 4; n <= max_number_vertices; ++n) {
-	  const Edges edges(n);
-	  for (Nat i = 1; i <= edges.N; ++i)
-	    assert(edges(edges(i)) == i);
-	}
+        const Nat max_number_vertices = 50;
+        for (Nat n = 4; n <= max_number_vertices; ++n) {
+          const Edges edges(n);
+          for (Nat i = 1; i <= edges.N; ++i)
+            assert(edges(edges(i)) == i);
+        }
       }
     }
   };
