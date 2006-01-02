c58f7c8e652cd09fb9cc1462b1519b967a3634f4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-02 15:21:40 +0000

    Was in MetaProgramming before.

---
+++ Traits/TypeTraitsContainer_Testobjects.hpp
@@ -0,0 +1,16 @@
+// Oliver Kullmann, 5.7.2005 (Swansea)
+
+#ifndef TYPETRAITSCONTAINERTESTOBJECTS_plmHTR
+
+#define TYPETRAITSCONTAINERTESTOBJECTS_plmHTR
+
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+  }
+
+}
+
+#endif
---
+++ Traits/TypeTraitsContainer_Tests.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 5.7.2005 (Swansea)
+
+#ifndef TYPETRAITSCONTAINERTESTS_101jNr
+
+#define TYPETRAITSCONTAINERTESTS_101jNr
+
+
+namespace OKlib {
+
+  namespace MetaProgramming {
+
+    /*!
+      \file TypeTraits_Tests.hpp
+      \todo Tests for metafunction is_std_container and has_size_function.
+    */
+
+  }
+
+}
+
+#endif
+
+
