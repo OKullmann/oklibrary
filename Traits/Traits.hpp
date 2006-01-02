bf2fbfa1727c87f3bf580f3cb951e39f8e26bd92
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-02 15:17:54 +0000

    New generic file, including everything from the module.

---
+++ Traits/Traits.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 30.12.2005 (Swansea)
+
+/*!
+  \file Traits.hpp
+  \brief Collecting all inclusions for module Traits
+  \todo For this kind of files we should also have some
+  test process.
+*/
+
+#ifndef TRAITS_99466TTrfKl
+
+#define TRAITS_99466TTrfKl
+
+#include "concept_tag.hpp"
+#include "floating_point_type.hpp"
+#include "index_type.hpp"
+#include "natural_number_type.hpp"
+#include "size_type.hpp"
+#include "string_type.hpp"
+
+#include "TypeTraits.hpp"
+#include "TypeTraitsContainer.hpp"
+
+#endif
