15c35ba622d1a96819c878ef24c445b1b1635417
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-09 15:09:36 +0000

    New testobjects included.

--- GeneralInputOutput/TestGeneralInputOutput.cpp
+++ GeneralInputOutput/TestGeneralInputOutput.cpp
@@ -4,6 +4,7 @@
 
 #include "RecursiveDirectoryIteration_Testobjects.hpp"
 #include "IOStreamFilters_Testobjects.hpp"
+#include "IOStreamSinks_Testobjects.hpp"
 
 int main() {
   return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
