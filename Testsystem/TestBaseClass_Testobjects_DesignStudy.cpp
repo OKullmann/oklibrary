e023e78fb020b6f22e853404a0090f6ee5a7b9cb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-10 16:00:13 +0000

    Started testing the new testsystem.

---
+++ Testsystem/TestBaseClass_Testobjects_DesignStudy.cpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 9.1.2006 (Swansea)
+
+#include "TestBaseClass_DesignStudy.hpp"
+
+#include "TestBaseClass_Tests_DesignStudy.hpp"
+
+namespace {
+
+  OKlib::TestSystem::RunTest test_testsystem(new OKlib::TestSystem::Test_TestSystem);
+
+}
+
---
+++ Testsystem/TestBaseClass_Tests_DesignStudy.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 9.1.2006 (Swansea)
+
+#ifndef TESTBASECLASSTESTSDESIGNSTUDY_iiUUyyTr5
+
+#define TESTBASECLASSTESTSDESIGNSTUDY_iiUUyyTr5
+
+#include "TestBaseClass_DesignStudy.hpp"
+#include "TestExceptions_DesignStudy.hpp"
+
+namespace OKlib {
+
+  namespace TestSystem {
+
+    class Test_TestSystem :  public OKlib::TestSystem::TestBase<Test_TestSystem> {
+      typedef Test_TestSystem test_type;
+      void test(OKlib::TestSystem::Basic, std::ostream& log) {
+        typedef OKlib::TestSystem::Basic level_type;
+        log << "Test: OKlib::TestSystem::Test_TestSystem, Basic\n";
+        // putting test objects into a private container, then running RunTests with this container
+      }
+    };
+
+  }
+
+}
+
+#endif
