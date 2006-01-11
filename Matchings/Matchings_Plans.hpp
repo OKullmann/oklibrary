7d4046a4dbcad2f641b36f289782654d26fbe01b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-11 19:00:06 +0000

    New modules regarding matching algorithms (for graphs) and matching autarkies.

---
+++ Matchings/Matchings_Plans.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 11.1.2006 (Swansea)
+
+/*!
+  \file Matchings_Plans.hpp
+  \brief Matching algorithms for graphs (in the Boost sense)
+  \todo First concepts for bipartite graphs are needed.
+  Then as a first simple algorithm, a matching problem
+  is reduced to a network flow problem, so that the maximum
+  flow algorithms from Boost can be used.
+  Then perhaps the basic direct algorithm ("Hungarian method")
+  for finding maximum matchings.
+*/
+
+namespace OKlib {
+  namespace Matchings {
+  }
+}
+
