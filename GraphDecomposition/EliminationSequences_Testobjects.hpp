64f5645537d30a696bb6a2a934eca2257c7a8a5c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-11 17:41:58 +0000

    Testobject now needs the graph is type-parameter.

--- GraphDecomposition/EliminationSequences_Testobjects.hpp
+++ GraphDecomposition/EliminationSequences_Testobjects.hpp
@@ -4,6 +4,8 @@
 
 #define ELIMINATIONSEQUENCESTESTOBJECTS_098Uyt
 
+#include <boost/graph/adjacency_list.hpp>
+
 #include "EliminationSequences.hpp"
 #include "EliminationSequences_Tests.hpp"
 
@@ -11,7 +13,7 @@ namespace OKlib {
 
   namespace GraphDecomposition {
 
-    Test_Width_elimination_sequence<Width_elimination_sequence> test_width_elimination_sequence;
+    Test_Width_elimination_sequence<Width_elimination_sequence, boost::adjacency_list<boost::setS, boost::listS, boost::undirectedS> > test_width_elimination_sequence;
 
   }
 
