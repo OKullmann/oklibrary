c8009421f5e5362e0fbd2a40311f2e3d0792dba7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-12 13:09:10 +0000

    Plans for the Latex-module (providing tools for latex-output).

---
+++ LaTeX/LaTeX_Plans.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 12.1.2006 (Swansea)
+
+/*!
+  \file LaTeX_Plans.hpp
+  \brief Plans for the LaTeX-Module
+  \todo Currently the module contains mainly code for producing
+  the timetable for the department. This has to be factored out into
+  some external module, so that only the latex-relateted components
+  remain.
+  \todo We should provide latex-output of clause-sets and
+  SAT-decision trees.
+*/
+
+/*!
+  \namespace OKlib::LaTeX
+  \brief Tools for LaTeX-Output
+*/
+
+namespace OKlib {
+  namespace LaTeX {
+  }
+}
+
