438b3ce3903ef87d8e30488c9bec243718e8368f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-20 10:59:12 +0000

    Basic declarations of integral types for logging purposes (actually yet only for the design study; but should be transferred to the final system).

---
+++ Testsystem/BasicDeclarations.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 19.1.2006 (Swansea)
+
+/*!
+  \file BasicDeclarations.hpp
+  \brief Basic declarations for the test system
+*/
+
+#ifndef BASICDECLARATIONS_kJhYt65
+
+#define BASICDECLARATIONS_kJhYt65
+
+namespace OKlib {
+  namespace TestSystem {
+
+    typedef unsigned long int depth_number_type;
+    typedef unsigned long int line_number_type;
+
+  }
+
+}
+
+#endif
