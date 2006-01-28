8d632a8f298b28f8e058d5d81f6ca4da9334de27
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-01-28 17:53:38 +0000

    New module regarding Ramsey theory (especially van-der-Waerden's problem).

---
+++ RamseyTheory/RamseyTheory_Plans.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 23.1.2006 (Swansea)
+
+/*!
+  \file RamseyTheory_Plans.hpp
+  \brief Plans for the module on Ramsey theory (especially
+  van der Waerden's problem)
+  \todo Transferring (and updating) generators for
+   - OKsolver/Experimental/Transformationen/Ramsey
+   - OKsolver/Experimental/Transformationen/VanderWaerden
+  Especially  providing active clause-sets is very interesting.
+*/
+
+/*!
+  \namespace OKlib::RamseyTheory
+  \brief Components regarding Ramsey theory
+*/
+
+namespace OKlib {
+  namespace RamseyTheory {
+  }
+}
+
