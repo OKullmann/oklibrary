ce7783d63c040a24dcfb62ed4b3c66cf2853b023
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-15 15:43:40 +0000

    Needs to be completed.

--- Messages/TestMessages.cpp
+++ Messages/TestMessages.cpp
@@ -1,14 +1,6 @@
 // Oliver Kullmann, 11.4.2005 (Swansea)
 
-// Temporary solution; to be displaced by the automatic system
-
-#include "Messages_Concept_Syntax_Declarations.hpp"
-#include "Messages_Concept_Syntax_Definitions.hpp"
-#include "Messages_Declarations.hpp"
-#include "Messages.hpp"
-#include "Messages_Models_Declarations.hpp"
-#include "Messages_Models_Definitions.hpp"
-#include "Messages_UserInterface.hpp"
+// to be established
 
 int main() {}
 
