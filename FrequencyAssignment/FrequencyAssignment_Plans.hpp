ee59ae2f4e20b0f3a3b8afabdd62957d26fc5404
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-15 17:18:53 +0000

    New module on applications to frequence assignment (nothing in there yet).

---
+++ FrequencyAssignment/FrequencyAssignment_Plans.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 14.2.2006 (Swansea)
+
+/*!
+  \file FrequencyAssignment_Plans.hpp
+  \brief Plans for the module on frequency assignment
+  \todo First the most important notions of frequency assignments
+  have to be captured and formulated as generalised satisfiability
+  problems.
+  \todo Find benchmark collections. Are there competitions on
+  the subject? (We look only at the decision side; likely most
+  activities in this field concentrate on optimisation?!)
+*/
+
+/*!
+  \namespace OKlib::FrequencyAssignment
+  \brief Active clause-sets for frequency assignment
+*/
+
+namespace OKlib {
+  namespace FrequencyAssignment {
+  }
+}
+
