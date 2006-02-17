235e1808205b740b6973b6149d9b55f6c4a936c0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-17 21:18:03 +0000

    New module for computing all satisfying assignment of a problem.

---
+++ AllSolutions/AllSolutions_Plans.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 17.2.2006
+
+/*!
+  \file AllSatisfyingAssignments_Plans.hpp
+  \brief Plans for the module on components for finding all
+  satisfying assignments.
+  \todo The first approach is to find an equivalent DNF
+  by DPLL (without further simplification it will actually
+  compute a hitting DNF).
+  \todo See HypergraphTransversals/DirectTransversalEnumeration.hpp
+  for the problem of finding all minimal satisfying assignments.
+  \todo Another approach is using OBDD's (this should likely go
+  into its own module).
+*/
+
+/*!
+  \namespace OKlib::AllSolutions
+  \brief Components for extracting all satisfying assignments (in
+  some form).
+*/
+
+namespace OKlib {
+  namespace AllSolutions {
+  }
+}
+
