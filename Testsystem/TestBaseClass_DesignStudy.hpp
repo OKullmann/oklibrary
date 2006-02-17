1f2efbecfaa838ff13eb85f928bb201927b2db50
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-17 13:01:03 +0000

    Class RunTest to RunTest_DesignStudy.hpp (and \todo's updated).

--- Testsystem/TestBaseClass_DesignStudy.hpp
+++ Testsystem/TestBaseClass_DesignStudy.hpp
@@ -3,7 +3,7 @@
 /*!
   \file TestBaseClass_DesignStudy.hpp
   \brief Design studies for the new test hierarchy.
-  \todo RunTest should go to its own file.
+  \todo Testlevel objects passed to the test hierarchy should be dynamically typed.
   \todo Try to extend the level hierarchy.
   \todo In case an unknown exception is thrown, there should be a global option to
   let this exception through.
@@ -13,24 +13,19 @@
 
 #define TESTBASECLASSTEMPORARY_8uXXzs
 
-#include <cstdlib>
+#include <ostream>
 #include <exception>
-#include <cassert>
-#include <string>
-#include <iomanip>
 
-#include <boost/ptr_container/ptr_list.hpp>
-#include <boost/timer.hpp>
+
 #include <boost/iostreams/filtering_stream.hpp>
 
-#include "TimeHandling.hpp"
 #include "IOStreamFilters.hpp"
 #include "MessagesBase.hpp"
 
 #include "BasicDeclarations.hpp"
 #include "TestFondement.hpp"
-#include "TestExceptions_DesignStudy.hpp"
 #include "TestLevel_Explanations.hpp"
+#include "TestExceptions_DesignStudy.hpp"
 
 namespace OKlib {
 
@@ -42,7 +37,7 @@ namespace OKlib {
     */
 
     template <class TestFunction>
-    class TestBase : public Test {
+    class TestBase : public ::OKlib::TestSystem::Test {
       const char* const file_name;
       typedef unsigned long int line_number_type;
       const line_number_type line_number;
@@ -119,114 +114,6 @@ namespace OKlib {
     protected :
       std::ostream& log_message(std::ostream& log, ::OKlib::TestSystem::line_number_type line) {
         return log << "log at line " << line << ": ";
-      }
-    };
-
-
-    // ###################################################
-
-
-    
-    class RunTest {
-      RunTest(const RunTest&); // not available
-      RunTest& operator =(const RunTest&); // not available
-
-    public :
-      typedef Test test_type;
-      typedef Test* test_pointer_type;
-
-      typedef boost::ptr_list<test_type> container_type;
-
-    private :
-
-      static container_type& handle_test_objects(const test_pointer_type p = 0) {
-        static container_type test_objects;
-        if (p)
-          test_objects.push_back(p);
-        return test_objects;
-      }
-
-    public :
-
-      RunTest(Test* const test) {
-        // takes over ownership of test object
-        handle_test_objects(test);
-      }
-
-      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, TestLevel& level) {
-        return run_tests(err, messages, log, level, handle_test_objects());
-      }
-        
-      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, TestLevel& level, container_type& test_objects) {
-
-        messages << "\n" << banner_messages() << "\nOKlib::TestSystem::RunTest\n\n";
-        messages << TimeHandling::currentDateTime("%A, %B %e, %Y; %H:%M:%S%n");
-        messages << ::OKlib::TestSystem::Documentation::TestLevelDescriptions(level) << "\n";
-        typedef container_type::size_type size_type;
-        {
-          const size_type& size(test_objects.size());
-          messages << size << " testobject";
-          if (size != 1) messages << "s";
-          messages << ". \n";
-        }
-        messages << std::endl;
-
-        int return_value = 0;
-        boost::timer timer;
-        TimeHandling::WallTime total_time;
-
-        typedef container_type::iterator iterator;
-        const iterator& end(test_objects.end());
-        size_type counter = 1;
-        size_type err_counter = 0;
-        for (iterator i(test_objects.begin()); i != end; ++i, ++counter) {
-          bool failed = false;
-          log << banner_log() << "\n" << "Test No. " << counter << std::endl;
-          try {
-            level.perform(*i, log);
-          }
-          catch(const OKlib::TestSystem::TestException& e) {
-            ++err_counter;
-            failed = true;
-            log.flush();
-            err << e << std::endl;
-            return_value = EXIT_FAILURE;
-          }
-          log << "Test No. " << counter << " ";
-          if (failed)
-            log << "FAILED";
-          else
-            log << "SUCCEEDED";
-          log <<"\n" << banner_log() << "\n\n";
-        }
-        log.flush();
-
-        {
-          messages << err_counter << " error";
-          if (err_counter != 1) messages << "s";
-          messages << ".\n";
-        }
-        {
-          const size_type& size(test_objects.size());
-          messages << size << " testobject";
-          if (size != 1) messages << "s";
-          messages << ". \n";
-        }
-        messages << ::OKlib::TestSystem::Documentation::TestLevelDescriptions(level) << "\n";
-        messages << "\nElapsed system time: " << timer.elapsed() << "s\n";
-        messages << "Elapsed total time: " << double(total_time) << "s\n";
-        messages << TimeHandling::currentDateTime("%A, %B %e, %Y; %H:%M:%S%n");
-        messages << "\nOKlib::TestSystem::RunTest " << "\n" << banner_messages() << std::endl;
-        return return_value;
-      }
-
-    private :
-
-      static std::string banner_log() {
-        return std::string(40, '#');
-      }
-      static std::string banner_messages() {
-        return std::string(40, '+');
       }
     };
 
