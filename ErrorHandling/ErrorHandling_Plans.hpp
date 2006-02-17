b871c9f6ca1a342bd71be7048b0892f4d24595af
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-17 21:19:36 +0000

    New module for error and exception handling.

---
+++ ErrorHandling/ErrorHandling_Plans.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 17.2.2006 (Swansea)
+
+/*!
+  \file ErrorHandling_Plans.hpp
+  \brief Plans for error handling
+  \todo The Boost method seems reasonable, so we should transfer it.
+  But better using own types.
+  \todo Transferring General/ErrorHandling.hpp, using Messages.
+*/
+
+/*!
+  \namespace OKlib::ErrorHandling
+  \brief Tools for error (and exception) handling
+*/
+
+namespace OKlib {
+  namespace ErrorHandling {
+  }
+}
+
