f7ab704d88c669eed22381b0d89382666c1737f4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-20 12:44:53 +0000

    Plans for the module with system-specific components.

---
+++ SystemSpecifics/SystemSpecifics_Plans.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 18.2.2006 (Swansea)
+
+/*!
+  \file SystemSpecifics_Plans.hpp
+  \brief Plans for the module for system-specific components
+*/
+
+/*!
+  \namespace OKlib::SystemSpecifics
+  \brief Tools for handling and testing tools which are depending
+  on the operating system or the compiler.
+*/
+
+namespace OKlib {
+  namespace SystemSpecifics {
+  }
+}
