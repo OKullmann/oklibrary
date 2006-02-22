1bf6f7bb8a108244e3b561c191b980f7e2f7798d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-22 14:26:30 +0000

    Extended.

--- ErrorHandling/ErrorHandling_Plans.hpp
+++ ErrorHandling/ErrorHandling_Plans.hpp
@@ -5,7 +5,10 @@
   \brief Plans for error handling
   \todo The Boost method seems reasonable, so we should transfer it.
   But better using own types.
-  \todo Transferring General/ErrorHandling.hpp, using Messages.
+  \todo Transferring General/ErrorHandling.hpp. Should we use Messages
+  instead of strings? Seems reasonable.
+  \todo We should use some macro, which adds to the error message
+  the line number and file name, compilation time, and other information.
 */
 
 /*!
