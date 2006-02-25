471861c1340525df477f468dbba793e469a3458e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-25 18:55:47 +0000

    Here we have OKlib::TestSystem::RunTest, but only the declarations.

---
+++ Testsystem/RunTest_Declarations.hpp
@@ -0,0 +1,55 @@
+// Oliver Kullmann, 25.2.2006 (Swansea)
+
+/*!
+  \file RunTestDeclarations.hpp
+  \brief Declarations for ::OKlib::TestSystem::RunTest
+*/
+
+#ifndef RUNTESTDECLARATIONS_jJJhhBc5
+
+#define RUNTESTDECLARATIONS_jJJhhBc5
+
+#include <ostream>
+#include <string>
+
+#include <boost/ptr_container/ptr_list.hpp>
+
+#include <Transitional/Testsystem/TestFondement.hpp>
+
+namespace OKlib {
+
+  namespace TestSystem {
+
+    /*!
+      \class RunTest
+      \brief Responsible for running tests
+    */
+
+    class RunTest {
+      RunTest(const RunTest&); // not available
+      RunTest& operator =(const RunTest&); // not available
+
+    public :
+
+      typedef ::OKlib::TestSystem::Test test_type;
+      typedef Test* test_pointer_type;
+      typedef boost::ptr_list<test_type> container_type;
+
+      RunTest(::OKlib::TestSystem::Test* const test);
+
+      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level);
+      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level, container_type& test_objects);
+
+    private :
+
+      static container_type& handle_test_objects(const test_pointer_type p = 0);
+
+      static std::string banner_log(); 
+      static std::string banner_messages();
+    };
+
+  }
+
+}
+
+#endif
