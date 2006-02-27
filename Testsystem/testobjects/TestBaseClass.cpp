605d7ad582d9b62a39423d9eb174de0a153f89df
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-27 12:42:52 +0000

    Was Messages/TestBaseClass_Testobjects_DesignStudy.hpp before.

---
+++ Testsystem/testobjects/TestBaseClass.cpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 9.1.2006 (Swansea)
+
+#include <Transitional/Testsystem/TestBaseClass_DesignStudy.hpp>
+
+#include <Transitional/Testsystem/tests/TestBaseClass.hpp>
+
+namespace {
+
+  ::OKlib::TestSystem::RunTest test_testsystem(new ::OKlib::TestSystem::tests::TestSystem);
+
+}
+
