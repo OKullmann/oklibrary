493bc25dcc2d388bcff3bc1aedd2ef39f420d866
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-27 12:47:38 +0000

    Was Messages/Testfunctions_DesignStudy.cpp before.

---
+++ Testsystem/testobjects/Testfunctions.cpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 8.1.2006 (Swansea)
+
+#include <Transitional/Testsystem/RunTest_DesignStudy.hpp>
+
+#include <Transitional/Testsystem/tests/Testfunctions.hpp>
+
+namespace {
+
+  ::OKlib::TestSystem::RunTest test1(new ::OKlib::TestSystem::tests::Test1);
+
+}
