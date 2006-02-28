f21ed923c4e29bc34c713772edd8cb916d839052
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-28 18:03:08 +0000

    Now complete.

--- Testsystem/messages/RunTest.hpp
+++ Testsystem/messages/RunTest.hpp
@@ -58,7 +58,20 @@ namespace OKlib {
 
         RunTestOpening(::OKlib::TestSystem::TestLevel& level, const size_type number_tests) : test_level(level), number_testobjects(number_tests) {}
 
-        void opening(std::ostream& out) const {
+        void opening(std::ostream& out, S<Basic>) const {
+          out << "\n";
+          IdentificationTestsystem()(out);
+          out << "\n";
+        }
+
+        void opening(std::ostream& out, S<Full>) const {
+          out << "\n";
+          IdentificationTestsystem()(out);
+          out << "\n";
+          out << ::OKlib::TestSystem::messages::TestLevelDescriptions(test_level) << "\n";
+        }
+
+        void opening(std::ostream& out, S<Extensive>) const {
           out << "\n";
           Banner()(out);
           out << "\n";
@@ -68,15 +81,17 @@ namespace OKlib {
           out << ::OKlib::TestSystem::messages::TestLevelDescriptions(test_level) << "\n";
         }
 
-        void print(std::ostream& out, L<en_GB>, S<Basic>) const {
-          opening(out);
+        template <class Level>
+        void print(std::ostream& out, L<en_GB>, Level) const {
+          opening(out, Level());
           out << number_testobjects << " testobject";
           if (number_testobjects != 1) out << "s";
           out << ". \n" << std::endl;
         }
 
-        void print(std::ostream& out, L<de_DE>, S<Basic>) const {
-          opening(out);
+        template <class Level>
+        void print(std::ostream& out, L<de_DE>, Level) const {
+          opening(out, Level());
           out << number_testobjects << " Testobjekt";
           if (number_testobjects != 1) out << "e";
           out << ". \n" << std::endl;
@@ -98,7 +113,15 @@ namespace OKlib {
 
         RunTestClosing(::OKlib::TestSystem::TestLevel& level, const size_type number_errors, const size_type number_tests, const double system_time, const double total_time) : test_level(level), number_errors(number_errors), number_testobjects(number_tests), system_time(system_time), total_time(total_time) {}
 
-        void closing(std::ostream& out) const {
+        void closing(std::ostream& out, S<Basic>) const {
+          out << std::endl;
+        }
+        void closing(std::ostream& out, S<Full>) const {
+          out << "\n";
+          out << ::OKlib::TestSystem::messages::TestLevelDescriptions(test_level) << "\n";
+          out << std::endl;
+        }
+        void closing(std::ostream& out, S<Extensive>) const {
           out << "\n";
           out << ::OKlib::TestSystem::messages::TestLevelDescriptions(test_level) << "\n";
           out << ::OKlib::Messages::Utilities::TimeDateStandardImplementation() << "\n";
@@ -106,11 +129,24 @@ namespace OKlib {
           IdentificationTestsystem()(out);
           out << "\n";
           Banner()(out);
-          out << std::endl;
+          out << "\n" << std::endl;
         }
 
-        void print(std::ostream& out, L<en_GB>, S<Basic>) const {
+        void print(std::ostream& out, L<en_GB>, S<Basic> l) const {
+          out << number_errors << " error";
+          if (number_errors != 1) out << "s";
+          out << ".\n";
+          closing(out, l);
+        }
+        void print(std::ostream& out, L<en_GB>, S<Full> l) const {
           out << "\nTEST END\n";
+          out << number_errors << " error";
+          if (number_errors != 1) out << "s";
+          out << ".";
+          closing(out, l);
+        }
+        void print(std::ostream& out, L<en_GB>, S<Extensive> l) const {
+          out << "\nTEST END\n\n";
           out << number_errors << " error";
           if (number_errors != 1) out << "s";
           out << ",\n";
@@ -119,18 +155,122 @@ namespace OKlib {
           out << ". \n";
           out << "\nElapsed system time: " << system_time << "s\n";
           out << "Elapsed total time: " << total_time << "s\n";
-          closing(out);
+          closing(out, l);
         }
 
-        void print(std::ostream& out, L<de_DE>, S<Basic>) const {
+        void print(std::ostream& out, L<de_DE>, S<Basic> l) const {
+          out << number_errors << " Fehler.\n";
+          closing(out, l);
+        }
+        void print(std::ostream& out, L<de_DE>, S<Full> l) const {
           out << "\nTESTENDE\n";
+          out << number_errors << " Fehler.";
+          closing(out, l);
+        }
+        void print(std::ostream& out, L<de_DE>, S<Extensive> l) const {
+          out << "\nTESTENDE\n\n";
           out << number_errors << " Fehler,\n";
           out << number_testobjects << " Testobjekt";
           if (number_testobjects != 1) out << "e";
           out << ". \n";
           out << "\nVerbrauchte Systemzeit: " << system_time << "s\n";
           out << "Verbrauchte Gesamtzeit: " << total_time << "s\n";
-          closing(out);
+          closing(out, l);
+        }
+
+      };
+
+      // ##################################################
+
+      struct BannerSingle {
+        const char banner_character;
+        const unsigned int number_banner_characters;
+        BannerSingle() :  banner_character('#'), number_banner_characters(40) {}
+        BannerSingle(const char banner_character, const unsigned int number_banner_characters) : banner_character(banner_character), number_banner_characters(number_banner_characters) {}
+        void operator()(std::ostream& out) const {
+          out << std::string(number_banner_characters, banner_character);
+        }
+      };
+
+      OKLIB_MESSAGES(SingleTestOpening) {
+        OKLIB_MESSAGES_PRINT
+
+        typedef ::OKlib::TestSystem::RunTest::container_type::size_type size_type;
+        const size_type count;
+
+        SingleTestOpening(const size_type count) : count(count) {}
+
+        void print(std::ostream& out, L<en_GB>, S<Basic>) const {}
+        void print(std::ostream& out, L<en_GB>, S<Full>) const {
+          out << "\n" << "Test No. " << count << std::endl;
+        }
+        void print(std::ostream& out, L<en_GB>, S<Extensive>) const {
+          out << "\n";
+          BannerSingle()(out);
+          out << "\n" << "Test No. " << count << std::endl;
+        }
+
+        void print(std::ostream& out, L<de_DE>, S<Basic>) const {}
+        void print(std::ostream& out, L<de_DE>, S<Full>) const {
+          out << "\n" << "Test Nr. " << count << std::endl;
+        }
+        void print(std::ostream& out, L<de_DE>, S<Extensive>) const {
+          out << "\n";
+          BannerSingle()(out);
+          out << "\n" << "Test Nr. " << count << std::endl;
+        }
+
+      };
+
+      // ##################################################
+
+      OKLIB_MESSAGES(SingleTestClosing) {
+        OKLIB_MESSAGES_PRINT
+
+        typedef ::OKlib::TestSystem::RunTest::container_type::size_type size_type;
+        const size_type count;
+        const bool failed;
+
+        SingleTestClosing(const size_type count, const bool failed) : count(count), failed(failed) {}
+
+        void print(std::ostream& out, L<en_GB>, S<Basic>) const {}
+        void print(std::ostream& out, L<en_GB>, S<Full>) const {
+          out << "Test No. " << count << " ";
+          if (failed)
+            out << "FAILED";
+          else
+            out << "SUCCEEDED";
+          out << std::endl;
+        }
+        void print(std::ostream& out, L<en_GB>, S<Extensive>) const {
+          out << "Test No. " << count << " ";
+          if (failed)
+            out << "FAILED (error found)";
+          else
+            out << "SUCCEEDED (no error found)";
+          out << "\n";
+          BannerSingle()(out);
+          out << std::endl;
+        }
+
+        void print(std::ostream& out, L<de_DE>, S<Basic>) const {}
+        void print(std::ostream& out, L<de_DE>, S<Full>) const {
+          out << "Test Nr. " << count << " ";
+          if (failed)
+            out << "FEHLGESCHLAGEN";
+          else
+            out << "ERFOLGREICH";
+          out << std::endl;
+        }
+        void print(std::ostream& out, L<de_DE>, S<Extensive>) const {
+          out << "Test No. " << count << " ";
+          if (failed)
+            out << "FEHLGESCHLAGEN (ein Fehler wurde gefunden)";
+          else
+            out << "ERFOLGREICH (kein Fehler wurde gefunden)";
+          out << "\n";
+          BannerSingle()(out);
+          out << std::endl;
         }
 
       };
