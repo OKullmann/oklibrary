3c6be54d90dd2834b8ffe65da453b75f5c13a3bd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-28 18:03:40 +0000

    General plans for the testsystem.

---
+++ Testsystem/plans/Testsystem.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 27.2.2006 (Swansea)
+
+/*!
+  \file Testsystem/plans/Testsystem.hpp
+  \brief Plans for the test system
+  \todo Rename module to "TestSystem".
+  \todo Build system:
+   - enable linking of only updated testobject-files
+   - is it possible to make out of the testobject.o-files
+     a *dynamic* link library (a static link library is possible
+     via ar)?
+*/
+
+/*!
+  \namespace OKlib::TestSystem
+  \brief The components for higher-order unit-testing
+*/
+
+namespace OKlib {
+  namespace TestSystem {
+  }
+}
+
+
