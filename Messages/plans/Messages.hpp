d41ce9c50b781d6c639cae14e10cc269a4ef0e3c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-02-28 18:00:47 +0000

    Was Messages_Plans.hpp before.

---
+++ Messages/plans/Messages.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 29.1.2006 (Swansea)
+
+/*!
+  \file Messages/plans/Messages.hpp
+  \brief Plans for the messages module
+  \todo Write tests.
+*/
+
+/*!
+  \namespace OKlib::Messages
+  \brief Module for objects representing units of meaning
+*/
+
+namespace OKlib {
+  namespace Messages {
+  }
+}
+
