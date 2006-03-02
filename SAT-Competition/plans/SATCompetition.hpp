a9bc7002744d4887933089d260be8b2a40940f30
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-02 18:47:27 +0000

    It is clear now that in-class defined friend, especially for class templates, should (in most cases) only be used if they are "ADL-enabled". Thus the old design, with the in-class defined friend-operators, was defective (while out-of-class definition would have caused problems regarding type-deduction).

--- SAT-Competition/plans/SATCompetition.hpp
+++ SAT-Competition/plans/SATCompetition.hpp
@@ -6,16 +6,6 @@
 
   \todo Rename the directory "SAT-Competition" to "SATCompetition".
 
-  \todo In SAT-Competition/LexicographicalEvaluation.hpp for the transition
-  from g++ version 4.0.2 to version 4.1.0 the friend-definitions in
-  class template LexicographicalEvaluationRandom had to be replaced
-  by member function declarations (see the code which was commented out),
-  since it didn't work anymore in ComputeLexigraphicalEvaluation.cpp.
-  Kind of clear that there is a problem, since ADL can't find typedef (as
-  LexicographicalEvaluationRandom::map_super_series_to_evaluations_type); however
-  putting class template Evaluation in ComputeLexigraphicalEvaluation.cpp into namespace
-  OKlib::SATCompetition didn't help, and neither did the use of using-declarations ---
-  why this?
 */
 
 
