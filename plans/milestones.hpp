f3bdfc4cb24ccdd3968d6aa59a9faa1f6ac9f8a5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-06 12:31:43 +0000

    The milestones for the Transitional-library, referring to milestones for the modules. Yet the file-hyperlinking in Doxygen does not work?

---
+++ plans/milestones.hpp
@@ -0,0 +1,116 @@
+// Oliver Kullmann, 3.3.2006 (Swansea)
+
+/*!
+  \file Transitional/plans/milestones.hpp
+*/
+
+/*!
+  \page CurrentVersions Current versions
+
+  3.3.2006
+
+  - Transitional 0.1.0 
+  - OKlibrary 0.0.1
+*/
+
+/*!
+  \page PageTargetsTransitional Targets for Transitional:
+
+  \section StartingVersion010 Starting with version 0.1.0
+
+  \subsection VersionTransitional011 0.1.1
+  \par Buildsystem version 0.2
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional012 0.1.2
+  \par Buildsystem version 0.3
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional013 0.1.3
+  \par Buildsystem version 0.4
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional014 0.1.4
+  \par Buildsystem version 0.5
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional015 0.1.5
+  \par Buildsystem version 0.6
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional016 0.1.6
+  \par Buildsystem version 0.7
+  (see Buildsystem/plans/milestones.hpp)
+
+  \subsection VersionTransitional017 0.1.7
+  \par Buildsystem version 0.8
+  (see Buildsystem/plans/milestones.hpp)
+  \par Messages version 0.8
+  (see Messages/plans/milestones.hpp)
+
+  \subsection VersionTransitional018 0.1.8
+  \par Buildsystem version 0.9
+  (see Buildsystem/plans/milestones.hpp)
+  \par Messages version 0.9
+  (see Messages/plans/milestones.hpp)
+
+  \subsection VersionTransitional019 0.1.9
+  \par Buildsystem version 1.0
+  (see Buildsystem/plans/milestones.hpp)
+
+  \section VersionTransitional020 0.2.0 Build and test system ready
+  \par Messages version 1.0
+  (see Messages/plans/milestones.hpp)
+  \par ProgramOptions version ???
+  \par ErrorHandling version ???
+  \par TestSystem version ???
+  \par General goals:
+   - Transition to new test system complete (all old tests transferred to the new test system).
+   - Except of warnings coming from Boost, no compiler warnings from our library.
+
+
+  \section VersionTransitional030 0.3.0 Complexity system ready
+  Complexity system completed and (partially) used
+
+  \section VersionTransitional040 0.4.0 Concepts ready
+  Basic concepts all written (C++ and library-infrastructure-related),
+  and the generic test system framework established and in use.
+
+  \section VersionTransitional050 0.5.0 Documentation ready
+  Documentation completed (regarding the basics)
+
+  \section VersionTransitional060 0.6.0 Package management ready
+  Extending the build system regarding package management (other
+  users)
+
+  \section VersionTransitional100 1.0.0 Foundations completed
+  A solid foundation is provided, now we can start.
+*/
+
+/*!
+   \page VersionHistoryTransitional Version history Transitional
+
+   \par 0.1.0 3/3/2006
+
+   Initial version number (here we just started versioning, while there
+   has been work on the library for around 2 years).
+*/
+
+/*!
+  \page VersionHistoryOKlibrary Version history OKlibrary
+
+  \par 0.0.1 3/3/2006
+
+  Initial version number (library basically empty).
+*/
+
+
+
+/*!
+  \namespace OKlib
+  \brief All components of the OKlibrary
+*/
+
+namespace OKlib {
+}
+
