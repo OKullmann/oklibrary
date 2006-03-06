3bcfdd9bc42db5b0c98fc96f2f21c1d8d5bd885f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-06 12:24:02 +0000

    The \todo's mentioned here moved to milestones.hpp.

--- Messages/plans/Messages.hpp
+++ Messages/plans/Messages.hpp
@@ -3,18 +3,5 @@
 /*!
   \file Messages/plans/Messages.hpp
   \brief Plans for the messages module
-  \todo We need some help for memory management of message objects (components,
-  or at least some guidelines).
-  \todo Write tests.
 */
-
-/*!
-  \namespace OKlib::Messages
-  \brief Module for objects representing units of meaning
-*/
-
-namespace OKlib {
-  namespace Messages {
-  }
-}
 
