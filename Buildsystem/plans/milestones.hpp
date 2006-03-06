27b90cac86d2715ce8c45d2660d045048dd41949
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-06 12:22:59 +0000

    Now every module should have plans/milestones.hpp.

---
+++ Buildsystem/plans/milestones.hpp
@@ -0,0 +1,68 @@
+ // Oliver Kullmann, 4.3.2006 (Swansea)
+
+/*!
+  \file Buildsystem/plans/milestones.hpp
+
+  \par Current version 0.1 (4.3.2006)
+
+  \par Version 0.2 :
+  In Buildsystem/plans/makefile_generic.hpp the following
+  topics are handled:
+  - Compilation
+  - Linking
+  - Cleaning
+
+  \par Version 0.3 :
+  In Buildsystem/plans/makefile_generic.hpp the following
+  topics are handled:
+  - Customisation
+
+  \par Version 0.4 :
+  In Buildsystem/plans/makefile_generic.hpp the following
+  topics are handled:
+  - Test system
+  - Targets
+  - Tools for testing
+  - Error messages of gcc should be processed
+
+  \par Version 0.5 :
+  In Buildsystem/plans/makefile_generic.hpp the following
+  topics are handled:
+  - Placement of makefile_generic
+  - Dependency files
+
+  \par Version 0.6 :
+  In Buildsystem/plans/makefile_ExternalSources.hpp the following
+  topics are handled:
+  - Gcc
+  - Boost
+
+  \par Version 0.7 :
+  In Buildsystem/plans/makefile_ExternalSources.hpp the following
+  topics are handled:
+  - General
+  - Tools
+  - PostgreSQL
+  - Other sources
+
+  \par Version 0.8 :
+  In Buildsystem/plans/Buildsystem.hpp the following
+  topics are handled:
+  - Makefiles in general
+  - Overhaul of the general targets
+
+  \par Version 0.9 :
+  In Buildsystem/plans/Buildsystem.hpp the following
+  topics are handled:
+  - Doxygen
+  - Documentation in general
+
+\par Version 1.0 :
+  In Buildsystem/plans/Buildsystem.hpp the following
+  topics are handled:
+  - Special runs
+  - "Full test"
+  - Directory structure
+
+*/
+
