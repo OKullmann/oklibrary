5d4c8761dd351ac39eaa41c1b3b3cd76cb76257b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-06 12:24:45 +0000

    Milestones until version 1.0.

---
+++ Messages/plans/milestones.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 3.3.2006 (Swansea)
+
+/*!
+  \namespace OKlib::Messages
+  \brief Module for objects representing units of meaning
+*/
+
+/*!
+  \file Messages/plans/milestones.hpp
+
+  \par Current version 0.7 (3.3.2006)
+
+  \par Version 0.8 :
+  Help for memory management of message objects (components, or at least some guidelines)
+  available.
+
+  \par Version 0.9 :
+  Documentation completed.
+
+  \par Version 1.0
+  Tests for all components available and applied.
+
+*/
+
+namespace OKlib {
+  namespace Messages {
+  }
+}
+
