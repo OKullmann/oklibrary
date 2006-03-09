97f9d954a2bc3446f24a5d2bdc1fd3c00c2e5800
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-03-09 15:57:55 +0000

    Update.

--- Messages/Utilities/plans/Utilities.hpp
+++ Messages/Utilities/plans/Utilities.hpp
@@ -3,12 +3,13 @@
 /*!
   \file Messages/Utilities/plans/Utilities.hpp
   \brief General plans for the Messages/Utilities module.
+  \todo Create milestones.
   \todo As a service for Testsystem/TestException_DesignStudy.hpp
   (and other parts of the test system) and for ProgramOptions we need
   classes (and macros) for file-identification purposes:
    - Per file we have __DATE__ (compilation date), __FILE__ (file name),
      __TIME__ (C) (compilation time), and
-     $Date: 2006/03/02 18:25:54 $ (last change date), $Name:  $ (current tag), $Revision: 1.2 $ (current
+     $Date: 2006/03/09 15:57:55 $ (last change date), $Revision: 1.3 $ (current
      revision number) (CVS).
    - For programs we also have the version, the author, name of program etc.
  \todo Perhaps we can offer also some help regarding __LINE__.
