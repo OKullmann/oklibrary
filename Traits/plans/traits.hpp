b247a2d129fae81dd1a275f89a2ed2f6848c2ab8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:28:20 +0000

    Moved from the main directory to here.

---
+++ Traits/plans/traits.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 29.12.2005
+
+/*!
+  \file traits/plans/traits.hpp
+  \brief Plans for the general traits module
+
+  \todo Submodules
+   - traits/TypeTraits.hpp
+   - traits/TypeTraitsContainer.hpp
+  perhaps need an overhaul?
+
+  \todo Test all metafunctions.
+*/
+
+/*!
+  \namespace OKlib::traits
+  \brief All traits meta-functions concerning the Standard
+
+  Models of a concept needing these metafunctions include the
+  respective metafunction-file and specialise the metafunction.
+  Module-specific traits metafunctions are located in
+  OKlib/Module/Traits
+  (otherwise the same as here).
+
+*/
+
+namespace OKlib {
+  namespace traits {
+  }
+}
+
