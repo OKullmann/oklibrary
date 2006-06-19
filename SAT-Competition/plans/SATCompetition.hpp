8b76624363e2eb5ab63a12032816d87a958df0d9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:23:44 +0000

    Updated.

--- SAT-Competition/plans/SATCompetition.hpp
+++ SAT-Competition/plans/SATCompetition.hpp
@@ -1,11 +1,17 @@
 // Oliver Kullmann, 2.3.2006 (Swansea)
 
 /*!
-  \file SAT-Competition/plans/SATCompetition.hpp
+  \file SATCompetition/plans/SATCompetition.hpp
   \brief Plans for the module on the evaluation of the SAT-2005 competition
-
-  \todo Rename the directory "SAT-Competition" to "SATCompetition".
-
 */
 
+/*!
+  \namespace OKlib::SATCompetition
+  \brief Tools for evaluating SAT competition (under various circumstances)
+*/
+
+namespace OKlib {
+  namespace SATCompetition {
+  }
+}
 
