858be31c2fdabd24123ba91d4d1d81e2edacb63f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:00:39 +0000

    Plans moved to sub-directory "plans", and otherwise the same updated like the other files in this module (especially regarding the improved traits-directory-structure).

--- Concepts/Variables.hpp
+++ Concepts/Variables.hpp
@@ -2,57 +2,16 @@
 
 /*!
   \file Concepts/Variables.hpp
-  \brief Concepts for Variables.
-  \todo Is a generalisation of the Variables-concept useful, where there is no default
-  constructor, and no handling of singular values? (Perhaps with the name "GeneralIndex" ?)
-  \todo It seems now artificial to me that variables are ordered by < : There is no default
-  meaning --- it could be the order as found in the input, or the order in the quantifier
-  prefix, etc. So only refinements extend == to a linear order, using special
-  predicate names, and there are refinements using <, which then is purely
-  implementation-defined (for pointers for example, while for variables with index
-  it's the index).
-  \todo Variables can have *domains*, which are types (with values), for example bool
-  or enumerations. Partial assignments restrict these domains further. Refinements
-  allow only special domains, for example an enumeration with N values, or
-  bool.
-  \todo There are variable categories
-   - Parameter (the "default")
-   - Universal
-   - Existential.
-  For a variable type Var the category is given with
-  OKlib::Variables::Traits::category_type<Var>::type,
-  which can be Parameter, Universal, Existential or Varyingly.
-  With OKlib::Variables::category(v) we get an enumerated value of type
-  enum Variable_categories {is_parameter = 0, is_existential = 1, is_universal = 2}.
-  The types Parameter, Universal, Existential or Varyingly are nearly numeric constants in the
-  MPL sense, only that in the MPL we must have an integral value, while we have an
-  enumeration. So with
-  OKlib::Variables::Traits::category_type<Var>::type::value
-  we get the compile-time constant.
-  The default of OKlib::Variables::category(v) is
-  OKlib::Variables::Traits::category_type<Var>::type::value, i.e.,
-  namespace OKlib { namespace Variables {
-  template <typename Var>
-  Variable_categories category(Var v) {
-    return OKlib::Variables::Traits::category_type<Var>::type::value;
-  }}}
-  This should be placed in OKlib/Variables/category.hpp.
-  We need the types for overloading, while we need the value to have a compile-time
-  constant.
-  \todo Active clause-sets use "variable structures" to manage their variables, allowing
-    for example to run through them, etc.
-    These variable structures should have a type member showing which combination
-    is possible (in principle).
+  \brief Concepts for Variables
 */
 
 #ifndef VARIABLES_095tYb
-
 #define VARIABLES_095tYb
 
-#include <Transitional/Traits/TypeTraits.hpp>
+#include <Transitional/traits/TypeTraits.hpp>
 
 #include <Transitional/Concepts/LibraryBasics.hpp>
-#include <Transitional/Traits/index_type.hpp>
+#include <Transitional/Variables/traits/index_type.hpp>
 
 namespace OKlib {
 
@@ -106,8 +65,8 @@ namespace OKlib {
     template <typename Var>
     struct VariablesWithIndex {
 
-      typedef typename OKlib::Concepts::index_type<Var>::type index_type;
-      BOOST_STATIC_ASSERT(::OKlib::MetaProgramming::is_unqualified_signedunsigned_integral<index_type>::value);
+      typedef typename OKlib::Variables::traits::index_type<Var>::type index_type;
+      BOOST_STATIC_ASSERT(::OKlib::traits::is_unqualified_signedunsigned_integral<index_type>::value);
 
       void constraints() {
         OKLIB_MODELS_CONCEPT_TAG(Var, VariablesWithIndex);
@@ -137,7 +96,7 @@ namespace OKlib {
     template <typename Var>
     struct VariablesAsIndex {
 
-      typedef typename OKlib::Concepts::index_type<Var>::type index_type;
+      typedef typename OKlib::Variables::traits::index_type<Var>::type index_type;
 
       void constraints() {
         OKLIB_MODELS_CONCEPT_TAG(Var, VariablesAsIndex);
