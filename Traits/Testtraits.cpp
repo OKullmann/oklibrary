0906cd0da23c0715b180bcff34bab99a459ace90
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:30:51 +0000

    Updated and renamed versions.

---
+++ Traits/Testtraits.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <Transitional/Testsystem/TestBaseClass.hpp>
+
+#include <Transitional/traits/TypeTraits_Testobjects.hpp>
+#include <Transitional/traits/TypeTraitsContainer_Testobjects.hpp>
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
+} 
---
+++ Traits/traits.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 30.12.2005 (Swansea)
+
+/*!
+  \file traits/traits.hpp
+  \brief Collecting all inclusions for module traits
+  \todo For this kind of files we should also have some
+  test process.
+*/
+
+#ifndef TRAITS_99466TTrfKl
+
+#define TRAITS_99466TTrfKl
+
+#include <Transitional/traits/TypeTraits.hpp>
+#include <Transitional/traits/TypeTraitsContainer.hpp>
+
+#endif
