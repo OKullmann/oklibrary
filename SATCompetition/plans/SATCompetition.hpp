908cb1d5fd6bb2705a805aad233817d7c9f67534
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 13:07:28 +0000

    Was in "SAT-Competiton" before.

---
+++ SATCompetition/plans/SATCompetition.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 2.3.2006 (Swansea)
+
+/*!
+  \file SATCompetition/plans/SATCompetition.hpp
+  \brief Plans for the module on the evaluation of the SAT-2005 competition
+*/
+
+/*!
+  \namespace OKlib::SATCompetition
+  \brief Tools for evaluating SAT competition (under various circumstances)
+*/
+
+namespace OKlib {
+  namespace SATCompetition {
+  }
+}
+
