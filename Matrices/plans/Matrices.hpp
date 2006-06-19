e056cda3ced8908d9e727381efaf62cd135810ad
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:09:09 +0000

    New module for matrices (yet only in the uBLAS-sense).

---
+++ Matrices/plans/Matrices.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 17.6.2006 (Swansea)
+
+/*!
+  \file Matrices/plans/Matrices.hpp
+  \brief Plans for the matrices module
+*/
+
+/*!
+  \namespace OKlib::Matrices
+  \brief Module for matrix computations
+*/
+
+namespace OKlib {
+  namespace Matrices {
+  }
+}
+
---
+++ Matrices/plans/milestones.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 17.6.2006 (Swansea)
+
+/*!
+  \file Matrices/plans/milestones.hpp
+
+  \par Current version 0.0.1 (17.6.2006)
+
+  \par Version 0.0.2 :
+  Is_equal and Is_symmetric are tested.
+
+  \par Version 0.1.0 :
+  Is_self_dual and Is_self_polar are tested (using for matrices dynamic
+  and static storage, and for the permutations generator at least fixed
+  size and dynamic size with lexicographical order.
+
+*/
+
