82fc3df548bb095cfda5701c8a47b23917ce679c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 11:48:36 +0000

    New traits class to extract range-types from permutation-generators.

---
+++ Combinatorics/traits/range_type.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 18.6.2006 (Swansea)
+
+/*!
+  \file Combinatorics/traits/range_type.hpp
+  \brief Traits-metafunction to extract the range type
+*/
+
+#ifndef COMBINATORICSRANGETYPE_kattrE4
+#define COMBINATORICSRANGETYPE_kattrE4
+
+namespace OKlib {
+  namespace Combinatorics {
+    namespace traits {
+
+      /*!
+        \class range_type
+        \brief Traits meta-function to extract the range type
+      */
+      
+      template <typename T>
+      struct range_type {
+        typedef typename T::range_type type;
+      };
+      
+    }
+    
+  }
+
+}
+
+#endif
