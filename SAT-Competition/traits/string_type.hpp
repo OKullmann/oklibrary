420e7121229a6a32638b0f573541a06f1d966820
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-19 12:23:05 +0000

    These were before in Traits.

---
+++ SAT-Competition/traits/string_type.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 30.12.2005 (Swansea)
+
+/*!
+  \file SATCompetition/traits/string_type.hpp
+  \brief Traits-metafunctions to extract the string type.
+*/
+
+#ifndef STRINGTYPE_jjdggtRE
+#define STRINGTYPE_jjdggtRE
+
+namespace OKlib {
+  namespace SATCompetition {
+    namespace traits {
+
+      /*!
+        \class string_type
+        \brief Traits meta-function to extract the string type.
+      */
+
+      template <typename T>
+      struct string_type {
+        typedef typename T::string_type type;
+      };
+
+    }
+
+  }
+
+}
+
+#endif
