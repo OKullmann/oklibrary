e1c0a3e741a56b6b61a4f6e202b4346eed5bd21b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-20 15:24:50 +0000

    Todo regarding different compiler versions added.

--- Messages/plans/Messages.hpp
+++ Messages/plans/Messages.hpp
@@ -3,6 +3,13 @@
 /*!
   \file Messages/plans/Messages.hpp
   \brief Plans for the messages module
+
+  \todo Testing:
+   - We need here a preliminary compiler switch, which disables
+     testing of fr_CA for versions different from 4.0.3 and 4.1.1.
+     Later we will request compiler version 4.1.1 or higher,
+     but first we have to see that there are no performance
+     problems (regarding compilation and execution).
 */
 
 /*!
