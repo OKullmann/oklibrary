1f1a4bb540a5996153c55aaac1d0be4dab250650
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-23 18:50:42 +0000

    Plans regarding the global README file.

---
+++ Buildsystem/plans/README.hpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 23.6.2006 (Swansea)
+
+/*!
+  \file Buildsystem/plans/README.hpp
+  \brief Plans for the README file
+
+  \todo Supported versions of gcc:
+  We need to clean-up regarding the supported versions of gcc.
+  Perhaps actually we should support only from 4.1.1 onwards.
+
+*/
+
