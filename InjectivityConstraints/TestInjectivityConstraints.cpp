db396ec972bd04ab9ca5a7f48c635eee0b745adc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-23 18:48:10 +0000

    Empty old test-stuff is no longer needed.

--- InjectivityConstraints/TestInjectivityConstraints.cpp
+++
@@ -1,8 +0,0 @@
-// Matthew Henderson, 24.4.2006 (Paderborn)
-
-/*!
-  \file InjectivityConstraints/TestInjectivityConstraints.cpp
-  \brief Empty test program to satisfy old test system.
-*/
-
-int main() {}
