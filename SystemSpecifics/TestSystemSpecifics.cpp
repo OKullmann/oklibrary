b68eb987f691e1ffcc12391637b33ebaf59cbbe1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-23 19:34:58 +0000

    Empty old tests.

--- SystemSpecifics/TestSystemSpecifics.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <Transitional/Testsystem/TestBaseClass.hpp>
-
-#include <Transitional/SystemSpecifics/SafeReading_Testobjects.hpp>
-
-int main() {
-  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
-} 
