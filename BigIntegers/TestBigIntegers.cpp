f8e2c837130fcbba2460ecb1c9cb9af748750313
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-26 15:59:00 +0000

    Either moved to plans, or not needed.

--- BigIntegers/TestBigIntegers.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <Transitional/Testsystem/TestBaseClass.hpp>
-
-#include <Transitional/BigIntegers/GMPWrapper_Testobjects.hpp>
-
-int main() {
-  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
-} 
