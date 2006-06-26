259646803704756816c4f654d21fdc87a81b1500
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-06-26 16:55:46 +0000

    Transferred.

---
+++ TreeDecompositions/plans/TreeDecompositions.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 2.1.2006 (Swansea)
+
+/*!
+  \file TreeDecompositions/plans/TreeDecompositions.hpp
+  \brief Plans for the module regarding tree decompositions
+*/
+
+/*!
+  \namespace OKlib::TreeDecompositions
+  \brief Module for tree decompositions
+*/
+
+namespace OKlib {
+  namespace TreeDecompositions {
+  }
+}
+
