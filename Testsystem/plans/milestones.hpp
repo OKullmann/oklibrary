b9ce29478c29781c6d6529faf4f3f29ca4a9e7eb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-26 18:28:13 +0000

    New targets 0.1.1 and 0.2 added.

--- Testsystem/plans/milestones.hpp
+++ Testsystem/plans/milestones.hpp
@@ -5,5 +5,20 @@
 
   \par Current version 0.1 (24.8.2006)
 
+  \par Version 0.1.1 :
+  The first todos in the other plans-files (which just ask to update
+  the doxygen-documentation and the plans) are performed.
+
+  \par Version 0.2 :
+  The new test system is ready to use.
+  
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1 : 24.8.2006, initial version number (the old test system is
+     working, while the new test system is being established).
+
 */
 
