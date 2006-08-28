1e371cfc980061a06305b14e9d15e2383a651602
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-28 12:50:19 +0000

    Start with simple todo.

---
+++ Testsystem/plans/BasicDeclarations.hpp
@@ -0,0 +1,10 @@
+// Oliver Kullmann, 27.8.2006 (Swansea)
+
+/*!
+  \file Testsystem/plans/BasicDeclarations.hpp
+  \brief Plans for the module with basic declarations for the test system
+
+  \todo Write a short motivation for the existence of Testsystem/BasicDeclarations.hpp.
+
+*/
+
