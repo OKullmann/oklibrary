776774ec028bb5a7726453603832494f67a5fe54
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-28 12:53:34 +0000

    Doxygen-comments added.

--- Testsystem/BasicDeclarations.hpp
+++ Testsystem/BasicDeclarations.hpp
@@ -11,7 +11,19 @@
 namespace OKlib {
   namespace TestSystem {
 
+    /*!
+      \typedef depth_number_type
+      \brief Unsigned integral type to represent the level of
+      nesting of test-metafunctions-calls.
+    */
+
     typedef unsigned long int depth_number_type;
+
+    /*!
+      \typedef  line_number_type;
+      \brief Unsigned integral type to represent source-code line numbers.
+    */
+
     typedef unsigned long int line_number_type;
 
   }
