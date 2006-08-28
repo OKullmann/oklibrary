039fa9aecdd86032b24c45bf23ddb90f9dae8b88
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-28 12:51:40 +0000

    Editorial.

--- Testsystem/plans/TestProgram.hpp
+++ Testsystem/plans/TestProgram.hpp
@@ -4,8 +4,8 @@
   \file Testsystem/plans/TestProgram.hpp
   \brief Plans for the core test program
 
-  \todo Once the transition is done, rename "TestProgram_DesignStudy.hpp" to "TestProgram.hpp".
   \todo Editorial clean-up, and doxygen-documentation.
   \todo Overhaul todos in TestProgram_DesignStudy.hpp, and transfer them to this file.
+  \todo Once the transition is done, rename "TestProgram_DesignStudy.hpp" to "TestProgram.hpp".
 */
 
