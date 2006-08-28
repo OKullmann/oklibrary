1a13ad3a760599a86be51a7ea9599dbdaa1cd094
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-28 12:52:38 +0000

    Update regarding target 0.1.1.

--- Testsystem/plans/milestones.hpp
+++ Testsystem/plans/milestones.hpp
@@ -7,7 +7,15 @@
 
   \par Version 0.1.1 :
   The first todos in the other plans-files (which just ask to update
-  the doxygen-documentation and the plans) are performed.
+  the doxygen-documentation and the plans) are performed:
+   - plans/TestFondement.hpp : DONE
+   - plans/BasicDeclarations.hpp : DONE
+   - plans/TestBaseClass.hpp : DONE
+   - plans/RunTest_Declarations.hpp
+   - plans/RunTest.hpp
+   - plans/TestExceptions.hpp
+   - plans/TestProgram.hpp.
+  And milestone 0.1.2 is created.
 
   \par Version 0.2 :
   The new test system is ready to use.
