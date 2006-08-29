b0b57d8a2bbca664030b4c08375e7fe52f47eebc
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2006-08-29 21:04:55 +0000

    New files necessary for transition from Testsystem (lowercase 's') to TestSystem (uppercase 'S').

---
+++ TestSystem/BasicDeclarations.hpp
@@ -0,0 +1,33 @@
+// Oliver Kullmann, 19.1.2006 (Swansea)
+
+/*!
+  \file TestSystem/BasicDeclarations.hpp
+  \brief Basic declarations for the test system
+*/
+
+#ifndef BASICDECLARATIONS_kJhYt65
+#define BASICDECLARATIONS_kJhYt65
+
+namespace OKlib {
+  namespace TestSystem {
+
+    /*!
+      \typedef depth_number_type
+      \brief Unsigned integral type to represent the level of
+      nesting of test-metafunctions-calls.
+    */
+
+    typedef unsigned long int depth_number_type;
+
+    /*!
+      \typedef  line_number_type;
+      \brief Unsigned integral type to represent source-code line numbers.
+    */
+
+    typedef unsigned long int line_number_type;
+
+  }
+
+}
+
+#endif
---
+++ TestSystem/RunTest_Declarations.hpp
@@ -0,0 +1,69 @@
+// Oliver Kullmann, 25.2.2006 (Swansea)
+
+/*!
+  \file TestSystem/RunTest_Declarations.hpp
+  \brief Declarations for the class ::OKlib::TestSystem::RunTest
+
+  With this class, tests are first inserted into a global container, and then executed.
+*/
+
+#ifndef RUNTESTDECLARATIONS_jJJhhBc5
+#define RUNTESTDECLARATIONS_jJJhhBc5
+
+#include <ostream>
+#include <string>
+
+#include <boost/ptr_container/ptr_list.hpp>
+
+#include <Transitional/TestSystem/TestFondement.hpp>
+
+namespace OKlib {
+
+  namespace TestSystem {
+
+    /*!
+      \class RunTest
+      \brief Responsible for running tests
+
+      Objects of type RunTest are initialised with a pointer to a TestSystem::Test object,
+      and the pointer is put into a global container.
+      Via the static member function RunTest::run_test all these tests are executed.
+
+      Meaning of the three output-streams passed to run_tests:
+       - err : error messages (extracted from the exceptions thrown by test-meta-functions)
+       - messages : messages from the test system, reporting on the general progress
+       - log : messages by the test-meta-functions (typically reporting on special test conditions).
+    */
+
+    class RunTest {
+      RunTest(const RunTest&); // not available
+      RunTest& operator =(const RunTest&); // not available
+
+    public :
+
+      //! The type of the test objects
+      typedef ::OKlib::TestSystem::Test test_type;
+      typedef test_type* test_pointer_type;
+      typedef boost::ptr_list<test_type> container_type;
+
+      //! The only constructor; inserts parameter "test" into the (singleton-)container of test-object(-pointers).
+      /*! RunTest takes over ownership of the object pointed to by "test". */
+      RunTest(test_pointer_type const test);
+
+      //! Runs all the tests in the build-in test-container; return Unix error code
+      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level);
+      //! Runs all the tests in the provided container test_objects; return Unix error code
+      static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level, container_type& test_objects);
+
+    private :
+
+      //! Contains as static object the (singleton-)test-container (of type container_type); p is inserted into the container if not null.
+      static container_type& handle_test_objects(const test_pointer_type p = 0);
+
+    };
+
+  }
+
+}
+
+#endif
