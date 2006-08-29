4b56f49bf251baccdcfaab5a933bdd430c6896d5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-29 13:37:49 +0000

    Doxygen-documentation extended (now alright), some editorial changes regarding the typedef test_pointer_type, and removed old functions banner_log, banner_messages.

--- Testsystem/RunTest_Declarations.hpp
+++ Testsystem/RunTest_Declarations.hpp
@@ -2,9 +2,9 @@
 
 /*!
   \file Testsystem/RunTest_Declarations.hpp
-  \brief Declarations for ::OKlib::TestSystem::RunTest
+  \brief Declarations for the class ::OKlib::TestSystem::RunTest
 
-  With this class tests are first inserted into a global container, and then executed.
+  With this class, tests are first inserted into a global container, and then executed.
 */
 
 #ifndef RUNTESTDECLARATIONS_jJJhhBc5
@@ -28,6 +28,11 @@ namespace OKlib {
       Objects of type RunTest are initialised with a pointer to a TestSystem::Test object,
       and the pointer is put into a global container.
       Via the static member function RunTest::run_test all these tests are executed.
+
+      Meaning of the three output-streams passed to run_tests:
+       - err : error messages (extracted from the exceptions thrown by test-meta-functions)
+       - messages : messages from the test system, reporting on the general progress
+       - log : messages by the test-meta-functions (typically reporting on special test conditions).
     */
 
     class RunTest {
@@ -36,21 +41,25 @@ namespace OKlib {
 
     public :
 
+      //! The type of the test objects
       typedef ::OKlib::TestSystem::Test test_type;
-      typedef Test* test_pointer_type;
+      typedef test_type* test_pointer_type;
       typedef boost::ptr_list<test_type> container_type;
 
-      RunTest(::OKlib::TestSystem::Test* const test);
+      //! The only constructor; inserts parameter "test" into the (singleton-)container of test-object(-pointers).
+      /*! RunTest takes over ownership of the object pointed to by "test". */
+      RunTest(test_pointer_type const test);
 
+      //! Runs all the tests in the build-in test-container; return Unix error code
       static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level);
+      //! Runs all the tests in the provided container test_objects; return Unix error code
       static int run_tests(std::ostream& err, std::ostream& messages, std::ostream& log, ::OKlib::TestSystem::TestLevel& level, container_type& test_objects);
 
     private :
 
+      //! Contains as static object the (singleton-)test-container (of type container_type); p is inserted into the container if not null.
       static container_type& handle_test_objects(const test_pointer_type p = 0);
 
-      static std::string banner_log(); 
-      static std::string banner_messages();
     };
 
   }
