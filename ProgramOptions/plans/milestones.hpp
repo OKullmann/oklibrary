5a9ac33ff6c2994b31d5b1686e29bd7f99b2e195
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-30 20:20:20 +0000

    Initial version 0.0.3.

---
+++ ProgramOptions/plans/milestones.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 30.8.2006 (Swansea)
+
+/*!
+  \file ProgramOptions/plans/milestones.hpp
+
+  \par Current version 0.0.3 (30.8.2006)
+
+  \par Version 0.0.4 :
+  The plans in ProgramOptions/plans/ProgramOptions.hpp are reconsidered, and made
+  systematic, so that milestones for reaching 0.1 can be created (at least the initial ones).
+
+  \par Version 0.1 :
+  The new program option system is ready to use. Especially it can be used for
+  Testsystem/TestProgram.cpp.
+  
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 30.8.2006, initial version number (some plans have been made, and there is the
+   old system ("Kommandozeile")).
+
+*/
+
