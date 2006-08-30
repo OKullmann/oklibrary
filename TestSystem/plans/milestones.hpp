dfb5843cd5fc81009d4f622235fcd0c79b8ab99c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-30 20:00:35 +0000

    New version of TestSystem: 0.1.1.

--- TestSystem/plans/milestones.hpp
+++ TestSystem/plans/milestones.hpp
@@ -3,21 +3,41 @@
 /*!
   \file TestSystem/plans/milestones.hpp
 
-  \par Current version 0.1 (24.8.2006)
+  \par Current version 0.1.1 (30.8.2006)
 
-  \par Version 0.1.1 :
+  \par Version 0.1.2 :
 
-  The first todos in the other plans-files (which just ask to update
-  the doxygen-documentation and the plans) are performed:
-   - plans/TestFondement.hpp : DONE
-   - plans/BasicDeclarations.hpp : DONE
-   - plans/TestBaseClass.hpp : DONE
-   - plans/RunTest_Declarations.hpp : DONE
-   - plans/RunTest.hpp : DONE
-   - plans/TestExceptions.hpp : DONE
-   - plans/TestProgram.hpp.
+  In TestSystem/plans/TestSystem.hpp the following topics are handled:
+  - Renaming (MH)
 
-  And milestone 0.1.2 is created.
+  In TestSystem/plans/BasicDeclarations.hpp the following topics are handled:
+  - Motivation
+    
+  In TestSystem/plans/TestBaseClass.hpp the following topics are handled:
+  - Messages
+  - Log-Messages
+  - Testing
+
+  \par Version 0.1.3 :
+
+  In TestSystem/plans/TestExceptions.hpp the following topics are handled:
+  - TestSystem::ErrorDescription
+  - TestSystem::TestException
+
+  \par Version 0.1.4 :
+
+  In TestSystem/plans/TestProgram.hpp the following topics are handled:
+  - Messages
+  - Infos
+
+  \par Version 0.1.5 :
+
+  Expand and update the tests, so that the test system is thoroughly tested.
+
+  \par Version 0.1.6 :
+
+  In TestSystem/plans/TestSystem.hpp the following topics are handled:
+  - Build system
 
   \par Version 0.2 :
   The new test system is ready to use.
@@ -29,6 +49,7 @@
 
    - 0.1 : 24.8.2006, initial version number (the old test system is
      working, while the new test system is being established).
+   - 0.1.1 : 30.8.2006, plans and todos have been updated, and initial doxygen documentation provided.
 
 */
 
