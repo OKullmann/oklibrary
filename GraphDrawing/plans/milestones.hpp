841707a75433196bdd46c936c589032b82646556
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-08-30 20:17:28 +0000

    Initial version number 0.0.2.

---
+++ GraphDrawing/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 30.8.2006 (Swansea)
+
+/*!
+  \file GraphDrawing/plans/milestones.hpp
+
+  \par Current version 0.0.2 (30.8.2006)
+
+  \par Version 0.1 :
+  Trees can be drawn, suitable for visualising the run of backtracking SAT solvers.
+  
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 30.8.2006, initial version number (some plans have been made).
+
+*/
+
