ab3d6abe3e7baed0b1bac598b7d47958113b2198
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-01 15:03:46 +0000

    Initial version 0.0.2.

---
+++ Equivalences/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 1.9.2006 (Swansea)
+
+/*!
+  \file Equivalences/plans/milestones.hpp
+
+  \par Current version 0.0.2 (1.9.2006)
+
+  \par Version 0.0.3 :
+
+  First we need some overview on the literature.
+  And then we need to make (initial) plans on how to proceed.
+
+  \par Version 0.1 :
+
+  We have concepts for active clause-sets representing equivalences, and concepts
+  for extracting equivalences from given clause-sets.
+
+  \par Version 0.2 :
+
+  First implementations.
+
+  
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 24.8.2006, initial version number (some initial thoughts).
+
+*/
+
