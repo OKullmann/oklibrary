fc36f91b59a66c916551fd962a81d4840f6f34e4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-06 12:52:15 +0000

    Extended.

--- Messages/plans/Messages.hpp
+++ Messages/plans/Messages.hpp
@@ -4,12 +4,15 @@
   \file Messages/plans/Messages.hpp
   \brief Plans for the messages module
 
+  \todo Update:
+  Move all relevant todos to the plans-directory, and update the doxygen-documentation.
+
   \todo Testing:
    - We need here a preliminary compiler switch, which disables
      testing of fr_CA for versions different from 4.0.3 and 4.1.1.
      Later we will request compiler version 4.1.1 or higher,
      but first we have to see that there are no performance
-     problems (regarding compilation and execution).
+     problems with these later gcc-versions (regarding compilation and execution).
 */
 
 /*!
