0a6137bbff4460ad2b56983c3dbdab536685819c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-06 12:54:10 +0000

    Initial version 0.0.5.

---
+++ Messages/Utilities/plans/milestones.hpp
@@ -0,0 +1,48 @@
+// Oliver Kullmann, 5.9.2006 (Swansea)
+
+/*!
+  \file Messages/Utilities/plans/milestones.hpp
+
+  \par Current version 0.0.5 (5.9.2006)
+
+  \par Version 0.0.6 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - QuickMessage
+
+  \par Version 0.0.7 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - Service for test exceptions
+  - Line number identification
+
+  \par Version 0.0.8 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - Warning and error messages:
+
+  \par Version 0.0.9 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - Time and Date
+
+  \par Version 0.0.10 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - Error handling
+
+  \par Version 0.1 :
+
+  In Messages/Utilities/plans/Utilities.hpp the following topics are handled:
+  - Testing
+  All components are tested (via the new test system).
+
+  
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.5 : 5.9.2006, initial version number (some simple components for time, dates and umlaute).
+
+*/
+
