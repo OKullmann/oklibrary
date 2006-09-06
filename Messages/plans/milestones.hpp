c2cafac4c8eb3b2d7be3f6eaf52f00c42b633356
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-06 12:53:31 +0000

    Towards version 0.8: milestones 0.7.1 - 0.7.3 added.

--- Messages/plans/milestones.hpp
+++ Messages/plans/milestones.hpp
@@ -5,6 +5,25 @@
 
   \par Current version 0.7 (3.3.2006)
 
+  \par Version 0.7.1 :
+
+   - In Messages/Utilities/plans/milestones.hpp version 0.0.6 is reached.
+   - The milestones 0.8, 0.9, 1.0 below have been refined.
+
+  \par Version 0.7.2 :
+
+   - In Messages/Utilities/plans/milestones.hpp version 0.0.7 is reached.
+
+  In Messages/plans/Messages.hpp the following topics are handled:
+  - Update
+
+  \par Version 0.7.3 :
+
+   - In Messages/Utilities/plans/milestones.hpp version 0.0.8 is reached.
+
+  In Messages/plans/Messages.hpp the following topics are handled:
+  -  Testing
+
   \par Version 0.8 :
   Help for memory management of message objects (components, or at least some guidelines)
   available.
@@ -12,7 +31,7 @@
   \par Version 0.9 :
   Documentation completed.
 
-  \par Version 1.0
+  \par Version 1.0 :
   Tests for all components available and applied (see "Testing" in Messages/plans/Messages.hpp)
 
 
