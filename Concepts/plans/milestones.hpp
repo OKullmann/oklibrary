7d4a45a843f5d881433b89eb4d2d568ae757fa03
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-11 13:33:53 +0000

    Initial version 0.1.

---
+++ Concepts/plans/milestones.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 10.9.2006 (Swansea)
+
+/*!
+  \file Concepts/plans/milestones.hpp
+
+  \par Current version 0.1 (10.9.2006)
+
+  \par Version 0.1.1 :
+  In Concepts/plans/Concepts.hpp the following
+  topics are handled:
+  - Update
+  And further milestones are created.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.1 : 10.9.2006; initial version number (concepts are under work for around 2 years).
+
+*/
+
