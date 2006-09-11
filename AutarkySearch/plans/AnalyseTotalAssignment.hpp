f561bdfd7b60f195860eb9c7d95897af9e5f3ce0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-11 13:19:36 +0000

    Editorial.

--- AutarkySearch/plans/AnalyseTotalAssignment.hpp
+++ AutarkySearch/plans/AnalyseTotalAssignment.hpp
@@ -5,12 +5,15 @@
   \brief Decision, whether a given partial assignment contains an autarky, and
   applications for autarky search (via enumeration of all total assignments, or
   local search through total assignments).
+
   \todo The basic algorithm is: Given a partial assignment phi, unassign all
   variables in critical clauses, and repeat this process until no critical clauses
   are left --- the maximal autarky contained in phi is obtained. Design and
   implement, using the data structures from SupportTotalAssignment.
+
   \todo Combination with enumeration of all total assignments yields
   autarky search in time O(2^n).
+
   \todo Combination with local search yields local search for autarkies (using
   total assignments). As the value of a total assignments the minimal number
   of critical clauses over all (non-empty) levels could be used. (If a low number
--- AutarkySearch/plans/EvaluatePartialAssignments.hpp
+++ AutarkySearch/plans/EvaluatePartialAssignments.hpp
@@ -5,11 +5,13 @@
   \brief Evaluation of partial assignments, computing a heuristical
   value of how close the assignment is to an autarky for a given
   clause-set.
+
   \todo The two most basic measures are the number of critical clauses
   and the number of variables of (assigned) variables involved in critical
   clauses. Since this gives preferences to small partial assignments, one
   can standardise these numbers by dividing through the number of
   all touched clauses resp. the number of all assigned variables.
+
   \todo The main design issue for evaluation concepts is how to
   integrate the evaluation process into the updating mechanism (so
   that unnecessary runs over the clause-set can be avoided).
