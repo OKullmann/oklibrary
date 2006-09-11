3a7e962e21b1e30e851a2da5105227dd774c9d67
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-11 13:21:39 +0000

    Initial version 0.0.3.

---
+++ AutarkySearch/plans/milestones.hpp
@@ -0,0 +1,29 @@
+ // Oliver Kullmann, 10.9.2006 (Swansea)
+
+/*!
+  \file AutarkySearch/plans/milestones.hpp
+
+  \par Current version 0.0.3 (10.9.2006)
+
+  \par Version 0.0.4 :
+  In AutarkySearch/plans/AutarkySearch.hpp the topic
+  "First implementation for AnalyseTotalAssignment" is further expanded,
+  and further milestones are created.
+
+  \par Version 0.1 :
+  We have a first implementation using the method of AnalyseTotalAssignment,
+  and start experimentation.
+
+  \par Version 1.0 :
+  Completely within the OKlibrary we can search for autarkies analysing total
+  assignments or searching through partial assignments.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.3 : 10.9.2006; initial version number (several plans).
+
+*/
+
