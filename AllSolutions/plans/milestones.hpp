b95a68f0e463b0815d95b4040719d62c4c96f3ca
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-11 13:19:10 +0000

    Initial version 0.0.2.

---
+++ AllSolutions/plans/milestones.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 10.9.2006 (Swansea)
+
+/*!
+  \file AllSolutions/plans/milestones.hpp
+
+  \par Current version 0.0.2 (10.9.2006)
+
+  \par Version 0.0.3 :
+  Expand the planes, and create milestones.
+
+  \par Version 0.1 :
+  We have first implementations for enumerating all solutions.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.2 : 10.9.2006; initial version number (a few plans).
+
+*/
+
