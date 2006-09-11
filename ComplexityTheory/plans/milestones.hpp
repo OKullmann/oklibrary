e271e05e84315ccb15b4b08f946034c78d5eb9d0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-09-11 13:33:08 +0000

    Initial version 0.0.2.

---
+++ ComplexityTheory/plans/milestones.hpp
@@ -0,0 +1,37 @@
+// Oliver Kullmann, 10.9.2006 (Swansea)
+
+/*!
+  \file ComplexityTheory/plans/milestones.hpp
+
+  \par Current version 0.0.2 (10.9.2006)
+
+  \par Version 0.0.3 :
+  In ComplexityTheory/plans/ComplexityTheory.hpp the following
+  topics are handled:
+  - Grammars
+  And milestones have been refined.
+
+  \par Version 0.1 :
+  In ComplexityTheory/plans/ComplexityTheory.hpp the following
+  topics are handled:
+  - Simulation
+
+  \par Version 0.2 :
+  In ComplexityTheory/plans/ComplexityTheory.hpp the following
+  topics are handled:
+  - NP-completeness
+
+  \par Version 0.3 :
+  In ComplexityTheory/plans/ComplexityTheory.hpp the following
+  topics are handled:
+  - NP-completeness 2
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.2 : 10.9.2006; initial version number (a few plans).
+
+*/
+
