313c77fed35617bb39da9950e38b5b416547ce61
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-02 17:52:09 +0000

    Moved from HypergraphColouring to here.

---
+++ RamseyTheory/TestRamseyTheory.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <Transitional/TestSystem/TestBaseClass.hpp>
+
+#include <Transitional/RamseyTheory/Van_der_Waerden_hypergraph_Testobjects.hpp>
+
+int main() {
+  return OKlib::TestSystem::TestBase::run_tests_default(std::cout);
+}
---
+++ RamseyTheory/Van_der_Waerden_hypergraph_Testobjects.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 15.5.2005 (Swansea)
+
+#ifndef VANDERWAERDENHYPERGRAPHTESTOBJECTSS_1U8VvT56
+
+#define VANDERWAERDENHYPERGRAPHTESTOBJECTSS_1U8VvT56
+
+#include <Transitional/RamseyTheory/Van_der_Waerden_hypergraph.hpp>
+#include <Transitional/RamseyTheory/Van_der_Waerden_hypergraph_Tests.hpp>
+
+namespace OKlib {
+
+  namespace Van_der_Waerden_hypergraph {
+
+    namespace Testobjects {
+
+      Test_Iterator_arithmetic_progression_Integer<Iterator_arithmetic_progression> test_iterator_arithmetic_progression;
+
+      Test_Arithmetic_progression_Integer<Arithmetic_progression> test_arithmetic_progression;
+      
+    }
+
+  }
+
+}
+
+#endif
