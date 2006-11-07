7691fa469b544aac1977be06d7cbaefdfd4c34e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:25:04 +0000

    Some initial plans.

---
+++ HittingClauseSets/plans/ExtendingHittingClauseSets.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 13.11.2005 (Swansea)
+
+/*!
+  \file HittingClauseSets/plans/ExtendingHittingClauseSets.hpp
+  \brief Algorithms for extending hitting clause-sets
+
+  \todo Write the following algorithm which systematically
+  searches for an extension of an input hitting clause-set F:
+  The input is enriched with Tr(F) (see (currently)
+  HypergraphTransversals/DirectTransversalEnumeration.hpp),
+  so that easily all interesting extensions by one clause
+  can be considered by a visitor; of these only the non-isomorphic
+  ones (that is, where the extended clause-sets are non-isomorphic)
+  are considered, and then recursively the algorithm tries to extend
+  the clause-set further, until the visitor is "satisfied" (or no
+  suitable extension exists). Either only the first solution is output,
+  or all.
+
+  \todo The first application of the previous algorithm is to find
+  for k >= 1 boolean hitting clause-sets with constant clause-length k and
+  2^k clauses, which have as many variables as possible. Interesting
+  also the structure of the extremal clause-sets (for example
+  regarding symmetry). Instead of maximising the number of variables
+  one can minimise the maximal variable occurrence. Also the total
+  number of finally obtained clause-sets is of interest (without removing
+  isomorphic ones) in the context of determining precise probabilities
+  for satisfiability of random formulas.
+*/
+
---
+++ HittingClauseSets/plans/HittingClauseSets.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 24.2.2006 (Swansea)
+
+/*!
+  \file HittingClauseSets/plans/HittingClauseSets.hpp
+  \brief General plans for the module on hitting clause-sets
+
+  \todo Given a rooted tree, a corresponding SMUSAT_{delta=1} needs to
+  to be constructed. Special cases for the full tree and the input tree.
+
+  \todo More generally, for an arbitrary rooted tree and a labelling of inner
+  nodes with variables, such that no variable occurs twice on any path from
+  the root to some leaf, construct the corresponding (tree-)hitting clause-set.
+*/
+
+/*!
+  \namespace OKlib::HittingClauseSets
+  \brief Module for components regarding hitting clause-sets
+*/
+
+namespace OKlib {
+  namespace HittingClauseSets {
+  }
+}
+
