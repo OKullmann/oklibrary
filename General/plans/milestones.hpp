a63f905fac216e3a93a3eefc286318592e5996eb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:18:30 +0000

    Initial version 0.5.

---
+++ General/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file General/plans/milestones.hpp
+
+  \par Current version 0.5 (6.11.2006; a stable (old-style) library)
+
+  \par Version 0.5.1 :
+  The first task is to create a plan and milestones for the transition
+  (see "Transition" in General/plans/General.hpp).
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.5 : 6.11.2006; initial version number (a stable library with many components, in the old style).
+
+*/
+
