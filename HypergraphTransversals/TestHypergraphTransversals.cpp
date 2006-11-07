0616cdf5279d92756cd1d7d5e858d90f15b5c2d9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:23:54 +0000

    Empty (old) tests removed.

--- HypergraphTransversals/TestHypergraphTransversals.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <Transitional/TestSystem/TestBaseClass.hpp>
-
-#include <Transitional/HypergraphTransversals/DirectTransversalEnumeration_Testobjects.hpp>
-
-int main() {
-  return OKlib::TestSystem::TestBase::run_tests_default(std::cout);
-}
