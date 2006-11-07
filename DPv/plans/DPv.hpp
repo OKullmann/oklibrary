465e18b0c05088c78626baeee3c567a3f28c28eb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:09:29 +0000

    Initial plans: How much can this module be used?

---
+++ DPv/plans/DPv.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file DPv/plans/DPv.hpp
+  \brief Old module for components around the DP-procedure
+
+  \todo The central question is how much of this module is still of use?
+  Perhaps not very much; but at least it could serve to establish the first "clues".
+*/
+
+/*!
+  \namespace OKlib::DPv
+  \brief Deprecated namespace containing components for Davis-Putnam resolution
+*/
+
+namespace OKlib {
+  namespace DPv {
+  }
+}
+
