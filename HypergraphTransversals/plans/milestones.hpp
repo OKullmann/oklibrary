dc0ccfe0c8c464f301ce71f63db450f51430ac7e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:27:18 +0000

    Initial version 0.0.3.

---
+++ HypergraphTransversals/plans/milestones.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file HypergraphTransversals/plans/milestones.hpp
+
+  \par Current version 0.0.3 (6.11.2006; several plans)
+
+  \par Version 0.0.4 :
+  Create milestones.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.3 : 6.11.2006; initial version number (several plans).
+
+*/
+
