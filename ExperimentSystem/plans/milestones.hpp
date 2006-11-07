2ca12ed308349b9645a206856286662edb00bd87
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:14:06 +0000

    Initial version number 0.1.

---
+++ ExperimentSystem/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file ExperimentSystem/plans/milestones.hpp
+
+  \par Current version 0.1 (6.11.2006; a working system exists, but not integrated into the OKlibrary)
+
+  \par Version 0.1.1 :
+  The first task is to make a plan and create milestones for the transition and extension of the old
+  system (see ExperimentSystem/plans/ExperimentSystem.hpp).
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.1 : 6.11.2006; initial version number (external working system).
+
+*/
