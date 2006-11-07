fa91759d367944ed020c9d6b606f6338140001be
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:19:48 +0000

    Some initial plans (for updating).

---
+++ GeneralInputOutput/plans/GeneralInputOutput.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 26.12.2005 (Swansea)
+
+/*!
+  \file GeneralInputOutput/plans/GeneralInputOutput.hpp
+  \brief Plans for the module on general input and output facilities
+
+  \todo Update plans
+  Write plans for all submodules, transfer all todo's, and update the doxygen-documentation.
+
+  IOStreamFilters :
+  IOStreamSinks :
+  Multiplexer :
+  RecursiveDirectoryIteration :
+
+  \todo New tests
+  Transfer the tests to the new test system
+*/
+
+/*!
+  \namespace OKlib::GeneralInputOutput
+  \brief Tools for handling input and output.
+*/
+
+namespace OKlib {
+  namespace GeneralInputOutput {
+  }
+}
