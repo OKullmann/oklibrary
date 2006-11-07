d6e5bc7db6fc1ba12a320fb7631ed5623d85b9cf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:20:22 +0000

    Initial version 0.1.

---
+++ GeneralInputOutput/plans/milestones.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file GeneralInputOutput/plans/milestones.hpp
+
+  \par Current version 0.1 (6.11.2006)
+
+  \par Version 0.1.1 :
+  In GeneralInputOutput/plans/GeneralInputOutput.hpp the topic
+  "Update plans" is further expanded, and further milestones are created.
+
+  \par Version 0.2 :
+  In GeneralInputOutput/plans/GeneralInputOutput.hpp the topic
+  "New tests" has been handled.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.1 : 6.11.2006; initial version number (several working sub-modules).
+
+*/
+
