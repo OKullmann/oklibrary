742d3653155c27009b3e89bdd657f32608ef7998
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:25:25 +0000

    Initial version 0.0.2.

---
+++ HittingClauseSets/plans/milestones.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file HittingClauseSets/plans/milestones.hpp
+
+  \par Current version 0.0.2 (6.11.2006; some plans)
+
+  \par Version 0.0.3 :
+  The first task is create milestones.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.1 : 6.11.2006; initial version number (some plans).
+
+*/
