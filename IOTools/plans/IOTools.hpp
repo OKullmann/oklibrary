b3604b27550eca05250570222df6ce5784355f26
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:32:27 +0000

    Initial plans: Is this module of any use anymore?

---
+++ IOTools/plans/IOTools.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file IOTools/plans/IOTools.hpp
+  \brief Plans for the deprecated module on input/output tools
+
+  \todo Decide what to do with this module. Is it used anymore at all ?!
+*/
+
+/*!
+  \namespace OKlib::IOTools
+  \brief Input/output tools (deprecated)
+*/
+
+namespace OKlib {
+  namespace IOTools {
+  }
+}
+
