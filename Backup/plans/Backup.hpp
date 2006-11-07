7bfad68ddbe6ed478acf9fcb35f8607ff8bf749b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 14:39:33 +0000

    Initial plans: What to do with this module?

---
+++ Backup/plans/Backup.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file Backup/plans/Backup.hpp
+  \brief Plans regarding the backup-program written by Tony Bao
+
+  \todo What to do with this module? In principle it should be useful to have a C++
+  program for the backup; the main question then is how far we investigate this code.
+  The code should offer basic functionality, which can at least be considered (no higher
+  design, but on the C-level it should be okay).
+*/
+
+/*!
+  \namespace OKlib::Backup
+  \brief Components for backups
+*/
+
+namespace OKlib {
+  namespace Backup {
+  }
+}
+
