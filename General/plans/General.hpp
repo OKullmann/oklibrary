89e7bf80349ea14a23138e26be8dee9c2fbc2ca1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-07 16:18:11 +0000

    Some initial plans (for the transition).

---
+++ General/plans/General.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 6.11.2006 (Swansea)
+
+/*!
+  \file General/plans/General.hpp
+  \brief Plans for the (old) module on "general" components
+
+  \todo Transition
+
+  At the end of the day, all the components in this module
+  should be transferred (and updated and extended) to appropriate
+  (new) modules (then working inside namespace OKlib).
+
+  This might take a while (for the whole module),
+  and an intermediate step is to update the tests to the new
+  test system.
+*/
+
