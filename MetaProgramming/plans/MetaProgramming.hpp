fe8f2bf511eafcf7a480fee97248bc706a5966d3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:43:35 +0000

    A few plans.

---
+++ MetaProgramming/plans/MetaProgramming.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 8.11.2006 (Swansea)
+
+/*!
+  \file MetaProgramming/plans/MetaProgramming.hpp
+  \brief Plans for the metaprogramming module
+
+  \todo Update
+  Write plans-files, transfer the todo's, and update the doxygen documentation:
+
+   - Logical.hpp : 
+   - Numerical.hpp : 
+   - TaggingPolymorphism.hpp : 
+
+   \todo Transfer tests to the new test system
+*/
+
+/*!
+  \namespace MetaProgramming
+  \brief Components to support meta programming
+*/
+
+namespace OKlib {
+  namespace MetaProgramming {
+  }
+}
+
