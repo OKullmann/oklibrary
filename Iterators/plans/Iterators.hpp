bb78df0a13b9a5f677f90b3df0dac23f2be96f2f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:24:19 +0000

    A few plans.

---
+++ Iterators/plans/Iterators.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 26.12.2005 (Swansea)
+
+/*!
+  \file Iterators/plans/Iterators.hpp
+  \brief Plans for the iterators module
+
+  \todo Update
+  Create plans, transfer the todo's, update the doxygen-documentation.
+  
+  \todo New test system
+  Switch to the new test system
+*/
+
+/*!
+  \namespace OKlib::Iterators
+  \brief General tools for iterator handling
+*/
+
+namespace OKlib {
+  namespace Iterators {
+  }
+}
---
+++ KLevelForcedAssignments/plans/KLevelForcedAssignments.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 17.12.2005 (Swansea)
+
+/*!
+  \file KLevelForcedAssignments/plans/KLevelForcedAssignments.hpp
+  \brief Plans for the module KLevelForcedAssignments
+
+  \todo First the scope (the (informal) concept) has to be established.
+*/
+
+/*!
+  \namespace OKlib::KLevelForcedAssignments
+  \brief Components for generalised unit clause elimination
+*/
+
+namespace OKlib {
+  namespace KLevelForcedAssignments {
+  }
+}
+
