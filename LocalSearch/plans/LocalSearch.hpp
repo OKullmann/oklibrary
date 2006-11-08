a1a7fb9d39f755c155ca86650a04e352d6889255
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:40:39 +0000

    Some plans (about local search in general).

---
+++ LocalSearch/plans/LocalSearch.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 7.11.2006 (Swansea)
+
+/*!
+  \file LocalSearch/plans/LocalSearch.hpp
+  \brief Plans for the general module on local search
+
+  \todo Create first milestones.
+*/
+
+/*!
+  \namespace OKlib::LocalSearch
+  \Components for supporting local search
+*/
+
+namespace OKlib {
+  namespace LocalSearch {
+  }
+}
+
