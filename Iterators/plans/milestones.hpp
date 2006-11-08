19c1a1264a45d3e1400050721dbc93d079561874
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:24:36 +0000

    Initial version 0.0.7.

---
+++ Iterators/plans/milestones.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 18.6.2006 (Swansea)
+
+/*!
+  \file Iterators/plans/milestones.hpp
+
+  \par Current version 0.0.7 (17.6.2006)
+
+  \par Version 0.0.6 :
+  In Iterators/plans/Iterators.hpp the item
+  "Update" has been handled, and new milestones created.
+
+  \par Version 0.1 :
+  In Iterators/plans/Iterators.hpp the item
+  "New test system" has been handled.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.7 : 7.11.2006; initial version number (some implementations).
+
+*/
