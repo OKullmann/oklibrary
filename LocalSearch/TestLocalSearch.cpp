e3fa5512dc94625ba87229cb16e2935a5eb948a7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:39:12 +0000

    Empty (old) tests removed.

--- LocalSearch/TestLocalSearch.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <Transitional/TestSystem/TestBaseClass.hpp>
-
-#include <Transitional/LocalSearch/SupportTotalAssignments_Testobjects.hpp>
-
-int main() {
-  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
-} 
