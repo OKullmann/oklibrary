6f6bdfbe7ebc759bec98a5b3c43fc96dddcdaf85
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:18:32 +0000

    First plans for sub-module NumberTheory.

---
+++ General/plans/NumberTheory.hpp
@@ -0,0 +1,10 @@
+// Oliver Kullmann, 8.11.2006 (Swansea)
+
+/*!
+  \file General/plans/NumberTheory.hpp
+  \brief Plans for the (old) module on number-theoretical functions
+
+  \todo The new module (where the current components should be transferred to)
+  is NumberTheory.
+*/
+
