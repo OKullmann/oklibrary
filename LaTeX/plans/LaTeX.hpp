1202537af4866036ab724ca2fbf72e25aa127637
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:37:30 +0000

    Initial plans (unclear future).

---
+++ LaTeX/plans/LaTeX.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 12.1.2006 (Swansea)
+
+/*!
+  \file LaTeX/plans/LaTeX.hpp
+  \brief Plans for the LaTeX-Module
+
+  \todo Currently the module contains mainly code for producing
+  the timetable for the department. This has to be factored out into
+  some external module, so that only the latex-relateted components
+  remain. (If this is useful at all.)
+
+  \todo We should provide latex-output of clause-sets and
+  SAT-decision trees.
+
+  \todo The file Data/Timetable_SQL is no longer needed?
+*/
+
+/*!
+  \namespace OKlib::LaTeX
+  \brief Tools for LaTeX-Output
+*/
+
+namespace OKlib {
+  namespace LaTeX {
+  }
+}
+
