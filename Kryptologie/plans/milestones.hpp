eed62eefb10c80199a883ac9cdbbf149e73322d4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:28:09 +0000

    Initial version 0.0.8.

---
+++ Kryptologie/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 18.6.2006 (Swansea)
+
+/*!
+  \file Kryptologie/plans/milestones.hpp
+
+  \par Current version 0.0.7 (7.11.2006)
+
+  \par Version 0.0.8 :
+  In Kryptologie/plans/Kryptologie.hpp the topic
+  "Update" has been handled, and further milestones created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.7 : 7.11.2006; initial version number (some imported software).
+
+*/
