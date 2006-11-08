ed37d51112aefb5289aeb9be6f8b499083cb9b0e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:44:00 +0000

    Initial version 0.1.

---
+++ MetaProgramming/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 18.6.2006 (Swansea)
+
+/*!
+  \file MetaProgramming/plans/milestones.hpp
+
+  \par Current version 0.1 (8.11.2006)
+
+  \par Version 0.1.1 :
+  In MetaProgramming/plans/MetaProgramming.hpp the item
+  "Update" has been handled, and new milestones are created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.1 : 8.11.2006; initial version number (several (tested) implementations).
+
+*/
