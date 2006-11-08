6896567666df2dfb4dcc5eb28f41002f4249d213
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-08 13:27:50 +0000

    Some plans.

---
+++ Kryptologie/plans/Kryptologie.hpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 7.11.2006 (Swansea)
+
+/*!
+  \file Kryptologie/plans/Kryptologie.hpp
+  \brief Plans for the temporary module on cryptological tools
+
+  \todo Update
+  First an overview on the content of the module, and what to do with is is needed.
+*/
+
+
