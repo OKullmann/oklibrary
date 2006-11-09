3bf47dc4ed0882cb2c79a46da3c3aae0b2265cea
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-09 18:31:50 +0000

    Update (somewhat expanding and mainly reorganising the todo's).

--- AutarkySearch/plans/AnalyseTotalAssignment.hpp
+++ AutarkySearch/plans/AnalyseTotalAssignment.hpp
@@ -9,13 +9,30 @@
   \todo The basic algorithm is: Given a partial assignment phi, unassign all
   variables in critical clauses, and repeat this process until no critical clauses
   are left --- the maximal autarky contained in phi is obtained. Design and
-  implement, using the data structures from SupportTotalAssignment.
+  implement, using the data structures from LocalSearch/plans/SupportTotalAssignments.hpp.
 
   \todo Combination with enumeration of all total assignments yields
   autarky search in time O(2^n).
 
-  \todo Combination with local search yields local search for autarkies (using
-  total assignments). As the value of a total assignments the minimal number
+  \todo Local installation of UBCSAT.
+
+  \todo Using UBCSAT
+  Analysing total assignments (and autarky reduction,
+  if successful) is added to UBCSAT, without further changes.
+
+  For that, include and use all triggers and their data structures needed from UBCSAT
+  library in AutarkySearch.
+   - These include: 
+     ReadCNF, LitOccurence, CandidateList, DefaultProcedures and Flip+FalseClauseList.
+
+  QUESTION (OK): Does this mean that we have already a first complete implementation?
+  If yes, then the current milestone 0.0.6 should become 0.1.
+
+  \todo Changing UBCSAT
+  Change the heuristics in UBCSAT, where now the goal is not to find a satisfying (total) assignment,
+  but a total assignment containing a non-trivial autarky.
+
+  As the value of a total assignments the minimal number
   of critical clauses over all (non-empty) levels could be used. (If a low number
   of variables is involved, then a low number of critical clauses is to be expected;
   so perhaps better to use the ratio (number of variables in level) / (number of
@@ -30,7 +47,16 @@
   (eval(phi_0), ..., eval(phi_{m-1}))
   of non-negative real numbers, and one can use measures of this list like
   the min for obtaining an evaluation of phi (the smaller the better).
-  So basically the problem of evaluating phi is delegates to the evaluation of
+  So basically the problem of evaluating phi is delegated to the evaluation of
   partial assignments; see EvaluatePartialAssignments.
+
+
+  \todo Integrating/assimilating UBCSAT
+  After we got an autarky searcher running, and also played around with the heuristics (mainly based
+  on UBCSAT components), we need to think about more appropriate data structures, this
+  all integrated with the OKlibrary, but perhaps using the UBCSAT framework and components.
+  Finally we need to think about to do everything within OKlibrary (using the generic
+  components, and thus enjoying greater generality and re-usability).
+
 */
 
