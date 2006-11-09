90cef9963c06fb44a3e4067d0285f064f105c31a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-09 18:34:24 +0000

    Update (amongst others, milestone 0.0.9 was renamed to 0.0.6, since 0.0.9 is not closer to 0.1 than for example 0.0.545, and it seems best to me to have consecutive version numbers).

--- AutarkySearch/plans/milestones.hpp
+++ AutarkySearch/plans/milestones.hpp
@@ -6,20 +6,17 @@
   \par Current version 0.0.3 (10.9.2006)
 
   \par Version 0.0.4 :
-  In AutarkySearch/plans/AutarkySearch.hpp the topic
+  In AutarkySearch/plans/AnalyseTotalAssignment.hpp the topic
   "First implementation for AnalyseTotalAssignment" is further expanded,
-  and (further) milestones are updated and created.
+  and (further) milestones are updated and created: DONE.
 
   \par Version 0.0.5 :
-  In AutarkySearch/plans/AutarkySearch.hpp the topic
+  In AutarkySearch/plans/AnalyseTotalAssignment.hpp the topic
   "Local installation of UBCSAT" is handled.
-  QUESTION (OK): Shouldn't "First implementation for AnalyseTotalAssignment"
-  come later than "Local installation of UBCSAT" ?
-  ANSWER (ML) 0.0.4 just means to expand plans, 0.0.5 is to install UBCSAT and 
-  then 0.1 is the actual first implementation.
 
-  \par version 0.0.9
-  We have successfully used all the UBCSAT triggers and their data structures .
+  \par version 0.0.6
+  In AutarkySearch/plans/AnalyseTotalAssignment.hpp the topic
+  "Using UBCSAT" is handled.
 
   \par Version 0.1 :
   We have a first implementation using the method of AnalyseTotalAssignment,
