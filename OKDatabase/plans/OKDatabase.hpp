6b8af666c2c0bb70105bb6918b6c0417641d4f8b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:34:00 +0000

    First plans for the transition.

---
+++ OKDatabase/plans/OKDatabase.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 9.11.2006 (Swansea)
+
+/*!
+  \file OKDatabase/plans/OKDatabase.hpp
+  \brief Plans for the (old) module concerning the random-formulas-database
+
+  \todo Documentation
+  Add minimal doxygen-documentation to the C++ files.
+
+  \todo make
+  Shall we use the local postgresql-installation for compilation?
+  Perhaps as the default.
+
+  \todo Other files
+  How to treat the .dtd and .sql files?
+
+  \todo OKgenerator
+  Connect to the (old) module OKsolver/Generator.
+
+*/
+
+
