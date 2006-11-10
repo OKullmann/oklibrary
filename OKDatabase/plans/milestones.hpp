c9794976ea764bd434e1bb7707351d30b91c5ced
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:34:17 +0000

    Initial version 0.3.

---
+++ OKDatabase/plans/milestones.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 9.11.2006 (Swansea)
+
+/*!
+  \file OKDatabase/plans/milestones.hpp
+
+  \par Current version 0.3 (9.11.2006)
+
+  \par Version 0.3.1 :
+  In OKDatabase/plans/OKDatabase.hpp the following topics are handled:
+  - Documentation
+  
+  \par Version 0.3.2 :
+  In OKDatabase/plans/OKDatabase.hpp the following topics are handled:
+   - make.
+  And further milestones are created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.3 : 9.11.2006; initial version number (a running system).
+
+*/
+
