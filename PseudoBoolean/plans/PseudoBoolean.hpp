e50fd43ad8cdab26aedffbfdb30beca8f5ab8870
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:39:34 +0000

    Very first plans.

---
+++ PseudoBoolean/plans/PseudoBoolean.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 17.12.2005 (Swansea)
+
+/*!
+  \file PseudoBoolean/plans/PseudoBoolean.hpp
+  \brief Plans for the module PseudoBoolean
+
+  \todo First the scope (the (informal) concept) has to be established.
+*/
+
+/*!
+  \namespace OKlib::PseudoBoolean
+  \brief Components for active clause-sets for pseudo-boolean constraints
+*/
+
+namespace OKlib {
+  namespace PseudoBoolean {
+  }
+}
+
