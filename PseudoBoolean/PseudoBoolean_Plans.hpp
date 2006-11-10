392c730edaa9e502d0184a05327010e005588d8d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:28:22 +0000

    Moved to plans.

--- PseudoBoolean/PseudoBoolean_Plans.hpp
+++
@@ -1,8 +0,0 @@
-// Oliver Kullmann, 17.12.2005 (Swansea)
-
-/*!
-  \file PseudoBoolean_Plans.hpp
-  \brief Plans for the module PseudoBoolean
-  \todo First the scope (the (informal) concept) has to be established.
-*/
-
