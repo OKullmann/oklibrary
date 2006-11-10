b060a0ddf639a8555a81364d2e0e70038d40d002
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:41:15 +0000

    Initial version 0.0.3.

---
+++ RandomGenerator/plans/milestones.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 9.11.2006 (Swansea)
+
+/*!
+  \file RandomGenerator/plans/milestones.hpp
+
+  \par Current version 0.0.3 (7.11.2006)
+
+  \par Version 0.0.4 :
+  In RandomGenerator/plans/RandomGenerator.hpp the topic
+  - Schedule
+  is expanded, and further milestones are created.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.3 : 9.11.2006; initial version number (first plans).
+
+*/
+
