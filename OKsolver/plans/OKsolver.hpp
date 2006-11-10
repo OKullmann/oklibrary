06e9c05991352c4895d2fc261c3d851f2e9373d5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-10 13:35:12 +0000

    First plans for the transfer.

---
+++ OKsolver/plans/OKsolver.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 10.11.2006 (Swansea)
+
+/*!
+  \file OKsolver/plans/OKsolver.hpp
+  \brief Plans for the module containing old OKsolver-etc-code
+
+  \todo Submodule "Datenbank" has been transferred to module OKdatabase,
+  and should sooner or later be removed.
+
+  \todo Submodule "Experimental":
+   - "AllgKlassen" : All content should be transferred
+     (that is, what is still relevant).
+   - "Graphen" : It seems there is nothing of real interest in here anymore.
+   - "Transformationen" : All content should be transferred
+     (that is, what is still relevant).
+   After completed transfer, module should be removed.
+
+   \todo Submodule "Generator" must be transferred to module RandomGenerator.
+
+   \todo Submodule "Hilfsprogramme" should be inspected, what the compilation-tool there
+   is doing, so that interesting functions can be incorporated into the build system.
+
+   \todo Submodule "Quellen" should be transferred to module OKsolver_1_0.
+
+*/
