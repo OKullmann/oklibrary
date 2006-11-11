3663fef84788a06d80dd3b103c0e4d5016d5c26d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-11 19:48:16 +0000

    Initial version 0.0.7.

---
+++ OrderRelations/plans/milestones.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 11.11.2006 (Swansea)
+
+/*!
+  \file OrderRelations/plans/milestones.hpp
+
+  \par Current version 0.0.7 (11.11.2006)
+
+  \par Version 0.0.8 :
+
+  In OrderRelations/plans/OrderRelations.hpp the following topics are handled:
+  - Update
+
+  And then update the milestones.
+
+  \par Version 0.1 :
+
+  IIn OrderRelations/plans/OrderRelations.hpp the following topics are handled:
+  - Tests.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.7 : 11.11.2006; initial version number (some implementations).
+
+*/
+
