051cf268ccd3dedbe7293591461fc1b958d1bec4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-13 13:19:41 +0000

    Initial version 0.0.2.

---
+++ Cryptanalysis/plans/milestones.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 12.11.2006 (Swansea)
+
+/*!
+  \file Cryptanalysis/plans/milestones.hpp
+
+  \par Current version 0.0.2 (12.11.2006)
+
+  \par Version 0.1 :
+  In Cryptanalysis/plans/Cryptanalysis.hpp the topics
+  - Implementing
+  - First test AES
+  have been handled, and tested generators are available.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.2 : 17.6.2006; initial version number (only some plans).
+
+*/
