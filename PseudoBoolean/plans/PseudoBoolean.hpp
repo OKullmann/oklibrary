31d961a70141bc51f2ceeeafcd27e4fe6ea31544
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-13 13:25:35 +0000

    Update.

--- PseudoBoolean/plans/PseudoBoolean.hpp
+++ PseudoBoolean/plans/PseudoBoolean.hpp
@@ -4,7 +4,8 @@
   \file PseudoBoolean/plans/PseudoBoolean.hpp
   \brief Plans for the module PseudoBoolean
 
-  \todo First the scope (the (informal) concept) has to be established.
+  \todo First the scope (the (informal) concept) has to be established,
+  and the relation to module LinearInequalities.
 */
 
 /*!
