c5cb382729748055c68f367030545565a0d23fe5
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2006-11-15 17:37:13 +0000

    Mostly small formatting changes. Some updates and expansion of milestones.

--- Buildsystem/plans/milestones.hpp
+++ Buildsystem/plans/milestones.hpp
@@ -6,124 +6,130 @@
   \par Current version 0.1.1 (3.11.2006)
   
   \par Version 0.1.2 :
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - (DONE) Documentation index page creation
-  - (DONE) html target
-  - Update makefile_recursive
-  - Document srcdir
-  - Linking to makefile_recursive
-  - Doxygen general
-  - Doxygen menu
-  - Makefiles in general
-  - Documentation (Examples and Concepts)
-  - System documentation
+   In Buildsystem/plans/Buildsystem.hpp the following topics are handled:
+   - Update makefile_recursive
+   - Document srcdir
+   - Linking to makefile_recursive
+   - Doxygen general
+   - Makefiles in general
+   - Documentation (Examples and Concepts)
+   - System documentation
   
   \par
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - (DONE) Extracting documentation building to seperate makefile
+   In Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
+   the following topics are handled:
+   - (DONE) Documentation index page creation
+   - (DONE) html target
+   - Doxygen menu
+   - Doxyfile
+   - Parameters
+   - Updating
+   - Meta-documentation
+   - ERROR REPORTING
 
   \par
-  In Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation.hpp the following topics are handled:
-  - Update todos
-  - Documentation
-
-  \par
-  In Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp the following topics are handled.
-  - ERROR REPORTING
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - (DONE) Extracting documentation building to seperate makefile
   
   \par Version 0.1.3 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Make-variables for compilation and linking
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Make-variables for compilation and linking
   
   \par
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - Overhaul of the general targets
+   In Buildsystem/plans/Buildsystem.hpp the following
+   topics are handled:
+   - Overhaul of the general targets
   
   \par Version 0.2 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Cleaning
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Cleaning
   
   \par
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - Testing the build system
+   In Buildsystem/plans/Buildsystem.hpp the following
+   topics are handled:
+   - Testing the build system
   
   \par Version 0.2.1 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Test cleaning
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Test cleaning
   
   \par Version 0.2.2 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Compilation
-  - Linking
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Compilation
+   - Linking
   
   \par Version 0.3 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Customisation
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Customisation
   
   \par Version 0.4 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Test system
-  - Targets
-  - Tools for testing
-  - Error messages of gcc should be processed
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Test system
+   - Targets
+   - Tools for testing
+   - Error messages of gcc should be processed
   
   \par Version 0.5 :
-  In Buildsystem/plans/makefile_generic.hpp the following
-  topics are handled:
-  - Placement of makefile_generic
-  - Dependency files
-  - Verbosity
+   In Buildsystem/plans/makefile_generic.hpp the following
+   topics are handled:
+   - Placement of makefile_generic
+   - Dependency files
+   - Verbosity
   
   \par Version 0.6 :
-  In Buildsystem/plans/makefile_ExternalSources.hpp the following
-  topics are handled:
-  - Gcc
-  - Boost
+   In Buildsystem/plans/makefile_ExternalSources.hpp the following
+   topics are handled:
+   - Gcc
+   - Boost
   
   \par Version 0.7 :
-  In Buildsystem/plans/makefile_ExternalSources.hpp the following
-  topics are handled:
-  - General
-  - Tools
-  - PostgreSQL
-  - Other sources
-  In Buildsystem/plans/README.hpp the following
-  topics are handled:
-  - Supported versions of gcc
+   In Buildsystem/plans/makefile_ExternalSources.hpp the following
+   topics are handled:
+   - General
+   - Tools
+   - PostgreSQL
+   - Other sources
+   In Buildsystem/plans/README.hpp the following topics are handled:
+   - Supported versions of gcc
 
   \par Version 0.8 :
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - Makefiles in general
+   In Buildsystem/plans/Buildsystem.hpp the following
+   topics are handled:
+   - Makefiles in general
 
   \par Version 0.9 :
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - Documentation in general
+   In Buildsystem/plans/Buildsystem.hpp the following
+   topics are handled:
+   - Documentation in general
 
   \par Version 1.0 :
-  In Buildsystem/plans/Buildsystem.hpp the following
-  topics are handled:
-  - Special runs
-  - "Full test"
-  - Directory structure
-  - System documentation
-  - Documentation index
-  - OKsystem/Buildsystem structure
+   In Buildsystem/plans/Buildsystem.hpp the following
+   topics are handled:
+   - Compilation information
+   - Special runs
+   - "Full test"
+   - Package construction script
+   - Nightly build
+   - Source code directory structure
+   - OKsystem/Buildsystem structure
 
   \par
-  In Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp the following topics are handled.
-  - CSS
+   In Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp the following topics are handled:
+   - Logo
+   - OKlibrary link
+   - Version numbers
+
+  \par
+   In Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp the following topics are handled:
+   - System documentation
+   - CSS
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
