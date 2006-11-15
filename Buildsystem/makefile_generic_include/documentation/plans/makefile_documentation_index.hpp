44eb7fcfaf581c59abbeb5402fb6d0aa4efc70ab
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2006-11-15 13:43:07 +0000

    Plans for documentation transferred here from Buildsystem/plans.

---
+++ Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp
@@ -0,0 +1,25 @@
+// Matthew Henderson, 14.11.2006 (Swansea)
+
+/*!
+  \file Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp
+  \brief Plans for the documentation index makefile
+
+  \todo CSS : Improving the cascading style sheet used by the documentation
+   index.
+
+  \todo ERROR REPORTING : regarding system_directories/doc/index.html
+
+   - Local documentation:
+
+     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm does not exist.
+
+     Doxygen appears with the wrong version number
+
+     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1+4.1.1/man/man3/mhash.3:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1 4.1.1/man/man3/mhash.3 does not exist.
+
+     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html does not exist.
+ 
+*/
