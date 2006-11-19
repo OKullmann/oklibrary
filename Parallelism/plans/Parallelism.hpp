765a195d64c8417d54e6489cc87c12b3a2c725ee
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:44:28 +0000

    Few initial plans.

---
+++ Parallelism/plans/Parallelism.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file Parallelism/plans/Parallelism.hpp
+  \brief Plans for the module on parallelism
+
+  \todo Investigate the possibilities provided by Boost-1.34.
+
+  \todo Decide, how to handle parallel computations for the OKlibrary.
+
+  \todo Transfer the tests to the new test system.
+
+*/
+
+/*!
+  \namespace OKlib::Parallelism
+  \brief Components for parallelising algorithms
+*/
+
+namespace OKlib {
+  namespace Parallelism {
+  }
+}
+
