74cc5da9b3e44a780c8023b8dd32c9d24e777769
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 19:01:16 +0000

    Initial version 0.0.5.

---
+++ Variables/plans/milestones.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file Variables/plans/milestones.hpp
+
+  \par Current version 0.0.5 (19.11.2006)
+
+  \par Version 0.0.6 :
+
+  In Variables/plans/Variables.hpp the following items are handled:
+   - Update
+   - Planning
+  And new milestones are created.
+
+  
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.5 : 19.11.2006, initial version number (some simple components, and plans (also elsewhere)).
+
+*/
+
