81f2ec0568a95e5e38a8c2531d38b2b2edae3d27
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:59:12 +0000

    Initial version 0.2.

---
+++ TestExecutionTools/plans/milestones.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file TestExecutionTools/plans/milestones.hpp
+
+  \par Current version 0.2 (19.11.2006)
+
+  \par Version 0.2.1 :
+  In TestExecutionTools/plans/TestExecutionTools.hpp the following items are handled:
+   - Update
+  And new milestones are created.
+ 
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.2 : 19.11.2006, initial version number (several components and plans).
+
+*/
+
