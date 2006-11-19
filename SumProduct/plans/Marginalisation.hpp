7e342407039c1dec802128823168a698d970df4d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:58:00 +0000

    First ideas.

---
+++ SumProduct/plans/Marginalisation.hpp
@@ -0,0 +1,7 @@
+// Oliver Kullmann, 10.8.2005 (Swansea)
+
+/*!
+  \file SumProduct/plans/Marginalisation.hpp
+  \brief Generic marginalisation (brute-force)
+*/
+
---
+++ SumProduct/plans/SumProduct.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file SumProduct/plans/SumProduct.hpp
+  \brief Plans for the module on SumProduct components
+
+  \todo Create plans and milestones.
+
+  \todo The first submodule should be for generic marginalisation.
+  (See the MSc dissertation of ???.)
+
+*/
+
+/*!
+  \namespace OKlib::SumProduct
+  \brief Components for sum-product (or belief-propagation) algorithms
+*/
+
+namespace OKlib {
+  namespace SumProduct {
+  }
+}
+
