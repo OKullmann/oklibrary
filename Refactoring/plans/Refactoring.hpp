6384a6af6fb7f0196c46f38d3e218b07802ae23d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:51:10 +0000

    Initial plans (module was dormant for quite some time).

---
+++ Refactoring/plans/Refactoring.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file Refactoring/plans/Refactoring.hpp
+  \brief Plans for the module on refactoring
+
+  \todo Update the doxygen-documentation, and create further plans(-files).
+
+  \todo Transfer the tests to the new test system.
+
+  \todo What about Refactoring/plans/Transition_Plans.hpp ?
+
+  \todo Investigating existing tools for parsing and refactoring C++ code.
+  Asking on the boost e-mail list.
+
+*/
+
+/*!
+  \namespace OKlib::Refactoring
+  \brief Tools for refactoring C++ code.
+*/
+
+namespace OKlib {
+  namespace Refactoring {
+  }
+}
+
