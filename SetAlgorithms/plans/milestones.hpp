2c847c7c83c07fc3cd7ccabbd5fc71e48d245711
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:56:47 +0000

    Initial version 0.3.

---
+++ SetAlgorithms/plans/milestones.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file SetAlgorithms/plans/milestones.hpp
+
+  \par Current version 0.3 (19.11.2006)
+
+  \par Version 0.3.1 :
+  In SetAlgorithms/plans/SetAlgorithms.hpp the following items are handled:
+   - Update
+  And create new milestones.
+
+  \par Version 0.4 :
+  In SetAlgorithms/plans/SetAlgorithms.hpp the following items are handled:
+   - New test system
+  And there are tests for all components. 
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.3 : 19.11.2006; initial version number (several components).
+
+*/
