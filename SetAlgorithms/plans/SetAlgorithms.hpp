9d24155fa79bcb78bfbbf6fd8df0b769781e47eb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:56:33 +0000

    First actions.

---
+++ SetAlgorithms/plans/SetAlgorithms.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file SetAlgorithms/plans/SetAlgorithms.hpp
+  \brief Plans for the module on sets-related algorithms and datastructures
+
+  \todo Update
+   - doxygen-documentation
+   - new plans, transfer todos
+
+   \todo New test system
+
+*/
+
+/*!
+  \namespace OKlib::SetAlgorithms
+  \brief Components for sets-related algorithms and datastructures
+*/
+
+
+namespace OKlib {
+  namespace SetAlgorithms {
+  }
+}
+
---
+++ TestExecutionTools/plans/TestExecutionTools.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file TestExecutionTools/plans/TestExecutionTools.hpp
+  \brief Plans for the test tools
+
+  \todo Update
+   - doxygen documentation
+   - new plans files, transferring todos
+
+  \todo New test system
+
+*/
+
+/*!
+  \namespace OKlib::TestExecutionTools
+  \brief Generic components to help with testing
+*/
+
+
+namespace OKlib {
+  namespace TestExecutionTools {
+  }
+}
+
