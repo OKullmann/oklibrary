c2bed336c7e3a23e288ce10b0b58b775df386452
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:38:55 +0000

    Empty tests.

--- SumProduct/TestSumProduct.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <Transitional/TestSystem/TestBaseClass.hpp>
-
-#include <Transitional/SumProduct/Marginalisation_Testobjects.hpp>
-
-int main() {
-  return OKlib::TestSystem::TestBase::run_tests_default(std::cerr);
-} 
