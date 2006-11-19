14a423dca858c0ae7e67b2a4982e01e34b7497b1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:52:41 +0000

    First actions proposed.

--- SATCompetition/plans/SATCompetition.hpp
+++ SATCompetition/plans/SATCompetition.hpp
@@ -3,6 +3,17 @@
 /*!
   \file SATCompetition/plans/SATCompetition.hpp
   \brief Plans for the module on the evaluation of the SAT-2005 competition
+
+  \todo Update
+   - doxygen documentation for all files
+   - new plans files, transferring todos
+
+   \todo New tests
+   Transfer to the new test system
+
+   \todo Messages
+   Use Messages.
+
 */
 
 /*!
