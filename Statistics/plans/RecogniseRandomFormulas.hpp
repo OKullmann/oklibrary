862976497a4ab5ddb7aa11205598fd4d4f550e0b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:57:30 +0000

    First plans and ideas.

---
+++ Statistics/plans/RecogniseRandomFormulas.hpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 10.11.2005 (Swansea)
+
+/*!
+  \file Statistics/plans/RecogniseRandomFormulas.hpp
+  \brief Tools for the regocnition of random formulas
+
+  \todo First tool: Given a clause-sequence, for each clause-size k
+  consider the sub-sequence of clauses of size k and test whether
+  this sequence is "random" in the standard sense.
+*/
+
---
+++ Statistics/plans/Statistics.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file Statistics/plans/Statistics.hpp
+  \brief Plans for the module with statistical tools
+
+  \todo Create milestones.
+
+*/
+
+/*!
+  \namespace OKlib::Statistics
+  \brief Components providing statistical tools
+*/
+
+namespace OKlib {
+  namespace Statistics {
+  }
+}
+
