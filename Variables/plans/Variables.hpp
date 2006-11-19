2b73cf7e64371a42e59cee6b7490a8d9caf2d71f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 19:01:00 +0000

    Very first plans.

---
+++ Variables/plans/Variables.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 19.11.2006 (Swansea)
+
+/*!
+  \file Variables/plans/Variables.hpp
+  \brief Plans for the variables module
+
+  \todo Update
+   - doxygen-documentation
+   - creating plans-files, transferring todos
+
+  \todo Planning
+  We have to create plans (also gathering was has been done, in the concepts module
+  and elsewhere).
+
+  \todo New test system
+
+*/
+
+/*!
+  \namespace OKlib::Variables
+  \brief Components for variables
+*/
+
+namespace OKlib {
+  namespace Variables {
+  }
+}
+
