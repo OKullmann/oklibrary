18dcf0416a7bd7f4ccb86ee21830edea0f2dd9b6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:46:06 +0000

    Some first plans.

---
+++ Parsing/plans/Parsing.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file Parsing/plans/Parsing.hpp
+  \brief Plans for the parsing modules
+
+  \todo Either change the name of the module to "Parser", or change the namespace to "Parsing".
+
+  \todo Update the doxygen-documentation for this module.
+
+  \todo Decide what to do with this module, and with parsing in general (is Boost::Spirit
+  to be used? What about regular expressions in the extended Standard library? Are there
+  new facilities in Boost 1.34 ?)
+
+  \todo Transfer the tests to the new system (where are the current test functions used?)
+
+*/
+
+/*!
+  \namespace OKlib::Parser
+  \brief Components to support parsing via Boost::Spirit.
+*/
+
+namespace OKlib {
+  namespace Parser {
+  }
+}
+
