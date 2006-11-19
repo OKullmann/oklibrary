d8076a855930aefe34e0d3bd950eb4348a138b33
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-19 18:51:26 +0000

    Initial version 0.0.7.

---
+++ Refactoring/plans/milestones.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file Refactoring/plans/milestones.hpp
+
+  \par Current version 0.0.7 (18.11.2006)
+
+  \par Version 0.0.8 :
+  The first task is to make plans and create milestones.
+
+  \par Version 0.1 :
+  We have a working system to automatically update the include-directives when
+  changing the directory structure.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.7 : 18.11.2006; initial version number (several components; module in unclear state).
+
+*/
---
+++ SearchDataStructures/plans/milestones.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 18.11.2006 (Swansea)
+
+/*!
+  \file SearchDataStructures/plans/milestones.hpp
+
+  \par Current version 0.0.7 (18.11.2006)
+
+  \par Version 0.0.8 :
+  In SearchDataStructures/plans/SearchDataStructures.hpp the following topics are handled:
+   - Update
+
+  \par Version 0.1 :
+  The module is updated and ready to use, and clear milestones for the further development exist.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.7 : 18.11.2006; initial version number (some components, some plans).
+
+*/
