b8870bdccd58b4225a8b20008dc7d51465fcfd22
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-22 17:16:41 +0000

    Update (no tests anymore).

--- InjectivityConstraints/testobjects/AllDifferent.cpp
+++ InjectivityConstraints/testobjects/AllDifferent.cpp
@@ -5,17 +5,9 @@
   \brief Objects for testing of algorithms for alldifferent constraint.
 */
 
-#include <Transitional/TestSystem/RunTest_DesignStudy.hpp>
 #include <Transitional/InjectivityConstraints/tests/AllDifferent.hpp>
 
-namespace OKlib {
-  namespace InjectivityConstraints {
-    namespace testobjects {
+namespace {
 
-      ::OKlib::TestSystem::RunTest test_all_different_adhoc(new ::OKlib::InjectivityConstraints::tests::TestAllDifferentAdhoc);
-      ::OKlib::TestSystem::RunTest test_all_different_by_set_size_comparison(new ::OKlib::InjectivityConstraints::tests::TestAllDifferent<AllDifferentBySetSizeComparison>);
-      ::OKlib::TestSystem::RunTest test_all_different_by_seen_before_vector_find(new ::OKlib::InjectivityConstraints::tests::TestAllDifferent<AllDifferentBySeenBeforeVectorFind>);
+}
 
-    }
-  }
-} 
