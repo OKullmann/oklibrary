7d89ba6f713251301cc31fbd6e83fb928ed6e5f2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-22 17:25:53 +0000

    First todo about the "Free Will Theorem".

---
+++ QuantumPhysics/plans/QuantumPhysics.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 22.11.2006 (Swansea)
+
+/*!
+  \file QuantumPhysics/plans/QuantumPhysics.hpp
+  \brief Plans regarding certain aspects of quantum physics
+
+  \todo Roll out the problems which are interesting for us
+  regarding the "Free Will Theorem".
+*/
+
+/*!
+  \namespace OKlib::QuantumPhysics
+  \brief Components related to certain aspects of quantum physics
+*/
+
+namespace OKlib {
+  namespace QuantumPhysics {
+  }
+}
+
