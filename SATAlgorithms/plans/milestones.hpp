221ed7bc322aae1e4ad196fa709afb0192348cde
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-23 18:05:28 +0000

    Initial version 0.0.5.

---
+++ SATAlgorithms/plans/milestones.hpp
@@ -0,0 +1,36 @@
+// Oliver Kullmann, 23.11.2006 (Swansea)
+
+/*!
+  \file SATAlgorithms/plans/milestones.hpp
+
+  \par Current version 0.0.5 (23.11.2006)
+
+  \par Version 0.0.6
+  Further milestones, leading to 0.1, have been created.
+
+  \par Version 0.1
+  In SATAlgorithms/plans/GenericBacktracking.hpp the following topics are handled:
+  - OKlib::SATAlgorithms::Backtracking
+
+  \par Version 0.2
+  In SATAlgorithms/plans/GenericBacktracking.hpp the following topics are handled:
+  - Test OKlib::SATAlgorithms::Backtracking
+
+  \par Version 0.3
+  In SATAlgorithms/plans/EnumerationAssignments.hpp the following topics are handled:
+  - Trivial SAT algorithm
+  And the further development of enumeration algorithms is planned.
+
+  \par Version 0.4
+  The enumeration algorithm discussed in SATAlgorithms/plans/EnumerationSubclausesets.hpp
+  has been implemented.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.5 : 23.11.2006; initial version number (several plans, and first prototype).
+
+*/
+
