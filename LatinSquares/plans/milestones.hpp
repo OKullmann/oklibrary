3f706c2be19a844992a41de414bfd4aa14817c00
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-23 18:02:11 +0000

    New version 0.0.3; and expanded milestones.

--- LatinSquares/plans/milestones.hpp
+++ LatinSquares/plans/milestones.hpp
@@ -3,32 +3,38 @@
 /*!
   \file LatinSquares/plans/milestones.hpp
 
-  \par Current version 0.0.2 (12.11.2006)
+  \par Current version 0.0.3 (23.11.2006)
 
-  \par Version 0.0.3
-  Further milestones, leading to 0.1, have been created.
+  \par Version 0.0.4
+  In LatinSquares/plans/Sudoku.hpp the following topics are handled:
+  - Complete implementation of OKlib::LatinSquares::SudokuProblem
+  - Complete implementation of OKlib::LatinSquares::Trivial_reduction_Sudoko
 
   \par Version 0.1
-  In LatinSquares/plans/LatinSquares.hpp the following
-  topics are handled:
+  In LatinSquares/plans/Sudoku.hpp the following topics are handled:
+  - Test OKlib::LatinSquares::SudokuProblem
+  - Test OKlib::LatinSquares::Trivial_reduction_Sudoko
+
+  \par Version 0.2
+  In LatinSquares/plans/LatinSquares.hpp the following topics are handled:
   - Concepts
   - Review
   - Basic direct algorithms
 
-  \par Version 0.2
+  \par Version 0.3
   Basic implementations of LS(v_ij) are provided and tested:
   - the direct incoding as a clause-set (as always, non-boolean)
   - the direct encoding, but managed as a virtual clause-set
 
-  \par Version 0.3
+  \par Version 0.4
   A basic implementation of LS(v_ij) as an alliance of injectivity constraints
   is provided (just using basic components) and tested.
 
-  \par Version 0.4
+  \par Version 0.5
   A basic implementation MOS(v_ijk) as an alliance of injectivity constraints
   is provided and tested.
 
-  \par Version 0.5
+  \par Version 0.6
   A basic implementation MOLS(v_ijk) as an alliance of injectivity constraints
   is provided and tested.
 
@@ -42,6 +48,7 @@
 
    - 0.0.1 : 8.3.2006; initial version number (only plans).
    - 0.0.2 : 12.11.2006; plans extended.
+   - 0.0.3 : 23.11.2006; started prototyping Sudoku components.
 
 */
 
