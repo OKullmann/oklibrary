dc850059aeb1dcfa03989d23fe1784028308dfc1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-23 18:01:24 +0000

    *** empty log message ***

---
+++ LatinSquares/plans/Sudoku.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 23.11.2006 (Swansea)
+
+/*!
+  \file LatinSquares/plans/Sudoku.hpp
+  \brief Plans for the Sudoku components
+
+  \todo As a prototype, implement Sudoku-solvers.
+
+  \todo Complete implementation of OKlib::LatinSquares::SudokuProblem
+  - Are all constraints stored together? Perhaps better we use the natural grouping
+  into 3 groups?
+
+  Compare with SATAlgorithms/plans/GenericBacktracking.hpp and with
+  Concepts/plans/ClauseSets.hpp for the concept discussion.
+
+  \todo Test OKlib::LatinSquares::SudokuProblem
+
+  \todo Complete implementation of OKlib::LatinSquares::Trivial_reduction_Sudoko
+  Perhaps this should be just a generic algorithm, applicable to any collection of
+  constraints (active clause-sets)?!
+
+  \todo Test OKlib::LatinSquares::Trivial_reduction_Sudoko
+
+*/
+
