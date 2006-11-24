ae813251ccaf2a54c6656ca9f6d61d019e2634fb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-24 23:09:00 +0000

    New version 0.0.3.

--- HypergraphColouring/plans/milestones.hpp
+++ HypergraphColouring/plans/milestones.hpp
@@ -3,11 +3,16 @@
 /*!
   \file HypergraphColouring/plans/milestones.hpp
 
-  \par Current version 0.0.2 (31.10.2006)
+  \par Current version 0.0.3 (24.11.2006)
 
-  \par Version 0.0.3 :
+  \par Version 0.0.4 :
+  In HypergraphColouring/plans/GreedyColouring.hpp the topic
+  - GreedyColouring.cpp
+  has been handled.
+
+  \par Version 0.0.5 :
   In HypergraphColouring/plans/HypergraphColouring.hpp the topic
-  "Translation" is further expanded, and further milestones are created.
+  "Translation" is further expanded, and related milestones are created.
 
   \par Version 0.1 :
   A translator is available, so that, together with the hypergraph constructions
@@ -18,7 +23,8 @@
 
   \par Version history
 
-   -  0.0.2 : 31.10.2006; initial version number (some plans).
+   - 0.0.2 : 31.10.2006; initial version number (some plans).
+   - 0.0.3 : 24.11.2006; simple application program exploring greedy colouring.
 
 */
 
