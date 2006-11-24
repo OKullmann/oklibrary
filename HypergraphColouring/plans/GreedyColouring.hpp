98201f9cd06b8b73150c72e15bc3d4691f6abfd4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-24 23:08:21 +0000

    Plans regarding greedy colouring.

---
+++ HypergraphColouring/plans/GreedyColouring.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 24.11.2006 (Swansea)
+
+/*!
+  \file HypergraphColouring/plans/GreedyColouring.hpp
+  \brief Plans regarding the greedy graph colouring algorithm
+
+  \todo GreedyColouring.cpp:
+   - Output optionally all optimal orderings (or just one).
+   - Use Messages.
+   - Write components, so that the algorithms can be tested.
+   - Doxygen documentation.
+
+  \todo Search for literature on heuristics for vertex orderings; from
+  CS-232 we have the traversing greedy colouring; it seems sensible to
+  combine this with a largest degree first-stratey.
+
+  \todo How do these algorithms translate into SAT algorithms?
+
+*/
+
