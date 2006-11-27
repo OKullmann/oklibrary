c380840b3e09767f568c64ee2e837e9a06895cfd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-27 15:06:30 +0000

    Starting with concepts for literals (is now needed); yet empty.

---
+++ Concepts/Literals.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 25.11.2006
+
+/*!
+  \file Concepts/Literals.hpp
+  \brief Concepts for literals
+*/
+
+#ifndef LITERALS_JJhala7t4
+#define LITERALS_JJhala7t4
+
+namespace OKlib {
+
+  namespace Concepts {
+
+  }
+
+}
+
+#endif
