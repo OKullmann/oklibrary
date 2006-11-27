dc4b8665f61900a3836b6df38f8b8acaf56ba4b8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-27 15:23:44 +0000

    First general plans (regarding the Kocken-Specker paradox).

--- QuantumPhysics/plans/QuantumPhysics.hpp
+++ QuantumPhysics/plans/QuantumPhysics.hpp
@@ -4,8 +4,35 @@
   \file QuantumPhysics/plans/QuantumPhysics.hpp
   \brief Plans regarding certain aspects of quantum physics
 
-  \todo Roll out the problems which are interesting for us
-  regarding the "Free Will Theorem".
+  \todo Detailed examination of Peres33PointConfiguration.cnf
+   - w.r.t. the number of variables or number of clauses the smallest
+     minimally unsatisfiable sub-clause-set
+   - also counting all minimally unsatisfiable and maximally satisfiable
+     sub-clause-sets
+   - and the largest satisfiable sub-clause-set (32 clauses)
+   - should be lean
+   - necessary and potentially necessary clauses etc. ({-1,-2,-3} is necessary, and also the
+     associated 2-clauses; removing the whole triple 1,8,9 also renders it sat, and this
+     seems to hold for all triples; it seems that also all binary clauses coming from
+     from the "free pairs" are necessary --- so perhaps all 33 variables are needed)
+   - based on the symmetries of the underlying figure, it should be not too hard
+     to determine the symmetries of the cnf
+   - resolution complexity
+   - is there some special structure?
+
+  \todo Testing realisability
+  Test the conjecture that every graph with a circuit of length 4 is orthogonally
+  realisable; a stronger conjecture is that after fulfilling all forced choices (given
+  by triplets) via an arbitrary traversal (in the sense as developed in CS-232)
+  and making always random choices exactly the realisable graphs are realised
+  with high probability. For this we need to create random graphs without
+  cycles of length 4.
+  Of course, a first test is performed with the hypergraph from the Peres configuration.
+
+  \todo Minimal configuration
+  What is the minimal number of vertices in a realisable graph which
+  is not 101-colourable? Develop plans to attack this problem.
+
 */
 
 /*!
