fde24e1feb6d8e1136fcbc5c93aee31adf172b2f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-27 15:24:06 +0000

    Initial version 0.0.7.

---
+++ QuantumPhysics/plans/milestones.hpp
@@ -0,0 +1,42 @@
+// Oliver Kullmann, 26.11.2006 (Swansea)
+
+/*!
+  \file QuantumPhysics/plans/milestones.hpp
+
+  \par Current version 0.0.7 (26.11.2006)
+
+  \par Version 0.0.8
+  In QuantumPhysics/plans/OrthogonalTriples.hpp the following topics are handled:
+  - Output CNFs
+  - Input and output of graphs
+  - Input and Output.
+
+  \par Version 0.0.9
+  In QuantumPhysics/plans/OrthogonalTriples.hpp the following topics are handled:
+  - Linear algebra
+  - Hypergraphs.
+
+  \par Version 0.0.10
+  In QuantumPhysics/plans/OrthogonalTriples.hpp the following topics are handled:
+  - Orthogonality relation
+  - Extract triangles
+  - Copying graphs.
+
+  \par Version 0.1
+  The application QuantumPhysics/OrthogonalTriples.cpp is fully functional, and
+  all components are outsourced and tested.
+
+  \par Version 0.2
+  In QuantumPhysics/plans/QuantumPhysics.hpp the following topics are handled:
+  - MU
+  - Testing realisability
+  - Minimal configuration.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.7 : 8.3.2006; initial version number (basic implementations, and plans).
+
+*/
