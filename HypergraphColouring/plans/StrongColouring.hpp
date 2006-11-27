c8dfbaecdc24aca232d2a1458cfad834d9853cef
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-27 15:09:52 +0000

    New sub-module about strong colourings for hypergraph (generalising Latin squares, Sudokus, and the like).

---
+++ HypergraphColouring/plans/StrongColouring.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 27.11.2006 (Swansea)
+
+/*!
+  \file HypergraphColouring/plans/StrongColouring.hpp
+  \brief Plans on strong hypergraph colouring (all vertices in any
+  hyperedges get different colours)
+
+  \todo InjectivityConstraints
+  Given a hypergraph G, the most natural way of handling the
+  strong k-colouring problem for G is to use for every hyperedge H
+  an injectivity constraint resp. a bijectivity constraint in case |H| = k.
+  See module InjectivityConstraints.
+  The question is, whether something special can be done here?
+
+  \todo Graph colouring conversion
+  Of course, the strong hypergraph colouring problem for G is the same
+  as the graph colouring problem for G_{[2]} (the 2-section of G).
+  For longer hyperedges the hypergraph treatment should be superior.
+  (What is the boundary for hyperedge sizes? Can for example 3-hyperedges
+  be efficiently translated into 3 2-edges? It seems quite sure that only for
+  a certain minimal hyperedge length the additional effort for strong
+  treatment of injectivity constraint is vindicated.)
+*/
+
