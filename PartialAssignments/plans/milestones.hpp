7eacc1f5ad0d6b57f1a4116517a1fd38e17cb4e0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-29 11:03:03 +0000

    Initial version 0.0.4.

---
+++ PartialAssignments/plans/milestones.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 8.3.2006 (Swansea)
+
+/*!
+  \file PartialAssignments/plans/milestones.hpp
+
+  \par Current version 0.0.4 (28.11.2006)
+
+  \par Version 0.0.5
+  In PartialAssignments/plans/MultivaluedPartialAssignments.hpp the following topics are handled:
+  -  Implementation OKlib::PartialAssignments::MultiPASS
+  And further milestones are created.
+
+    \par Version 0.0.6
+  In PartialAssignments/plans/MultivaluedPartialAssignments.hpp the following topics are handled:
+  -  Test OKlib::PartialAssignments::MultiPASS
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 28.11.2006; initial version number (some plans, one prototype).
+
+*/
+
