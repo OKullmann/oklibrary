34aaa261a4ad2e07ebf88b2b77e98e3d9652005a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-11-29 11:01:14 +0000

    Initial version 0.0.2.

---
+++ Literals/plans/milestones.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 28.11.2006 (Swansea)
+
+/*!
+  \file Literals/plans/milestones.hpp
+
+  \par Current version 0.0.2 (28.11.2006)
+
+  \par Version 0.0.3 :
+  In Literals/plans/Literals.hpp the following topics are handled:
+  - Implement trivial literals
+
+  \par Version 0.0.4 :
+  In Literals/plans/Literals.hpp the following topics are handled:
+  - First prototype
+  And further milestones are created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.2 : 28.11.2006; initial version number (some concepts).
+
+*/
