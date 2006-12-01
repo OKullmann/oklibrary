f80e85326a8dd74c703ffb1457ecaf11e6b667b1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-01 20:47:09 +0000

    Some extensions.

--- Messages/plans/LanguageNames.hpp
+++ Messages/plans/LanguageNames.hpp
@@ -6,8 +6,15 @@
 
   \todo Write tests (in the new test system).
 
-  \todo Once available, the std::map in class Messages::LanguageName should be replaced
-  by a hash map; and finally a specialised string-algorithm shall be used.
+  \todo OKlib::Messages::LanguageName just translates strings into the value
+  given by the strings interpreted as constants --- isn't there some Boost
+  component for that?
+
+  \todo Once available (and having tests in place), the std::map in class
+  Messages::LanguageName should be replaced by a hash map.
+
+  \todo Finally, one should use a specialised string-matching algorithm (for realising the map
+  in Messages::LanguageName).
 
 */
 
