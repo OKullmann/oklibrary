720b7d7d2f8441cf0671cd0cdcad4501c83316f7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-02 17:49:27 +0000

    Update, and todo about doxygen-documentation (which misinterpretes a macro).

--- Messages/plans/Messages.hpp
+++ Messages/plans/Messages.hpp
@@ -12,15 +12,15 @@
      Messages/LanguageNames.hpp : DONE
      Messages/messages/Languages.hpp : DONE
      Messages/Languages.hpp : DONE
-     Messages/Levels.hpp :
-     Messages/messages/Levels.hpp : 
-     Messages/MessagesBase.hpp : 
-     Messages/MessagesMain.hpp : 
-     Messages/Messages_Testapplication.cpp :
-     Messages/Utilities/TimeAndDate.hpp :
-     Messages/Utilities/Umlaute.hpp : 
+     Messages/Levels.hpp : DONE
+     Messages/messages/Levels.hpp : DONE
+     Messages/MessagesBase.hpp : DONE
+     Messages/MessagesMain.hpp : DONE
+     Messages/Messages_Testapplication.cpp : DONE
+     Messages/Utilities/TimeAndDate.hpp : DONE
+     Messages/Utilities/Umlaute.hpp : DONE.
      
-   - Especially it must be precisely documented how to use and write message classes.
+   - It must be precisely documented how to use and write message classes.
 
   \todo Directory structure:
    - To conform with the current recursive build system, we need a new sub-directory
@@ -38,6 +38,11 @@
      problems with these later gcc-versions (regarding compilation and execution).
    - When removing Messages/Messages_Testapplication.cpp, we must make sure that all these tests are
      incorporated.
+
+  \todo Doxygen-documentation of message-classes
+  Using the macro OKLIB_MESSAGES_PRINT, doxygen misunderstands it as belonging to
+  the following member --- how to correct this behaviour of doxygen?
+
 */
 
 /*!
