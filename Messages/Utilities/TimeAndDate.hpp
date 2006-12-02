a2c10659b6c9d3dd6b8b6671ef6e46213ab20460
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-02 17:43:41 +0000

    Usage of macro OKLIB_MESSAGES eliminated, and todo moved to plans.

--- Messages/Utilities/TimeAndDate.hpp
+++ Messages/Utilities/TimeAndDate.hpp
@@ -23,18 +23,9 @@ namespace OKlib {
         For these messages, level and language specifications are ignored: The level is always
         that of the standard C-output, while the language is always given by the current locale.
 
-        \todo On the one hand it would be nice if we could specify different levels (say, the
-        implementation below as "full", one without timezone, seconds and day as "basic", and
-        one with full names for day and month (possibly also with day-number and week-number)
-        as "extensive"), but since this is not provided by the standard, we had to define it
-        all ourselves (that is, the order of the components). So perhaps the implementation
-        below is a reasonable compromise. (But the best thing would be if the format string
-        could be determined, and then one would simply replace the abbreviated forms by
-        their full forms, and potentially add something.)
-
       */
 
-      OKLIB_MESSAGES(TimeDateStandardImplementation) {
+      struct TimeDateStandardImplementation : ::OKlib::Messages::MessagesBase {
 
         std::ostream& print(std::ostream& out) const {
           return ::TimeHandling::currentDateTime("%c", out);
