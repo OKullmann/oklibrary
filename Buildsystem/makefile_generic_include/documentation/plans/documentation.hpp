9e68da13dd8dca6d8122323094649562a2d1b9bd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-03 12:58:18 +0000

    Update and correction:  - completed todos removed  - reorganisation  - moved todos which concerned documentation of other build-system    components to their plans-file (documentation of some component is    always a *local* matter, to be discussed in the corresponding plans;    here we have only *tools* for building documentation).

--- Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
+++ Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
@@ -2,58 +2,9 @@
 
 /*!
   \file Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
-  \brief Plans for documentation in general 
+  \brief Plans for documentation-creation
 
-  \todo (DONE) Documentation index page creation:
-   - In $(system_directories)/doc there is a index to all the OKplatform
-    documentation, including external resources. This index also contains
-    links to the web pages of Boost, Doxygen, Gcc etc
-
-  \todo (DONE) html target:
-   - "html" should be possible from any level (creating always the
-     complete documentation) (this should be automatically achieved
-     once makefile_recursive is eliminated) 
-
-  \todo Parameters:
-   - (DONE) Full overview on the parameters for makefile_generic (including the environment
-     variables).
-
-  \todo Doxyfile:
-   - What is the role of the doxygen configuration tag BUILTIN_STL_SUPPORT?
-     Should we enable this?
-
-  \todo Doxygen menu
-   -  Perhaps we should drop the file-list altogether?
-     (This could make the system more user-friendly, by restricting the attention to
-     the most appropriate choice?) On the other hand, always having to click on the
-     directory items might not be always best, so perhaps we should keep the file-list.
-   - Sometimes it would be good to show also other files (especially for Buildsystem).
-     This is mainly the issue of how to get doxygen showing makefiles etc. (perhaps
-     just as plain text files, without any special formatting).
-
-  \todo System documentation:
-   - Document the basic version control settings (location of server, configuration, how to use it).
-   - Document the build system (general ideas, functionality).
-
-  \todo Updating:
-   - Checking on the existing documentation, and updating if necessary.               
-   - The documentation needs to be rewritten to be aimed more at the user.
-     The documentation of the main targets should remain in the makefile
-     preamble, but the documentation of local variables should be moved
-     to inline comments. One needs to distinguish between targets and variables.
-     The disctinction below about make-variables should be reflected
-     (somehow) in their documentation.
-   - What is required from those makefile.definitions.mak ? Update the list in
-     makefile_generic.mak, and reflect on it.
-   - Adding inline comments for an overview on functionality.
-
-  \todo Meta-documentation:
-   - Meta-documentation about usage of the buildsystem.
-
-  \todo CSS : Improving the cascading style sheet used by the documentation
-   index.
-
-  \todo ERROR REPORTING : regarding system_directories/doc/index.html
+  \todo ERROR REPORT : regarding system_directories/doc/index.html
 
    - Local documentation:
 
@@ -67,5 +18,41 @@ The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSou
 
      An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html:
 The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html does not exist.
+
+  \todo Doxygen general:
+   - Can doxygen tell which other files include a file (not in graph form, but in text form)?
+   - For functions there is a "callergraph" --- shall we use it?
+   - How to integrate a *general* todo list into Doxygen?
+   - How to avoid that a leading "include" in a Doxygen-comment is interpreted as
+     a doxygen-command? And how to avoid that apparently certain "keywords" are captured by
+     doxygen?
+   - How to obtain general statistics: About the number of classes, lines of code, etc. (best
+     with some statistics on the change over time)
+     and then also on the version numbers of the modules (again with changes over time).
+     If doxygen does not support it yet, then we should submit some feature request, and postphone
+     this item.
+   - We should get the newest configuration file, and transfer our information from the old
+     configuration file.
+   - It appears that all .cpp-files are considered as linked together?
+   - Can makefiles be incorporated?!
+   - Can we have nested lists? Numbered lists? These things perhaps should go into
+     our general documentation --- or we have some example files, which demonstrate
+     our use of doxygen.
+
+  \todo Doxygen menu
+   -  Perhaps we should drop the file-list altogether?
+     (This could make the system more user-friendly, by restricting the attention to
+     the most appropriate choice?) On the other hand, always having to click on the
+     directory items might not be always best, so perhaps we should keep the file-list.
+   - Sometimes it would be good to show also other files (especially for Buildsystem).
+     This is mainly the issue of how to get doxygen showing makefiles etc. (perhaps
+     just as plain text files, without any special formatting).
+
+  \todo Doxyfile:
+   - What is the role of the doxygen configuration tag BUILTIN_STL_SUPPORT?
+     Should we enable this?
+
+  \todo CSS : Improving the cascading style sheet used by the documentation
+   index.
 
 */
