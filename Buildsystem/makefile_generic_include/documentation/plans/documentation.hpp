40fa443143918f032d21ed34b6c0f2e7274fa06c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-04 10:42:03 +0000

    Update (especially regarding the use of doxygen).

--- Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
+++ Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
@@ -4,25 +4,21 @@
   \file Buildsystem/makefile_generic_include/documentation/plans/documentation.hpp
   \brief Plans for documentation-creation
 
-  \todo ERROR REPORT : regarding system_directories/doc/index.html
+  \bug Local documentation : regarding system_directories/doc/index.html
 
-   - Local documentation:
-
-     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm:
-The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm does not exist.
-
-     Doxygen appears with the wrong version number
-
-     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1+4.1.1/man/man3/mhash.3:
-The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1 4.1.1/man/man3/mhash.3 does not exist.
-
-     An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html:
-The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html does not exist.
+  <ul>
+    <li> An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Boost/boost_1_33_1/index.htm does not exist. </li>
+   <li> Doxygen appears with the wrong version number </li>
+   <li> An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1+4.1.1/man/man3/mhash.3:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Mhash/0.9.7.1 4.1.1/man/man3/mhash.3 does not exist. </li>
+   <li> An error occurred while loading file:///h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html:
+The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSources/Postgresql/doc/postgresql/html/index.html does not exist.</li>
+  </ul>
 
   \todo Doxygen general:
    - Can doxygen tell which other files include a file (not in graph form, but in text form)?
    - For functions there is a "callergraph" --- shall we use it?
-   - How to integrate a *general* todo list into Doxygen?
    - How to avoid that a leading "include" in a Doxygen-comment is interpreted as
      a doxygen-command? And how to avoid that apparently certain "keywords" are captured by
      doxygen?
@@ -31,13 +27,18 @@ The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSou
      and then also on the version numbers of the modules (again with changes over time).
      If doxygen does not support it yet, then we should submit some feature request, and postphone
      this item.
-   - We should get the newest configuration file, and transfer our information from the old
-     configuration file.
    - It appears that all .cpp-files are considered as linked together?
    - Can makefiles be incorporated?!
-   - Can we have nested lists? Numbered lists? These things perhaps should go into
+   - See the doxygen-related problems in Messages.
+   - What is the role of the doxygen configuration tag BUILTIN_STL_SUPPORT?
+     Should we enable this? : Enabled; it should help --- check.
+   - Can we have nested lists? Numbered lists? These things should go into
      our general documentation --- or we have some example files, which demonstrate
-     our use of doxygen.
+     our use of doxygen :
+     For simple lists we do it as here, otherwise we use the html-elements. Documented in the main documentation-document. DONE
+   - How to integrate a *general* todo list into Doxygen? With xrefitem. However it seems we no longer need it : DONE
+   - Can doxygen extract a list with all modules and their current version numbers? Installed module_version : DONE
+   - We should get the newest configuration file, and transfer our information from the old configuration file : DONE
 
   \todo Doxygen menu
    -  Perhaps we should drop the file-list altogether?
@@ -47,10 +48,6 @@ The file or folder /h/21/GemeinsameBasis/SAT-Algorithmen/OKplatform//ExternalSou
    - Sometimes it would be good to show also other files (especially for Buildsystem).
      This is mainly the issue of how to get doxygen showing makefiles etc. (perhaps
      just as plain text files, without any special formatting).
-
-  \todo Doxyfile:
-   - What is the role of the doxygen configuration tag BUILTIN_STL_SUPPORT?
-     Should we enable this?
 
   \todo CSS : Improving the cascading style sheet used by the documentation
    index.
