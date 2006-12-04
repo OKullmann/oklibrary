c97333ff6dc0a41e8f94c2b692d183749248e649
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-04 10:46:15 +0000

    Completed todo Update removed.

--- Messages/plans/Messages.hpp
+++ Messages/plans/Messages.hpp
@@ -4,25 +4,6 @@
   \file Messages/plans/Messages.hpp
   \brief Plans for the messages module (as a whole)
 
-  \todo Update:
-   - Move all relevant todos to the plans-directory : DONE.
-
-   - Update the doxygen-documentation.
-
-     Messages/LanguageNames.hpp : DONE
-     Messages/messages/Languages.hpp : DONE
-     Messages/Languages.hpp : DONE
-     Messages/Levels.hpp : DONE
-     Messages/messages/Levels.hpp : DONE
-     Messages/MessagesBase.hpp : DONE
-     Messages/MessagesMain.hpp : DONE
-     Messages/Messages_Testapplication.cpp : DONE
-     Messages/Utilities/TimeAndDate.hpp : DONE
-     Messages/Utilities/Umlaute.hpp : DONE.
-     
-   - It must be precisely documented how to use and write message classes:
-     The simplest way is to write a nice "Messages/demo_Messages_1.cpp" etc. : DONE.
-
   \todo Testing:
    - We need here a preliminary compiler switch, which disables
      testing of fr_CA for versions different from 4.0.3 and 4.1.1.
