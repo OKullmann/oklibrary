6dc6fe0bcbd2392e57267969804186fd0a4c2203
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-04 10:45:15 +0000

    Update of todo "SimpleMessage".

--- Messages/Utilities/plans/Utilities.hpp
+++ Messages/Utilities/plans/Utilities.hpp
@@ -5,19 +5,8 @@
   \brief General plans for the Messages/Utilities module.
 
   \todo SimpleMessage:
-  Create a SimpleMessage class (for quick creation of message objects out of strings):
-    - SimpleMessage(mstring) creates a message object with mstring the message in the default
-      language at basic level; there are two further parameters for full and extensive level,
-      with default values the empty string.
-      All three levels for the default language are implemented.
-    - The question is now how to specify further languages ?
-        With a type-list the languages are specified, and then at run-time
-        values are provided: SimpleMessageF<Language-list> (derived from SimpleMessage;
-        "F" like "foreign").
-      The class template SimpleMessageF provides for each language implementations
-      for all three levels (similar to SimpleMessage), where in case of the empty string the message
-      from the level below is chosen.
-    This all in a new file Utilities/SimpleMessages.
+  Create a class TrivialMessage<X>, which takes a reference to a streamable
+  object of type X, and prints this to the stream (independent of language and level) : DONE
 
   \todo Service for test exceptions:
   As a service for Testsystem/TestException_DesignStudy.hpp
@@ -25,7 +14,7 @@
   classes (and macros) for file-identification purposes:
    - Per file we have __DATE__ (compilation date), __FILE__ (file name),
      __TIME__ (C) (compilation time), and
-     $Date: 2006/12/03 13:08:01 $ (last change date), $Revision: 1.8 $ (current
+     $Date: 2006/12/04 10:45:15 $ (last change date), $Revision: 1.9 $ (current
      revision number) (CVS).
    - For programs we also have the version, the author, name of program etc.
 
