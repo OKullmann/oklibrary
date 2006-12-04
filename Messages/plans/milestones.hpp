756a78633c9b3c3020745d6401ac84fed9299f0a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-04 11:15:29 +0000

    New version 0.7.2.

--- Messages/plans/milestones.hpp
+++ Messages/plans/milestones.hpp
@@ -3,12 +3,7 @@
 /*!
   \file Messages/plans/milestones.hpp
 
-  \module_version Messages version 0.7.1 (3.12.2006)
-
-  \par Version 0.7.2 :
-
-   - In Messages/Utilities/plans/milestones.hpp version 0.0.6 is reached :REACHED
-   - The milestones 0.8, 0.9, 1.0 below have been refined.
+  \module_version Messages version 0.7.2 (4.12.2006)
 
   \par Version 0.7.3 :
 
@@ -19,7 +14,7 @@
    - In Messages/Utilities/plans/milestones.hpp version 0.0.8 is reached.
 
   In Messages/plans/Messages.hpp the following topics are handled:
-  -  Testing
+   -  Testing
 
   \par Version 0.8 :
   Help for memory management of message objects (components, or at least some guidelines)
@@ -29,7 +24,8 @@
   Documentation completed.
 
   \par Version 1.0 :
-  Tests for all components available and applied (see "Testing" in Messages/plans/Messages.hpp)
+   - In Messages/Utilities/plans/milestones.hpp version 0.1 is reached.
+   - Tests for all components available and applied.
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
@@ -38,6 +34,7 @@
 
    - 0.7   : 3.3.2006; initial version number (module functional).
    - 0.7.1 : 3.12.2006; complete update of documentation.
+   - 0.7.2 : 4.12.2006; next version of Utilities reached (and overall update).
 
 */
 
