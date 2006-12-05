f9c724e0d6e4e8fb7133a586e3e4489c84266e13
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-05 17:31:41 +0000

    Wrong include corrected.

--- TestSystem/testobjects/TestBaseClass.cpp
+++ TestSystem/testobjects/TestBaseClass.cpp
@@ -1,6 +1,6 @@
 // Oliver Kullmann, 9.1.2006 (Swansea)
 
-#include <Transitional/TestSystem/TestBaseClass_DesignStudy.hpp>
+#include <Transitional/TestSystem/RunTest_DesignStudy.hpp>
 
 #include <Transitional/TestSystem/tests/TestBaseClass.hpp>
 
