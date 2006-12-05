1dcf0f9b0590884a7e65172aca70fc9025a66841
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-05 17:25:07 +0000

    Specialised function trivial_message for C-string handling corrected (problem with reference to temporary).

--- Messages/Utilities/TrivialMessage.hpp
+++ Messages/Utilities/TrivialMessage.hpp
@@ -2,7 +2,7 @@
 
 /*!
   \file Messages/Utilities/TrivialMessage.hpp
-  \brief Component to wrap a streamable object as message object
+  \brief Components to wrap a streamable object as a message object
 */
 
 #ifndef TRIVIALMESSAGE_jdjNNaml74re34
@@ -19,26 +19,41 @@ namespace OKlib {
       /*!
         \class TrivialMessage
         \brief TrivialMessage<X>(x) prints x
-      */
 
+        Attention: Takes parameter x by (const) reference.
+      */
       template <typename X>
       struct TrivialMessage : ::OKlib::Messages::MessagesBase {
         typedef X value_type;
         explicit TrivialMessage(const value_type& x) : x(x) {}
-        std::ostream& print(std::ostream& out) const {
-          return out << x;
-        }
+        std::ostream& print(std::ostream& out) const { return out << x; }
       private :
         TrivialMessage& operator =(const TrivialMessage&); // no copy assignment
         const value_type& x;
       };
-
       /*!
-        \brief Convenience function template for class TrivialMessage
+        \class TrivialMessageC
+        \brief TrivialMessageC<X>(x) prints x
+
+        Different from TrivialMessage, here x is passed by value.
       */
+      template <typename X>
+      struct TrivialMessageC : ::OKlib::Messages::MessagesBase {
+        typedef X value_type;
+        explicit TrivialMessageC(const value_type x) : x(x) {}
+        std::ostream& print(std::ostream& out) const { return out << x; }
+      private :
+        const value_type x;
+      };
+
+      //! Convenience function template for class TrivialMessage
       template <typename X>
       inline TrivialMessage<X> trivial_message(const X& x) {
         return TrivialMessage<X>(x);
+      }
+      //! Overload for C-strings (to avoid potential code bloat)
+      inline TrivialMessageC<const char*> trivial_message(const char*  x) {
+        return TrivialMessageC<const char*>(x);
       }
     }
   }
