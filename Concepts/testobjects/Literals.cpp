c04c699773249a3bdc2ccaf9a11b2721d4cbace6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-05 17:39:23 +0000

    Testing the concepts and archetypes regarding literals.

---
+++ Concepts/testobjects/Literals.cpp
@@ -0,0 +1,13 @@
+// Oliver Kullmann, 5.12.2006
+
+/*!
+  \file Concepts/testobjects/Literals.hpp
+  \brief Checking whether concept and archetype definitions are consistent
+*/
+
+#include <Transitional/Concepts/Literals.hpp>
+
+namespace {
+
+}
+
