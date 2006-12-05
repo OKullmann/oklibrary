7196b24ad93c3be2f989c49a70e9ff0bebf9674e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-05 17:32:41 +0000

    Small additions.

--- TestSystem/plans/TestProgram.hpp
+++ TestSystem/plans/TestProgram.hpp
@@ -6,12 +6,14 @@
 
   \todo Messages:
   Using module Messages.
+
   \todo Infos:
   Complete the printed information about the input (output if the number of input parameters is not as expected).
-    We also need a more formal description of the set of possible inputs.
+  We also need a more formal description of the set of possible inputs.
 
   \todo ProgramOptions:
   Using module ProgramOptions. In this way we also get more components, which can be tested.
+  Here also input-checking takes place.
 
   \todo Transition:
   Once the transition is done, rename "TestProgram_DesignStudy.hpp" to "TestProgram.hpp", and check the doxygen-documentation (extending it as needed).
