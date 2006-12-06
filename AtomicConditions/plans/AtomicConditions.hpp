fc6619b672ab0bf5a96064c483f85ca00062bbe5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 13:21:41 +0000

    First plans for atomic-conditions components.

---
+++ AtomicConditions/plans/AtomicConditions.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 6.12.2006 (Swansea)
+
+/*!
+  \file AtomicConditions/plans/AtomicConditions.hpp
+  \brief Plans for atomic conditions
+
+  \todo Traits-functions:
+  - Literals/traits/var_type.hpp : Is this the right place for the var-implementation?
+  - Literals/traits/cond_type.hpp : Is this the right place for the cond-implementation?
+
+  \todo Implement trivial atomic conditions literals
+  - bool as atomic condition
+  - perhaps also integers??
+
+  \todo First prototype of general atomic condition
+  Very first prototype using std::set.
+
+*/
+
+/*!
+  \namespace OKlib::Literals
+  \brief Components for literals
+*/
+
+namespace OKlib {
+  namespace Literals {
+  }
+}
+
