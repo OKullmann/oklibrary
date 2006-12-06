16374886d69e1f9ea6c303e86f1bf59cdf4e3671
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 13:17:56 +0000

    Doxygen-correction.

--- Concepts/testobjects/Literals.cpp
+++ Concepts/testobjects/Literals.cpp
@@ -1,7 +1,7 @@
 // Oliver Kullmann, 5.12.2006
 
 /*!
-  \file Concepts/testobjects/Literals.hpp
+  \file Concepts/testobjects/Literals.cpp
   \brief Checking whether concept and archetype definitions are consistent
 */
 
