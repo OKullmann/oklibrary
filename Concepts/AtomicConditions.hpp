be5773a050032400a6e34cea78ce5dabb39f2241
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 13:20:14 +0000

    First prototype finished.

--- Concepts/AtomicConditions.hpp
+++ Concepts/AtomicConditions.hpp
@@ -14,14 +14,34 @@
 
 #include <Transitional/Concepts/LibraryBasics.hpp>
 
+#include <Transitional/AtomicConditions/traits/value_type.hpp>
+
 namespace OKlib {
 
   namespace Concepts {
 
         struct AtomicCondition_tag : virtual BasicRequirements_tag, virtual FullyConstructible_tag,  virtual EqualitySubstitutable_tag, virtual LinearOrder_tag {};
 
+    /*!
+      \class AtomicCondition
+      \brief Concept for basic atomic conditions
+
+      Atomic conditions are fully constructible, and a(n) (implementation-defined) linear order
+      is defined on them. They represent boolean-valued functions on their value type.
+
+      So we have an associated type traits traits::value_type, and via the function
+      AtomicConditions::eval the boolean value of the condition on a value is evaluated.
+      Given a value, the atomic condition which is exactly true resp. false on this value
+      can be constructed.
+
+      Since built-in types can model atomic conditions, for tehm in general the initialisation
+      behaviour of built-in types must be assumed.
+    */
+
     template <typename AC>
     struct AtomicCondition {
+
+      typedef typename OKlib::AtomicConditions::traits::value_type<AC>::type value_type;
 
       void constraints() {
         OKLIB_MODELS_CONCEPT_TAG(AC, AtomicCondition);
@@ -35,8 +55,17 @@ namespace OKlib {
         OKLIB_MODELS_CONCEPT_REQUIRES(AC, LinearOrder);
         OKLIB_MODELS_CONCEPT_TAG(AC, LinearOrder);
 
+        static_cast<bool>(OKlib::AtomicConditions::eval(cc, v));
+        static_cast<bool>(OKlib::AtomicConditions::eval(c, v));
+
+        AC(v);
+        AC(v, true);
+        AC(v, false);
       }
 
+      AC c;
+      const AC cc;
+      const value_type v;
     };
 
   }
