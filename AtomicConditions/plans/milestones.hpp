1a60deaabca93d6a04ee07c2af0c02e5bb996d81
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 13:22:12 +0000

    Initial version 0.0.1.

---
+++ AtomicConditions/plans/milestones.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 6.12.2006 (Swansea)
+
+/*!
+  \file AtomicConditions/plans/milestones.hpp
+
+  \module_version AtomicConditions 0.0.1 (6.12.2006)
+
+  \par Version 0.0.2 :
+  In AtomicConditions/plans/AtomicConditions.hpp the following topics are handled:
+  - Traits-functions
+  - Implement trivial atomic conditions literals.
+
+  \par Version 0.0.3 :
+  In AtomicConditions/plans/AtomicConditions.hpp the following topics are handled:
+  - First prototype of general atomic condition.
+
+  And further milestones are created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   -  0.0.1 : 6.12.2006; initial version number.
+
+*/
