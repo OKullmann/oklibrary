d964325709cc2d3bf3c82ff98e88a23d618336ce
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 13:18:23 +0000

    Just reading yet the AtomicConditions-concept-file.

---
+++ Concepts/testobjects/AtomicConditions.cpp
@@ -0,0 +1,13 @@
+// Oliver Kullmann, 6.12.2006
+
+/*!
+  \file Concepts/testobjects/AtomicConditions.cpp
+  \brief Checking whether concept and archetype definitions are consistent
+*/
+
+#include <Transitional/Concepts/AtomicConditions.hpp>
+
+namespace {
+
+}
+
