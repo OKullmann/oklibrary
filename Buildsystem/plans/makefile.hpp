40cb8dc8b1f3c0722fe720468b52aefafd139bf2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-06 19:34:20 +0000

    Plans for the buildsystem-makefile.

---
+++ Buildsystem/plans/makefile.hpp
@@ -0,0 +1,10 @@
+// Oliver Kullmann, 6.12.2006 (Swansea)
+
+/*!
+  \file Buildsystem/plans/makefile.hpp
+  \brief Plans for the buildsystem-makefile
+
+  \todo Directories with makefile-links:
+  Improve generic_dirs
+*/
+
