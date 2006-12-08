4bb3b409490b549ff1bb23f1ad6c3ba7505a997a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-08 15:38:19 +0000

    Started implementation of bool as an atomic condition.

---
+++ AtomicConditions/TrivialAtomicConditions.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 7.12.2006 (Swansea)
+
+/*!
+  \file AtomicConditions/TrivialAtomicConditions.hpp
+  \brief Typedefs for trivial models of concept AtomicConditions
+
+  \todo Complete definition of AC_bool, and test it syntactically.
+  \todo Test AC_bool semantically.
+*/
+
+#ifndef TRIVIALATOMICCONDITIONS_jBffRql9
+#define TRIVIALATOMICCONDITIONS_jBffRql9
+
+namespace OKlib {
+  namespace AtomicConditions {
+
+    typedef bool AC_bool;
+
+    namespace traits {
+      
+    }
+  }
+}
+
+#endif
+
