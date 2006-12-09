290ab49572921d7f572892c34c55102bfbc71612
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-09 00:57:26 +0000

    First implementation completed.

--- AtomicConditions/TrivialAtomicConditions.hpp
+++ AtomicConditions/TrivialAtomicConditions.hpp
@@ -4,23 +4,50 @@
   \file AtomicConditions/TrivialAtomicConditions.hpp
   \brief Typedefs for trivial models of concept AtomicConditions
 
-  \todo Complete definition of AC_bool, and test it syntactically.
   \todo Test AC_bool semantically.
 */
 
 #ifndef TRIVIALATOMICCONDITIONS_jBffRql9
 #define TRIVIALATOMICCONDITIONS_jBffRql9
 
+#include <Transitional/AtomicConditions/traits/value_type.hpp>
+#include <Transitional/AtomicConditions/eval.hpp>
+
 namespace OKlib {
   namespace AtomicConditions {
+
+    /*!
+      \typedef AC_bool
+      \brief Boolean values as atomic conditions
+    */
 
     typedef bool AC_bool;
 
     namespace traits {
+
+      template <>
+      struct value_type<AC_bool> {
+        typedef bool type;
+      };
+    }
+
+    template <>
+    inline bool eval(const AC_bool& ac, const bool& b) {
+      return ac == b;
+    }
+
+  }
+
+  namespace Concepts {
+    namespace traits {
       
+      template <>
+      struct concept_tag<OKlib::AtomicConditions::AC_bool> {
+        typedef OKlib::Concepts::AtomicCondition_tag type;
+      };
+
     }
   }
 }
 
 #endif
-
