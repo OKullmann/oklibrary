23f6ca202d0fae31760cbb6e706387bfc64503c6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-09 19:48:09 +0000

    Small additions.

--- Literals/var.hpp
+++ Literals/var.hpp
@@ -3,6 +3,9 @@
 /*!
   \file Literals/var.hpp
   \brief Primary function template for extracting the variable from a literal
+
+  \todo For the future perhaps we should use a call traits instead of the fixed
+  argument type const L& ?
 */
 
 #ifndef VAR_jJJn1010156
@@ -18,7 +21,7 @@ namespace OKlib {
     */
 
     template <typename L>
-    typename OKlib::Literals::traits::var_type<L>::type var(const L& x);
+    inline typename OKlib::Literals::traits::var_type<L>::type var(const L& x);
 
   }
 }
