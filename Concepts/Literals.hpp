7776594032d09a82b9d320791891b5f926edcc91
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-09 01:05:47 +0000

    Concept-prototype completed.

--- Concepts/Literals.hpp
+++ Concepts/Literals.hpp
@@ -3,9 +3,6 @@
 /*!
   \file Concepts/Literals.hpp
   \brief Concepts for literals
-
-  \todo To be completed:
-   - Create (and test) archetype.
 */
 
 #ifndef LITERALS_JJhala7t4
@@ -18,6 +15,8 @@
 
 #include <Transitional/Literals/traits/var_type.hpp>
 #include <Transitional/Literals/traits/cond_type.hpp>
+#include <Transitional/Literals/var.hpp>
+#include <Transitional/Literals/cond.hpp>
 
 namespace OKlib {
 
@@ -60,7 +59,9 @@ namespace OKlib {
         OKLIB_MODELS_CONCEPT_TAG(Lit, LinearOrder);
 
         OKLIB_MODELS_CONCEPT_REQUIRES(var_type, Concepts::Variables);
+        OKLIB_MODELS_CONCEPT_TAG(var_type, Concepts::Variables);
         OKLIB_MODELS_CONCEPT_REQUIRES(cond_type, Concepts::AtomicCondition);
+        OKLIB_MODELS_CONCEPT_TAG(cond_type, Concepts::AtomicCondition);
         
         static_cast<var_type>(OKlib::Literals::var(lc));
         static_cast<var_type>(OKlib::Literals::var(l));
@@ -68,18 +69,41 @@ namespace OKlib {
         static_cast<cond_type>(OKlib::Literals::cond(lc));
         static_cast<cond_type&>(OKlib::Literals::cond(l));
 
-        Lit(v, cond);
+        Lit(v);
       }
 
       Lit l;
       const Lit lc;
       const var_type v;
-      const cond_type cond;
     };
 
+    class Literals_Archetype {
+      typedef OKlib::Concepts::convertible_to_bool convertible_to_bool;
+    public :
+      typedef Literals_tag concept_tag;
+      typedef OKlib::Concepts::Variables_Archetype var_type;
+      typedef OKlib::Concepts::AtomicCondition_Archetype cond_type;
+      Literals_Archetype() {}
+      Literals_Archetype(var_type, const cond_type&) {}
+      convertible_to_bool operator ==(const Literals_Archetype&) const { return convertible_to_bool(); }
+      convertible_to_bool operator !=(const Literals_Archetype&) const { return convertible_to_bool(); }
+      convertible_to_bool operator <(const Literals_Archetype&) const { return convertible_to_bool(); }
+      convertible_to_bool operator >(const Literals_Archetype&) const { return convertible_to_bool(); }
+      convertible_to_bool operator <=(const Literals_Archetype&) const { return convertible_to_bool(); }
+      convertible_to_bool operator >=(const Literals_Archetype&) const { return convertible_to_bool(); }
 
+    };
   }
-
+  namespace Literals {
+    template <>
+    inline OKlib::Concepts::Literals_Archetype::var_type var(const OKlib::Concepts::Literals_Archetype&) { return OKlib::Concepts::Literals_Archetype::var_type(); }
+    template <>
+    inline OKlib::Concepts::Literals_Archetype::cond_type cond(const OKlib::Concepts::Literals_Archetype&) { return OKlib::Concepts::Literals_Archetype::cond_type(); }
+    template <>
+    inline OKlib::Concepts::Literals_Archetype::cond_type& cond(OKlib::Concepts::Literals_Archetype&) { static OKlib::Concepts::Literals_Archetype::cond_type c; return c; }
+  }
+  
 }
+
 
 #endif
