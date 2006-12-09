7e6ef9e4733afb94b33e14d35961a672994b29d9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-09 19:48:55 +0000

    Tests activated.

--- Literals/testobjects/TrivialLiterals.cpp
+++ Literals/testobjects/TrivialLiterals.cpp
@@ -12,10 +12,10 @@
 
 namespace {
 
-//   ::OKlib::TestSystem::RunTest test_trivialliteral_signed_char(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_signed_char>);
-//   ::OKlib::TestSystem::RunTest test_trivialliteral_short_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_short_int>);
-//   ::OKlib::TestSystem::RunTest test_trivialliteral_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_int>);
-//   ::OKlib::TestSystem::RunTest test_trivialliteral_long_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_long_int>);
+  ::OKlib::TestSystem::RunTest test_trivialliteral_signed_char(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_signed_char>);
+  ::OKlib::TestSystem::RunTest test_trivialliteral_short_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_short_int>);
+  ::OKlib::TestSystem::RunTest test_trivialliteral_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_int>);
+  ::OKlib::TestSystem::RunTest test_trivialliteral_long_int(new OKlib::Concepts::tests::Literals_basic<OKlib::Literals::Literals_long_int>);
 
 }
 
