2c03233017a4bd4dd1d558686b9c4b3293208ca4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-09 19:48:38 +0000

    Trivial literals implemented.

--- Literals/TrivialLiterals.hpp
+++ Literals/TrivialLiterals.hpp
@@ -2,17 +2,25 @@
 
 /*!
   \file Literals/TrivialLiterals.hpp
-  \brief Typedefs for trivial models of concept Literals
-
-  \todo Complete implementations.
+  \brief Typedefs for models of concept Literals given by signed integral types
 */
 
 #ifndef TRIVIALLITERALS_jdjNNqpqoh7u5
 #define TRIVIALLITERALS_jdjNNqpqoh7u5
 
+#include <cstdlib>
+
+#include <Transitional/Concepts/traits/concept_tag.hpp>
+
+#include <Transitional/Concepts/Variables.hpp>
+#include <Transitional/Concepts/Literals.hpp>
+#include <Transitional/Concepts/JoinedConceptTags.hpp>
+
 #include <Transitional/Variables/TrivialVariables.hpp>
 #include <Transitional/AtomicConditions/TrivialAtomicConditions.hpp>
 
+#include <Transitional/Literals/traits/var_type.hpp>
+#include <Transitional/Literals/traits/cond_type.hpp>
 #include <Transitional/Literals/var.hpp>
 #include <Transitional/Literals/cond.hpp>
 
@@ -25,29 +33,73 @@ namespace OKlib {
     typedef int Literals_int;
     typedef long int Literals_long_int;
 
+    namespace traits {
+
+      template <>
+      struct var_type<Literals_signed_char> { typedef OKlib::Variables::Variables_signed_char type; };
+      template <>
+      struct var_type<Literals_short_int> { typedef OKlib::Variables::Variables_short_int type; };
+      template <>
+      struct var_type<Literals_int> { typedef OKlib::Variables::Variables_int type; };
+      template <>
+      struct var_type<Literals_long_int> { typedef OKlib::Variables::Variables_long_int type; };
+
+      template <>
+      struct cond_type<Literals_signed_char> { typedef OKlib::AtomicConditions::AC_bool type; };
+      template <>
+      struct cond_type<Literals_short_int> { typedef OKlib::AtomicConditions::AC_bool type; };
+      template <>
+      struct cond_type<Literals_int> { typedef OKlib::AtomicConditions::AC_bool type; };
+      template <>
+      struct cond_type<Literals_long_int> { typedef OKlib::AtomicConditions::AC_bool type; };
+
+    }
+
+    template <>
+    inline traits::var_type<Literals_signed_char>::type var(const Literals_signed_char& x) { return std::abs(x); }
+    template <>
+    inline traits::var_type<Literals_short_int>::type var(const Literals_short_int& x) { return std::abs(x); }
+    template <>
+    inline traits::var_type<Literals_int>::type var(const Literals_int& x) { return std::abs(x); }
+    template <>
+    inline traits::var_type<Literals_long_int>::type var(const Literals_long_int& x) { return std::abs(x); }
+
+    template <>
+    inline traits::cond_type<Literals_signed_char>::type cond(const Literals_signed_char& x) { return x > 0; }
+    template <>
+    inline traits::cond_type<Literals_short_int>::type cond(const Literals_short_int& x) { return x > 0; }
+    template <>
+    inline traits::cond_type<Literals_int>::type cond(const Literals_int& x) { return x > 0; }
+    template <>
+    inline traits::cond_type<Literals_long_int>::type cond(const Literals_long_int& x) { return x > 0; }
+
+    template <>
+    inline void set_cond(Literals_signed_char& x, const traits::cond_type<Literals_signed_char>::type& cond) {
+      if (cond) x = std::abs(x);
+      else x = - std::abs(x);
+    }
+    template <>
+    inline void set_cond(Literals_short_int& x, const traits::cond_type<Literals_short_int>::type& cond) {
+      if (cond) x = std::abs(x);
+      else x = - std::abs(x);
+    }
+    template <>
+    inline void set_cond(Literals_int& x, const traits::cond_type<Literals_int>::type& cond) {
+      if (cond) x = std::abs(x);
+      else x = - std::abs(x);
+    }
+    template <>
+    inline void set_cond(Literals_long_int& x, const traits::cond_type<Literals_long_int>::type& cond) {
+      if (cond) x = std::abs(x);
+      else x = - std::abs(x);
+    }
+
   }
 
   namespace Concepts {
     namespace traits {
-
-      // PROBLEM with global concept_tag !
       
-//       template <>
-//       struct concept_tag<OKlib::Literals::Literals_signed_char> {
-//         typedef OKlib::Concepts::Literals_tag type;
-//       };
-//       template <>
-//       struct concept_tag<OKlib::Literals::Literals_short_int> {
-//         typedef OKlib::Concepts::Literals_tag type;
-//       };
-//       template <>
-//       struct concept_tag<OKlib::Literals::Literals_int> {
-//         typedef OKlib::Concepts::Literals_tag type;
-//       };
-//       template <>
-//       struct concept_tag<OKlib::Literals::Literals_long_int> {
-//         typedef OKlib::Concepts::Literals_tag type;
-//       };
+      // Since Literals::Literals_signed_char is the same type as Variables::Variables_signed_char, the definitions from Variables/TrivialVariables.hpp must be used here.
 
     }
   }
