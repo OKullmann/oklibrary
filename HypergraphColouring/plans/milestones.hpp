38788ca986cee85cf49b7b8558636f6d0d2ed2a5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-15 12:49:16 +0000

    Updated, and new version 0.0.4.

--- HypergraphColouring/plans/milestones.hpp
+++ HypergraphColouring/plans/milestones.hpp
@@ -3,14 +3,24 @@
 /*!
   \file HypergraphColouring/plans/milestones.hpp
 
-  \module_version HypergraphColouring 0.0.3 (24.11.2006)
+  \module_version HypergraphColouring 0.0.4 (15.12.2006)
 
-  \par Version 0.0.4 :
+  \par Version 0.0.5 :
+  In HypergraphColouring/plans/GreedyColouring.hpp the topic
+  - Outsourcing
+  has been handled.
+
+  \par Version 0.0.6 :
+  In HypergraphColouring/plans/GreedyColouring.hpp the topic
+  - Full_greedy_colouring
+  has been handled.
+
+  \par Version 0.0.7 :
   In HypergraphColouring/plans/GreedyColouring.hpp the topic
   - GreedyColouring.cpp
   has been handled.
 
-  \par Version 0.0.5 :
+  \par Version 0.0.8 :
   In HypergraphColouring/plans/HypergraphColouring.hpp the topic
   "Translation" is further expanded, and related milestones are created.
 
@@ -25,6 +35,8 @@
 
    - 0.0.2 : 31.10.2006; initial version number (some plans).
    - 0.0.3 : 24.11.2006; simple application program exploring greedy colouring.
+   - 0.0.4 : 15.12.2006; the components from the greedy-colouring application have been
+               outsourced to the sub-module GreedyColouring.hpp.
 
 */
 
