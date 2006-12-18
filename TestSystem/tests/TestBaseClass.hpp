97ddfccc3cdd07d7942ec7e3d286c59d41d78d77
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-18 14:38:16 +0000

    OKLIB_FILE_ID introduced, and various output-improvements.

--- TestSystem/tests/TestBaseClass.hpp
+++ TestSystem/tests/TestBaseClass.hpp
@@ -11,6 +11,7 @@
 #include <sstream>
 
 #include <Transitional/Messages/Utilities/TrivialMessage.hpp>
+#include <Transitional/Messages/messages/Levels.hpp>
 
 #include <Transitional/TestSystem/TestBaseClass_DesignStudy.hpp>
 #include <Transitional/TestSystem/RunTest_DesignStudy.hpp>
@@ -20,10 +21,17 @@ namespace OKlib {
   namespace TestSystem {
     namespace tests {
 
+# define OKLIB_FILE_ID new ::OKlib::Messages::Utilities::FileIdentification \
+      (__FILE__, __DATE__, __TIME__, "$Date: 2006/12/18 14:38:16 $", "$Revision: 1.3 $")
+
       /*!
         \class TestSystem
         \brief Testing the test system
+        
         \todo Complete it.
+        \todo Doxygen documentation.
+        \todo Improve the output (a more systematical approach for better structuring; and replacing the use of
+        trivial messages).
       */
       
       class TestSystem :  public ::OKlib::TestSystem::TestBase {
@@ -52,11 +60,17 @@ namespace OKlib {
           log(trivial_message("\nFIRST TEST\n"), __LINE__, __FILE__);
           {
             std::stringstream test_err, test_messages, test_log;
+            OKlib::Messages::MessagesBase::set(test_log, OKlib::Messages::MessagesBase::level(log_stream()));
+            OKlib::Messages::MessagesBase::set(test_err, OKlib::Messages::MessagesBase::level(log_stream()));
+            OKlib::Messages::MessagesBase::set(test_messages, OKlib::Messages::MessagesBase::level(log_stream()));
             test_objects.push_back(new LocalTest1(0));
             ::OKlib::TestSystem::RunTest::run_tests(test_err, test_messages, test_log, test_level, test_objects);
-            log(trivial_message("content of test_err:\n" + test_err.str()), __LINE__, __FILE__);
-            log(trivial_message("content of test_messages:\n" + test_messages.str()), __LINE__, __FILE__);
-            log(trivial_message("content of test_log:\n" + test_log.str()), __LINE__, __FILE__);
+            log(trivial_message("Output of the three streams after RunTest::run_tests:\n"), __LINE__, __FILE__);
+            log(trivial_message("content of test_err:\n" + test_err.str()));
+            log(trivial_message("content of test_messages:\n" + test_messages.str()));
+            log(trivial_message("test_log level = "));
+            log(OKlib::Messages::messages::LevelNames(OKlib::Messages::MessagesBase::level(test_log)));
+            log(trivial_message("\ncontent of test_log:\n" + test_log.str()));
             if (not test_err.str().empty())
               OKLIB_THROW("Error thrown!");
             if (test_messages.str().empty())
@@ -67,11 +81,17 @@ namespace OKlib {
           log(trivial_message("\nSECOND TEST\n"), __LINE__, __FILE__);
           {
             std::stringstream test_err, test_messages, test_log;
+            OKlib::Messages::MessagesBase::set(test_log, OKlib::Messages::MessagesBase::level(log_stream()));
+            OKlib::Messages::MessagesBase::set(test_err, OKlib::Messages::MessagesBase::level(log_stream()));
+            OKlib::Messages::MessagesBase::set(test_messages, OKlib::Messages::MessagesBase::level(log_stream())); 
             test_objects.push_back(new LocalTest1(1));
             ::OKlib::TestSystem::RunTest::run_tests(test_err, test_messages, test_log, test_level, test_objects);
-            log(trivial_message("content of test_err:\n" + test_err.str()), __LINE__, __FILE__);
-            log(trivial_message("content of test_messages:\n" + test_messages.str()), __LINE__, __FILE__);
-            log(trivial_message("content of test_log:\n" + test_log.str()), __LINE__, __FILE__);
+            log(trivial_message("Output of the three streams after RunTest::run_tests:\n"), __LINE__, __FILE__);
+            log(trivial_message("content of test_err:\n" + test_err.str()));
+            log(trivial_message("content of test_messages:\n" + test_messages.str()));
+            log(trivial_message("test_log level = "));
+            log(OKlib::Messages::messages::LevelNames(OKlib::Messages::MessagesBase::level(test_log)));
+            log(trivial_message("\ncontent of test_log:\n" + test_log.str()));
             if (test_err.str().empty())
               OKLIB_THROW("No error!");
             if (test_messages.str().empty())
@@ -86,4 +106,5 @@ namespace OKlib {
   }
 }
 
+# undef OKLIB_FILE_ID
 #endif
