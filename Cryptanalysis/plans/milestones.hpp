a33eaf7acb55b2ca2e1e3d442d66fe4656d70ee0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-18 14:21:39 +0000

    Update.

--- Cryptanalysis/plans/milestones.hpp
+++ Cryptanalysis/plans/milestones.hpp
@@ -3,13 +3,33 @@
 /*!
   \file Cryptanalysis/plans/milestones.hpp
 
-  \par Current version 0.0.2 (12.11.2006)
+  \module_version Cryptanalysis 0.0.2 (12.11.2006)
+
+  \par Version 0.0.3 :
+  Further milestones leading to version 0.1 are created.
 
   \par Version 0.1 :
-  In Cryptanalysis/plans/Cryptanalysis.hpp the topics
+  In Cryptanalysis/plans/Rijndael.hpp the topics
   - Implementing
-  - First test AES
+  - First tests AES
+  - Rijndael
+
   have been handled, and tested generators are available.
+
+  \par Version 0.2 :
+  In Cryptanalysis/plans/Rijndael.hpp the topics
+  - Further tests
+  - Good constraints
+  - Overview
+
+  have been handled.
+
+  \par Version 0.3 :
+  In Cryptanalysis/plans/Factorisation.hpp the topics
+  - Literature review
+  - Interesting good constraint representations
+
+  have been handled.
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
