bdbfe7cf4ea6033fa9cdcc0e8525f37e9eb6ad59
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-18 14:26:02 +0000

    Extensions (regarding the computation of the chromatic number and deciding critical non-2-colourable square hypergraphs).

--- HypergraphColouring/plans/HypergraphColouring.hpp
+++ HypergraphColouring/plans/HypergraphColouring.hpp
@@ -11,18 +11,49 @@
   \todo For interesting systematically generated classes of (hyper)graphs,
   create active clause-sets translating k-colouring problems.
 
-  \todo Investigate the problem of determining the chromatic number.
+  \todo Investigate the problem of determining the chromatic number:
+  <ul>
+   <li> For finding an optimal colouring for a graph G the following strategy seems natural:
+    <ol>
+     <li> Find a reasonable upper bound chi(G) <= u by some quick heuristics (different forms
+              of greedy colouring).
+     </li>
+     <li> Find a reasonable lower bound l <= chi(G) by some quick heuristics, among them
+          finding cliques in G.
+     </li>
+     <li> Comprising the found cliques into hyperedges, a hypergraph G' is constructed (with
+          chi_s(G') = chi(G), where chi_s is the strong colouring number), and then via
+          a (generalised) SAT solver the interval l <= chi_s(G') <= u is narrowed to a point
+          (perhaps here bisection is sensible?!); see HypergraphColouring/plans/StrongColouring.hpp
+          for strong colourings of hypergraphs.
+     </li>
+    </ol>
+   </li>
+   <li> For finding an optimal colouring for a hypergraph G we should try the same strategy:
+    <ol>
+     <li> Find some lower bound l and some upper bound u. </li>
+     <li> Close the gap with a (generalised) SAT solver. </li>
+    </ol>
+   </li>
+   <li> And the same for strong hypergrap colouring. </li>
+  </ul>
 
   \todo Erdoes and Hajnal introduced the function m(k) associated with property B:
-  The minimal number of clauses of non-2-colourable k-uniform hypergraphs.
+  The minimal number of hyperedges of non-2-colourable k-uniform hypergraphs.
   Known are only m(0) = m(1) = 1, m(2) = 3, m(3) = 7 (see the latest OK-report (XXX)
   on autarkies and hypergraph colouring), while 19 <= m(4) <= 23. Given n, m, k,
   it should be possible to construct a nice SAT problem whose solutions are the k-uniform
   non-2-colourable hypergraphs G with n vertices and m edges. Then we can try to compute
   values for m(k).
+
   This can also be generalised to higher chromatic numbers (in the literature there is the
   generalised property B(s)? it seems more natural to consider c >= 2 for "non-c-colourable",
   and to consider m(c, k) (with m(2, k) = m(k))).
+
+  \todo In [Robertson, Seymour, Thomas: Permanents, Pfaffian orientations, and even directed circuits]
+  a poly-time algorithm for deciding whether a square hypergraph is minimal non-2-colourable is
+  given; very interesting to investigate this algorithm.
+
 */
 
 /*!
