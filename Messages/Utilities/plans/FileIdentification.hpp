fa76a5ed67d41c4d97a0db963f864a8068d46692
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-18 14:28:53 +0000

    Plans for the sub-module on file-identification.

---
+++ Messages/Utilities/plans/FileIdentification.hpp
@@ -0,0 +1,10 @@
+// Oliver Kullmann, 17.12.2006 (Swansea)
+
+/*!
+  \file Messages/Utilities/plans/FileIdentification.hpp
+  \brief Plans for the file identification utilities
+
+  \todo File identification: See Messages/Utilities/FileIdentification.hpp. DONE
+
+*/
+
