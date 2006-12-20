355215c95fc83e7296a3162dbcad806d8c34f89e
Matthew Henderson <M.J.Henderson@Swansea.ac.uk>
2006-12-20 12:46:22 +0000

    Further plans about Logo and OKlibrary link.

--- Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp
+++ Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp
@@ -6,9 +6,14 @@
 
   \todo Logo
    - Perhaps the logo can be improved. Maybe just blue, without any gradient.
-  
+   - An possible alternative is not to use any logo, and only use the stylesheet
+     to specify a nice font.  
+
   \todo OKlibrary link
-   - The documentation for OKlibrary should stand out from the others.
+   - The documentation for OKlibrary should stand out from the others. 
+   - Perhaps we have a section just for the OKlibrary which has links to the
+     documentation for the html documentation and also links to the 
+     meta-documentation.
 
   \todo Version numbers
    - instead of "v0.0.1" we should have "Transitional Version 0.1.0"
