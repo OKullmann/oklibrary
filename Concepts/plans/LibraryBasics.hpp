d92b64f13edc7bb5e2c93110637018f19bd7736c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-30 19:27:07 +0000

    Collected todos.

---
+++ Concepts/plans/LibraryBasics.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 29.12.2006 (Swansea)
+
+/*!
+  \file Concepts/plans//LibraryBasics.hpp
+  \brief Plans for most basic concepts
+
+  \todo Should the helper-classes for archetype-definitions (like Concepts::convertible_to_bool) go into their own file?
+
+  \todo Should boost/type_traits.hpp be replaced by the tr1-components?
+
+  \todo Perhaps the macros need some clean-up.
+
+  \todo Transfer the tests to the new test system.
+*/
+
