cc97e8bb64ace2b0d7c62fe90cd191b198674864
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-30 19:28:56 +0000

    New module for basic graph-components (mainly support for Boost::Graph).

---
+++ Graphs/plans/Graphs.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 30.12.2006 (Swansea)
+
+/*!
+  \file Graphs/plans/Graphs.hpp
+  \brief Plans for the general module on graph-components (especially support for Boost::Graph)
+
+  \todo Collect:
+  Collect all related components (and requests) from modules in
+  the library, and transfer the todos to this module:
+   - HypergraphColouring
+   - QuantumPhysics
+   - ???
+*/
+
+/*!
+  \namespace OKlib::Graphs
+  \brief Components for working with graphs (especially support for Boost::Graph)
+*/
+
+namespace OKlib {
+  namespace Graphs {
+  }
+}
+
---
+++ Graphs/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 30.12.2006 (Swansea)
+
+/*!
+  \file Graphs/plans/milestones.hpp
+
+  \module_version Graphs 0.0.2 (30.12.2006)
+
+  \par Version 0.0.3 :
+  In Graphs/plans/Graphs.hpp the topic
+  - Collect
+  has been handled, and further milestones have been created.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 30.12.2006; initial version number (some components in other modules).
+
+*/
