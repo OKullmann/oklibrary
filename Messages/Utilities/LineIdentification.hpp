6b4f60cfea4c5c1eb0581ad775d53db6884ee1f0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-30 19:33:37 +0000

    Untested message class for source-code line numbers.

---
+++ Messages/Utilities/LineIdentification.hpp
@@ -0,0 +1,46 @@
+// Oliver Kullmann, 30.12.2006 (Swansea)
+
+/*!
+  \file Messages/Utilities/LineIdentification.hpp
+  \brief Message class to provide source-code line-number information
+*/
+
+#ifndef LINEIDENTIFICATION_HHcaoryGb384
+#define LINEIDENTIFICATION_HHcaoryGb384
+
+#include <Transitional/Messages/MessagesMain.hpp>
+#include <Transitional/Messages/LineHandling.hpp>
+
+namespace OKlib {
+  namespace Messages {
+    namespace Utilities {
+
+      /*!
+        \class LineIdentification
+        \brief Line number indentification (for source-code files)
+      */
+
+      OKLIB_USING_MESSAGES
+
+      struct LineIdentification : ::OKlib::Messages::MessagesPrePost {
+        OKLIB_MESSAGES_PRINT
+
+        LineIdentification(
+                           std::string const line) :
+          line(line)
+        {}
+
+        void print(std::ostream& out, L<en_GB>, S<Basic>) const {
+          l_start(out) << "line number = " << line;
+        }
+
+      private :
+
+        std::string line;
+      };
+
+    }
+  }
+}
+
+#endif
