3c07474851fe70a919977e62713f192e5ad30e9c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2006-12-30 19:26:23 +0000

    Collected plans regarding basic concepts.

---
+++ Concepts/plans/Basics.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 29.12.2006 (Swansea)
+
+/*!
+  \file Concepts/plans/Basics.hpp
+  \brief Plans regarding basic concepts
+
+  \todo Test the archetypes etc.
+
+  \todo Write FullyConstructibleLt.
+
+  \todo Write FullyConstructibleLo.
+
+  \todo Use OKlib::Concepts::convertible_to_bool.
+
+  \todo Write semantical tests.
+
+*/
