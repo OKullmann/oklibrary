4c65e084d8542097079a62f0effb7267866a26d6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-02 16:46:53 +0000

    Line numbers now handled by message class Utilities::LineIdentification.

--- TestSystem/messages/TestExceptions.hpp
+++ TestSystem/messages/TestExceptions.hpp
@@ -27,10 +27,10 @@ namespace OKlib {
         \class ErrorDescription
         \brief The unit to describe the point in call history leading to a test failure.
 
-        A message class; object contain C strings for the file name, the line number and the
-        name of the type of the test class, an integer for the nesting depth, plus a
-        shared-pointer to a message-object describing the test-level (class ErrorDescription
-        takes over ownership of this message-object-pointer).
+        A message class; an object contains shared pointers to message objects for the file name,
+        the line number and the test-level (class ErrorDescription takes over ownership of these
+        message-object-pointers), a C string with name of the type of the test class, and an integer
+        for the nesting depth.
 
         \todo Provide different output-levels.
         \todo Provide different output-languages.
@@ -44,7 +44,7 @@ namespace OKlib {
         typedef std::tr1::shared_ptr< ::OKlib::Messages::MessagesPrePost> MessagePointer;
 
         MessagePointer file;
-        const char* line;
+        MessagePointer line;
         const char* type_test_class;
         MessagePointer level_description;
         ::OKlib::TestSystem::depth_number_type depth;
@@ -53,7 +53,7 @@ namespace OKlib {
 
         ErrorDescription(
                          ::OKlib::Messages::Utilities::FileIdentification* const file_,
-                         const char* const line_,
+                         ::OKlib::Messages::Utilities::LineIdentification* const line_,
                          const char* const type_test_class_,
                          ::OKlib::TestSystem::messages::TestLevelDescriptions* const level_description_,
                          const ::OKlib::TestSystem::depth_number_type depth_
@@ -65,19 +65,21 @@ namespace OKlib {
           depth(depth_)
         {
           assert(file.get());
-          assert(line);
+          assert(line.get());
           assert(type_test_class);
           assert(level_description.get());
+
           file -> set_prefix(1);
+          line -> set_prefix(1);
           level_description -> set_prefix(1);
         }
 
         void print(std::ostream& out, L<en_GB>, S<Basic>) const {
           assert(file.get());
-          assert(line);
+          assert(line.get());
           assert(type_test_class);
           assert(level_description.get());
-          out << *file << "\n line number = " << line << "\n test type = " << type_test_class << "\n" << *level_description << "\n test depth = " << depth << "\n";
+          out << *file << "\n" << *line << "\n test type = " << type_test_class << "\n" << *level_description << "\n test depth = " << depth << "\n";
         }
       
       };
