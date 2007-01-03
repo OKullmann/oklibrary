6b85d116faad14720435e73fe56cf8b34f1494d5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-03 17:09:56 +0000

    Started (explicit) development of concepts for active clause-sets.

---
+++ Concepts/plans/ActiveClauseSets.hpp
@@ -0,0 +1,70 @@
+// Oliver Kullmann, 3.1.2007 (Swansea)
+
+/*!
+  \file Concepts/plans/ActiveClauseSets.hpp
+  \brief Plans for the concept of an active clause-set
+
+  \todo Update with Concepts/plans/ClauseSets.hpp (transferring ideas from
+  there to here).
+
+  \todo Partial assignment:
+  An active clause-set (ACLS F in the sequel) has a connection to the
+  main partial assignment:
+   - it can process the new assignments
+   - and it can undo them.
+
+  The concept of partial assignments needs to support this. Actually, speaking
+  of "partial assignments" might be a bit misleading, since literals for us
+  are kind of arbitrary constraints ?!
+
+  A problem here is, that we might have different types of literals (for example
+  unary and binary literals), and because of static typing then we need
+  actually a tuple of partial assignments, collected into the "super-assignment" phi.
+
+  \todo Evaluation:
+  The current partial assignment phi can be evaluated (written phi(F)),
+  returning a 4-valued object:
+   - 0 if all total assignments extending phi falsify F
+   - 1 if all total assignments extending phi satisify F
+   - 0/1 if there are total assignments extending phi falsifing and
+     satisfying F
+   - unknown (can not be returned if all variables of F got a unique value).
+
+  So we need to extend Boost::tribool by a "4-valued logic"; we need literature
+  on that (see verification of circuits).
+
+  \todo Implied literals:
+  The literal type of F is the same as for the associated partial assignment.
+  Processing the new assignment creates a list of implied literals
+  (or should this be a partial assignment?).
+
+  If there are for example literals for equality, then this included
+  implied equivalences.
+
+  \todo Support for local search:
+  Given a total assignment for the variables, an alteration is produced which
+  satisfies F or comes as close as possible to that.
+  (Total assignment means that all variables are fixed; perhaps here we should
+  consider only unary literals?)
+
+  For autarky search, here it must also be possible to disable variables; this is
+  easiest understand by referring to an underlying clause-model.
+
+  \todo Causes of conflict:
+  For partial assignments, let "phi' <= phi" be true if var(phi') <= var(phi)
+  and for all in var(phi') the literals phi'(v) weakens phi(v) (the set of
+  allowed values of phi'(v) contains the set of allowed values of phi(v)).
+
+  Now if phi(F) = 0, then phi' is computed with phi' <= phi and phi'(F) = 0.
+
+  \todo Autarkies:
+  What is an appropriate notions of autarkies? Easiest is to refer to some
+  underlying clause-model ?!
+
+  \todo For a "strong ACLS" all above operations are poly-time in the number
+  of variables, and are always complete (no "unknow" for evaluation, all implied
+  literals and equivalences, the changed total assignment is satisfying).
+
+  \todo Should we have statistics for an underlying clause-model?
+  
+*/
