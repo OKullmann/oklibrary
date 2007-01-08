f7395ac0f47b5669b09b984948486c6aaebf2ecc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-08 13:25:30 +0000

    Initial version 0.0.3.

---
+++ Learning/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 8.1.2007 (Swansea)
+
+/*!
+  \file Learning/plans/milestones.hpp
+
+  \module_version Learning 0.0.3 (8.1.2007)
+
+  \par Version 0.0.4 :
+  The first task seems to be to determine the abstract framework (which
+  components in the solving process are responsible for learning?)
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 8.1.2007; initial version number (several plans).
+
+*/
