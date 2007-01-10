9ca3a857320dd9bb5796ebdbdd86400113835ee0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-10 16:16:17 +0000

    Plans for the message-classes regarding line handling.

---
+++ Messages/plans/LineHandling.hpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 8.1.2007 (Swansea)
+
+/*!
+  \file Messages/plans/LineHandling.hpp
+  \brief Plans regarding the line-handling message-classes
+
+  \todo Extend doxygen-documentation for Messages::MessagesPrePost
+
+  \todo Write demo-programs for Messages::MessagesLines and Messages::MessagesPrePost
+
+*/
+
