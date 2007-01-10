7e5134e71d76976058b21d9e69233b591ff8e934
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-10 16:20:44 +0000

    Partial update (handled type-output and line-ends).

--- TestSystem/messages/TestExceptions.hpp
+++ TestSystem/messages/TestExceptions.hpp
@@ -8,9 +8,11 @@
 #ifndef TESTEXCEPTIONSMESSAGES_jJbv448865RtFd
 #define TESTEXCEPTIONSMESSAGES_jJbv448865RtFd
 
+#include <string>
 #include <cassert>
 #include <tr1/memory>
 
+#include <Transitional/SystemSpecifics/Demangling.hpp>
 #include <Transitional/Messages/MessagesMain.hpp>
 #include <Transitional/Messages/LineHandling.hpp>
 #include <Transitional/Messages/Utilities/FileIdentification.hpp>
@@ -27,16 +29,16 @@ namespace OKlib {
         \class ErrorDescription
         \brief The unit to describe the point in call history leading to a test failure.
 
-        A message class; an object contains shared pointers to message objects for the file name,
-        the line number and the test-level (class ErrorDescription takes over ownership of these
-        message-object-pointers), a C string with name of the type of the test class, and an integer
-        for the nesting depth.
+        A message class; an object contains shared pointers to message objects for
+        the file name, the line number and the test-level (class ErrorDescription
+        takes over ownership of these message-object-pointers), a C string with
+        name of the type of the test class, and an integer for the nesting depth.
 
         \todo Provide different output-levels.
         \todo Provide different output-languages.
       */
 
-      struct ErrorDescription : ::OKlib::Messages::MessagesBase {
+      struct ErrorDescription : ::OKlib::Messages::MessagesPrePost {
         OKLIB_MESSAGES_PRINT
 
       private :
@@ -45,7 +47,7 @@ namespace OKlib {
 
         MessagePointer file;
         MessagePointer line;
-        const char* type_test_class;
+        std::string type_test_class;
         MessagePointer level_description;
         ::OKlib::TestSystem::depth_number_type depth;
 
@@ -60,13 +62,12 @@ namespace OKlib {
                          ) :
           file(file_),
           line(line_),
-          type_test_class(type_test_class_),
+          type_test_class(::OKlib::SystemSpecifics::Demangle()(type_test_class_)),
           level_description(level_description_),
           depth(depth_)
         {
           assert(file.get());
           assert(line.get());
-          assert(type_test_class);
           assert(level_description.get());
 
           file -> set_prefix(1);
@@ -77,9 +78,12 @@ namespace OKlib {
         void print(std::ostream& out, L<en_GB>, S<Basic>) const {
           assert(file.get());
           assert(line.get());
-          assert(type_test_class);
           assert(level_description.get());
-          out << *file << "\n" << *line << "\n test type = " << type_test_class << "\n" << *level_description << "\n test depth = " << depth << "\n";
+          l_start(out) << *file; l_end(out);
+          l_start(out) << *line; l_end(out);
+          l_start(out) << " test class = " << type_test_class; l_end(out);
+          l_start(out) << *level_description; l_end(out);
+          l_start(out) << " test depth = " << depth; l_end(out);
         }
       
       };
