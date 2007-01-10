986988f9f32e351e7b775975e17fb633d5ec4e80
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-10 15:03:12 +0000

    Correction.

--- Buildsystem/makefile_generic_include/documentation/plans/documentation_index.mak.hpp
+++ Buildsystem/makefile_generic_include/documentation/plans/documentation_index.mak.hpp
@@ -1,7 +1,7 @@
 // Matthew Henderson, 14.11.2006 (Swansea)
 
 /*!
-  \file Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation_index.hpp
+  \file Buildsystem/makefile_generic_include/documentation/plans/documentation_index.mak.hpp
   \brief Plans for the documentation index makefile
 
   \todo Different organisation:
