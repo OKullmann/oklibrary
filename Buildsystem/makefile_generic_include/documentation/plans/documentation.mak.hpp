dc052f6123902b7c1922cb06c1298b0662fcc168
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-10 15:03:32 +0000

    Corrections.

--- Buildsystem/makefile_generic_include/documentation/plans/documentation.mak.hpp
+++ Buildsystem/makefile_generic_include/documentation/plans/documentation.mak.hpp
@@ -1,6 +1,6 @@
 // Matthew Henderson, 14.11.2006 (Swansea)
 
 /*!
-  \file Buildsystem/makefile_generic_include/documentation/plans/makefile_documentation.hpp
+  \file Buildsystem/makefile_generic_include/documentation/plans/documentation.mak.hpp
   \brief Plans for the documentation makefile
 */
