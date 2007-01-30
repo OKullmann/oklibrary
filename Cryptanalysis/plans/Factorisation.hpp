b84cd20aabd2128c6a572bb401550b1a2b6f1123
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-01-30 17:25:54 +0000

    Some ideas added.

--- Cryptanalysis/plans/Factorisation.hpp
+++ Cryptanalysis/plans/Factorisation.hpp
@@ -26,14 +26,31 @@
    <li> Constraints in general:
     <ol>
      <li> The next thing is to use a representation as an alliance of
-          active clause-sets, using the school method, but for an
-          arbitrary basis b. Active clause-sets for addition and
-          multiplication of digits seem most natural. In this way
-          we can optimise on b.
+          (strong) active clause-sets, using the school method, but for an
+          arbitrary basis b, which gives the domain size for the variables.
+          In this way we can optimise on b.<p></p>
+
+          (Strong) Active clause-sets for addition and
+          multiplication of digits seem most natural. A further parameter
+          could be the number k of digits considered as one block (or should
+          this be handled by using a larger b? seems so!).<p></p>
+
+          The implementation of strong
+          active clause-sets likely utilises (large) tables, and possibly
+          finite automata. From constraints a+b=c we might get equations,
+          so we need them as literals; from constraints a*b=c we could also
+          get for example a=c/b, and the question is whether we should handle
+          also such functional dependencies.
      </li>
-     <li> Likely the more complicated algorithms considered for Boolean
-          CNF allow a nicer representation using constraints --- of course
+     <li> Likely the more complicated multiplications algorithms
+          allow a nicer representation using constraints --- of course
           the challenge is to find good constraints!
+     </li>
+     <li> We can also investigate different representations of numbers (not
+          using the decimal expansion).
+     </li>
+     <li> And from methods for factorisation perhaps we could infer
+          additional constraints (at least).
      </li>
     </ol>
    </li>
