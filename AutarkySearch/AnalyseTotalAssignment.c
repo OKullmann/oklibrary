37a89f0a50476f905fe3e4026106c9ae0fe2a6d9
Matthew Lewsey <csmatthewl@Swansea.ac.uk>
2007-02-02 17:22:02 +0000

    AnalyseTotalAssignment.c now finds largest autarky given a total assignment.

--- AutarkySearch/AnalyseTotalAssignment.c
+++ AutarkySearch/AnalyseTotalAssignment.c
@@ -1,5 +1,8 @@
-// Who created this file, and when?
+//Matthew Lewsey, 2.2.2007 (Swansea)
 
+#include "ubcsat.h"
+#include <stdio.h>
+ 
 /*!
   \file AutarkySearch/AnalyseTotalAssignment.c
   \brief Supplementing UBCSAT by analysing total assignments for
@@ -11,157 +14,130 @@
   loop variables etc.).
 */
 
-#include <stdio.h>
-
-#include <ubcsat.h>
- 
-
 void Initialise();
 void AnalyseTotalAssignment();
 
 void Initialise()
 {
-  // call the relevant UBCSAT library procedures to detach this function from being inside the UBCSAT system
-  UINT32 i= 1;
+  //call the relevant UBCSAT library procedures to detach this function from being inside the UBCSAT system
   void ReadCNF();
   ReadCNF();
-  void CreateLitOccurence(); // allows useage of aNumLitOcc, pLitClause
-  CreateLitOccurence();
-  void CreateCandidateList(); // allows useage of aCandidateList
-  CreateCandidateList();
-  void CreateDefaultStateInfo(); // allows the usage of aVarValue array once initialised
-  CreateDefaultStateInfo();
-  void CreateFalseClauseList(); // allows useage of the new false clause list after variable flip
-  CreateFalseClauseList();
+  void CreatLitOccurence();
+  CreateLitOccurence(); //allows useage of aNumLitOcc, pLitClause
+  void CreateCandidateList();
+  CreateCandidateList(); //allows useage of aCandidateList
+  void CreateDefaultStateInfo();
+  CreateDefaultStateInfo(); //allows the usage of aVarValue array once initialised
+  void CreateFalseClauseList();
+  CreateFalseClauseList(); //allows useage of the new false clause list after variable flip
 
-  // UBCSAT uses aVarInit to determine how variable i is to be assigned (2 = random)
-  while(i <= iNumVars) {
-    aVarInit[i++] = 2;
+  //UBCSAT uses aVarInit to determine how variable i is to be assigned (2 = random)
+  for(UINT32 i=1;i<=iNumVars;i++) {
+    aVarInit[i] = 2;
   } 
 
   void DefaultInitVars();
-  DefaultInitVars(); // initially assigns ALL variables randomnly (randomnly due to above loop)
-  void InitDefaultStateInfo();
-  InitDefaultStateInfo(); // necessary to get aNumTrueLit working
+  DefaultInitVars(); //initially assigns ALL variables randomnly (randomnly due to above loop)
+  void DefaultStateInto();
+  InitDefaultStateInfo(); //necessary to get aNumTrueLit working
   void InitFalseClauseList();
-  InitFalseClauseList(); // necessary to get aFalseList working
+  InitFalseClauseList(); //necessary to get aFalseList working
 }
 
 
 void AnalyseTotalAssignment()
-{
-  BOOL bRemoveVars = FALSE, bNegLitSat, bPosLitSat, bOneLitTrue = FALSE;
-  BOOL *aCurrAutarky;
-  BOOL *aTmpCurrAutarky;
-  UINT32 iiVar, iiVarLitValue, iiFlipVarLitValue, iCurrClauseNum, iCurrVar, iCurrVarLitValue, iCurrFlipVarLitValue, iCurrLit, iCurrAutarkySize;
-  UINT32 *aLitClauses;
-  UINT32 i, j, k, m;
 
-  sFilenameIn = "sample.cnf";
-  Initialise();
+{
+  BOOL bOneLitTrue = FALSE, bNotDone = TRUE;
+  BOOL *aCurrAutarky;
+  UINT32 *aStack, iStackSize, iCurrStackVar;
+  UINT32 iCurrClauseNum, iCurrVar, iCurrLit,  iCurrFlipVarLitValue, iCurrAutarkySize, iClauseLen;
+  UINT32 *aLitClauses;
 
   aCurrAutarky = (BOOL *)malloc((iNumVars+1) * sizeof(BOOL));
-  for(i=1;i<=iNumVars;i++) {
-    iiVar = i;
-    for(j=1;j<=iNumVars;j++) {
+  iStackSize = 0;
+  aStack = (UINT32 *)malloc((iNumVars) * sizeof(UINT32));
+
+    for(UINT32 j=0;j<=iNumVars;j++) {
       aCurrAutarky[j] = TRUE;
     }
 
-    iiVarLitValue = aVarValue[i] ? i*2 : (i*2) + 1; 
-    iiFlipVarLitValue = aVarValue[i] ? iiVarLitValue + 1  : iiVarLitValue - 1;
-    aLitClauses = pLitClause[iiVarLitValue];
-
-    for(j=0;j<aNumLitOcc[iiVarLitValue];j++) {
-      if(aNumTrueLit[aLitClauses[j]] == 1) {
-        for(k=0;k<aClauseLen[aLitClauses[j]];k++) {
-          iCurrVar = pClauseLits[aLitClauses[j]][k] / 2;
-          aCurrAutarky[iCurrVar] = FALSE;  
-        }
-      }
-    }
-    for(j=0;j<iNumFalse;j++) {
+    for(UINT32 j=0;j<iNumFalse;j++) {
       iCurrClauseNum = aFalseList[j];
-      bRemoveVars = TRUE;
-      aTmpCurrAutarky = aCurrAutarky;    
-      for(k=0;k<aClauseLen[iCurrClauseNum];k++) {
-        iCurrVar = pClauseLits[iCurrClauseNum][k] / 2;
-        if(iCurrVar == iiVar) {
-          bRemoveVars = FALSE;
-          break;
-        } else {
-          aTmpCurrAutarky[iCurrVar] = FALSE;
-        }             
+      iClauseLen = aClauseLen[iCurrClauseNum];
+      for(UINT32 k=0;k<iClauseLen;k++) {
+	iCurrVar = pClauseLits[iCurrClauseNum][k-aClauseLen[iCurrClauseNum]] / 2;
+	aCurrAutarky[iCurrVar] = FALSE;
       }
-      if(bRemoveVars) aCurrAutarky = aTmpCurrAutarky;
     }
-    // this loop needs to be tested, above it works
-    for(j=1;j<=iNumVars;j++) {
-      if(aCurrAutarky[j]) {
-        if(j == iiVar) {
-	  iCurrVarLitValue = iiFlipVarLitValue;
-          iCurrFlipVarLitValue = aVarValue[j] ? j*2 : (j*2)+1;
-	} else {
-	  iCurrVarLitValue = aVarValue[j] ? j*2 : (j*2)+1;
-          iCurrFlipVarLitValue = aVarValue[j] ? (j*2)+1 : j*2;
+
+    for(UINT32 j=1;j<=iNumVars;j++) {
+      if(aCurrAutarky[j] == FALSE) continue;
+      iCurrFlipVarLitValue = aVarValue[j] ? (j*2)+1 : j*2;
+      aLitClauses = pLitClause[iCurrFlipVarLitValue];
+      for(UINT32 k=0;k<aNumLitOcc[iCurrFlipVarLitValue];k++) {
+	iCurrClauseNum = aLitClauses[k];
+	iClauseLen = aClauseLen[iCurrClauseNum];
+	for(UINT32 m=0;m<iClauseLen*2;m++) {
+	  if(m >= iClauseLen){
+	    iCurrVar = pClauseLits[iCurrClauseNum][m - iClauseLen] / 2;
+	    if(aCurrAutarky[iCurrVar]) {
+	      aStack[iStackSize] = iCurrVar;
+	      iStackSize++;
+	      aCurrAutarky[iCurrVar] = FALSE;
+	    }
+	  } else {
+	    iCurrLit = pClauseLits[iCurrClauseNum][m];
+	    iCurrVar = iCurrLit / 2;
+	    bOneLitTrue = aCurrAutarky[iCurrVar] && ((aVarValue[iCurrVar] && iCurrLit % 2 == 0)  || (!aVarValue[iCurrVar] && iCurrLit % 2 == 1 ));
+	    if(bOneLitTrue) break;
+	  }
 	}
-        aLitClauses = pLitClause[iCurrFlipVarLitValue];
-        for(k=0;k < aNumLitOcc[iCurrFlipVarLitValue];k++) {
-          bOneLitTrue = FALSE;
-          aTmpCurrAutarky = aCurrAutarky;
-          for(m=0;m < aClauseLen[aLitClauses[k]];m++) {
-            iCurrLit = pClauseLits[aLitClauses[k]][m];
-            iCurrVar = iCurrLit / 2;
-            aTmpCurrAutarky[iCurrVar] = FALSE;
-            if(iCurrVar == iiVar) {
-               bNegLitSat = aVarValue[iCurrVar] && (iCurrLit == (iCurrVar*2)+1);
-               bPosLitSat = (!aVarValue[iCurrVar]) && (iCurrLit == iCurrVar*2);
-            } else {
-              bNegLitSat = (!aVarValue[iCurrVar]) && (iCurrLit == (iCurrVar*2)+1);
-              bPosLitSat = aVarValue[iCurrVar] && (iCurrLit == iCurrVar*2);
-            }
-            bOneLitTrue = aCurrAutarky[iCurrVar] && (bNegLitSat || bPosLitSat) ? TRUE : FALSE;
-            if(bOneLitTrue)  break;  
-          }
-          if(!bOneLitTrue) aCurrAutarky = aTmpCurrAutarky;
-        } 
+	if(!bOneLitTrue) { 
+	  while(bNotDone) {
+	    iCurrStackVar = aStack[iStackSize - 1];
+	    iStackSize--;
+	    iCurrFlipVarLitValue = aVarValue[iCurrStackVar] ? (j*2)+1 : j*2;
+	    aLitClauses = pLitClause[iCurrFlipVarLitValue];
+	    for(UINT32 m=0;m<aNumLitOcc[iCurrFlipVarLitValue];m++) {
+	      iCurrClauseNum = aLitClauses[m];
+	      iClauseLen = aClauseLen[iCurrClauseNum];
+	      for(UINT32 n=0;n<iClauseLen;n++) {
+		iCurrLit = pClauseLits[iCurrClauseNum][n];
+		iCurrVar = iCurrLit / 2;
+		if(aCurrAutarky[iCurrVar] == TRUE) {
+		  aStack[iStackSize] = iCurrVar;
+		  iStackSize++;
+		  aCurrAutarky[iCurrVar] = FALSE;
+		}
+	      }
+	    }
+	    bNotDone = iStackSize > 0;
+	  }
+	}
       }
     }
+
     iCurrAutarkySize = 0;
-    for(j = 1;j <= iNumVars;j++) {
+    for(UINT32 j = 1;j <= iNumVars;j++) {
       if(aCurrAutarky[j]) iCurrAutarkySize++;
     }
-    if(i == 1) {
-      iBestScore = iCurrAutarkySize;
-      iNumCandidates = 1;
-      aCandidateList[0] = i;
-    } else {
-      if(iCurrAutarkySize <= iBestScore) {
-        if(iCurrAutarkySize == iBestScore) {
-          aCandidateList[iNumCandidates++] = i;
-        } else {
-          iBestScore = iCurrAutarkySize;
-          iNumCandidates = 1;
-          aCandidateList[0] = i;
-        }
-      }
-    }
-  }
-  if (iNumCandidates > 1) {
-    iFlipCandidate = aCandidateList[RandomInt(iNumCandidates)];
-  } else {
-    iFlipCandidate = aCandidateList[0];
-  }
+    printf("iCurrAutarkySize=%d\n",iCurrAutarkySize);
+ 
   void DefaultFlip();
   DefaultFlip();
   void CheckTermination();
   CheckTermination();
-  // Output function needs to be added
+  if(bSolutionFound) printf("solution found\n"); else printf("solution not found yet\n");
+  //Output function needs to be added
   void UpdateFalseClauseList();
   UpdateFalseClauseList();
 }
 
-
-int main() {
+int main() 
+{
+  sFilenameIn = "sample.cnf";
   Initialise();
   AnalyseTotalAssignment();
 }
+
