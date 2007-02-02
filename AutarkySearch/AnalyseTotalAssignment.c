e635b29aca07762dbaacd78c234b837a37eb1b36
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-02-02 20:43:31 +0000

    Most obvious editorial corrections, and bug-report (program unusable). The most important thing is to move the code to (proper) C++; in the current state the code is not testable (because there are no components). And the doxygen documentation needs to be established.

--- AutarkySearch/AnalyseTotalAssignment.c
+++ AutarkySearch/AnalyseTotalAssignment.c
@@ -1,4 +1,4 @@
-//Matthew Lewsey, 2.2.2007 (Swansea)
+// Matthew Lewsey, 2.2.2007 (Swansea)
 
 #include "ubcsat.h"
 #include <stdio.h>
@@ -7,6 +7,15 @@
   \file AutarkySearch/AnalyseTotalAssignment.c
   \brief Supplementing UBCSAT by analysing total assignments for
   contained autarkies.
+
+  \bug Program is faulty, and produces a segmentation fault for simple
+  3-CNF input. Valgrind-output:
+
+==32686== Conditional jump or move depends on uninitialised value(s)
+==32686==    at 0x40727A: ReadCNF (in /home/csoliver/SAT-Algorithmen/OKplatform/system_directories/bin/AnalyseTotalAssignment)
+==32686==    by 0x400FD9: Initialise (AnalyseTotalAssignment.c:33)
+==32686==    by 0x4016D5: main (AnalyseTotalAssignment.c:147)
+
 
   \todo Add file creation date (first file line).
   \todo Create Doxygen documentation.
@@ -19,29 +28,28 @@ void AnalyseTotalAssignment();
 
 void Initialise()
 {
-  //call the relevant UBCSAT library procedures to detach this function from being inside the UBCSAT system
+  // call the relevant UBCSAT library procedures to detach this function from being inside the UBCSAT system
   void ReadCNF();
   ReadCNF();
-  void CreatLitOccurence();
-  CreateLitOccurence(); //allows useage of aNumLitOcc, pLitClause
+  void CreateLitOccurence();
+  CreateLitOccurence(); // allows useage of aNumLitOcc, pLitClause
   void CreateCandidateList();
-  CreateCandidateList(); //allows useage of aCandidateList
+  CreateCandidateList(); // allows useage of aCandidateList
   void CreateDefaultStateInfo();
-  CreateDefaultStateInfo(); //allows the usage of aVarValue array once initialised
+  CreateDefaultStateInfo(); // allows the usage of aVarValue array once initialised
   void CreateFalseClauseList();
-  CreateFalseClauseList(); //allows useage of the new false clause list after variable flip
+  CreateFalseClauseList(); // allows useage of the new false clause list after variable flip
 
-  //UBCSAT uses aVarInit to determine how variable i is to be assigned (2 = random)
-  for(UINT32 i=1;i<=iNumVars;i++) {
+  // UBCSAT uses aVarInit to determine how variable i is to be assigned (2 = random)
+  for(UINT32 i=1; i <= iNumVars; ++i)
     aVarInit[i] = 2;
-  } 
 
   void DefaultInitVars();
-  DefaultInitVars(); //initially assigns ALL variables randomnly (randomnly due to above loop)
-  void DefaultStateInto();
-  InitDefaultStateInfo(); //necessary to get aNumTrueLit working
+  DefaultInitVars(); // initially assigns ALL variables randomnly (randomnly due to above loop)
+  void InitDefaultStateInfo();
+  InitDefaultStateInfo(); // necessary to get aNumTrueLit working
   void InitFalseClauseList();
-  InitFalseClauseList(); //necessary to get aFalseList working
+  InitFalseClauseList(); // necessary to get aFalseList working
 }
 
 
@@ -58,27 +66,26 @@ void AnalyseTotalAssignment()
   iStackSize = 0;
   aStack = (UINT32 *)malloc((iNumVars) * sizeof(UINT32));
 
-    for(UINT32 j=0;j<=iNumVars;j++) {
+    for(UINT32 j=0; j <= iNumVars; ++j)
       aCurrAutarky[j] = TRUE;
-    }
 
-    for(UINT32 j=0;j<iNumFalse;j++) {
+    for(UINT32 j=0; j < iNumFalse; ++j) {
       iCurrClauseNum = aFalseList[j];
       iClauseLen = aClauseLen[iCurrClauseNum];
-      for(UINT32 k=0;k<iClauseLen;k++) {
+      for(UINT32 k=0; k < iClauseLen; ++k) {
 	iCurrVar = pClauseLits[iCurrClauseNum][k-aClauseLen[iCurrClauseNum]] / 2;
 	aCurrAutarky[iCurrVar] = FALSE;
       }
     }
 
-    for(UINT32 j=1;j<=iNumVars;j++) {
+    for(UINT32 j=1; j <= iNumVars; ++j) {
       if(aCurrAutarky[j] == FALSE) continue;
       iCurrFlipVarLitValue = aVarValue[j] ? (j*2)+1 : j*2;
       aLitClauses = pLitClause[iCurrFlipVarLitValue];
-      for(UINT32 k=0;k<aNumLitOcc[iCurrFlipVarLitValue];k++) {
+      for(UINT32 k=0; k < aNumLitOcc[iCurrFlipVarLitValue]; ++k) {
 	iCurrClauseNum = aLitClauses[k];
 	iClauseLen = aClauseLen[iCurrClauseNum];
-	for(UINT32 m=0;m<iClauseLen*2;m++) {
+	for(UINT32 m=0; m < iClauseLen*2; ++m) {
 	  if(m >= iClauseLen){
 	    iCurrVar = pClauseLits[iCurrClauseNum][m - iClauseLen] / 2;
 	    if(aCurrAutarky[iCurrVar]) {
@@ -93,21 +100,21 @@ void AnalyseTotalAssignment()
 	    if(bOneLitTrue) break;
 	  }
 	}
-	if(!bOneLitTrue) { 
+	if(! bOneLitTrue) { 
 	  while(bNotDone) {
 	    iCurrStackVar = aStack[iStackSize - 1];
-	    iStackSize--;
+	    --iStackSize;
 	    iCurrFlipVarLitValue = aVarValue[iCurrStackVar] ? (j*2)+1 : j*2;
 	    aLitClauses = pLitClause[iCurrFlipVarLitValue];
-	    for(UINT32 m=0;m<aNumLitOcc[iCurrFlipVarLitValue];m++) {
+	    for(UINT32 m=0; m < aNumLitOcc[iCurrFlipVarLitValue]; ++m) {
 	      iCurrClauseNum = aLitClauses[m];
 	      iClauseLen = aClauseLen[iCurrClauseNum];
-	      for(UINT32 n=0;n<iClauseLen;n++) {
+	      for(UINT32 n=0; n < iClauseLen; ++n) {
 		iCurrLit = pClauseLits[iCurrClauseNum][n];
 		iCurrVar = iCurrLit / 2;
 		if(aCurrAutarky[iCurrVar] == TRUE) {
 		  aStack[iStackSize] = iCurrVar;
-		  iStackSize++;
+		  ++iStackSize;
 		  aCurrAutarky[iCurrVar] = FALSE;
 		}
 	      }
@@ -119,17 +126,17 @@ void AnalyseTotalAssignment()
     }
 
     iCurrAutarkySize = 0;
-    for(UINT32 j = 1;j <= iNumVars;j++) {
-      if(aCurrAutarky[j]) iCurrAutarkySize++;
+    for(UINT32 j = 1; j <= iNumVars; ++j) {
+      if(aCurrAutarky[j]) ++iCurrAutarkySize;
     }
-    printf("iCurrAutarkySize=%d\n",iCurrAutarkySize);
+    printf("iCurrAutarkySize=%lu\n",iCurrAutarkySize);
  
   void DefaultFlip();
   DefaultFlip();
   void CheckTermination();
   CheckTermination();
   if(bSolutionFound) printf("solution found\n"); else printf("solution not found yet\n");
-  //Output function needs to be added
+  // Output function needs to be added
   void UpdateFalseClauseList();
   UpdateFalseClauseList();
 }
