79d338ea12eea8a9cfb2cb29449c0b7a17769aab
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-02-06 18:36:33 +0000

    New todo about documentation.

--- TestSystem/plans/TestSystem.hpp
+++ TestSystem/plans/TestSystem.hpp
@@ -4,6 +4,11 @@
   \file TestSystem/plans/TestSystem.hpp
   \brief Plans for the test system (as a whole)
 
+  \todo Documentation:
+   - especially the division of labour between the three streams,
+     and how the log-facilities work at the different verbosity-levels
+     need to be explained.
+
   \todo Build system:
    - enable linking of only updated testobject-files
    - is it possible to make out of the testobject.o-files
