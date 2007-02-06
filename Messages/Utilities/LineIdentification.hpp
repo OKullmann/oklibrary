963036cbe3000c7ba51513f356245853b9bb50be
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-02-06 15:39:19 +0000

    Editorial changes, and macro OKLIB_LINE from TestSystem moved to here.

--- Messages/Utilities/LineIdentification.hpp
+++ Messages/Utilities/LineIdentification.hpp
@@ -2,11 +2,13 @@
 
 /*!
   \file Messages/Utilities/LineIdentification.hpp
-  \brief Message class to provide source-code line-number information
+  \brief Message class to provide source-code line-number information, plus supporting macro
 */
 
 #ifndef LINEIDENTIFICATION_HHcaoryGb384
 #define LINEIDENTIFICATION_HHcaoryGb384
+
+#include <string>
 
 #include <Transitional/Messages/MessagesMain.hpp>
 #include <Transitional/Messages/LineHandling.hpp>
@@ -24,23 +26,28 @@ namespace OKlib {
 
       struct LineIdentification : ::OKlib::Messages::MessagesPrePost {
         OKLIB_MESSAGES_PRINT
-
-        LineIdentification(
-                           std::string const line) :
-          line(line)
-        {}
-
+        LineIdentification(std::string const line) : line(line) {}
         void print(std::ostream& out, L<en_GB>, S<Basic>) const {
           l_start(out) << "line number = " << line;
         }
         void print(std::ostream& out, L<de_DE>, S<Basic>) const {
           l_start(out) << "Zeilennummer = " << line;
         }
-
       private :
-
         std::string line;
       };
+
+    /*!
+      \def OKLIB_LINE
+      \brief Macro to create a message object with the current line number
+
+      By OKLIB_LINE a pointer to a LineIdentification-object of dynamic storage creation is created.
+    */
+
+# define OKLIB_NUMBER(N) # N
+    //! putting quotes around the line number
+# define OKLIB_INTERMEDIATE_LINE(X) OKLIB_NUMBER(X)
+# define OKLIB_LINE new ::OKlib::Messages::Utilities::LineIdentification(OKLIB_INTERMEDIATE_LINE(__LINE__))
 
     }
   }
