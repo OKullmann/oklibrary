e8a7c4378af6d6ee2dee02ee3df192b76ee17265
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-03-15 22:43:27 +0000

    Plans for the new module on minimally unsatisfiable problems.

---
+++ MinUnsat/plans/MinUnsat.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 13.3.2007 (Swansea)
+
+/*!
+  \file MinUnsat/plans/MinUnsat.hpp
+  \brief Plans for the module on minimal unsatisfiability
+
+  \todo Generator-plans mentioned elsewhere, and relevant here
+  should move to here.
+
+  \todo SNS-matrices, Pfaffian graphs etc. (possibly this should
+  go to its own module on combinatorial autarkies?)
+  <ul>
+   <li> All the different formulations should be implemented
+     (that is, the respective reductions). </li>
+   <li> Get an overview on the existing poly-time SNS-matrix
+     regocnition algorithms, and implement them. </li>
+   <li> Implement the related SAT decision algorithms. </li>
+   <li> Implement the related MINUNSAT-algorithms. </li>
+  </ul>
+*/
+
+/*!
+  \namespace OKlib::MinUnsat
+  \brief Components for handling minimal unsatisfiability
+*/
+
+namespace OKlib {
+  namespace MinUnsat {
+  }
+}
+
