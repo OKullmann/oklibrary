2dfa0c87b9f4a1169b8267f90400aafbfea762b1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-03-29 18:47:06 +0000

    Doxygen-documentation added, and Test_ContainerConcept corrected (did not use the template parameter, but a fixed class (template) instead.

--- Concepts/std_Container_Tests.hpp
+++ Concepts/std_Container_Tests.hpp
@@ -1,5 +1,17 @@
 // Oliver Kullmann, 3.7.2005 (Swansea)
 
+/*!
+  \file Concepts/std_Container_Tests.hpp
+  \brief Generic tests related to container concepts from the standard.
+  \deprecated Move to the new test system.
+
+  \todo The only test available yet, Test_ContainerConcept, actually
+  is a meta-test (testing the concept), so this should go (with the
+  new test system, and once arbitrary directory-nesting is avaiable)
+  to Concepts/tests/tests.
+  \todo Write tests for Concepts::Container.
+*/
+
 #ifndef STDCONTAINERTESTS_bbvcC
 #define STDCONTAINERTESTS_bbvcC
 
@@ -16,22 +28,27 @@
 #include <Transitional/TestSystem/TestExceptions.hpp>
 
 namespace OKlib {
-
   namespace Concepts {
+
+    /*!
+      \class Test_ContainerConcept
+      \brief Test for a container concept by instantiating container-types
+      from the standard library.
+    */
 
     template <template <typename C> class ContainerConcept>
     class Test_ContainerConcept {
       void constraints() {
-        boost::function_requires<Container<std::vector<int> > >();
-        boost::function_requires<Container<std::vector<std::string> > >();
-        boost::function_requires<Container<std::list<int> > >();
-        boost::function_requires<Container<std::list<std::string> > >();
-        boost::function_requires<Container<std::deque<int> > >();
-        boost::function_requires<Container<std::deque<std::string> > >();
-        boost::function_requires<Container<std::set<int> > >();
-        boost::function_requires<Container<std::set<std::string> > >();
-        boost::function_requires<Container<std::multiset<int> > >();
-        boost::function_requires<Container<std::multiset<std::string> > >();
+        boost::function_requires<ContainerConcept<std::vector<int> > >();
+        boost::function_requires<ContainerConcept<std::vector<std::string> > >();
+        boost::function_requires<ContainerConcept<std::list<int> > >();
+        boost::function_requires<ContainerConcept<std::list<std::string> > >();
+        boost::function_requires<ContainerConcept<std::deque<int> > >();
+        boost::function_requires<ContainerConcept<std::deque<std::string> > >();
+        boost::function_requires<ContainerConcept<std::set<int> > >();
+        boost::function_requires<ContainerConcept<std::set<std::string> > >();
+        boost::function_requires<ContainerConcept<std::multiset<int> > >();
+        boost::function_requires<ContainerConcept<std::multiset<std::string> > >();
       }
     };
 
