53e850c8b5a6270237ed6a0c61bf9a4a56623583
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-03-29 18:50:28 +0000

    Yet only plans.

---
+++ InputOutput/tests/ClauseSetAdaptors.hpp
@@ -0,0 +1,13 @@
+// Oliver Kullmann, 29.3.2007 (Swansea)
+
+/*!
+  \file InputOutput/tests/ClauseSetAdaptors.hpp
+  \brief Tests for clause-set-adaptors
+
+  \todo Write generic tests for the concept (in module Concepts).
+
+  \todo Write test for InputOutput::Statistics.
+  \todo Write test for InputOutput::CLSAdaptorStatistics
+  \todo Write test for InputOutput::RawDimacsCLSAdaptor
+*/
+
