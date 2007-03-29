ced825fb7c0b0f8845378ac2e1f013dc265c49a9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-03-29 18:55:59 +0000

    Update, and new todos (about reaching version 0.1).

--- InputOutput/plans/InputOutput.hpp
+++ InputOutput/plans/InputOutput.hpp
@@ -4,16 +4,20 @@
   \file Transitional/InputOutput/plans/InputOutput.hpp
   \brief Plans for the module on SAT-related input/output.
 
-  \todo Update
+  \todo Update:
   Get an overview on the current state of the module, create new
   plans-files for the sub-modules, move the todo's to there, update
   the doxygen-documentation.
 
-   - InputOutput/ClauseSetAdaptors.hpp :
+   - InputOutput/ClauseSetAdaptors.hpp : DONE
    - InputOutput/Dimacs.hpp :
    - InputOutput/Exceptions.hpp :
 
   \todo Move to the new test system.
+
+  \todo Complete the tests.
+
+  \todo Complete the functionality.
 
   \todo Input and output of partial assignments.
 
