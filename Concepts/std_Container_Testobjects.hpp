6dac97c6eefe75d5e3b7455db2f3967a7fee435d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-03-29 18:45:05 +0000

    Namespace-qualifications added.

--- Concepts/std_Container_Testobjects.hpp
+++ Concepts/std_Container_Testobjects.hpp
@@ -9,13 +9,12 @@
 #include <Transitional/Concepts/std_Container_Tests.hpp>
 
 namespace OKlib {
-
   namespace Concepts {
 
-    OKLIB_BASIC_CONCEPT_TEST_TAG(Container)
-    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(Container)
+    OKLIB_BASIC_CONCEPT_TEST_TAG(OKlib::Concepts::Container)
+    OKLIB_BASIC_CONCEPT_TEST_ARCHETYPE(OKlib::Concepts::Container)
 
-    template class Test_ContainerConcept<Container>;
+    template class OKlib::Concepts::Test_ContainerConcept<OKlib::Concepts::Container>;
 
   }
 
