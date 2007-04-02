5d838325d13c7ec49a8ef37f10ac13bf82a132a7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-04-02 14:46:59 +0000

    New version 0.1.2.

--- Concepts/plans/milestones.hpp
+++ Concepts/plans/milestones.hpp
@@ -3,17 +3,18 @@
 /*!
   \file Concepts/plans/milestones.hpp
 
-  \module_version Concepts 0.1.1 (9.12.2006)
+  \module_version Concepts 0.1.2 (2.4.2007)
 
-  \par Version 0.1.2 :
+  \par Version 0.1.2 : REACHED.
   In Concepts/plans/Concepts.hpp the following topics are handled:
-  - Update
+  - Update : DONE
 
-  And further milestones are created.
+  And further milestones are created : DONE
 
   \par Version 0.1.3 :
   In Concepts/plans/Concepts.hpp the following topics are handled:
   - Split the module
+  - Cannibalise the approach from 200203
   - Develop concepts
   - Develop a general strategy for creating generic tests
 
@@ -36,6 +37,7 @@
 
    - 0.1   : 10.9.2006; initial version number (concepts are under work for around 2 years).
    - 0.1.1 : 9.12.2006; prototypes of concepts for variables, atomic conditions and literals developed.
+   - 0.1.2 : 2.4.2007; all concepts cleaned-up, so that the todos are relevant, and the doxygen-documentation is well-readable. Now concept-development can really start (at the planning level, while yet the generic test system is not ready).
 
 */
 
