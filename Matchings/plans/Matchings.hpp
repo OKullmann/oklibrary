da9fb7c31ee2f6d1105be2da1ae05af6add04ba8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-04-08 15:49:38 +0000

    Updates (mainly editorial).

--- Matchings/plans/Matchings.hpp
+++ Matchings/plans/Matchings.hpp
@@ -3,22 +3,34 @@
 /*!
   \file Matchings/plans/Matchings.hpp
 
-  \todo Literature and software review
+  \todo Correction:
+  Find the date in the cvs-logs for Matchings/plans/milestones.hpp
+  (originally the first version was 0.0.1, but this was changed to
+  0.0.2, since there were already some more plans there).
+
+  \todo Literature and software review on matching algorithms
 
   \todo Concepts for bipartite graphs are needed.
 
-  \todo First Implementation: As a first simple algorithm, a matching problem
-  is reduced to a network flow problem, so that the maximum
-  flow algorithms from Boost can be used.
+  \todo First implementations
+  <ul>
+   <li> As a first simple algorithm, a matching problem
+   is reduced to a network flow problem, so that the maximum
+   flow algorithms from Boost can be used. </li>
+   <li> Hungarian method: Implement the basic direct algorithm
+   for finding maximum matchings. </li>
+  </ul>
 
-  \todo Hungarian method: Implement the basic direct algorithm
-  for finding maximum matchings.
-
-  \todo Injectivity constraints:
-  For the 1-strong active clause-sets INJ(V,D) (see module
-  InjectitivyConstraints) an algorithm is needed for a bipartite graph
-  finding out the set of edges which are not element of some
-  maximum matching.
+  \todo Injectivity constraints
+  <ul>
+   <li> For the 1-strong active clause-sets INJ(V,D) (see
+   InjectivityConstraints/plans/InjectivityConstraints.hpp)
+   an algorithm is needed for a bipartite graph
+   finding out the set of edges which are not element of some
+   maximum matching. </li>
+   <li> Concepts are then needed to employ such algorithms for implementing
+   injectivity-constraints. </li>
+  </ul>
 
   \todo Online: We need also "online"-versions of these
   algorithms, allowing to remove some edges and recomputing
@@ -28,12 +40,23 @@
 
   \todo Counting:
   We need (exact) algorithms for counting of perfect matchings
-   - in bipartite graphs
-   - in graphs
-   - in general graphs
-   - in hypergraphs
-   - in general hypergraphs.
+  <ul>
+   <li> in bipartite graphs </li>
+   <li> in graphs </li>
+   <li> in general graphs </li>
+   <li> in hypergraphs </li>
+   <li> in general hypergraphs. </li>
+  </ul>
   Direct implementations as well as translations to #SAT problems.
+
+  \todo Surplus:
+   <ul>
+    <li> Efficient computations of the surplus of a bipartite graph. </li>
+    <li> Efficient decision whether the surplus is positive. </li>
+   </ul>
+   With these algorithms it can be decided whether a clause-set is
+   matching-lean, etc.; see MatchingAutarkies/plans/MatchingAutarkies.hpp.
+  
 */
 
 /*!
