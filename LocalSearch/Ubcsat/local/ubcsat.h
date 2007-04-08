5f77712d85e55d1a85ac7ccc05f2de61e6f2508f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-04-08 16:15:38 +0000

    Was before in AutarkySearch; improved include-guard, and doxygen-docu slightly extended.

---
+++ LocalSearch/Ubcsat/local/ubcsat.h
@@ -0,0 +1,54 @@
+/*!
+  \file LocalSearch/Ubcsat/ubcsat.h
+  \brief The fundamental header for of UBCSAT (now with
+  include-guard).
+*/
+
+/*
+
+      ##  ##  #####    #####   $$$$$   $$$$   $$$$$$    
+      ##  ##  ##  ##  ##      $$      $$  $$    $$      
+      ##  ##  #####   ##       $$$$   $$$$$$    $$      
+      ##  ##  ##  ##  ##          $$  $$  $$    $$      
+       ####   #####    #####  $$$$$   $$  $$    $$      
+  ======================================================
+  SLS SAT Solver from The University of British Columbia
+  ======================================================
+  ...Developed by Dave Tompkins (davet [@] cs.ubc.ca)...
+  ------------------------------------------------------
+  .......consult legal.txt for legal information........
+  ......consult revisions.txt for revision history......
+  ------------------------------------------------------
+  ... project website: http://www.satlib.org/ubcsat ....
+  ------------------------------------------------------
+  .....e-mail ubcsat-help [@] cs.ubc.ca for support.....
+  ------------------------------------------------------
+
+*/
+
+#ifndef UBCSAT_jBBgfrt
+#define UBCSAT_jBBgfrt
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ubcsat-limits.h"
+#include "ubcsat-types.h"
+
+#include "ubcsat-lit.h"
+#include "ubcsat-mem.h"
+#include "ubcsat-time.h"
+#include "ubcsat-io.h"
+#include "ubcsat-internal.h"
+#include "ubcsat-globals.h"
+#include "ubcsat-triggers.h"
+
+#include "algorithms.h"
+#include "reports.h"
+
+#include "mylocal.h"
+
+#endif
+
