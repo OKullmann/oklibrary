14ac137fbe75bc34f0fe56fbbe698a1fa1ecb12b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-08 14:05:38 +0000

    More remarks on the (not working) css-sheets; it seems easiest to replace all of this by just one fixed web page.

--- Buildsystem/Generic/documentation_building/plans/documentation_index.mak.hpp
+++ Buildsystem/Generic/documentation_building/plans/documentation_index.mak.hpp
@@ -9,13 +9,32 @@
   \todo Page organisation
    - On my laptop the columns are arranged in two rows (independent of the horizontal space
      available (a lot)). So something with the dimensions seems to be wrong.
+     OK: I changed some settings, so that it works now on my laptop, but
+     not the three columns with the links are not centered anymore on
+     cs-wsok (they appear left-aligned to the subtitle)??
+   - Using Mozilla or Firefox the arrangement is completely different (and
+     not as wished) ??
+   - What kind of style-sheet is used?? Syntax like "#content" I (OK) cannot
+     find in any documentation??
    - In documentation_index_style.css one finds hard-coded dimensions --- this doesn't look right.
+   - Documentation is needed (in the form of inline-comments for the css-file.
 
   \todo Using css:
-  Is it really worth it? The main page of the OKlibrary documentation likely will forevery just
-  stay one page, so the generality enabled by css will likely never be used. But it seems that
-  creating web pages is much more complicated in the css-way?
-  What tools exist to create easily such web pages?
+  <ul>
+   <li> Is it really worth it? The main page of the OKlibrary documentation
+   likely will forevery just stay one page, so the generality enabled by css
+   will likely never be used. But it seems that creating web pages is much
+   more complicated in the css-way?
+   What tools exist to create easily such web pages? And how to check them??
+   </li>
+   <li> Perhaps we have a few general style-elements, and the rest is
+   ordinary html?? </li>
+   <li> There is the general problem of how to create these web-pages
+   (based on make-variables): Perhaps asciidoc can help? </li>
+   <li> The question arises, why to create the index-page at all??
+   It would be much easier to just create a fixed (version-controlled)
+   web-page, which is adjusted accordingly?!! </li>
+  </ul>
 
   \todo Meta tags:
    - All the documentation should be available (finally) on the web; thus the index page will become
