928f0c0c04c70e78a559088872371cb411ced284
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-20 23:51:31 +0100

    Initial version 0.0.3; plans for explorations.
    Another topic is testing of the test-system.

---
+++ Parallelism/plans/milestones.hpp
@@ -0,0 +1,36 @@
+// Oliver Kullmann, 20.5.2007 (Swansea)
+
+/*!
+  \file Parallelism/plans/milestones.hpp
+  
+  \module_version Parallelism 0.0.3 (20.5.2007)
+
+  \par Version 0.0.4
+
+  \par
+   In Parallelism/plans/Parallelism.hpp the following topics are handled:
+    - New test system
+
+
+  \par Version 0.0.5
+
+  \par
+   In Parallelism/plans/Parallelism.hpp the following topics are handled:
+    - Investigate libraries for processes and threads
+
+
+  \par Version 0.1
+
+  \par
+   In Parallelism/plans/Parallelism.hpp the following topics are handled:
+    - Decide, how to handle parallel computations for the OKlibrary.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 20.5.2007; initial version number (the some experimental code).
+
+*/
+
