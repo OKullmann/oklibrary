d5732e875bd1b0dfb5f3cefc2d44953ea80dd8b2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-22 11:31:24 +0100

    Initial version 0.0.4 (several plans, and the experience with the (old) OKsolver)

---
+++ FailedLiteralReduction/plans/milestones.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 22.5.2007 (Swansea)
+
+/*!
+  \file FailedLiteralReduction/plans/milestones.hpp
+  
+  \module_version FailedLiteralReduction 0.0.4 (22.5.2007)
+
+  \par Version 0.0.5 :
+
+  \par
+   In FailedLiteralReduction/plans/FailedLiteralReduction.hpp the following topics are handled:
+    - Foundations
+
+  And new milestones have been created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 22.5.2007; initial version number (several plans, and the experience with the (old) OKsolver).
+
+*/
+
