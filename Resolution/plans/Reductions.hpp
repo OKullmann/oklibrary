438e59fe279827c173d8ee6a774c7648f7447840
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-22 11:39:00 +0100

    New submodule about resolution reductions.
    For example used for preprocessing.

---
+++ Resolution/plans/Reductions.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 22.5.2007 (Swansea)
+
+/*!
+  \file Resolution/plans/Reductions.hpp
+  \brief Plans for resolution reductions
+
+  Here we handle reductions which have to do with adding resolvents,
+  while the partial-assignment based approaches are handled in module
+  FailedLiteralReduction (see plans/FailedLiteralReduction.hpp) and,
+  for the general case, in module KLevelForcedAssignments (see
+  plans/KLevelForcedAssignments.hpp).
+
+  \todo Implement the reductions from the Kullmann/Luckhardt-preprints.
+
+  \todo Implement bounded resolution.
+
+  \todo All kinds of binary-clause reasoning are handled by module
+  FailedLiteralReduction, or?
+
+*/
+
