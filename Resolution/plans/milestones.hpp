550226f90069a33f19cb0961c9d3da5d35657ece
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-22 11:40:46 +0100

    Initial version 0.0.3 (some plans).

---
+++ Resolution/plans/milestones.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 22.5.2007 (Swansea)
+
+/*!
+  \file Resolution/plans/milestones.hpp
+  
+  \module_version Resolution 0.0.3 (22.5.2007)
+
+  \par Version 0.0.4 :
+
+  \par
+   In Resolution/plans/Resolution.hpp the following topics are handled:
+    - The basic problems
+
+  And new milestones have been created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 22.5.2007; initial version number (several plans).
+
+*/
+
