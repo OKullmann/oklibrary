a3ed66aeb1ca011c4632f9ddc3b3abd66eff0fc1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-05-25 18:21:00 +0100

    Plans for the mailing-lists.

---
+++ Buildsystem/plans/MailingLists.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 24.5.2007 (Swansea)
+
+/*!
+  \file Buildsystem/plans/MailingLists.hpp
+  \brief Plans for the mailing lists
+
+  \todo Mailing list
+  <ul>
+   <li> We need to set up an OKlibrary mailing list with web interface. </li>
+   <li> "Mailman" http://www.gnu.org/software/mailman/ seems to be a good choice ?! </li>
+   <li> To use Mailman, a web server is needed: Is Apache the best choice for us? (Perhaps we need the web server anyway for the OKplatform-Internet page?) </li>
+  </ul>
+
+*/
+
