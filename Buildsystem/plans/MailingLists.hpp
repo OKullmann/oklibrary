234f57aa4a3358cd9cbf6119c634724050080d95
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-04 15:27:31 +0100

    About creating the user mailing-list.

--- Buildsystem/plans/MailingLists.hpp
+++ Buildsystem/plans/MailingLists.hpp
@@ -4,11 +4,35 @@
   \file Buildsystem/plans/MailingLists.hpp
   \brief Plans for the mailing lists
 
-  \todo Mailing list
+  \todo Mailing list software
   <ul>
    <li> We need to set up an OKlibrary mailing list with web interface. </li>
    <li> "Mailman" http://www.gnu.org/software/mailman/ seems to be a good choice ?! </li>
    <li> To use Mailman, a web server is needed: Is Apache the best choice for us? (Perhaps we need the web server anyway for the OKplatform-Internet page?) </li>
+   <li> At least two lists: "Developers" and "Users"
+  </ul>
+
+  \todo User mailing list
+  <ul>
+  <li> Invite the participants:
+  <ul>
+   <li> Joshua Buresh-Oppenheim: Module Resolution </li>
+   <li> Stefan Szeider and Marko Samer: Module Backdoors </li>
+   <li> Hans van Maaren and Marijn Heule: General (?) </li>
+   <li> John Franco and Sean Weaver: General (?) </li>
+   <li> Ines Lynce and Joao Marques-Silva: General (?) </li>
+   <li> Daniel Le Berre (Beobachter) </li>
+   <li> Zumkeller: ?? </li>
+   <li> Arnold Beckmann: ?? </li>
+   <li> Xishun Zhal: ?? </li>
+   <li> Victor Marek and Mirek Truszczynski: ?? </li>
+  </ul>
+  </li>
+  <li> Only by invitation (yet). </li>
+  <li> How can we create special "views" for the users? They should be able to register
+  for modules or subjects, and then get commit-notifications related to those modules.
+  Sending them also the patches? Or the new files??
+  </li>
   </ul>
 
 */
