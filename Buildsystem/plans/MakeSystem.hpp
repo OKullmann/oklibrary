ce60a6a50e62fc9fda448856cc135df0f5e85626
Oliver Kullmann <csoliver@cs-wsok.swansea.ac.uk>
2007-06-04 21:26:01 +0100

    Plans for the make-specific part of the build system (the "engine").
    Yet the question is how to use cmake.

---
+++ Buildsystem/plans/MakeSystem.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 4.6.2007 (Swansea)
+
+/*!
+  \file Buildsystem/plans/MakeSystem.hpp
+  \brief Plans for the core part of the build system, the
+  make- or make-like engine
+
+  \todo CMake
+  <ul>
+   <li> Investigate how our main tasks can be handled by cmake. (It seems
+   to me a nice system, and likely we should do the transition.) </li>
+   <li> How would the work flow be with the new system (the 2 levels,
+   the cmake- and the make-level)? </li>
+   <li> Is ccmake useful? </li>
+  </ul>
+
+*/
+
