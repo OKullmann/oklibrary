5437c552cdd54fc021da66b2d8a30f61cbc126fe
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-06 11:42:07 +0100

    Plans regarding the whole library (which cannot assigned to a single module).

---
+++ plans/Transitional.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 6.6.2007 (Swansea)
+
+/*!
+  \file Transitional/plans/Transitional.hpp
+  \brief Plans for the Transitional-library which do not belong to a single module
+
+  \todo Upgrade to gcc-4.2.0
+  <ul>
+   <li> Correct the template-errors:
+    <ol>
+     <li> Precise problem description: XXX </li>
+     <li> Methods to overcome the problem: XXX </li>
+     <li> Files affected: XXX </li>
+    </ol>
+   </li>
+  </ul>
+
+*/
+
+/*!
+  \namespace OKlib
+  \brief All components of the OKlibrary
+*/
+
+namespace OKlib {
+}
+
+
+
+
+
