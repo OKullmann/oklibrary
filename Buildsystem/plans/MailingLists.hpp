efab55effdb8662709ecdc87b07d87a7b8b567b2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-06 11:38:29 +0100

    Updates.

--- Buildsystem/plans/MailingLists.hpp
+++ Buildsystem/plans/MailingLists.hpp
@@ -4,20 +4,23 @@
   \file Buildsystem/plans/MailingLists.hpp
   \brief Plans for the mailing lists
 
-  \todo Mailing list software
+  \todo Mailing list software : DONE (decided to use "Mailman")
   <ul>
    <li> We need to set up an OKlibrary mailing list with web interface. </li>
    <li> "Mailman" http://www.gnu.org/software/mailman/ seems to be a good choice ?! </li>
-   <li> To use Mailman, a web server is needed: Is Apache the best choice for us? (Perhaps we need the web server anyway for the OKplatform-Internet page?) </li>
-   <li> At least two lists: "Developers" and "Users"
+  </ul>
+
+  \todo Mailing lists:
+  <ul>
+   <li> At least two lists: "Developers" and "Users" </li>
   </ul>
 
   \todo User mailing list
   <ul>
   <li> Invite the participants:
   <ul>
-   <li> Joshua Buresh-Oppenheim: Module Resolution </li>
-   <li> Stefan Szeider and Marko Samer: Module Backdoors </li>
+   <li> Joshua Buresh-Oppenheim: module Resolution </li>
+   <li> Stefan Szeider and Marko Samer: new module Backdoors </li>
    <li> Hans van Maaren and Marijn Heule: General (?) </li>
    <li> John Franco and Sean Weaver: General (?) </li>
    <li> Ines Lynce and Joao Marques-Silva: General (?) </li>
@@ -26,6 +29,7 @@
    <li> Arnold Beckmann: ?? </li>
    <li> Xishun Zhal: ?? </li>
    <li> Victor Marek and Mirek Truszczynski: ?? </li>
+   <li> Ulrich Berger: New module Formalisation </li>
   </ul>
   </li>
   <li> Only by invitation (yet). </li>
