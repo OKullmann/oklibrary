b4215c24a072c765b273fa268252622ec7bc4b22
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-09 10:00:16 +0100

    Initial plans for applications.

--- Games/plans/Games.hpp
+++ Games/plans/Games.hpp
@@ -3,6 +3,26 @@
 /*!
   \file Games/plans/Games.hpp
   \brief Module for game playing and SAT
+
+  \todo What other games are interesting?
+  <ul>
+   <li> Go seems to be too "strategical". </li>
+   <li> "Nine men's morris" should be a solved game(?);
+   this could be alright for SAT. </li>
+   <li> Checkers should also be solved(?), and could pose
+   some problems (interesting) due to the jump sequences. </li>
+   <li> Backgammon perhaps involves too much special
+   knowledge. </li>
+   <li> "Tic, Tac, Toe" and generlisations could be a nice
+   exercise. </li>
+   <li> For Sudoku see module LatinSquares. </li>
+  </ul>
+
+  \todo Kind of clear, that with (generalised) SAT, also
+  with QBF, one does not get a "player", but only a
+  "problem solver" --- can this be a subprocedure for
+  a "player" ?
+
 */
 
 /*!
