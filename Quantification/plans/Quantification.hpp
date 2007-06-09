649a7b51726a7ee112f413b46a96c89ca4d2801c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-09 10:01:03 +0100

    Very first thoughts on QBF (and beyond).

---
+++ Quantification/plans/Quantification.hpp
@@ -0,0 +1,28 @@
+ 
+// Oliver Kullmann, 9.6.2007 (Swansea)
+
+/*!
+  \file Quantification/plans/Quantification.hpp
+  \brief Module for quantified boolean formulas and generalisations
+
+  \todo Main challenge are the concepts.
+
+  \todo What are the input formats? See SAT evaluation.
+
+  \todo Applications:
+  <ul>
+   <li> See sub-module Games/plans/Chess.hpp. </li>
+  </ul>
+
+*/
+
+/*!
+  \namespace OKlib::Quantification
+  \brief Components for quantified generalised SAT problems
+*/
+
+namespace OKlib {
+  namespace Quantification {
+  }
+}
+
