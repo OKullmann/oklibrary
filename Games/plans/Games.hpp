3e903777d698c1edcca751520e0e075090400793
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-09 02:39:43 +0100

    New module for (combinatorial) games.

---
+++ Games/plans/Games.hpp
@@ -0,0 +1,17 @@
+// Oliver Kullmann, 9.6.2007 (Swansea)
+
+/*!
+  \file Games/plans/Games.hpp
+  \brief Module for game playing and SAT
+*/
+
+/*!
+  \namespace OKlib::Games
+  \brief Components related to (combinatorial) games
+*/
+
+namespace OKlib {
+  namespace Games {
+  }
+}
+
