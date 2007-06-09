2d957b4fe02a282fc1003150064ab0cd719b86f4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-09 02:40:29 +0100

    Plans for handling chess.

---
+++ Games/plans/Chess.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 9.6.2007 (Swansea)
+
+/*!
+  \file Games/plans/Chess.hpp
+  \brief Submodule for chess playing and SAT
+
+  \todo The appropriate decision problem seems to be:
+  Given a position P and a natural number k (where the position
+  includes the party who is to move), does there exist a legal
+  sequence of k moves, so that finally the enemies king is taken?
+
+  \todo Encodings
+  <ul>
+  
+  </ul>
+  
+*/
+
