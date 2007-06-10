a3277f4884237b9ed5d7e8d591949dba36fb809a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-10 13:25:37 +0100

    New milestone 0.0.5.

--- HypergraphTransversals/plans/milestones.hpp
+++ HypergraphTransversals/plans/milestones.hpp
@@ -6,7 +6,19 @@
   \module_version HypergraphTransversals 0.0.3 (6.11.2006)
 
   \par Version 0.0.4 :
-  Create milestones.
+  Create milestones, and further plans.
+
+  \par Version 0.0.5 :
+
+  \par
+   In HypergraphTransversals/plans/HypergraphTransversals.hpp the following
+   topics are handled:
+    - Literature exploration
+
+  \par
+   In HypergraphTransversals/plans/DirectTransversalEnumeration.hpp the
+   following topics are handled:
+    - Concepts
 
   -------------------------------------------------------------------------------------------------------------------------------------
 
