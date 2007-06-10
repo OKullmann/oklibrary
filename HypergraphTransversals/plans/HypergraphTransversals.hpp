4ad852555a1aef1af07eec775b962ceecab1c6e0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-10 13:24:55 +0100

    Editorial update.

--- HypergraphTransversals/plans/HypergraphTransversals.hpp
+++ HypergraphTransversals/plans/HypergraphTransversals.hpp
@@ -4,21 +4,33 @@
   \file HypergraphTransversals/plans/HypergraphTransversals.hpp
   \brief Plans for the module on hypergraph transversals
 
-  \todo What is known meanwhile about the existence of polynomial
-  delay enumeration of Tr(G) for hypergraphs G? </li>
-
-  \todo For example Wahlstroem has an algorithm for enumerating
-  Tr(G) for 3-uniform G --- worth implementing? Likely one needs
-  to extract a heuristical generalisation.
-
-  \todo The dual problem to hypergraph transversal enumeration
-  is enumeration of all edge covers of a hypergraph ---
-  has this formulation advantages / disadvantages?
-
-  \todo The complementary problem (by hyperedge-complementation)
-  to hypergraph transversal enumeration is enumeration of all
-  independent sets --- again, has this formulation advantages /
-  disadvantages?
+  \todo Literature exploration:
+  <ul>
+   <li>What is known meanwhile about the existence of polynomial
+   delay enumeration of Tr(G) for hypergraphs G? </li>
+   <li> For example Wahlstroem has an algorithm for enumerating
+   Tr(G) for 3-uniform G --- worth implementing? Likely one needs
+   to extract a heuristical generalisation. </li>
+   <li> The dual problem to hypergraph transversal enumeration
+   is enumeration of all edge covers of a hypergraph ---
+   has this formulation advantages / disadvantages? </li>
+   <li> The complementary problem (by hyperedge-complementation)
+   to hypergraph transversal enumeration is enumeration of all
+   independent sets --- again, has this formulation advantages /
+   disadvantages? </li>
+   <li> The graph case:
+    <ul>
+    <li> Transversals for graphs are called "vertex covers"; are
+    there special algorithms? </li>
+    <li> Also the independent sets in graph deserve attention. </li>
+    <li> For graphs, finding a minimum edge cover can be done
+    in polynomial time by means of maximum matching; worth
+    investigating? Dualisation yields, that minimum transversals
+    can be computed for hypergraphs with vertex degrees at most
+    2. </li>
+    </ul>
+   </li>
+  </ul>
 
   \todo So the three problems
   <ol>
@@ -32,18 +44,6 @@
   actually perform the transformation, but provide a "view".
   Perhaps this should be supported by the hypergraph concepts
   (otherwise we have to handle expression templates).
-
-  \todo The graph case:
-  <ul>
-   <li> Transversals for graphs are called "vertex covers"; are
-   there special algorithms? </li>
-   <li> Also the independent sets in graph deserve attention. </li>
-   <li> For graphs, finding a minimum edge cover can be done
-   in polynomial time by means of maximum matching; worth
-   investigating? Dualisation yields, that minimum transversals
-   can be computed for hypergraphs with vertex degrees at most
-   2. </li>
-  </ul>
 
 */
 
