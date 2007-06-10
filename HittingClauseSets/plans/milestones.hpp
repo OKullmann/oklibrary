fe51bd3b49703c9004c4b847392f1081efc11fde
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-10 04:31:56 +0100

    Editorial.

--- HittingClauseSets/plans/milestones.hpp
+++ HittingClauseSets/plans/milestones.hpp
@@ -3,7 +3,7 @@
 /*!
   \file HittingClauseSets/plans/milestones.hpp
 
-  \par Current version 0.0.2 (6.11.2006; some plans)
+  \module_version HittingClauseSets 0.0.2 (6.11.2006)
 
   \par Version 0.0.3 :
   The first task is create milestones.
@@ -13,6 +13,6 @@
 
   \par Version history
 
-   -  0.1 : 6.11.2006; initial version number (some plans).
+   -  0.0.2 : 6.11.2006; initial version number (some plans).
 
 */
