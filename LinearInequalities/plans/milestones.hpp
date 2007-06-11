2c94f08677c9e571a978b264c4c3b41159ddb041
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-11 01:29:18 +0100

    *** Initial version number 0.0.4 (module LinearInequalities) ***

---
+++ LinearInequalities/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 11.6.2007 (Swansea)
+
+/*!
+  \file LinearInequalities/plans/milestones.hpp
+  
+  \module_version LinearInequalities 0.0.4 (11.6.2007)
+
+  \par Version 0.0.5
+
+   Create further milestones.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 11.6.2007; initial version number (several plans, and an (old) implementation in OKsolver/Experimental/Transformationen).
+
+*/
+
