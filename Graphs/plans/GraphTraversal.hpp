423f6d1a9ab2623127a68215a85b7d99927a5794
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-11 13:34:22 +0100

    Submodule for graph traversal (especially for the implementation of the algorithm from course-module CS-232).

---
+++ Graphs/plans/GraphTraversal.hpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 11.6.2007 (Swansea)
+
+/*!
+  \file Graphs/plans/GraphTraversal.hpp
+  \brief Plans for the sub-module on graph-traversal
+
+  \todo Implement the generic graph_traversal algorithm from
+  the CS-232 script (generic enough, and with enough event-points,
+  so that protocolling and visualisation are possible).
+
+*/
+
