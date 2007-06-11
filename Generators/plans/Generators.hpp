1b2ae8cab498c338b73450bbd7a41313b87f6697
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-11 01:21:00 +0100

    Editorial, and todo about Tseitin transformation to its own file.

--- Generators/plans/Generators.hpp
+++ Generators/plans/Generators.hpp
@@ -2,87 +2,19 @@
 
 /*!
   \file Generators/plans/Generators.hpp
-  \brief Plans for the module Generators (for creating SAT problems)
+  \brief Plans for the module %Generators (for creating SAT problems)
 
   \todo Transferring (and updating):
-   - OKsolver/Experimental/Transformationen/FaronsPuzzle
-   - OKsolver/Experimental/AllgKlassen200203/TransformationsBiclique
+  <ul>
+   <li> OKsolver/Experimental/Transformationen/FaronsPuzzle </li>
+   <li> OKsolver/Experimental/AllgKlassen200203/TransformationsBiclique </li>
+  </ul>
   The main generators should produce generalised clause-sets.
   If output and/or boolean formulas is needed, then further
   transformers are to be used.
   The variables are tuples of values.
 
-  \todo Implementing the Tseitin-transformation (with variations):
-  <ol>
-   <li>
-   Given a propositional formula F built from negation, conjunction
-   and disjunction (for simplicity we consider only binary operations
-   here), by the process specified below, a refinement
-   of the Tseitin-translation, we obtain a 3-CNF F' over the variables
-   from F plus new auxiliary variable such that
-
-   - every satisfying assignment for F can (easily) be extended
-     to a satisfying assignment for F';
-   - every satisfying assignment for F' is a satisfying assignment
-     for F (when restricted to the variables in F).
-
-   The process F -> F' works, using auxiliary variables v(G) for
-   subformulas G of F, by collecting clauses (to avoid renaming
-   of existing variables, let v(a) = a for variables a from F):
-
-   For a sub-formula G of F, where G is of the form
-     op G_1 ... G_n
-   (for us n <= 2), with op one of negation, conjunction or disjunction
-   (written for convenience in prefix form), add the clauses
-
-   i) (op v(G_1) ... v(G_n)) -> v(G)
-   ii) v(G) -> (op v(G_1) ... v(G_n))
-
-   where in case G occurs positively in F, i) can be dropped,
-   while if G occurs negatively in F, ii) can be dropped
-   (so the process might be non-deterministic).
-
-   F' is obtained by adding the unit clause {v(F)} to all these clauses.
-   </li>
-   <li> Is the following useful?
-
-   Let us denote by t(F) the result where always
-   one of (i), (ii) is dropped (appropriately).
-   A class of horn-like formulas is the class of formulas
-
-     F = C_1 and ... and C_k
-
-   where each C_i is of the form
-
-     (P_1 and ... and P_m) -> Q
-
-   where the P_i are arbitrary positive and-or-formulas, while
-   Q is a positive and-formula. So the P_i and all their sub-occurrences
-   occur negatively, while Q occurs positively.
-   Thus t(F) is a 3-CNF with (essentially) clauses of the form
-
-   1) (a or b) -> c
-   2) (a and b) -> c
-   3) a -> (b and c)
-
-   where a, b, c are variables (atomic formulas).
-   Types 1 and 3 translate into 2 binary Horn-clauses each, while type 2 is
-   already a Horn clause.
-
-   Thus t(F) is a Horn formula. Since t(F) is computable in linear time,
-   and SAT decision for Horn formulas is achievable in linear time,
-   SAT decision for those generalised formulas F is achievable in linear time,
-   </li>
-   <li> Is there a general input format for boolean formulas? </li>
-   <li> Some obvious variations on the transformation F -> F':
-    - handling of arbitrary and-or-arities
-    - performing simplifications on the clauses
-    - either using always (i)+(ii), or exactly one of them, or using some
-      heuristic.
-   </li>
-  </ol>
-
-  \todo And also we need active clause-sets for biclique-transformations for
+  \todo We need active clause-sets for biclique-transformations for
   interesting classes of graphs (including complete graphs).
 
   \todo The HKB-formulas (MUSAT_{delta=2}, where every literal occurs
@@ -92,16 +24,18 @@
   given an arbitrary graph and a set of vertices to be matched.
   Directly, and as active clause-sets.
 
-  \todo Generators for finding incidence structures and designs
+  \todo %Generators for finding incidence structures and designs:
   Naturally modelled by boolean variables I(p, b) ("I" like
   "incident", "p" like "point", "b" like "block").
   Searching for example for balanced designs.
   Very simple the search for projective planes of order n:
-   - n^2 + n + 1 p's and b's
-   - for all p <> p' there is exactly one b with I(p,b) and I(p',b)
-   - for all b <> b' there is exactly one p with I(p, b) and I(p, b')
-   - for all b there are two p, p' of p1,p2,p3,p4 (fixed variables!)
-     with not I(p, b) and not I(p', b).
+  <ul>
+   <li> n^2 + n + 1 p's and b's </li>
+   <li> for all p <> p' there is exactly one b with I(p,b) and I(p',b) </li>
+   <li> for all b <> b' there is exactly one p with I(p, b) and I(p, b') </li>
+   <li> for all b there are two p, p' of p1,p2,p3,p4 (fixed variables!) with not I(p, b)
+   and not I(p', b). </li>
+  </ul>
   Modelled directly or via active clause-sets (especially that every two points
   are on one line, and every two lines intersect in one point!).
   QUESTION OK: Where does this topic belong to? To module LatinSquares?
