2457545f5ea2477c132130e2b5882b1113a6c172
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-11 01:28:17 +0100

    *** Initial version number 0.0.5 (module Generators) ***

---
+++ Generators/plans/milestones.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 11.6.2007 (Swansea)
+
+/*!
+  \file Generators/plans/milestones.hpp
+  
+  \module_version Generators 0.0.5 (11.6.2007)
+
+  \par Version 0.0.6
+
+   Create further milestones.
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.5 : 11.6.2007; initial version number (several plans, and two (old) implementations in OKsolver/Experimental/Transformationen).
+
+*/
+
