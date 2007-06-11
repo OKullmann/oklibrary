2e7f63430d97667f38ffc95747f0dae1cb5b60d1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-11 11:13:01 +0100

    Part related to general configuration management moved to new file Configuration.hpp.

--- Buildsystem/plans/Internet.hpp
+++ Buildsystem/plans/Internet.hpp
@@ -6,44 +6,6 @@
 
   \todo Configuration problem:
   <ul>
-   <li> A central problem is: How to create a central repository
-   of configuration data, usable by the various web pages as well
-   as by the build system?!
-    <ol>
-     <li> Perhaps a new subdirectory Buildsystem/Configuration? </li>
-     <li> Inside we have a make-file containing only variable settings
-     \verbatim
-CONFIGVAR1=value1
-CONFIGVAR2=value2
-     \endverbatim
-     To be used by the C preprocessor, this has to be translated into
-     \verbatim
-#define CONFIGVAR1 value1
-#define CONFIGVAR2 value2
-     \endverbatim
-     This seems to work whatever value1 is (as long value1 does not contain
-     line breaks).
-     To be used by m4, this has to be translated into
-     \verbatim
-m4_define(CONFIGVAR1,value1)
-m4_define(CONFIGVAR2,value2)
-     \endverbatim
-     while for usage by cmake the translation is
-     \verbatim
-SET(CONFIGVAR1 value1)
-SET(CONFIGVAR2 value2)
-     \endverbatim
-     In both cases there might be problems if value1 for example contains
-     a closing bracket? Perhaps we could just use the export-facility of make,
-     so that these variables become environment variables?!? But then we would
-     loose control.
-     </li>
-     <li> If we are going to use CMake, then perhaps the variable settings should
-     be done in the cmake-syntax. </li>
-     <li> Splitting the definitions over several files is likely preferable from the
-     order point of view. Shouldn't be too complicated to use them. </li>
-    </ol>
-   </li>
    <li> We need a simple system which allows
    to add invisible variables to the html-pages, whose visible values
    are updated by running some program (which has the list of current
