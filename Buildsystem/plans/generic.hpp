3edf26a8fc82240de69311b0bd4f269cbfaf822a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-14 12:28:58 +0100

    Several updates, and todos moved to plans/TargetSpecifications.hpp.
    Also several editorial improvements.

--- Buildsystem/plans/generic.hpp
+++ Buildsystem/plans/generic.hpp
@@ -5,7 +5,7 @@
   \brief Plans for the generic makefile
 
   \bug Mixing C and C++:
-  If we have files XXX.c and XXX.cpp (with the same prefix) then
+  If we have files %XXX.c and XXX.cpp (with the same prefix) then
   the build-system apparently does not work properly.
   Either this gets corrected, or at least documented
   (it appears we should forbid this).
@@ -19,7 +19,7 @@
   \bug Incorrect build of new tests II:
   While error I is as expected (at this time), the buildsystem behaves
   differently when called within a module or from a higher level:
-   - Target new_check works from for example from level OKsystem w.r.t.
+   - Target new_check works from for example from level OKsystem %w.r.t.
      building test-object files if needed,
    - but from within the module this doesn't work.
 
@@ -32,21 +32,25 @@
       </ul>
       And others.
     </li>
-    <li>Targets
+    <li> Targets
       <ul>
        <li>
         The targets should be more precisely specified. For example, in the 
         documentation of the targets all, optimised and unoptimised we need 
-        to speak about compilation of *test*-programs!.
+        to speak about compilation of *test*-programs!
        </li>
       </ul>
     </li>
     <li> Specify the functions of the build system as precise as possible. </li>
   </ul>
 
-  \todo .source_libraries
+  \todo .source_libraries: DONE (moved to Buildsystem/plans/TargetSpecifications.hpp)
   - Like we have for link libraries, the specification of source libraries should
   be (potentially) given in a .source_libraries file.
+  - The (recursive) make-variable source_libraries is kept,
+  predefined as "$(OKSystem_include) $(Boost_include)" (potentially
+  changed in the locale makefile): If .source_libraries exists, then it
+  overrides $(source_libraries).
 
   \todo Setting the paths to GCC and Boost link libraries
     - The setting of the Make variable "alternative_library_path" needs to be reviewed.
@@ -62,7 +66,7 @@
   \todo General_options:
   The current function of "General_options" is taken over by the new variable "Debug_options".
 
-  \todo Dependency files:
+  \todo Dependency files: UPDATE once we know cmake better (it should solve such problems)
     - If when creating the .d-files an error occurs (for example
      due to an inaccessible header file), then for some reason subsequent "make check" erroneously succeeds.
     - Using the gcc option "-MP" ?
@@ -76,17 +80,17 @@
     - By default the make-process outputs it's basic assumptions (what compiler to be used, what
       external libraries, etc.).
 
-  \todo Directory structure:
+  \todo Directory structure: DONE (moved to Buildsystem/plans/TargetSpecifications.hpp)
     - A module can have arbitrary submodules (with capital names) for (only) .hpp-files, each
       with its own generic sub-directories "plans", "tests" and "messages". However only at the base level
       we have the sub-directories "testobjects", "applications" and "implementations", which contain (only)
-      .cpp (and .c) files. Every file in applications implements main(), and no other files. Compiled
+      .cpp (and .c) files. Every file in applications implements %main(), and no other files. Compiled
       testobject-files go to lib/tests/Module, compiled implementations go to lib, compiled test-programs go
       to bin/tests, compiled applications go to bin. Thus names for implementation files and application files
       need to be unique for the whole library, while names for testobject-files need to be unique within a module.
     - Or we allow more freedom --- seems to be needed!
 
-  \todo Test system:
+  \todo %Test system:
     - The most immediate thing is to make the test system work correctly, which (at least?)
       means that the make-system gets to know the relevant dependencies, so that recompilation
       of tests happens whenever necessary (at this time (18.6.2006) the test object files need
@@ -97,13 +101,13 @@
 
   \todo OKsystem/Buildsystem structure:
   <ul>
-  <li>Probably the directory Generic should be elimated and it's subdirectories lifted to OKsystem/Buildsystem?</li>
+  <li> Probably the directory Generic should be elimated and it's subdirectories lifted to OKsystem/Buildsystem?</li>
    <li>In Transitional/Buildsystem the two makefiles 
     <ul>
        <li>OKsystem/Buildystem/Generic/documentation_index.mak</li>
        <li>OKsystem/Buildystem/Generic/doxygen_documentation.mak</li>
      </ul>
-     should be merged into a single file  Transitional/Buildsystem/documentation_building.mak 
+     should be merged into a single file Transitional/Buildsystem/documentation_building.mak 
    </li>
    <li>
     All the plans files should be merged into a single documentation_building.hpp plans file
@@ -111,23 +115,21 @@
    </li>
   </ul> 
 
-  \todo Compilation:
+  \todo Compilation: DONE (moved to Buildsystem/plans/TargetSpecifications.hpp)
     - The names of the created .o-files and executables should reflect "all" compiler options.
     - There are generic links to the unoptimised and the optimised version (the latest).
       The test system uses these.
-    - For every created file.o and file we have file.compilation_log (in the same directory
+    - For every created "file.o" and "file" we have the "file.compilation_log" (in the same directory
       where these files go). Optionally we can switch it off.
     - There is a make-variable for optional name extensions.
 
-  \todo Customisation: 
-    - We keep our two-stages process: Every .cpp-files yields a .o-file, and finally exactly those .cpp-files with main()
+  \todo Customisation: DONE (moved to Buildsystem/plans/TargetSpecifications.hpp)
+    - We keep our two-stages process: Every .cpp-files yields a .o-file, and finally exactly those .cpp-files with %main()
       yield executables (the same for .c).
     - Every .cpp-file has a .source_libraries file (if needed), a .link_libraries and .link_libraries_optimised file (if needed),
       and a .compile_options and a .compile_options_optimised file (if needed).
-    - The link-libraries for a .cpp-file with main() are collected from the link-libraries of the .cpp-files linked to it,
+    - The link-libraries for a .cpp-file with %main() are collected from the link-libraries of the .cpp-files linked to it,
       and from the link-library-file for this .cpp-file itself.
-    - The (recursive) make-variable source_libraries is kept, predefined as "$(OKSystem_include) $(Boost_include)" (potentially
-      changed in the locale makefile): If .source_libraries exists, then it overrides $(source_libraries).
 
 
   \todo Log directory:
@@ -151,7 +153,7 @@
     - Cleaning of test-objectfiles and test-programs.
     - Cleaning of test-depencies.
 
-  \todo Test cleaning:
+  \todo %Test cleaning:
     We need specialised cleaning regarding the test system :
     - Cleaning of special or all versions of the test-timestamps.
     - Cleaning of test-objectfiles and test-programs.
@@ -160,7 +162,7 @@
   \todo Error messages of gcc should be processed:
     - We should support using a tool like TextFilt or STLFilt.
 
-  \todo Tools for testing:
+  \todo %Tools for testing: DONE (moved to Buildsystem/plans/TargetSpecifications.hpp)
     - valgrind has a (new) flag "--error-exitcode=", by which we should be able to get an error
       making the build-process stop in case valgrind finds an error.
     - We should use gcov from time to time (supported by the build system) to check
