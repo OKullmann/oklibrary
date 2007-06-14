c7f7894ea8783a21482067cffeb1e77d99df90f8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-14 12:22:16 +0100

    Extensions regarding principal questions, external sources and doxygen.

--- Buildsystem/plans/Configuration.hpp
+++ Buildsystem/plans/Configuration.hpp
@@ -4,6 +4,14 @@
   \file Buildsystem/plans/Configuration.hpp
   \brief Plans regarding configuration management
 
+  \todo Primary versus derived configurations
+  <ul>
+   <li> The primary configurations are under version control. </li>
+   <li> Derived values (like source- and link-libraries for external libraries)
+   are to be found perhaps in system_directories/configurations ? There might be also
+   a symbolic link to the primary configurations? </li>
+  </ul>
+
   \todo Configuration data
   <ul>
    <li> A central problem is: How to create a central repository
@@ -53,6 +61,54 @@ SET(CONFIGVAR2 value2)
   way). It must be possible to update the system (installing a new
   cmake, for example, or a new gcc), perhaps by setting variables
   about supported and recommended versions.
+
+  \todo external_sources_versions.mak: UPDATE needed (%w.r.t. the new view
+  of the configuration center)
+  <ul>
+   <li> The role of external_sources_versions.mak must
+   be clarified. </li>
+   <li> Names like
+   <code> doxygen_supported_not_recommended_version_numbers </code>
+   are misleading (these versions are not "not recommended"), and
+   they are too long. </li>
+  </ul>
+
+  \todo Make-variables for external libraries : UPDATE needed (%w.r.t. the new view
+  of the configuration center)
+  <ul>
+   <li> How to define variables like Boost and Ubcsat in general?
+     We need a nice little method (plus documentation), so that we can add easily as
+     many external libraries as we want. </li>
+   <li> And then we have make-variables "gcc_version_number" (for generic.mak) and
+     "gcc-version" (for boost.mak) which likely should have the same names. </li>
+   <li> In system_definitions.mak we have a definition of Boost which seems wrong
+     to me (OK) (or at least misleading). DONE (resp. moved to other todos) </li>
+  </ul>
+
+  \todo Definitions for doxygen:
+  <ul>
+   <li> The Doxyfile should be part of the (primary) configuration system. </li>
+   <li> Perhaps it would be preferable, if the following variable definitions
+    <ol>
+     <li> ALIASES </li>
+     <li> PREDEFINED </li>
+     <li> EXPAND_AS_DEFINED </li>
+     <li> FILE_PATTERNS </li>
+    </ol>
+    could come from a separate configuration file --- how to achieve this? </li>
+   <li> Allows the Doxyfile for includes or macro expansion? </li>
+   <li> Perhaps it's best to define a "master-doxyfile" in the primary configuration system,
+   containing macros or includes for the above four definitions, and then the build system
+   builds the finale Doxyfile (in the derived configurations) in dependency on these
+   includes/macro-definitions. (Alternatively, one can specify directly a doxyfile to
+   be used --- how to achieve this with cmake? In the normal case, target html depends
+   on the doxyfile to be created if necessary, but if some variable is specified on the
+   make command line, then the doxyfile is not used.) </li>
+   <li> With every new version of doxygen, the update-wizzard of doxygen has to be run
+   on the created doxyfile, one has to study the changes, and update the master-doxyfile
+   accordingly. </li>
+  </ul>
+
 
 */
 
