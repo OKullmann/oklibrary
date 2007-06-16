12618d8bd6197d95139d2f0105679e2adaf987de
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:10:01 +0100

    Plans about projections (for heuristics).

---
+++ Satisfiability/Heuristics/Projections/plans/general.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Satisfiability/Heuristics/Projections/plans/general.hpp
+  \brief Module for projections (making branching tuples comparable)
+
+*/
+
+/*!
+  \namespace OKlib::Satisfiability::Heuristics::Projections
+  \brief Components for projections
+
+  Alias "Prj".
+*/
+
+namespace OKlib {
+  namespace Satisfiability {
+    namespace Heuristics {
+      namespace Projections {
+      }
+    }
+  }
+  namespace SatHeuPrj = Satisfiability::Heuristics::Projections;
+}
+
