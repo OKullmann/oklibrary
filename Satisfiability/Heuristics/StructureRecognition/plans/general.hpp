50f805fefd05bb5659d83cb681a3e26810a6f1cb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:15:57 +0100

    Plans for structure recognition (to support SAT solving).

---
+++ Satisfiability/Heuristics/StructureRecognition/plans/general.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Satisfiability/Heuristics/StructureRecognition/plans/general.hpp
+  \brief Module for recognising structures in problem instances
+
+  \todo Move Transitional/Statistics/plans/RecogniseRandomFormulas.hpp
+  to here.
+
+*/
+
+/*!
+  \namespace OKlib::Satisfiability::Heuristics::StructureRecognition
+  \brief Components for structure recognition
+
+  Alias "Rec".
+*/
+
+namespace OKlib {
+  namespace Satisfiability {
+    namespace Heuristics {
+      namespace StructureRecognition {
+      }
+    }
+  }
+  namespace SatHeuRec = Satisfiability::Heuristics::StructureRecognition;
+}
+
