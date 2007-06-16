7de6249180c1937204ab76a4e30527b26c6a1e50
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:17:30 +0100

    Plans regarding the whole satisfiability-part of the OKlibrary.

---
+++ Satisfiability/plans/general.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Transitional/Satisfiability/plans/general.hpp
+  \brief Contains all super-modules regarding (generalised) satisfiability
+  issues
+
+*/
+
+/*!
+  \namespace OKlib::Satisfiability
+  \brief The part of the OKlibrary concerned with (generalised) satisfiability
+
+  Alias "Sat".
+*/
+
+namespace OKlib {
+  namespace Satisfiability {
+  }
+  namespace Sat = Satisfiability;
+}
+
