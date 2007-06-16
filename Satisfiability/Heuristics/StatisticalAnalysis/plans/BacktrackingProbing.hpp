0753f6d491ac8100f264105013e206c02204d27d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:13:30 +0100

    Plans about the analysis of backtracking via Knuth's method.

---
+++ Satisfiability/Heuristics/StatisticalAnalysis/plans/BacktrackingProbing.hpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Satisfiability/Heuristics/StatisticalAnalysis/plans/BacktrackingProbing.hpp
+  \brief Submodule for tools supporting probling as proposed by Donald Knuth
+
+  See Section 5.3 in [Foundations of %Algorithms Using C++ Pseudocode; Neapolitan, Naimipour 2004].
+
+  \todo Write first outlines.
+
+*/
