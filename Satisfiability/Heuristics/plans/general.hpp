ddbaf0f104919ea0fdbd222150b373b1dc164f05
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:16:45 +0100

    Plans for heuristics.

---
+++ Satisfiability/Heuristics/plans/general.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Satisfiability/Heuristics/plans/general.hpp
+  \brief Super-module for heuristics
+
+*/
+
+/*!
+  \namespace OKlib::Satisfiability::Heuristics
+  \brief Components for heuristics
+
+  Alias "Heu".
+*/
+
+namespace OKlib {
+  namespace Satisfiability {
+    namespace Heuristics {
+    }
+  }
+  namespace SatHeu = Satisfiability::Heuristics;
+}
+
