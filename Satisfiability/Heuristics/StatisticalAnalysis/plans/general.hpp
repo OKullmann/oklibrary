b8aff35bf4399ca1223b1302066e818973ccbadc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-16 21:12:55 +0100

    Plans about statistical analysis for heuristics-support.

---
+++ Satisfiability/Heuristics/StatisticalAnalysis/plans/general.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 16.6.2007 (Swansea)
+
+/*!
+  \file Satisfiability/Heuristics/StatisticalAnalysis/plans/general.hpp
+  \brief Module with statistical analysis tools for heuristics
+
+  \todo Move Transitional/Statistics/plans/RecogniseRandomFormulas.hpp
+  to here.
+
+*/
+
+/*!
+  \namespace OKlib::Satisfiability::Heuristics::StatisticalAnalysis
+  \brief Components for statistical analysis tools supporting heuristics
+
+  Alias "Sta".
+*/
+
+namespace OKlib {
+  namespace Satisfiability {
+    namespace Heuristics {
+      namespace StatisticalAnalysis {
+      }
+    }
+  }
+  namespace SatHeuSta = Satisfiability::Heuristics::StatisticalAnalysis;
+}
+
