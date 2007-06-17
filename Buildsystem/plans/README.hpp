bc4b537837425d38169629ce861782a90c30ccea
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-17 17:49:20 +0100

    New todo about automatisation (and integration with the configuration system).

--- Buildsystem/plans/README.hpp
+++ Buildsystem/plans/README.hpp
@@ -4,11 +4,20 @@
   \file Buildsystem/plans/README.hpp
   \brief Plans for the README file
 
+  \todo Automatic generation:
+  <ul>
+   <li> The README-file must be automatically produced from
+   the configuration data, using some template, which is
+   filled with the current information by preprocessing. </li>
+   <li> Besides the text-file, also an html-page is produced. </li>
+  </ul>
+
   \todo Supported versions of gcc:
   We need to clean-up regarding the supported versions of gcc.
   Perhaps actually we should support only from 4.1.1 onwards.
 
-  \todo Update:
+
+  \todo Update: DONE (see new todo "Automatic generation")
   As much as possible the information in the README file must be kept
   automatically synchronised with the real library.
 */
