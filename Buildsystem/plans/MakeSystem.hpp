10feaad38a59acd66a71097db667ee0279af8d02
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-22 00:10:06 +0100

    Editorial.

--- Buildsystem/plans/MakeSystem.hpp
+++ Buildsystem/plans/MakeSystem.hpp
@@ -104,6 +104,7 @@
    useful for that purpose) </li>
   </ul>
 
+
   \todo Design : DONE (required for the new cmake-system)
    - Larger makefiles should be composed (via inclusion) out of smaller
    makefiles (if possible; otherwise there must be a "copy-and-paste"-comment
