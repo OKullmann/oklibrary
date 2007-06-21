e3a37d02b511af2ac71bc6bff58196f1d51371ef
Oliver Kullmann <csoliver@cs-wsok.swansea.ac.uk>
2007-06-21 21:18:24 +0100

    Initial version number 0.0.3 (some plans, some preparations).

---
+++ SumProduct/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 21.6.2007 (Swansea)
+
+/*!
+  \file SumProduct/plans/milestones.hpp
+  
+  \module_version SumProduct 0.0.3 (21.6.2007)
+
+  \par Version 0.0.4 :
+
+  \par
+   In SumProduct/plans/Marginalisation.hpp the following topics are handled:
+    - %Implementation
+
+   \par
+   In  SumProduct/plans/SumProduct.hpp the following topics are handled:
+    - Different levels of generalisation
+    - Concepts
+
+  And new milestones have been created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 21.6.2007; initial version number (some plans, and theoretical
+   preparations regarding sum-product for trees).
+
+*/
+
