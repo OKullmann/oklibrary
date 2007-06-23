aa05a957c2baf0c072cb1b29e148e19c4c7b8b0b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-23 11:23:42 +0100

    Plans for the replacement of Mupad.

---
+++ Mupad/plans/general.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 23.6.2007 (Swansea)
+
+/*!
+  \file Transitional/Mupad/plans/general.hpp
+  \brief Plans for the (deprecated) Mupad-module
+
+  \todo Rewriting:
+  After a new computer-algebra system has been chosen (see "Computer algebra system"
+  in Transitional/plans/Transitional.hpp), all modules need to be redesigned
+  and reimplemented:
+  <ol>
+   <li> Mupad/Design.mup : Just the incidence matrix of the Fano plane. </li>
+   <li> Mupad/Hilfsfunktionen.mup : Various general functions </li>
+   <li> Mupad/Orthogonal.mup : Conflict combinatorics </li>
+   <li> Mupad/SATCounting.mup : Precise calculations of satisfiability
+   probabilities in the standard random-formula model (constant density) </li>
+   <li> Mupad/TestenFormelGenAES.mup : Testing the random clause-set
+   generator </li>
+   <li> Mupad/VanderWaerden.mup : Computing optimal resolution trees etc.
+   for van der Waerden-clause-sets </li>
+   <li> Mupad/tau.mup : Just the general tau-function. </li>
+  </ol>
+
+*/
+
