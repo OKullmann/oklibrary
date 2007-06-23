4dc118c7ea80fddb41abaf5fd64f62e3b44f85f3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-23 23:34:25 +0100

    *** New version number 0.0.4 (module LocalSearch) ***

--- LocalSearch/plans/milestones.hpp
+++ LocalSearch/plans/milestones.hpp
@@ -1,21 +1,14 @@
 /*!
   \file LocalSearch/plans/milestones.hpp
 
-  \module_version LocalSearch 0.0.3 (6.4.2007)
-
-  \par Version 0.0.4 :
-
-  \par
-   In LocalSearch/plans/LocalSearch.hpp the following topics are handled:
-   - Improved installation of UBCSAT : DONE
-   - Create further milestones : DONE
-   - Move Ubcsat-components from module AutarkySearch to here : DONE
+  \module_version LocalSearch 0.0.4 (23.6.2007)
 
   \par Version 0.0.5 :
 
   \par
    In LocalSearch/plans/LocalSearch.hpp the following topics are handled:
    - Installation of UBCSAT completed
+   - Contact the developers of Ubcsat
    - Create further milestones
 
   \par
@@ -28,6 +21,9 @@
   \par Version history
 
    - 0.0.3 : 6.4.2007; initial version number (some plans, and first installation of UBCSAT).
+   - 0.0.4 : 23.6.2007; corrected Ubcsat-source-code (incompatible types, which led
+   to errors on 64-bit machines), and put the changed Ubcsat-files under version
+   control.
 
 */
 
