3f9ac7c2ff685826f90f43e4283c4bcca455b985
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-24 11:12:47 +0100

    More on latex-packaging and bibtex-databases.

--- plans/Annotations.hpp
+++ plans/Annotations.hpp
@@ -32,7 +32,11 @@
      <li> Other developers macros. </li>
     </ol>
    </li>
-   <li> Perhaps at this level one needs to find out how to write a latex-"package" ! </li>
+   <li> Perhaps at this level one needs to find out how to write a latex-"package" !
+    <ol>
+     <li> The doc-system should be used for documenting the macros. </li>
+    </ol>
+   </li>
    <li> What to do with Latex_macros/Basis.tex and Latex_macros/Definitionen.tex ? </li>
   </ul>
 
@@ -49,7 +53,27 @@
    </li>
    <li> What to do with Latex_bib/EigArt.bib ? </li>
    <li> Bibtex-entries can easily be centralised, but what about the annotations? </li>
-   <li> Is there some real bibtex-database-system ? </li>
+   <li> Is there some real bibtex-database-system ?
+    <ol>
+     <li> "bibulus" (XML and Perl) http://www.nongnu.org/bibulus </li>
+     <li> "MibibTex" (close to XSLT) http://lifc.univ-fcomte.fr/~hufflen/texts/mlbibtex/mlbibtex/mlbibtex.html </li>
+     <li> "BibTex++" (Java) http://bibtex.enstb.org </li>
+    </ol>
+   </li>
+   <li> Useful bibtex-database-management tools?
+    <ol>
+     <li> http://www.tug.org/tex-archive/biblio/bibtex/utils/ </li>
+     <li> http://bibliographic.openoffice.org/biblioo-sw.html </li>
+     <li> pybliographer (python-tool) http://pybliographer.org </li>
+     <li> JBibtexManagager (Java) http://jabref.sourceforge.net </li>
+    </ol>
+   </li>
+   <li> What to do about online-repositories?
+    <ol>
+     <li> http://www.math.utah.edu:8080/pub/tex/bib/index-table.html </li>
+     <li> http://liinwww.ira.uka.de/bibliography/index.html </li>
+    </ol>
+   </li>
   </ul>
 
 */
