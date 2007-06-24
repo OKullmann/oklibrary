5d45b02db411c7d99a42997aa8d6204c1bab29b5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-24 09:48:52 +0100

    New plans-file for the Annotations-part of Transitional.
    Once there is , then this file becomes
    Transitional/Annotations/plans/general.hpp.
    The main problem faced now is about shared macros and shared
    bibtex-entries.

---
+++ plans/Annotations.hpp
@@ -0,0 +1,56 @@
+// Oliver Kullmann, 24.6.2007 (Swansea)
+
+/*!
+  \file Transitional/plans/Annotations.hpp
+  \brief Plans for the Annotations-part of the OKlibrary, where
+  latex-documents are handled
+
+  \todo Partially moving repository Annotations:
+  <ul>
+   <li> Some parts should move to Transitional, namely
+    <ol>
+     <li> "FullDocumentation" </li>
+     <li> the latex-support (needs clean-up) </li>
+    </ol>
+    Transitional/Annotations shall be the basis for writing documents and even
+    joint papers on the library.
+   </li>
+   <li> "History_OKsystem" seems obsolete, and should be removed. </li>
+   <li> "Release-1_0" perhaps should move (in some form) to Transitional/plans. </li>
+   <li> Research articles stay in the separate repository "Annotations" (only for internal
+   use). </li>
+  </ul>
+
+
+  \todo Latex macros
+  <ul>
+   <li> Latex macros need to be shared between several places:
+    <ol>
+     <li> Transitional/Annotations </li>
+     <li> The independent Annotations repository </li>
+     <li> OK's own macros </li>
+     <li> Other developers macros. </li>
+    </ol>
+   </li>
+   <li> Perhaps at this level one needs to find out how to write a latex-"package" ! </li>
+   <li> What to do with Latex_macros/Basis.tex and Latex_macros/Definitionen.tex ? </li>
+  </ul>
+
+
+  \todo Bibtex-database
+  <ul>
+   <li> Bibtex-entries need to be shared between several places:
+    <ol>
+     <li> Transitional/Annotations </li>
+     <li> The independent Annotations repository </li>
+     <li> OK's own bibtex-database </li>
+     <li> Other developers bibtex-database. </li>
+    </ol>
+   </li>
+   <li> What to do with Latex_bib/EigArt.bib ? </li>
+   <li> Bibtex-entries can easily be centralised, but what about the annotations? </li>
+   <li> Is there some real bibtex-database-system ? </li>
+  </ul>
+
+*/
+
