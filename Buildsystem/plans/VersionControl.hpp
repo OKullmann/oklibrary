8c617984e3d3261718221a64cf422601805b0d56
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-27 17:35:53 +0100

    Formatting, and about using configuration data for notification-e-mails.
    <li>-items should be indented (to make reading the source-code easier).

--- Buildsystem/plans/VersionControl.hpp
+++ Buildsystem/plans/VersionControl.hpp
@@ -198,19 +198,21 @@ mutt -s "OKlibrary::Annotations Git Push -- $USER" O.Kullmann@Swansea.ac.uk m.j.
    the text in "hooks/post-receive"), which we should examine (it seems we should also set up
    an OKlibrary-e-mail-list ?!).
    <ul>
-   <li>
-   To configure this script to send emails to a list of users we add, for example, the following lines to
-   the "config" file (OK: which "config"-file?):
-   \verbatim
-   [hooks]
-        mailinglist = M.J.Henderson@Swansea.ac.uk, O.Kullmann@Swansea.ac.uk
-   \endverbatim 
-   </li>
-   <li> Should we use a link instead of replacing the original post-receive script? (OK: a link to what?) </li>
-   <li> We should "name" each repository by putting the name "Annotations" or "Transitional" in the
-   "description" file (OK: which "description"-file?). Then the name appears in the emails. </li>
-   <li> Seems that each email always has [SCM] in the header. But we need to have various possibilities.
-   For example, just [OKLIB], [OKLIB][DEVEL], [OKLIB][USER], etc... How to achieve this? </li>
+    <li>
+    To configure this script to send emails to a list of users we add, for example, the following lines to
+    the "config" file (OK: which "config"-file?):
+    \verbatim
+    [hooks]
+         mailinglist = M.J.Henderson@Swansea.ac.uk, O.Kullmann@Swansea.ac.uk
+    \endverbatim 
+    </li>
+    <li> Should we use a link instead of replacing the original post-receive script? (OK: a link to what?) </li>
+    <li> We should "name" each repository by putting the name "Annotations" or "Transitional" in the
+    "description" file (OK: which "description"-file?). Then the name appears in the emails. </li>
+    <li> Seems that each email always has [SCM] in the header. But we need to have various possibilities.
+    For example, just [OKLIB], [OKLIB][DEVEL], [OKLIB][USER], etc... How to achieve this? </li>
+    <li> The configuration data about developers, library names etc. must go to Configuration (see
+    Buildsystem/plans/Configuration.hpp). </li>
    </ul>
    </li>
    <li> Git can handle symbolic links, so all symbolic links should go
