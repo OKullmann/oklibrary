2591ef4b921bf0aa194f7c2c447e9cc3ea29e236
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-29 13:02:13 +0100

    Editorial update.

--- ComputerAlgebra/Mupad/plans/general.hpp
+++ ComputerAlgebra/Mupad/plans/general.hpp
@@ -1,8 +1,9 @@
 // Oliver Kullmann, 23.6.2007 (Swansea)
 
 /*!
-  \file Transitional/Mupad/plans/general.hpp
+  \file Transitional/ComputerAlgebra/Mupad/plans/general.hpp
   \brief Plans for the (deprecated) Mupad-module
+  \deprecated To be replaced by an open-source system.
 
   \todo Rewriting:
   After a new computer-algebra system has been chosen (see "Computer algebra system"
