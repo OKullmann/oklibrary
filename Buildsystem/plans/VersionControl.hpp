44d85e209288c49d0e9cd0351014772a9c005015
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-29 22:58:19 +0100

    Update git-space-usage.

--- Buildsystem/plans/VersionControl.hpp
+++ Buildsystem/plans/VersionControl.hpp
@@ -104,6 +104,7 @@ error: failed to push to 'csoliver@cs-wsok:LaptopArchiv/OKsystem/Transitional'
      <li> 24.6.2007; 4.3 MB; Transitional total: 15.4 MB </li>
      <li> 26.6.2007: 4.3 MB; Transitional total: 15.4 MB </li>
      <li> 27.6.2007: 4.4 MB; Transitional total: 15.4 MB </li>
+     <li> 29.6.2007: 4.4 MB; Transitional total: 15.6 MB </li>
     </ol>
    </li>
    <li> Secure pushs to the central repository on cs-oksvr:
