3a8fde458c33bb5c9d53e5fd392feb86721b917c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-29 22:28:47 +0100

    Update.

--- InputOutput/plans/InputOutput.hpp
+++ InputOutput/plans/InputOutput.hpp
@@ -4,6 +4,7 @@
   \file Transitional/InputOutput/plans/InputOutput.hpp
   \brief Plans for the module on SAT-related input/output.
 
+
   \todo Update:
   Get an overview on the current state of the module, create new
   plans-files for the sub-modules, move the todo's to there, update
@@ -13,11 +14,18 @@
    - InputOutput/Dimacs.hpp :
    - InputOutput/Exceptions.hpp :
 
+
+  \todo See Transitional/Satisfiability/ProofSystems/DPv/Input_output.hpp
+
+
   \todo Move to the new test system.
+
 
   \todo Complete the tests.
 
+
   \todo Complete the functionality.
+
 
   \todo Input and output of partial assignments.
 
