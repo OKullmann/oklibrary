5c4100e05574f609c8a460f6e88619fafee079cb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-06-29 13:03:03 +0100

    Plans for the use of computer-algebra-systems in general.

---
+++ ComputerAlgebra/plans/general.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 29.6.2007 (Swansea)
+
+/*!
+  \file Transitional/ComputerAlgebra/plans/general.hpp
+  \brief General plans regarding computer algebra (and SAT)
+
+
+  \todo General principles
+  <ul>
+   <li> The computer-algebra-system is also based on generic
+   concepts, which mirror the more refined C++ concepts. </li>
+   <li> These concepts express (typically) the most general
+   level, without all the distinctions regarding
+   efficiency. </li>
+   <li> The computer-algebra-system thus also serves as an
+   experimental area for the design of concepts in the OKlibrary
+   (regarding the "mathematical" concepts, not the programming ones). </li>
+   <li> Concepts and (generic) algorithm for the OKlibrary are first
+   prototyped in the context of the computer-algebra-system (if
+   possible). </li>
+  </ul>
+  Likely these principles should go to ComputerAlgebra/docus/general.hpp.
+
+
+  \todo Create a milestones-file.
+  
+*/
+
