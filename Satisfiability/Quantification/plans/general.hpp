971610a184d92aa9dc3eec90dc2036c6d6231e3f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-01 11:40:55 +0100

    Update, and extension of plans.

--- Satisfiability/Quantification/plans/general.hpp
+++ Satisfiability/Quantification/plans/general.hpp
@@ -1,13 +1,41 @@
- 
 // Oliver Kullmann, 9.6.2007 (Swansea)
 
 /*!
-  \file Quantification/plans/Quantification.hpp
+  \file Quantification/plans/general.hpp
   \brief Module for quantified boolean formulas and generalisations
 
-  \todo Main challenge are the concepts.
 
-  \todo What are the input formats? See SAT evaluation.
+  \todo Main challenge are the concepts.
+  <ul>
+   <li> Arbitrary "constraint-problems" can be quantified. </li>
+   <li> "Quantification" is a structuring-process on the variables. </li>
+   <li> "Models" for a true formula are now maps which realise the existential variables,
+   while "counter-examples" for a false formula realise the universal variables. </li>
+   <li> For such realisations different specifications can be used to yield a
+   compression. </li>
+   <li> The validation of "models" and "counter-examples" becomes more important
+   (and complex) --- the field includes for example the proof systems of propositional logic
+   (only universal variables, and validity is to be shown, or only existential variables,
+   and falsity is to be shown)! </li>
+   <li> A question is, whether also other quantifier (besides "for all" and "exists")
+   should be considered here, or whether they %mean really a different thing (to be
+   considered in another module)? </li>
+   <li> Another link is to first-order logic (with equality), considering a fixed structure
+   {0,1} with the operations "and, or, not" --- this is a special case of checking whether for
+   a given structure a first-order formula holds ("expression complexity"). One could consider
+   the whole problem of "quantified generalised constraint-problems" as the problem of
+   deciding whether in some fixed structure (with equality, functions and
+   relations) a first-order sentence holds (however this view is too much fixed on
+   a specific representation of a problem!). </li>
+  </ul>
+
+
+  \todo What are the input formats?
+  <ul>
+   <li> See SAT evaluation. </li>
+   <li> This should become a part of Satisfiability/Interfaces. </li>
+  </ul>
+
 
   \todo Applications:
   <ul>
