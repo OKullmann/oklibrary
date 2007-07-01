8b26fae7e3a804543c65fbe5360f780191b58e56
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-01 11:52:42 +0100

    Updates.

--- Structures/NumberTheory/BigIntegers/plans/GMPWrapper.hpp
+++ Structures/NumberTheory/BigIntegers/plans/GMPWrapper.hpp
@@ -4,18 +4,23 @@
   \file BigIntegers/plans/GMPWrapper.hpp
   \brief Plans for the sub-module for wrapping the GMP-C-approach into a class according to the basic concept.
 
-  \todo Design the concept of a wrapper.
 
-  The main first "client" is the OKgenerator (see
-  RandomGenerator/plans/RandomGenerator.hpp)
+  \todo Design the concept of a wrapper.
+  <ul>
+   <li> The main first "client" is the OKgenerator (see
+   RandomGenerator/plans/RandomGenerator.hpp) </li>
+  </ul>
+
 
   \todo Write tests for the concepts
 
-  \todo Implementation of the basic wrappers:
+
+  \todo %Implementation of the basic wrappers:
   I guess, best based on the C-interface.
   <ul>
    <li> constructor uses mpz_init </li>
    <li> destructor uses mpz_clear </li>
   </ul>
+
 */
 
--- Structures/NumberTheory/BigIntegers/plans/general.hpp
+++ Structures/NumberTheory/BigIntegers/plans/general.hpp
@@ -1,10 +1,21 @@
 // Oliver Kullmann, 19.10.2005 (Swansea)
 
 /*!
-  \file BigIntegers/plans/BigIntegers.hpp
+  \file BigIntegers/plans/general.hpp
   \brief Plans for the module with big integer tools
 
+
+  \todo Update the namespace.
+
+
+  \todo What is going on with Boost?
+
+
   \todo See the concepts in Concepts/plans/BigIntegers.hpp.
+  <ul>
+   <li> They shall move to here. </li>
+  </ul>
+
 */
 
 /*!
