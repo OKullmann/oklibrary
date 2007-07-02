e560c196810c0b9acfd0075d5559e8e2414c68b0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-02 11:35:02 +0100

    *** Initial version number 0.1.0 (module ExternalSources) ***

---
+++ Buildsystem/ExternalSources/plans/milestones.hpp
@@ -0,0 +1,33 @@
+// Oliver Kullmann, 2.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/ExternalSources/plans/milestones.hpp
+  
+  \module_version ExternalSources 0.1.0 (2.7.2007)
+
+  \par Version 0.1.1
+
+  \par
+   In Transitional/Buildsystem/ExternalSources/plans/general.hpp the following topics are handled:
+    - Renaming
+    - Mailman
+    - CMake-documentation building
+    - Improving building Boost
+    - Problems with building Boost
+    - Building Ubcsat
+    - Documenting ExternalSources.mak
+    - Building documentation
+    - R
+    - Downloading sources
+    - Plans-structure for ExternalSources : DONE
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1.0 : 2.7.2007; extracted from Buildsystem (version 0.1.6). The system
+   is running, extensions can be done by "copy-and-paste", but the system grew "organically", so redesign is needed, and also the functionality is too restricted (and sporadic).
+
+*/
+
