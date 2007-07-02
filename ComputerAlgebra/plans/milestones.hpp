802536b42a146bb61e75453ba356a1d460468383
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-02 11:40:43 +0100

    *** Initial version number 0.1.0 (part ComputerAlgebra) ***

---
+++ ComputerAlgebra/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 2.7.2007 (Swansea)
+
+/*!
+  \file ComputerAlgebra/plans/milestones.hpp
+
+  \module_version ComputerAlgebra 0.1.0 (2.7.2007)
+
+  \par Version 0.1.1
+
+  \par
+   In ComputerAlgebra/plans/general.hpp the following topics are handled:
+    - General principles
+    - %Test system
+    - Input checking
+    - Partial compilation
+    - Abstraction
+    - Contexts
+
+  \par
+   In ComputerAlgebra/Mupad/plans/general.hpp the following topics are handled:
+    - Rewriting
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1.0 : 2.7.2007; quite a few Mupad-functions produced (but without higher design, and not connected to the main concepts of (generalised) satisfiability)
+
+*/
+
