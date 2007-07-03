c189eb6c1ce5d92b3f5d18b42f65adbce7706150
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-03 13:27:48 +0100

    *** Initial version number 0.0.4 (module Buildsystem/Configuration) ***
    Also new milestones 0.0.5 and 0.0.6 created.

---
+++ Buildsystem/Configuration/plans/milestones.hpp
@@ -0,0 +1,38 @@
+// Oliver Kullmann, 3.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/Configuration/plans/milestones.hpp
+  
+  \module_version Buildsystem/Configuration 0.0.4 (3.7.2007)
+
+  \par Version 0.0.5
+
+  \par
+   In Buildsystem/Configuration/plans/general.hpp the following topics are handled:
+    - Configuration data overview
+    - Configuration data format
+    - Primary versus derived configurations
+
+  \par
+   In Buildsystem/Configuration/plans/Persons.hpp the following topics are handled:
+    - Developers information
+
+  \par Version 0.0.6
+
+  \par
+   In Buildsystem/Configuration/plans/general.hpp the following topics are handled:
+    - Software management
+    - external_sources_versions.mak
+    - Make-variables for external libraries
+    - Definitions for doxygen
+    - Developers information
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 3.7.2007; initial version number (some unification regarding configuration-data for external sources, doxygen-data, html-related data, but altogether no systematic approach).
+
+*/
+
