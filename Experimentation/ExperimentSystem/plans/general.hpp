3c574279b3e796ad9baf1b7d28afa4e1ebd3868b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-03 01:10:32 +0100

    Updates.

--- Experimentation/ExperimentSystem/plans/general.hpp
+++ Experimentation/ExperimentSystem/plans/general.hpp
@@ -1,19 +1,29 @@
 // Oliver Kullmann, 2.11.2005 (Swansea)
 
 /*!
-  \file ExperimentSystem/plans/ExperimentSystem.hpp
+  \file ExperimentSystem/plans/general.hpp
   \brief Launching and monitoring experiments on remote machines.
+
+
+  \todo Update namespaces.
+
 
   \todo First step is to transfer the experiment-system from OKgenerator
   (see directory OKsolver/Generator) to the (transitional) library.
   Necessary extensions (improvements):
-   - Descriptors:
-     + It must be possible to process "ad-hoc experiments"; the simplest
+  <ul>
+   <li> Descriptors:
+    <ol>
+     <li> It must be possible to process "ad-hoc experiments"; the simplest
      way to do so is to allow as input a directory-name, and then all
-     files in there will be processed.
-     + Additional parameters specifying the experiment like the abortion-time.
-   - Database (see module OKDatabase) must be extended to administer also
-     ad-hoc experiments.
+     files in there will be processed. </li>
+     <li> Additional parameters specifying the experiment like the abortion-time. </li>
+    </ol>
+   </li>
+   <li> Database (see module OKDatabase) must be extended to administer also
+   ad-hoc experiments. </li>
+  </ul>
+
 
   \todo The launch-and-monitor system follows the old 3 steps (preparation,
   processing, transfer), but now all these steps can be launched automatically.
@@ -31,9 +41,11 @@
   single tools for the different actions (querying the status, launching an experiment),
   and use simple copy-and-paste to transfer data from one step to another.
 
+
   \todo The tests must be written in such a fashion, that they are largely testable without
   accessing ssh; for testing the parts essentially using ssh one can then simply use
   the host machine of the testing process.
+
 */
 
 /*!
--- Experimentation/ExperimentSystem/plans/milestones.hpp
+++ Experimentation/ExperimentSystem/plans/milestones.hpp
@@ -3,17 +3,17 @@
 /*!
   \file ExperimentSystem/plans/milestones.hpp
 
-  \par Current version 0.1 (6.11.2006; a working system exists, but not integrated into the OKlibrary)
+  \module_version 0.1 (6.11.2006)
 
   \par Version 0.1.1 :
   The first task is to make a plan and create milestones for the transition and extension of the old
-  system (see ExperimentSystem/plans/ExperimentSystem.hpp).
+  system (see ExperimentSystem/plans/general.hpp).
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
 
   \par Version history
 
-   -  0.1 : 6.11.2006; initial version number (external working system).
+   -  0.1 : 6.11.2006; initial version number (a working system exists, but not integrated into the OKlibrary).
 
 */
