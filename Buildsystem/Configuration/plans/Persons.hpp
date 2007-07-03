5f8ca3ff09953cb82e0a168a5404a8ad16ff4e0d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-03 13:26:37 +0100

    Update, new plans-file "Persons.hpp", some todos moved to ExternalSources.

---
+++ Buildsystem/Configuration/plans/Persons.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 3.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/Configuration/plans/Persons.hpp
+  \brief Plans regarding management of personal data
+
+
+  \todo Developers information
+  <ul>
+   <li> Compare with "Developers" in Buildsystem/plans/Release.hpp </li>
+   <li> We should have a subdirectory Configuration/Developers, containing files
+    <ol>
+     <li> current_main </li>
+     <li> past_main </li>
+     <li> past_external </li>
+     <li> current_external </li>
+    </ol>
+    Each of those files contains lines like
+    <ol>
+     <li> OK "Oliver Kullmann" O.Kullmann@Swansea.ac.uk </li>
+     <li> MH "Matthew Henderson" XXX </li>
+     <li> ML "Matthew Lewsey" XXX </li>
+    </ol>
+    Or is there some "official" file format for such address-data?!
+   </li>
+   <li> Out of these files an html-file is produced. </li>
+  </ul>
+
+*/
+
+
--- Buildsystem/Configuration/plans/general.hpp
+++ Buildsystem/Configuration/plans/general.hpp
@@ -1,27 +1,31 @@
 // Oliver Kullmann, 11.6.2007 (Swansea)
 
 /*!
-  \file Buildsystem/plans/Configuration.hpp
+  \file Buildsystem/Configuration/plans/general.hpp
   \brief Plans regarding configuration management
 
 
-  \todo Primary versus derived configurations
+  \todo Configuration data overview :
+  We need to get an overview on the different types of configuration data.
   <ul>
-   <li> The primary configurations are under version control. </li>
-   <li> Derived values (like source- and link-libraries for external libraries)
-   are to be found perhaps in system_directories/configurations ? There might be also
-   a symbolic link to the primary configurations? </li>
+   <li> Build-related </li>
+   <li> External sources:
+    <ol>
+     <li> See Buildsystem/ExternalSources/plans/Configuration.hpp. </li>
+    </ol>
+   </li>
+   <li> html-pages </li>
+   <li> doxygen </li>
+   <li> Personal information:
+    <ol>
+     <li> See Buildsystem/Configuration/plans/Persons.hpp. </li>
+    </ol>
   </ul>
 
 
-  \todo Configuration data
+  \todo Configuration data format
   <ul>
-   <li> A central problem is: How to create a central repository
-   of configuration data, usable by the various web pages as well
-   as by the build system?!
-    <ol>
-     <li> Perhaps a new subdirectory Buildsystem/Configuration? </li>
-     <li> Inside we have a make-file containing only variable settings
+   <li> We could use makefile-syntax, that is, we have make-files containing only variable settings
      \verbatim
 CONFIGVAR1=value1
 CONFIGVAR2=value2
@@ -47,47 +51,21 @@ SET(CONFIGVAR2 value2)
      a closing bracket? Perhaps we could just use the export-facility of make,
      so that these variables become environment variables?!? But then we would
      loose control.
-     </li>
-     <li> If we are going to use CMake, then perhaps the variable settings should
+   </li>
+   <li> If we are going to use CMake, then perhaps the variable settings should
      be done in the cmake-syntax. </li>
-     <li> Splitting the definitions over several files is likely preferable from the
+   <li> Splitting the definitions over several files is likely preferable from the
      order point of view. Shouldn't be too complicated to use them. </li>
-    </ol>
    </li>
   </ul>
 
 
-  \todo Software management:
-  All information about whether software (like doxygen, cmake, gcc etc.)
-  needed is installed locally or system-wide, and where to find it,
-  must be found in the configuration directory (in an easily accesible
-  way). It must be possible to update the system (installing a new
-  cmake, for example, or a new gcc), perhaps by setting variables
-  about supported and recommended versions.
-
-
-  \todo external_sources_versions.mak: UPDATE needed (%w.r.t. the new view
-  of the configuration center)
+  \todo Primary versus derived configurations
   <ul>
-   <li> The role of external_sources_versions.mak must
-   be clarified. </li>
-   <li> Names like
-   <code> doxygen_supported_not_recommended_version_numbers </code>
-   are misleading (these versions are not "not recommended"), and
-   they are too long. </li>
-  </ul>
-
-
-  \todo Make-variables for external libraries : UPDATE needed (%w.r.t. the new view
-  of the configuration center)
-  <ul>
-   <li> How to define variables like Boost and Ubcsat in general?
-     We need a nice little method (plus documentation), so that we can add easily as
-     many external libraries as we want. </li>
-   <li> And then we have make-variables "gcc_version_number" (for generic.mak) and
-     "gcc-version" (for boost.mak) which likely should have the same names. </li>
-   <li> In system_definitions.mak we have a definition of Boost which seems wrong
-     to me (OK) (or at least misleading). DONE (resp. moved to other todos) </li>
+   <li> The primary configurations are under version control. </li>
+   <li> Derived values (like source- and link-libraries for external libraries)
+   are to be found perhaps in system_directories/configurations ? There might be also
+   a symbolic link to the primary configurations? </li>
   </ul>
 
 
@@ -114,29 +92,6 @@ SET(CONFIGVAR2 value2)
    on the created doxyfile, one has to study the changes, and update the master-doxyfile
    accordingly. </li>
   </ul>
-
-
-  \todo Developers information:
-  <ul>
-   <li> Compare with "Developers" in Buildsystem/plans/Release.hpp </li>
-   <li> We should have a subdirectory Configuration/Developers, containing files
-    <ol>
-     <li> current_main </li>
-     <li> past_main </li>
-     <li> past_external </li>
-     <li> current_external </li>
-    </ol>
-    Each of those files contains lines like
-    <ol>
-     <li> OK "Oliver Kullmann" O.Kullmann@Swansea.ac.uk </li>
-     <li> MH "Matthew Henderson" XXX </li>
-     <li> ML "Matthew Lewsey" XXX </li>
-    </ol>
-    Or is there some "official" file format for such address-data?!
-   </li>
-   <li> Out of these files an html-file is produced. </li>
-  </ul>
-
 
 */
 
