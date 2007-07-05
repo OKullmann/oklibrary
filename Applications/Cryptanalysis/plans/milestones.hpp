9cf968c0061a91ae4eed01e65a5d415e0d2a7ad1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-05 01:44:43 +0100

    *** New version number 0.0.3 (module Applications/Cryptanalysis) ***
    Various updates.

--- Applications/Cryptanalysis/plans/milestones.hpp
+++ Applications/Cryptanalysis/plans/milestones.hpp
@@ -3,39 +3,71 @@
 /*!
   \file Cryptanalysis/plans/milestones.hpp
 
-  \module_version Cryptanalysis 0.0.2 (12.11.2006)
+  \module_version Applications/Cryptanalysis 0.0.3 (5.7.2007)
 
-  \par Version 0.0.3 :
-  Further milestones leading to version 0.1 are created.
+  \par Version 0.0.4
+
+  \par
+   In Cryptanalysis/plans/Rijndael.hpp the following topics are handled:
+    - Overview
+
+  
+  \par Version 0.0.5
+
+  \par
+   In Cryptanalysis/plans/Rijndael.hpp the following topics are handled:
+    - Computer algebra implementation
+    - Rijndael
+
+
+  \par Version 0.0.6
+
+  \par
+   In Cryptanalysis/plans/Rijndael.hpp the following topics are handled:
+    - Partitioning into active clauses
+
+
+  \par Version 0.0.7
+
+  \par
+   In Cryptanalysis/plans/Rijndael.hpp the following topics are handled:
+    - Encoding AES
+
+
+  \par Version 0.0.8
+
+  \par
+   In Cryptanalysis/plans/Rijndael.hpp the following topics are handled:
+    - First tests AES
+    - Further tests
+
 
   \par Version 0.1 :
-  In Cryptanalysis/plans/Rijndael.hpp the topics
-  - Implementing
-  - First tests AES
-  - Rijndael
 
-  have been handled, and tested generators are available.
+  \par
+  A system for Rijndael has been implemented and tested, which can
+  create interesting representations of various cryptographic problems
+  (including CNF-representations).
 
   \par Version 0.2 :
-  In Cryptanalysis/plans/Rijndael.hpp the topics
-  - Further tests
-  - Good constraints
-  - Overview
 
-  have been handled.
+  \par
+   In Cryptanalysis/plans/Factorisation.hpp the following topics are handled:
+    - Literature review
+    - Interesting good constraint representations
 
-  \par Version 0.3 :
-  In Cryptanalysis/plans/Factorisation.hpp the topics
-  - Literature review
-  - Interesting good constraint representations
-
-  have been handled.
-
+  \par
+   In Cryptanalysis/plans/general.hpp the following topics are handled:
+    - Update namespaces.
+    - What about DES?
+    - RSA
+    - In SAT2007
 
   -------------------------------------------------------------------------------------------------------------------------------------
 
   \par Version history
 
-   -  0.0.2 : 17.6.2006; initial version number (only some plans).
+   - 0.0.2 : 12.11.2006; initial version number (only some plans).
+   - 0.0.3 : 5.7.2007; further plans and milestones created.
 
 */
