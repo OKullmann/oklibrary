6628c264610a76c30463001c96754f35db7ad0a2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-06 18:50:52 +0100

    *** Initial version number 0.2 (module Buildsystem/OKlibBuilding) ***

---
+++ Buildsystem/OKlibBuilding/plans/milestones.hpp
@@ -0,0 +1,104 @@
+// Oliver Kullmann, 6.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/OKlibBuilding/plans/milestones.hpp
+  
+  \module_version Buildsystem/OKlibBuilding 0.2 (6.7.2007)
+
+  \par Version 0.2.1
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/general.hpp the following topics are handled:
+    - bug Mixing C and C++
+    - bug Incorrect build of new tests I
+    - bug Incorrect build of new tests II
+    - %Test system
+    - OKsystem/Buildsystem structure : DONE
+    - Specification of build system : DONE
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/TargetSpecifications.hpp the following topics are handled:
+    - General look and feel
+
+  \par
+   In Buildsystem/OKlibBuilding/Targets/html/plans/general.hpp the following topics are handled:
+    - Doxygen general
+    - Directory structure : DONE
+
+
+  \par Version 0.2.2
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/general.hpp the following topics are handled:
+    - Renaming
+    - Documenting the build system
+    - Log directory
+    - Setting the paths to GCC and Boost link libraries
+    - User control of system versions
+    - General_options
+    - Dependency files
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/TargetSpecifications.hpp the following topics are handled:
+    - Target "all":
+    - Target "check"
+    - Target "prebuild"
+
+  \par
+   In Buildsystem/OKlibBuilding/Targets/html/plans/general.hpp the following topics are handled:
+    - Doxygen menu and main page review
+
+
+  \par Version 0.2.3
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/general.hpp the following topics are handled:
+    - Verbosity
+    - Targets
+    - Role of srcdir
+    - Cleaning
+    - %Test cleaning
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/TargetSpecifications.hpp the following topics are handled:
+    - Target "html"
+    - Target "reset"
+    - Target "new-module"
+
+
+  \par Version 0.2.4
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/general.hpp the following topics are handled:
+    - Error messages of gcc should be processed
+    - Linking and options
+    - Force make
+    - Compilation information
+    - Nightly build
+    - New targets
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/MakeSystem.hpp the following topics are handled:
+    - CMake
+
+
+  \par Version 0.2.5
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/general.hpp the following topics are handled:
+    - Complexity system:
+    - Measurements
+
+  \par
+   In Buildsystem/OKlibBuilding/plans/MakeSystem.hpp the following topics are handled:
+    - Competition extraction tools
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.2   : 6.7.2007; initial version number (the generic makefile is the most advanced part of the buildsystem).
+
+*/
+
