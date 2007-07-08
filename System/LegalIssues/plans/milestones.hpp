47cffe50d6d148178e378896b2a91c2ab421ba6c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-08 15:46:08 +0100

    *** Initial version number 0.0.3 (module LegalIssues) ***

---
+++ System/LegalIssues/plans/milestones.hpp
@@ -0,0 +1,45 @@
+// Oliver Kullmann, 8.7.2007 (Swansea)
+
+/*!
+  \file LegalIssues/plans/milestones.hpp
+  
+  \module_version LegalIssues 0.0.3 (8.7.2007)
+
+
+  \par Version 0.0.4
+
+  \par
+   In LegalIssues/plans/Licence.hpp the following topics are handled:
+    - Problems with licence
+
+
+  \par Version 0.0.5
+
+  \par
+   In LegalIssues/plans/Licence.hpp the following topics are handled:
+    - Plans on license installation
+
+
+  \par Version 0.0.6
+
+  \par
+   In LegalIssues/plans/Licence.hpp the following topics are handled:
+    - Licence installation
+
+
+  \par Version 0.1
+
+  \par
+   In LegalIssues/plans/Licence.hpp the following topics are handled:
+    - Licence documentation
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3   : 8.7.2007; initial version number (several considerations and discussions; decided to use GPLv3).
+
+*/
+
