449ed30041813e70dae73ec63f54c0934d18d003
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-09 19:00:00 +0100

    Todo about discussing the prerequisites in the documentation.

---
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/Git.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 9.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/ExternalSources/SpecialBuilds/plans/Git.hpp
+  \brief Plans regarding installation of Git
+
+  \todo Prerequisites
+  <ul>
+   <li> The build system can install asciidoc if needed; put it into
+   the documentation. (Is it available in recent distributions?) </li>
+   <li> What to do about xmlto --- is this available in recent
+   distributions, so that we don't need to provide an installation
+   ourselves (only documentation)?
+    <ol>
+     <li> Suse 10.0 has it. </li>
+    </ol>
+   </li>
+  </ul>
+
+*/
+
