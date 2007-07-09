6274296dc609d6c4af6b3946b8a4d8b8cdafad3a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-09 09:36:30 +0100

    *** Initial version number 0.0.4 (module Heuristics/StatisticalAnalysis) ***

---
+++ Satisfiability/Heuristics/StatisticalAnalysis/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 8.7.2007 (Swansea)
+
+/*!
+  \file Heuristics/StatisticalAnalysis/plans/milestones.hpp
+  
+  \module_version Heuristics/StatisticalAnalysis 0.0.4 (8.7.2007)
+
+
+  \par Version 0.0.5
+
+  \par
+   In Heuristics/StatisticalAnalysis/plans/general.hpp the following topics are handled:
+    - Update namespaces.
+    - Move Transitional/Statistics/plans/TimeSeriesAnalysis.hpp to here : DONE
+
+  \par
+   In Heuristics/StatisticalAnalysis/plans/BacktrackingProbing.hpp the following topics are handled:
+    - Literature review
+
+  And further milestones are created.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 8.7.2007; initial version number (several interesting plans).
+
+*/
+
