fadebe11bc8b50dde5560152df6fae4f0e8b2368
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-13 10:47:39 +0100

    Plans for the set-up makefile (for the very first set-up).

---
+++ Buildsystem/plans/SetUp.hpp
@@ -0,0 +1,14 @@
+// Oliver Kullmann, 13.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/plans/SetUp.hpp
+  \brief Plans for the makefile "SetUp.mak", which does the very first set-up.
+
+
+  \todo Further actions by the setup-process:
+  <ul>
+   <li> Create a copy in ~/bin of the oklib-script, and make it executable. </li>
+  </ul>
+
+*/
+
