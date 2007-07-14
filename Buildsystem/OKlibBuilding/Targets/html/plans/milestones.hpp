b35286ded02a0c6c66c49989d3752e877aef607f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-14 14:00:38 +0100

    *** Initial version number 0.1 (module OKlibBuilding/Targets/html) ***

---
+++ Buildsystem/OKlibBuilding/Targets/html/plans/milestones.hpp
@@ -0,0 +1,42 @@
+// Oliver Kullmann, 14.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/OKlibBuilding/Targets/html/plans/milestones.hpp
+  
+  \module_version Buildsystem/OKlibBuilding/Targets/html 0.1 (14.7.2007)
+
+  \par Version 0.1.1
+
+  \par
+   In Buildsystem/OKlibBuilding/Targets/html/plans/general.hpp the following topics are handled:
+    - Doxygen error messages
+    - Environment-variable expansion
+    - Doxygen general
+    - Directory structure : DONE
+
+
+  \par Version 0.1.2
+
+  \par
+   In Buildsystem/OKlibBuilding/Targets/html/plans/general.hpp the following topics are handled:
+    - Move
+    - Doxygen menu and main page review
+    - Search engine
+
+
+  \par Version 0.1.3
+
+  \par
+   In Buildsystem/OKlibBuilding/Targets/html/plans/general.hpp the following topics are handled:
+    - Other code
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1   : 14.7.2007; initial version number (the doxygen-documentation works, the basic functionality is there).
+
+*/
+
