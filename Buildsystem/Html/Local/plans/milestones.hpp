d1de6187ca3d78ceb967a992fc8d10159a7c614c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-15 17:26:29 +0100

    *** Initial version number 0.1 (module Buildsystem/Html/Local) ***

---
+++ Buildsystem/Html/Local/plans/milestones.hpp
@@ -0,0 +1,44 @@
+// Oliver Kullmann, 15.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/Html/Local/plans/milestones.hpp
+  
+  \module_version Buildsystem/Html/Local 0.1 (15.7.2007)
+
+  \par Version 0.1.1
+
+  \par
+   In Buildsystem/Html/Local/plans/general.hpp the following topics are handled:
+    - Rewrite local home page
+    - Using css : DONE
+    - Page appearance : DONE
+    - bug "Naming" : DONE
+
+
+  \par Version 0.1.2
+
+  \par
+   In Buildsystem/Html/Local/plans/general.hpp the following topics are handled:
+    - Boost-documentation
+    - GCC-documentation
+
+
+  \par Version 0.1.3
+
+  \par
+   In Buildsystem/Html/Local/plans/general.hpp the following topics are handled:
+    - Further documentation
+    - Page organisation
+    - Meta tags
+    - Improved logo
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1   : 15.7.2006; initial version number (experience with the first version of the local home page, and the new version got started).
+
+*/
+
