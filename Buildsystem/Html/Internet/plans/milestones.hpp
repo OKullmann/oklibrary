2c04f81599ec7a45d447412909ebd761eec3b1a8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-15 17:25:57 +0100

    *** Initial version number 0.0.2 (module Buildsystem/Html/Internet) ***

---
+++ Buildsystem/Html/Internet/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 15.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/Html/Internet/plans/milestones.hpp
+  
+  \module_version Buildsystem/Html/Internet 0.0.2 (15.7.2007)
+
+  \par Version 0.0.3
+
+  \par
+   In Buildsystem/Html/Internet/plans/general.hpp the following topics are handled:
+    - Content of the Internet-page
+
+
+  \par Version 0.0.4
+
+  \par
+   In Buildsystem/Html/Internet/plans/general.hpp the following topics are handled:
+    - Building the Internet-page
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 15.7.2006; initial version number (plans regarding the Internet page).
+
+*/
+
