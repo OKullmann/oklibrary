b4a50fc38438a60a40af9a1c5b3a408b83ce828e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-16 22:13:57 +0100

    *** Initial version number 0.2 (module ExternalSources/SpecialBuilds) ***

---
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
@@ -0,0 +1,66 @@
+// Oliver Kullmann, 16.7.2007 (Swansea)
+
+/*!
+  \file Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
+  
+  \module_version Buildsystem/ExternalSources/SpecialBuilds 0.2 (16.7.2007)
+
+  \par Version 0.2.1
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/general.hpp the following topics are handled:
+    - Build Sage
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/Coq.hpp the following topics are handled:
+    - Install ocaml (3.10.0)
+    - Install ocaml 3.09.3
+    - Install Coq (8.1)
+
+
+  \par Version 0.2.2
+
+  \par
+   In Transitional/Buildsystem/ExternalSources/SpecialBuilds/plans/general.hpp the following topics are handled:
+    - Documentation
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/Mailman.hpp the following topics are handled:
+    - Building Mailman
+
+
+  \par Version 0.2.3
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/Boost.hpp the following topics are handled:
+    - Improving building Boost
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/Gcc.hpp the following topics are handled:
+    - Gcc
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/R.hpp the following topics are handled:
+    - R-documentation
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/CMake.hpp the following topics are handled:
+    - CMake-documentation building
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/Ubcsat.hpp the following topics are handled:
+    - Building Ubcsat
+
+  \par
+   In Buildsystem/ExternalSources/SpecialBuilds/plans/PostgreSQL.hpp the following topics are handled:
+    - Missing pgsql/libpq-fe.h
+    - Improvements
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.2   : 16.7.2007; initial version, several special systems can be build (the special builds are the stronger parts of module ExternalSources, while the general structure is lagging behind).
+
+*/
