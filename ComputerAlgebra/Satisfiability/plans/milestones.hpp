34933282d04f4f52e727137d5512be4aac3f23e5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-29 09:39:26 +0100

    *** Initial version number 0.0.5 (module ComputerAlgebra/Satisfiability) ***

---
+++ ComputerAlgebra/Satisfiability/plans/milestones.hpp
@@ -0,0 +1,50 @@
+// Oliver Kullmann, 29.7.2007 (Swansea)
+
+/*!
+  \file ComputerAlgebra/Satisfiability/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Satisfiability 0.0.5 (29.7.2007)
+
+
+  \par Version 0.0.6
+
+  \par ComputerAlgebra/Satisfiability/TauMachinery version 0.0.5
+  (see ComputerAlgebra/Satisfiability/TauMachinery/plans/milestones.hpp)
+
+  \par
+   In ComputerAlgebra/Satisfiability/plans/SatisfactionProblems.hpp the following topics are handled:
+    - %Variables
+    - Value set
+    - Total assignments
+    - Domain association and allowed total assignments
+    - The notion of "condition"
+
+
+  \par Version 0.0.7
+
+  \par ComputerAlgebra/Satisfiability/TauMachinery version 0.0.6
+  (see ComputerAlgebra/Satisfiability/TauMachinery/plans/milestones.hpp)
+
+  \par
+   In ComputerAlgebra/Satisfiability/plans/SatisfactionProblems.hpp the following topics are handled:
+    - Partial assignments
+    - %Evaluation of partial assignments
+    - Functions for conditions
+    - Backdoors
+
+  \par
+   In ComputerAlgebra/Satisfiability/plans/general.hpp the following topics are handled:
+    - Relations to other modules
+    - Update plans
+ 
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.5 : 29.7.2007; several plans, started re-implementing the
+   mupad-tau-functionality.
+
+*/
+
