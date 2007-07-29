0934c4ebd5388761cad0c66f947eb937e0963314
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-07-29 09:40:51 +0100

    *** Initial version 0.0.4 (ComputerAlgebra/Satisfiability/TauMachinery) ***

---
+++ ComputerAlgebra/Satisfiability/TauMachinery/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 29.7.2007 (Swansea)
+
+/*!
+  \file ComputerAlgebra/Satisfiability/TauMachinery/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Satisfiability/TauMachinery 0.0.4 (29.7.2007)
+
+
+  \par Version 0.0.5
+
+  \par
+   In ComputerAlgebra/Satisfiability/TauMachinery/plans/general.hpp the following topics are handled:
+    - Tau for Maxima
+
+
+  \par Version 0.0.6
+
+  \par
+   In ComputerAlgebra/Satisfiability/TauMachinery/plans/general.hpp the following topics are handled:
+    - Reimplement the remaining functionality from Mupad/tau.hpp
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 29.7.2007; started re-implementing the Mupad-functions in Maxima.
+
+*/
+
