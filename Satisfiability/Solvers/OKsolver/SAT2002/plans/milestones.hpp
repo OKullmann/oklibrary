9f62cac5a4926de2703ac2b5f10549bca42053a1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-18 03:20:37 +0100

    *** Initial version 0.9 (OKsolver/SAT2002) ***

---
+++ Satisfiability/Solvers/OKsolver/SAT2002/plans/milestones.hpp
@@ -0,0 +1,52 @@
+// Oliver Kullmann, 18.8.2007 (Swansea)
+
+/*!
+  \file Solvers/OKsolver/SAT2002/plans/milestones.hpp
+  
+  \module_version OKsolver/SAT2002 0.9 (18.8.2007)
+
+
+  \par Version 0.9.1
+
+  \par
+   In Solvers/OKsolver/SAT2002/plans/general.hpp the following topics are handled:
+    - bug Uninitialised values with the old OKsolver
+
+
+ \par Version 0.9.2
+
+  \par
+   In Solvers/OKsolver/SAT2002/plans/general.hpp the following topics are handled:
+    - Language standards
+
+
+ \par Version 0.9.3
+
+  \par
+   In Solvers/OKsolver/SAT2002/plans/general.hpp the following topics are handled:
+    - Buildsystem
+
+
+ \par Version 0.9.4
+
+  \par
+   In Solvers/OKsolver/SAT2002/plans/general.hpp the following topics are handled:
+    - Complete the help facilities of the OKsolver
+
+
+ \par Version 0.9.5
+
+  \par
+   In Solvers/OKsolver/SAT2002/plans/general.hpp the following topics are handled:
+    - Add doxygen-documentation
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.9   : 18.8.2007; initial version number (the old OKsolver mainly unchanged since SAT2002; a bug found by Marijn Heule showed up with newer versions of gcc)
+
+*/
+
