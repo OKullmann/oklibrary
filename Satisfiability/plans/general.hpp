2f1b8e400d01441605c8dc46a2219b58bc98f56e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-18 12:20:27 +0100

    On planning at the level of a "part" of Transitional.
    It seems we should have only milestones at the level of
    "super-modules", the elements of a "part".

--- Satisfiability/plans/general.hpp
+++ Satisfiability/plans/general.hpp
@@ -5,6 +5,25 @@
   \brief Contains all super-modules regarding (generalised) satisfiability
   issues
 
+
+  \todo Milestones
+  <ul>
+   <li> It seems that at least for now, there are no milestones at the level
+   of super-module Satisfiability. </li>
+   <li> Having milestones also at this level seems artificial (for now).
+   One wants to say directly that Solvers or Heuristics should advance,
+   without introducing artificial combination-milestones. </li>
+   <li> However, like namespace administration, there are todos also at
+   this level? Perhaps for now they go directly to Transitional/plans? </li>
+   <li> The tree structure of the planning system (setting milestones must
+   follow the hierarchies: the "manager" cannot directly give orders
+   to the "workers") is a bit of a hindrance here, but in general it
+   seems appropriate. </li>
+  </ul>
+
+
+  \todo Update namespaces.
+
 */
 
 /*!
