bd4fe8b17b70e8311dc919127275c1dd55a9ce43
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-22 11:49:24 +0100

    *** Initial version 0.5 (Experimentation/RandomGenerator) ***
    Replaced old version number, which didn't reflect the existence of
    the OKgenerator (and the other tools).

--- Experimentation/RandomGenerator/plans/milestones.hpp
+++ Experimentation/RandomGenerator/plans/milestones.hpp
@@ -3,19 +3,30 @@
 /*!
   \file RandomGenerator/plans/milestones.hpp
 
-  \module_version Experimentation/RandomGenerator 0.0.3 (7.11.2006)
+  \module_version Experimentation/RandomGenerator 0.5 (22.8.2007)
 
-  \par Version 0.0.4 :
+
+  \par Version 0.5.1 :
   In RandomGenerator/plans/general.hpp the topic
-  - Schedule
+  - Basic revision
 
   is expanded, and further milestones are created.
+
+
+  \par Version 0.6 :
+  All tools are compiling and running correctly.
+
+
+  \par Version 0.7 :
+  All implementations and updated according to the OKlibrary-standard.
+
+  
 
   -------------------------------------------------------------------------------------------------------------------------------------
 
   \par Version history
 
-   -  0.0.3 : 9.11.2006; initial version number (first plans).
+   -  0.5   : 22.8.2007; initial version number (we have the OKgenerator and accompanying tools, however there are bugs related to the AES-implementation used and the reading of program-options, and the library used for big-integers is sub-standard (with macros like "gcd")).
 
 */
 
