98756c7f4cff0ed4fcb0bb344e3ddd4656541bd8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-22 01:01:46 +0100

    *** Initial version 0.0.4 (Graphs/BoostSupport) ***

---
+++ Combinatorics/Graphs/BoostSupport/plans/milestones.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 22.8.2007 (Swansea)
+
+/*!
+  \file Graphs/BoostSupport/plans/milestones.hpp
+
+  \module_version %Graphs/BoostSupport 0.0.4 (22.8.2007)
+
+  \par Version 0.0.5
+
+  \par
+   In Graphs/BoostSupport/plans/general.hpp the following topics are handled:
+    - Investigate whether there is something usable
+    - Collect
+
+  \par
+   Further milestones have been created.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 22.8.2006; initial version number (some components for input/output, some simple auxiliary components).
+
+*/
