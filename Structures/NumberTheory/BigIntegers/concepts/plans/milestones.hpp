e366840b74de8359980821825d25d6dbae0ff590
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-23 01:04:18 +0100

    *** Initial version 0.0.4 (BigIntegers/concepts) ***

---
+++ Structures/NumberTheory/BigIntegers/concepts/plans/milestones.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 23.8.2007 (Swansea)
+
+/*!
+  \file BigIntegers/concepts/plans/milestones.hpp
+
+  \module_version BigIntegers/concepts 0.0.4 (23.8.2007)
+
+
+  \par Version 0.0.5
+
+  \par
+   In BigIntegers/concepts/plans/general.hpp the following topics are handled:
+    - Boost
+    - The basic concept
+    - First client
+
+  \par
+   Further milestones are created.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 23.8.2007; initial version number (basic plans, and some experience in module RandomGenerator)
+
+*/
+
