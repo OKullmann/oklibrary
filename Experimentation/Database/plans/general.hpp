f0eafcee364853a36f51bd8b8ca9e4db442b543e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-23 15:17:22 +0100

    Updated plans for the (OK)database.

--- Experimentation/Database/plans/general.hpp
+++ Experimentation/Database/plans/general.hpp
@@ -1,28 +1,41 @@
 // Oliver Kullmann, 9.11.2006 (Swansea)
 
 /*!
-  \file OKDatabase/plans/OKDatabase.hpp
-  \brief Plans for the module concerning the random-formulas-database
+  \file Experimentation/Database/plans/general.hpp
+  \brief Plans for the module concerning the formulas-database
 
+
+  \todo Documentation
+  <ul>
+   <li> Add minimal doxygen-documentation to the C++ files. </li>
+  </ul>
+
+
+  \todo make
+  <ul>
+   <li> Shall we use the local postgresql-installation for compilation
+   (as include-source)? Perhaps as the default. </li>
+  </ul>
+
+
+  \todo Other files
+  <ul>
+   <li> How to treat the .dtd and .sql files? </li>
+  </ul>
+
+
+  \todo Transfer:
   First the old module is transferred to the OKlibrary, and then
-  it is extended.
+  it is extended. DONE (transferred; extensions are to be planned)
 
-  \todo Documentation:
-  Add minimal doxygen-documentation to the C++ files.
 
-  \todo OKgenerator:
+  \todo OKgenerator: DONE
   Connect to the module RandomGenerator (see RandomGenerator/plans/RandomGenerator.hpp), and to the old module OKsolver/Generator.
   This should concern mostly the tools for experiment-processing etc.
   Likely all those programs should be transferred to this module.
 
-  \todo make:
-  Shall we use the local postgresql-installation for compilation
-  (as include-source)? Perhaps as the default.
 
-  \todo Other files:
-  How to treat the .dtd and .sql files?
-
-  \todo Renaming:
+  \todo Renaming: DONE
   Once we can do directory-renaming with the version control system, this
   module should be renamed; possibilities:
   - OKdatabase
--- Experimentation/Database/plans/milestones.hpp
+++ Experimentation/Database/plans/milestones.hpp
@@ -1,31 +1,44 @@
 // Oliver Kullmann, 9.11.2006 (Swansea)
 
 /*!
-  \file OKDatabase/plans/milestones.hpp
+  \file Experimentation/Database/plans/milestones.hpp
 
-  \module_version OKDatabase 0.3 (9.11.2006)
+  \module_version Experimentation/Database 0.3 (9.11.2006)
+
 
   \par Version 0.3.1 :
-  In OKDatabase/plans/OKDatabase.hpp the following topics are handled:
-  - Documentation
+
+  \par
+   In Database/plans/general.hpp the following topics are handled:
+    - Transfer : DONE
+    - OKgenerator : DONE
+    - Renaming : DONE
+
   
   \par Version 0.3.2 :
-  In OKDatabase/plans/OKDatabase.hpp the following topics are handled:
-   - make
-   - Other files
 
-  And further milestones are created.
+  \par
+   In Database/plans/general.hpp the following topics are handled:
+    - Documentation
 
-  \par Version 0.4 :
-  The old system compiles (correctly) within the OKlibrary (using
-  the update random generator).
+
+  \par Version 0.3.3 :
+
+  \par
+   In Database/plans/general.hpp the following topics are handled:
+    - make
+    - Other files
+
+  \par
+   Further milestones are created.
+
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
 
   \par Version history
 
-   -  0.3 : 9.11.2006; initial version number (a running system).
+   - 0.3   : 9.11.2006; initial version number (a running system).
 
 */
 
