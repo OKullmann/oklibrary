f9c7fe2a694ff58184600eb3f6f0676de235c0e0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-23 17:07:25 +0100

    Update of plans for the competition evaluation.

--- Experimentation/Competition/plans/general.hpp
+++ Experimentation/Competition/plans/general.hpp
@@ -1,15 +1,31 @@
 // Oliver Kullmann, 2.3.2006 (Swansea)
 
 /*!
-  \file SATCompetition/plans/SATCompetition.hpp
+  \file Experimentation/Competition/plans/general.hpp
   \brief Plans for the module on the evaluation of the SAT-2005 competition
+  (and its general useage)
+
+
+  \todo Update namespaces
+  <ul>
+   <li> Introduce new namespaces and namespace-aliases according
+   to the new standard. </li>
+   <li> Update all files. </li>
+  </ul>
+
 
   \todo Update
-   - doxygen documentation for all files
-   - new plans files, transferring todos
+  <ul>
+   <li> doxygen documentation for all files </li>
+   <li> new plans files, transferring todos </li>
+   <li> Use anonymous namespaces in implementation files. </li>
+   <li> General updates of code according to our coding standard. </li>
+  </ul>
+
 
    \todo New tests
    Transfer to the new test system
+
 
    \todo Messages
    Use Messages.
--- Experimentation/Competition/plans/milestones.hpp
+++ Experimentation/Competition/plans/milestones.hpp
@@ -1,27 +1,47 @@
 // Oliver Kullmann, 18.11.2006 (Swansea)
 
 /*!
-  \file SATCompetition/plans/milestones.hpp
+  \file Experimentation/Competition/plans/milestones.hpp
 
-  \par Current version 0.5 (18.11.2006)
+  \module_version Experimentation/Competition 0.5 (18.11.2006)
+
 
   \par Version 0.5.1 :
-  In SATCompetition/plans/SATCompetition.hpp the following topics are handled:
-   - Update
-  And new milestones are created.
+
+  \par
+   In SATCompetition/plans/SATCompetition.hpp the following topics are handled:
+    - Update namespaces
+
+
+  \par Version 0.5.2 :
+
+  \par
+   In SATCompetition/plans/SATCompetition.hpp the following topics are handled:
+    - Update
+
+  \par
+   Create new milestones.
+
 
   \par Version 0.6 :
-  In SATCompetition/plans/SATCompetition.hpp the following topics are handled:
-   - New tests
-   - Messsages
-  And overall the module is fully documented.
+
+  \par
+   In SATCompetition/plans/SATCompetition.hpp the following topics are handled:
+    - New tests
+    - Messsages
+
+  \par
+   Overall the module is fully documented.
+
 
   \par Version 0.7 :
   The module is fully synchronised with the OKlibrary (using ProgramOptions etc.).
 
+
   \par Version 1.0 :
   A flexible and powerfull toolset is provided for evaluating various forms of competitions,
   fully integrated with the OKlibrary.
+
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
