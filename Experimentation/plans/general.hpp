3cda7f5996391c5ed320159922cea520991de133
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-23 17:55:55 +0100

    *** Initial version 0.4 (Experimentation) ***

---
+++ Experimentation/plans/general.hpp
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 23.8.2007 (Swansea)
+
+/*!
+  \file Experimentation/plans/general.hpp
+  \brief Plans for the experimentation support in general
+
+
+  \todo Update namespaces.
+  
+*/
+
