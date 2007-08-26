fac7073e5912c15bd050539c6dbff3692464d641
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-08-27 00:36:23 +0100

    *** New version 0.0.8 (Structures/Cryptology) ***

--- Structures/Cryptology/plans/general.hpp
+++ Structures/Cryptology/plans/general.hpp
@@ -1,20 +1,49 @@
 // Oliver Kullmann, 7.11.2006 (Swansea)
 
 /*!
-  \file Kryptologie/plans/Kryptologie.hpp
-  \brief Plans for the temporary module on cryptological tools
+  \file Structures/Cryptology/plans/Kryptologie.hpp
+  \brief Plans for the module on cryptological tools
 
 
-  \todo Update:
+  \todo Update: DONE
   First an overview on the content of the module, and what to do with it, is needed.
   <ul>
-   <li> A new module "Cryptology" should be created, with all these tools
-   (perhaps best in part "Structures"). </li>
-   <li> Most important is to create a concept for
+   <li> DONE (new todos created)
+   Most important is to create a concept for
    AES-encoding/decoding, choosing some reasonable implementation
    (Kryptologie/rijndael.hpp and Kryptologie/rijndael.cpp seem to be fine,
    at least for the start), and writing a wrapper for it (of course,
    testing it). </li>
+   <li> A new module "Cryptology" should be created, with all these tools
+   (perhaps best in part "Structures"). DONE </li>
+  </ul>
+
+
+  \todo Concept for aes-usage
+  <ul>
+   <li> Create a concept for AES-usage (see client Experimentation/RandomGenerator/BlockChiffAES.hpp). </li>
+   <li> Replace the C-header in rijndael.hpp by the corresponding C++ headers. </li>
+   <li> Create doxygen-documentation for rijndael.hpp/.cpp. </li>
+   <li> Implement the AES-concept by rijndael. </li>
+   <li> Create a generic test function for AES (see the tests in
+   Experimentation/RandomGenerator) </li>
+   <li> Test the AES-wrapper of rijndael. </li>
+  </ul>
+
+
+  \todo MHash
+  <ul>
+   <li> Update the usage of the MHash-library. </li>
+   <li> Are the newer version of the MHash-library? </li>
+   <li> HashMD5.cpp shall become an application. </li>
+   <li> What about "newHash" ? </li>
+  </ul>
+
+
+  \todo OKgenerator
+  <ul>
+   <li> Either transfer or remove the file with link-library-information for the
+   OKgenerator. </li>
   </ul>
 
 */
--- Structures/Cryptology/plans/milestones.hpp
+++ Structures/Cryptology/plans/milestones.hpp
@@ -1,14 +1,24 @@
 // Oliver Kullmann, 18.6.2006 (Swansea)
 
 /*!
-  \file Kryptologie/plans/milestones.hpp
+  \file Structures/Cryptology/plans/milestones.hpp
 
-  \module_version Kryptologie 0.0.7 (7.11.2006)
+  \module_version Structures/Cryptology 0.0.8 (27.8.2007)
 
 
-  \par Version 0.0.8 :
-  In Kryptologie/plans/Kryptologie.hpp the topic
-  "Update" has been handled, and further milestones created.
+  \par Version 0.0.9 :
+
+  \par
+   In Structures/Cryptology/plans/Kryptologie.hpp the following topics have been handled:
+    - Concept for aes-usage
+
+
+  \par Version 0.1 :
+
+  \par
+   In Structures/Cryptology/plans/Kryptologie.hpp the following topics have been handled:
+    - MHash
+    - OKgenerator
 
 
 
@@ -16,6 +26,7 @@
 
   \par Version history
 
-   -  0.0.7 : 7.11.2006; initial version number (some imported software).
+   - 0.0.7 : 7.11.2006; initial version number (some imported software).
+   - 0.0.8 : 27.8.2007; reorganisation.
 
 */
