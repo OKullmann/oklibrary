f6d450403927bc380f4bec3e7d7dbcdfafd85224
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-04 09:42:44 +0100

    Plans for package building made more concrete.

--- Buildsystem/ReleaseProcess/plans/PackageBuilding.hpp
+++ Buildsystem/ReleaseProcess/plans/PackageBuilding.hpp
@@ -12,29 +12,9 @@
    <li> The appropriate "user clone" has to be created in advance
    (see Buildsystem/ReleaseProcess/plans/Release.hpp) --- and also this clone has
    been tested. </li>
-   <li> A package exists for every new version number of Transitional. </li>
-   <li> Packages need precise names; proposal (for the next release):
-   \verbatim
-OKlib_0.1.6_31072007
-   \endverbatim
-   this is, after the name comes the version number, and then the date
-   (day) of packaging. But this is non-standard, so better we use
-   \verbatim
-OKlibrary-0.1.6
-   \endverbatim
-    <ol>
-     <li> Should also the time be included? Likely not (it must not
-     matter). </li>
-     <li> Should the name be "OKplatform" ? </li>
-     <li> Regarding the date formatting, we should stick to this
-     European form. </li>
-     <li> Parts (without the external libraries etc.) should be suffixed
-     like "-minimal". </li>
-    </ol
-   </li>
-   <li> Packages must be stored in an accesible depot. At least the
+   <li> Packages must be stored in an accessible depot. At least the
    core-packages need to be backed-up on cs-oksvr. See
-   "ExternalSources repository" in Buildsystem/plans/Release.hpp. </li>
+   "ExternalSources repository" in Buildsystem/ReleaseProcess/plans/Release.hpp. </li>
    <li> Package data must be on the Internet-page
    (as automatical as possible).
     <ol>
@@ -43,22 +23,72 @@ OKlibrary-0.1.6
     </ol>
    </li>
    <li> Also a ((semi-)automatical) e-mail to the notification list. </li>
-   <li> Shall we make a distinction between "major" and "minor" versions? </li>
-  </ul>
+   <li> DONE (basic ideas are clear now; text needs to be transferred to full
+   documentation)
+   Packages are called like
+   \verbatim
+OKlibrary-0.2.0.8
+   \endverbatim
+   where "0.2.0" is the main version number and ".8" the current "stage"
+   (the whole string "0.2.0.8" is given by make-variable "transitional_version").
+    <ol>
+     <li> So a new package is only created when at least the current stage
+     is advanced; users who wish to by fully update use Git. </li>
+     <li> Parts (without the external libraries etc.) should be suffixed
+     like "-minimal". </li>
+     <li> Regarding the date formatting, we should stick to this
+     European form. DONE (no dates in the name; otherwise we use for
+     example "04.09.2007") </li>
+     <li> Should the name be "OKplatform" ? DONE (no, yet no use
+     for OKplatform) </li>
+     <li> DONE (no time included; this is available via the history)
+     Should also the time be included? Likely not (it must not
+     matter). </li>
+    </ol>
+   </li>
+   <li> DONE (explained in more details in the next item)
+   A package exists for every new version number of Transitional. </li>
+   <li> DONE (question answered; to be entered into the full documentation)
+   Shall we make a distinction between "major" and "minor" versions? Yes,
+   in the documentation and announcements we make a differences between the
+   four types of advances (related to the four digits): Advancing to
+   "1.0.0.0" should be a "big thing", advancing to "0.3.0.0" still gets
+   some publicity, advancing to "0.2.1.0" is announced on internal mailing
+   lists, still with explanations what did change, while finally advancing
+   to "0.2.0.4" only creates automated messages. </li>
+   <li> DONE (no, we use standard package names, not such home-cooked)
+   Packages need precise names; proposal (for the next release):
+   \verbatim
+OKlib_0.1.6_31072007
+   \endverbatim
+   this is, after the name comes the version number, and then the date
+   (day) of packaging. </li>
+   </ul>
 
 
   \todo The script
   <ul>
+   <li> Synchronise the following with "Distributing the library" in
+   Buildsystem/ReleaseProcess/plans/Release.hpp. </li>
+   <li> Name of this Bash script: "CreatePackage". </li>
    <li> Main steps for the full package:
     <ol>
      <li> mkdir OKplatform etc. </li>
      <li> Copy the user-clone. </li>
      <li> Copy external sources. </li>
-     <li> make prebuild </li>
-     <li> make html </li>
+     <li> Copy documents. </li>
+     <li> Run Buildsystem/Makefile (for setup). </li>
+     <li> oklib --prebuild </li>
+     <li> oklib html </li>
      <li> Compress. </li>
     </ol>
    </li>
+   <li> For this to work we need to make the internal url's relative.
+   See "Install configuration system" in Buildsystem/Html/plans/general.hpp. </li>
+   <li> So a complete package comes with documentation, and for the rest
+   the standard build system can be used (no need for additional makefiles).
+   However, there should be additional targets for the oklib-masterscript
+   to "compile and test" everything. </li>
    <li> Variations:
     <ol>
      <li> "minimal" without external sources and html. </li>
