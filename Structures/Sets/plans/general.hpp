e1562786d0fe9f5c5c40202f3359da53fd7b0b08
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-08 12:47:10 +0100

    Update on sets and ordinal numbers.

---
+++ Structures/Sets/plans/general.hpp
@@ -0,0 +1,14 @@
+// Oliver Kullmann, 8.9.2007 (Swansea)
+
+/*!
+  \file Structures/Sets/plans/general.hpp
+  \brief Plans regarding set theory and set algorithms in general
+
+
+  \todo Create a milestones file.
+
+
+  \todo Move module SetAlgorithms to here.
+
+*/
+
