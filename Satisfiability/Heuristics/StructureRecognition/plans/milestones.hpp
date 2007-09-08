89ac9bc71cbfdb37db49d5f108d0fe7d0f4fa5fc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-08 21:00:42 +0100

    *** Initial version 0.0.1 (Heuristics/StructureRecognition) ***

---
+++ Satisfiability/Heuristics/StructureRecognition/plans/milestones.hpp
@@ -0,0 +1,34 @@
+// Oliver Kullmann, 8.9.2007 (Swansea)
+
+/*!
+  \file Heuristics/StructureRecognition/plans/milestones.hpp
+  
+  \module_version Heuristics/StructureRecognition 0.0.1 (8.9.2007)
+
+
+  \par Version 0.0.2
+
+  \par
+   In Heuristics/StructureRecognition/plans/general.hpp the following topics are handled:
+    - Update namespaces : DONE
+    - Move Transitional/Statistics/plans/RecogniseRandomFormulas.hpp : DONE
+
+
+  \par Version 0.0.3
+
+  \par
+   In Heuristics/StructureRecognition/plans/RecogniseRandomFormulas.hpp the following topics are handled:
+
+  \par
+   Further milestones are created.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.1 : 8.9.2007; some vague ideas.
+
+*/
+
