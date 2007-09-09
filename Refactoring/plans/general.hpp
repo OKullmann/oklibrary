ab0d581d56f0a837b65eb7806a901d8f11e47dbd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-09 21:05:13 +0100

    Updated plans on refactoring tools.

--- Refactoring/plans/general.hpp
+++ Refactoring/plans/general.hpp
@@ -1,17 +1,29 @@
 // Oliver Kullmann, 18.11.2006 (Swansea)
 
 /*!
-  \file Refactoring/plans/Refactoring.hpp
+  \file Refactoring/plans/general.hpp
   \brief Plans for the module on refactoring
+
+
+  \todo Move this module to part Programming.
+
 
   \todo Update the doxygen-documentation, and create further plans(-files).
 
+
   \todo Transfer the tests to the new test system.
+
 
   \todo What about Refactoring/plans/Transition_Plans.hpp ?
 
+
   \todo Investigating existing tools for parsing and refactoring C++ code.
-  Asking on the boost e-mail list.
+  <ul>
+   <li> There was an announcement recently (September 2007) on the boost e-mail list
+   about a C/C++ parser etc. library (supported by Apple). See what they have
+   on refactoring. </li>
+   <li> There should be C tools for refactoring --- those should be also useful. </li>
+  </ul>
 
 */
 
