7604973ab01516d33668d994ca6cff8fa2d49a53
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-09 01:07:48 +0100

    Added const-qualification.
    Likely in the future additionally also non-const versions are needed.

--- Programming/Sequences/Comparisons.hpp
+++ Programming/Sequences/Comparisons.hpp
@@ -16,6 +16,8 @@
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
 
+#include <Transitional/Programming/Sequences/declarations.hpp>
+
 namespace OKlib {
   namespace Programming {
     namespace Sequences {
@@ -41,19 +43,18 @@ namespace OKlib {
         </ul>
 
       */
-
+      
+      template <class Range1, class Range2, class ImpPol>
+      class Common_part;
+      
       struct Implementation_common_part {};
       struct Implementation_common_part_std : virtual Implementation_common_part {};
       struct Implementation_common_part_self : virtual Implementation_common_part {};
-
-      template
-      <class Range1, class Range2, class ImpPol>
-      class Common_part;
-
+      
       template
       <class Range1, class Range2>
       struct Common_part<Range1, Range2, Implementation_common_part_std> {
-        typedef typename boost::range_iterator<Range1>::type iterator_type;
+        typedef typename boost::range_const_iterator<Range1>::type iterator_type;
         iterator_type operator()(const Range1& r1, const Range2& r2) const {
           if (boost::size(r1) <= boost::size(r2))
             return std::mismatch(boost::begin(r1), boost::end(r1), boost::begin(r2)).first;
@@ -64,9 +65,9 @@ namespace OKlib {
       template
       <class Range1, class Range2>
       struct Common_part<Range1, Range2, Implementation_common_part_self> {
-        typedef typename boost::range_iterator<Range1>::type iterator_type;
+        typedef typename boost::range_const_iterator<Range1>::type iterator_type;
         iterator_type operator()(const Range1& r1, const Range2& r2) const {
-          typedef typename boost::range_iterator<Range2>::type iterator2_type;
+          typedef typename boost::range_const_iterator<Range2>::type iterator2_type;
           const iterator_type& end(boost::end(r1));
           const iterator2_type& end2(boost::end(r2));
           iterator_type it(boost::begin(r1));
