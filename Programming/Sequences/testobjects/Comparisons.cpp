fca2f1e5a7cbaa3934d6c691f7ab19482edf578d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-09 01:13:04 +0100

    Very basic testing for "common part" (of sequences/ranges).

--- Programming/Sequences/testobjects/Comparisons.cpp
+++ Programming/Sequences/testobjects/Comparisons.cpp
@@ -2,9 +2,17 @@
 
 #include <Transitional/TestSystem/RunTest_DesignStudy.hpp>
 
+#include <Transitional/TestSystem/RunTest_DesignStudy.hpp>
+
+#include <Transitional/Programming/Sequences/declarations.hpp>
+#include <Transitional/Programming/Sequences/Comparisons.hpp>
 #include <Transitional/Programming/Sequences/tests/Comparisons.hpp>
 
 namespace {
+
+  ::OKlib::TestSystem::RunTest test_common_part_std(new ::OKlib::Prg::Seq::tests::CommonPart< ::OKlib::Prg::Seq::Common_part, ::OKlib::Prg::Seq::Implementation_common_part_std>);
+
+  ::OKlib::TestSystem::RunTest test_common_part_self(new ::OKlib::Prg::Seq::tests::CommonPart< ::OKlib::Prg::Seq::Common_part, ::OKlib::Prg::Seq::Implementation_common_part_self>);
 
 }
 
--- Programming/Sequences/tests/Comparisons.hpp
+++ Programming/Sequences/tests/Comparisons.hpp
@@ -12,24 +12,53 @@
 #ifndef COMPARISONS_nnxcad5410o
 #define COMPARISONS_nnxcad5410o
 
-#include <Transitional/TestSystem/TestBaseClass_DesignStudy.hpp>
+#include <vector>
 
-#include <Transitional/Programming/Sequences/Comparisons.hpp>
+#include <Transitional/TestSystem/TestBaseClass_DesignStudy.hpp>
 
 namespace OKlib {
   namespace Programming {
     namespace Sequences {
       namespace tests {
 
+        /*!
+          \class CommonPart
+          \brief Testing functors for computing longest common initial sequences
+        */
+
 # define OKLIB_FILE_ID new ::OKlib::Messages::Utilities::FileIdentification \
       (__FILE__, __DATE__, __TIME__, "$Date: 8.9.2007 23:20:00 $", "$Revision: 1 $")
 
-        
+        template <template <class R1, class R2, class IP> class CP, class ImpPol>
+        OKLIB_TEST_CLASS(CommonPart) {
+          OKLIB_TEST_CLASS_C(CommonPart) {}
+        private :
+          void test(::OKlib::TestSystem::Basic) {
+            {
+              typedef std::vector<int> seq_t;
+              typedef CP<seq_t, seq_t, ImpPol> com_t;
+              com_t com;
+              seq_t v1, v2;
+              if (com(v1, v2) != v1.begin())
+                OKLIB_THROW("OKLIB_TEST_EQUAL");
+              v1.push_back(1);
+              if (com(v1, v2) != v1.begin())
+                OKLIB_THROW("OKLIB_TEST_EQUAL");
+              v2.push_back(1);
+              if (com(v1, v2) != v1.end())
+                OKLIB_THROW("OKLIB_TEST_EQUAL");
+               v2.push_back(2);
+              if (com(v1, v2) != v1.end())
+                OKLIB_THROW("OKLIB_TEST_EQUAL");
+            }
+          }
+        };
 
       }
     }
   }
 }
 
+# undef OKLIB_FILE_ID
 #endif
 
