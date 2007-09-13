82852fa2ddbe05a03baaf0537f9daca4e52c8748
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-13 11:38:49 +0100

    Update on text to be included in every file.
    Now the shorter version, but mentioning that the licence text comes
    with the library.

--- System/LegalIssues/Header.s
+++ System/LegalIssues/Header.s
@@ -1,9 +1,5 @@
 # Copyright XXXX-2007 Oliver Kullmann
 # This file is part of the OKlibrary.
 # OKlibrary is free software; you can redistribute it and/or modify it under the terms of
-# the GNU General Public License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
-# The OKlibrary is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details. A copy of the GNU General Public License 
-# is included in this library; see also <http://www.gnu.org/licenses/>.
+# the GNU General Public License as published by the Free Software Foundation and included
+# in this library; either version 3 of the License, or (at your option) any later version.
