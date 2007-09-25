ea150ab3e7f6fd03ae20c91462d023327f94b094
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-25 11:19:39 +0100

    *** Initial version 0.0.4 (Combinatorics/Enumeration) ***

---
+++ Combinatorics/Enumeration/plans/general.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 25.9.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Combinatorics/Enumeration/plans/general.hpp
+  \brief General plans on the enumeration module.
+
+
+  \todo Update namespaces.
+
+
+  \todo Create a development plan and milestones.
+
+*/
---
+++ Combinatorics/Enumeration/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 25.9.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Combinatorics/Enumeration/plans/milestones.hpp
+
+  \module_version Combinatorics/Enumeration 0.0.4 (25.9.2007)
+
+
+  \par Version 0.0.5 :
+
+  \par
+   In Combinatorics/Enumeration/plans/general.hpp the following
+   topics are handled:
+    - Update namespaces
+    Create a development plan and milestones.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 25.9.2007; initial version number (several quite extensive plans).
+
+*/
+
