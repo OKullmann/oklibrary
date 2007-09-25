43e3efc6008defab849f74c273ef2437d767d7bf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-25 10:42:56 +0100

    *** Initial version 0.0.3 (Combinatorics/Hypergraphs) ***

---
+++ Combinatorics/Hypergraphs/plans/milestones.hpp
@@ -0,0 +1,40 @@
+// Oliver Kullmann, 25.9.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Combinatorics/Hypergraphs/plans/milestones.hpp
+
+  \module_version Combinatorics/Hypergraphs 0.0.3 (25.9.2007)
+
+
+  \par Version 0.0.4 :
+
+  \par Combinatorics/Hypergraphs/concepts 0.0.4
+  (see Combinatorics/Hypergraphs/concepts/plans/milestones.hpp)
+
+  \par Combinatorics/Hypergraphs/Transversals 0.0.5
+  (see Hypergraphs/Transversals/plans/milestones.hpp)
+
+  \par
+   In Hypergraphs/plans/general.hpp the following
+   topics are handled:
+    - Update namespaces
+
+  \par
+   Create further milestones.
+
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 25.9.2007; initial version number (several plans).
+
+*/
+
