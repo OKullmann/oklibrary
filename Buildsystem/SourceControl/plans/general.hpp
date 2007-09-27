ea7a3bde25a54e2d830591538aa5577aa8d1fc60
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-09-27 09:15:47 +0100

    Update Git-space-usage.

--- Buildsystem/SourceControl/plans/general.hpp
+++ Buildsystem/SourceControl/plans/general.hpp
@@ -405,6 +405,7 @@ git mv file1 file2 dir1 dir2 Annotations
      before measurement) </li>
      <li> 16.9.2007; 5.8 MB; Transitional total: 18.6 MB </li>
      <li> 23.9.2007; 6.0 MB; Transitional total: 19.2 MB </li>
+     <li> 26.9.2007; 6.1 MB; Transitional total: 19.5 MB </li>
     </ol>
    </li>
   </ul>
