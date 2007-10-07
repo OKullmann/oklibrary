be096499f5398881c52839a2770c7836936e4941
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-07 16:10:07 +0100

    *** Initial version 0.0.1 (ComputerAlgebra/Satisfiability/Aldor) ***

---
+++ ComputerAlgebra/Satisfiability/Aldor/plans/general.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 7.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Aldor/plans/general.hpp
+  \brief General plans regarding SAT at the Aldor/Axiom level
+
+
+  \todo Develop plans
+  <ul>
+   <li> After the installation of Axiom/Aldor, some experience
+   is needed. </li>
+  </ul>
+
+
+  \todo Create milestones.
+
+*/
+
---
+++ ComputerAlgebra/Satisfiability/Aldor/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 7.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Aldor/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Satisfiability/Aldor 0.0.1 (7.10.2007)
+
+
+  \par Version 0.0.2
+
+  In ComputerAlgebra/Satisfiability/Aldor/plans/general.hpp the following
+  topics are handled:
+   - Develop plans
+   - Create milestones.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.1 : 7.10.2007; initial version number (just a few general ideas).
+
+*/
+
