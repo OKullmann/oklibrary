39f6316c5723f4c425faa8ba71f5411af9d6a8cd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-07 12:17:12 +0100

    *** Initial version 0.0.4 (ComputerAlgebra/Satisfiability) ***

---
+++ ComputerAlgebra/Satisfiability/plans/milestones.hpp
@@ -0,0 +1,37 @@
+ 
+// Oliver Kullmann, 6.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Satisfiability 0.0.4 (6.10.2007)
+
+
+  \par Version 0.0.5
+
+  \par ComputerAlgebra/Satisfiability/Lisp version 0.0.6
+  (see ComputerAlgebra/Satisfiability/Lisp/plans/milestones.hpp)
+
+  \par
+   In ComputerAlgebra/Satisfiability/plans/general.hpp the following
+   topics are handled:
+    - Create a development plan
+
+  \par
+   Create further milestones.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 6.10.2007; initial version; started transferring the Mupad-code to the Lisp-module
+
+*/
+
