313abc68a034dc880d39e54b74ce2da9c28b9ff3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-09 21:57:13 +0100

    First attempt to install Maxima.
    Must ask on the mailing list.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/Maxima.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/Maxima.hpp
@@ -12,7 +12,46 @@ License, or any later version. */
 
   \todo Install Maxima
   <ul>
-   <li> Maxima 5.13 seems appropriate. </li>
+   <li> Maxima 5.13 seems appropriate.
+    <ol>
+     <li> First install clisp (should we supply this?). </li>
+     <li>
+     \verbatim
+Installations> mkdir Maxima
+builds> mkdir Maxima
+Maxima> tar -xzf ../../sources/Maxima/maxima-5.13.0.tar.gz
+Maxima> cd maxima-5.13.0/
+maxima-5.13.0> LANG=C ../configure --prefix=${OKplatform}/ExternalSources/Installations/Maxima
+maxima-5.13.0> LANG=C make
+     \endverbatim
+     yields
+     \verbatim
+Compiling file /home/kullmann/csoliver/SAT-Algorithmen/OKplatform/ExternalSources/builds/Maxima/maxima-5.13.0/src/clmacs.lisp ...
+*** - READ from
+#<INPUT BUFFERED FILE-STREAM CHARACTER
+  #P"/home/kullmann/csoliver/SAT-Algorithmen/OKplatform/ExternalSources/builds/Maxima/maxima-5.13.0/src/clmacs.lisp"
+  @317>: #<PACKAGE CUSTOM> has no external symbol with name "*FLOATING-POINT-RATIONAL-CONTAGION-ANSI*"
+The following functions were used but not defined:
+ MAXIMA::MFUNCALL
+0 errors, 0 warnings
+make[1]: *** [binary-clisp/maxima.mem] Error 1
+make[1]: Leaving directory `/home/kullmann/csoliver/SAT-Algorithmen/OKplatform/ExternalSources/builds/Maxima/maxima-5.13.0/src'
+make: *** [all-recursive] Error 1
+     \endverbatim
+     ??? The same error without setting prefix.
+     \verbatim
+maxima-5.13.0> clisp --version
+WARNING: *FOREIGN-ENCODING*: reset to ASCII
+GNU CLISP 2.33.2 (2004-06-02) (built on g242.suse.de [10.10.103.242])
+Software: GNU-C 3.3.4 (pre 3.3.5 20040809) ANSI-C-Programm
+Features:
+(CLOS LOOP COMPILER CLISP ANSI-CL COMMON-LISP LISP=CL INTERPRETER SOCKETS GENERIC-STREAMS
+ LOGICAL-PATHNAMES SCREEN FFI GETTEXT UNICODE BASE-CHAR=CHARACTER PC386 UNIX)
+Installation directory: /usr/lib/clisp/
+User language: ENGLISH
+Machine: I686 (I686) csltok.swan.ac.uk [128.163.146.167]
+     \endverbatim
+   </li>
    <li> The installation has a sub-target "access-script":
     <ol>
      <li> This Bash-script is copied to ~/bin (overwriting existing
