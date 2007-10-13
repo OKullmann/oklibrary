c737076c5469de0b8a82d4b3e94910145c33d70d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-13 23:51:27 +0100

    Basic code maintenance.
    Editorial, variable locality, const-correctness.

--- Satisfiability/Solvers/OKsolver/SAT2002/BaumRes.c
+++ Satisfiability/Solvers/OKsolver/SAT2002/BaumRes.c
@@ -10,16 +10,9 @@ License, or any later version. */
   \brief Implementations of the functions for tree pruning
 */
 
-                       /* OKsolver; 5.3.1998 */
-
-/* Autor: Oliver Kullmann, Universitaet Frankfurt am Main, Germany */
-
-/* Angelegt: 16.3.2001 */
-
-/* Einheit: BaumRes */
-
 #include <stdlib.h>
 #include <string.h> /* fuer C++ (memset; 14.8.2001) */
+#include <assert.h>
 
 #include "OK.h"
 #include "VarLitKlm.h"
@@ -44,66 +37,53 @@ VarMenge aktrelV0;
 /* ------------------------------------------------------------------------- */
 
 
-size_t BedarfBaumResV( void )
-{
+size_t BedarfBaumResV( void ) {
   return 2 * GroesseVarMenge;
 }
 
-void *BaumResV(void *Z)
-{
+void *BaumResV(void* Z) {
   aktrelV = (VarMenge) Z; Z = (void *) (aktrelV + ANZWORTE);
   aktrelV0 = (VarMenge) Z; Z = (void *) (aktrelV0 + ANZWORTE);
   return Z;
 }
 
 
-void AufraeumenBaumRes( void )
-{
-  Pfadinfo *Z; unsigned int i;
-  if (Pfad != NULL)
-    {
-      for (Z = Pfad, i = 0; i < N; Z++, i++)
-	free(Z -> M);
-      Pfad = NULL;
+void AufraeumenBaumRes( void ) {
+  if (Pfad != NULL) {
+    Pfadinfo* Z = Pfad;
+    for (unsigned int i = 0; i < N; ++Z, ++i) {
+      assert(Z);
+      free(Z -> M);
     }
+    Pfad = NULL;
+  }
 }
-
 
 /* ------------------------------------------------------------------------- */
 
 
-
-__inline__ bool enthalten (VAR v)
-{
+__inline__ bool enthalten (const VAR v) {
   return ((*(VPosition(v)) & VMaske(v)) != 0UL);
 }
 
-
-
-__inline__ static void hinzufuegen (VarMenge M)
-{
-  ZWort Z;
-  for (Z = aktrelV; Z != aktrelV0; Z++, M++)
-      *Z |= *M;
+__inline__ static void hinzufuegen (VarMenge M) {
+  for (ZWort Z = aktrelV; Z != aktrelV0; ++Z, ++M)
+    *Z |= *M;
 }
 
-
-
-__inline__ void hinzufuegenKl (KLN k)
-{
+__inline__ void hinzufuegenKl (const KLN k) {
 #ifndef LITTAB
   hinzufuegen(VarK(k));
 #else
-  VarMaske *z; unsigned int i;
-  for (z = ersteVK(k), i = UrLaenge(k); i > 0; z++, i--)
+  VarMaske* z = ersteVK(k);
+  for (unsigned int i = UrLaenge(k); i > 0; ++z, --i) {
+    assert(z -> Position);
     *(z -> Position) |= (z -> Maske);
+  }
 #endif
 }
 
-
-
-__inline__ void setzenKl (KLN k)
-{
+__inline__ void setzenKl (const KLN k) {
 #ifndef LITTAB
   memcpy((void *) aktrelV, (void *)(VarK(k)), GroesseVarMenge);
 #else
@@ -112,61 +92,45 @@ __inline__ void setzenKl (KLN k)
 #endif
 }
 
-
-
-__inline__ void relVhinzufuegen (void)
-{
-  KLN k;
-  k = Tiefe -> k;
+__inline__ void relVhinzufuegen (void) {
+  const KLN k = Tiefe -> k;
   if (k != NULL)
     hinzufuegenKl(k);
   else
     hinzufuegen(Tiefe -> M);
 }
 
-__inline__ void relVMhinzufuegen (void)
-{
+__inline__ void relVMhinzufuegen (void) {
   hinzufuegen(Tiefe -> M);
 }
 
 
 /* die naechste Operation ist wohl die am haeufigsten benutzte */
 
-__inline__ void Kln_eintragen_relV(KLN k)
-{
+__inline__ void Kln_eintragen_relV(const KLN k) {
   Tiefe -> k = k;
 }
 
-
-
-__inline__ void aktV_eintragen_relV(void)
-{
+__inline__ void aktV_eintragen_relV(void) {
   if (Tiefe -> M == NULL)
     Tiefe -> M = (VarMenge) xmalloc(GroesseVarMenge);
   memcpy((void *)(Tiefe -> M), (void *) aktrelV, GroesseVarMenge);
-}
-  
+} 
 
-__inline__ void aktV_volleintragen_relV(void)
-{
+__inline__ void aktV_volleintragen_relV(void) {
   Tiefe -> k = NULL;
   if (Tiefe -> M == NULL)
     Tiefe -> M = (VarMenge) xmalloc(GroesseVarMenge);
   memcpy((void *)(Tiefe -> M), (void *) aktrelV, GroesseVarMenge);
 }
 
-
-__inline__ void aktV_speichern( void )
-{
+__inline__ void aktV_speichern( void ) {
   memcpy((void *) aktrelV0, (void *) aktrelV, GroesseVarMenge);
 }
 
-__inline__ void hinzufuegenS ( void )
-{
+__inline__ void hinzufuegenS ( void ) {
   hinzufuegen(aktrelV0);
 }
 
-
 #endif
-
 
