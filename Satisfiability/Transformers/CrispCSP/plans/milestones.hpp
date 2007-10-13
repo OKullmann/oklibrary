052f4129c764f4a23e33a85b44a89c9b0e70a935
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-13 03:09:58 +0100

    *** Initial version 0.0.2 (Transformers/CrispCSP) ***

---
+++ Satisfiability/Transformers/CrispCSP/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 13.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Satisfiability/Transformers/CrispCSP/plans/milestones.hpp
+  
+  \module_version Transformers/CrispCSP 0.0.2 (13.10.2007)
+
+
+  \par Version 0.0.3
+
+  \par
+   In CrispCSP/plans/general.hpp the following
+   topics are handled:
+    - Update namespaces
+    - Create milestones
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 13.10.2007; initial version number (some plans).
+
+*/
+
