482426f696cc19d18cefa57bb3946f32e43dfb4e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-13 03:10:24 +0100

    *** Initial version 0.0.1 (Transformers/SATModuloTheories) ***

---
+++ Satisfiability/Transformers/SATModuloTheories/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 13.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Satisfiability/Transformers/SATModuloTheories/plans/milestones.hpp
+  
+  \module_version Transformers/SATModuloTheories 0.0.1 (13.10.2007)
+
+
+  \par Version 0.0.2
+
+  \par
+   In SATModuloTheories/plans/general.hpp the following
+   topics are handled:
+    - Update namespaces
+    - Create milestones
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.1 : 13.10.2007; initial version number.
+
+*/
+
