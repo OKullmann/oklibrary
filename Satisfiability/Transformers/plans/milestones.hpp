aa74e6d1e3d617e5abfd20a62b5b181fd53b7984
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-10-13 03:10:56 +0100

    *** Initial version 0.0.3 (Satisfiability/Transformers) ***

---
+++ Satisfiability/Transformers/plans/milestones.hpp
@@ -0,0 +1,40 @@
+// Oliver Kullmann, 13.10.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Satisfiability/Transformers/plans/milestones.hpp
+  
+  \module_version Satisfiability/Transformers 0.0.3 (13.10.2007)
+
+
+  \par Version 0.0.4
+
+  \par Transformers/Generators version 0.0.6
+  (see Satisfiability/Transformers/Generators/plans/milestones.hpp)
+
+  \par Transformers/SATModuloTheories version 0.0.2
+  (see Satisfiability/Transformers/SATModuloTheories/plans/milestones.hpp)
+
+  \par Transformers/CrispCSP version 0.0.3
+  (see Satisfiability/Transformers/CrispCSP/plans/milestones.hpp)
+
+  \par
+   In Satisfiability/Transformers/plans/general.hpp the following
+   topics are handled:
+    - Update namespaces
+    - Create milestones
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 13.10.2007; initial version number (several old generators, and plans).
+
+*/
+
