af577bda4ddb9fff3c78c0bfbbea4001aeca61fd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-11-24 23:04:52 +0000

    Update of plans on generating subsets.

--- Combinatorics/Enumeration/plans/Subsets.hpp
+++ Combinatorics/Enumeration/plans/Subsets.hpp
@@ -10,39 +10,56 @@ License, or any later version. */
   \brief %Tools for enumeration of all subsets of a set.
 
 
-  \todo Concept of a subset: a set (sequence) of iterators.
-  So a general concept of a set (container, sequence) is needed; or perhaps just using
-  ranges is enough?!
+  \todo Concepts
+  <ul>
+   <li> A subset: a set (sequence) of iterators. </li>
+   <li> So a general concept of a set (container, sequence) is needed; or perhaps
+   just using ranges is enough?! </li>
+   <li> An iterator over all subsets (or, for example, over all subsets of size k)
+   dereferenced should yield the subset as an in-place-modification of some internal
+   storage. </li>
+   <li> Identifying a subset with its characteristic function, we can use the tools
+   from GrayCodes.hpp to enumerate all subsets (flipping only one element at a time).
+   The iterator should made available the information about the element flipped.
+   </li>
+  </ul>
 
 
-  \todo An iterator over all subsets (or, for example, over all subsets of size k)
-  dereferenced should yield the subset as an in-place-modification of some internal storage.
+  \todo Organisation
+  <ul>
+   <li> We should create a sub-module "Subsets" of Combinatorics/Enumeration. </li>
+   <li> Combinatorics.hpp and related files shall move to that new sub-module
+   according to General/plans/General.hpp. </li>
+  </ul>
 
 
-  \todo Identifying a subset with its characteristic function, we can use the tools
-  from GrayCodes.hpp to enumerate all subsets (flipping only one element at a time).
-  The iterator should made available the information about the element flipped.
+  \todo Algorithmic framework
+  <ul>
+   <li> There should be a general algorithmic framework, allowing to run through
+   all subsets (or "sub-structures"), applying an algorithm to each subset, and
+   gathering statistics. </li>
+   <li> An application is as follows: For given n, k compute the clause-sets with
+   all k-clauses over variables 1, ..., n (there should be generator for this,
+   generalising the complete hypergraphs K_n^k). To each sub-clause-set apply the
+   enumeration-SAT-algorithm, gathering as much information as possible (at least
+   for each clause-count c we need the number of satisfiable clause-sets --- with
+   this we can compute the probability that a random clause-sequence is satisfiable).
+   </li>
+   <li> See SATAlgorithms/EnumerationSubclausesets.hpp for a generic algorithm for
+   clause-sets. Perhaps later this framework might be generalised. </li>
+  </ul>
 
 
-  \todo Refinements:
-   - Enumerating all partial hypergraphs of a hypergraph with bipartite edge-vertex graph
-     should yield also these bipartite graphs (respectively the corresponding information)
-     for the partial hypergraphs.
-   - In the same vain, when enumerating all sub-clause-sets of a clause-set (or something
-     similar) with such an auxiliary structure, then the enumeration should (possibly)
-     make available this structure also for the enumerated objects.
-
-
-  \todo There should be a general algorithmic framework, allowing to run through all
-  subsets (or "sub-structures"), applying an algorithm to each subset, and gathering
-  statistics. An application is as follows: For given n, k compute the clause-sets with
-  all k-clauses over variables 1, ..., n (there should be generator for this, generalising
-  the complete hypergraphs K_n^k). To each sub-clause-set apply the enumeration-SAT-
-  algorithm, gathering as much information as possible (at least for each clause-count c
-  we need the number of satisfiable clause-sets --- with this we can compute the probability
-  that a random clause-sequence is satisfiable).
-  See SATAlgorithms/EnumerationSubclausesets.hpp for a generic algorithm for
-  clause-sets. Perhaps later this framework might be generalised.
+  \todo Refinements
+  <ul>
+   <li> Enumerating all partial hypergraphs of a hypergraph with bipartite
+   edge-vertex graph should yield also these bipartite graphs (respectively
+   the corresponding information) for the partial hypergraphs. </li>
+   <li> In the same vein, when enumerating all sub-clause-sets of a
+   clause-set (or something similar) with such an auxiliary structure,
+   then the enumeration should (possibly) make available this structure
+   also for the enumerated objects. </li>
+  </ul>
 
 */
 
