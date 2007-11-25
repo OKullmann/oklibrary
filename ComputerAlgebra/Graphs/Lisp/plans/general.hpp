4482552e0700a35bc859542161c1e2868fb45428
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-11-25 07:14:12 +0000

    Maxima function for graph output, and generating Kneser graphs.

---
+++ ComputerAlgebra/Graphs/Lisp/plans/general.hpp
@@ -0,0 +1,34 @@
+// Oliver Kullmann, 24.11.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Graphs/Lisp/plans/general.hpp
+  \brief Plans for graphs via computer-algebra-systems in general
+
+
+  \todo Create milestones
+
+
+  \todo Graph concepts
+  <ul>
+   <li> A graph is just a 2-element list, consisting of the set of
+   vertices, and a set of 2-element vertex sets. </li>
+   <li> How to handle "properties" ? </li>
+   <li> Can we tag such objects as being "graphs" ? </li>
+  </ul>
+
+
+  \todo Generators.mac
+  <ul>
+   <li> Extend the documentation. </li>
+   <li> Write documentation. </li>
+   <li> Can we add comments to the graph-output? </li>
+   <li> Should we give the graphs in the dot-output a dedicated name? </li>
+  </ul>
+
+*/
+
