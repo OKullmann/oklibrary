1ee0ab553eea109b7d449910b0d7b2e45b00c588
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-11-30 13:30:08 +0000

    First Maxima-components for hypergraphs.

---
+++ ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 30.11.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
+  \brief Plans for Maxima-components for hypergraphs
+
+
+  \todo Create milestones
+
+
+  \todo Write tests
+
+
+  \todo Write docus
+
+*/
+
