1880893490c09efc7fc9164742c0b5dee17f563f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-11-30 13:31:20 +0000

    First basic Maxima components for boolean clause-sets.

---
+++ ComputerAlgebra/Satisfiability/Lisp/ClauseSets/plans/general.hpp
@@ -0,0 +1,33 @@
+// Oliver Kullmann, 30.11.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/ClauseSets/plans/general.hpp
+  \brief Plans for Maxima-components regarding clause-sets
+
+
+  \todo Create milestones
+
+
+  \todo Write tests
+
+
+  \todo Write docus
+
+
+  \todo Input and output
+  <ul>
+   <li> Functions currently in ComputerAlgebra/Satisfiability/Lisp/ClauseSets/BasicOperations.mac
+   should move to their own file "InputOutput.mac". </li>
+   <li> The usage of "print" likely should be replaced by "printf" from
+   package "stringproc". </li>
+   <li> We need also reading from Dimacs-files.
+  </ul>
+
+
+*/
+
