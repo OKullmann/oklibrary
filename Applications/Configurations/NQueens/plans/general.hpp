28d5d916ae06adc6225b500d320b6c68d2cb60c3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-03 10:03:09 +0000

    More on "n Queens".

--- Applications/Configurations/NQueens/plans/general.hpp
+++ Applications/Configurations/NQueens/plans/general.hpp
@@ -24,6 +24,24 @@ License, or any later version. */
    solutions, for example in lexicographical order,
    with polynomial delay (or finding the first and
    the next solution). </li>
+   <li> For what n can solvers find one solutions, and for what n can
+   one find all solutions (or count them) ? </li>
+   <li> In [P.A.Geelen. Dual viewpoint heuristics for binary constraint
+   satisfaction problems. 10th European Conference on Artificial Intelligence,
+   31-35, 1992] apparently the n-Queens problem is considered with a
+   heuristics similar (in "spirit") to the tau-heuristics. </li>
+   <li> Compare "Heuristics" in
+   ComputerAlgebra/Satisfiability/Lisp/Backtracking/plans/general.hpp. </li>
+  </ul>
+
+
+  \todo Organisation
+  <ul>
+   <li> See ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/Generators.mac
+   for a constraint propagator. </li>
+   <li> And see "n Queens" in
+   ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/plans/general.hpp
+   for further plans (in the context of the Lisp/Maxima system). </li>
   </ul>
 
 
@@ -35,20 +53,84 @@ License, or any later version. */
    set (see Hypergraphs/IndependentSets/plans/general.hpp) of
    size n. </li>
    <li> Another formulation is as a crisp CSP-problem (see
-   Transformers/CrispCSP/plans/general.hpp). </li>
+   Transformers/CrispCSP/plans/general.hpp and
+   ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/Generators.mac).
+    <ol>
+     <li> The following assumes the columns-representation (see below). </li>
+     <li> The conditions on the moves of the queens can be formulated
+     directly by one constraint. </li>
+     <li> More powerfull is to use two constraints, one is the bijectivity
+     constraint, and the other captures the 2 * (2 n - 1) diagonales:
+     The power comes from the bijectivity constraint. </li>
+     <li> The bijectivity constraint allows for intelligent algorithms;
+     it comprises the vertical and horizontal moves. Could one also
+     have such matching techniques for the diagonal moves? (Or for
+     all moves together??) </li>
+     <li> The quest is to use more active *clauses* (like the bijectivity
+     condition). </li>
+    </ol>
+   </li>
    <li> And, of course, various (generalised) clause-sets-
    formulations. </li>
-   <li> and, of course, active clause-sets. </li>
+   <li> And, of course, active clause-sets. </li>
   </ul>
 
 
   \todo Different formulations
   <ul>
-   <li> The most natural perspective is the queens-perspective,
-   that is, n variables for the n queens. </li>
-   <li> One could also consider the field-perspective, that
-   is, boolean n^2 variables (with a queen or not). </li>
-   <li> And one can combine these perspectives. </li>
+   <li> The most "natural" perspective is the queens-perspective,
+   that is, n variables for the n queens (where the value set
+   is the set of squares). The set of total assignments has
+   size n^(n^2) here. </li>
+   <li> The queens-perspective has the disadvantage of added symmetries,
+   which are avoided by the column perspective (n variables for the
+    columns, each with values {1,...,n}), or, equivalently,
+    the row perspective. We get the smaller state-space n^n.
+    It seems that this perspective is inherently better than
+    the "extreme" queens-perspective from above (but perhaps this
+    "extreme" perspective has other representations in it(!)). </li>
+  <li> One could also consider the field-perspective, that
+   is, n^2 boolean variables (with a queen or not). This yields the
+   state-space size 2^(n^2). </li>
+    <li> And one can combine these perspectives. </li>
+   <li> See [Barbara M. Smith, Modelling, 2006] for an entry into the
+   literature; [Nadel 2006; Representation Selection for Constraint Satisfaction:
+   A Case Study Using n-Queens] seems to be the major study. </li>
+  </ul>
+
+
+  \todo Active clause-sets for the moves-condition
+  <ul>
+   <li> In the following we are using the columns-perspective (see above).
+   </li>
+   <li> The domain association perhaps best is realised here as a square matrix
+   of order n with boolean entries. </li>
+   <li> Propagation happens as follows:
+    <ol>
+     <li> There is the deque of unit-clauses to be processed. Once we reach
+     a column (again) which is in this deque we remove it (this should always
+     be the leftmost entry of the deque). </li>
+     <li> We run through all columns from left to right, at each step
+     always performing all eliminations for all elements of the deque
+     (respectively removing it from the deque if we reach it (again)). </li>
+     <li> When processing a column and we find it become a unit, it is
+     (immediately) added to the end of the deque (and thus is processed
+     directly with the new column). </li>
+     <li> We stop processing once a round didn't create a new unit-clause. </li>
+     <li> Since we need only to run through all current unit clauses and to
+     add to the right, remove from the left, perhaps a list is better
+     than a deque. </li>
+     <li> We also stop once a column becomes empty. We have counters for
+     the domain sizes (the occupied elements in a column). </li>
+    </ol>
+   </li>
+   <li> An interesting question is, whether the look-ahead for this condition
+   (that is, the condition strengthened by (complete) look-ahead) could
+   be implement more efficient, using the properties of this special
+   case? In general, when implementing the look-ahead as an operator
+   (creating a new active clause-set from a given one), it would need
+   from the given active clause-set the set of implied units (and the
+   given active clause-set should be operatable in "look-ahead mode"). </li>
   </ul>
 
 */
---
+++ ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/plans/general.hpp
@@ -0,0 +1,79 @@
+// Oliver Kullmann, 2.12.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/plans/general.hpp
+  \brief Plans for Maxima-components regarding constraint satisfaction problems
+
+
+  \todo Create milestones
+
+
+  \todo Write tests
+
+
+  \todo Write docus
+
+
+  \todo "n Queens"
+  <ul>
+   <li> See Configurations/NQueens/plans/general.hpp. </li>
+   <li> Write a generator for boolean CNF (perhaps also in this module). </li>
+   <li> Write a checker for the solutions. </li>
+   <li> Especially when all solutions are to be determined, symmetry
+   considerations seem necessary. </li>
+   <li> Application of "constraint_backtracking" (from
+   ComputerAlgebra/Satisfiability/Lisp/Backtracking/ConstraintSatisfaction.mac)
+   seems rather succesful (at least regarding the number of nodes;
+   the following shows n -> #nodes):
+    <ol>
+     <li> 1,2,3 -> 1 </li>
+     <li> 4 -> 2 </li>
+     <li> 5 -> 3 </li>
+     <li> 6 -> 2 </li>
+     <li> 7 -> 3 </li>
+     <li> 8 -> 5 </li>
+     <li> 9 -> 6 </li>
+     <li> 10 -> 6 </li>
+     <li> 11 -> 6 </li>
+     <li> 12 -> 8 </li>
+     <li> 20 -> 15 </li>
+     <li> 30 -> 25 </li>
+    </ol>
+    (This table should be filled out, and linear regression performed.)
+   </li>
+   <li> Could one show that we have actually a polytime procedure ?
+   (It seems that a solution exists if n <> 2,3). </li>
+   <li> With a C++ implementation one could investiate larger n. </li>
+   <li> What is the performance of constraint solvers on this problem? </li>
+   <li> The above good performance of "constraint_backtracking" is likely
+   just due to the look-ahead and the ordering heuristics. (One should
+   have a look at the trees: How much branching is there?). This will
+   likely break down when finding (or counting) all solutions. </li>
+   <li> Results of "constraint_backtracking_counting" (from
+   ComputerAlgebra/Satisfiability/Lisp/Backtracking/ConstraintSatisfaction.mac)
+   (the number of solutions and the number of nodes):
+    <ol>
+     <li> 1 ->   [1,  1]  </li>
+     <li> 2,3 -> [0,  1]  </li>
+     <li> 4 ->   [2,  3]  </li>
+     <li> 5 ->   [10, 16] </li>
+     <li> 6 ->   [4,  7]  </li>
+     <li> 7 ->   [40, 48] </li>
+     <li> 8 ->   [92, 143] </li>
+     <li> 9 ->   [352, 632] </li>
+    </ol>
+    (This problem has only total solutions, and thus the weakness of the
+    general approach, that only total assignments can be taken into
+    account, is irrelevant here.)
+   </li>
+   <li> Of course, one could go here for optimal splitting trees. </li>
+  </ul>
+
+
+*/
+
