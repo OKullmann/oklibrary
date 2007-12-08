10e849308d3639471038073cd5885fa26baa4e94
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-08 17:49:41 +0000

    *** Initial version 0.5 (ComputerAlgebra/Mupad) ***
    (This is just the upbeat to the elimination of this module.)

--- ComputerAlgebra/Mupad/plans/general.hpp
+++ ComputerAlgebra/Mupad/plans/general.hpp
@@ -8,17 +8,33 @@ License, or any later version. */
 /*!
   \file ComputerAlgebra/Mupad/plans/general.hpp
   \brief Plans for the (deprecated) Mupad-module
-  \deprecated To be replaced by an open-source system.
+  \deprecated To be replaced by Maxima.
+
+
+  \todo Rewrite hypergraph transversal functions in Maxima
+  <ul>
+   <li> Mupad/Orthogonal.mup : Hypergraphs transversals </li>
+   <li>
+    <ol>
+     <li> sub_min_l, sub_min, ist_sub_min </li>
+     <li> streichen, hinzufuegen, streichen_bedingt </li>
+     <li> union_S, star </li>
+     <li> ist_exakt_transversal </li>
+     <li> Transversals_exakt </li>
+     <li> enthaelt_disjunkt </li>
+     <li> ist_speziell_exakt_transversal </li>
+     <li> Transversals </li>
+    </ol>
+   </li>
+  </ul>
 
 
   \todo Rewriting:
-  After a new computer-algebra system has been chosen (see "Computer algebra system"
-  in Transitional/plans/general.hpp), all modules need to be redesigned
-  and reimplemented:
-  <ol>
+  All modules are to be redesigned and reimplemented in Maxima
+  <ul>
    <li> Mupad/Design.mup : Just the incidence matrix of the Fano plane. </li>
    <li> Mupad/Hilfsfunktionen.mup : Various general functions </li>
-   <li> Mupad/Orthogonal.mup : Conflict combinatorics </li>
+   <li> Mupad/Orthogonal.mup : others than above </li>
    <li> Mupad/SATCounting.mup : Precise calculations of satisfiability
    probabilities in the standard random-formula model (constant density) </li>
    <li> Mupad/TestenFormelGenAES.mup : Testing the random clause-set
@@ -26,7 +42,7 @@ License, or any later version. */
    <li> Mupad/VanderWaerden.mup : Computing optimal resolution trees etc.
    for van der Waerden-clause-sets </li>
    <li> Mupad/tau.mup : See ComputerAlgebra/Satisfiability/TauMachinery/plans/general.hpp. DONE (moved elsewhere) </li>
-  </ol>
+  </ul>
 
 */
 
---
+++ ComputerAlgebra/Mupad/plans/milestones.hpp
@@ -0,0 +1,38 @@
+// Oliver Kullmann, 8.12.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Mupad/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Mupad 0.5 (8.12.2007)
+
+
+  \par Version 0.5.1
+
+  \par
+   In ComputerAlgebra/Mupad/plans/general.hpp the following topics are handled:
+    - Rewrite hypergraph transversal functions in Maxima
+
+  \par
+   Further milestones are created.
+
+
+  \par Version 0.6
+
+  \par
+   All functions have been transferred, and this module can be eliminated.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.5   : 8.12.2007; many functions implemented.
+
+*/
+
