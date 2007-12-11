1aa50b3b6b4efc301b766bc7edf972f595289203
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-11 14:42:33 +0000

    Update on the generic graph-traversal algorithm from the lecture.

--- Combinatorics/Graphs/plans/GraphTraversal.hpp
+++ Combinatorics/Graphs/plans/GraphTraversal.hpp
@@ -13,12 +13,33 @@ License, or any later version. */
   \todo Implement the generic graph_traversal algorithm from
   the CS-232 script (generic enough, and with enough event-points,
   so that protocolling and visualisation are possible).
+  <ul>
+   <li> The buffer concept is the same as in Boost::graph. </li>
+   <li> For the visitor concept we should look at the concept of "BFS Visitor"
+   in Boost::graph. </li>
+   <li> The marking object should follow property maps from Boost::graph. </li>
+   <li> Yet we only manage undirected graphs? </li>
+  </ul>
+
+
+  \todo Implement the main applications or graph_traversal from
+  the CS-232 script
+  <ul>
+   <li> Visitor, buffer and marking objects, as supplied to graph_traversal,
+   in most of these cases depend on the input graph. Perhaps one could have then
+   an overloaded version with just one aggregate as input? </li>
+  </ul>
 
 
   \todo Graph traversal is a basic technique for all forms of
   applications of graph theory to SAT; a general overview on
   algorithms and implementations is needed, and also on overview
   on the potential applications.
+  <ul>
+   <li> Boost::breadth_first_search is verys similar to graph_traversal. </li>
+   <li> Boost::depth_first_search should be simulatable by graph_traversal.
+   </li>
+  </ul>
 
 */
 
