c75fd7451ee901e6d48c7ceadd07aede9fa7106d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-11 19:45:59 +0000

    Some more ideas regarding games like chess.

--- Applications/Games/plans/Chess.hpp
+++ Applications/Games/plans/Chess.hpp
@@ -6,14 +6,17 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*!
-  \file Games/plans/Chess.hpp
+  \file Applications/Games/plans/Chess.hpp
   \brief Submodule for chess playing and SAT
 
 
-  \todo The appropriate decision problem seems to be:
-  Given a position P and a natural number k (where the position
-  includes the party who is to move), does there exist a legal
-  sequence of k moves, so that finally the enemies king is taken?
+  \todo Basic decision problem
+  <ul>
+   <li> The appropriate decision problem seems to be:
+   Given a position P and a natural number k (where the position
+   includes the party who is to move), does there exist a legal
+   sequence of k moves, so that finally the enemies king is taken? </li>
+  </ul>
 
 
   \todo Encodings
@@ -43,43 +46,75 @@ License, or any later version. */
      </li>
     </ol>
    <li> A move changes all position variables accordingly (for the next stage):
-   "If w, e2-e4" then all fields excepts of e2 and e4 are invariant, while e2 is now
-   empty, and e4 contains the figure from e2."
-   </li>
-   <li> Better one has a hierarchy of abstractions for the moves (using active clause-sets).
-   For example from one position to the next; as usual, the main challenge is to provide
-   good inference for partial assignments.
-   </li>
-   <li> With active clause-sets much more easily positional knowledge can be incorporated;
-   then we more in direction to a general "player" (not just a check-mate solver), but
-   then likely we need optimisation. Are there optimisation variants of QBF?
-   We need some like "(Quantor-prefix) F(x) >= k", where x is the variable vector, and
-   F(X) is an evaluation function. If the decision "F(x) >= k" is rule-based, then perhaps
-   we can fit it into the decision-framework? </li>
+   "If w, e2-e4" then all fields excepts of e2 and e4 are invariant, while e2 is
+   now empty, and e4 contains the figure from e2." </li>
+   <li> Better one has a hierarchy of abstractions for the moves (using active
+   clause-sets). For example from one position to the next; as usual, the main
+   challenge is to provide good inference for partial assignments. </li>
+   <li> With active clause-sets much more easily positional knowledge can be
+   incorporated; then we more in direction to a general "player" (not just a
+   check-mate solver), but then likely we need optimisation. </li>
+   <li> Are there optimisation variants of QBF?
+   We need some like "(Quantor-prefix) F(x) >= k", where x is the variable
+   vector, and F(X) is an evaluation function. If the decision "F(x) >= k"
+   is rule-based, then perhaps we can fit it into the decision-framework? </li>
   </ul>
 
 
   \todo Problem formulations
   <ul>
-   <li> That in position P white has a check mate in 2 moves can be expressed by the QBF
+   <li> That in position P white has a check mate in 2 moves can be expressed
+   by the QBF
      <center>
-     There exists a move_1 of white such that for moves_2 of black there exists a move_3
-     of white such that for all moves_4 of black there exists a move_5 of white, such that
-     after that the black king as been taken.
+     There exists a move_1 of white such that for moves_2 of black there exists
+     a move_3 of white such that for all moves_4 of black there exists a move_5
+     of white, such that after that the black king as been taken.
      </center>
-     For each of the 6 stages we have the position-variables (in 6 variations) and the
-     rules for excluding illegal moves linking the position variables with the five
-     move variables. The initial position is encoded by unit-clauses, the final situation
-     without the black king is encoded by the assertion, that no field contains the
-     black king.
+     For each of the 6 stages we have the position-variables (in 6 variations)
+     and the rules for excluding illegal moves linking the position variables
+     with the five move variables. The initial position is encoded by
+     unit-clauses, the final situation without the black king is encoded by the
+     assertion, that no field contains the black king.
    </li>
-   <li> Besides asking whether a position has an enforced winning, one can also
-   ask if both sides help, what is the fastest win? For example, what is the shortest
-   game (from the initial position)? One problem in the formulation is that either we have
-   an for-all quantifier for the last move of black, or we formulate as a rule that a check
-   must be eliminated if possible. </li>
-   <li> Likely the whole problem is very hard, and a great challenge should be just to
-   reach the simple brute-force performance of a simple chess-programm. </li>
+   <li> Besides asking whether a position has an enforced winning, one
+   can also ask if both sides help, what is the fastest win? For example,
+   what is the shortest game (from the initial position)? One problem in
+   the formulation is that either we have an for-all quantifier for the
+   last move of black, or we formulate as a rule that a check must be eliminated
+   if possible. </li>
+   <li> Or we count solutions. </li>
+   <li> Optimising the heuristics could also be very interesting. </li>
+   <li> Likely the whole problem is very hard, and a great challenge should be
+   just to reach the simple brute-force performance of a simple chess-programm.
+   </li>
+  </ul>
+
+
+  \todo Active clauses?
+  <ul>
+   <li> It seems hard to identify active clauses (i.e., poly-time "partially
+   instantiable constraints"). </li>
+   <li> One could try to add artificial constraints, corresponding to rules of
+   good playing ?!? </li>
+   <li> Such an active clause could have a certain horizon, and could ask
+   whether something desirable could be achieved in a few moves. </li>
+   <li> Still there would be no "poly-time" intelligent algorithm involved.
+   </li> Is any "intelligent algorithm" known for chess? Besides brute-force?
+   </li>
+  </ul>
+
+
+  \todo Iterated finite functions
+  <ul>
+   <li> Chess can be described by a transition function, how to go from one
+   position to the next. </li>
+   <li> Analogous to the situation with model checking, one then considers
+   the iteration of this function from one starting position. </li>
+   <li> So one could use such tools, and apply the standard considerations.
+   </li>
+   <li> An additional perspective would be to look for invariants. </li>
+   <li> One could hope that perhaps some end-games could be characterised
+   by such invariants? </li>
   </ul>
 
 
@@ -87,6 +122,31 @@ License, or any later version. */
 
 
   \todo Is there some C/C++ library for chess-programming?
+  <ul>
+   <li> What about crafty? </li>
+   <li gnu-chess? </li>
+  </ul>
+
+
+  \todo Generalised chess
+  <ul>
+   <li> Playing chess with different dimensions than 8, and/or with different
+   pieces, should be interesting, since less is known, and the problem also
+   becomes scalable. </li>
+  </ul>
+
+
+  \todo Completeness of generalised chess
+  <ul>
+   <li> It seems that generalised chess is PSPACE-complete, given the number of
+   moves as part of the input? </li>
+   <li> While it is EXPTIME-complete, given that the number is moves is not
+   bounded? </li>
+   <li> Given the PSPACE-completeness, one could then express for example QBF
+   (or what is "closest") via such a generalised chess problem! Worth a try. </li>
+   <li> Which problems could be translated into standard chess? And how would the
+   problems look like? </li>
+  </ul>
   
 */
 
--- Applications/Games/plans/general.hpp
+++ Applications/Games/plans/general.hpp
@@ -6,32 +6,44 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*!
-  \file Games/plans/general.hpp
-  \brief Module for game playing and SAT
+  \file Applications/Games/plans/general.hpp
+  \brief Module for game playing via SAT (and beyond)
 
 
   \todo Update namespaces.
 
 
+  \todo Create milestones.
+
+
   \todo What other games are interesting?
   <ul>
-   <li> Go seems to be too "strategical". </li>
-   <li> "Nine men's morris" should be a solved game(?);
+   <li> Go seems to be too "strategical" (?). </li>
+   <li> "Nine men's morris" ("Muehle" in German) should be a solved game(?);
    this could be alright for SAT. </li>
    <li> Checkers should also be solved(?), and could pose
    some problems (interesting) due to the jump sequences. </li>
-   <li> Backgammon perhaps involves too much special
-   knowledge. </li>
-   <li> "Tic, Tac, Toe" and generlisations could be a nice
-   exercise. </li>
+   <li> Backgammon perhaps involves too much special knowledge. </li>
+   <li> "Tic, Tac, Toe" and generalisations could be a nice exercise. </li>
    <li> For Sudoku see module LatinSquares. </li>
   </ul>
 
 
-  \todo Kind of clear, that with (generalised) SAT, also
-  with QBF, one does not get a "player", but only a
-  "problem solver" --- can this be a subprocedure for
-  a "player" ?
+  \todo "Problem solver"
+  <ul>
+   <li> Kind of clear, that with (generalised) SAT, also
+   with QBF, one does not get a "player", but only a
+   "problem solver" --- can this be a subprocedure for
+   a "player" ? </li>
+   <li> Another aspect could be counting of solutions. </li>
+  </ul>
+
+
+  \todo Tools for translations
+  <ul>
+   <li> We need some nice tools to ease the translations. </li>
+   <li> One approach is via transition relations and model checking. </li>
+  </ul>
 
 */
 
