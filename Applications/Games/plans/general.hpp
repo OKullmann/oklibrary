0ec5a50775ef0c0da5cda1f1feda16bdb9d8cb40
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-13 18:41:30 +0000

    More on literature about Poker.

--- Applications/Games/plans/general.hpp
+++ Applications/Games/plans/general.hpp
@@ -33,13 +33,23 @@ License, or any later version. */
    the player then can use his "psychological" and "strategic" skills
    for the next move.
     <ol>
-     <li> Poker should have some combinatorial content. </li>
+     <li> Poker should have some combinatorial content. (But if the aspect
+     of betting strategies, modelling players behaviour etc. is too strong,
+     then it might not be a good candidate.) </li>
      <li> This is mixed with probabilities, for which a good theory
      perhaps must be compiled in advance. </li>
      <li> The advice could be a series of possible "best moves",
      with associated probability distributions about "what could be"
      and "what could happen". </li>
-     <li> First we need some overview on the literature. </li>
+     <li> First we need some overview on the literature.
+      <ul>
+       <li> http://en.wikipedia.org/wiki/Poker defines the rules and the
+       various games. </li>
+       <li> The PhD thesis
+       http://www.cs.ualberta.ca/~darse/Papers/billings-phd.html
+       should give a first overview. </li>
+      </ul>
+     </li>
     </ol>
    </li>
   </ul>
