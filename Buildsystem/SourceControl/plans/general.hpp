d7785349665b3a873114ba40170bee9a02b69aee
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-17 16:50:46 +0000

    Extensions regarding the problem of re-setting the main repository.

--- Buildsystem/SourceControl/plans/general.hpp
+++ Buildsystem/SourceControl/plans/general.hpp
@@ -18,7 +18,9 @@ License, or any later version. */
    <li> What are the alternatives? The present method, to create a clone,
    reset the clone, and to create a new bare repository from the clone,
    which replaces the old repository, looks like a waste of time. </li>
-   <li> And we have the problem: What does "cloning" clone?? See below. </li>
+   <li> And we have the problem: What does "cloning" clone?? See below
+   ("Cloning"). </li>
+   <li> The above should be asked at the git-mailing-list (MG). </li>
    <li> We need some process which defines what is in the main repository,
    and how to clone all that:
     <ol>
