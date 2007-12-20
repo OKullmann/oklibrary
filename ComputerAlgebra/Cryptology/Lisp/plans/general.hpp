306209047a730092b8dfd5cbee1cdd4cc887c061
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-20 14:58:39 +0000

    *** New version 0.0.5 (ComputerAlgebra/Cryptology/Lisp) ***

---
+++ ComputerAlgebra/Cryptology/Lisp/plans/general.hpp
@@ -0,0 +1,16 @@
+// Oliver Kullmann, 20.12.2007 (Swansea)
+/* Copyright 2007 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Cryptology/Lisp/plans/general.hpp
+  \brief Plans for cryptological tools realised in Maxima/Lisp
+
+
+  \todo Create milestones. DONE
+
+*/
+
