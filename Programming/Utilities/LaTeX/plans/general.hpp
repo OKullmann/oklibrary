78bb87a2033b6c2e2d0848bda60c32466b651d78
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-22 15:05:36 +0000

    Updated plans on Latex-production.

--- Programming/Utilities/LaTeX/plans/general.hpp
+++ Programming/Utilities/LaTeX/plans/general.hpp
@@ -10,17 +10,39 @@ License, or any later version. */
   \brief Plans for the LaTeX-Module
 
 
-  \todo Currently the module contains mainly code for producing
-  the timetable for the department. This has to be factored out into
-  some external module, so that only the latex-relateted components
-  remain. (If this is useful at all.)
+  \todo Cleaning up
+  <ul>
+   <li> Remove code which concerns the timetable-database:
+    <ol>
+     <li> "Doc_Object_Implementation.hpp" </li>
+     <li> "Latex_Handler_Implementation.hpp" </li>
+    </ol>
+   </li>
+   <li> DONE (files which are concerned only with the timetable have been
+   removed)
+   Currently the module contains mainly code for producing
+   the timetable for the department. This has to be factored out into
+   some external module, so that only the latex-relateted components
+   remain. (If this is useful at all.) </li>
+   <li> DONE (removed) The file Data/Timetable_SQL is no longer needed? </li>
+  </ul>
 
 
-  \todo We should provide latex-output of clause-sets and
-  SAT-decision trees.
+  \todo Future plans
+  <ul>
+   <li> We should provide latex-output of clause-sets and
+   SAT-decision trees. </li>
+   <li> Compare with the Maxima-functionality. </li>
+  </ul>
 
 
-  \todo The file Data/Timetable_SQL is no longer needed?
+  \todo Update namespaces
+
+
+  \todo Update namespace-usage
+
+
+  \todo Create milestones.
 
 */
 
