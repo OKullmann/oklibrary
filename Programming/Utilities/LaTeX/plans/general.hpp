8aa5b60d617e1a3171667acc4b4645659095290b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2007-12-22 11:42:06 +0000

    Partial editorial update.
    The whole things needs review.

--- Programming/Utilities/LaTeX/plans/general.hpp
+++ Programming/Utilities/LaTeX/plans/general.hpp
@@ -6,18 +6,22 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*!
-  \file LaTeX/plans/LaTeX.hpp
+  \file LaTeX/plans/general.hpp
   \brief Plans for the LaTeX-Module
+
 
   \todo Currently the module contains mainly code for producing
   the timetable for the department. This has to be factored out into
   some external module, so that only the latex-relateted components
   remain. (If this is useful at all.)
 
+
   \todo We should provide latex-output of clause-sets and
   SAT-decision trees.
 
+
   \todo The file Data/Timetable_SQL is no longer needed?
+
 */
 
 /*!
