647b6e0fe659d296379e2bb42d232b4a4d4622c5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-01-02 23:36:25 +0000

    More generators for MUSAT-formulas.
    And some more thoughts on random Sudoku-problems.

--- ComputerAlgebra/Satisfiability/Lisp/ClauseSets/plans/Generators.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/ClauseSets/plans/Generators.hpp
@@ -53,9 +53,26 @@ License, or any later version. */
    <li> For the creation of random Sudoku-problems the easiest approach
    seems to fix the number f of fixed fields, and then choose f random
    fields from the N^2 fields, and for each chosen field choose a random
-   value from {1,...,N}. </li>
-   <li> Such random Sudoku-problems should also undergo a phase transition
-   (for fixed p and appropriate f) ?! </li>
+   value from {1,...,N}. In other words, using the natural notion of
+   (non-boolean) literals a random problem is just given by a random
+   partial assignment with a fixed number of f variables.
+    <ol>
+     <li> Such random Sudoku-problems should also undergo a phase transition
+     (for fixed p and appropriate f) ?! </li>
+     <li> Creation of the partial assignment (with non-boolean variables)
+     simply as a random clause by the OKgenerator. </li>
+     <li> For the analysis one can filter out partial assignments which
+     are not r_k-consistent (so r_0-consistency just means that the
+     partial assignment does not (directly) falsify a clause); no
+     consistency check then would be "-1-consistency". </li>
+     <li> To speed up the creation, filtering out of r_k-inconsistent
+     partial assignments can be done already during the creation (without
+     disturbing the probability distribution). </li>
+     <li> However, even r_1-reduction for box-dimension 2 takes a long
+     time in Maxima (90 s). Thus this filtering cannot be done in Maxima,
+     but we need C++ components. </li>
+    </ol>
+   </li>
    <li> We also need generators for Latin square completion problems,
    etc. Compare Applications/LatinSquares/plans/general.hpp. </li>
    <li> Can something be done about
@@ -82,9 +99,29 @@ Free space: 16453216 bytes.
 
   \todo Translations to MUSAT
   <ul>
-   <li> Implement the other elements of the reduction. </li>
+   <li> Implement the other elements of the reduction.
+    <ol>
+     <li> The reduction of SAT to MUSAT. DONE </li>
+     <li> The product-construction. </li>
+    </ol>
+   </li>
    <li> DONE Implement the basic UNSAT-MUSAT reduction from
    [Papadimitriou, Wolfe; 1988]. </li>
+  </ul>
+
+
+  \todo SMUSAT(1) and tree hitting clause-sets
+  <ul>
+   <li> The basic question is how to represent trees. </li>
+   <li> And we need two variants: One where the inner nodes are
+   labelled with variables, and one without (here standard variables
+   are supplied). </li>
+   <li> The construction works also in case of "regular variable
+   labellings", where variables may occur at many places, but only
+   once on a path. </li>
+   <li> In this general form we get exactly the "tree hitting clause-sets".
+   </li>
+   <li> How to get the MUSAT(1)-clause-sets from SMUSAT(1) ? </li>
   </ul>
 
 */
