e440a2b1a506031d6826c238d35e2f0026b1dfbc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-01-09 19:10:36 +0000

    Editorial updates.

--- ComputerAlgebra/Cryptology/Lisp/plans/AdvancedEncryptionStandard.hpp
+++ ComputerAlgebra/Cryptology/Lisp/plans/AdvancedEncryptionStandard.hpp
@@ -1,12 +1,12 @@
 // Matthew Gwynne, 8.12.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2008 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file ComputerAlgebra/Cryptology/Lisp/plans/Rijndael.hpp
+  \file ComputerAlgebra/Cryptology/Lisp/plans/AdvancedEncryptionStandard.hpp
   \brief Plans for cryptological tools regarding the Rijndael cipher using Maxima
 
 
