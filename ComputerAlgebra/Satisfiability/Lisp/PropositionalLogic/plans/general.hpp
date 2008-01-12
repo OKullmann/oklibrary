b9d3018dc57edd337fd120eef4ca3bb0f4b78cb5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-01-12 20:00:37 +0000

    New module PropositionalFormulas, and first functions (Maxima).

---
+++ ComputerAlgebra/Satisfiability/Lisp/PropositionalLogic/plans/general.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 12.1.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/PropositionalLogic/plans/general.hpp
+  \brief Plans for Maxima-components regarding propositional logic
+
+
+  \todo Create milestones.
+
+
+  \todo Translations to CNF/DNF
+  <ul>
+   <li> Write direct translation (without new variables). </li>
+   <li> Complete the Tseitin translation. </li>
+  </ul>
+
+*/
