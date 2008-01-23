1b3174d25014216033c8ae4b9b9335ee54347e7f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-01-23 03:16:59 +0000

    New module on numerical computations in Maxima/Lisp.
    With a simple function for general minimisation.

---
+++ ComputerAlgebra/Numerical/Lisp/plans/general.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 23.1.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Numerical/Lisp/plans/general.hpp
+  \brief Plans for numerical Maxima/Lisp functions in general
+
+
+  \todo Function "min_scanning" 
+  (ComputerAlgebra/Numerical/Lisp/Minimisation.mac)
+  <ul>
+   <li> Is it avoidable that argument-names like "f8765" are capured by
+   function definitions (so that in the presence of a functiion with
+   name f8765 min_scanning is not working)?? </li>
+  </ul>
+
+
+  \todo Implement the "downhill simplex method in multidimensions"
+  (according to Nelder and Mead).
+
+*/
+
