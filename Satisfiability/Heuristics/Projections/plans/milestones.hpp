63265b79c63546cb413d710441de3236a83788e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-01-24 11:20:10 +0000

    *** Initial version 0.0.2 (Heuristics/Projections) ***

---
+++ Satisfiability/Heuristics/Projections/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 24.1.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Heuristics/Projections/plans/milestones.hpp
+  
+  \module_version Heuristics/Projections 0.0.2 (24.1.2008)
+
+
+  \par Version 0.0.3
+
+  \par
+   In Satisfiability/Heuristics/Projections/plans/general.hpp the following
+   topics are handled:
+    - Incorporate the theory from OK's SAT-Handbook-article: planning
+
+ 
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 24.1.2008; initial version number (several plans).
+
+*/
+
