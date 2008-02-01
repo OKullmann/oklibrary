fcd50f7d5825b60c82498ad9680f874106392f11
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-02-01 00:14:04 +0000

    Completed the basic generalised UCP-reductions (with autarkies) and tests.
    Added the stronger reductions of "weak autarkies".

---
+++ ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/general.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 31.1.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/general.hpp
+  \brief Plans for Maxima-components regarding SAT-reductions
+
+
+  \todo Create milestones
+
+
+  \todo Write docus
+
+
+  \todo Strengthen the tests
+  <ul>
+   <li> We need more systematic coverage (with small clause-sets). </li>
+   <li> We should also employ some form of data-tables so that the tests
+   mainly run through these tables. </li>
+   <li> And we should strengthen re-use of sub-tests between the various (rather
+   similar) test-functions. </li>
+  </ul>
+
+*/
+
