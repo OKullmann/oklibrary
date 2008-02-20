8a9c2367d82ca50a8cc2d09752b85e09dc8e8480
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-02-20 16:48:48 +0000

    Now supermodule ComputerAlgebra/DataStructures.
    First helper-functions for Maxima-list-processing.

---
+++ ComputerAlgebra/DataStructures/Lisp/plans/general.hpp
@@ -0,0 +1,18 @@
+// Oliver Kullmann, 20.2.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/DataStructures/Lisp/plans/general.hpp
+  \brief Plans for Maxima-components regarding data structures
+
+
+  \todo Create milestones
+
+
+  \todo Move the helper-functions from Cryptology here.
+
+*/
