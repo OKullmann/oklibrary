341a0b4a0937bf93530693b7b2b4d96d4e0c71d2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-02-24 10:39:08 +0000

    Functions w.r.t. decomposing a clause-set into hitting clause-sets.

---
+++ ComputerAlgebra/Satisfiability/Lisp/ConflictCombinatorics/plans/HittingClauseSets.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 23.2.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/ConflictCombinatorics/plans/HittingClauseSets.hpp
+  \brief Plans related to hitting clause-sets
+
+
+  \todo Constructions
+  <ul>
+   <li> The set of hitting clause-sets is a sub-monoid (with zero) of
+   CLS with "or_cs" (see
+   ComputerAlgebra/Satisfiability/Lisp/ClauseSets/plans/Constructions.hpp).
+   </li>
+  </ul>
+
+
+  \todo Decomposing clause-sets into hitting clause-sets
+  <ul>
+   <li> DONE
+   Write a function which actually computes the conflict-partition-number
+   of a clause-set (the smallest number of hitting sub-clause-sets which
+   partition the clause-set). </li>
+  </ul>
+
+*/
+
