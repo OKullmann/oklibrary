6e32d7af010f11b17ddb2ae32bd147b37abe0027
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-03-07 14:18:04 +0000

    Bug-report for Rijndael-tests: They take far too long.

--- ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/plans/general.hpp
+++ ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/plans/general.hpp
@@ -1,5 +1,5 @@
 // Matthew Gwynne, 19.2.2008 (Swansea)
-/* Copyright 2007, 2008 Oliver Kullmann
+/* Copyright 2008 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,6 +8,11 @@ License, or any later version. */
 /*!
   \file ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/plans/general.hpp
   \brief Plans for the Rijndael crypto system in Maxima/Lisp
+
+
+  \bug The test
+  ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/testobjects/AdvancedEncryptionStandard.mac
+  takes far too long. The current test is only acceptable at level 2.
 
 
   \todo Create data
