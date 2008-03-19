e02be818fc119e3ca835493db6e5b26139932d9f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-03-19 13:43:25 +0000

    Small update of plans for ADTs regarding sets.

--- Structures/Sets/plans/general.hpp
+++ Structures/Sets/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 8.9.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2008 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -13,7 +13,7 @@ License, or any later version. */
   \todo Create a milestones file.
 
 
-  \todo Move module SetAlgorithms to here.
+  \todo Move module SetAlgorithms to here. DONE
 
 */
 
