951f873fa285e80afdfa5632aad0af4b1402c5ca
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-03-21 14:30:17 +0000

    Started with new supermodule Algebra (currently only Lisp/Maxima).

---
+++ ComputerAlgebra/Algebra/Lisp/plans/general.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 21.3.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/plans/general.hpp
+  \brief Plans in general for algebra functionality via Maxima/Lisp
+
+
+  \todo Create milestones
+
+
+  \todo Move the finite-fields functions here.
+
+*/
+
