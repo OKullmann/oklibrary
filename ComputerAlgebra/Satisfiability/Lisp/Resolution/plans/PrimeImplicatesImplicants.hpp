311a911237f1d6d308aa4457b22d285cf1870588
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-03-27 17:16:17 +0000

    Plans on computing prime implicates from hitting clause-sets.

--- ComputerAlgebra/Satisfiability/Lisp/Resolution/plans/PrimeImplicatesImplicants.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Resolution/plans/PrimeImplicatesImplicants.hpp
@@ -41,5 +41,39 @@ License, or any later version. */
    </li>
   </ul>
 
+
+  \todo Hitting clause-sets
+  <ul>
+   <li> The input of Quine/McCluskey is a (special) hitting clause-set
+   (namely a full clause-set) --- can we somehow generalise the algorithm
+   to work with arbitrary hitting clause-sets? </li>
+   <li> Given a hitting clause-set F, we can easily compute for each
+   clause C in F a prime implicate C' <= C of F, by greedily removing
+   literals from C and checking whether still F implies C' holds.
+    <ol>
+     <li> Can all prime implicates of F be obtained in this way (for
+     fixed F)? Likely not (examples?). </li>
+     <li> Likely there can be exponentially many prime implicates
+     C' <= C ? </li>
+     <li> And likely it is hard to find some C' of minimal length? </li>
+     <li> Once we know phi_C' * F is unsatisfiable, it should be possible
+     to read off directly from this computation which literals from C'
+     can be further dropped. </li>
+    </ol>
+   </li>
+   <li> In this way we obtain F' <= F consisting of some prime implicates
+   of F such that F' is equivalent to F. By removing redundant clauses
+   from F' we can further go into the direction of a shortest CNF
+   representation of the underlying boolean function.
+    <ol>
+     <li> Since SAT solvers can be used to obtain F, in this way we can
+     harness the power of SAT solvers (w.r.t. computing small splitting
+     trees) for computing small CNF/DNF representations. </li>
+    </ol>
+   </li>
+   <li> It should be possible to compute with polynomial delay
+   all prime implicates which are subsuming some clause from F.
+  </ul>
+
 */
 
