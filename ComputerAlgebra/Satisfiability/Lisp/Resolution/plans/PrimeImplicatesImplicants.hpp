fea304e17b3ebf5fc996069fd48ceeb74d32c81e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-03-27 15:37:28 +0000

    Improved algorithm for Quine/McCluskey, and further plans.
    The new implementation expresses directly the original idea of
    Quine/McCluskey (and is also faster).

--- ComputerAlgebra/Satisfiability/Lisp/Resolution/plans/PrimeImplicatesImplicants.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Resolution/plans/PrimeImplicatesImplicants.hpp
@@ -17,5 +17,29 @@ License, or any later version. */
    2-subsumption resolution (example?). </li>
   </ul>
 
+
+  \todo Dualisation
+  <ul>
+   <li> Implement the algorithm given by "dual_cs" in
+   ComputerAlgebra/Satisfiability/Lisp/ClauseSets/Constructions.mac
+   </li>
+   <li> Or sh0uld this go somewhere else?? </li>
+  </ul>
+
+
+  \todo Connections
+  <ul>
+   <li> The above dualisation yields a representation of all
+   satisfying assignments:
+    <ol>
+     <li> See
+     ComputerAlgebra/Satisfiability/Lisp/Counting/plans/general.hpp
+     </li>
+     <li> See AllSolutions/plans/general.hpp, and especially
+     AllSolutions/plans/MinimalAssignments.hpp. </li>
+    </ol>
+   </li>
+  </ul>
+
 */
 
