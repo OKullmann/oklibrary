4d1a204fdc76f46fe02aa165e5ee7b67c59b7570
Matthew Gwynne <360678@Swansea.ac.uk>
2008-04-16 22:37:26 +0100

    Initial Analysis functions for AES field operations.

---
+++ ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/Rijndael/plans/FieldOperationsAnalysis.hpp
@@ -0,0 +1,27 @@
+// Matthew Gwynne, 12.4.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/Rijndael/plans/FieldOperationsAnalysis.hpp
+  \brief Plans for the Cryptanalysis of the Rijndael field operations in Maxima/Lisp
+
+  
+  \todo Provide tests for all functions
+  
+  
+  \todo Generate good CNF hitting clause-sets for the AES Sbox
+  
+  
+  \todo Extracting prime implicate representations from the hitting-cls-representations
+  
+  
+  \todo Compute *all* prime implicates (for the purpose of analysis):
+  
+ 
+  \todo Find the symmetries of the AES Sbox DNF
+
+*/
