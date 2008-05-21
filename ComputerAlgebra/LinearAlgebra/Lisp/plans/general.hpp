77ee27f874a381dc4c8e2ec1a51f1dc646be138c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-05-21 23:30:38 +0100

    Plans on linear algebra in Maxima.

---
+++ ComputerAlgebra/LinearAlgebra/Lisp/plans/general.hpp
@@ -0,0 +1,32 @@
+// Oliver Kullmann, 21.5.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/LinearAlgebra/Lisp/plans/general.hpp
+  \brief Plans on linear algebra in Maxima
+
+
+  \todo Create milestones
+
+
+  \todo Write tests
+
+
+  \todo Write docus
+
+
+  \todo Indices
+  <ul>
+   <li> Also hermitian_rank_eig and hermitian_rank_charpoly should
+   be changed to compute the indices as pnn_indices. </li>
+   <li> So that we get pnn_indices_eig and pnn_indices_charpoly. </li>
+   <li> Perhaps then we create a generic function for computing the
+   hermitian rank from a version of pnn_indices. </li>
+  </ul>
+
+*/
+
