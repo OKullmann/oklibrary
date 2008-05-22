ab8a14ef01a987e6efe8b741dbd576a613e6f7b8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-05-22 10:21:40 +0100

    Editorial updates.
    Providing some doxygen-documentation.

--- General/Matrices.hpp
+++ General/Matrices.hpp
@@ -1,31 +1,53 @@
 // Oliver Kullmann, 26.4.2003 (Swansea)
-/* Copyright 2003 - 2007 Oliver Kullmann
+/* Copyright 2003 - 2007, 2008 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
-#ifndef MATRICESWAECHTER
+/*!
+  \file General/Matrices.hpp
+  \brief Simple class template for square matrices, as a wrapper around
+  arrays.
 
-#define MATRICESWAECHTER
+  Given a matrix M, via M(i,j) we obtain the element at position (i,j),
+  while by M[i] we obtain a pointer to row i.
+
+  \deprecated Old code
+*/
+
+#ifndef MATRICESWAECHTER_pokGr45E
+#define MATRICESWAECHTER_pokGr45E
 
 #include <algorithm>
 
 namespace Matrices {
 
-  // Simple quadratic matrix class, wrapping arrays.
-  // Providing:
-  // dimension, equality test, type-safe assignment,
-  // two version for non-mutable (default) and mutable elements.
-
   enum storage_class { mutable_elements, non_mutable_elements };
 
-  template <typename i_type, typename v_type, i_type dimension, storage_class st = non_mutable_elements>
+  /*!
+    \class QuadMatrix
+    \brief Simple square-matrices class, wrapping arrays
+
+    Providing
+    <ul>
+     <li> dimension </li>
+     <li> equality test </li>
+     <li> type-safe assignment </li>
+     <li> element-access via operator () </li>
+     <li> row-access via operator [] </li>
+     <li> two version for non-mutable (default) and mutable elements </li>
+    </ul>
+  */
+
+  template <typename i_type,
+            typename v_type,
+            i_type dimension,
+            storage_class st = non_mutable_elements>
   class QuadMatrix {
   public :
     typedef const v_type array_type[dimension][dimension];
     typedef  v_type array_type_mutable[dimension][dimension];
-
     typedef i_type index_type;
     typedef v_type value_type;
     
@@ -35,13 +57,8 @@ namespace Matrices {
     template <storage_class stc>
     QuadMatrix(const QuadMatrix<i_type, v_type, dimension, stc>& M) : a(M.a) {}
     
-    value_type operator() (index_type i, index_type j) const {
-      return a[i][j];
-    }
-    
-    const v_type* operator[] (index_type i) const {
-      return a[i];
-    }
+    value_type operator() (index_type i, index_type j) const { return a[i][j]; }
+    const v_type* operator[] (index_type i) const { return a[i]; }
     
   private :
     const array_type& a;
@@ -52,7 +69,6 @@ namespace Matrices {
   class QuadMatrix<i_type, v_type, dimension, mutable_elements> {
   public :
     typedef v_type array_type[dimension][dimension];
-
     typedef i_type index_type;
     typedef v_type value_type;
     
@@ -60,19 +76,10 @@ namespace Matrices {
 
     QuadMatrix(array_type& a_) : a(a_) {}
     
-    value_type&  operator() (index_type i, index_type j) {
-      return a[i][j];
-    }
-    value_type operator() (index_type i, index_type j) const {
-      return a[i][j];
-    }
-    
-    v_type* operator[] (index_type i) {
-      return a[i];
-    }
-    const v_type* operator[] (index_type i) const {
-      return a[i];
-    }
+    value_type&  operator() (index_type i, index_type j) { return a[i][j]; }
+    value_type operator() (index_type i, index_type j) const { return a[i][j]; }
+    v_type* operator[] (index_type i) { return a[i]; }
+    const v_type* operator[] (index_type i) const { return a[i]; }
     
   private :
     array_type& a;
