a1f3ad7537805bde08b3a0ed5a0b977ded0a6464
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-05-28 20:39:57 +0100

    Small update of the general plans on ComputerAlgebra/Satisfiability.

--- ComputerAlgebra/Satisfiability/plans/general.hpp
+++ ComputerAlgebra/Satisfiability/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 6.10.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2008 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -11,6 +11,10 @@ License, or any later version. */
 
 
   \todo Create a development plan
+  <ul>
+   <li> Yet only the Lisp part. </li>
+   <li> But we need to check what happens with Axiom. </li>
+  </ul>
 
 */
 
