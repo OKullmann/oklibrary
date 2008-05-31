7336c1da10512fa50b9c6c58053d8b5a3e673bb0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-05-31 17:55:43 +0100

    Added (preliminarily) GPLv3 and copyright by Andrej Vodopivec.

--- ComputerAlgebra/MaximaInternals/optload.lisp
+++ ComputerAlgebra/MaximaInternals/optload.lisp
@@ -1,3 +1,9 @@
+/* Copyright 2008 Andrej Vodopivec
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
 (defvar *add-lineinfop* t)
 
 (defun add-lineinfo (lis)
