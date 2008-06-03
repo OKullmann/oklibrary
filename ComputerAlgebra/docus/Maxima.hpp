1d64d839845387059a94e5218ec37c3eb61d704d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-06-03 10:47:16 +0100

    Update of documentation on Maxima-support provided by "oklib".

--- ComputerAlgebra/docus/Maxima.hpp
+++ ComputerAlgebra/docus/Maxima.hpp
@@ -13,7 +13,7 @@ License, or any later version. */
   <h1> Maxima/Lisp in the OKlibrary </h1>
 
 
-  <h2> Configuration </h2>
+  <h2> Calling the supported Maxima console </h2>
 
   <ul>
    <li> For questions related to building Maxima see
@@ -44,9 +44,42 @@ oklib --maxima --batch=FILE
     \endverbatim
     the file FILE is batch-processed by Maxima.
    </li>
-   <li> Inside a Maxima-file (in the context of the OKlibrary) inclusion of
+   <li> As can be seen from the make-output, "oklib --maxima" just calls the
+   Maxima console provided with initial definitions.
+    <ol>
+     <li> The template for these initial definitions is the file
+     <code>$(OKbuildsystem)/MasterScript/SpecialProcessing/maxima-init.mac</code>.
+     </li>
+     <li> As usual for such templates, configuration variables are accessed
+     via "m4_SHELL(variable)". </li>
+    </ol>
+   </li>
+   <li> The redefinition-warnins shown when starting Maxima in this way
+   are about changing the behaviour when parsing files:
+    <ol>
+     <li> By default, "oklib_load" and "oklib_include" (see below)
+     do not provide debugging information for the parsed functions. </li>
+     <li> This saves run-time and storage space. </li>
+     <li> If the debugging information (filename and fileline of the
+     creating function for list-values) is needed, then it can be
+     activated by setting "oklib_load_annotation" to true. </li>
+     <li> The standard functions (like "load") are not affected (they
+     always provide annotations). </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  <h2> Preloaded functionality </h2>
+
+  <ul>
+   <li> Inside a Maxima-file (in the context of the OKlibrary), inclusion of
    other Maxima-files happens via <code>oklib_include(filename)</code>,
-   where filename is the relative path starting with "Transitional". </li>
+   where filename is the relative path starting with "Transitional".
+    <ol>
+     <li> In this way multiple inclusions are avoided. </li>
+    </ol>
+   </li>
    <li> Access to configuration-variables:
     <ol>
      <li> From a Maxima-session, via "system(string_with_shell_code)" one can
@@ -57,6 +90,40 @@ oklib --maxima --batch=FILE
      build system are available. </li>
      <li> For convenience <code>OKplatform</code> and <code>OKsystem</code>
      are directly defined as variables. </li>
+    </ol>
+   </li>
+   <li> Storing results at given time intervals:
+    <ol>
+     <li> The variable "oklib_session_name" contains the name used for
+     storing results in a file via "oklib_save". </li>
+     <li> Calling <code>oklib_save(arguments)</code> triggers the following actions:
+      <ol>
+       <li> If "oklib_store" is false, then nothing happens. </li>
+       <li> If not "oklib_storage_interval"-many run-time minutes have elapsed
+       since the last file-storage (or since the start of the session). then
+       nothing happens. </li>
+       <li> Otherwise, if you used "oklib_save()" then all variables in
+       the environment of this call will be saved to file, while if you
+       used for example "oklib_save('a,'b)", then only the values of
+       variables a and b will be saved. </li>
+      </ol>
+     </li>
+     <li> Via "load(filename)" these variable-values are made accessible in a
+     Maxima-session. </li>
+    </ol>
+   </li>
+   <li> Variable <code>oklib_test_level</code> controls the test levels,
+   similar to the C++ higher-order unit test system:
+    <ol>
+     <li> <code>oklib_test_level = 0</code> means the "basic" test level (for
+     permanent testing). </li>
+     <li> <code>oklib_test_level = 1</code> means the "full" test level (to be
+     used before submitting to the central repository). </li>
+     <li> <code>oklib_test_level = 2</code> means the "extensive" test level
+     (to be used before a new release). </li>
+     <li> <code>oklib_test_level</code> may have higher values. </li>
+     <li> To run absolutely all tests, use <code>oklib_test_level = inf</code>.
+     </li>
     </ol>
    </li>
   </ul>
