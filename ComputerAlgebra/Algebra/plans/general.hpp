af824830dd353d9210cdd57255dde21763a4b148
Matthew Gwynne <aeternus@anima.(none)>
2008-06-05 19:11:50 +0100

    *** Initial version 0.0.4 (ComputerAlgebra/Algebra/) ***

---
+++ ComputerAlgebra/Algebra/plans/general.hpp
@@ -0,0 +1,15 @@
+// Matthew Gwynne, 5.6.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/plans/general.hpp
+  \brief Plans in general for algebra functionality
+
+  \todo DONE Create milestones
+
+*/
+
---
+++ ComputerAlgebra/Algebra/plans/milestones.hpp
@@ -0,0 +1,40 @@
+// Matthew Gwynne, 5.6.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Algebra 0.0.4 (5.6.2008)
+
+
+  \par Version 0.0.5
+
+  \par
+   In ComputerAlgebra/Algebra/plans/general.hpp 
+   the following topics are handled:
+    - Create milestones DONE 
+
+  \par ComputerAlgebra/Algebra/Lisp version 0.0.5
+  (see ComputerAlgebra/Algebra/Lisp/plans/milestones.hpp)
+  
+  
+  \par Version 0.0.6
+
+  \par ComputerAlgebra/Algebra/Lisp version 0.0.6
+  (see ComputerAlgebra/Algebra/Lisp/plans/milestones.hpp)
+
+
+  \par
+   Further milestones are created.
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+   - 0.0.4 : 5.6.2008; initial version number, Lisp submodule.
+
+*/
