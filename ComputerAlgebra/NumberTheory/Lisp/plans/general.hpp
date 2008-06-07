f8bc77e2e40409b19367a4715a22922988b1821a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-06-07 11:37:58 +0100

    Correcting the Maxima function integer_partition.

--- ComputerAlgebra/NumberTheory/Lisp/plans/general.hpp
+++ ComputerAlgebra/NumberTheory/Lisp/plans/general.hpp
@@ -9,5 +9,26 @@ License, or any later version. */
   \file ComputerAlgebra/NumberTheory/Lisp/plans/general.hpp
   \brief General plans for number theory in the Maxima computer algebra system
 
+
+  \todo ext_integer_partitions
+  <ul>
+   <li> That Maxima yields "integer_partitions(0) = {}" is a bug;
+   tell the Maxima mailing list.
+    <ol>
+     <li> The documentation states "Thus 0 has no partitions.";
+     this is false (since [] is a partition of 0). </li>
+     <li> Accordingly, also "integer_partitions(0,0) = {}" is false;
+     it must be {[]}. </li>
+     <li> Furthermore the documentation states that n can be an
+     integer, but for example "integer_partitions(-1) = int_partitions(-1)".
+     </li>
+    </ol>
+   </li>
+   <li> We should also correct "integer_partitions(0,0)", but how to overload
+   ext_integer_partitions on the number of arguments? </li>
+   <li> If the Maxima function would be corrected, then our function
+   ext_integer_partitions shall be eliminated. </li>
+  </ul>
+
 */
 
