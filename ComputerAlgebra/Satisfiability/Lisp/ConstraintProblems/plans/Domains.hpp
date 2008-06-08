1c2a17da1c36615bf0f80a933b860c968db28ff2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-06-08 09:36:37 +0100

    Editorial updates of plans for constraint problems.
    Plans on "n queens" moved to dedicated Generator-plans (created by
    next submission).

---
+++ ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/plans/Domains.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 8.6.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/ConstraintProblems/plans/Domains.hpp
+  \brief Plans regarding domains in constraint problems
+
+
+  \todo Write tests
+
+
+  \todo Concept of a "domain association"
+  <ul>
+   <li> It would be more natural to allow arbitrary variables in domain. </li>
+   <li> Should this be a more general type of constraint? </li>
+  </ul>
+
+*/
+
