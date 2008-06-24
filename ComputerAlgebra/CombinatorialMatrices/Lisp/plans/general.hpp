4f90429b28fd93a75f7b37a4a6628294c2829571
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-06-24 09:13:06 +0100

    Update of plans on combinatorial matrices regarding basic notions.
    And how to memoise the functions computing the values.
    
    And some editorial changes.

--- ComputerAlgebra/CombinatorialMatrices/Lisp/plans/general.hpp
+++ ComputerAlgebra/CombinatorialMatrices/Lisp/plans/general.hpp
@@ -13,7 +13,7 @@ License, or any later version. */
   \todo Write tests
 
 
-  \todo Basic operations
+  \todo Basic notions
   <ul>
    <li> A "(combinatorial) matrix" is a triple [R,C,f], where R is the set of
    row indices, C the set of column indices, and f is a map with domain
@@ -21,26 +21,80 @@ License, or any later version. */
    <li> While a "square matrix" is a pair [I, f], where f is a map with
    domain I x I. </li>
    <li> Square matrices [I,f] can be converted to matrices [I,I,f]. </li>
-   <li> Or should f be an array function? </li>
    <li> A matrix "over M" is a matrix whose values are elements of M. </li>
    <li> So combinatorial matrices over {0,1} are exactly the *incidence
    structures*, while combinatorial matrices over {-1,0,+1} are exactly
    the general boolean clause-sets. </li>
+   <li> We also have "ordered matrices" and "ordered square matrices",
+   where instead of index-sets we have repetition-free lists. </li>
+   <li> Abbreviations:
+    <ol>
+     <li> "cm" for combinatorial matrices </li>
+     <li> "scm" for square combinatorial matrices </li>
+     <li> "sycm" for symmetric combinatorial matrices </li>
+     <li> "ocm" for ordered combinatorial matrices </li>
+     <li> "oscm" for ordered square combinatorial matrices </li>
+     <li> "osycm" for ordered symmetric combinatorial matrices. </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  \todo Memoisation
+  <ul>
+   <li> A "completely lazy" function f in a cm [A,B,f] recomputes
+   its values every time again. </li>
+   <li> For example for conflict matrices for clause-sets this creates
+   a big overhead. </li>
+   <li> In the following way we can implement "lazy" functions f, which
+   compute the value only once, and then keep the result:
+    <ol>
+     <li> One possibility is that f(i,j) just calls some f2[i,j]; this
+     f2 would then necessarily be a global function. </li>
+     <li> Hash-maps can be built into function terms:
+     \verbatim
+f1 : buildq([hm : sm2hm({[1,77]})],
+       lambda([n], block([v : ev_hm(hm,n)], 
+         if v=false then set_hm(hm,n,bfloat(log(n!))) else v)));
+     \endverbatim
+     is a fine memoised function. </li>
+     <li> One has to keep in mind that the table is built into f1;
+     if this is not wished then (as with using array-functions) one
+     has to use global objects. </li>
+     <li> On the other hand,
+     \verbatim
+f2 : buildq([a : make_array(fixnum,1000)],
+       lambda([n], block([v : a[n-1]], 
+         if v=0 then a[n-1] : bfloat(log((n-1)!)) else v)))$
+     \endverbatim
+     doesn't work, apparently due to evaluation problems. </li>
+    </ol>
+   </li>
+   <li> We should set-up some framework for defining such functions easily.
+   </li>
+   <li> Should for example the conflict matrix of a clause-set always be
+   computed in this way? </li>
+  </ul>
+
+
+  \todo Conversions
+  <ul>
    <li> A matrix [R,C,f] can be converted to a Maxima matrix
    <code> genmatrix(lambda([i,j],f(listify(R)[i],listify(C)[j])),
             length(R), length(C)); </code>
    </li>
    <li> A square matrix M=[I,f] is converted into a directed graph dg(M) with
-   vertex set I and an directed edge from i to j for i # j iff f(i,j) # 0. </li>
+   vertex set I and an directed edge from i to j for i # j iff f(i,j) # 0.
+   </li>
    <li> dgl(M) has a loop at entry [i,i] iff f(i,i) # 0. </li>
    <li> The general hypergraph hyp(M) for matrix M=[I,J,f] has vertex set I,
    hyperedge set J, and hyperedge j contains vertex i iff f(i,j) # 0. </li>
-   <li> The general clause-set cls(M) for matrix M=[I,J,f] has variable set I,
-   clause-index set J, while clause j contains variable i positively, negatively
-   or not iff f(i,j) > 0, < 0, = 0 respectively. </li>
+   <li> The labelled clause-set cls(M) for matrix M=[I,J,f] has variable set I,
+   clause-index set J, while clause j contains variable i positively,
+   negatively or not iff f(i,j) > 0, < 0, = 0 respectively. </li>
    <li> For a general hypergraph G we can form the vertex-edge incidence matrix
    as well as the edge-vertex incidence matrix. </li>
-   <li> For a general clause-set F we can form the clause-variable matrix as
+   <li> For a labelled clause-set F we can form the clause-variable matrix as
    well as the variable-clause matrix. </li>
    <li> For a directed graph (with loops) G we can form the adjacency
    matrix. </li>
@@ -54,9 +108,9 @@ License, or any later version. */
    <li> A combinatorial matrix is "complete" if no entry is zero.
     <ol>
      <li> Write predicate "completecomp". </li>
-     <li> Perhaps "1-complete" is all entries are 1 ? </li>
+     <li> Perhaps "1-complete" if all entries are 1 ? </li>
      <li> Perhaps we call a matrix "constant", "1-constant" etc. </li>
-     <li> Then 1-complete com's would be complete 1-constant com's. </li>
+     <li> Then 1-complete com's would be 1-constant com's. </li>
     </ol>
    </li>
    <li> A "multi-biclique" corresponds to a complete sub-matrix of a
@@ -75,7 +129,7 @@ License, or any later version. */
    <li> A biclique-partitioning corresponds to a representation of the
    matrix as a sum of 1-complete submatrices:
     <ol>
-     <li> Since rows are columns have names, such a sum has a natural
+     <li> Since rows and columns have names, such a sum has a natural
      meaning. </li>
      <li> We should implement such a "submatrix_sum". </li>
      <li> However, here we do not have submatrices, since the entries
