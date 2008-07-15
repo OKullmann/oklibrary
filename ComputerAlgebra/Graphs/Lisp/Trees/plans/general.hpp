78a04cb311a131a4f7ab9cc4708fd54caa399337
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-07-15 11:57:52 +0100

    Started with module Graphs/Trees.
    Some simple generators provided (not tested yet).

---
+++ ComputerAlgebra/Graphs/Lisp/Trees/plans/general.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 15.7.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Graphs/Lisp/Trees/plans/general.hpp
+  \brief Plans regarding trees (as combinatorial objects)
+
+
+  \todo Isomorphism testing
+  <ul>
+   <li> We need a poly-time test for the isomorphism of trees. </li>
+   <li> We need also to detect whether a graph is a pathgraph or
+   a star. </li>
+   <li> This seems to belong to Graphs/Lisp/Isomorphisms/Trees.mac. </li>
+   <li> Or should we place it in this module? </li>
+  </ul>
+
+*/
+
