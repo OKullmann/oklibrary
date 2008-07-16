03f4dfc1777a473d510faa9341007f433bdc5819
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-07-16 08:48:25 +0100

    Started with the module on (generalised) rings and modules.

---
+++ ComputerAlgebra/Algebra/Lisp/Ringframes/plans/BasicNotions.hpp
@@ -0,0 +1,49 @@
+// Oliver Kullmann, 15.7.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/Ringframes/plans/BasicNotions.hpp
+  \brief Plans about the general notions for ringframes
+  
+  
+  \todo Notions and abbreviations for ringframes
+  <ul>
+   <li> Most general a "ringframe" [R,+,*]. </li>
+   <li> A "ringframe" : "rngfrm" </li>
+   <li> A "prering" : "prrng" </li>
+   <li> A "semiring" : "srng" </li>
+   <li> A "nearring" : "nerrng" </li>
+   <li> A "ring" : "rng" </li>
+   <li> A "field" : "fld" </li>
+   <li> A "skewfield" : "skfld" </li>
+   <li> Commutative : "c" </li>
+   <li> Associative : "a" </li>
+   <li> "Pseudo" : "p" </li>
+   <li> "Null" : "n" </li>
+   <li> "Unit" : "u" </li>
+   <li> "left" : "l" </li>
+   <li> "right" : "r" </li>
+   <li> "additively" : "add" </li>
+   <li> "multiplicately" : "mul" </li>
+  </ul>
+
+
+  \todo Notions and abbreviations for modules and generalisations
+  <ul>
+   <li> Most general is a "moduleframe", which is the operation of a
+   ringframe on a groupod. </li>
+   <li> A "moduleframe" : "mdlfrm" </li>
+   <li> A "premodule" : "prmdl" (the operation of a prering on a groupoid) </li>
+   <li> A "semimodule" : "smdl" (the operation of a semiring on a commutative
+   monoid) </li>
+   <li> A "module" : "mdl" (the operation of a ring on an abelian group) </li>
+   <li> A "vectorspace" : "vcsp" (the operation of a (skew)field on an abelian
+   group) </li>
+  </ul>
+
+*/
+
---
+++ ComputerAlgebra/Algebra/Lisp/Ringframes/plans/general.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 15.7.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/Ringframes/plans/general.hpp
+  \brief Plans in general regarding ringframes
+
+
+  \todo Organisation
+  <ul>
+   <li> Move ComputerAlgebra/Algebra/Lisp/FiniteFields.mac to
+   Ringframes/Fields. </li>
+  </ul>
+
+
+  \todo Write tests
+
+
+  \todo Homomorphisms
+
+*/
+
