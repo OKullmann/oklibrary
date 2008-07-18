d8f037c88447a6dec8d06a46b46e40abbeb88624
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-07-18 11:46:53 +0100

    Tools for solving linear programs (using Maxima-simplex).

---
+++ ComputerAlgebra/Numerical/Lisp/plans/LinearProgramming.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 18.7.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Numerical/Lisp/plans/LinearProgramming.hpp
+  \brief Plans for numerical linear programming tools
+
+
+  \todo Handling of variables
+  <ul>
+   <li> How can the list
+   \verbatim
+create_list(eval_string(sconcat("x",i)),i,1,n)
+   \endverbatim
+   be made the first argument of "block", so that x1, x2, ...,
+   become local variables? </li>
+  </ul>
+
+*/
+
