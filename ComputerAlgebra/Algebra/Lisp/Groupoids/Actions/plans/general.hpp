5c3fcd2a76918d0e97408e9722a7ddf8b4196091
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-07-22 10:14:02 +0100

    Update on naming conventions regarding actions.

---
+++ ComputerAlgebra/Algebra/Lisp/Groupoids/Actions/plans/general.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 22.7.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/Groupoids/Actions/plans/general.hpp
+  \brief Plans in general regarding actions of sets
+  
+  
+  \todo Notions and notations
+  <ul>
+   <li> "act", "ract" </li>
+   <li> "mp" for map, "sm" for set-map, "hm" for hash-map (compare with
+   ComputerAlgebra/DataStructures/Lisp/plans/HashMaps.hpp) </li>
+   <li> "trs_sm" for transformations as set-maps, and "trs_l" for
+   transformations as lists (compare with
+   ComputerAlgebra/Algebra/Lisp/Groupoids/Semigroups/plans/general.hpp). </li>
+  </ul>
+
+
+  \todo Homomorphisms
+
+*/
+
