68b013945c08d85994a8fe2ab316603a56953694
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-08-01 00:42:11 +0100

    Extensions for basic plans on matroids.

--- ComputerAlgebra/Matroids/Lisp/plans/general.hpp
+++ ComputerAlgebra/Matroids/Lisp/plans/general.hpp
@@ -18,7 +18,11 @@ License, or any later version. */
    <li> Let "mtr" be the abbreviation for "matroid"; then perhaps we
    have "mtr_x" for the variations:
     <ol>
-     <li> "is" for independent subsets </li>
+     <li> "is" for independent subsets (a hypergraph [V,M] such that M is
+     non-empty, hereditary (stable under subset-formation), and fulfils
+     the exchange property, i.e., for A, B in M with |A| < |B| there exists
+     x in B - A with A + {x} in M; for implicitly given matroids, which
+     could be infinite, we need further distinctions) </li>
      <li> "ds" for dependent subsets </li>
      <li> "cr" for circuits </li>
      <li> "bs" for bases </li>
@@ -38,6 +42,71 @@ License, or any later version. */
    "d" is reserved to indicate "dual notions". </li>
    <li> We also need to be compatible with oriented matroids; see
    ComputerAlgebra/Matroids/Lisp/OrientedMatroids/plans/general.hpp. </li>
+   <li> The direct definition is inefficient:
+    <ol>
+     <li> Implicit representation of the base set is of importance. </li>
+     <li> And perhaps the existential quantifiers in the axioms can
+     additionally be realised, e.g. for "_is" a function f(A,B) which
+     yields x in B - A. </li>
+     <li> In this way, through the different axiomatisations, many different
+     functions arise, given directly or derived. One needs a system for that.
+     </li>
+     <li> Perhaps we use for example "isr", that is, "independent set
+     realiser". One could also use "issf" ("sf" for "Skolem function"). </li>
+     <li> Perhaps important also the variation where e.g. f(A,B) is the set
+     of all x in B - A s.t. A + x is independent. </li>
+     <li> And also for the various set systems (independent sets, circuits,
+     etc.) we can have explicit and implicit representations. One could use
+     "imp" for "implicit", and "exp" for "explicit". </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  \todo Examples
+  <ul>
+   <li> Given a combinatorial matrix over a skew field, subsets of the
+   column-set, or, alternatively, of the row-set, are independent iff they
+   are linearly independent (with the Maxima function "rank" one can
+   handle the field of reals or the field of rationals, while for other
+   fields apparently no functions are provided, and so we need to provide
+   generic functions ourselves). </li>
+  </ul>
+
+
+  \todo Greedy algorithm
+  <ul>
+   <li> Given a hereditary hypergraph [V,E] with non-empty E, and a weight
+   function w: V -> RR, the greedy algorithm determines a maximal element B
+   of E as follows:
+    <ol>
+     <li> B := {}; </li>
+     <li> if there is no x in V - B with B + {x} in E, return B; </li>
+     <li> otherwise choose such x with w(x) maximal, set B := B + {x},
+     and repeat from the previous step. </li>
+    </ol>
+   </li>
+   <li> [V,E] is a matroid if and only if the greedy algorithm yields for
+   every w a maximal element of E which is also of maximal weight (amongst
+   all maximal elements of E). </li>
+   <li> The two main variation seem to dependent on how to find x:
+    <ol>
+     <li> Either one has to run through all of V - B, </li>
+     <li> or a special function computes them. </li>
+    </ol>
+   </li>
+   <li> So here for more efficient implementation a function f(I) would be
+   needed, which for a given independent set I computes the set of all
+   x in V - I s.t. I + {x} is still independent.
+    <ol>
+     <li> For example for Kruskal's algorithm, f needed internal state,
+     and would start with all edges possible, and with every edge added
+     to B would remove all edges which now lead to cycles. </li>
+     <li> How is Kruskal's algorithm doing that? </li>
+    </ol>
+   </li>
+   <li> The greedy algorithm can find all bases of maximal weight; if x
+   is always chosen randomly, do we then sample all bases fairly? </li>
   </ul>
 
 */
