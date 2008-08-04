850ab37e2330eb0851a52241a3f084667be034f5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-08-05 00:09:09 +0100

    Mostly editorial improvements.
    Also removed some illegal tests.
    And update to the new min-degree-function.
    
    Do not use "first" (or "last") on sets.

---
+++ ComputerAlgebra/Graphs/Lisp/Trees/plans/Representations.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 4.8.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Graphs/Lisp/Trees/plans/Representations.hpp
+  \brief Plans regarding the creation and usage of tree representations
+
+
+  \todo tree2pruefercode_og
+  <ul>
+   <li> This function is very slow. </li>
+   <li> We need also a more efficient version. </li>
+   <li> Then perhaps the old version is renamed to "tree2pruefercode_bydef_og".
+   </li>
+  </ul>
+
+*/
+
