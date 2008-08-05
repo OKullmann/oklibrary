6e29a147cd8a2c9cb7099d689728c1dcefbb4c0d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-08-05 09:56:15 +0100

    Update on "(bi)polar hypergraphs".

--- ComputerAlgebra/Graphs/Lisp/plans/general.hpp
+++ ComputerAlgebra/Graphs/Lisp/plans/general.hpp
@@ -84,23 +84,16 @@ License, or any later version. */
 
   \todo Graphs as hypergraphs
   <ul>
-   <li> Graphs G = [V,E] with loops have a natural representation as general
-   hypergraphs nghg(G) = [V,V,f] (the "neighbourhood hypergraph"), where f(v)
-   is the set of neighbours of v (the set of vertices adjacent to v). </li>
-   <li> Such hypergraphs could be called "square"; but this notion
-   has already been introduced by Seymour for general hypergraphs [V,E,f] with
-   just |V| = |E| (and (combinatorial) matrices are not really adequate
-   as a framework). </li>
-   <li> Better we call objects [V,f] "polar general hypergraphs" ("pghg"),
-   where [V,V,f] is a general hypergraph; these are sets V together with
-   functions f, which assign to every element of V a subset of V. </li>
-   <li> The translation function thus would be called "g2pghg(G)". </li>
-   <li> These are the hypergraph-versions of polar incidence structures;
-   see ComputerAlgebra/IncidenceStructures/Lisp/plans/general.hpp. </li>
-   <li> Specialising correspondences [R,A,B] to "relationals" [R,A],
-   considering only symmetric relations, representing the relation R <= A^2
-   by a function from A to P(A), and changing the order, we have exactly the
-   polar hypergraphs. </li>
+   <li> The topic is discussed in "Polar hypergraphs" in
+   ComputerAlgebra/Hypergraphs/Lisp/plans/Basics.hpp. </li>
+   <li> We need a good overview on the different representations of graphs
+   via hypergraphs, incidence structures and combinatorial matrices:
+    <ol>
+     <li> On the one hand, these representations show different points of
+     view. </li>
+     <li> And on the other hand they provide different data structures! </li>
+    </ol>
+   </li>
   </ul>
 
 
--- ComputerAlgebra/Hypergraphs/Lisp/plans/Basics.hpp
+++ ComputerAlgebra/Hypergraphs/Lisp/plans/Basics.hpp
@@ -53,5 +53,48 @@ License, or any later version. */
    </li>
   </ul>
 
+
+  \todo Polar hypergraphs
+  <ul>
+   <li> While a "square hypergraph" is a hypergraph [V,E] with |V| = |E|, we
+   introduce the notion of a "bipolar hypergraph" ("bphg") as a pair [V,f],
+   s.t. [V,V,f] is a general hypergraph. </li>
+   <li> A "polar hypergraph" is a bipolar hypergraph [V,f] s.t. for all
+   y in f(x) we also have x in f(y). </li>
+   <li> Specialising correspondences [R,A,B] to "relationals" [R,A],
+   considering only symmetric relations, representing the relation R <= A^2
+   by a function from A to P(A), and changing the order, we have exactly the
+   polar hypergraphs; dropping "symmetric", we get the bipolar hypergraphs.
+   </li>
+   <li> Another view is that the bipolar hypergraphs [V,f] correspond exactly
+   to the digraphs with vertex set V, where f(v) is the set of out-neighbours,
+   while polar hypergraphs correspond exactly to the graphs with vertex set
+   V. </li>
+   <li> The notion of a "polar hypergraph" comes from incidence structures:
+    <ol>
+     <li> Given an incidence structure [I,A,B], a "polarity" p is a bijection
+     p: A -> B such that I(x,p(y)) = I(y,p(x)) for all x, y in A. </li>
+     <li> Given such a polarity, we obtain a "polar indicence structure" as
+     a pair [I',A] with I': A^2 -> {0,1} through I'(x,y) := I(x,p(y)) (where
+     I' is symmetric). </li>
+     <li> And while incidence structures correspond to general hypergraphs,
+     polar incidence structures correspond to polar hypergraphs. </li>
+     <li> See ComputerAlgebra/IncidenceStructures/Lisp/plans/general.hpp. </li>
+    </ol>
+   </li>
+   <li> Relations with graphs:
+    <ol>
+     <li> We have natural translations gl2phg, dgl2bphg and phg2gl, bphg2dgl.
+     </li>
+     <li> Additionally we also have bphg2g, which is the same as g2dg after
+     bphg2dgl. </li>
+     <li> For the direction (d)gl2(b)phg, it seems most natural to implement
+     the function f through hash-maps. </li>
+     <li> Additionally we have stdgl2phg and stddgl2bphg, which use arrays.
+     </li>
+    </ol>
+   </li>
+  </ul>
+
 */
 
