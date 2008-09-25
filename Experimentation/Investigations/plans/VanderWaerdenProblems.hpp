6d88515796649b3ac8a5f2c8808c5e4ec86f0638
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-09-25 05:14:47 +0100

    Experiments on van der Waerden numbers, and some plans for solving them.

--- Experimentation/Investigations/plans/VanderWaerdenProblems.hpp
+++ Experimentation/Investigations/plans/VanderWaerdenProblems.hpp
@@ -20,6 +20,63 @@ License, or any later version. */
   </ul>
 
 
+  \todo Van der Waerden numbers
+  <ul>
+   <li> Via
+   \verbatim
+VanderWaerden-O3-DNDEBUG k n > VanderWaerden_2_k_n.cnf
+   \endverbatim
+   boolean problems can be generated quickly. </li>
+   <li> In Maxima we have "output_vanderwaerden2_stdname(k,n)" (in
+   ComputerAlgebra/Satisfiability/Lisp/Generators/VanderWaerdenProblems.mac. </li>
+   <li> vanderwaerden(2,3) = 9 (density = 3.6) </li>
+   <li> vanderwaerden(2,4) = 35 (density = 10.7) </li>
+   <li> vanderwaerden(2,5) = 178 (density = 43.5)
+    <ol>
+     <li> Relatively easy for OKsolver-2002 (710s, 109703 nodes). </li>
+     <li> march_pl needs only 51529 nodes and 238 seconds, so the "double
+     look-ahead" seems to be successful here. </li>
+     <li> minisat needs 213 seconds, so also clause-learning seems applicable
+     here. </li>
+    </ol>
+   </li>
+   <li> vanderwaerden(2,6) = 1132 (density = 225.6) </li>
+    <ol>
+     <li> n = 1000
+      <ul>
+       <li> ubcsat-irots seems best. </li>
+       <li> But finding a solution seems hopeless; less than 1400 falsified
+       clauses seems to require billions of steps --- the difference between
+       100,000 and 10,000,000 steps is very small. </li>
+       <li> We need to look into the Heule-et-al article. </li>
+      </ul>
+     </li>
+     <li> n = 1132
+      <ol>
+       <li> OKsolver-2002
+       \verbatim
+> OKsolver_2002-O3-DNDEBUG -M -D30 VanderWaerden_2_6_1132.cnf
+       \endverbatim
+       looks hopeless (no node of depth 30 solved in one hour). </li>
+      </ol>
+     </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  \todo Symmetry breaking for van der Waerden numbers
+  <ul>
+   <li> As for any hypergraph colouring problems, one can choose any variable
+   and set it to true (or to false), due to the symmetry between 0 and 1. </li>
+   <li> This is best "told" the solver, so that it can use this for the
+   branching variable at the root. </li>
+   <li> No other symmetry breaking seems possible without conditioning. </li>
+   <li> For small problems one needs to determine the full automorphism group
+   of the clause-sets. </li>
+  </ul>
+
+
   \todo Overview on performance of SAT solvers
   <ul>
    <li> We need an overview how SAT solvers perform on the different van der
@@ -225,6 +282,17 @@ PercentSuccess = 100.00
      whole computation is checked via a simple sieve of Erathostenes. </li>
     </ol>
    </li>
+  </ul>
+
+
+  \todo Faster local search
+  <ul>
+   <li> For greentao it seems the only structure which can be exploited is
+   the complement-invariance. </li>
+   <li> For vanderwaerden there is much more structure which could be exploited
+   (using "virtual" clause-sets). </li>
+   <li> We should try to understand why the different local search algorithms
+   behave so differently on the various problem classes. </li>
   </ul>
 
 */
