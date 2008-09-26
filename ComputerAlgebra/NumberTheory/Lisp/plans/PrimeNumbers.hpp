fc5b8902c4b03d8e5340e6eabdbf5ac14c6ddaac
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-09-26 05:21:07 +0100

    On the number of arithmetic progressions amongst the primes.
    MG: please write some tests.

---
+++ ComputerAlgebra/NumberTheory/Lisp/plans/PrimeNumbers.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 7.6.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/NumberTheory/Lisp/plans/PrimeNumbers.hpp
+  \brief Plans regarding prime numbers
+
+
+  \todo Connections
+  <ul>
+   <li> See Experimentation/Investigations/plans/AdditiveNumberTheory.hpp.
+   </li>
+  </ul>
+
+*/
+
---
+++ Experimentation/Investigations/plans/AdditiveNumberTheory.hpp
@@ -0,0 +1,36 @@
+// Oliver Kullmann, 26.9.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/plans/AdditiveNumberTheory.hpp
+  \brief On investigations into additive number theory
+  
+  
+  \todo The distribution of arithmetic progressions amongst primes
+  <ul>
+   <li> The task is to find a nice (thus very likely approximated) law for
+   the values in the list ln_arithprog_primes_c(k,n) (see
+   ComputerAlgebra/NumberTheory/Lisp/PrimeNumbers.mac) for fixed k >= 1. </li>
+   <li> Ploted via e.g.
+   \verbatim
+plot2d([discrete,create_list(i,i,1,1000),ln_arithprog_primes_c(3,1000)]);
+   \endverbatim
+   </li>
+   <li> For k = 1,2 this is trivial. </li>
+   <li> For k >= 3 regression is to be performed; most powerful is using R,
+   but for initial considerations also simple_linear_regression (use
+   'load("stats")') can be used. </li>
+   <li> There are role models for the regression function in the literature.
+   </li>
+   <li> One should also consider n_arithprog_primes_nc[k,n] (the non-cumulative
+   data, i.e., as list the difference list of the above list). </li>
+   <li> And one can consider the densities
+   ln_arithprog_primes_c(k,n) / create_list(i,i,1,n). </li>
+  </ul>
+
+*/
+
