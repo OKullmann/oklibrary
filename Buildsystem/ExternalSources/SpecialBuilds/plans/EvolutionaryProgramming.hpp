4c61900a33647f2239cc82eac9d914b6c7c264aa
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-10-01 14:33:39 +0100

    Started plans on installing tools for generic programming.

---
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/EvolutionaryProgramming.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 1.10.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Buildsystem/ExternalSources/SpecialBuilds/plans/EvolutionaryProgramming.hpp
+  \brief Plans regarding installation of systems for genetic algorithms etc.
+
+
+  \todo Frameworks for genetic algorithms
+  <ul>
+   <li> EO Evolutionary Computation Framework http://eodev.sourceforge.net/
+   looks reasonable, and is LGPL. </li>
+  </ul>
+
+*/
