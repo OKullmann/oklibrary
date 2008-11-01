d979b7e70742b6993782dec9a0ffe057d1a16f12
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-11-01 16:09:16 +0000

    Update on the performance of solvers on Green-Tao problems.

--- Experimentation/Investigations/plans/VanderWaerdenProblems.hpp
+++ Experimentation/Investigations/plans/VanderWaerdenProblems.hpp
@@ -275,6 +275,10 @@ Memory used           : 1826.34 MB
 CPU time              : 85622.6 s
      \endverbatim
      </li>
+     <li> Also for march_pl it looks hopeless (after 51 hours
+     no progress visible; 138348 clauses were added by
+     preprocessing). </li>
+     <li> UnitMarch </li>
      <li> Gets harder for n = 30000, but still rather easy:
      \verbatim
 ubcsat-okl -alg rnovelty+ -runs 10 -cutoff 10000000 -i GreenTao_2_5_30000.cnf
