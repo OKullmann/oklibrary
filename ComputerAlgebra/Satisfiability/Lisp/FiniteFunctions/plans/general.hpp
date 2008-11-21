46858a7b03dd70099fb936532c63f882992c3db1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-11-21 21:16:42 +0000

    On the basic concepts for finite functions (Maxima/Lisp-level).

--- ComputerAlgebra/Satisfiability/Lisp/FiniteFunctions/plans/general.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/FiniteFunctions/plans/general.hpp
@@ -31,12 +31,38 @@ License, or any later version. */
    together with conversions to other forms. </li>
    <li> Such "conversions" can involve themselves satisfiability problems in
    various forms. </li>
+   <li> The notion of a "finite function":
+    <ol>
+     <li> A triple [n,m,f], where n is the number of inputs, m the number
+     of outputs, and f the function, which accepts lists of length n,
+     and returns lists of length m. </li>
+     <li> Let's speak of a "n x m" finite function. </li>
+     <li> The default is boolean values; shall this be "false, true", or
+     "0, 1", or perhaps "-1, +1" ? </li>
+     <li> If a quadruple [n,m,f,D] is used, then for input and output
+     the set of values is D. </li>
+     <li> And if a quintuple [n,m,f,D,C] is used, then the values for the
+     inputs are taken from D, while the values for the output are taken
+     from C. </li>
+     <li> Abbreviations: "bf" for "boolean function", "ff" for "finite
+     function" (in general). </li>
+     <li> An important construction of a finite function is from a
+     "gate list", which is a list L consistings of pairs [I,f], where
+     f is an n x m ff, while I is a list of length n specifying the
+     inputs of n, which are either pairs [i,j] for the j-th function in L
+     with output number j, or are numbers i for the i-th input. </li>
+     <li> For the output we need to specify a list with pairs [i,j]
+     or i (as before). </li>
+     <li> Perhaps, when the function has only one output, then a singleton
+     list [i] can be used. </li>
+    </ol>
+   </li>
   </ul>
 
 
   \todo Boolean circuits
   <ul>
-   <li> Of interest finding boolean circuits (w.r.t. different bases) for
+   <li> Of interest is finding boolean circuits (%w.r.t. different bases) for
    given boolean functions. </li>
    <li> See the work of Alexander S. Kulikov for example. </li>
   </ul>
@@ -44,6 +70,10 @@ License, or any later version. */
 
   \todo Representations of fundamental functions:
   <ul>
+   <li> We need the standard names for the 16 binary functions (which are
+   then provided as constants). </li>
+   <li> We also need all the functions in the context of adding,
+   multiplication, subtraction and division. </li>
    <li> Cardinality constraints:
     <ol>
      <li> At least k out of n boolean variables are true. </li>
