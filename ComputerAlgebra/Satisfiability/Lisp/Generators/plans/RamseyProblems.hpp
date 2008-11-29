fc026caa3d1d0dbeb9dabf54e879ce8c422bcfbb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-11-29 21:15:04 +0000

    Editorial corrections.

--- ComputerAlgebra/Satisfiability/Lisp/Generators/plans/RamseyProblems.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/plans/RamseyProblems.hpp
@@ -79,10 +79,10 @@ License, or any later version. */
          allows a colouring without one. </li>
         </ol>
        </li>
-       <li> Perhaps rather than setting specific colours, only the fact that the
-       two colours must be different should be coded. Perhaps by reducing 2 
-       variables a,b to just variable a, by replacing variable b with literal 
-       -a. </li>
+       <li> Perhaps rather than setting specific colours, only the fact that
+       the two colours must be different should be coded. Perhaps by reducing
+       2 variables a,b to just variable a, by replacing variable b with
+       literal -a. </li>
       </ul>
      </li>
      <li> More general than above, also for T with one affected edge one
@@ -133,13 +133,17 @@ License, or any later version. */
      position of these two triangles. </li>
      <li> Use of the Pigeon Hole Principle to collapse more edges:
       <ul>
-       <li> In general, when considering NR([q,q],r) with n vertices, 
-       by the Pigeon Hole Principle, given x monochromatic p-cliques, 
-       ceil(x / r) of the p-cliques must be the same colour. </li>
-       <li> Therefore, in the case of NR([p,p],2) it should be possible to 
-       collapse half (rounded up) of the p-cliques (i.e all the variables
-       representing the edges) to a single variable, for each p considered. 
-       </li>
+       <li> In general, when considering NR([p,p],2) with n vertices, 
+       by the pigeonhole principle, given x monochromatic p-cliques, 
+       ceil(x / 2) of the p-cliques must be the same colour. </li>
+       <li> Therefore it should be possible to collapse half (rounded up) of
+       the p-cliques (i.e all the variables representing the edges) to a
+       single variable, for each p considered. </li>
+       <li> Given the symmetry of the two colours (i.e the fact that
+       flipping the colours in a given total assignment doesn't affect
+       satisfiability), we can then set one of the above mentioned variables
+       (collapsed using the PHP) to a particular colour. For instance, setting
+       half of the p-cliques (for a given p) to colour 1. </li>
        <li> For example, consider NR([4,4],2), n=18. With MG's idea, we find 5
        monochromatic triangles in the graph. At least 3 of these must be
        coloured 1, or at least 3 of these must be coloured 2, therefore, we
@@ -153,23 +157,19 @@ License, or any later version. */
        <li> A proof is needed. (This should be similar to the proof for MG's
        idea, as intuitively the idea is that every p-clique, given they are
        vertex-disjoint, can be transformed to any other by some permutation in 
-       S_n) </li>
+       S_n). </li>
       </ul>
-     <li> Also given the symmetry of the two colours (i.e the fact that flipping 
-     the colours in a given total assignment doesn't affect satisfiability), 
-     we can then set one of the above mentioned variables (collapsed using the 
-     PHP) to a particular colour. For instance, setting half of the p-cliques
-     (for a given p) to colour 1. </li>
+     </li>
      <li> It seems that this process yields more variable reductions than
      the above process?
       <ul> 
-       <li> The method used below for calculation assumes that, starting with l 
-       unaffected vertices, if a process/idea needs x unaffected vertices for 
-       application at each step, and it affects y vertices, then the x - y 
-       unaffected vertices can be reused at each step, leaving l - y vertices to 
-       act on, until finally there are x - y <=  z  < x unaffected vertices 
-       left, at which point, z < x, therefore the process can no longer be 
-       applied, giving floor((l - (x - y)) / y) possible applications of the 
+       <li> The method used below for calculation assumes that, starting with
+       l unaffected vertices, if a process/idea needs x unaffected vertices
+       for application at each step, and it affects y vertices, then the x - y
+       unaffected vertices can be reused at each step, leaving l - y vertices
+       to act on, until finally there are x - y <=  z  < x unaffected vertices
+       left, at which point, z < x, therefore the process can no longer be
+       applied, giving floor((l - (x - y)) / y) possible applications of the
        procedure.
         <ul>
          <li> For example, NR([4,4],2), n=l=18 </li>
@@ -184,12 +184,13 @@ License, or any later version. */
          affected, 6 unaffected. </li>
          <li> Choose 6 more, and another 3 for a triangle, leaving 15 vertices
          affected, 3 unaffected. </li>
-         <li> The procedure must stop here as we need 6 unaffected vertices, but
-         only have 3. Giving 5 monochromatic triangles. </li>
-         <li> Since there must always be (x-y) unaffected vertices more than the
-         y we affect to allow application (i.e x in total), we can simply take 
-         l - (x-y) and then divide by y (taking the floor as we can not apply 
-         the procedure for any number of unaffected vertices less than x). </li>
+         <li> The procedure must stop here as we need 6 unaffected vertices,
+         but only have 3. Giving 5 monochromatic triangles. </li>
+         <li> Since there must always be (x-y) unaffected vertices more than
+         the y we affect to allow application (i.e x in total), we can simply
+         take l - (x-y) and then divide by y (taking the floor as we can not
+         apply the procedure for any number of unaffected vertices less than
+         x). </li>
         </ul>
        </li>
        <li> Taking NR([5,5],2), n=43 as an example.
@@ -202,9 +203,9 @@ License, or any later version. */
          the vertex sets of each path need to be disjoint. </li>
          <li> floor((l - (x - y)) / y) = floor((43 - (2))/3) = floor(41/3) = 13
          vertex-disjoint multicolour paths of length 2. </li>
-         <li> Each of these paths comprises of 2 edges which we are setting with
-         a colour, therefore there are 13 * 2 = 26 edges/variables being set.
-         </li>
+         <li> Each of these paths comprises of 2 edges which we are setting
+         with a colour, therefore there are 13 * 2 = 26 edges/variables being
+         set. </li>
         </ul>
        </li>
        <li> Using MG's suggestion for NR([5,5],2), n=43
@@ -214,28 +215,29 @@ License, or any later version. */
          floor((43 - 14) / 4) = floor(29 / 4) = 7 vertex-disjoint monochromatic
          4-cliques. </li>
          <li> So therefore we can find 7 vertex-disjoint monochromatic 
-         4-cliques. Each clique has 6 edges, representing 6 variables, which can 
-         be reduced to a single variable, so 7 * 6 = 42 edges, which can be 
-         represented using only 6 edges, so 42 - 6 = 36 variables removed. </li>
+         4-cliques. Each clique has 6 edges, representing 6 variables, which
+         can be reduced to a single variable, so 7 * 6 = 42 edges, which can
+         be represented using only 6 edges, so 42 - 6 = 36 variables removed.
+         </li>
          <li> 7 4-cliques affect 7 * 4 = 28 vertices, leaving 43 - 28 = 15
          unaffected vertices. </li>
          <li> NR([3,3],2) = 6,  so x = 6, y = 4, l = 15. </li>
-         <li> This gives, floor((l - (x - y)) / y) = floor((15 - (6 - 3)) / 3) =
-         floor((15 -3)/3) = floor(12/3) = 4 vertex-disjoint monochromatic 
+         <li> This gives, floor((l - (x - y)) / y) = floor((15 - (6 - 3)) / 3)
+         = floor((15 -3)/3) = floor(12/3) = 4 vertex-disjoint monochromatic
          3-cliques. </li>
-         <li> Each 3-clique has 3 edges, representing 3 variables, which can be 
-         reduced to a single variable, so 4 * 3 = 12 edges/variables before
-         reduction, 12 - 4 = 8 removed. %I.e, since 4 variables are being used to
-         instead of 12, we have removed/collapsed 8 variables. </li>
+         <li> Each 3-clique has 3 edges, representing 3 variables, which can
+         be reduced to a single variable, so 4 * 3 = 12 edges/variables before
+         reduction, 12 - 4 = 8 removed. %I.e, since 4 variables are being used
+         to instead of 12, we have removed/collapsed 8 variables. </li>
          <li> So without taking into account the PHP statement above, we see a
          reduction/loss of 36 + 8 = 44 variables. OK : Likely for the second
          step the above process can be used?! MG : Yes, this is possible,
          and should result in more variables removed/set for n=45. </li>
-         <li> Including the PHP statement above, we can set 4 of the 7 4-cliques
-         to a colour c, and we can replace 2 of the variables representing the 4
-         3-cliques with 1 variable. Therefore we lose an additional 5 variables,
-         4 set to a specific colour and 2 reduced to 1 (a loss of 1), so 44 + 5 
-         = 49 variables lost. ??? </li>
+         <li> Including the PHP statement above, we can set 4 of the 7
+         4-cliques to a colour c, and we can replace 2 of the variables
+         representing the 4 3-cliques with 1 variable. Therefore we lose an
+         additional 5 variables, 4 set to a specific colour and 2 reduced to
+         1 (a loss of 1), so 44 + 5 = 49 variables lost. ??? </li>
         </ul>
        </li>
       </ul>
@@ -263,15 +265,16 @@ License, or any later version. */
      <li> There is now a function "ramsey2_sym_break_rec" which generates a set
      of clauses to break the symmetries described in MG's idea above. </li>
      <li> The need for this function to take mappings from variables to 
-     their "meanings" should be alleviated once there is are "rank" and "unrank"
-     functions for the enumeration of k-subsets 
-     (See "Module Enumeration" in ComputerAlgebra/plans/general.hpp ). It should
-     also only need the "n" for the Ramsey problem then. </li>
-     <li> Should this function also consider the PHP idea mentioned above? </li>
+     their "meanings" should be alleviated once there is are "rank" and
+     "unrank" functions for the enumeration of k-subsets 
+     (See "Module Enumeration" in ComputerAlgebra/plans/general.hpp ). It
+     should also only need the "n" for the Ramsey problem then. </li>
+     <li> Should this function also consider the PHP idea mentioned above?
+     </li>
     </ul>
    </li>
-   <li> How do the above ideas generalise to Ramsey problems which aren't of the form
-   R([q,q],r) ? </li>
+   <li> How do the above ideas generalise to Ramsey problems which aren't of
+   the form R([q,q],r) ? </li>
   </ul>
 
 */
