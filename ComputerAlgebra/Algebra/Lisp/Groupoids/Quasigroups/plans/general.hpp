a1d002b8ba3832a6cfc7f9e5f9c08350556a02d9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-12-29 18:38:36 +0000

    Update of tests (especially regarding quasi-groups).
    And new sub-module on quasi-groups.

---
+++ ComputerAlgebra/Algebra/Lisp/Groupoids/Quasigroups/plans/general.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 27.12.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/Groupoids/Quasigroups/plans/general.hpp
+  \brief Plans in general regarding quasi-groups
+
+
+  \todo Connections to other modules
+  <ul>
+   <li> See todo "Quasigroups" in
+   Applications/AlgebraicStructures/plans/Groupoids.hpp. </li>
+   <li> See
+   ComputerAlgebra/Satisfiability/Lisp/Generators/plans/LatinSquares.hpp. </li>
+  </ul>
+
+*/
+
