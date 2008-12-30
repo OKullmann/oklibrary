0273a18195d7263b48b18c28412b7dbfb2a0f449
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2008-12-30 13:14:35 +0000

    Started new module on LatinSquares (at Maxima/Lisp level).

---
+++ ComputerAlgebra/CombinatorialMatrices/Lisp/LatinSquares/plans/general.hpp
@@ -0,0 +1,13 @@
+// Oliver Kullmann, 30.12.2008 (Swansea)
+/* Copyright 2008 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/CombinatorialMatrices/Lisp/LatinSquares/plans/general.hpp
+  \brief General plans regarding latin squares in general
+
+*/
+
