74fc5c435911cfb18c5951b2021f334cf994392c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-03 11:30:49 +0000

    Started considering "association schemes".

---
+++ ComputerAlgebra/docus/CourseCombinatorics_LintWilson/Chapter30.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 1.1.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/docus/CourseCombinatorics_LintWilson/Chapter30.hpp
+  \brief Chapter "Association schemes" from [A Course in %Combinatorics]
+
+
+  <h1> Basic notions </h1>
+
+  <ul>
+   <li> See module
+   ComputerAlgebra/CombinatorialMatrices/Lisp/AssociationSchemes. </li>
+   <li> And especially
+   ComputerAlgebra/CombinatorialMatrices/Lisp/AssociationSchemes/plans/general.hpp,
+   where currently the fundamental notions and structures are discussed.
+   </li>
+  </ul>
+
+*/
+
--- ComputerAlgebra/docus/CourseCombinatorics_LintWilson/index.hpp
+++ ComputerAlgebra/docus/CourseCombinatorics_LintWilson/index.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 3.7.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -25,6 +25,10 @@ License, or any later version. */
    <li> Trees: CourseCombinatorics_LintWilson/Chapter02.hpp </li>
    <li> Colorings of graphs and Ramsey's theorem:
    CourseCombinatorics_LintWilson/Chapter03.hpp </li>
+   <li> Strongly regular graphs and partial geometries:
+   CourseCombinatorics_LintWilson/Chapter21.hpp </li>
+   <li> Association schemes:
+   CourseCombinatorics_LintWilson/Chapter30.hpp </li>
   </ol>
 
 */
