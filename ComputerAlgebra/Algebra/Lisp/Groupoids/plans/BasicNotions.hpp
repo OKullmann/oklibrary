1325f06812ea3f750bef61fa351ad06064a9ed47
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-04 13:37:18 +0000

    Renamed "grpn" to "ugrp".

--- ComputerAlgebra/Algebra/Lisp/Groupoids/plans/BasicNotions.hpp
+++ ComputerAlgebra/Algebra/Lisp/Groupoids/plans/BasicNotions.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 9.7.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -49,8 +49,8 @@ License, or any later version. */
     <ol>
      <li> Two consecutive digits don't look good?
       <ol>
-       <li> We could speak of "grpn" (group with neutral element) and
-       "grpi" (group with inverse), and "grpni". </li>
+       <li> We could speak of "ugrp" (unital group, i.e., group with neutral
+       element) and "grpi" (group with inverse), and "ugrpi". </li>
        <li> Then we should also use "qgrps". </li>
       </ol>
      </li>
