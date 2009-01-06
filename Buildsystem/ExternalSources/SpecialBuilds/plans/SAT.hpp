6e32ea5ba441692604e6d04094fa2e503bfa05af
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-06 20:12:57 +0000

    Update on installing OpenSMT.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/SAT.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/SAT.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 16.10.2007 (Swansea)
-/* Copyright 2007, 2008 Oliver Kullmann
+/* Copyright 2007, 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -71,7 +71,7 @@ License, or any later version. */
    </li>
    <li> OpenSMT
     <ol>
-     <li> http://verify.inf.unisi.ch/tools </li>
+     <li> http://verify.inf.unisi.ch/opensmt </li>
     </ol>
    </li>
   </ul>
