c57799ecf0c25869e22450cdf915a89adc3fd87b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-06 01:56:02 +0000

    *** Initial version 0.0.3 (Combinatorics/Lisp/Enumeration) ***

---
+++ ComputerAlgebra/Combinatorics/Lisp/Enumeration/plans/milestones.hpp
@@ -0,0 +1,48 @@
+// Oliver Kullmann, 5.1.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Combinatorics/Lisp/Enumeration/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/Combinatorics/Enumeration 0.0.3 (5.1.2009)
+
+
+  \par Version 0.0.4
+
+  \par
+   In ComputerAlgebra/Combinatorics/Lisp/Enumeration/plans/general.hpp the
+   following topics are handled:
+    - Enumerating all k-subsets
+
+  \par
+   Expand the following milestones.
+
+
+  \par Version 0.0.5
+
+  \par
+   The basic functions for enumerating (and ranking) k-subsets are
+   provided.
+
+
+  \par Version 0.1
+
+  \par
+   Basic functionality for enumerating k-subsets and subsets is
+   provided, basic concepts have been developed, and a development
+   plan has been established.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.3 : 5.1.2009; basic plans to start with.
+
+*/
+
