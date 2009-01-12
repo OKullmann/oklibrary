3c0be535d71bd5721dd60c3ca2940f745bee4bc4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-12 22:30:17 +0000

    *** Initial version 0.2 (Algebra/Lisp/Groupoids) ***

---
+++ ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp
@@ -0,0 +1,55 @@
+// Oliver Kullmann, 12.1.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp
+
+  \module_version Algebra/Lisp/Groupoids 0.2 (12.1.2009)
+
+
+  \par Version 0.2.1
+
+  \par
+   In ComputerAlgebra/Algebra/Lisp/Groupoids/plans/Homomorphisms.hpp the
+   following topics are handled:
+    - Complete the implementation
+
+
+  \par Version 0.2.2
+
+  \par
+   In ComputerAlgebra/Algebra/Lisp/Groupoids/plans/BasicNotions.hpp the
+   following topics are handled:
+    - Notions and notations
+
+
+  \par Version 0.2.3
+
+  \par
+   In ComputerAlgebra/Algebra/Lisp/Groupoids/plans/general.hpp the
+   following topics are handled:
+    - Write tests
+
+
+  \par Version 0.3
+
+  \par
+   In ComputerAlgebra/Algebra/Lisp/Groupoids/plans/general.hpp the
+   following topics are handled:
+    - Forms of "constructive" groupoids
+    - Straight-line programs
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.2   : 12.1.2009; initial version number, basic functionality established
+ regarding groupoids, the most important special cases, and regarding operations and actions.
+
+*/
--- ComputerAlgebra/Algebra/Lisp/plans/milestones.hpp
+++ ComputerAlgebra/Algebra/Lisp/plans/milestones.hpp
@@ -8,18 +8,28 @@ License, or any later version. */
 /*!
   \file ComputerAlgebra/Algebra/Lisp/plans/milestones.hpp
 
-  \module_version ComputerAlgebra/Algebra/Lisp 0.0.5 (15.6.2008)
+  \module_version ComputerAlgebra/Algebra/Lisp 0.0.5.1 (12.1.2009)
 
 
   \par Version 0.0.6
+
+  \par Algebra/Lisp/Groupoids version 0.2.1
+  (see ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp)
 
   \par
    In ComputerAlgebra/Algebra/Lisp/plans/general.hpp the following topics
    are handled:
     - Modules
-    - Semirings, rings, fields
-    - Modular arithmetic
-    - Actions and operations
+    - Semirings, rings, fields : DONE
+    - Module UniversalAlgebra : DONE
+    - Modular arithmetic : DONE
+    - Actions and operations : DONE
+
+
+  \par Version 0.0.7
+  
+  \par Algebra/Lisp/Groupoids version 0.2.3
+  (see ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp)
 
   \par
    In ComputerAlgebra/Algebra/Lisp/plans/FiniteFields.hpp
