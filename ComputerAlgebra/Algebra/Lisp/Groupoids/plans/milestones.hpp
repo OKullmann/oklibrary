598542a2c65685a378260a02b83bd8f816090026
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-14 14:34:11 +0000

    Update.

--- ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp
+++ ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp
@@ -8,7 +8,7 @@ License, or any later version. */
 /*!
   \file ComputerAlgebra/Algebra/Lisp/Groupoids/plans/milestones.hpp
 
-  \module_version Algebra/Lisp/Groupoids 0.2 (12.1.2009)
+  \module_version Algebra/Lisp/Groupoids 0.2,0,1 (14.1.2009)
 
 
   \par Version 0.2.1
@@ -17,6 +17,9 @@ License, or any later version. */
    In ComputerAlgebra/Algebra/Lisp/Groupoids/plans/Homomorphisms.hpp the
    following topics are handled:
     - Complete the implementation
+
+  \par
+   Add basic functionality regarding null and idempotent elements : DONE
 
 
   \par Version 0.2.2
