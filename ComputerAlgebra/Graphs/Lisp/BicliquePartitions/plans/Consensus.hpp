ba807c1208c5327f85bc6b1fc0e3cdea2914feee
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-15 17:01:51 +0000

    Plans for computing all maximal bicliques via the "consensus" algorithm.

---
+++ ComputerAlgebra/Graphs/Lisp/BicliquePartitions/plans/Consensus.hpp
@@ -0,0 +1,35 @@
+// Oliver Kullmann, 15.1.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Graphs/Lisp/BicliquePartitions/plans/Consensus.hpp
+  \brief Plans regarding implementing the "consensus algorithm" from [XXX] for computing all maximal bicliques
+  
+  
+  \todo Notions and notations
+  <ul>
+   <li> A kind of dictionary is needed. </li>
+   <li> Compare with the functions for resolution. </li>
+  </ul>
+
+
+  \todo The basic "consensus" operation
+  <ul>
+   <li> Discuss the basic forms. </li>
+  </ul>
+
+
+  \todo Computing the maximal elements in a list of bicliques
+
+
+  \todo The consensus iteration
+  <ul>
+   <li> Discuss the basic forms. </li>
+  </ul>
+
+*/
+
