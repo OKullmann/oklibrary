84423c12773398fad1c3879e7af88d6f7c05e7e8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-17 23:38:37 +0000

    Editorial updates on plans on Ramsey-generators in part Applications.

--- Applications/RamseyTheory/plans/Van_der_Waerden_hypergraph.hpp
+++ Applications/RamseyTheory/plans/Van_der_Waerden_hypergraph.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 31.10.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,24 +10,30 @@ License, or any later version. */
   \brief Plans on the module on computing van der Waerden numbers
 
 
-  \todo Reactive test system.
+  \todo Reactivate test system.
 
 
   \todo Update namespace.
 
 
-  \todo New test system:
-  Transform the tests to the new testsystem.
+  \todo New test system
+  <ul>
+   <li> Transform the tests to the new testsystem.
+  </ul>
 
 
-  \todo Update:
-  Transfer the todo's from RamseyTheory/Van_der_Waerden_hypergraph.hpp
-  to here (thereby updating them).
+  \todo Update
+  <ul>
+   <li> Transfer the todo's from RamseyTheory/Van_der_Waerden_hypergraph.hpp
+   to here (thereby updating them). </li>
+  </ul>
 
 
-  \todo Generator:
-  Write a class template for generating van der Waerden-hypergraphs
-  (as model of a general hypergraph concept).
+  \todo Generator
+  <ul>
+   <li> Write a class template for generating van der Waerden-hypergraphs
+   (as model of a general hypergraph concept). </li>
+  </ul>
 
 */
 
--- Applications/RamseyTheory/plans/general.hpp
+++ Applications/RamseyTheory/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 23.1.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,8 +10,11 @@ License, or any later version. */
   \brief Plans for the module on %Ramsey theory (especially
   van der Waerden's problem)
 
+  Here components are located on applying generalised SAT solving for
+  solving Ramsey-type problems.
 
-  \todo Update namespaces.
+
+  \todo Update namespaces
 
 
   \todo %Ramsey theory
@@ -21,16 +24,21 @@ License, or any later version. */
    <li> Such translators should only produce a hypergraph-colouring problem
    (to be handled then by the general translations in module
    Hypergraphs/Colourings). </li>
+   <li> However, for the "non-diagonal" forms we need to create non-boolean
+   clause-sets. </li>
   </ul>
 
 
   \todo Transfer:
-  Transferring (and updating) generators for
   <ul>
-   <li> OKsolver/Experimental/Transformationen/Ramsey </li>
-   <li> OKsolver/Experimental/Transformationen/VanderWaerden </li>
+   <li> Transferring (and updating) generators for
+    <ol>
+     <li> OKsolver/Experimental/Transformationen/Ramsey </li>
+     <li> OKsolver/Experimental/Transformationen/VanderWaerden </li>
+    </ol>
+   </li>
+   <li> Especially  providing active clause-sets is very interesting. </li>
   </ul>
-  Especially  providing active clause-sets is very interesting.
 
 */
 
