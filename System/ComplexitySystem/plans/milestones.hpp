9ce3224072ba400b765305f684faeff217ee6d9a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-18 11:58:54 +0000

    Plans on how to do precise time measurements.
    And editorial update.

--- System/ComplexitySystem/plans/milestones.hpp
+++ System/ComplexitySystem/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 25.6.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,37 +8,73 @@ License, or any later version. */
 /*!
   \file ComplexitySystem/plans/milestones.hpp
 
-  \par Current version 0.0.2 (25.6.2006; no code yet, but plans)
+  \module_version ComplexitySystem 0.0.2 (25.6.2006)
+
 
   \par Version 0.0.2 :
-  ComplexitySystem/plans/ComplexitySystem.hpp: "Designing the file structure"
+
+  \par
+   In ComplexitySystem/plans/general.hpp the following topics are handled:
+    - Designing the file structure
+
 
   \par Version 0.0.3 :
-  ComplexitySystem/plans/ComplexitySystem.hpp: "Designing the level system"
+
+  \par
+   In ComplexitySystem/plans/general.hpp the following topics are handled:
+    - Designing the level system
+
 
   \par Version 0.0.4 :
-  ComplexitySystem/plans/ComplexitySystem.hpp: "Designing the makefile"
+
+  \par
+   In ComplexitySystem/plans/general.hpp the following topics are handled:
+    - Designing the makefile
+    - Precision of measurements
+
 
   \par Version 0.0.5 :
-  ComplexitySystem/plans/ComplexitySystem.hpp: "Designing measurement classes"
+
+  \par
+   In ComplexitySystem/plans/general.hpp the following topics are handled:
+    - Designing measurement classes
+    - Update namespaces
+
 
   \par Version 0.0.6 :
-  ComplexitySystem/plans/ComplexitySystem.hpp: "Designing measurement accumulation"
 
+  \par
+   In ComplexitySystem/plans/general.hpp the following topics are handled:
+    - Designing measurement accumulation
+
+  
   \par Version 0.1.0 :
-  Quite complete specifications available. Enough experience with the boost serialisation library.
+
+  \par
+   Quite complete specifications available. Enough experience with the boost
+   serialisation library.
+
 
   \par Version 0.2.0 :
-  A first implementation.
+
+  \par
+   A first implementation.
+
 
   \par Version 0.3.0 :
-  Tested it with several modules; first experiences.
+
+  \par
+   Tested it with several modules; first experiences.
+
 
   \par Version 0.4.0 :
-  A simple but reasonable viewing systems exists.
 
-  \par Version 1.0.0 :
-  All modules are fully measured.
+  \par
+   A simple but reasonable viewing systems exists.
+
+
+  \par Version 1.0.0 : All modules are fully measured.
+
 
 
   -------------------------------------------------------------------------------------------------------------------------------------
