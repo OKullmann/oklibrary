6264b452c4568301b3eaa51d02005a026b0b2a15
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-22 20:25:38 +0000

    Editorial update.

--- Buildsystem/ExternalSources/SpecialBuilds/docus/Gap.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/docus/Gap.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 12.7.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -54,10 +54,6 @@ License, or any later version. */
    <li> There is no build-directory (which could be removed),
    but all is put into the installation-directory. </li>
   </ul>
-
-
-  <h3> Prerequisites </h3>
-
 
 
   <h3> Make targets </h3>
