b3ffd4cc2e24652f00f55c7049ff409f3d650e65
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-27 12:49:55 +0000

    Editorial corrections.
    Please regard the limit of 80 characters per line.
    And a bit care regarding separating lines and spaces makes
    it look nicer.

--- ComputerAlgebra/Satisfiability/Lisp/Generators/plans/RamseyProblems.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/plans/RamseyProblems.hpp
@@ -267,19 +267,21 @@ generalised_ucp1(fcs2cs(apply_pa_fcs(ramsey_symbr1e_pass(3,6), ofcs2fcs(ramsey2_
    the form R([q,q],r) ? </li>
   </ul>
 
+
   \todo Symmetry breaking by recursive application of PHP
   <ul>
    <li> Another conjectured symmetry breaking technique, making use of the
-    Pigeon Hole principle is as follows : 
+    pigeonhole principle, is as follows : 
     <ul>
-     <li> Fix a vertex "v", and then consider edges from "v" to all other 
-     vertices. Half (rounded up) of such edges will be of one colour, and so we
-     add binary clauses to show the equivalence of the colouring of these edges,
-     chosen arbitrarily.</li>
-     <li> Apply the same technique recursively to the half of the vertices which 
-     had an edge to them "coloured" by the first step. </li>
-     <li> Apply the same technique recursively to the half of the vertices which
-     didn't have an edge to them "coloured the first step. </li>
+     <li> Fix a vertex v, and then consider edges from v to all other 
+     vertices. </li>
+     <li> Half (rounded up) of such edges will be of one colour, and so we
+     add binary clauses to show the equivalence of the colouring of these
+     edges, chosen arbitrarily. </li>
+     <li> Apply the same technique recursively to the half of the vertices
+     which had an edge to them "coloured" by the first step. </li>
+     <li> Apply the same technique recursively to the half of the vertices
+     which didn't have an edge to them "coloured" the first step. </li>
     </ul>
    </li>
    <li> A basic implementation seems to be : 
@@ -302,7 +304,7 @@ ramsey_symbr3_cs_m(m,n) := block([mid_p,rs : {},edge_equivs],
    <li> The basic idea here is that once you apply the PHP initially, you then
    have two sets of vertices (1) ones that have been touched (2) ones that 
    haven't been touched. Within each set ((1) or (2)) the vertices are 
-   essentially the same and so one can simply consider the induced subgraphs
+   essentially the same, and so one can simply consider the induced subgraphs
    and apply the same PHP again. </li>
    <li> Some rough workings suggest that this method should result in 60 
    variable reductions for n=43 (using the same idea as in the other symmetry 
