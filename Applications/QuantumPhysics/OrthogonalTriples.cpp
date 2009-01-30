3134576ba9b92f0ed5d704dd714c79f53400e5e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-01-30 14:03:31 +0000

    Repair: Parameter ev.size leads to compilation problems, and is not needed.

--- Applications/QuantumPhysics/OrthogonalTriples.cpp
+++ Applications/QuantumPhysics/OrthogonalTriples.cpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 25.11.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -204,7 +204,7 @@ inline Graph orthogonality_relation(const Range& r) {
       if (orthogonal(*ip, *jp)) ev.push_back(edge_type(i, j));
     }
   }
-  Graph g(ev.begin(), ev.end(), boost::size(r), ev.size());
+  Graph g(ev.begin(), ev.end(), boost::size(r));
   assert(num_vertices(g) == boost::size(r));
   assert(num_edges(g) == ev.size());
   return g;
