81d841e3cd91702c90cf337b14c257c9de973ca3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-02-04 12:22:32 +0000

    Further editorial corrections.
    The page-break at 80 characters should be taken seriously (and not left
    to some automatism).
    Words and phrases, which are not used by referenced, need to be
    quoted (or otherwise distinguished).

--- ComputerAlgebra/Satisfiability/Lisp/Generators/docus/RamseyProblems.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/docus/RamseyProblems.hpp
@@ -17,24 +17,24 @@ License, or any later version. */
 
   <ul>
    <li> One may generate a symmetry breaking partial assignment for a given
-   %Ramsey problem of the form R(q,q;2) > n in the following way : 
+   %Ramsey problem of the form "R(q,q;2) > n" in the following way : 
    \verbatim
 RSP_pass : ramsey_symbr1_pass(q,n);
    \endverbatim
    </li>
    <li> One can also generate additional clauses for symmetry breaking of a 
-   given %Ramsey problem of the form R(q,q;2) > n in the following way :
+   given %Ramsey problem of the form "R(q,q;2) > n" in the following way :
    \verbatim
 RSB : ramsey_symbr2_cs(n);
    \endverbatim
    </li>
    <li> The above assumes that variables are given in the form "colv({a,b})" 
-   for edges connecting vertices "a" and "b". This need not be the case, and the
-   variables may be of any form (assuming they can be negated, and "abs" works 
-   on their negation), and then the second and third argument to the function are
-   functions mapping from the undirected edges (%e.g., {a,b}) to their corresponding 
-   undirected edges, and vice versa. </li>
-   <li> The reason for the above notation ("colv({a,b})") etc in the example 
+   for edges connecting vertices "a" and "b". This need not be the case, and
+   the variables may be of any form (assuming they can be negated, and "abs"
+   works on their negation), and then the second and third argument to the
+   function are functions mapping from the undirected edges (%e.g., {a,b}) to
+   their corresponding undirected edges, and vice versa. </li>
+   <li> The reason for the above notation ("colv({a,b})") etc. in the example 
    above is that the result of this can easily be converted to the format "aSb"
    sometimes used when generating clause-sets for the %Ramsey problems in the 
    extended Dimacs format. This can be done like so :
@@ -42,8 +42,8 @@ RSB : ramsey_symbr2_cs(n);
 RSB_e : map(lambda([a], map(lambda([b], sconcat(if sign(b) = neg then "-" else "",listify(args(abs(b))[1])[1],"S",listify(args(abs(b))[1])[2])),a)), RSB);
    \endverbatim
    </li>
-   <li> For a generated clause-set, the following code may be used to output the
-   clause-set in extended Dimacs format : 
+   <li> For a generated clause-set, the following code may be used to output
+   the clause-set in extended Dimacs format: 
    \verbatim
 colv2str(x) := block([vals_abs],
   vals_abs : listify(args(abs(x))[1]),
