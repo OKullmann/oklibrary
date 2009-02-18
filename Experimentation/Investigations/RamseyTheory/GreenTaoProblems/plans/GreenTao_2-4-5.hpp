09bbbd55684f4a4ddc5c4cef63c41a8d34613bb4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-02-18 15:56:39 +0000

    Update on experiments with Green-Tao problems.

--- Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_2-4-5.hpp
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_2-4-5.hpp
@@ -222,6 +222,8 @@ average length successful tries = 0
     <ol>
      <li> 39 runs with  cutoff=10^9 didn't find a solution (min between 1
      and 6). </li>
+     <li> 6 runs with  cutoff=4*10^9 didn't find a solution (min between 1
+     and 3). </li>
     </ol>
    </li>
    <li> n=4225
