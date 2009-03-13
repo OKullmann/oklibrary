6f6021e58ceae0433bac07e7e66403a8ba080687
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-03-13 20:12:43 +0000

    Todos on completing the implementation of the biclique-consensus algorithm.
    Actions for ML.

--- ComputerAlgebra/Graphs/Lisp/Bicliques/plans/Consensus.hpp
+++ ComputerAlgebra/Graphs/Lisp/Bicliques/plans/Consensus.hpp
@@ -39,6 +39,48 @@ License, or any later version. */
   \todo The consensus iteration
   <ul>
    <li> Discuss the basic forms. </li>
+   <li> Provide all basic definitions in
+   ComputerAlgebra/Graphs/Lisp/Bicliques/Consensus.mac (avoiding "similar").
+   </li>
+  </ul>
+
+
+  \todo Handling general graphs
+  <ul>
+   <li> Can the algorithms be generalised to  handle general graphs? </li>
+   <li> Regarding vertex bicliques: the bicliques of a general graph are
+   exactly the bicliques of the underlying graph (with loops). </li>
+   <li> Regarding edge bicliques, we can simply obtain them from the vertex
+   bicliques by using all combinations of underlying edges. </li>
+  </ul>
+
+
+  \todo Improving the implementations
+  <ul>
+   <li> "con_adj_gl_b1_1_b2_1_p" looks confusing; perhaps all these functions
+   can be captured by a single function. </li>
+  </ul>
+
+
+  \todo Compute all maximal bicliques
+  <ul>
+   <li> "con_alg1/2" should likely use "consensus". </li>
+   <li> We need also these algorithms without the argument C.
+    <ol>
+     <li> The trivial choice is just to take the edges on their own. </li>
+     <li> Does the running time depend on C (strongly)? </li>
+     <li> If yes, then some heuristics is needed. </li>
+     <li> Likely, we need some reasonable algorithm for finding a biclique
+     cover of small size.
+      <ol>
+       <li> One possibility is to chose some yet uncovered edge, find a maximal
+       biclique (in the original graph) covering this edge, and repeating this
+       process. </li>
+      <ol>
+     </li>
+    </ol>
+   </li>
+   <li> Perhaps "C" should be called "LB". </li>
   </ul>
 
 */
