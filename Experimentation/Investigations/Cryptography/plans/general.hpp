5b04d58c6c09a51380c076d6dcdd252c72017f24
Matthew Gwynne <360678@swan.ac.uk>
2009-03-28 21:16:43 +0000

    Started experimental investigations into AES.

---
+++ Experimentation/Investigations/Cryptography/plans/general.hpp
@@ -0,0 +1,15 @@
+// Matthew Gwynne, 28.03.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/Cryptography/plans/general.hpp
+  \brief General plans regarding investigations into Cryptographic systems.
+  
+  
+  \todo Create milestones.
+
+*/
