4bc71840cb727a27df919e9b1ffa12439e1b59c9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-04-04 08:44:29 +0100

    Editorial updates.

--- ComputerAlgebra/RamseyTheory/Lisp/VanderWaerden/plans/general.hpp
+++ ComputerAlgebra/RamseyTheory/Lisp/VanderWaerden/plans/general.hpp
@@ -7,7 +7,7 @@ License, or any later version. */
 
 /*!
   \file ComputerAlgebra/VanderWaerdenTheory/Lisp/VanderWaerden/plans/general.hpp
-  \brief Plans regarding Van der Waerden problems
+  \brief Plans regarding van der Waerden problems
 
 
   \todo Relations to other modules
@@ -21,18 +21,18 @@ License, or any later version. */
   </ul>
 
 
-  \todo Automorphisms of Van-der-Waerden hypergraphs
+  \todo Automorphisms of van-der-Waerden hypergraphs
   <ul>
    <li> We need to investigate experimentally the symmetries of these
    hypergraphs. </li>
   </ul>
 
 
-  \todo Automorphisms of Van-der-Waerden clause-sets
+  \todo Automorphisms of van-der-Waerden clause-sets
   <ul>
-   <li> The obvious automorphisms of diagonal Van-der-Waerden clause-sets are
+   <li> The obvious automorphisms of diagonal van-der-Waerden clause-sets are
    given by the inner product of automorphisms for the underlying
-   Van-der-Waerden hypergraph and the S_m when using m parts. </li>
+   van-der-Waerden hypergraph and the S_m when using m parts. </li>
    <li> Are there more? </li>
   </ul>
 
@@ -40,25 +40,28 @@ License, or any later version. */
   \todo Van-der-Waerden numbers
   <ul>
    <li> In RamseyTheory/Lisp/VanderWaerden/Numbers.mac we need to provide
-   everything known about Van-der-Waerden numbers. </li>
+   everything known about van-der-Waerden numbers. </li>
    <li> See "Systematic notations for the numbers in %Ramsey theory" in
    ComputerAlgebra/RamseyTheory/plans/general.hpp. </li>
    <li> And see
    Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/Transversals/plans/general.hpp.
    </li>
-   <li> Transversal and independence numbers
-    <ol>
-     <li> tau_arithprog_hg(k,n) is the transversal number of arithprog_hg(k,n),
-     while alpha_arithprog_hg(k,n) is the independence number of
-     arithprog_hg(k,n). </li>
-     <li> For fixed k, we want to easily extract the sequences
-     tau_arithprog_hg(k,-) and alpha_arithprog_hg(k,-), and likely it's also
-     best to specify the values by providing the known initial sequences
-     (and potentially sporadic values). </li>
-     <li> Perhaps we provide the initial sequences as values of
-     tau_arithprog_seq[k] and alpha_arithprog_seq[k] (perhaps best
-     starting with index 1):
-     \verbatim
+  </ul>
+
+
+  \todo Transversal and independence numbers
+  <ul>
+   <li> tau_arithprog_hg(k,n) is the transversal number of arithprog_hg(k,n),
+   while alpha_arithprog_hg(k,n) is the independence number of
+   arithprog_hg(k,n). </li>
+   <li> For fixed k, we want to easily extract the sequences
+   tau_arithprog_hg(k,-) and alpha_arithprog_hg(k,-), and likely it's also
+   best to specify the values by providing the known initial sequences
+   (and potentially sporadic values). </li>
+   <li> Perhaps we provide the initial sequences as values of
+   tau_arithprog_seq[k] and alpha_arithprog_seq[k] (perhaps best
+   starting with index 1):
+   \verbatim
 tau_arithprog_seq[3] : [
 0,0,1,1,1,2,3,4,4,5,
 5,6,6,6,7,8,9,10,11,11,
@@ -67,17 +70,17 @@ tau_arithprog_seq[3] : [
 25,26,27,28,29,30,31,32,33,34,
 34,35,36,36,37,38,39,39,40,41
 ]$
-     \endverbatim
+   \endverbatim
+   </li>
+   <li> Perhaps these lists are then transferred into a hash-map, which
+   additionally contains the other "sporadic" values. </li>
+   <li> Methods for lower bounds on tau_arithprog_hg(k,n):
+    <ul>
+     <li> For natural numbers 0 <= p, q with p+q=n we have
+     tau_arithprog_hg(k,n) >= tau_arithprog_hg(k,p) + tau_arithprog_hg(k,q).
      </li>
-     <li> Perhaps these lists are then transferred into a hash-map, which
-     additionally contains the other "sporadic" values. </li>
-     <li> Methods for lower bounds on tau_arithprog_hg(k,n):
-      <ul>
-       <li> For natural numbers 0 <= p, q with p+q=n we have
-       tau_arithprog_hg(k,n) >= tau_arithprog_hg(k,p) + tau_arithprog_hg(k,q).
-       </li>
-       <li> This method yields for the above sequence tau_arithprog_seq[3]:
-       \verbatim
+     <li> This method yields for the above sequence tau_arithprog_seq[3]:
+     \verbatim
 lb(n) := block([L : tau_arithprog_seq[3], l, m:0],
  l : length(L),
  for k : max(1,n-l) thru min(n-1,l) do block(
@@ -93,90 +96,88 @@ map(lb,create_list(i,i,1,60)) - tau_arithprog_seq[3];
 0,0,-1,-1,-1,-1,-1,-1,-1,-1,
 0,-1,-1,0,-1,-1,-1,0,-1,-1
 ]
-       \endverbatim
-       </li>
-       <li> At exactly the indices 15,42 do we get a non-trivial lower bound
-       (at these places we actually have exactly one minimum-transversal 
-       before, thus the transversal number should(?) increase by one). </li>
-       <li> Namely, L[15] = 7 = L[12] + L[3] = L[8] + L[7], and
-       L[42] = 26 = L[39] + L[3]. </li>
-       <li> Otherwise the bound is trivial. </li>
-      </ul>
+     \endverbatim
      </li>
-     <li> Methods fur upper bounds on tau_arithprog_hg(k,n):
-      <ul>
-       <li> tau_arithprog_hg(k,n) <= n. </li>
-       <li> More generally, for 0 <= p <= n we have tau_arithprog_hg(k,n) <=
-       tau_arithprog_hg(k,p) + (n - p). </li>
-       <li> Alternatively, for p >= 0 we have tau_arithprog_hg(k,n) <=
-       tau_arithprog_hg(k,n+p) - tau_arithprog_hg(k,p)
-       (from this by tau_arithprog_hg(k,n+p) <= tau_arithprog_hg(k,p)+n
-       we obtain the upper bound n). </li>
-       <li> It seems that the last bound is hardly useful; so all what we get
-       is the trivial bound that advancing one steps costs at most one. </li>
-       <li> For natural numbers n >= 0 and k >= 2 the following functions
-       checks whether the base-k representation of n contains the digit k-1:
-       \verbatim
+     <li> At exactly the indices 15,42 do we get a non-trivial lower bound
+     (at these places we actually have exactly one minimum-transversal 
+     before, thus the transversal number should(?) increase by one). </li>
+     <li> Namely, L[15] = 7 = L[12] + L[3] = L[8] + L[7], and
+     L[42] = 26 = L[39] + L[3]. </li>
+     <li> Otherwise the bound is trivial. </li>
+    </ul>
+   </li>
+   <li> Methods fur upper bounds on tau_arithprog_hg(k,n):
+    <ul>
+     <li> tau_arithprog_hg(k,n) <= n. </li>
+     <li> More generally, for 0 <= p <= n we have tau_arithprog_hg(k,n) <=
+     tau_arithprog_hg(k,p) + (n - p). </li>
+     <li> Alternatively, for p >= 0 we have tau_arithprog_hg(k,n) <=
+     tau_arithprog_hg(k,n+p) - tau_arithprog_hg(k,p)
+     (from this by tau_arithprog_hg(k,n+p) <= tau_arithprog_hg(k,p)+n
+     we obtain the upper bound n). </li>
+     <li> It seems that the last bound is hardly useful; so all what we get
+     is the trivial bound that advancing one steps costs at most one. </li>
+     <li> For natural numbers n >= 0 and k >= 2 the following functions
+     checks whether the base-k representation of n contains the digit k-1:
+     \verbatim
 maxdigitp(k,n) := 
  some_s(lambda([d],is(d=k-1)),int2polyadic(n,k))$
-       \endverbatim
-       while
-       \verbatim
+     \endverbatim
+     while
+     \verbatim
 maxdigits(k,n) := subset(setmn(0,n-1),lambda([x],maxdigitp(k,x)))$
-       \endverbatim
-       is the set of all such numbers from 0 to n-1. The basic fact here is,
-       that for prime numbers k, maxdigits(k,n) is a transversal of
-       arithprog_hg(k,n), but where the vertex set is {0,...,n-1}
-       (instead of {1,...,n}, as it is). </li>
-       <li> Thus
-       \verbatim
+     \endverbatim
+     is the set of all such numbers from 0 to n-1. The basic fact here is,
+     that for prime numbers k, maxdigits(k,n) is a transversal of
+     arithprog_hg(k,n), but where the vertex set is {0,...,n-1}
+     (instead of {1,...,n}, as it is). </li>
+     <li> Thus
+     \verbatim
 transdig_ap(k,n) := map(lambda([x],x+1), maxdigits(k,n))$
-       \endverbatim
-       yields a transversal of arithprog_hg(k,n) for n >= 0. </li>
-       <li> A counter-example for non-prime k is given by
-       transdig_ap(10,19) = {10}, which is not a transversal of
-       arithprog_hg(10,19), since the hyperedge {1,3,5,7,9,11,13,15,17,19}
-       is missed:
-       \verbatim
+     \endverbatim
+     yields a transversal of arithprog_hg(k,n) for n >= 0. </li>
+     <li> A counter-example for non-prime k is given by
+     transdig_ap(10,19) = {10}, which is not a transversal of
+     arithprog_hg(10,19), since the hyperedge {1,3,5,7,9,11,13,15,17,19}
+     is missed:
+     \verbatim
 transversal_p(transdig_ap(10,19),arithprog_hg(10,19));
  false
-       \endverbatim
-       </li>
-       <li> In other words, tau_arithprog_hg(k,n) <= ubmd(k,n))
-       for prime numbers k, where
-       \verbatim
+     \endverbatim
+     </li>
+     <li> In other words, tau_arithprog_hg(k,n) <= ubmd(k,n))
+     for prime numbers k, where
+     \verbatim
 ubmd(k,n) := length(transdig_ap(k,n))$
-       \endverbatim
-       </li>
-       <li> For k=3 and n <= 60 this upper bound coincides with the above
-       lower bound lb exactly for [1,2,4,5,6,11,13,14,15,16,40,41,42]. </li>
-       <li> For natural numbers k and a we have
-       ubmd(k,((k-2)*k^a+1)/(k-1)) = ((k-2)*k^a+1)/(k-1) - (k-1)^a. </li>
-       <li> Using
-       \verbatim
+     \endverbatim
+     </li>
+     <li> For k=3 and n <= 60 this upper bound coincides with the above
+     lower bound lb exactly for [1,2,4,5,6,11,13,14,15,16,40,41,42]. </li>
+     <li> For natural numbers k and a we have
+     ubmd(k,((k-2)*k^a+1)/(k-1)) = ((k-2)*k^a+1)/(k-1) - (k-1)^a. </li>
+     <li> Using
+     \verbatim
 ubmda(k,a) := block([n : ((k-2)*k^a+1)/(k-1)], [n, n-(k-1)^a])$
-       \endverbatim
-       for [n,b] = ubmda(k,a) we have tau_arithprog_hg(k,n) <= b. </li>
-       <li> For k=3 this yields tau_arithprog_hg(3,(3^a+1)/2) <= 
-       (3^a+1)/2 - 2^a.
-       \verbatim
+     \endverbatim
+     for [n,b] = ubmda(k,a) we have tau_arithprog_hg(k,n) <= b. </li>
+     <li> For k=3 this yields tau_arithprog_hg(3,(3^a+1)/2) <= 
+     (3^a+1)/2 - 2^a.
+     \verbatim
 map(lambda([a],ubmda(3,a)),create_list(i,i,1,6));
  [[2,0],[5,1],[14,6],[41,25],[122,90],[365,301]]
-       \endverbatim
-       (one sees that for a <= 4 the upper bound is sharp, and coincides with
-       the lower bound). </li>
-       <li> It should be possible to at least compute tau_arithprog_hg(3,122),
-       so that we can determine whether this bound is still sharp (in general
-       it is not). </li>
-       <li> But perhaps we should use the general function ubmd(k,n), and not
-       just the computation of special values via ubmda(k,a). </li>
-      </ul>
-     </li>
-     <li> For our stored values, we should always have that they are best
-     relatively to the current knowledge (so that no further calculations
-     are needed for them). </li>
-    </ol>
+     \endverbatim
+     (one sees that for a <= 4 the upper bound is sharp, and coincides with
+     the lower bound). </li>
+     <li> It should be possible to at least compute tau_arithprog_hg(3,122),
+     so that we can determine whether this bound is still sharp (in general
+     it is not). </li>
+     <li> But perhaps we should use the general function ubmd(k,n), and not
+     just the computation of special values via ubmda(k,a). </li>
+    </ul>
    </li>
+   <li> For our stored values, we should always have that they are best
+   relatively to the current knowledge (so that no further calculations
+   are needed for them). </li>
   </ul>
 
 */
