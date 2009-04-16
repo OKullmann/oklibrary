20a02881d95e4ad183219db8a8a81f421cf2e18a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-04-16 13:50:13 +0100

    Plans on computing Schur-numbers.

--- Experimentation/Investigations/RamseyTheory/plans/general.hpp
+++ Experimentation/Investigations/RamseyTheory/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 14.11.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -11,6 +11,19 @@ License, or any later version. */
   
   
   \todo Create milestones.
+
+
+  \todo Schur numbers
+  <ul>
+   <li> For a natural number r >= 1 the Schur-number schur(r) is the smallest
+   n such that for every partition {1,...,n} at least one part contains a
+   triple (x,y,z) with x+y=z. </li>
+   <li> Apparently only schur(1)=2, schur(2)=5, schur(3)=14 and schur(4)=45
+   are known. </li>
+   <li> While schur(5)=160 is conjectured (research problem 8.1 in
+   [Landman, Robertson, 2003]). This might be feasible, since the clause-set
+   consists only of binary and ternary clauses. </li>
+  </ul>
 
 */
 
