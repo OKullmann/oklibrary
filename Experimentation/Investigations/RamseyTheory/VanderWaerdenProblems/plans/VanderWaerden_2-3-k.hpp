e94adeb04b519ae91072060edc98d49f0a1f75e8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-04-21 21:15:02 +0100

    On OKsolver_2002 for vanderwaerden_2(3,k)-problems.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
@@ -13,14 +13,11 @@ License, or any later version. */
   \todo Literature overview
   <ul>
    <li> A conjecture seems to be that vdw_2(3,k) <= k^2 for k >= 3. </li>
-   <li> The known values with k=1, ..., 16:
+   <li> The known values with k=1, ..., 16 are available via
+   vanderwaerden3k(k):
    \verbatim
-k vdw_2(3,k)
-1 3
-2 6
-3 9
-
-16 238
+create_list(vanderwaerden3k(k),k,1,17);
+ [3,6,9,18,22,32,46,58,77,97,114,135,160,186,218,238,unknown]
    \endverbatim
    </li>
   </ul>
@@ -28,6 +25,27 @@ k vdw_2(3,k)
 
   \todo Performance of OKsolver-2002
   <ul>
+   <li> k=4, n=17,18: 7 resp. 9 nodes. </li>
+   <li> k=5, n=21,22: 6 resp. 19 nodes. </li>
+   <li> k=6, n=31,32: 24 resp. 51 nodes. </li>
+   <li> k=7, n=45,46: 11 resp. 139 nodes. </li>
+   <li> k=8, n=57,58: 109 resp. 479 nodes. </li>
+   <li> k=9, n=76,77: 702 resp. 1591 nodes. </li>
+   <li> k=10, n=96,97: 129 resp. 10545 nodes. </li>
+   <li> k=11, n=113,114:
+    <ol>
+     <li> 37374 resp. 41563 nodes. </li>
+     <li> With minisat2-preprocessor: identical results (so apparently
+     the preprocessor does nothing). </li>
+    </ol>
+   </li>
+   <li> k=12, n=134, 135: 283568 resp. 281381 nodes (~ 7 minutes). </li>
+   <li> k=13, n=159,160:
+   \verbatim
+s SATISFIABLE
+c sat_status=1 initial_maximal_clause_length=13 initial_number_of_variables=159 initial_number_of_clauses=7216 initial_number_of_literal_occurrences=31398 running_time(s)=4187.7 number_of_nodes=1590137 number_of_single_nodes=0 number_of_quasi_single_nodes=0 number_of_2-reductions=13477703 number_of_pure_literals=455 number_of_autarkies=0 number_of_missed_single_nodes=1 max_tree_depth=43 number_of_table_enlargements=0 reduced_maximal_clause_length=0 reduced_number_of_variables=0 reduced_number_of_clauses=0 reduced_number_of_literal_occurrences=0 number_of_1-autarkies=12836 number_of_initial_unit-eliminations=0 number_of_new_2-clauses=0 maximal_number_of_added_2-clauses=0 initial_number_of_2-clauses=0 file_name=VanDerWaerden_2-3-13_159.cnf
+
+   </li>
    <li> k=16, n=238: Running it with monitoring level 20 appears hopeless.
    And using the m2pp-variant doesn't make a difference, since the
    preprocessor has no effect. </li>
@@ -35,6 +53,8 @@ k vdw_2(3,k)
 
 
   \todo Performance of march_pl
+  <ul>
+   <li> 
 
 
   \todo Performance of satz215
