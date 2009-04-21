e781bfad7b948f183c8546ab619ec210c6b303ad
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-04-21 23:26:37 +0100

    More on the behaviour of satz215-m2pp on vdW-transversals.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/Transversals/plans/UsingSAT.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/Transversals/plans/UsingSAT.hpp
@@ -406,6 +406,15 @@ awk -f OKsystem/OKlib/Experimentation/Investigations/RamseyTheory/VanderWaerdenP
 46 905109
 47 1907973
 48 1349065
+49 5374740
+50 3643376
+52 6347608
+53 8677472
+55 13042785
+56 17220536
+57 35031247
+59 57243648
+60 112143984
      \endverbatim
      and for satisfiable instances
      \verbatim
@@ -425,8 +434,14 @@ awk -f OKsystem/OKlib/Experimentation/Investigations/RamseyTheory/VanderWaerdenP
 36 93047
 40 33605
 41 33146
+51 1319128
+54 5174993
+58 13127223
      \endverbatim
      It seems to help.
+     </li>
+     <li> Which of the 3 solves OKsolver_2002-m2pp, satz215-m2pp or march_pl
+     is best needs to be determined, but roughly they show similar abilities.
      </li>
     </ol>
    </li>
