863b7defad1be1b60ebec10def0b30d4a1ca9c8c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-04 11:25:04 +0100

    Updates on plans for finite model theory.
    The direct motication is the implementation of the (absolute)
    NP-completeness of SAT.

--- Logic/FiniteModelTheory/plans/general.hpp
+++ Logic/FiniteModelTheory/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 9.7.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -17,6 +17,7 @@ License, or any later version. */
   <ul>
    <li> Get an overview on interesting issues and their potential relations
    to the OKlibrary. </li>
+   <li> See "Finite model theory" in ComputerAlgebra/plans/general.hpp. </li>
   </ul>
 
 */
