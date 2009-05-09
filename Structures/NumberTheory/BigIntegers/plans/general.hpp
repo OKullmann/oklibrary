ee150ded9607e699eb01b7760f7053dd83d71eea
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-09 14:25:44 +0100

    Editorial.

--- Structures/NumberTheory/BigIntegers/plans/general.hpp
+++ Structures/NumberTheory/BigIntegers/plans/general.hpp
@@ -1,12 +1,12 @@
 // Oliver Kullmann, 19.10.2005 (Swansea)
-/* Copyright 2005 - 2007 Oliver Kullmann
+/* Copyright 2005 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file BigIntegers/plans/general.hpp
+  \file Structures/NumberTheory/BigIntegers/plans/general.hpp
   \brief Plans for the module with big integer tools
 
 
@@ -15,7 +15,8 @@ License, or any later version. */
 
   \todo Boost
   <ul>
-   <li> Regarding the concepts see BigIntegers/concepts/plans/general.hpp. </li>
+   <li> Regarding the concepts see BigIntegers/concepts/plans/general.hpp.
+   </li>
    <li> And what about implementations? </li>
   </ul>
 
