2170c2bbd6660381b821c44535593bb0388c2a0f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-11 16:04:16 +0100

    Added information and links regarding what is known on Hales-Jewett numbers.

--- Experimentation/Investigations/RamseyTheory/plans/general.hpp
+++ Experimentation/Investigations/RamseyTheory/plans/general.hpp
@@ -53,6 +53,97 @@ License, or any later version. */
    <li> It is known that halesjewett_r(2) = r. </li>
    <li> Likely we should create a new module
    ComputerAlgebra/RamseyTheory/Lisp/HalesJewett. </li>
+   <li> There is a project about Hales-Jewett numbers:
+   http://michaelnielsen.org/polymath1/index.php
+    <ol>
+     <li> So there is actually considerable interest in computing Hales-Jewett
+     numbers! </li>
+     <li> We have http://www.math.ucsd.edu/~etressle/hj32.pdf, where
+     halesjewett_2(3) = 4 is shown, directly with a proof by case distinctions,
+     and mentioning also an algorithm. </li>
+     <li> At
+     http://michaelnielsen.org/polymath1/index.php?title=Hales-Jewett_theorem
+     we find likely the most up-to-date bounds. </li>
+     <li> halesjewett_r(3):
+      <ol>
+       <li> r=3: > 13 </li>
+       <li> r=4: > 37 </li>
+       <li> r=5: > 84 </li>
+       <li> r=6: > 103 </li>
+       <li> These case use vanderwaerden_r(3). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(4):
+      <ol>
+       <li> r=2: > 11 </li>
+       <li> r=3: > 97 </li>
+       <li> r=4: > 349 </li>
+       <li> r=5: > 751 </li>
+       <li> r=6: > 3259 </li>
+       <li> These case use vanderwaerden_r(4). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(5):
+      <ol>
+       <li> r=2: > 59 </li>
+       <li> r=3: > 302 </li>
+       <li> r=4: > 2609 </li>
+       <li> r=5: > 6011 </li>
+       <li> r=6: > 14173 </li>
+       <li> These case use vanderwaerden_r(5). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(6):
+      <ol>
+       <li> r=2: > 226 </li>
+       <li> r=3: > 1777 </li>
+       <li> r=4: > 18061 </li>
+       <li> r=5: > 49391 </li>
+       <li> r=6: > 120097 </li>
+       <li> These case use vanderwaerden_r(6). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(7):
+      <ol>
+       <li> r=2: > 617 </li>
+       <li> r=3: > 7309 </li>
+       <li> r=4: > 64661 </li>
+       <li> These case use vanderwaerden_r(7). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(8):
+      <ol>
+       <li> r=2: > 1069 </li>
+       <li> r=3: > 34057 </li>
+       <li> These case use vanderwaerden_r(8). </li>
+      </ol>
+     </li>
+     <li> halesjewett_r(9):
+      <ol>
+       <li> r=2: > 3389 </li>
+       <li> These case use vanderwaerden_r(9). </li>
+      </ol>
+     </li>
+     <li> Also the density considerations are of interest, since the
+     hypergraphs sequences have the density property, i.e., the quotients
+     alpha_halesjewett_hg(k,N)) / k^N -> 0 for fixed k. </li>
+     <li> For k=3 these independence numbers are studied at
+     http://michaelnielsen.org/polymath1/index.php?title=Upper_and_lower_bounds
+     where c_n = alpha_halesjewett_hg(3,n)) is used. </li>
+     <li> Precise values are known for 0 <= n <= 6: 1,2,6,18,52,150,450. </li>
+     <li> A simple greedy algorithm is given there, but his works only up
+     to n <= 3. </li>
+     <li> Apparently their main algorithm is a genetic one:
+     http://michaelnielsen.org/polymath1/index.php?title=Genetic_algorithm
+     </li>
+     <li> We must really work on hypergraph transversal algorithms! </li>
+     <li> The general case is considered at
+     http://michaelnielsen.org/polymath1/index.php?title=Higher-dimensional_DHJ_numbers
+     where the notation c_{n,k} = alpha_halesjewett_hg(k,n)) is used. </li>
+     <li> A simple upper bound is c_{n,k} <= (k-1)*k^{n-1}; parameter pairs
+     where this upper bound is attained are called "saturated". </li>
+    </ol>
+   </li>
   </ul>
 
 */
