9f1ab3d9bfd15cf15065c04e88ea6ec7b6b1e7bd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-17 21:31:47 +0100

    Small editorial corrections.

--- Structures/Cryptology/HashMD5lib.cpp
+++ Structures/Cryptology/HashMD5lib.cpp
@@ -1,11 +1,11 @@
 // Oliver Kullmann, 1.10.2002 (Swansea)
-/* Copyright 2002 - 2007 Oliver Kullmann
+/* Copyright 2002 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
-// Name: HashMD5.hpp
+// Name: HashMD5lib.cpp
 // Autor: Oliver Kullmann (O.Kullmann@Swansea.ac.uk)
 // Version: 0.9
 // Datum: 1.10.2002
