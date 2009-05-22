1dab5971117d13a68c81ad618ebc0826d0df8f8b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-22 19:20:56 +0100

    Removed infinite loops.

--- Satisfiability/FiniteFunctions/SATParser.hpp
+++ Satisfiability/FiniteFunctions/SATParser.hpp
@@ -26,29 +26,29 @@ namespace OKlib {
         int c = 0; // Character reading
         int rawLiteral = -1;
         std::vector<std::vector<int> > clauseSet;
-        while ( (c = inputFile.peek()) != EOF ) {
+        while (inputFile and (c = inputFile.peek()) != EOF) {
           switch(c) {
           case 'c' :
             inputFile.get();
             // Skip
-            while((c = inputFile.get()) != '\n');
+            while(inputFile and (c = inputFile.get()) != '\n');
             break;
           case 'p' :
             inputFile.get();
             // Skip
-            while((c = inputFile.get()) != '\n');
+            while(inputFile and (c = inputFile.get()) != '\n');
             break;
           default :
             std::vector<int> clause;
             inputFile >> rawLiteral;
-            while (rawLiteral) {
+            while (inputFile and rawLiteral) {
               clause.push_back(rawLiteral);
               inputFile >> rawLiteral;
             }
             clauseSet.push_back(clause);
             // Jump to newline
             c = inputFile.get();
-            while((c != '\n') && (c != EOF)) {
+            while(inputFile and (c != '\n') && (c != EOF)) {
               c = inputFile.get();
             }
             break;
