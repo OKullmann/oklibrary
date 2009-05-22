d529e157ee495635d319f393d534dcc12de334cf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-22 01:00:24 +0100

    Provided the licence.

--- Satisfiability/FiniteFunctions/QuineMcCluskey.cpp
+++ Satisfiability/FiniteFunctions/QuineMcCluskey.cpp
@@ -1,3 +1,10 @@
+// Oliver Kullmann, 22.5.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
 #include <OKlib/Satisfiability/FiniteFunctions/SATParser.hpp>
 #include <OKlib/Satisfiability/FiniteFunctions/QuineMcCluskey.hpp>
 #include<fstream>
