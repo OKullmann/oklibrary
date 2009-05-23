cbf4454760189215c35c64ce8bac8f2a63bd06d4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-23 21:24:18 +0100

    greentao_3(3,4,4) > 1000.

---
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 23.5.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
+  \brief On investigations into Green-Tao numbers greentao_3(3,4,k)
+  
+  
+  
+  \todo greentao_3(3,4,4) > 1000
+  <ul>
+   <li> Creating problems via output_greentao_stdname([3,4,4],n). </li>
+   <li> n=500 trivially satisfiable (adaptnovelty+). </li>
+   <li> n=600 trivially satisfiable. </li>
+   <li> n=700 trivially satisfiable. </li>
+   <li> n=900 trivially satisfiable. </li>
+   <li> n=1000 trivially satisfiable. </li>
+  </ul>
+  
+*/
+
