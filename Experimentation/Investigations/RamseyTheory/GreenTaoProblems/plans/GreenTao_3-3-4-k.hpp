d1700f3f715b2c7e3821806b0dad26d73fde26c9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-24 19:24:21 +0100

    greentao_3(3,4,4) > 1500.

--- Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
@@ -9,9 +9,8 @@ License, or any later version. */
   \file Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
   \brief On investigations into Green-Tao numbers greentao_3(3,4,k)
   
-  
-  
-  \todo greentao_3(3,4,4) > 1000
+
+  \todo greentao_3(3,4,4) > 1500
   <ul>
    <li> Creating problems via output_greentao_stdname([3,4,4],n). </li>
    <li> n=500 trivially satisfiable (adaptnovelty+). </li>
@@ -19,6 +18,16 @@ License, or any later version. */
    <li> n=700 trivially satisfiable. </li>
    <li> n=900 trivially satisfiable. </li>
    <li> n=1000 trivially satisfiable. </li>
+   <li> n=1100 still easy to solve (cutoff=100*10^3). </li>
+   <li> n=1200 still easy to solve (cutoff=10^6). </li>
+   <li> n=1300 rather easy to solve (cutoff=10^6). </li>
+   <li> n=1400: one solution in 10 runs with cutoff=10^6. </li>
+   <li> n=1500: only min=3 for 10 runs with cutoff=10*10^6; cutoff=100*10^6
+   found a solution in the third run (osteps=39412430, seed=1928236138). </li>
+   <li> n=1512 </li>
+   <li> n=1525: 10 runs with cutoff=100*10^6 yield only min=2. </li>
+   <li> n=1550 looks (at first sight) unsatisfiable, 10 runs with
+   cutoff=100*10^6 yield min=3 (twice; so cutoff=10^9 would be needed). </li>
   </ul>
   
 */
