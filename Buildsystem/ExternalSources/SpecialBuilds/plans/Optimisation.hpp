e7c6e980de7c7dd192162bf06036297c93035767
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-26 22:39:43 +0100

    Plans on installing LP and ILP software.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/Optimisation.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/Optimisation.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 24.7.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -14,6 +14,18 @@ License, or any later version. */
   <ul>
    <li> COmputational INfrastructure for Operations Research(COIN-OR)
    http://www.coin-or.org/index.html </li>
+  </ul>
+
+
+  \todo Linear and integer programming
+  <ul>
+   <li> lp_solver
+    <ol>
+     <li> ftp://ftp.es.ele.tue.nl/pub/lp_solve </li>
+     <li> The R module lpSolve yields an interface; but we should also
+     have the original software (with its documentation). </li>
+    </ol>
+   </li>
   </ul>
 
 
