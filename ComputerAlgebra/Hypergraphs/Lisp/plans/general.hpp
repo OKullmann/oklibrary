34616c1e9b3138086c2ca1bed3024b9002504350
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-05-27 09:00:42 +0100

    Some updates w.r.t. independent sets in hypergraphs.

--- ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
+++ ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 30.11.2007 (Swansea)
-/* Copyright 2007, 2008 Oliver Kullmann
+/* Copyright 2007, 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,6 +8,15 @@ License, or any later version. */
 /*!
   \file ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
   \brief Plans for Maxima-components for hypergraphs
+
+
+  \todo Place of IndependentSets.mac
+  <ul>
+   <li> The question is whether this belongs to module Transversals or not.
+   </li>
+   <li> Yet it is some kind of interface, which could stay at this top level.
+   </li>
+  </ul>
 
 
   \todo Create milestones
