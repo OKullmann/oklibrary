d2ef0e6b4e0374a78acd82ac97a263cefed89d0c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-06-03 22:36:16 +0100

    Started listing all current experiments.
    MG: please enter your currently running experiments.
    OK: enter experiments running on cs-wsok and cs-oksvr.

---
+++ Experimentation/Investigations/plans/CurrentExperiments.hpp
@@ -0,0 +1,34 @@
+// Oliver Kullmann, 3.6.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/plans/CurrentExperiments.hpp
+  \brief Overview on currently running experiments
+
+  Just to see what experiments are running at *this* time.
+
+
+  <h1> Ramsey theory </h1>
+
+  <h2> Green-Tao problems </h2>
+
+  <ul>
+   <li> RamseyTheory/GreenTaoProblems/plans/GreenTao_3-3-4-k.hpp
+    <ol>
+     <li> greentao_3(3,4,4) </li>
+     <li> greentao_3(3,4,5) </li>
+    </ol>
+   </li>
+   <li> RamseyTheory/GreenTaoProblems/plans/GreenTao_4-3-3-3-3.hpp
+    <ol>
+     <li> greentao_4(3,3,3,3) </li>
+    </ol>
+   </li>
+  </ul>
+  
+*/
+
