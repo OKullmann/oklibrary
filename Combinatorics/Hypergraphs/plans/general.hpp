7289bcad391717323e5c6c357e8d77e727aedf54
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-06-11 09:53:40 +0100

    Editorial update.

--- Combinatorics/Hypergraphs/plans/general.hpp
+++ Combinatorics/Hypergraphs/plans/general.hpp
@@ -1,12 +1,12 @@
 // Oliver Kullmann, 8.7.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file Hypergraphs/plans/general.hpp
+  \file Combinatorics/Hypergraphs/plans/general.hpp
   \brief Components for hypergraph algorithms
 
 
