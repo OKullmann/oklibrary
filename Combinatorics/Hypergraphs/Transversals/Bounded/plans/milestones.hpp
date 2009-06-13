d006fd3118b409df67c7c72e7e65f4e3848c521a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-06-13 11:02:49 +0100

    *** Initial version 0.0.4 (Hypergraphs/Transversals/Bounded) ***

---
+++ Combinatorics/Hypergraphs/Transversals/Bounded/plans/milestones.hpp
@@ -0,0 +1,30 @@
+// Oliver Kullmann, 13.6.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Hypergraphs/Transversals/Bounded/plans/milestones.hpp
+
+  \module_version Hypergraphs/Transversals/Bounded 0.0.4 (13.6.2009)
+
+
+  \par Version 0.0.5
+
+  \par
+   In Hypergraphs/Transversals/Bounded/plans/general.hpp the
+   following topics are handled:
+    - Trivial algorithm for input G, k
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.4 : 13.6.2009; a first implementation of the basic algorithm exists, and some further plans.
+
+*/
+
