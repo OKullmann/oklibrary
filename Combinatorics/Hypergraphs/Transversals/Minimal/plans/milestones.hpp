1ff592f030e5c2f835adc01a6b049f93363a302e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-06-13 11:03:22 +0100

    *** Initial version 0.0.2 (Hypergraphs/Transversals/Minimal) ***

---
+++ Combinatorics/Hypergraphs/Transversals/Minimal/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 13.6.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Hypergraphs/Transversals/Minimal/plans/milestones.hpp
+
+  \module_version Hypergraphs/Transversals/Minimal 0.0.2 (13.6.2009)
+
+
+  \par Version 0.0.3 :
+
+  \par
+   In Transversals/Minimal/plans/DirectTransversalEnumeration.hpp the
+   following topics are handled:
+    - Concepts
+    - %Implementation
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.0.2 : 13.6.2009; several plans.
+
+*/
+
