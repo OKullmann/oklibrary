3d969f545e06be6d9f7ef27e71f8319ab3ed5ef1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-06-14 00:00:22 +0100

    Moved components for vdW-hypergraphs (C++) to Combinatorics-part.

--- Applications/RamseyTheory/plans/Van_der_Waerden_hypergraph.hpp
+++ Applications/RamseyTheory/plans/Van_der_Waerden_hypergraph.hpp
@@ -28,35 +28,5 @@ License, or any later version. */
    </li>
   </ul>
 
-
-  \todo Reactivate test system.
-
-
-  \todo Update namespace.
-
-
-  \todo New test system
-  <ul>
-   <li> Transform the tests to the new testsystem. </li>
-  </ul>
-
-
-  \todo Update
-  <ul>
-   <li> Transfer the todo's from
-   Applications/RamseyTheory/Van_der_Waerden_hypergraph.hpp to here (thereby
-   updating them). </li>
-   <li> Likely this file should be renamed to VanderWaerdenHypergraphs.hpp.
-   </li>
-   <li> The generation of hypergraphs should be part of
-   Combinatorics/Hypergraphs/Generators. </li>
-   <li> So the content of this file should be moved to
-   Combinatorics/Hypergraphs/Generators/plans/VanderWaerdenProblems.hpp. </li>
-   <li> All the functionality like arithprog_primes_hg from the Maxima/Lisp
-   level (and all the other functions there in
-   ComputerAlgebra/Satisfiability/Lisp/Generators/VanderWaerdenProblems.mac)
-   should be available there. </li>
-  </ul>
-
 */
 
