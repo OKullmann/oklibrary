a29c1858a9ffc558cf77299da120d19febe3ed0d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-07-12 17:16:43 +0100

    Providing SAT generators for (basic) Hindman problems.

---
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/plans/HindmanProblems.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 12.7.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/Generators/plans/HindmanProblems.hpp
+  \brief Plans for Maxima-generators related to Hindman problems (and generalisations)
+
+
+  \todo Improve generator names
+  <ul>
+   <li> The current names are rather unsystematic. </li>
+   <li> Also the notion of "degeneration" needs to be replaced by "injective"
+   (appropriately). </li>
+  </ul>
+
+
+  \todo Write tests
+
+*/
+
