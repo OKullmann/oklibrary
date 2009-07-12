2053337c70d67edc2439ac981bf2dff97a491698
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-07-12 13:10:14 +0100

    Providing forced assignments of generalised_ucp in the order they were found.

--- ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/GeneralisedUCP.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/GeneralisedUCP.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 15.3.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,9 +10,16 @@ License, or any later version. */
   \brief Plans for Maxima-components regarding r_k-reductions
 
 
+  \todo Update names
+  <ul>
+   <li> All names need to be changed, reflecting their (main) argument
+   types. </li>
+  <ul>
+
+
   \todo More efficient implementations
   <ul>
-   <li> Instead of generalised_ucp(F,1) the more efficient ucp_cs_0
+   <li> Instead of generalised_ucp(F,1) the more efficient ucp_0_cs
    (or ucp_cs_1 etc.) should be used where possible. </li>
    <li> Except of the most basic implementations. </li>
    <li> Perhaps we should allow alternative implementations of r_1 as
