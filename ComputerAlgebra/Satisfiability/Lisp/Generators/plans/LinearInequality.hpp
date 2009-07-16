06f16d7125167b54d319096810d9c94b2bb980b0
Matthew Gwynne <360678@swan.ac.uk>
2009-07-16 14:17:46 +0100

    Added functions for generation of cardinality constraints in Maxima.

---
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/plans/LinearInequality.hpp
@@ -0,0 +1,28 @@
+// Matthew Gwynne, 16.7.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/Generators/plans/LinearInequality.hpp
+  \brief Plans for Maxima-generators (of SAT-problems) related to linear inequalities
+
+
+  \todo Docus
+
+
+  \todo Add adder circuit translation
+  <ul>
+   <li> Also provide the translation centred around the Tseitin translation of
+   the boolean adder circuits, for comparison. </li>
+   <li> See Satisfiability/Transformers/Generators/plans/LinInequal.hpp for 
+   a C++ implementation. </li>
+  </ul>
+
+
+  \todo Add statistics functions
+   
+*/
+
