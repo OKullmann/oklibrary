b28c7b88980b4845bb107c34a25b0303d9098acc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-07-19 00:32:52 +0100

    General discussions of "cardinality constraints".
    And rather complete discussion of the "direct representations".

--- ComputerAlgebra/Satisfiability/Lisp/Generators/plans/CardinalityConstraints.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/plans/CardinalityConstraints.hpp
@@ -22,6 +22,111 @@ License, or any later version. */
   </ul>
 
 
+  \todo Representing "cardinality constraints"
+  <ul>
+   <li> We need some direct representation of a condition that the (integer)
+   sum of some given boolean variables is =,>,>=,<,<= some bound B. </li>
+   <li> We need some general scheme for such special
+   constraint-representations. </li>
+   <li> Following our general philosophy of using lists, we could use a
+   list with first element a string which identifies the constraint;
+   in this case the string would be "cardinality". </li>
+   <li> The second element would be one of the five relations. </li>
+   <li> Then comes the set of relevant literals. </li>
+   <li> Finally the number B. </li>
+   <li> It would be good if for the relation we would not just use some string,
+   but the actually Maxima-presentation of the corresponding Maxima-operator:
+   Now how to refer to the Maxima-operator "<" ? Apparently this is done
+   by using strings. Now how from a string to get back the operator?
+   Apparently by for example subst("<=",r,r(3,4)), which yields 3 <= 4. </li>
+   <li> For example
+   \verbatim
+C : ["cardinality", "<", {v1,-v2,v3}, 6];
+Cs : subst(C[2], r, r(sum_l(listify(C[3])), C[4]));
+  v3-v2+v1 < 6
+Csa : at(Cs, [v1=1, v2=0, v3=1]);
+  2 < 6
+is(Csa);
+  true
+   \endverbatim
+   </li>
+   <li> The literals here however are boolean literals, so substitution
+   of values 0,1 seems inappropriate? On the one hand, boolean variables
+   are supposed to be "positive functions", and also we have variables like
+   "1". </li>
+   <li> Perhaps we make the distinction that if a cardinality constraint
+   occurs in a mixed problem, then the interpretation of the other part
+   (boolean or non-boolean CNF or DNF) is determinative, while free-standing
+   cardinality constraints are interpreted in the ordinary arithmetic sense
+   as above, based on further specifications of the domains of the variables
+   involved. </li>
+   <li> So in the context of a boolean clause-set one can use cardinality
+   constraints like ["cardinality", "=", {1,2,3}, 2], which is equivalent
+   to the CNF {{1,2},{1,3},{2,3},{-1,-2,-3}} (while
+   ["cardinality", "=", {1,-2,3}, 2] is equivalent to the CNF
+   {{1,-2},{1,3},{-2,3},{-1,2,-3}}). </li>
+   <li> How to call these constraints? "card" for "cardinality constraint"?
+   And then further specialised regarding boolean or non-boolean variables,
+   and CNF- or DNF-interpretation of variables? </li>
+  </ul>
+
+
+  \todo Direct realisations
+  <ul>
+   <li> For all types of clause-sets (boolean and non-boolean, CNF and DNF)
+   and all types of (in)equalities provide the direct (combinatorial)
+   realisations. </li>
+   <li> Let L be a set of boolean literals (of size m).
+    <ol>
+     <li> sum(L) < B has the CNF-realisation consisting of all B-subsets
+     of L, which are (then) complemented. </li>
+     <li> Example L = {1,-2,3} and B=2: {{-1,2},{-1,-3},{2,-3}} </li>
+     <li> The number of clauses is thus binom(m,B). </li>
+     <li> sum(L) <= B is equivalent to sum(L) < B+1. </li>
+     <li> sum(L) >= B has the DNF-realisation consisting of all B-subsets
+     of L. </li>
+     <li> Example L = {1,-2,3} and B=2: {{1,-2},{1,3},{-2,3}} </li>
+     <li> So the number of clauses is binom(m,B). </li>
+     <li> sum(L) > B is equivalent to sum(L) >= B+1. </li>
+     <li> sum(L) >= B is equivalent to sum(-L) <= m-B. </li>
+     <li> sum(L) <= B is equivalent to sum(-L) >= m-B. </li>
+     <li> sum(L) = B is equivalent to sum(L) <= B and sum(L) >= B. </li>
+     <li> So the CNF-representation of sum(L) = B is the union of the
+     CNF-representations of sum(L) < B+1 and sum(-L) < m-B+1. </li>
+     <li> That is, we have all (B+1)-subsets of L, complemented, and
+     all (m-B+1)-subsets of L. </li>
+     <li> The number of clauses is thus binom(m,B+1) + binom(m,m-B+1) =
+     binom(m,B+1) + binom(m,B-1). </li>
+     <li> Example L = {1,-2,3} and B=2: {{-1,2,-3},{1,-2},{1,3},{-2,3}}. </li>
+     <li> And the DNF-representation of sum(L) = B is the product of the
+     DNF-representations of sum(L) >= B and sum(-L) >= m-B. </li>
+     <li> That is, we have of B-subsets of L and all (m-B)-subsets of
+     -L. </li>
+     <li> Example L = {1,-2,3} and B=2:
+     {{1,-2},{1,3},{-2,3}} x {{-1},{2},{-3}} = {{1,-2,-3},{1,2,3},{-1,-2,3}}.
+     </li>
+     <li> One sees that the DNF-representation just lists all total satisfying
+     assignments (no resolution is possible between them). </li>
+     <li> The number of clauses is thus binom(m,B). </li>
+    </ol>
+   </li>
+   <li> These clause-sets coincide with the sets of all prime implicates resp.
+   prime implicants:
+    <ol>
+     <li> Only for the "="-forms resolutions are possible. </li>
+     <li> For the CNF-representations all clashing clauses overlap
+     in at least two literals, and thus all resolutions are blocked. </li>
+     <li> And above we already realised that no resolution is possible
+     regarding the DNF-realisations. </li>
+    </ol>
+    So without using additional variables these representations are optimal.
+   </li>
+   <li> And also using the DNF-representations seems always nearly to use
+   the same number of clauses, so that translations DNF -> CNF seem fruitless.
+   </li>
+  </ul>
+
+
   \todo Provide complete specifications
   <ul>
    <li> For cardinality_totalizer_cs, cardinality_comparator_cs and
