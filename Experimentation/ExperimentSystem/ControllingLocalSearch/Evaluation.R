6fcde23f339d3c3b7c2b09c8bef41d5def825559
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-07-21 20:18:20 +0100

    More documentation.

--- Experimentation/ExperimentSystem/ControllingLocalSearch/Evaluation.R
+++ Experimentation/ExperimentSystem/ControllingLocalSearch/Evaluation.R
@@ -22,4 +22,6 @@ read_ubcsat = function(filename, ...) {
 #     sat  min     osteps     msteps       seed
 # is assumed.
 # If the (given) line-counters are to be ignored, use the additional argument
-# "row.names=NULL".
+# "row.names=NULL". And if m leading lines are to be ignored, use
+# "skip=m".
+
