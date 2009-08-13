93fff3188d551edcba8dc9c4ebd36519d6ab5bfb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-08-13 11:10:39 +0100

    Started new module on pseudo-boolean constraints.

---
+++ ComputerAlgebra/Satisfiability/Lisp/PseudoBoolean/plans/general.hpp
@@ -0,0 +1,16 @@
+// Oliver Kullmann, 13.8.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/PseudoBoolean/plans/general.hpp
+  \brief Plans regarding pseudo-boolean constraints
+
+
+  \todo The notion of a "pseudo-boolean constraint"
+
+*/
+
