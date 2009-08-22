89deb9fc828aebab78f11a92f88e90b66b08fbfe
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-08-22 20:05:59 +0100

    Correction: Every plans-directory needs "general.hpp".
    And todo about reviewing the basic schemes employed in this submodule.

---
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/RamseyTheory/plans/general.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 22.8.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+   This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Satisfiability/Lisp/Generators/RamseyTheory/plans/general.hpp
+  \brief General plans on generators from Ramsey theory
+
+
+  \todo Systematising output functionality
+  <ul>
+   <li> See "Improve generator names" in
+   Satisfiability/Lisp/Generators/RamseyTheory/plans/HindmanProblems.hpp. </li>
+   <li> See "Create a systematic naming scheme" in
+   Satisfiability/Lisp/Generators/RamseyTheory/plans/RamseyProblems.hpp. </li>
+   <li> See "Standardisation" and "Systematic output and output-functions" in
+   /Satisfiability/Lisp/Generators/RamseyTheory/plans/VanderWaerdenProblems.hpp.
+   </li>
+  </ul>
+
+*/
+
