a09bd803d18011a8f74bc95785215ba81de34aef
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-09-03 15:16:15 +0100

    Adding labels to unlabelled rooted trees.

---
+++ ComputerAlgebra/Trees/Lisp/plans/Basics.hpp
@@ -0,0 +1,19 @@
+// Oliver Kullmann, 3.9.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Trees/Lisp/plans/Basics.hpp
+  \brief Plans regarding basic tree-functionality
+
+
+  \todo rt2lrt_il
+  <ul>
+   <li> A more intelligent and more efficient implementation is needed. </li>
+   <li> Currently the effort is quadratic, while it should be linear. </li>
+  </ul>
+
+*/
