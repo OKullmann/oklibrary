fe3981a2cb8fdc327f43354e2a129f8d5e9c7a11
Matthew Gwynne <360678@swan.ac.uk>
2009-09-22 14:19:17 +0100

    Added todos regarding moving picosat and minisat2 reading functionality to wrapper scripts.

---
+++ Experimentation/ExperimentSystem/SolverMonitoring/plans/Picosat.hpp
@@ -0,0 +1,22 @@
+// Matthew Gwynne, 22.9.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ExperimentSystem/SolverMonitoring/plans/Picosat.hpp
+  \brief General plans monitoring Picosat
+
+
+  \todo Move read_picosat_output functionality to wrapper script
+  <ul>
+   <li> The functionality currently presented by read_picosat_output
+   should go to a wrapper script, such as "picosat-okl", which
+   causes picosat to output it's results in a form readable by
+   read.table in R. </li>
+  </ul>
+
+*/
+
