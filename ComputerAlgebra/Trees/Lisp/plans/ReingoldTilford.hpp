c62f1945fe468cd3b84856b9a6767b3b9869faa7
RWang <502618@swan.ac.uk>
2009-10-01 23:12:37 +0100

    Added correct files endings. Using create_list instead of makelist. Modified the copyright information. Commented unfinished code to avoid syntax errors. ReingoldTilford.mac has been added to the include-system.

--- ComputerAlgebra/Trees/Lisp/plans/ReingoldTilford.hpp
+++ ComputerAlgebra/Trees/Lisp/plans/ReingoldTilford.hpp
@@ -1,5 +1,5 @@
 // Rui Wang, 28.09.2009 (Swansea)
-/* Copyright 2007, 2008, 2009 Oliver Kullmann
+/* Copyright 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -60,3 +60,5 @@ License, or any later version. */
    </li> 
   </ul> 
  
+*/
+
