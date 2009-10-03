96695742129f7e9655a6904c23e96e49b5407d9b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-10-03 23:32:58 +0100

    Removed superfluous include.

--- Programming/Sequences/testobjects/Comparisons.cpp
+++ Programming/Sequences/testobjects/Comparisons.cpp
@@ -1,11 +1,9 @@
 // Oliver Kullmann, 8.9.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
-
-#include <OKlib/TestSystem/RunTest_DesignStudy.hpp>
 
 #include <OKlib/TestSystem/RunTest_DesignStudy.hpp>
 
