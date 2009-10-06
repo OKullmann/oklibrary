34379ca4c8d9d43703f9497ac1bc3a9bf0557829
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-10-06 09:31:37 +0100

    More information and plans on handling Maxima memory-limits.

--- Buildsystem/MasterScript/SpecialProcessing/plans/Maxima_Init.hpp
+++ Buildsystem/MasterScript/SpecialProcessing/plans/Maxima_Init.hpp
@@ -33,8 +33,40 @@ frame_stack : 2^13;
    \endverbatim
    seems to work. </li>
    <li> So we should use variables "frame_stack, binding_stack, c_stack,
-   heap_size, list_stack". If they only work for Ecl, then we should add
+   heap_size, lisp_stack". If they only work for Ecl, then we should add
    the suffix "_ecl". </li>
+   <li> This yields
+   \verbatim
+frame_stack : 2^13;
+binding_stack : 2^16;
+c_stack : 2^20;
+heap_size : 2^30;
+lisp_stack : 2^17;
+:lisp (ext:set-limit 'ext:frame-stack $frame_stack)
+:lisp (ext:set-limit 'ext:binding-stack $binding_stak)
+:lisp (ext:set-limit 'ext:c-stack $c_stak)
+:lisp (ext:set-limit 'ext:heap-size $heap_size)
+:lisp (ext:set-limit 'ext:lisp-stack $lisp_stack)
+   \endverbatim
+   </li>
+   <li> Now we should define Maxima functions "set_frame_stack_ecl", etc.,
+   which perform these Lisp-instructions --- how to do this?
+    <ol>
+     <li> As in ComputerAlgebra/DataStructures/Lisp/HashMaps.lisp, we have to
+     create five Lisp-functions in a file MemoryManagement.lisp, encapsulating
+     the above instructions. This can then be included by oklib_include. </li>
+     <li> The Maxima functions "set_frame_stack_ecl" etc. are simple wrappers
+     for these Lisp-functions (so that also from the command-line we can use
+     these functions without using ":lisp"). </li>
+     <li> We simply call these Lisp functions "set_frame_stack_ecl_" etc. </li>
+     <li> But these Lisp functions likely can't use easily the Maxima-variables
+     like "frame_stack", so we need to provide these variables as
+     Lisp-variables, which are then read and write from Maxima. </li>
+     <li> Perhaps then we don't use global variables, but just setter and
+     getter functions? </li>
+     <li> 
+    </ol>
+   </li>
    <li> One also has the command-line parameters "--heap-size, --lisp-stack,
    --frame-stack, --c-stack", but it seems they are not useful here. </li>
    <li> MG mentioned that increasing certain of the above values has a
