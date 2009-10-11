3319864713ab550ab286d7657339bb30332c4c3f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-10-11 17:05:55 +0100

    More data (adaptnovelty+ and samd).

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
@@ -323,6 +323,8 @@ satz215 VanDerWaerden_2-3-16_238.cnf 990144.910 600383827 304409259 99369777769 
    <li> But with vanderwaerden_2(3,17) (below) we see that adaptnovelty+
    is better; perhaps, as with vdw_2(4,k), getting stronger with higher
    cutoff. </li>
+   <li> However, for vanderwaerden_2(3,18) samd again seems clearly better
+   --- this needs to be investigated more thoroughly. </li>
   </ul>
 
 
@@ -452,7 +454,7 @@ BestSolution_Max = 2.000000
   </ul>
 
 
-  \todo vanderwaerden_2(3,18) > 311
+  \todo vanderwaerden_2(3,18) >= 312
   <ul>
    <li> The prediction (see above) is n=314. </li>
    <li> n=300 found satisfiable with cutoff=10^6. </li>
@@ -513,6 +515,50 @@ BestSolution_Max = 2.000000
      (the histogram of osteps is nearly strictly decreasing, only at about
      3*10^6 we seem to have a local minimum, followed by a slight increase,
      peaking at 3.5*10^6). </li>
+     <li> cutoff=8*10^6:
+     \verbatim
+> ubcsat-okl -alg adaptnovelty+ -runs 2000 -cutoff 8000000 -i VanDerWaerden_2-3-18_312.cnf -solve | tee VanDerWaerden_2-3-18_312.cnf_AUS2
+Clauses = 26889
+Variables = 312
+TotalLiterals = 121302
+FlipsPerSecond = 56414
+BestStep_Mean = 1106252.638500
+Steps_Mean = 8000000.000000
+Steps_Max = 8000000.000000
+PercentSuccess = 0.00
+BestSolution_Mean = 1.000500
+BestSolution_Median = 1.000000
+BestSolution_Min = 1.000000
+BestSolution_Max = 2.000000
+   1    2
+1999    1
+2000
+     \endverbatim
+     (now the histogram is strictly decreasing).
+     </li>
+     <li> cutoff=10^6 (to compare samd and adaptnovelty+):
+     \verbatim
+> ubcsat-okl -alg samd -runs 8000 -cutoff 1000000 -i VanDerWaerden_2-3-18_312.cnf -solve | tee VanDerWaerden_2-3-18_312.cnf_AUS3
+Clauses = 26889
+Variables = 312
+TotalLiterals = 121302
+FlipsPerSecond = 108486
+BestStep_Mean = 296836.281000
+Steps_Mean = 1000000.000000
+Steps_Max = 1000000.000000
+PercentSuccess = 0.00
+BestSolution_Mean = 1.140750
+BestSolution_Median = 1.000000
+BestSolution_Min = 1.000000
+BestSolution_Max = 4.000000
+   1    2    3    4
+6885 1105    9    1
+8000
+
+> ubcsat-okl -alg adaptnovelty+ -runs 8000 -cutoff 1000000 -i VanDerWaerden_2-3-18_312.cnf -solve | tee VanDerWaerden_2-3-18_312.cnf_AUS4
+
+     \endverbatim
+     So actually samd seems to clearly outperform adaptnovelty+. </li>
     </ol>
    </li>
   </ul>
