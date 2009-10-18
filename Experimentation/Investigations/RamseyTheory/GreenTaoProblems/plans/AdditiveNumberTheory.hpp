4fafd0fc540df2ea0ac5342d2aefb468c43e88fe
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-10-19 00:39:29 +0100

    Fitting the number of arithmetic progressions in the primes.
    For k = 3,4,5.

--- Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/AdditiveNumberTheory.hpp
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/AdditiveNumberTheory.hpp
@@ -40,6 +40,155 @@ plot2d([discrete,create_list(i,i,1,1000),create_list(n_arithprog_primes_nc[3,n],
 plot2d([discrete, sizes_strata_indmon_ohg(arithprog_primes_ohg(3,1000))]);
 plot2d([discrete, sizes_cstrata_indmon_ohg(arithprog_primes_ohg(3,1000))]);
    \endverbatim
+   </li>
+   <li> Using Applications/RamseyTheory/CountProgressions_GreenTao.cpp
+   and linear regression in R:
+   \verbatim
+RamseyTheory> ~/csoliver/SAT-Algorithmen/OKplatform/system_directories/bin/CountProgressions_GreenTao-O3-DNDEBUG 3 10000 > GT_3_10000
+
+>  E = read.table("OKsystem/OKlib/Applications/RamseyTheory/GT_3_10000", colClasses=c("integer","integer"),header=TRUE)
+x = log(E$n)
+y = log(E$nhyp)
+L = lm(y ~ x)
+summary(L)
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)
+(Intercept) -2.3098350  0.0010763   -2146   <2e-16 ***
+x            1.8719533  0.0001301   14389   <2e-16 ***
+Residual standard error: 0.01281 on 9993 degrees of freedom
+Multiple R-squared:     1,      Adjusted R-squared:     1
+F-statistic: 2.07e+08 on 1 and 9993 DF,  p-value: < 2.2e-16
+
+C = coefficients(L)
+f = function(x) {exp(C[1]) * x^(C[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f(E$n))
+exp(C[1])
+  0.09927763
+C[2]
+  1.871953
+   \endverbatim
+   Thus f(n) = 0.09927763 * n^1.871953 is a good approximation of the number
+   of 3-progressions in the first n prime numbers (for n <= 10000). </li>
+   <li> Using non-linear regression:
+   \verbatim
+X = E$n
+Y = E$nhyp
+NL = nls(Y ~ a*(X^b), start = c(a=0.09927763, b=1.871953))
+summary(NL)
+  Parameters:
+   Estimate Std. Error t value Pr(>|t|)
+a 8.331e-02  1.883e-05    4424   <2e-16 ***
+b 1.892e+00  2.511e-05   75374   <2e-16 ***
+Residual standard error: 741.2 on 9993 degrees of freedom
+Number of iterations to convergence: 4
+Achieved convergence tolerance: 4.871e-08
+C2 = coefficients(NL)
+C2
+         a          b
+0.08330711 1.89230541
+f2 = function(x) {C2[1] * x^(C2[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f2(E$n))
+   \endverbatim
+   So f2(n) = 0.08330711 * n^1.89230541 seems to be an even better model. </li>
+   <li> For N=100000 we need a C++ program which doesn't store the
+   progressions. For N=30000 by non-linear regression we obtain
+   f3(n) = 0.07487019 * n^1.90377210. We should consider larger n. </li>
+   <li> Now for k=4:
+   \verbatim
+RamseyTheory> ~/csoliver/SAT-Algorithmen/OKplatform/system_directories/bin/CountProgressions_GreenTao-O3-DNDEBUG 4 20000 > GT_4_20000
+
+>  E = read.table("OKsystem/OKlib/Applications/RamseyTheory/GT_4_20000", colClasses=c("integer","integer"),header=TRUE)
+x = log(E$n)
+y = log(E$nhyp)
+L = lm(y ~ x)
+summary(L)
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)
+(Intercept) -3.3482256  0.0010711   -3126   <2e-16 ***
+x            1.7696832  0.0001195   14812   <2e-16 ***
+Residual standard error: 0.01645 on 19973 degrees of freedom
+Multiple R-squared: 0.9999,     Adjusted R-squared: 0.9999
+F-statistic: 2.194e+08 on 1 and 19973 DF,  p-value: < 2.2e-16
+
+C = coefficients(L)
+f = function(x) {exp(C[1]) * x^(C[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f(E$n))
+exp(C[1])
+  0.03514666
+C[2]
+  1.769683
+
+X = E$n
+Y = E$nhyp
+NL = nls(Y ~ a*(X^b), start = c(a=0.03514666, b=1.769683))
+summary(NL)
+Parameters:
+   Estimate Std. Error t value Pr(>|t|)
+a 2.663e-02  7.532e-06    3535   <2e-16 ***
+b 1.799e+00  2.920e-05   61627   <2e-16 ***
+Residual standard error: 612 on 19973 degrees of freedom
+Number of iterations to convergence: 5
+Achieved convergence tolerance: 1.020e-07
+C2 = coefficients(NL)
+C2
+         a          b
+0.02662577 1.79946138
+f2 = function(x) {C2[1] * x^(C2[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f2(E$n))
+   \endverbatim
+   So f_4(n) = 0.02662577 * x^1.79946138 is a good model (for n <= 20000).
+   </li>
+   <li> Now for k=5:
+   \verbatim
+RamseyTheory> ~/csoliver/SAT-Algorithmen/OKplatform/system_directories/bin/CountProgressions_GreenTao-O3-DNDEBUG 5 40000 > GT_5_40000
+
+>  E = read.table("OKsystem/OKlib/Applications/RamseyTheory/GT_5_40000", colClasses=c("integer","integer"),header=TRUE)
+x = log(E$n)
+y = log(E$nhyp)
+L = lm(y ~ x)
+summary(L)
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)
+(Intercept) -4.8968771  0.0010886   -4498   <2e-16 ***
+x            1.6911613  0.0001123   15058   <2e-16 ***
+Residual standard error: 0.01988 on 39344 degrees of freedom
+Multiple R-squared: 0.9998,     Adjusted R-squared: 0.9998
+F-statistic: 2.268e+08 on 1 and 39344 DF,  p-value: < 2.2e-16
+
+C = coefficients(L)
+f = function(x) {exp(C[1]) * x^(C[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f(E$n))
+exp(C[1])
+  0.007469874
+C[2]
+  1.691161
+
+X = E$n
+Y = E$nhyp
+NL = nls(Y ~ a*(X^b), start = c(a=0.007469874, b=1.691161))
+summary(NL)
+Parameters:
+   Estimate Std. Error t value Pr(>|t|)
+a 5.413e-03  1.881e-06    2878   <2e-16 ***
+b 1.723e+00  3.349e-05   51449   <2e-16 ***
+Residual standard error: 329.6 on 39344 degrees of freedom
+Number of iterations to convergence: 6
+Achieved convergence tolerance: 1.735e-08
+C2 = coefficients(NL)
+C2
+          a           b
+0.005413189 1.723282665
+f2 = function(x) {C2[1] * x^(C2[2])}
+plot(E$n,E$nhyp)
+lines(E$n,f2(E$n))
+   \endverbatim
+   So f_5(n) = 0.005413189 * x^1.723282665 is a good model (for n <= 40000).
+   </li>
    <li> And one can consider the densities
    ln_arithprog_primes_c(k,n) / create_list(i,i,1,n). </li>
    <li> Hard to believe that there is nothing in the literature / on the
