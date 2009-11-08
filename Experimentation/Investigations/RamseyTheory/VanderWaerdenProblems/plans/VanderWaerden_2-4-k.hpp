19708f57e601e5e6fc9b8d79834d3e94dba4eed4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-08 18:51:13 +0000

    vanderwaerden_2(4,10) > 326.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-4-k.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-4-k.hpp
@@ -1044,7 +1044,7 @@ BestSolution_Max = 59.000000
   </ul>
 
 
-  \todo vanderwaerden_2(4,10) > 325
+  \todo vanderwaerden_2(4,10) > 326
   <ul>
    <li> We don't have a nice prediction, except of that it's greater (or 
    equal) than 309. </li>
@@ -1182,13 +1182,26 @@ E = eval_ubcsat("VanDerWaerden_2-4-10_330.cnf", params=list(runs=100,cutoff=1000
    <li> n = 325, cutoff=10^8: found one solution (seed=1755037834,
    osteps=38900247):
    \verbatim
+> E=read_ubcsat("VanDerWaerden_2-4-10_325.cnf_OUT2")
+ 1  2  3  4  5 21 22 23 24 25 26 27
+10 17  7  6  2  2  6 11 15 16  7  1
+100
 > E = read_ubcsat("VanDerWaerden_2-4-10_325.cnf_OUT")
  0  1  2  3  5 20 21 22 23 24 25 26
  1  6 18  4  4  1  2  4  9 13 16  3
 81
    \endverbatim
    </li>
-   <li> n = 326, cutoff=10^8 </li>
+   <li> n = 326, cutoff=10^8: found one solution (seed=64752191,
+   osteps=96247637) in 162 runs:
+   \verbatim
+> E = read_ubcsat("VanDerWaerden_2-4-10_326.cnf_OUT")
+ 0  1  2  3  4  5 21 22 23 24 25 26 27
+ 1  6 33 18  6  4  4  4  9 18 36 15  8
+162
+   \endverbatim
+   So we should use cutoff=2*10^8 now. </li>
+   <li> n=327: </li>
    <li> n=330 with adaptnovelty+
     <ol>
      <li> cutoff=10^7 
