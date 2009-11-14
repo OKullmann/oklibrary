1b3ea69e040a684c2d252a4ede6602340d1c0296
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-14 15:17:27 +0000

    On enumerating and counting latin squares.

--- ComputerAlgebra/CombinatorialMatrices/Lisp/LatinSquares/plans/BasicNotions.hpp
+++ ComputerAlgebra/CombinatorialMatrices/Lisp/LatinSquares/plans/BasicNotions.hpp
@@ -73,6 +73,57 @@ false
    <li> Is the general-matrix-isomorphism already handled when we consider
    only reduced matrices? But what would then be the notion of
    isomorphism between fully standardised squares? </li>
+   <li> It would be good if all reduced latin squares of order n could
+   be enumerated for small n. Then a general construction yields the
+   enumeration of all latin squares of order n.
+    <ol>
+     <li> Easiest to just list one (representative) element of each
+     paratopy class. </li>
+     <li> The number of paratopy classes is given by sequence A003090:
+     \verbatim
+1, 1, 1, 2, 2, 12, 147, 283657, 19270853541, 34817397894749939
+     \endverbatim
+     </li>
+     <li> While the number of isotopy classes is given by sequence A040082:
+     \verbatim
+1, 1, 1, 2, 2, 22, 564, 1676267, 115618721533, 208904371354363006
+     \endverbatim
+     </li>
+     <li> The simplest latin square is the cyclic group of order n; for
+     1 <= n <= 3 this is the only paratopy class, and in fact the only
+     isotopy class. </li>
+     <li> For n <= 4 every loop is a group. So the second isotopy class
+     for n=4 is given by the Kleinian 4-group. </li>
+     <li> While the second isotopy class for n=5 is given by nassoc_rls. </li>
+     <li> Definitely also for n=6 we should have representatives of the 12
+     paratopy classes and the 22 isotopy classes in our library. </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  \todo Counting
+  <ul>
+   <li> DONE
+   The knowledge on counting reduced latin squares should be made
+   available, say num_rls(n). </li>
+   <li> This is sequence A000315:
+   \verbatim
+1, 1, 1, 4, 56, 9408, 16942080, 535281401856, 377597570964258816, 
+7580721483160132811489280,
+5363937773277371298119673540771840
+   \endverbatim
+   </li>
+   <li> DONE
+   While for the number of arbitrary latin squares (with value set
+   {1,...,n}) we have
+   num_ls(n) = num_rls(n) * n! * (n-1)!. </li>
+   <li> It would perhaps be better if for example we had
+   \verbatim
+num_ls(12) = 19120211066880000 * num_rls(12)
+   \endverbatim
+   (instead of "19120211066880000*unknown")? </li>
+   <li> See above for isotopy classes and paratopy classes. </li>
   </ul>
 
 */
