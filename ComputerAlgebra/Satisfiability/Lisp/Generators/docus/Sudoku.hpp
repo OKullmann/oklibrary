36476d813945d4c8827a5d1b84cda78e21f3c587
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-29 03:25:51 +0000

    More on obtain some "reasoning" from the run of a SAT solver on Sudoku-problems.

--- ComputerAlgebra/Satisfiability/Lisp/Generators/docus/Sudoku.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Generators/docus/Sudoku.hpp
@@ -172,9 +172,49 @@ c number_of_1-autarkies                 0
 c number_of_new_2-clauses               0
 c maximal_number_of_added_2-clauses     0
 c file_name                             S_EasterMonster.cnf
-   \endverbatim
 
-   Remark: The number of 2-reductions cannot be used as a measure of
+> OKsolver_2002-O3-DNDEBUG -O -F S_EasterMonster.cnf
+
+A : rest(rest(read_nested_list("S_EasterMonster.cnf.pa")[1]),-1)$
+B : sublist(A,lambda([x],is(x>0)));
+length(B);
+  81
+output_fcs("", cs2fcs({setify(-B)}), "Sol_EasterMonster");
+
+> vi Sol_EasterMonster # removal of non-clausal lines
+> cp S_EasterMonster.cnf S_EasterMonster_Neg.cnf
+> cat Sol_EasterMonster >> S_EasterMonster_Neg.cnf
+
+>  OKsolver_2002-O3-DNDEBUG S_EasterMonster_Neg.cnf
+s UNSATISFIABLE
+c sat_status                            0
+c initial_maximal_clause_length         81
+c initial_number_of_variables           729
+c initial_number_of_clauses             10552
+c initial_number_of_literal_occurrences 23430
+c number_of_initial_unit-eliminations   490
+c reddiff_maximal_clause_length         21
+c reddiff_number_of_variables           490
+c reddiff_number_of_clauses             8984
+c reddiff_number_of_literal_occurrences 19760
+c number_of_2-clauses_after_reduction   1343
+c running_time(sec)                     0.0
+c number_of_nodes                       9
+c number_of_single_nodes                0
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                100
+c number_of_pure_literals               0
+c number_of_autarkies                   0
+c number_of_missed_single_nodes         0
+c max_tree_depth                        3
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 0
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             S_EasterMonster_Neg.cnf
+   \endverbatim
+ 
+  Remark: The number of 2-reductions cannot be used as a measure of
    hardness, since it depends on the order the variables are encountered.
 
 */
--- Experimentation/Investigations/LatinSquares/plans/Sudoku.hpp
+++ Experimentation/Investigations/LatinSquares/plans/Sudoku.hpp
@@ -24,31 +24,87 @@ License, or any later version. */
 
   \todo Finding hard instances for box-dimension 3
   <ul>
-   <li> A source for "hard" instances is [extreme Su Doku; Wayne Gould]. </li>
-   <li> The cover-text says "... Wayne Gould, who'll take you deep inside his
-   genius psyche.", and "..., he has been named by Time Magazine as one of
-   the 100 people who shaped the world.". </li>
-   <li> One sees that enlightenment is needed. </li>
-   <li> As one can see in
-   ComputerAlgebra/Satisfiability/Lisp/Generators/docus/Sudoku.hpp, apparently
-   most problems only need one r_2-reduction, while perhaps the most difficult
-   instance in that collection needs five r_2-reductions (without
-   backtracking, and thus also uniqueness is confirmed). </li>
-   <li> We should search for problems which are as hard as possible; see
-   "Sampling of minimally uniquely satisfiable problems" in
-   ComputerAlgebra/Satisfiability/Lisp/Generators/plans/Sudoku.hpp. </li>
+   <li> A source for "hard" instances is [extreme Su Doku; Wayne Gould].
+    <ol>
+     <li> The cover-text says "... Wayne Gould, who'll take you deep inside his
+     genius psyche.", and "..., he has been named by Time Magazine as one of
+     hte 100 people who shaped the world.". </li>
+     <li> One sees that enlightenment is needed. </li>
+     <li> As one can see in
+     ComputerAlgebra/Satisfiability/Lisp/Generators/docus/Sudoku.hpp,
+     apparently most problems only need one r_2-reduction, while perhaps the
+     most difficult instance in that collection needs five r_2-reductions
+     (without backtracking, and thus also uniqueness is confirmed). </li>
+     <li> So likely this source is without further interest. </li>
+    </ol>
+   </li>
+   <li> A better source is [Denis Berthier, The Hidden Logic of Sudoku, 2007]
+    <ol>
+     <li> See the collections at http://www.carva.org/denis.berthier . </li>
+     <li> This source only consider these "humanoid rules" (AI-nonsense). </li>
+     <li> Perhaps all these rules are covered by r_2 for the strong encoding?
+     </li>
+     <li> In principle the all-different constraint can not be covered, but
+     perhaps this doesn't happen for these small problems (box-dimension 3)?
+     </li>
+    </ol>
+   </li>
+   <li> We should search for problems which are as hard as possible.
+    <ol>
+     <li> See
+     "Sampling of minimally uniquely satisfiable problems" in
+     ComputerAlgebra/Satisfiability/Lisp/Generators/plans/Sudoku.hpp. </li>
+     <li> The "EasterMonster" (see
+     Satisfiability/Lisp/Generators/docus/Sudoku.hpp) needs 9 nodes with
+     the OKsolver_2002; this is the hardest example yet encountered. </li>
+     <li> There should be harder *unsatisfiable* problems. </li>
+     <li> While for the uniquely satisfiable problems actually it would be
+     good if the OKsolver could enumerate all solution --- since here this
+     would yield the proof of uniqueness, and only this would be comparable
+     to the rule-based approach, which also always shows uniqueness. </li>
+     <li> At least we need to extract the solution, add it negated to the
+     conditions, and see what the complexity of the refutation is. </li>
+     <li> This refutation also contains the direct proof that the given total
+     assignment is the unique solution. </li>
+     <li> Perhaps it is then enough to search for short (tree-like) resolution
+     refutations of the original sdk-clause-set ("empty field") plus the
+     negated (total) satisfying assignment. </li>
+     <li> The refutation of the "EasterMonster" with negated solution actually
+     doesn't get any harder than finding the solution; see
+     Satisfiability/Lisp/Generators/docus/Sudoku.hpp. </li>
+     <li> See below for the "human editing". </li>
+    </ol>
+   </li>
    <li> For the "human" solution of such problems, "explanations" are needed
    for the solution, which is just a demonstration of the sequence of forced
    assignments.
     <ol>
-     <li> In that book the condition "without reductio ad absurdum"
-     is stated. Can this be made precise? </li>
+     <li> In [extreme Su Doku; Wayne Gould] the condition "without reductio ad
+     absurdum" is stated. Can this be made precise? </li>
+     <li> In [Denis Berthier The Hidden Logic of Sudoku, 2007] one finds a
+     more systematic approach, and also an attempt at defining what "without
+     guessing" could mean. </li>
      <li> Perhaps r_2-reduction would not be eligible here, and perhaps
-     one should use full-strength bijectivity-constraints (where then
-     r_2-reduction might be applicable). </li>
+     one should use full-strength bijectivity-constraints? </li>
      <li> Of course, one can simulate r_2-reduction via resolution, which
      is "forward reasoning"; so perhaps one should search for short
      resolution derivations of the forced assignments. </li>
+     <li> Actually, the last remark seems crucial, showing that all that talk
+     about "trial and error" and so on is nonsense: Every forced assignment
+     can be demonstrated by r_k for k big enough, and this "trial-and-error"
+     derivation can be translated into a "direct logical reasoning", namely
+     a resolution refutation (of the unit-clause corresponding to the
+     forced assignment)! </li>
+     <li> One should do so for example for the above "EasterMonster"-example,
+     where [Denis Berthier] claims "no resolution rule is yet known to be
+     applicable to this puzzle". </li>
+     <li> He speaks of "rules", while the resolution proof is not a general
+     rule; so well, perhaps some form of automated generalisation is possible?
+     </li>
+     <li> The full refutation of the EasterMonster with negation is just 9
+     nodes (and 100 r_2-reductions). Of course a lot of unit-propagations are
+     involved. A good representation of this refutation would be valuable.
+     </li>
     </ol>
    </li>
   </ul>
