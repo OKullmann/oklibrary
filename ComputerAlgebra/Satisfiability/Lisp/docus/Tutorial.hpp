c9633852bf7d11fdc98bdcf141fee40224f76422
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-29 00:50:43 +0000

    Editorial updates (corrections).

--- ComputerAlgebra/Satisfiability/Lisp/docus/Tutorial.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/docus/Tutorial.hpp
@@ -27,11 +27,13 @@ License, or any later version. */
    obtain the underlying variable of a literal l, the function var_l(l) is
    provided.
    \verbatim
+oklib_load_all();
+
 (%i12) var_l(-1);
 (%o12)                                 1
    \endverbatim
    </li>
-   <li> For a set of variables S, the literals_v(S) function returns the
+   <li> For a set S of variables, by literals_v(S) we compute the
    set of all literals with underlying variable in S.
    \verbatim
 (%i13) literals_v({1,2,3});
@@ -41,79 +43,79 @@ License, or any later version. */
    <li> A "boolean clause" is a set C of boolean literals such that C and the
    complement of C, comp_sl(C), have empty intersection.
    \verbatim
-(%i14) c1:{1,-2};
+(%i14) C1:{1,-2};
 (%o14)                             {- 2, 1}
-(%i15) intersect(c1,comp_sl(c1));
+(%i15) intersect(C1,comp_sl(C1));
 (%o15)                                {}
    \endverbatim
    </li>
    <li> A "boolean clause-set" is a set of boolean clauses.
    \verbatim
-(%i16) c2:{-1,3}$
-(%i17) c3:{2,3}$
-(%i18) c:{c1,c2,c3};
+(%i16) C2:{-1,3}$
+(%i17) C3:{2,3}$
+(%i18) F:{C1,C2,C3};
 (%o18)                   {{- 2, 1}, {- 1, 3}, {2, 3}}
    \endverbatim
    </li>
    <li> The variables of a clause-set can be obtained by the var_cs function:
    \verbatim
-(%i19) var_cs(c);
+(%i19) var_cs(F);
 (%o19)                             {1, 2, 3}
    \endverbatim
    </li>
-   <li> The number of variables of a clause-set can be obtained by by the
+   <li> The number of variables of a clause-set can be obtained by the
    nvar_cs function.
    \verbatim
-(%i20) nvar_cs(c);
+(%i20) nvar_cs(F);
 (%o20)                                 3
    \endverbatim
    </li>
    <li> The number of clauses of a clause-set is given by the ncl_cs function.
    \verbatim
-(%i21) ncl_cs(c);
+(%i21) ncl_cs(F);
 (%o21)                                 3
    \endverbatim
    </li>
    <li> The number of literal occurences in a clause-set is given by the
-   nlitocc function.
+   nlitocc_cs function.
    \verbatim
-(%i22) nlitocc(c);
+(%i22) nlitocc_cs(F);
 (%o22)                                 6
    \endverbatim
    </li>
    <li> A "formal boolean clause-set" is a pair [V, F], where V is a set of
    variables, while F is a clause-set using only variables from V.
-   The function cs_to_fcs(F) returns the formal clause-set of a clause-set F.
+   The function cs2fcs(F) returns the formal clause-set of a clause-set F.
    \verbatim
-(%i23) f:cs_to_fcs(c);
+(%i23) FF:cs2fcs(F);
 (%o23)             [{1, 2, 3}, {{- 2, 1}, {- 1, 3}, {2, 3}}]
    \endverbatim
    </li>
    <li> The variables of a formal clause-set can be obtained by the
-   var_cs_f function.
+   var_fcs function.
    \verbatim
-(%i24) var_cs_f(f);
+(%i24) var_fcs(FF);
 (%o24)                             {1, 2, 3}
    \endverbatim
    </li>
    <li> The number of variables of a formal clause set is given by the
-   nvar_f function.
+   nvar_fcs function.
    \verbatim
-(%i25) nvar_f(f);
+(%i25) nvar_fcs(FF);
 (%o25)                                 3
    \endverbatim
    </li>
    <li> The number of clauses of a formal clause set is given by the
-   ncl_f function.
+   ncl_fcs function.
    \verbatim
-(%i26) ncl_f(f);
+(%i26) ncl_fcs(FF);
 (%o26)                                 3
    \endverbatim
    </li>
    <li> A formal clause-set can be printed in Dimacs format using the
-   print_cs_f function. The first argument is a comment.
+   print_fcs function. The first argument is a comment.
    \verbatim
-(%i27) print_cs_f("Sample boolean clause set #001",f)$
+(%i27) print_fcs("Sample boolean clause set #001",FF)$
 c Sample boolean clause set #001
 p cnf 3 3
 -2 1 0
@@ -124,7 +126,7 @@ p cnf 3 3
    <li> For output to a file, use the output_fcs function. By default, the
    file is located in the directory where you started Maxima.
    \verbatim
-(%i28) output_fcs("Sample boolean clause set #001",f,"001.cnf")$
+(%i28) output_fcs("Sample boolean clause set #001",FF,"001.cnf")$
    \endverbatim
    </li>
   </ol>
@@ -181,11 +183,11 @@ p cnf 3 3
   <h2> Special generators </h2>
 
   <ol>
-   <li> VanDerWaerden2_cs_f(k,n) is the Boolean clause-set whose solutions
+   <li> vanderwaerden2_fcs(k,n) is the Boolean clause-set whose solutions
    are the partitionings of {1,...,n} into two parts such that none of them
    contains an arithmetic progression of size k.
    \verbatim
-(%i34) vdw38:VanDerWaerden2_cs_f(3,8);
+(%i34) vdw38:vanderwaerden2_fcs(3,8);
 (%o34) [{1, 2, 3, 4, 5, 6, 7, 8}, {{- 8, - 7, - 6}, {- 8, - 6, - 4},
 {- 8, - 5, - 2}, {- 7, - 6, - 5}, {- 7, - 5, - 3}, {- 7, - 4, - 1},
 {- 6, - 5, - 4}, {- 6, - 4, - 2}, {- 5, - 4, - 3}, {- 5, - 3, - 1},
@@ -194,7 +196,7 @@ p cnf 3 3
 {6, 7, 8}}]
 (%i35) dll_simplest_first_shortest_clause(vdw38);
 (%o35)                               true
-(%i36) vdw39:VanDerWaerden2_cs_f(3,9);
+(%i36) vdw39:vanderwaerden2_fcs(3,9);
 (%o36) [{1, 2, 3, 4, 5, 6, 7, 8, 9}, {{- 9, - 8, - 7}, {- 9, - 7, - 5},
 {- 9, - 6, - 3}, {- 9, - 5, - 1}, {- 8, - 7, - 6}, {- 8, - 6, - 4},
 {- 8, - 5, - 2}, {- 7, - 6, - 5}, {- 7, - 5, - 3}, {- 7, - 4, - 1},
