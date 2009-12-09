140f913daebe35515b79cc37ec074d1ab812a4a7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-09 11:41:51 +0000

    Editorial update.

--- Concepts/plans/Clauses.hpp
+++ Concepts/plans/Clauses.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 24.7.2005 (Swansea)
-/* Copyright 2005 - 2007 Oliver Kullmann
+/* Copyright 2005 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -25,36 +25,40 @@ License, or any later version. */
   \todo Concepts for and-clauses, or-clauses, etc.
 
 
-  \todo Shall there be also "active clauses" ?! Seems reasonable;
-  perhaps there is actually not a big difference, since the literals
-  need not to be there anyway --- however with regard to active
-  clauses more care is needed for the concept, and this might be
-  quite right. An active clause perhaps is something like
-  a "small active clause-set" (see Concepts/plans/ActiveClauseSets.hpp),
-  in which we can also grasp into. The hierarchy then would be
-  <ol>
-   <li> (generalised) literals </li>
-   <li> active clauses </li>
-   <li> active clause-sets </li>
-   <li> alliances of active clause-sets </li>
-  </ol>
-
-
-  \todo The relations to
+  \todo Shall there be also "active clauses" ?!
   <ul>
-   <li> Concepts/plans/ClauseSets.hpp </li>
-   <li> Concepts/plans/ActiveClauseSets.hpp </li>
+   <li> Seems reasonable; perhaps there is actually not a big difference,
+   since the literals need not to be there anyway --- however with regard to
+   active clauses more care is needed for the concept, and this might be
+   quite right. </li>
+   <li> An active clause perhaps is something like
+   a "small active clause-set" (see Concepts/plans/ActiveClauseSets.hpp),
+   in which we can also grasp into. </li>
+   <li> The hierarchy then would be
+    <ol>
+     <li> (generalised) literals </li>
+     <li> active clauses </li>
+     <li> active clause-sets </li>
+     <li> alliances of active clause-sets </li>
+    </ol>
+   </li>
   </ul>
-  needs to be determined.
+
+
+  \todo Relations
+  <ul>
+   <li> What are the relations to Concepts/plans/ClauseSets.hpp and
+   Concepts/plans/ActiveClauseSets.hpp? </li>
+  </ul>
 
 
   \todo Watched literals
   <ul>
-   <li> Motivated by the submission of Armin Biere to SAT2007, for the look-ahead
-   the clause could manage the two watched literals in them (inclusive the two
-   pointers to the respective next clauses). </li>
-   <li> The main point here is to find out (as easily as possible) when possibly
-   a clause became unit. </li>
+   <li> Motivated by the submission of Armin Biere to SAT2007, for the
+   look-ahead the clauses could manage the two watched literals in them
+   (inclusive the two pointers to the respective next clauses). </li>
+   <li> The main point here is to find out (as easily as possible) when
+   possibly a clause became unit. </li>
    <li> Also interesting here to find out when possibly a clause became binary
    (here we would need three watched literals per clause). </li>
    <li> Taking care of those special literals in a clause would be the
@@ -62,32 +66,33 @@ License, or any later version. */
   </ul>
 
 
-  \todo For an earlier attempt see
-  OKsolver/Experimental/AllgKlassen200203/Clauses.hpp.
+  \todo Old concepts
+  <ul>
+   <li> For an earlier attempt see
+   OKsolver/Experimental/AllgKlassen200203/Clauses.hpp. </li>
+  </ul>
 
 
   \todo A fundamental issue here is the relation to the concept of
-  partial assignments (see Concepts/plans/PartialAssignments.hpp):
+  partial assignments (see Concepts/plans/PartialAssignments.hpp)
   <ul>
    <li> It should be possible to easily switch between
    the concepts of partial assignments and clauses; perhaps the only
    difference is in the complexity of the operations: while clauses correspond
    to sets (using space proportionally to their size), partial assignments
    correspond to total maps (using a special value "unassigned"; supporting
-   constant time access).
-   </li>
+   constant time access). </li>
    <li> As set-objects partial assignments and clause-sets
-   are identical (for standard literals), namely they are sets of pairs of variables
-   and (forbidden) values, interpreted as constituting the assignment or as
-   literals. </li>
+   are identical (for standard literals), namely they are sets of pairs of
+   variables and (forbidden) values, interpreted as constituting the
+   assignment or as literals. </li>
    <li> Regarding the operations considered above, from the concept-side (the
-   operations and their complexity) there is quite some difference between clauses
-   and partial assignments (for example regarding watched literals). So turning a
-   partial assignment into a clause (and vice versa) must be possible (lossless),
-   but might be more costly. </li>
-   <li> The CNF(DNF)-representation of some condition is just a representation of
-   those assignments we realised as falsifying (satisfying).
-   </li>
+   operations and their complexity) there is quite some difference between
+   clauses and partial assignments (for example regarding watched literals).
+   So turning a partial assignment into a clause (and vice versa) must be
+   possible (lossless), but might be more costly. </li>
+   <li> The CNF(DNF)-representation of some condition is just a representation
+   of those assignments we realised as falsifying (satisfying). </li>
   </ul>
 
 */
