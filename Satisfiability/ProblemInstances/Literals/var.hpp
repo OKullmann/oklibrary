a7d3135cd9baaa1c53a24f16ff6875625155f0c6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-12 22:38:37 +0000

    Todo regarding update of namespaces.

--- Satisfiability/ProblemInstances/Literals/var.hpp
+++ Satisfiability/ProblemInstances/Literals/var.hpp
@@ -1,13 +1,15 @@
 // Oliver Kullmann, 8.12.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file ProblemInstances/Literals/var.hpp
+  \file OKlib/Satisfiability/ProblemInstances/Literals/var.hpp
   \brief Primary function template for extracting the variable from a literal
+
+  \todo Update namespaces.
 
   \todo For the future perhaps we should use a call traits instead of the fixed
   argument type const L& ?
