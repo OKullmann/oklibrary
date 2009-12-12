0cbd262766ac71d68e85fbf4f06c6bf5f50cd196
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-12 22:38:17 +0000

    Correction of syntax errors.

--- Satisfiability/Values/StatusTypes.hpp
+++ Satisfiability/Values/StatusTypes.hpp
@@ -21,25 +21,28 @@ namespace OKlib {
   namespace Satisfiability {
     namespace Values {
 
-      enum Assignment_status { val0 = 0, val1 = 1, unassigned = 2 }
-      template <> struct first_value<Assignment_status> {
-        enum { value = val0 }
-      }
-      template <> struct last_value<Assignment_status> {
-        enum { value = val1 }
-      }
-      template <> struct exceptional_value<Assignment_status> {
-        enum { value = unassigned }
-      }
-
-      enum Sat_status { falsified = 0, satisfied = 1, open = 2 }
-      template <> struct first_value<Sat_status> {
-        enum { value = falsified }
-      }
-      template <> struct last_value<Sat_status> {
-        enum { value = open }
+      enum Assignment_status { val0 = 0, val1 = 1, unassigned = 2 };
+      namespace traits {
+        template <> struct first_value<Assignment_status> {
+          enum { value = val0 };
+        };
+        template <> struct last_value<Assignment_status> {
+          enum { value = val1 };
+        };
+        template <> struct exceptional_value<Assignment_status> {
+          enum { value = unassigned };
+        };
       }
 
+      enum Sat_status { falsified = 0, satisfied = 1, open = 2 };
+      namespace traits {
+        template <> struct first_value<Sat_status> {
+          enum { value = falsified };
+        };
+        template <> struct last_value<Sat_status> {
+          enum { value = open };
+        };
+      }
 
     }
   }
