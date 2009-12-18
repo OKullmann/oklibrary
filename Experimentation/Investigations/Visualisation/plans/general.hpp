522c7586d0f623aacab45a753b3aeef712ed88cf
RWang <502618@swan.ac.uk>
2009-12-18 23:40:02 +0000

    Created a subdirectory visualisation.

---
+++ Experimentation/Investigations/Visualisation/plans/general.hpp
@@ -0,0 +1,13 @@
+// Rui Wang, 18.12.2009 (Swansea)
+/* Copyright 2009 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/Visualisation/plans/general.hpp
+  \brief Plans regarding investigations of graph drawing.
+
+
+*/
