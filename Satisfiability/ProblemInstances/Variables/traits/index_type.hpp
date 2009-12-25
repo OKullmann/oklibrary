8b567d1a6f913923f373557aa03f879c844cfb4e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-25 19:46:29 +0000

    Editorial update.

--- Satisfiability/ProblemInstances/Variables/traits/index_type.hpp
+++ Satisfiability/ProblemInstances/Variables/traits/index_type.hpp
@@ -1,12 +1,12 @@
 // Oliver Kullmann, 30.12.2005 (Swansea)
-/* Copyright 2005 - 2007 Oliver Kullmann
+/* Copyright 2005 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file ProblemInstances/Variables/traits/index_type.hpp
+  \file OKlib/Satisfiability/ProblemInstances/Variables/traits/index_type.hpp
   \brief Traits-metafunctions to extract the index type
 */
 
