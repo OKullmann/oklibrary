d57c899b5e92c7fc6d4762c9fd08f35ac0f5b59c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-25 20:30:39 +0000

    Editorial update.

--- Satisfiability/ProblemInstances/Literals/traits/cond_type.hpp
+++ Satisfiability/ProblemInstances/Literals/traits/cond_type.hpp
@@ -1,12 +1,12 @@
 // Oliver Kullmann, 5.12.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2009 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
 /*!
-  \file ProblemInstances/Literals/traits/cond_type.hpp
+  \file OKlib/Satisfiability/ProblemInstances/Literals/traits/cond_type.hpp
   \brief Traits-metafunctions to extract the condition type
 */
 
