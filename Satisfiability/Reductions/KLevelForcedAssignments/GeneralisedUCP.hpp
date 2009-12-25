7a31c354ecb798cc44c99d5013ff7eac58798705
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-25 22:02:36 +0000

    Divided class for r_k-reduction into class for r_1-reduction and the rest.

--- Satisfiability/Reductions/KLevelForcedAssignments/GeneralisedUCP.hpp
+++ Satisfiability/Reductions/KLevelForcedAssignments/GeneralisedUCP.hpp
@@ -26,6 +26,8 @@ License, or any later version. */
 #include <boost/range/const_iterator.hpp>
 #include <boost/utility.hpp>
 
+#include <OKlib/Satisfiability/Reductions/UnitClausePropagation/ClsAdaptorUCP.hpp>
+
 namespace OKlib {
   namespace Satisfiability {
     namespace Reductions {
@@ -47,139 +49,22 @@ namespace OKlib {
 
           \todo Write unit-tests
 
-          \todo Harmonise with UnitClausePropagation::CLSAdaptorUcp
-          <ul>
-           <li> See
-           OKlib/Satisfiability/Reductions/UnitClausePropagation/ClsAdaptorUCP.hpp.
-           </li>
-          </ul>
-
         */
 
         template <class WatchedClauses, class Assignment>
-        struct CLSAdaptorKUcp {
-          typedef WatchedClauses clause_type;
-          typedef typename clause_type::value_type literal_type;
-          typedef Assignment assignment_type;
-
-          typedef std::vector<clause_type> clause_set_type;
-
-          typedef std::vector<literal_type> bclause_list_type;
-          typedef std::vector<bclause_list_type> bclause_set_type;
-          typedef typename bclause_list_type::const_iterator iterator_bclauses;
-
-          typedef typename clause_set_type::iterator iterator_clauses;
-          typedef std::list<iterator_clauses> wclause_list_type;
-          typedef std::vector<wclause_list_type> wclause_set_type;
-          typedef typename wclause_list_type::iterator iterator_wclauses;
-
-          typedef typename clause_set_type::size_type size_type;
-
-          CLSAdaptorKUcp() : empty_cl(false), contradicting_ucl(false) {}
-          
-
-          // Functionality for CLSAdaptor
-          typedef literal_type int_type;
-          typedef std::string string_type;
-          void comment(const string_type& com_) {
-            if (com.empty()) com = com_;
-            else com += "\nc" + com_;
-          }
-          void n(const int_type n_) {
-            num_var = n_;
-            max_lit_index = 2*num_var;
-            F2.reserve(max_lit_index+1);
-            FW.reserve(max_lit_index+1);
-          }
-          void c(const int_type c_) {
-            num_cl = c_;
-            F.reserve(num_cl);
-          }
-          void tautological_clause(int_type) const {}
-          template <class Range>
-          void clause(const Range& clause, int_type) {
-            const size_type s = boost::size(clause);
-            if (s == 0) { empty_cl = true; return; }
-            typedef typename boost::range_const_iterator<Range>::type range_iterator;
-            const range_iterator bc = boost::const_begin(clause);
-            if (s == 1) {
-              const literal_type x = *bc;
-              if (not f.add_forced(x))
-                contradicting_ucl : true;
-              return;
-            }
-            if (s == 2) {
-              const literal_type x = *bc;
-              const literal_type y = *boost::next(bc);
-              F2[index(x)].push_back(y);
-              F2[index(y)].push_back(x);
-              return;
-            }
-            F.push_back(clause_type(bc, boost::const_end(clause)));
-            const iterator_clauses C = F.back();
-            const literal_type x = C -> first();
-            const literal_type y = C -> second();
-            FW[index(x)].push_back(C);
-            FW[index(y)].push_back(C);
-          }
-          void finish() const {}
-          bool empty_clause() const { return empty_cl; }
+        struct CLSAdaptorKUcp : OKlib::Satisfiability::Reductions::UnitClausePropagation::CLSAdaptorUcpW<WatchedClauses,Assignment> {
+          typedef OKlib::Satisfiability::Reductions::UnitClausePropagation::CLSAdaptorUcpW<WatchedClauses,Assignment> base_class;
 
           // return true iff a contradiction was found
-          bool perform_kucp(const size_type k) {
-            if (empty_cl) return true;
+          bool perform_kucp(const typename base_class::size_type k) {
+            if (this -> empty_cl) return true;
             if (k == 0) return false;
-            if (contradicting_ucl) return true;
-            if (k == 1) { return perform_ucp(); }
+            if (this -> contradicting_ucl) return true;
+            if (k == 1) { return this -> perform_ucp(); }
             // XXX to be implemented
             return false;
           }
 
-        private :
-          clause_set_type F;
-          bclause_set_type F2;
-          wclause_set_type FW;
-          assignment_type f;
-          int_type num_var;
-          int_type num_cl;
-          int_type max_lit_index;
-          string_type com;
-          bool empty_cl;
-          bool contradicting_ucl;
-          std::stringstream add_com;
-
-          // translate a literal lit into an index from 0 to max_lit_index
-          // (inclusive; index num_var is not used):
-          int_type index(const int_type lit) const {
-            assert(lit >= - num_var);
-            assert(lit != 0);
-            assert(lit <= num_var);
-            return lit + num_var;
-          }
-
-          // return true iff a contradiction was found
-          bool perform_ucp() {
-            while (not f.empty()) {
-              const int_type x = - f.top(); f.pop();
-              const int_type x_i = index(x);
-              {
-                const iterator_bclauses end = F2[x_i].end();
-                for (iterator_bclauses i = F2[x_i].begin(); i != end; ++i)
-                  if (not f.push_forced(*i)) return true;
-              }
-              const iterator_wclauses end = FW[x_i].end();
-              for (iterator_wclauses i = FW[x_i].begin(); i != end;) {
-                const iterator_clauses j = *(i++);
-                const int_type y = *j -> remove(x, f);
-                if (y == 0) return true;
-                if (y == x) continue;
-                if (not f.push_forced(y)) return true;
-                FW[x_i].remove(i);
-                FW[index(y)].push_back(j);
-              }
-            }
-            return false;
-          }
         };
 
       }
--- Satisfiability/Reductions/UnitClausePropagation/ClsAdaptorUCP.hpp
+++ Satisfiability/Reductions/UnitClausePropagation/ClsAdaptorUCP.hpp
@@ -22,6 +22,8 @@ License, or any later version. */
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
 #include <boost/range/size.hpp>
+#include <boost/range/const_iterator.hpp>
+#include <boost/utility.hpp>
 
 #include <OKlib/Satisfiability/ProblemInstances/Literals/TrivialLiterals.hpp>
 #include <OKlib/Satisfiability/ProblemInstances/Literals/var.hpp>
@@ -250,6 +252,144 @@ namespace OKlib {
           return lit + num_var;
         }
       };
+
+
+        /*!
+          \class CLSAdaptorUcpW
+          \brief Transferring a (boolean) clause-set into a clause-list, and then performing UCP using watched literals
+
+          Requirements:
+          <ul>
+           <li> the literal type is a signed integral type </li>
+          </ul>
+
+          \todo Complete implementation
+
+          \todo Write unit-tests
+
+          \todo Harmonise with UnitClausePropagation::CLSAdaptorUcp
+          <ul>
+           <li> See above.
+           </li>
+          </ul>
+
+        */
+
+        template <class WatchedClauses, class Assignment>
+        struct CLSAdaptorUcpW {
+          typedef WatchedClauses clause_type;
+          typedef typename clause_type::value_type literal_type;
+          typedef Assignment assignment_type;
+
+          typedef std::vector<clause_type> clause_set_type;
+
+          typedef std::vector<literal_type> bclause_list_type;
+          typedef std::vector<bclause_list_type> bclause_set_type;
+          typedef typename bclause_list_type::const_iterator iterator_bclauses;
+
+          typedef typename clause_set_type::iterator iterator_clauses;
+          typedef std::list<iterator_clauses> wclause_list_type;
+          typedef std::vector<wclause_list_type> wclause_set_type;
+          typedef typename wclause_list_type::iterator iterator_wclauses;
+
+          typedef typename clause_set_type::size_type size_type;
+
+          CLSAdaptorUcpW() : empty_cl(false), contradicting_ucl(false) {}
+          
+
+          // Functionality for CLSAdaptor
+          typedef literal_type int_type;
+          typedef std::string string_type;
+          void comment(const string_type& com_) {
+            if (com.empty()) com = com_;
+            else com += "\nc" + com_;
+          }
+          void n(const int_type n_) {
+            num_var = n_;
+            max_lit_index = 2*num_var;
+            F2.reserve(max_lit_index+1);
+            FW.reserve(max_lit_index+1);
+          }
+          void c(const int_type c_) {
+            num_cl = c_;
+            F.reserve(num_cl);
+          }
+          void tautological_clause(int_type) const {}
+          template <class Range>
+          void clause(const Range& clause, int_type) {
+            const size_type s = boost::size(clause);
+            if (s == 0) { empty_cl = true; return; }
+            typedef typename boost::range_const_iterator<Range>::type range_iterator;
+            const range_iterator bc = boost::const_begin(clause);
+            if (s == 1) {
+              const literal_type x = *bc;
+              if (not f.add_forced(x))
+                contradicting_ucl : true;
+              return;
+            }
+            if (s == 2) {
+              const literal_type x = *bc;
+              const literal_type y = *boost::next(bc);
+              F2[index(x)].push_back(y);
+              F2[index(y)].push_back(x);
+              return;
+            }
+            F.push_back(clause_type(bc, boost::const_end(clause)));
+            const iterator_clauses C = F.back();
+            const literal_type x = C -> first();
+            const literal_type y = C -> second();
+            FW[index(x)].push_back(C);
+            FW[index(y)].push_back(C);
+          }
+          void finish() const {}
+          bool empty_clause() const { return empty_cl; }
+
+        private :
+          clause_set_type F;
+          bclause_set_type F2;
+          wclause_set_type FW;
+          assignment_type f;
+          int_type num_var;
+          int_type num_cl;
+          int_type max_lit_index;
+          string_type com;
+          bool empty_cl;
+          bool contradicting_ucl;
+          std::stringstream add_com;
+
+          // translate a literal lit into an index from 0 to max_lit_index
+          // (inclusive; index num_var is not used):
+          int_type index(const int_type lit) const {
+            assert(lit >= - num_var);
+            assert(lit != 0);
+            assert(lit <= num_var);
+            return lit + num_var;
+          }
+
+          // return true iff a contradiction was found
+          bool perform_ucp() {
+            while (not f.empty()) {
+              const int_type x = - f.top(); f.pop();
+              const int_type x_i = index(x);
+              {
+                const iterator_bclauses end = F2[x_i].end();
+                for (iterator_bclauses i = F2[x_i].begin(); i != end; ++i)
+                  if (not f.push_forced(*i)) return true;
+              }
+              const iterator_wclauses end = FW[x_i].end();
+              for (iterator_wclauses i = FW[x_i].begin(); i != end;) {
+                const iterator_clauses j = *(i++);
+                const int_type y = *j -> remove(x, f);
+                if (y == 0) return true;
+                if (y == x) continue;
+                if (not f.push_forced(y)) return true;
+                FW[x_i].remove(i);
+                FW[index(y)].push_back(j);
+              }
+            }
+            return false;
+          }
+        };
 
       }
     }
