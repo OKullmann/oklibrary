f98a5c0f84a9d3cb21f6066922f5cc3e08c0f6c9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-01-18 20:04:08 +0000

    Data update.

--- Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_4-3-3-3-4.hpp
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_4-3-3-3-4.hpp
@@ -462,17 +462,41 @@ E = eval_ubcsat("GreenTao_L_4-3-3-3-4_1030.cnf", params=list(runs=200,cutoff=100
 170
      \endverbatim
      </li>
-     <li> Logarithmic translation, adaptnovelty+ </li>
+     <li> Logarithmic translation, adaptnovelty+, cutoff=10^7: </li>
      <li> Weak standard reduced translation: Evaluating
      \verbatim
 E = eval_ubcsat("GreenTao_R_4-3-3-3-4_1030.cnf", params=list(runs=200,cutoff=1000000))
      \endverbatim
-     by plot(E$alg,E$best): </li>
+     by plot(E$alg,E$best) and eval_ubcsat_dataframe(E):
+     \verbatim
+adaptnoveltyp :
+ 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+ 4  3  9 10 12 16 30 35 28 27 14  8  2  1  1
+gwsat :
+ 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+ 1  1  7  8  9 25 21 22 31 24 23  9  9  6  3  1
+     \endverbatim
+     </li>
      <li> Strong standard reduced translation: Evaluating
      \verbatim
 E = eval_ubcsat("GreenTao_SR_4-3-3-3-4_1030.cnf", params=list(runs=200,cutoff=1000000))
      \endverbatim
-     by plot(E$alg,E$best): </li>
+     by plot(E$alg,E$best) and eval_ubcsat_dataframe(E):
+     \verbatim
+adaptnoveltyp :
+ 8  9 10 11 12 13 14 15 16 17 18 19 20 21 22
+ 1  2  2 11 15 18 25 20 37 25 20 12  6  3  3
+gwsat :
+11 12 13 14 15 16 17 18 19 20 21 22 23 24
+10  6  7 10 22 34 20 27 15 17 13 12  4  3
+rnovelty :
+13 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42
+ 1  2  2  1  3  3  3  2  7  5  2  6 10 13  9 11  9 11  9 14 14  8  9 12  4  7
+     \endverbatm
+     </li>
+     <li> Strong standard reduced translation, adaptnovelty+, cutoff=10^7:
+
+     </li>
     </ol>
    </li>
   </ul>
