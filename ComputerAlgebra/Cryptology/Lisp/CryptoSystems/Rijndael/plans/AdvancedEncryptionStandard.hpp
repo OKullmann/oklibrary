f70ed3b1df6f411e1bf7c8b110bacc361b2641ce
Matthew Gwynne <360678@swan.ac.uk>
2010-01-22 10:17:10 +0000

    Converted aes_round to matrix representation.

--- ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/plans/AdvancedEncryptionStandard.hpp
+++ ComputerAlgebra/Cryptology/Lisp/CryptoSystems/Rijndael/plans/AdvancedEncryptionStandard.hpp
@@ -58,8 +58,8 @@ License, or any later version. */
      <li> aes_decrypt_l </li>
      <li> aes_encrypt_f </li>
      <li> aes_decrypt_f </li>
-     <li> aes_round </li>
-     <li> aes_inv_round </li>
+     <li> aes_round : DONE </li>
+     <li> aes_inv_round : DONE </li>
      <li> aes_key_expansion : DONE </li>
      <li> rijn_mixcolumns : DONE </li>
      <li> rijn_mixcolumn : DONE </li>
