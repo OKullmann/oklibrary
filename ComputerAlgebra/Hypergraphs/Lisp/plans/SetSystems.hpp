6832fcac65898c251d0364d05ed2d3d1bad61c0e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-01-23 13:59:16 +0000

    Update of todos.

--- ComputerAlgebra/Hypergraphs/Lisp/plans/SetSystems.hpp
+++ ComputerAlgebra/Hypergraphs/Lisp/plans/SetSystems.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 3.9.2008 (Swansea)
-/* Copyright 2008, 2009 Oliver Kullmann
+/* Copyright 2008, 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -32,18 +32,6 @@ License, or any later version. */
 
   \todo Unions and intersections
   <ul>
-   <li> DONE (we supply "lunion" and "lintersection", which, like "lmin", can
-   be applied to lists and sets)
-   Naming conventions:
-    <ol>
-     <li> Yet, with tree_reduce, one doesn't need to make a distinction between
-     union_ls and union_ss. </li>
-     <li> However for the future one might choose different implementations. </li>
-     <li> One could of course just supply a generic function, which checks whether
-     as set of sets or a list of sets is given. </li>
-     <li> Or one could always apply "listify"; this seems easiest. </li>
-    </ol>
-   </li>
    <li> Choosing between the different methods to apply union iteratively:
     <ol>
      <li> We have two basic extreme test cases: Disjoint union (by
@@ -97,54 +85,6 @@ var_cs_tree(F) := var_sl(tree_reduce(union,F))$
      <li> So it seems that with tree_reduce one is on the safe side. </li>
     </ol>
    </li>
-   <li> DONE (implemented uapply in
-   ComputerAlgebra/DataStructures/Lisp/Lists.mac)
-   About the argument-length restriction to "apply":
-    <ol>
-     <li> We could assume that "apply" takes arbitrarily many
-     arguments. </li>
-     <li> This rules out to use ECL as the underlying Lisp. </li>
-     <li> The only alternative would be to write dedicated implementations
-     of union_listset etc.; perhaps at some point in the future we'll do
-     so. </li>
-     <li> Then actually we should encourage the use of "union_listset" etc.
-     </li>
-     <li> The current implementation of union_listset=list_sets_union
-     queries maximal_argument_length_union: The underlying algorithm
-     can be generalised to obtain an unrestricted version of apply.
-    </ol>
-   </li>
-   <li> DONE (in the case of mostly disjoint sets tree_reduce is still
-   considerably faster)
-   A simple alternative algorithm for union_listset:
-   \verbatim
-union_ls_alt(L) := setify(uaapply(append, map(listify,L)))$
-   \endverbatim
-   This is much faster if the sets of mostly disjoint (and apparently
-   not much slower if not). </li>
-   <li> DONE
-   However, here actually we encounter the upper-bound of 4095 on
-   the argument-length! So "apply(union,x)" is simply handled differently
-   than "apply(append,x)" ! </li>
-   <li> DONE (the choice is between apply and tree_reduce)
-   "xreduce" should not be used, as
-   \verbatim
-(%i632) uuapply(union,create_list({},i,1,100000));
-Evaluation took 7.4890 seconds (7.5370 elapsed)
-(%o632) {}
-(%i633) xreduce(union,create_list({},i,1,100000));
-Evaluation took 686.2800 seconds (692.9140 elapsed)
-(%o633) {}
-   \endverbatim
-   shows (with Ecl, on csltok); with CLisp we have
-   \verbatim
-(%i2) uuapply(union,create_list({},i,1,100000));
-Evaluation took 10.1505 seconds (10.1909 elapsed) using 41.200 MB.
-(%o2) {}
-(%i3) xreduce(union,create_list({},i,1,100000));
-Evaluation took 10.2634 seconds (10.3040 elapsed) using 45.777 MB.
-   \endverbatim
-   </li>
   </ul>
 
 
@@ -171,20 +111,6 @@ create_set([A]) := setify(apply(create_list,A))$
    worth the effort? </li>
    <li> Ask on the Maxima mailing list. </li>
    <li> This should belong to ComputerAlgebra/Sets. </li>
-  </ul>
-
-
-  \todo DONE (it seems that this bug won't be corrected in Maxima, so we are
-  using our own functions)
-  Bug in Maxima: corr_cartesian_product
-  <ul>
-   <li> DONE
-   Report the bug corrected by corr_cartesian_product to the Maxima
-   mailing list and to the bug-reporting system. </li>
-   <li> Once corrected, remove all applications of corr_cartesian_product.
-   </li>
-   <li> See "Document important programming techniques" in
-   ComputerAlgebra/plans/Maxima.hpp. </li>
   </ul>
 
 */
