5ab3b0a844f1c17ce0aa60e6792f6d74f5cf0040
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-01-31 08:48:40 +0000

    Editorial updates.

--- Satisfiability/Reductions/FailedLiteralReduction/plans/general.hpp
+++ Satisfiability/Reductions/FailedLiteralReduction/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 17.12.2005 (Swansea)
-/* Copyright 2005 - 2007, 2008 Oliver Kullmann
+/* Copyright 2005 - 2007, 2008, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -23,8 +23,8 @@ License, or any later version. */
   <ul>
    <li> Clarify the relations to modules
     <ul>
-     <li> KLevelForcedAssignments: This module considers variations on K=2 and K=3,
-     while KLevelForcedAssignments considers arbitrary K. </li>
+     <li> KLevelForcedAssignments: This module considers variations on K=2 and
+     K=3, while KLevelForcedAssignments considers arbitrary K. </li>
      <li> Resolution </li>
     </ul>
    </li>
@@ -32,15 +32,15 @@ License, or any later version. */
   </ul>
 
 
-  \todo Transferring the implementation used in OKsolver.
+  \todo Transferring the implementation used in OKsolver_2002
 
 
   \todo Timestamp
   <ul>
    <li> The (old) OKsolver used a timestamp
    (see OKlib/Programming/Utilities/TimeStamps/plans/general.hpp),
-   incremented with each new literal processed
-   (attempting to find a failed literal), which was used in two ways:
+   incremented with each new literal processed (attempting to find a failed
+   literal), which was used in two ways:
    <ol>
     <li> Assignments to variables used this timestamp, so that for a
     new round (with a new envisaged failed literal) assignments
@@ -58,11 +58,12 @@ License, or any later version. */
    <li> If some reduction was performed, which other
    reductions have to be tested? </li>
    <li> While the timestamp trick uses forward-implications,
-   here we need to trace implications backwards: We need to find out the clauses
-   shortened by the performed reduction, and then we need to find all literals
-   which imply the negations of the literals in those clauses --- those need to
-   be (re)tested. Testing this could be expensive. </li>
-   <li> In [Heule et al, SAT 2004] "tree-based look-ahead" needs investigation. </li>
+   here we need to trace implications backwards: We need to find out the
+   clauses shortened by the performed reduction, and then we need to find all
+   literals which imply the negations of the literals in those clauses ---
+   those need to be (re)tested. Testing this could be expensive. </li>
+   <li> In [Heule et al, SAT 2004] "tree-based look-ahead" needs
+   investigation. </li>
   </ul>
    
 
@@ -75,8 +76,8 @@ License, or any later version. */
   <ul>
    <li> The cleanest way seems to me to handle all
    2-clauses separately, with a directed graph structure, and to maintain
-   the invariant that this graph is a strict order (irreflexive and transitive),
-   and for every arc x -> y we have also the arc -y -> -x. </li>
+   the invariant that this graph is a strict order (irreflexive and
+   transitive), and for every arc x -> y we have also the arc -y -> -x. </li>
    <li> UCP then proceeds in stages: For envisaged failed literal x -> 1
    we obtain all derived unit clauses in one go and transfer them to the
    partial assignment, and then we perform the unit clause propagation;
@@ -89,22 +90,23 @@ License, or any later version. */
   \todo Interface
   <ul>
    <li> Independent of the implementation, the interface should be that
-   for each literal the list of "obtainable" implied other literals is available
-   (this handles the 2-clauses and more) ---  either, as discussed before, all
-   implications are explicitely stored, or they are computed. </li>
-   <li> The two extremes are either only r_2, or with full local learning. </li>
+   for each literal the list of "obtainable" implied other literals is
+   available (this handles the 2-clauses and more) ---  either, as discussed
+   before, all implications are explicitely stored, or they are computed. </li>
+   <li> The two extremes are either only r_2, or with full local learning.
+   </li>
   </ul>
 
 
   \todo Graph representation
   <ul>
-   <li> The known implications can be stored directly (as under local learning),
-   which is fastest, but can use a lot of space, or we use further distributing
-   levels (which can save considerable memory). </li>
-   <li> The general problem is that of a given transitive relation R, where for each
-   source we want to know all reachable sinks: This can be stored directly
-   as a table, or we use intermediate steps (employing a directed graph).
-   Perhaps such algorithms are used for database-implementations? </li>
+   <li> The known implications can be stored directly (as under local
+   learning), which is fastest, but can use a lot of space, or we use further
+   distributing levels (which can save considerable memory). </li>
+   <li> The general problem is that of a given transitive relation R, where
+   for each source we want to know all reachable sinks: This can be stored
+   directly as a table, or we use intermediate steps (employing a directed
+   graph). Perhaps such algorithms are used for database-implementations? </li>
   </ul>
    
 
@@ -113,44 +115,52 @@ License, or any later version. */
    <li> Who is performing the r_2-reduction? </li>
    <li> One could imagine that every
    active clause-sets itself performs it, "flushing" at least when UCP has been
-   completed the found unit clauses to the other active clause-sets --- but then
-   each active clause-set had to know about the others. </li>
-   <li> And since r_2-reduction
-   is like branching ("more so" than just UCP), likely it is better that the alliance
-   handles it: Choosing a literal for probing ("setting it to false"), performing UCP
-   for the whole alliance where the active clause-sets are in probing mode, and
-   performing the reduction if possible. </li>
-   <li> This would also imply that the local learning
-   is under the control of the alliance; seems to make good sense. (Alliances can
-   themselves be considered as active clause-sets, and thus we also have the
-   possibility to perform failed literal reduction in one active clause-set if we want.)
-   </li>
+   completed the found unit clauses to the other active clause-sets --- but
+   then each active clause-set had to know about the others. </li>
+   <li> And since r_2-reduction is like branching ("more so" than just UCP),
+   likely it is better that the alliance handles it: Choosing a literal for
+   probing ("setting it to false"), performing UCP for the whole alliance
+   where the active clause-sets are in probing mode, and performing the
+   reduction if possible. </li>
+   <li> This would also imply that the local learning is under the control of
+   the alliance; seems to make good sense. (Alliances can themselves be
+   considered as active clause-sets, and thus we also have the possibility to
+   perform failed literal reduction in one active clause-set if we want.) </li>
   </ul>
 
 
   \todo 2-clauses
   <ul>
-   <li> Perhaps at least in the presence of a local-learning-facility in the alliance,
-   the alliance members should hand over all 2-clauses to this facility?! </li>
-   <li> Perhaps better not,
-   since in general this cannot be done reasonably. </li>
-   <li> Perhaps initially, when the alliance is
-   set up, then the members have the possibility to hand over 2-clauses to the local
-   learning facility (and getting rid off if themselves --- if they want), and later there
-   might still be the possibility that if an alliance member detects a global 2-clause that
-   it can be handed over to the local learning facility. </li>
+   <li> Perhaps at least in the presence of a local-learning-facility in the
+   alliance, the alliance members should hand over all 2-clauses to this
+   facility?! </li>
+   <li> Perhaps better not, since in general this cannot be done reasonably.
+   </li>
+   <li> Perhaps initially, when the alliance is set up, then the members have
+   the possibility to hand over 2-clauses to the local learning facility (and
+   getting rid off if themselves --- if they want), and later there might
+   still be the possibility that if an alliance member detects a global
+   2-clause that it can be handed over to the local learning facility. </li>
   </ul>
 
 
-  \todo There is the recurrent theme of "local vs. global": "Local" means relative to the
-  current partial assignment, while "global" means relative to the initial (empty)
-  partial assignment. There could be also forms in between, but they seem hard to handle.
+  \todo There is the recurrent theme of "local vs. global"
+  <ul>
+   <li> "Local" means relative to the current partial assignment, while
+   "global" means relative to the initial (empty) partial assignment. </li>
+   <li> There could be also forms in between, but they seem hard to handle.
+   </li>
+  </ul>
 
 
-  \todo Double look-ahead:
-  What about the integration of partial double-look-ahead (partial r_3), as discussed
-  for example in the SAT2007-paper by Heule/vanMaaren ? How much is missed with such a
-  partial scheme? And what could be reasonable heuristics for the selection??
+  \todo Double look-ahead
+  <ul>
+   <li> What about the integration of partial double-look-ahead (partial
+   r_3), as discussed for example in the SAT2007-paper by Heule/vanMaaren ?
+   </li>
+   <li> How much is missed with such a partial scheme? And what could be
+   reasonable heuristics for the selection?? </li>
+  </ul>
 
 */
 
