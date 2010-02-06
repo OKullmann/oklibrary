7090ac4d1600b31abb6b982c46e8d5fe4792f77c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-02-06 14:17:50 +0000

    Plans on extending minisat2 by an option for outputting learned clauses.

---
+++ Satisfiability/Solvers/Minisat/Minisat2/plans/general.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 6.2.2010 (Swansea)
+/* Copyright 2010 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Satisfiability/Solvers/Minisat/Minisat2/plans/general.hpp
+  \brief Plans for maintaining Minisat2
+
+
+  \todo Output learned clauses
+  <ul>
+   <li> It would be useful, if we could add another option which makes
+   minisat2 output (certain) learned clauses. </li>
+   <li> Definitely learned unit-clauses are of interest, and perhaps in general
+   one could use a size bound (with default value 1). </li>
+  </ul>
+
+*/
+
