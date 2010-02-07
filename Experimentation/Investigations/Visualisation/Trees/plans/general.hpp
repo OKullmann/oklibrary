e55b334a92953e1f402a090408aa3e84c2c22c56
RWang <edison.rwang@googlemail.com>
2010-02-07 11:32:28 +0000

    Updated the plan regarding experimentation of splitting trees.

--- Experimentation/Investigations/Visualisation/Trees/plans/general.hpp
+++ Experimentation/Investigations/Visualisation/Trees/plans/general.hpp
@@ -1,5 +1,5 @@
 // Rui Wang, 18.12.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,4 +10,17 @@ License, or any later version. */
   \brief Plans regarding experiments with visualisation of trees.
 
 
+  \todo Splitting trees.
+  <ul> 
+   <li> Pure splitting trees are a starting point. </li>
+   <li> For generators, see ComputerAlgebra/Satisfiability/Lisp/Generators
+   </li>
+   <li> For solvers, see
+   ComputerAlgebra/Satisfiability/Lisp/Backtracking/DLL_solvers.mac </li>
+   <li> Further problems: Ramsey problems. See
+   ComputerAlgebra/Satisfiability/Lisp/Generators/RamseyTheory/. For more
+   information, refer to Experimentation/Investigations/RamseyTheory </li>
+  </ul>
+
 */
+
