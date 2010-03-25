4ec9749c24fcfedd006356cce243afcede42789a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-03-25 09:10:02 +0000

    Editorial.

--- Satisfiability/Reductions/KLevelForcedAssignments/plans/general.hpp
+++ Satisfiability/Reductions/KLevelForcedAssignments/plans/general.hpp
@@ -53,7 +53,7 @@ License, or any later version. */
    <li> Binary clauses are taken out of F: for every literal x there are
    vectors U(x) of literals, and for {x,y} we have U(x) containing y, and
    U(y) containing x. </li>
-   <li> Clauses with length >= 3 support watched literals; see
+   <li> %Clauses with length >= 3 support watched literals; see
    Satisfiability/ProblemInstances/Clauses/concepts/WatchedLiterals.hpp.
     <ol>
      <li> The invariant is that watched literals are (currently) not set
