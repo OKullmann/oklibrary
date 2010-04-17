7e361528052505eeb6a7cad4c2852b2a000db40c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-04-17 13:03:28 +0100

    Plans regarding the Green-Tao-number system.

---
+++ ComputerAlgebra/RamseyTheory/Lisp/GreenTao/plans/general.hpp
@@ -0,0 +1,25 @@
+// Oliver Kullmann, 17.4.2010 (Swansea)
+/* Copyright 2010 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/RamseyTheory/Lisp/GreenTao/plans/general.hpp
+  \brief Plans regarding Green-Tao problems
+
+
+  \todo Relations to other modules
+  <ul>
+   <li> See ComputerAlgebra/Hypergraphs/Lisp/Generators/GreenTao.mac for
+   the function arithprog_primes_hg(k,n) and its variations. </li>
+   <li> See
+   ComputerAlgebra/Satisfiability/Lisp/Generators/RamseyTheory/GreenTaoProblems.mac
+   for the SAT translation. </li>
+   <li> See
+   Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/general.hpp
+   for investigations. </li>
+  </ul>
+
+*/
