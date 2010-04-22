48797e089001ace3cd9aed1783eb853d45b34933
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-04-22 10:58:36 +0100

    Plans on sequence-tools in the context of Ramsey theory.

---
+++ ComputerAlgebra/RamseyTheory/Lisp/plans/Sequences.hpp
@@ -0,0 +1,28 @@
+// Oliver Kullmann, 22.4.2010 (Swansea)
+/* Copyright 2010 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/RamseyTheory/Lisp/plans/Sequences.hpp
+  \brief Plans regarding general tools for sequences occurring in Ramsey theory
+
+
+  \todo deharmonise_eval_2
+  <ul>
+   <li> Write tests. </li>
+  </ul>
+
+
+  \todo Transformations
+  <ul>
+   <li> The functions transform_threshold_l, transform_steps_l and
+   alphatau_l, alphastep2tau, tau2alphastep from
+   ComputerAlgebra/RamseyTheory/Lisp/VanderWaerden/Numbers.mac should be moved
+   here. </li>
+  </ul>
+
+*/
+
