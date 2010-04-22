8be1d96f2483c2cd67de39203ee1a2d69caff109
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-04-22 20:30:23 +0100

    vanderwaerden_2(3,22) > 461.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/VanderWaerden_2-3-k.hpp
@@ -941,7 +941,7 @@ E = eval_ubcsat("VanDerWaerden_2-3-19_348.cnf", params=list(runs=100,cutoff=1000
   </ul>
 
 
-  \todo vanderwaerden_2(3,22) > 460
+  \todo vanderwaerden_2(3,22) > 461
   <ul>
    <li> The prediction is vanderwaerden_2(3,22) = 465. </li>
    <li> Experience with k=20 is that gsat-tabu is best, however we should
@@ -966,7 +966,9 @@ E = eval_ubcsat("VanDerWaerden_2-3-19_348.cnf", params=list(runs=100,cutoff=1000
 300
    \endverbatim
    </li>
-   <li> n=461, gsat-tabu, cutoff=2*10^8: </li>
+   <li> n=461, gsat-tabu, cutoff=2*10^8: In 39 runs a solution was found
+   (seed=696047484, osteps=20698025). It seems one should less steps. </li>
+   <li> n=462, gsat-tabu, cutoff=5*10^7 </li>
   </ul>
 
 */
