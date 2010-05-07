6bd07a6db195ef9d0b03d5cf4789f674690ac691
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-05-07 09:43:45 +0100

    Data update.

--- Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-4-4-k.hpp
+++ Experimentation/Investigations/RamseyTheory/GreenTaoProblems/plans/GreenTao_3-4-4-k.hpp
@@ -211,6 +211,13 @@ License, or any later version. */
 100
      \endverbatim
      </li>
+     <li> cutoff=18*10^6:
+     \verbatim
+ 5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 27 33
+ 1  1  1  3  5  1  4  4 11  6  6  8 14  5  4  8  6  2  5  2  1  1  1
+100
+     \endverbatim
+     </li>
     </ol>
    </li>
   </ul>
