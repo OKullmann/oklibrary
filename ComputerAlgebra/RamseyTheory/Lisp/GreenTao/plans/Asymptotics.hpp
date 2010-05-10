3d432fe99998e7165fc25fe06a35cbd2accf664e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-05-10 10:06:06 +0100

    Plans on estimating the factors in the Grosswald-Hagis-product.

---
+++ ComputerAlgebra/RamseyTheory/Lisp/GreenTao/plans/Asymptotics.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 10.5.2010 (Swansea)
+/* Copyright 2010 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/RamseyTheory/Lisp/GreenTao/plans/Asymptotics.hpp
+  \brief Plans regarding asymptotic estimations
+
+
+  \todo Connections
+  <ul>
+   <li> See
+   Structures/NumberTheory/PrimeNumbers/plans/GrosswaldHagisFormula.hpp. </li>
+  </ul>
+
+
+  \todo Estimating the factors in the infinite Grosswald-Hagis-product
+  <ul>
+   <li> An estimation for factor_C_gh_inf(k) is needed, so that for a given
+   max_p it can be estimated, how large possibly the contribution of the
+   remaining factors (with p > max_p) could be. </li>
+  </ul>
+
+*/
