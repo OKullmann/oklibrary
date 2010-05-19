5fa896b207cea4bb670c86c876d62b13e1811019
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-05-19 08:04:26 +0100

    Update.

--- Structures/NumberTheory/PrimeNumbers/plans/general.hpp
+++ Structures/NumberTheory/PrimeNumbers/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 9.5.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -7,10 +7,13 @@ License, or any later version. */
 
 /*!
   \file Structures/NumberTheory/PrimeNumbers/plans/general.hpp
-  \brief Plans for the module with primenumber tools
+  \brief Plans for the module with prime number tools
 
 
   \todo Create namespace
+
+
+  \todo Create milestones
 
 */
 
