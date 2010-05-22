a8d511c6a66c820d90665e65da177c0c4d944c43
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-05-22 14:00:21 +0100

    No archives of packages anymore.

--- plans/general.hpp
+++ plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 6.6.2007 (Swansea)
-/* Copyright 2007, 2008, 2009 Oliver Kullmann
+/* Copyright 2007, 2008, 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -444,7 +444,8 @@ namespace SatAlg = Satisfiability::Algorithms;
      </li>
      <li> The external sources: We have the checksums and version numbers,
      this should be enough. </li>
-     <li> The packages archive: backed up on the departmental server, which
+     <li> DONE (no package-history needed)
+     The packages archive: backed up on the departmental server, which
      should suffice for the local backup (see also below). </li>
      <li> Current packages: Can be recreated if needed, and a backed up
      on the departmental server, which should suffice. </li>
