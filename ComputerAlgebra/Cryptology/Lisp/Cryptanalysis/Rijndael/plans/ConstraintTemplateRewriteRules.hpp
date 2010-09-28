1578f9832a287314a3d55aa8f1073ff09ca1073a
Matthew Gwynne <360678@swan.ac.uk>
2010-09-28 14:33:54 +0100

    Moved newer AES constraint rewrite functions into separate files.

---
+++ ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/Rijndael/plans/ConstraintTemplateRewriteRules.hpp
@@ -0,0 +1,15 @@
+// Matthew Gwynne, 28.9.2010 (Swansea)
+/* Copyright 2010 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/Rijndael/plans/ConstraintTemplateRewriteRules.hpp
+  \brief Plans for the translation of Rijndael using constraint template rewrite systems.
+
+
+  \todo Move appropriate todos here
+
+*/
