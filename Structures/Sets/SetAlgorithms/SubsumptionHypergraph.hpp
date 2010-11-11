5289966abe3b75293d8064d4d3b6a10eba7b3a6b
Matthew Gwynne <360678@swan.ac.uk>
2010-11-11 15:49:09 +0000

    Added documentation for each of the different functions for the C++ subsumption hypergraph generator.

--- Structures/Sets/SetAlgorithms/SubsumptionHypergraph.hpp
+++ Structures/Sets/SetAlgorithms/SubsumptionHypergraph.hpp
@@ -32,8 +32,8 @@ License, or any later version. */
   <ul>
    <li> Specification of Subsumption_hypergraph needs more explanations, and
    it needs the concept! </li>
-   <li> All member functions need a specification. </li>
-   <li> Every class and function needs documentation! </li>
+   <li> DONE All member functions need a specification. </li>
+   <li> DONE Every class and function needs documentation! </li>
    <li> DONE In cases of larger arguments the argument-types of functions 
    should be const-references. </li>
    <li> DONE Everything not used by the user must be private. </li>
@@ -105,13 +105,53 @@ namespace OKlib {
 
     public :
 
+      //! The type of container for hyperedges associated with the subsumption
+      //! hypergraph.
       typedef typename std::list<Int> hyperedge_type;
+      //! The type of vertices associated with the subsumption hypergraph.
+      //! Note here that these are index/integral types as the hypergraph
+      //! is standardised.
       typedef Int vertex_type;
+      //! The type of the set system corresponding to the subsumption 
+      //! hypergraph itself, and specifically the container of it's
+      //! hyperedges (see Subsumption_hypergraph::hyperedges).
       typedef std::list<hyperedge_type> set_system_type;
       
+      //! The list of vertices in the subsumption hypergraph, one for
+      //! every set in F.
+      //!
+      //! Note here that these are index/integral types as the hypergraph
+      //! is standardised. 
       const hyperedge_type vertex_set;
+      //! The hyperedges of the subsumption hypergraph, one for each set in G 
+      //! and a vertex in each edge H for each set in F which subsumes the 
+      //! set in G corresponding to H.
       const set_system_type hyperedges;
 
+      /*!
+        \function subsumption_hypergraph
+        \brief Computes the subsumption hypergraph.
+        
+        Given two ordered set system as ranges f_range and g_range (of type 
+        RangeF and RangeG respectively) containing elements of type
+        arbitrary type, constructs the standardised ordered subsumption 
+        hypergraph.
+        
+        The subsumption hypergraph is returned as the hyperedge list 
+        [H_1,...,H_|G|] where for all 1 <= j <= |G|, we have H[j] is the sublist
+        of [1,...,|F|], i is in H[j] if F[i] subsumes G[j] and H[j] is a list of
+        Ints where Int is the size type of f_range.
+        
+        The Maxima-specification is subsumption_std_ohg(F,G) (see
+        ComputerAlgebra/Hypergraphs/Lisp/Basics.mac).
+        
+        @param f_range the conceptual vertices of the subsumption hypergraph, 
+        i.e., the list of sets which subsumes elements of g_range.
+        @param g_range the list of sets which are mapped to hyperedges, i.e., 
+               the sets which are subsumed by elements of f_range.
+        @return the std::list of hyperedges in the subsumption hypergraph of 
+                f_range (subsuming sets) w.r.t g_range (subsumed sets).
+      */
       Subsumption_hypergraph(const RangeF& f_range, 
                              const RangeG& g_range):
         vertex_set(fill_vertex_set(boost::size(f_range))), 
@@ -119,13 +159,22 @@ namespace OKlib {
 
     private:
 
+      //! Iterator type used for iterating over F (set systems) types
       typedef typename boost::range_const_iterator<RangeF>::type f_iterator_type;
+      //! Iterator type used for iterating over G (set systems) types
       typedef typename boost::range_const_iterator<RangeG>::type g_iterator_type;
+      //! Type of the values in F.
       typedef typename boost::range_value<RangeF>::type f_value_type;
+      //! Type of std::map from elements of F to their index in F.
       typedef std::map<f_value_type, Int> hyperedge_map_type;
+      //! Type of a non-standardised hyperedge in the subsumption hypergraph, 
+      //! i.e., a set system with with subsumed sets as elements of each set.
       typedef std::list<f_value_type> hyperedge_nonstd_type;
+      //! Size type of F.
       typedef typename boost::range_size<RangeF>::type f_size_type;
 
+      //! Given a maximum value returns a list of vertices 0 to size_f,
+      //! intended for use as the vertex set of the subsumption hypergraph.
       static const hyperedge_type fill_vertex_set(const f_size_type size_f) {
         hyperedge_type vertex_set;
         const boost::counting_iterator<Int> v_begin(0);
@@ -134,6 +183,9 @@ namespace OKlib {
         return(vertex_set);
       }
 
+      //! Takes an ordered set system and returns a map which maps each
+      //! set in the set system to the index of the set in the set system
+      //! starting at 0.
       static hyperedge_map_type fill_hyperedge_map(const RangeF& f_range) {
         hyperedge_map_type hyperedge_map;
         for(
@@ -148,6 +200,10 @@ namespace OKlib {
         return(hyperedge_map);
       }
 
+      //! Takes a set system with sets contained in RangeF and a map
+      //! from all such sets to an integral type, returning a set of
+      //! integral type elements having mapped each set to the element
+      //! assigned to it by the given map.
       static
       hyperedge_type standardise_hyperedge(const hyperedge_nonstd_type& edge, 
                                            const hyperedge_map_type& hmap) {
@@ -166,6 +222,9 @@ namespace OKlib {
         return(new_edge);
       }
 
+      //! Takes a set c_range and a set system f_range and returns
+      //! the set system containing all sets in f_range which subsume
+      //! (i.e. are a subset of) c_range.
       template <class RangeC>
       static
       hyperedge_nonstd_type all_subsuming(const RangeC c_range, 
@@ -183,6 +242,8 @@ namespace OKlib {
         return(subsumes_set);
       }
 
+      //! Given the input ranges computes the hyperedges of the subsumption 
+      //! hypergraph.
       static const set_system_type subsumption_hypergraph(const RangeF& f_range,
                                                           const RangeG& g_range) {
         set_system_type hyperedges;
@@ -199,6 +260,31 @@ namespace OKlib {
       }
     };
 
+
+    /*!
+      \function subsumption_hypergraph
+      \brief Computes the subsumption hypergraph.
+
+      Given two ordered set system as ranges f_range and g_range (of type 
+      RangeF and RangeG respectively) containing elements of type
+      arbitrary type, constructs the standardised ordered subsumption 
+      hypergraph.
+
+      The subsumption hypergraph is returned as the hyperedge list 
+      [H_1,...,H_|G|] where for all 1 <= j <= |G|, we have H[j] is the sublist
+      of [1,...,|F|], i is in H[j] if F[i] subsumes G[j] and H[j] is a list of
+      Ints where Int is the size type of f_range.
+
+      The Maxima-specification is subsumption_std_ohg(F,G) (see
+      ComputerAlgebra/Hypergraphs/Lisp/Basics.mac).
+
+      @param f_range the conceptual vertices of the subsumption hypergraph, 
+                     i.e., the list of sets which subsumes elements of g_range.
+      @param g_range the list of sets which are mapped to hyperedges, i.e., 
+                      the sets which are subsumed by elements of f_range.
+      @return the std::list of hyperedges in the subsumption hypergraph of 
+              f_range (subsuming sets) w.r.t g_range (subsumed sets).
+    */
 
     template<class RangeF, class RangeG>
     typename std::list<std::list<typename boost::range_difference<RangeF>::type> >  
