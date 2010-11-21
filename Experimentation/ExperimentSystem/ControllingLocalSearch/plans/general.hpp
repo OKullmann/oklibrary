933dc96830deb20de008fbe71530dc9859ab8c46
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-21 09:58:58 +0000

    Todo for urgently creating milestones.
    This should be established in a session with MG, OK (soon).

--- Experimentation/ExperimentSystem/ControllingLocalSearch/plans/general.hpp
+++ Experimentation/ExperimentSystem/ControllingLocalSearch/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 19.7.2008 (Swansea)
-/* Copyright 2008, 2009 Oliver Kullmann
+/* Copyright 2008, 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -9,7 +9,19 @@ License, or any later version. */
   \file ExperimentSystem/ControllingLocalSearch/plans/general.hpp
   \brief %Tools for running and evaluating local search algorithms
 
-  Especially we consider running Ubcsat, while tools are often written in R.
+  Especially we consider running Ubcsat, and tools for evaluation
+  are to be written in R.
+
+
+  \todo Create milestones
+  <ul>
+   <li> This is now urgently needed (also quite something has been
+   established). </li>
+   <li> The current version should be 0.2 (some tools have been established).
+   </li>
+   <li> For 0.3 concentration on finishing run_ubcsat and related evaluation
+   tools. </li>
+  </ul>
 
 
   \todo Meta heuristics
