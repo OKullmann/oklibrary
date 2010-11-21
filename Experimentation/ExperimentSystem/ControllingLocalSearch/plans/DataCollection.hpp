01dbe9cb00c92197c34ee6422100ac8e7a2ca8fc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-21 09:42:14 +0000

    Merged plans for evaluation tools, and moved to their proper place.
    MG, please note that this should have been done before.
    Also note that "DONE" is not to be put in brackets (what would this
    mean?).

--- Experimentation/ExperimentSystem/ControllingLocalSearch/plans/DataCollection.hpp
+++ Experimentation/ExperimentSystem/ControllingLocalSearch/plans/DataCollection.hpp
@@ -108,32 +108,8 @@ gsat_simple.run_ubcsat_log
   <ul>
    <li> When producing a function like run_ubcsat, then it is a must to
    produce at the same time basic evaluation tools for the dataframe. </li>
-   <li> Given the dataframe in E, the best algorithms seems best graphed
-   by
-   \verbatim
-plot(E$alg,E$min)
-   \endverbatim
-   which plots per algorithm the boxplots of the min-distribution. </li>
-   <li> One only needs to make sure that on the x-axis all algorithms are
-   listed. </li>
-   <li> Sorting the algorithms
-    <ol>
-     <li> Considering single algorithms by e.g.
-     \verbatim
-> table(E$min[E$alg=="adaptnoveltyp"])
-     \endverbatim
-     (note that currently algorithm names are inappropriately handled). </li>
-     <li> These tables can be put into a linear order by sorting first
-     according to min-value reached (the lower the better), and second by
-     count obtained (the higher the better). </li>
-     <li> A function should be written which prints out the
-     sorted tables in a nice way. </li>
-     <li> As a first attempt we have eval_ubcsat_dataframe, which just shows
-     all results in table form. </li>
-     <li> Perhaps then the (first) evaluation tool just uses
-     plot(E$alg,E$min), followed by printing those sorted tables. </li>
-    </ol>
-   </li>
+   <li> See "Evaluation tools for run-ubcsat" in
+   ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp. </li>
   </ul>
 
 
@@ -382,64 +358,8 @@ function(input, output="$TARGET-$ALG.result", command=ubcsat_command,
   </ul>
 
 
-  \todo Evaluating the data frames
-  <ul>
-   <li> Functions are needed for standard evaluations.
-    <ol>
-     <li> At the R-level, we go for convenience, and overloading functions
-     like "summary" or "plot" seems be the right way. </li>
-     <li> One needs to find out how to do this, how to recognise our
-     specific data frames. </li>
-     <li> "summary" should show data, while "plot" might run through a
-     serious of plots, showing the data from different angles. </li>
-    </ol>
-   </li>
-   <li> STATUS ??? how is this different from what ubcsat-okl does ???
-   Given a file Data produced by ubcsat-okl (with first line naming
-   the column headers), one could create a nicer formatting by e.g.
-   \verbatim
-awk 'NR == 1 {printf("%8s %8s %8s %8s %11s\n", $1,$2,$3,$4,$5)} NR != 1 && NF > 0 {printf("%5u %2u %8u %8u %8u %11u\n", $1,$2,$3,$4,$5,$6)}' Data
-   \endverbatim
-   producing e.g.
-   \verbatim
-     sat      min   osteps   msteps        seed
-    1  0       13     8525    20000  1352592974
-    2  0       16     5984    20000  3521402437
-
- 9999  0       15     4420    20000  4085996178
-10000  0       12     5802    20000  3176480652
-   \endverbatim
-   However this should be part of the extended ubcsat-okl (see above). </li>
-   <li> DONE (function read_ubcsat does this)
-   Reading into R by
-   \verbatim
-> E = read.table("Data", colClasses = c("character", "factor", "integer", "integer", "integer", "character"))
-   \endverbatim
-   and simple evaluation by "summary(E)" or, better,
-   \verbatim
-> summary(E[,c("sat","min","osteps")])
-   \endverbatim
-   Also the simple
-   \verbatim
-> table(E$min)
-   \endverbatim
-   is interesting (perhaps actually most interesting(!); may be plotted by
-   "plot(table(E$min))"). </li>
-   <li> Sort the data frame by
-    <ol>
-     <li> first smallest average number of falsified clauses, </li>
-     <li> then smallest average number of (total) steps. </li>
-    </ol>
-   </li>
-   <li> Also the average number of steps to the optimum is relevant:
-   When it is closer to the cutoff, then increasing the cutoff likely
-   might yield an improvement, while if not then the algorithm wanders
-   randomly around without achieving something (so increasing the cutoff
-   seems of little value). </li>
-  </ul>
-
-
-  \bug (DONE Moved to DataCollection.R) Misplaced run_ubcsat
+  \bug DONE (moved to DataCollection.R)
+  Misplaced run_ubcsat
   <ul>
    <li> run_ubcsat is misplaced in Evaluation.R. </li>
    <li> See ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp.
--- Experimentation/ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp
+++ Experimentation/ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 11.9.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,11 +10,89 @@ License, or any later version. */
   \brief %Tools for evaluating local search algorithms
 
 
-  \bug (DONE Moved to DataCollection.R)
+  \bug DONE (moved to DataCollection.R)
   The functions for running Ubcsat are misplaced here
   <ul>
    <li> See ExperimentSystem/ControllingLocalSearch/plans/DataCollection.hpp.
    </li>
+  </ul>
+
+
+  \todo Evaluation tools for run_ubcsat
+  <ul>
+   <li> Functions are needed for standard evaluations.
+    <ol>
+     <li> At the R-level, we go for convenience, and overloading functions
+     like "summary" or "plot" seems be the right way. </li>
+     <li> One needs to find out how to do this, how to recognise our
+     specific data frames. </li>
+     <li> "summary" should show data, while "plot" might run through a
+     serious of plots, showing the data from different angles. </li>
+    </ol>
+   </li>
+   <li> Given the dataframe in E, the best algorithms seems best graphed
+   by
+   \verbatim
+plot(E$alg,E$min)
+   \endverbatim
+   which plots per algorithm the boxplots of the min-distribution. </li>
+   <li> One only needs to make sure that on the x-axis all algorithms are
+   listed. </li>
+   <li> Sorting the algorithms
+    <ol>
+     <li> Considering single algorithms by e.g.
+     \verbatim
+> table(E$min[E$alg=="adaptnoveltyp"])
+     \endverbatim
+     (note that currently algorithm names are inappropriately handled). </li>
+     <li> These tables can be put into a linear order by sorting first
+     according to min-value reached (the lower the better), and second by
+     count obtained (the higher the better). </li>
+     <li> Also the time needed should at least be shown. </li>
+     <li> A function should be written which prints out the
+     sorted tables in a nice way. </li>
+     <li> As a first attempt we have eval_ubcsat_dataframe, which just shows
+     all results in table form. </li>
+     <li> Perhaps then the (first) evaluation tool just uses
+     plot(E$alg,E$min), followed by printing those sorted tables. </li>
+    </ol>
+   </li>
+   <li> Also the average number of steps to the optimum is relevant:
+   When it is closer to the cutoff, then increasing the cutoff likely
+   might yield an improvement, while if not then the algorithm wanders
+   randomly around without achieving something (so increasing the cutoff
+   seems of little value). </li>
+   <li> DONE (function read_ubcsat does this ??? WHAT IS "THIS" ???)
+   Reading into R by
+   \verbatim
+> E = read.table("Data", colClasses = c("character", "factor", "integer", "integer", "integer", "character"))
+   \endverbatim
+   and simple evaluation by "summary(E)" or, better,
+   \verbatim
+> summary(E[,c("sat","min","osteps")])
+   \endverbatim
+   Also the simple
+   \verbatim
+> table(E$min)
+   \endverbatim
+   is interesting (perhaps actually most interesting(!); may be plotted by
+   "plot(table(E$min))"). </li>
+   <li> STATUS ??? how is this different from what ubcsat-okl does ???
+   Given a file Data produced by ubcsat-okl (with first line naming
+   the column headers), one could create a nicer formatting by e.g.
+   \verbatim
+awk 'NR == 1 {printf("%8s %8s %8s %8s %11s\n", $1,$2,$3,$4,$5)} NR != 1 && NF > 0 {printf("%5u %2u %8u %8u %8u %11u\n", $1,$2,$3,$4,$5,$6)}' Data
+   \endverbatim
+   producing e.g.
+   \verbatim
+     sat      min   osteps   msteps        seed
+    1  0       13     8525    20000  1352592974
+    2  0       16     5984    20000  3521402437
+
+ 9999  0       15     4420    20000  4085996178
+10000  0       12     5802    20000  3176480652
+   \endverbatim
+   However this should be part of the extended ubcsat-okl (see above). </li>
   </ul>
 
 */
