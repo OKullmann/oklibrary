ce915af2ab72957dee762c7567647207f900aea4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-27 14:06:35 +0000

    Completed determination of palindromic solutions for k=21.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/21.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/21.hpp
@@ -114,8 +114,8 @@ License, or any later version. */
 
   \todo Palindromic numbers
   <ul>
-   <li> vanderwaerden_g([3,21],"pd") = 405 =(conj) vanderwaerden([3,21])-11.
-   </li>
+   <li> vanderwaerden_g([3,21],"pd") = 405 =(conj) vanderwaerden([3,21])-11
+   (assuming that all n>417 are unsolvable). </li>
    <li> precosat236:
     <ol>
      <li> n=416: unsat, 14336s. </li>
@@ -131,6 +131,9 @@ License, or any later version. */
      <li> n=406: unsat, 19780s. </li>
      <li> n=405: unsat, 1455s. </li>
      <li> n=404: sat, 7s. </li>
+     <li> n=403: unsat, 1293s. </li>
+     <li> n=402: sat, 1680s. </li>
+     <li> n=401: unsat, 1303s. </li>
     </ol>
    </li>
    <li> For odd n very quickly in all runs one variable is set --- which is
@@ -157,6 +160,20 @@ License, or any later version. */
    unsatisfiable instances, already with quite low cutoffs. </li>
    <li> The simplest strategy to exploit this would be the screening of an
    interval of n values, with relatively low cutoff and, say, run=100. </li>
+   <li> RunPdVdW3k
+    <ol>
+     <li> "RunPdVdW3k 21 380 416 gsat-tabu 100 1000000": alle 380<=n<=400 sat,
+     and then
+     \verbatim
+401 0
+402 1
+403 0
+404 1
+     \endverbatim
+     and all 405<=n<=416 "unsat". </li>
+     <li> Also all 1 <= n <= 380 are sat. </li>
+    </ol>
+   </li>
   </ul>
 
 */
