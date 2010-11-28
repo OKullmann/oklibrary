ba88e59d1b414a92dfcde703e829839cf5e8b570
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-28 22:55:34 +0000

    Editorial correction.

--- Satisfiability/Transformers/Generators/PdVanderWaerdenCNF.cpp
+++ Satisfiability/Transformers/Generators/PdVanderWaerdenCNF.cpp
@@ -6,7 +6,7 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*!
-  \file Satisfiability/Transformers/Generators/PdVanderWaerdenCNF.hpp
+  \file Satisfiability/Transformers/Generators/PdVanderWaerdenCNF.cpp
   \brief Application for creating SAT instances expressing 2-coloured palindromised van der Waerden problems
 
   <ul>
