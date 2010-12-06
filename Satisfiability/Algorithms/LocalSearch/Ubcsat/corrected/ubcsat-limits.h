4b12e3cb681f4bd0e61f254a35efe79116068f78
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-06 16:55:29 +0000

    Original code from Ubcsat version 1.0.0.

---
+++ Satisfiability/Algorithms/LocalSearch/Ubcsat/corrected/ubcsat-limits.h
@@ -0,0 +1,45 @@
+/*
+
+      ##  ##  #####    #####   $$$$$   $$$$   $$$$$$    
+      ##  ##  ##  ##  ##      $$      $$  $$    $$      
+      ##  ##  #####   ##       $$$$   $$$$$$    $$      
+      ##  ##  ##  ##  ##          $$  $$  $$    $$      
+       ####   #####    #####  $$$$$   $$  $$    $$      
+  ======================================================
+  SLS SAT Solver from The University of British Columbia
+  ======================================================
+  ...Developed by Dave Tompkins (davet [@] cs.ubc.ca)...
+  ------------------------------------------------------
+  .......consult legal.txt for legal information........
+  ......consult revisions.txt for revision history......
+  ------------------------------------------------------
+  ... project website: http://www.satlib.org/ubcsat ....
+  ------------------------------------------------------
+  .....e-mail ubcsat-help [@] cs.ubc.ca for support.....
+  ------------------------------------------------------
+
+*/
+
+#define DEFAULTHEAPSIZE 4194304
+#define MAXHEAPS 1024
+
+#define LITSPERCHUNK 262144
+
+#define MAXNUMALG 128
+#define MAXALGPARMS 16
+
+#define MAXFXNLIST 32
+
+#define MAXCNFLINELEN 4096
+
+#define MAXREPORTS 64
+#define MAXREPORTPARMS 8
+
+#define MAXPARMLINELEN 4096
+#define MAXTOTALPARMS 128
+
+#define MAXITEMLIST 256
+#define MAXITEMLISTSTRINGLENGTH 128
+
+#define MAXREPORTHEADERSTRING 256
+
