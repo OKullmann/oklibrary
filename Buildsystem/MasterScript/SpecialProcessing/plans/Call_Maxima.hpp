774b239d361d55181ae2ef4d29c261a1237a3166
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-16 11:16:29 +0000

    Updates on locality-issues with Maxima.

--- Buildsystem/MasterScript/SpecialProcessing/plans/Call_Maxima.hpp
+++ Buildsystem/MasterScript/SpecialProcessing/plans/Call_Maxima.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 11.8.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -16,9 +16,11 @@ License, or any later version. */
     <ol>
      <li> We need to make sure that if a user has his own Maxima installation,
      then there is no interference. </li>
-     <li> Thus all files used and all files created by Maxima (itself) must
+     <li> DONE
+     Thus all files used and all files created by Maxima (itself) must
      reside in the Maxima installation directory. </li>
-     <li> Currently this is handled by redefining HOME. </li>
+     <li> DONE
+     Currently this is handled by redefining HOME. </li>
      <li> It would be good if this could be replaced by using the (new)
      command line option "--userdir=", however at least under Ecl and CLisp
      the file .maxima_history is not ruled by userdir (but just by the
@@ -27,15 +29,35 @@ License, or any later version. */
      responsibility of rmaxima). </li>
     </ol>
    </li>
+   <li> Automatic runs of Maxima shouldn't change the history:
+    <ol>
+     <li> It seems that the maxima-executable itself doesn't manage the
+     history, but it is rmaxima which does that. </li>
+     <li> The history file is Installation_dir/.maxima_history. </li>
+     <li> DONE
+     This placement is achieved by resetting HOME. </li>
+     <li> DONE (it seems that running rmaxima in batch-mode doesn't
+     change the history)
+     Testsystem-runs, and also other runs produced by scripts
+     shouldn't interfere with the standard history. </li>
+     <li> Confirm on the Maxima mailing-list that batch-mode doesn't interact
+     with the history. </li>
+     <li> Shouldn't we use for the testruns and other script-runs just
+     the maxima-executable ?! It seems that rmaxima here is of no use. </li>
+     <li> So the Maxima configuration-file should have "maxima_call_okl",
+     which actually calls *maxima*, and "rmaxima_call_okl", which call
+     rmaxima. </li>
+    </ol>
+   </li>
    <li> No interference of different invocations:
     <ol>
      <li> We need to make sure that we can start several Maxima sessions via
      "oklib --maxima" without interferences with themselves or with a run of
      the Maxima test system. </li>
-     <li> See "Improving the test system" in
-     ComputerAlgebra/TestSystem/Lisp/plans/general.hpp. </li>
      </li> And furthermore several simultaneous runs of the test system should
      be possible. </li>
+     <li> And finally (most important for experimentation), we need to run
+     in parallel Maxima in batch-mode. </li>
      <li> The culprit is maxima-init.mac, which is written and then
      possibly corrupted (in the interval between writing of the file,
      and Maxima actually reading it) by these processes. </li>
@@ -44,27 +66,29 @@ License, or any later version. */
      where to search for the initialisation-file. </li>
      <li> So the only possibility is to create for each "run" a new
      userdir-directory. </li>
-     <li> An oklib-invocation would be one run, and one testsystem-run
-     would be one run (just a single run for the whole test-run). </li>
+     <li> An oklib-invocation would be one run, one testsystem-run
+     would be one run (just a single run for the whole test-run), and
+     one batchmode-invocation would be one run. </li>
      <li> In this temporary userdir-directory a symbolic link to the
      .maxima/binary directory must be placed, to avoid recompilation of
      some of the packages each time. </li>
-     <li> What about the file .maxima_history ? </li>
-     <li> Yet (see above) its placement can only by controlled by using
-     HOME. </li>
-     <li> The standard oklib-invocation should use the standard .maxima_history
-     file (read it, and write it upon exit). This needed to be done
-     by a symbolic link. </li>
-     <li> But testsystem-runs, and also other runs produced by scripts
-     shouldn't interfere with the standard history, and so for these
-     invocation no link to the .maxima_history should be set. </li>
-     <li> Actually, it seems that it is rmaxima, which creates this history
-     file --- shouldn't we use for the testruns and other script-runs just
-     the maxima-executable ?! In this way we would solve the problem, that
-     such automated runs could destroy the Maxima-history. </li>
-     <li> So the Maxima configuration-file should have "maxima_call_okl",
-     which actually calls *maxima*, and "rmaxima_call_okl", which call
-     rmaxima. </li>
+     <li> So we need special Maxima-calls for
+      <ul>
+       <li> ordinary usage </li>
+       <li> testsystem-runs </li>
+       <li> batch-mode </li>
+      </ul>
+      Especially for the batch-mode we need further support to make it
+      more convenient. And the testsystem-runs all just use one common
+      directory for all the runs (however different invocations of the
+      testsystem create different such directories).
+     </li>
+     <li> As said above, also for ordinary usage different directories are
+     created, however they all share the same history (different from the
+     other forms of Maxima-calls, which don't change history). </li>
+     <li> The directory names should indicate the type of call plus a time
+     stamp, now finer than a second, however still human-readable. </li>
+     <li> These temporary directories need to be removed after usage. </li>
     </ol>
    </li>
    <li> If we ever can get rid off redefining HOME, then we need to remember
