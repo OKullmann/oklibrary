1d9d990c5fd65e9e7e0ae4fb0bfeb929f97cecb1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-22 13:42:16 +0000

    More reflections on r-bases and r_1-bases.

--- Satisfiability/Reductions/Bases/plans/general.hpp
+++ Satisfiability/Reductions/Bases/plans/general.hpp
@@ -24,6 +24,10 @@ License, or any later version. */
    </li>
    <li> The only assumption on r is that r(F) is sat-equivalent to F, and that
    "r found F unsatisfiable" is visible via r(F) = {{}}. </li>
+   <li> The assumption "{} in r(F) <=> r(F) = {{}}" is not used at the
+   Maxima-level (see
+   ComputerAlgebra/Satisfiability/Lisp/Primality/plans/RBases.hpp), but it
+   seems appropriate for the "computational" C++ level. </li>
    <li> Consider now a clause-set F. An "r-base" for F is a sub-clause-set F'
    with the property that every clause C in F-F' follows from F' "via r", that
    is, we have r(vp_C * F') = {{}} (where vp_C is the partial assignment which
@@ -32,6 +36,12 @@ License, or any later version. */
    <li> See rand_rbase_cs(F,red_) in
    ComputerAlgebra/Satisfiability/Lisp/Primality/RBases.mac for a randomised
    computation at Maxima-level of some base. </li>
+   <li> With the assumption of minimality we call F' an "r-generating subset
+   of F". So the r-bases of F are the minimal r-generating subsets of F. </li>
+   <li> A strong additional assumption on r is transitivity, that is,
+   if F' is r-generating for F, and if F'' is r-generating for F', then F'' is
+   r-generating for F. </li>
+   <li> However r_1 doesn't have this property: XXX </li>
   </ul>
 
 
@@ -49,6 +59,9 @@ License, or any later version. */
   <ul>
    <li> The first task is to compute r_1-bases (one could also speak of
    "UCP-bases"). </li>
+   <li> The r_1_bases of a clause-set F are exactly those F' <= F such that
+   all C in F-F' can be derived from F' modulo subsumption by input-resolution.
+   </li>
    <li> Important for efficiency, that elimination of clauses and moving to
    a different clause can be done without much overhead. </li>
    <li> It seems that UnitClausePropagation::CLSAdaptorUcpW basically fulfills
@@ -63,6 +76,17 @@ License, or any later version. */
      watched literals. </li>
      <li> Perhaps we have to just use the underlying clause-class
      Clauses::WatchedLiterals_mono. </li>
+    </ol>
+   </li>
+   <li> The problem with the Maxima-specification rand_rbase_cs is how to
+   reproduce the Maxima-function random_permutation:
+    <ol>
+     <li> The Maxima-documentation says that the "Knuth shuffle algorithm"
+     is used. </li>
+     <li> And then we need to simulate the Maxima random-generator. </li>
+     <li> This should all be possible without too much effort. </li>
+     <li> However perhaps for now we just use examples with a unique base.
+     </li>
     </ol>
    </li>
   </ul>
