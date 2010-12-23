a731948d4d751ba79b24e619c9e18e942de8cea6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-23 20:41:31 +0000

    Editorial updates.

--- ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/RBases.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/Reductions/plans/RBases.hpp
@@ -22,26 +22,38 @@ License, or any later version. */
 
   \todo Organisation
   <ul>
-   <li> DONE The concept of r-bases is independent of prime-clauses. </li>
+   <li> DONE The concept of r-bases can be applied to arbitrary clause-sets,
+   not just to the set of all prime-clauses of some boolean function. </li>
    <li> DONE
-   So likely this module should be moved to module Lisp/Reductions. </li>
+   So likely this module should be moved from Lisp/Primality to module
+   Lisp/Reductions. </li>
    <li> What remains there is the application to compute "interesting" subsets
    of the set of all prime-clauses. </li>
   </ul>
 
 
-  \todo Bases of the set of prime clauses relative to some reduction
+  \todo The notion of a base of a clause-set relative to some reduction
   <ul>
    <li> Consider some reduction r (i.e., r: CLS -> CLS with r(F)
    satisfiability-equivalent to F). </li>
    <li> A sub-clause-set F' of a clause-set F is an "r-base of F" if
-   for every clause D in F - F' and for the corresponding partial assignment
+   for every clause D in F and for the corresponding partial assignment
    phi_D^0 the empty clause is element of r(phi_D^0 * F'), while this is not
    true for any strict subset of F'. </li>
    <li> That means that all missing clauses D can be derived from F' by means
    of r. </li>
+  </ul>
+
+
+  \todo Considering special reductions
+  <ul>
    <li> There is exactly one r_0-base of every F, namely the result of
    subsumption elimination in F. </li>
+  </ul>
+
+
+  \todo Application to the representation of boolean functions
+  <ul>
    <li> If F is the set of prime implicates of some boolean function, which is
    too big to be included in some CNF-representation, then the hope is that
    r_1-bases or r_2-bases might be much smaller, while still having enough
@@ -49,12 +61,17 @@ License, or any later version. */
    <li> The reasoning is that if r is too strong (for example if r detects
    every unsatisfiability, then the bases of (any) F are exactly the
    irredundant cores) then we get smaller F', however we possibly gave up
-   too much information, and SAT-solving might be much harder. </li>
+   too much inference power, and SAT-solving might be much harder. </li>
    <li> The r_k-bases of such F are exactly the minimal (w.r.t
    subset-inclusion) r_k-based representation via CNFs not using additional
    variables (see "Investigating conditions and their representations" in
    Investigations/Cryptography/AdvancedEncryptionStandard/plans/general.hpp)
    of the underlying boolean function. </li>
+  </ul>
+
+
+  \todo Sampling of r-bases
+  <ul>
    <li> DONE (we have rand_rbase_cs in
    ComputerAlgebra/Satisfiability/Lisp/Reductions/RBases.mac)
    First we just need the natural randomised algorithm for producing some
@@ -70,6 +87,11 @@ License, or any later version. */
      </li>
     </ol>
    </li>
+  </ul>
+
+
+  \todo Computing minimum r-bases
+  <ul>
    <li> Searching for minimum bases (that is, of minimum size) might be very
    expensive, but, of course, should be implemented. </li>
   </ul>
