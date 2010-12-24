65154529547f86419c4102e35c019d1cc7eec2e0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-24 00:26:53 +0000

    Editorial updates.

--- Satisfiability/Solvers/OKsolver/plans/OKsolver_3_0.hpp
+++ Satisfiability/Solvers/OKsolver/plans/OKsolver_3_0.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 4.2.2006 (Swansea)
-/* Copyright 2006 - 2007 Oliver Kullmann
+/* Copyright 2006 - 2007, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,7 +8,7 @@ License, or any later version. */
 /*!
   \file Solvers/OKsolver/plans/OKsolver_3_0.hpp
   \brief Plans for the module for the successor of OKsolver_2_0;
-  perhaps for SAT 2010.
+  perhaps for SAT 2013.
 
   OKsolver3.0 strengthens OKsolver2.0 by using more complex methods,
   managed by forms of global resource management.
@@ -20,22 +20,34 @@ License, or any later version. */
   \todo Update namespaces
 
 
-  \todo One main challenge here seems to be parallel computations:
-  Running different algorithms at the same time (even if only one processor is
-  available), for example splitting v -> 0 and v -> 1 in parallel, and using the
-  learned clauses from both sides. There must be some kind of central
-  resource management. This enables also to use more time-consuming
-  methods "when the opportunity arises".
+  \todo Parallelism
+  <ul>
+   <li> One main challenge here seems to be parallel computations:
+   Running different algorithms at the same time (even if only one processor is
+   available), for example splitting v -> 0 and v -> 1 in parallel, and using
+   the learned clauses from both sides. </li>
+   <li> There must be some kind of central
+   resource management. This enables also to use more time-consuming
+   methods "when the opportunity arises". </li>
+  </ul>
 
 
-  \todo It should then also be possible to distribute a solution process for
-  example via the Internet (for big problems).
+  \todo Distributed solving
+  <ul>
+   <li> It should then also be possible to distribute a solution process for
+   example via the Internet (for big problems). </li>
+  </ul>
 
 
-  \todo A main topic for efficient resource management is the measurement of progress;
-  while for OKsolver_2_0 for example likely the simplest prediction strategy for the remaining
-  running time is used (see Statistics/TimeSeriesAnalysis), now we are trying to use more
-  sophisticated methods.
+  \todo Progress measurement
+  <ul>
+   <li> A main topic for efficient resource management is the measurement of
+   progress. </li>
+   <li> While for OKsolver_2_0 for example likely the simplest prediction
+   strategy for the remaining running time is used (see
+   Statistics/TimeSeriesAnalysis), now we are trying to use more sophisticated
+   methods. </li>
+  </ul>
 
 
   \todo Module KLevelForcedAssignments is used, going (much) beyond r_2.
@@ -44,25 +56,42 @@ License, or any later version. */
   \todo Methods based on graph splitting are used now.
 
 
-  \todo And at least cardinality constraints and injectivity constraints are added to the
-  repertoire of active clause-sets (including extraction of structure from ordinary clause-sets).
-  See modules
-   - PseudoBoolean
-   - LinearInequalities
-   - InjectivityConstraints
-   - SATModuloTheories.
+  \todo Constraints
+  <ul>
+   <li> At least cardinality constraints and injectivity constraints are added
+   to the repertoire of active clause-sets (including extraction of structure
+   from ordinary clause-sets). </li>
+   <li> See modules
+    <ol>
+     <li> PseudoBoolean </li>
+     <li> LinearInequalities </li>
+     <li> InjectivityConstraints </li>
+     <li> SATModuloTheories </li>
+    </ol>
+   </li>
+  </ul>
 
 
-  \todo OKsolver3.0 should be strong enough to handle the main cases of hard structural example for
-  resolution (Tseitin formulas, matching formulas) in polynomial time (also for the clause-translations).
+  \todo Hard examples
+  <ul>
+   <li> OKsolver3.0 should be strong enough to handle the main cases of hard
+   structural example for resolution (Tseitin formulas, matching formulas) in
+   polynomial time (also for the clause-translations). </li>
+  </ul>
 
 
-  \todo Matching autarkies (see module MatchingAutarkies) and linear autarkies are appropriately
-  integrated.
+  \todo Autarkies
+  <ul>
+   <li> Matching autarkies (see module MatchingAutarkies) and linear autarkies
+   are appropriately integrated. </li>
+  </ul>
 
 
-  \todo On the side of random formulas and local search, module SumProduct ("belief  propagation")
-  is integrated.
+  \todo Belief propagation
+  <ul>
+   <li> On the side of random formulas and local search, module SumProduct
+   ("belief  propagation") is integrated. </li>
+  </ul>
 
 
   \todo Finally, OKsolver3.0 handles also quantifiers.
