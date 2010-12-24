882ed2ab8f90fafb69f8187d662a5e0b2c04d7ac
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-24 23:42:08 +0000

    Editorial updates.

--- plans/Annotations.hpp
+++ plans/Annotations.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 24.6.2007 (Swansea)
-/* Copyright 2007, 2008 Oliver Kullmann
+/* Copyright 2007, 2008, 2010 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -11,7 +11,11 @@ License, or any later version. */
   latex-documents are handled
 
 
-  \bug On cs-wsok OKsystems/Annotations does not use the local bibtex files, but the ones of the user.
+  \bug Bibtex-problem
+  <ul>
+   <li> On cs-wsok OKsystems/Annotations does not use the local bibtex files,
+   but the ones of the user. </li>
+  </ul>
 
 
   \todo Partially moving repository Annotations:
@@ -41,7 +45,8 @@ License, or any later version. */
      <li> Other developers macros. </li>
     </ol>
    </li>
-   <li> Perhaps at this level one needs to find out how to write a latex-"package" !
+   <li> Perhaps at this level one needs to find out how to write a
+   latex-"package" !
     <ol>
      <li> The doc-system should be used for documenting the macros. </li>
     </ol>
