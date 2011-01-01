bf629d06fc7ae70d39691484463e7cec7b206ad6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-01 12:39:44 +0000

    Moved todos on "good" local search algorithms to the general todos.

--- Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/general.hpp
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/general.hpp
@@ -1,5 +1,5 @@
 // Matthew Gwynne, 27.3.2009 (Swansea)
-/* Copyright 2009, 2010 Oliver Kullmann
+/* Copyright 2009, 2010, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -416,6 +416,27 @@ License, or any later version. */
     </ol>
    </li>
    <li> DONE Plan for more experiments! </li>
+  </ul>
+
+
+  \todo Determining a "good" local search algorithm to use on AES instances
+  <ul>
+   <li> Question: what is a good algorithm to use, considering the algorithms
+   tested by "run_ubcsat" (see 
+   Experimentation/ExperimentSystem/ControllingLocalSearch/Evaluation.R) </li>
+   <li> Presumably local search will not do well on AES instances compared to 
+   DPLL/CDCL solvers, as often local search solvers perform
+   poorly on industrial and crafted instances, which the AES translations
+   are likely similar to, considering the large number of new variables 
+   involved. </li>
+   <li> What sort of metrics to use to determine a good algorithm once the
+   experiment has been run?
+   <ul>
+    <li> Sorting first by the average number of falsified clauses and then
+    by the number of steps seems reasonable, as we wish to minimise the number
+    of falsified clauses as much as possible. </li>
+   </ul>
+   </li>
   </ul>
 
 */
