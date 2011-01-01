bf3a863f01a355af63c71030598a77033a6c8910
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-01 10:27:03 +0000

    *** Initial version 0.1 (Investigations/Cryptography/AdvancedEncryptionStandard) ***

---
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/milestones.hpp
@@ -0,0 +1,34 @@
+// Oliver Kullmann, 1.1.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/milestones.hpp
+
+  \module_version Investigations/Cryptography/AdvancedEncryptionStandard 0.1 (1.1.2011)
+
+
+  \par Version 0.1.1 : PLANNING SAT2011 opened
+
+  \par
+   Create subdirectory for SAT2011, and move related files there.
+
+  \par
+   Create further milestones.
+
+
+  \par Version 0.2 : PLANNING SAT2011 COMPLETED
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1   : 1.1.2011; initial version number (various plans and first results regarding AES "box" representations).
+
+*/
+
