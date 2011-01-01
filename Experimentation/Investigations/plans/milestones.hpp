28cae4636a8bfdc91a6ec1761eee04e917307462
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-01 11:53:55 +0000

    Editorial corrections.

--- Experimentation/Investigations/plans/milestones.hpp
+++ Experimentation/Investigations/plans/milestones.hpp
@@ -1,3 +1,10 @@
+// Oliver Kullmann, 1.1.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
 /*!
   \file Experimentation/Investigations/plans/milestones.hpp
 
