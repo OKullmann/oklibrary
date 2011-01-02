d6a48d9c9dba5565ec4b267b3dc6cc906b1583a1
Matthew Gwynne <360678@swan.ac.uk>
2011-01-02 16:51:22 +0000

    Started cleaning general SAT2011 AES experimentation todos.

---
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/Experimentation.hpp
@@ -0,0 +1,118 @@
+// Matthew Gwynne, 2.1.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/Experimentation.hpp
+  \brief AES experiments related to the SAT 2011 paper
+
+
+  \todo Links
+  <ul>
+   <li> See 
+   Investigations/Cryptography/AdvancedEncryptionStandard/plans/AESKeyDiscovery.hpp
+   for experiments regarding breaking AES (i.e. finding the key given the
+   plaintext and ciphertext. </li>
+   <li> See 
+   Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/SmallScaleKeyDiscovery.hpp
+   for experiments regarding breaking small scale AES (i.e. finding the key
+   given the plaintext and ciphertext. </li>
+   <li> See 
+   Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/EncryptionDecryption.hpp 
+   for experiments regarding computing encryption and decryption using the
+   AES SAT translation. </li>
+   <li> See 
+   Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/BoxRepresentations.hpp
+   for experiments regarding representations of the AES and small scale boxes.
+   </li>
+  </ul>
+
+
+  \todo Open problems
+  <ul>
+   <li> Here is a list of experiments which still need to be run, or
+   questions for which we still do not know the answer. 
+   <ol>
+    <li> XXX </li>
+   </ol>
+   </li>
+  </ul>
+
+
+  \todo Prepare experiments for the SAT 2011 paper
+  <ul>
+   <li> Milestones are needed. </li>
+   <li> A full update of the existing investigations-reports is needed. </li>
+   <li> Systematic variations on the various boxes:
+    <ol>
+     <li> In order to find out about the effects of box-translations, a good
+     method should be to "turn off" all other boxes, i.e., making them the
+     identity. </li>
+     <li> This could perhaps distinguish between the various occurrences of the
+     box under consideration. </li>
+     <li> For this to be well-tested, we should have reasonable possibilities
+     to create also the corresponding crypto-system, so that we can check
+     encryption/decryption. </li>
+     <li> The translation system shouldn't have any variables regarding the
+     identities created by those identity-boxes. </li>
+     <li> Perhaps for version 1.0 of the Maxima module Cryptology we plan to
+     have a fully integrated system, which from a very general specification
+     creates the cryptosystems as well as the various SAT or CSP translations.
+     </li>
+    </ol>
+   </li>
+   <li> First experiment-running script:
+    <ol>
+     <li> Using three steps: experiment-creation, experiment-running,
+     experiment-evaluation. </li>
+     <li> An experiment is representing via a directory with a good descriptive
+     name, where inside all information is found for reproduction and
+     restarting. </li>
+     <li> It seems best to have first a script which takes as parameters
+     the generalised AES-parameters plus optionally a seed (default=1),
+     so that these parameters fully specify a sequence of generalised
+     AES problems, and then all combinations of SAT solvers and translations
+     are executed. </li>
+     <li> The problems are created by computing the key by the Maxima
+     random-number generator, encrypting the all-0 plaintext, and then
+     taking the problem of guessing the key from given plain- and ciphertext.
+     </li>
+     <li> The major problem here is running Maxima in parallel:
+      <ol>
+       <li> See "Improve locality" in
+       Buildsystem/MasterScript/SpecialProcessing/plans/Call_Maxima.hpp,
+       especially point "No interference of different invocations". </li>
+       <li> Until now it was not a major issue, however now running, say, 30
+       experiments in parallel won't work out without a solution. </li>
+      </ol>
+     </li>
+    </ol>
+   </li>
+  </ul>
+
+
+  \todo Solvers to be used for experimentation : NEEDS UPDATE
+  <ul>
+   <li> We need a list of the solvers we will use for all experiments. </li>
+   <li> As time goes on, some solvers might not be used for later experiments
+   based on earlier results, and this can be recorded here. </li>
+   <li> Solvers to be used: 
+    <ul>
+     <li> OKsolver_2002 </li>
+     <li> minisat2 </li>
+     <li> ubcsat (1-0-0) </li>
+     <li> picosat913 </li>
+     <li> Satz215 </li>
+     <li> sp </li>
+     <li> march_pl </li>
+    </ul>
+   </li>
+   <li> Any solvers to be used which are not currently in the library, need
+   to be added to BuildSystem/ExternalSources/SpecialBuilds/plans/SAT.hpp .
+   </li>
+  </ul>
+
+*/
