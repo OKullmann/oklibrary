59c2a11726e8db8685353415e79fb764e678525e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-09 11:28:06 +0000

    Editorial correction.

--- Satisfiability/Interfaces/InputOutput/plans/milestones.hpp
+++ Satisfiability/Interfaces/InputOutput/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 10.9.2006 (Swansea)
-/* Copyright 2006 - 2007, 2008, 2009, 2010 Oliver Kullmann
+/* Copyright 2006 - 2007, 2008, 2009, 2010, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -24,6 +24,7 @@ License, or any later version. */
    are handled:
     - Add application tests
     - Move CLSAdaptorFilter
+
 
   \par Version 0.0.11 :
 
