0b04782073ef026119ece8f9c90a8eeef9093bb7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-13 16:07:53 +0000

    Update of version numbers.

--- Satisfiability/Interfaces/InputOutput/FilterDimacs.cpp
+++ Satisfiability/Interfaces/InputOutput/FilterDimacs.cpp
@@ -147,7 +147,7 @@ namespace {
   const std::string program = "FilterDimacs";
   const std::string err = "ERROR[" + program + "]: ";
 
-  const std::string version = "0.0.4";
+  const std::string version = "0.0.5";
 
 }
 
