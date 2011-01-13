35e99fa54e06a35cc1866224dd23d64e69352fa6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-01-13 18:08:41 +0000

    Started investigating the boolean complexity of linear maps.

--- Experimentation/Investigations/BooleanFunctions/plans/Permutations.hpp
+++ Experimentation/Investigations/BooleanFunctions/plans/Permutations.hpp
@@ -256,6 +256,160 @@ XXX
    </li>
    <li> More complicated are linear bijections x -> M*x for invertible matrices
    M. </li>
+   <li> Considering the complement of the diagonal matrix:
+    <ol>
+     <li> Just a natural example, to start the investigations.
+     \verbatim
+diagz(n) := zeromatrix(n,n)+1 - ident(n);
+for n : 1 thru 20 do print(n,newdet(diagz(n)));
+1 0 
+2 -1 
+3 2 
+4 -3 
+5 4 
+6 -5 
+7 6 
+8 -7 
+9 8 
+10 -9 
+11 10 
+12 -11 
+13 12 
+14 -13 
+15 14 
+16 -15 
+17 16 
+18 -17 
+19 18 
+20 -19 
+     \endverbatim
+     </li>
+     <li> So (over RR) det(diagz(n)) = (-1)^(n-1) * (n-1), and thus over ZZ_2
+     we have that diagz(n) is invertible iff n is even. </li>
+     <li> Experiment:
+     \verbatim
+> mkdir ExpLTrans
+> cd ExpLTrans
+
+for n : 1 thru 8 do output_m_fullcnf(diagz(n),sconcat("LTrans_", n, ".cnf"));
+
+ALG=~/OKplatform/system_directories/bin/QuineMcCluskeySubsumptionHypergraphFullStatistics-n16-O3-DNDEBUG; for (( n=1; n<=8; ++n )); do ${ALG} LTrans_${n}.cnf n; done
+
+for (( n=1; n<=8; ++n )); do F=LTrans_${n}.cnf; echo ${F}; cat ${F}_primes_stats; cat ${F}_shg_stats; done
+
+LTrans_1.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+2 1 1 0 1 0 1
+ length count
+1 1
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+1 1 1 0 1 0 1
+ length count
+1 1
+
+LTrans_2.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+4 4 8 0 8 0 1
+ length count
+2 4
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+4 8 12 0 12 0 1
+ length count
+1 4
+2 4
+
+LTrans_3.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+6 40 144 0 144 0 1
+ length count
+3 16
+4 24
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+40 56 224 0 224 0 1
+ length count
+4 56
+
+LTrans_4.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+8 112 448 0 448 0 1
+ length count
+4 112
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+112 240 1792 0 1792 0 1
+ length count
+7 128
+8 112
+
+LTrans_5.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+10 336 1600 0 1600 0 1
+ length count
+4 80
+5 256
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+336 992 13312 0 13312 0 1
+ length count
+12 320
+14 640
+16 32
+
+LTrans_6.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+12 1144 6624 0 6624 0 1
+ length count
+4 120
+6 1024
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+1144 4032 96256 0 96256 0 1
+ length count
+21 768
+24 1920
+25 1280
+32 64
+
+LTrans_7.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+14 4264 29344 0 29344 0 1
+ length count
+4 168
+7 4096
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+4264 16256 696320 0 696320 0 1
+ length count
+38 1792
+42 5376
+44 8960
+64 128
+
+LTrans_8.cnf
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+16 16608 131968 0 131968 0 1
+ length count
+4 224
+8 16384
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+16608 65280 5111808 0 5111808 0 1
+ length count
+71 4096
+76 14336
+79 28672
+80 17920
+128 256
+     \endverbatim
+     </li>
+     <li> These counts look pretty regular, and one should be able to determine
+     the laws. </li>
+     <li> For n=8 some prime-clauses of length 4 are
+     \verbatim
+-10 -9 -2 1 0
+-10 -9 -1 2 0
+-10 -2 -1 9 0
+-10 1 2 9 0
+     \endverbatim
+     This partially expresses that (M*x)_1 - (M*x)_2 = x_1 + x_2. </li>
+     </li>
+    </ol>
+   </li>
   </ul>
 
 
