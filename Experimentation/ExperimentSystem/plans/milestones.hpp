e341062350d50cfa7f081a546abcc002f9c158da
Matthew Gwynne <360678@swan.ac.uk>
2011-01-17 10:04:46 +0000

    Added todo on improving R function for aggregating multiple rows into single rows with more columns.

--- Experimentation/ExperimentSystem/plans/milestones.hpp
+++ Experimentation/ExperimentSystem/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 6.11.2006 (Swansea)
-/* Copyright 2006 - 2007, 2010 Oliver Kullmann
+/* Copyright 2006 - 2007, 2010, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -16,6 +16,7 @@ License, or any later version. */
   \par
    In ExperimentSystem/plans/general.hpp the following topics are handled:
     - Simple script for monitoring remote processes
+    - Improve naming and documentation of rows2column_df
 
   \par
    All submodules have milestones.
