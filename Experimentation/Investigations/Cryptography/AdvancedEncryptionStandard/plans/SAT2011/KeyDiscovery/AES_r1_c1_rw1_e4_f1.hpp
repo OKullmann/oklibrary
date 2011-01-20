d65a0df234c6222f1bb2557ae493203c593d126d
Matthew Gwynne <360678@swan.ac.uk>
2011-01-20 14:44:41 +0000

    Further experiments for one round AES.
    Also corrected inaccurate estimate of OKsolver runtime after rerunning.

--- Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/AES_r1_c1_rw1_e4_f1.hpp
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/AES_r1_c1_rw1_e4_f1.hpp
@@ -35,9 +35,34 @@ shell> $OKlib/Experimentation/Investigations/Cryptography/AdvancedEncryptionStan
    </li>
    <li> Using the OKsolver:
    \verbatim
-shell> OKsolver_2002-O3-DNDEBUG -D30 -M ssaes_r1_c4_rw4_e8_f1_keyfind.cnf
+shell> OKsolver_2002-O3-DNDEBUG -D15 -M ssaes_r1_c4_rw4_e8_f1_keyfind.cnf
+s SATISFIABLE
+c sat_status                            1
+c initial_maximal_clause_length         256
+c initial_number_of_variables           5928
+c initial_number_of_clauses             88892
+c initial_number_of_literal_occurrences 261032
+c number_of_initial_unit-eliminations   264
+c reddiff_maximal_clause_length         0
+c reddiff_number_of_variables           264
+c reddiff_number_of_clauses             808
+c reddiff_number_of_literal_occurrences 2472
+c number_of_2-clauses_after_reduction   82432
+c running_time(sec)                     362.9
+c number_of_nodes                       646
+c number_of_single_nodes                1
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                120548
+c number_of_pure_literals               0
+c number_of_autarkies                   0
+c number_of_missed_single_nodes         0
+c max_tree_depth                        14
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 0
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             ssaes_r1_c4_rw4_e8_f1_keyfind.cnf
    \endverbatim
-   takes just under an hour.
    </li>
    <li> Using precosat236:
    \verbatim
@@ -45,8 +70,9 @@ shell> precosat236 ssaes_r1_c4_rw4_e8_f1_keyfind.cnf
    \endverbatim
    returns the correct result in 5.2 seconds.
    </li>
-   <li> This seems to take a very long time, especially given
-   we have no MixColumns included! </li>
+   <li> With "seed : 2$" and "seed : 3", these runtimes and statistics
+   seem stable. </li>
+   <li> This is slow, especially given we have no MixColumns included! </li>
   </ul>
 
 */
