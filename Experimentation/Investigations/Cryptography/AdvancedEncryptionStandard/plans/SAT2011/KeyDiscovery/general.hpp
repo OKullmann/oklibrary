407654e933ddefc7833edd56d91592268b8346bc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-02-15 22:06:29 +0000

    Editorial correction.
    The full stops just comes after the last character.

--- Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/general.hpp
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/general.hpp
@@ -51,7 +51,7 @@ License, or any later version. */
   n_2_1.hpp respectively. ??? what does this mean ???
 
   As an example, experiments on the standard one round AES (no final round,
-  with a final key addition), are in 128/4_4_8/1_1.hpp .
+  with a final key addition), are in 128/4_4_8/1_1.hpp.
 
 
   \todo Add milestones
