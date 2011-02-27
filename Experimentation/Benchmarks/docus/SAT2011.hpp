0b6b6c6d4bacf1305f313f70e0c0e7f77d4d22d5
Matthew Gwynne <360678@swan.ac.uk>
2011-02-27 15:23:07 +0000

    Updated AES WWW-site link in AES packages.

--- Experimentation/Benchmarks/docus/SAT2011.hpp
+++ Experimentation/Benchmarks/docus/SAT2011.hpp
@@ -228,7 +228,7 @@ OKlib/Experimentation/Benchmarks> tar -cjf SAT2011_AES.tar.bz2 SAT2011_AES
    </li>
    <li> The package has md5sum
    \verbatim
-7c3ebba26799fe3ba56bdf837ae9989f  SAT2011_AES.tar.bz2
+2e546d139a317cd2fb455a0882c14f19  SAT2011_AES.tar.bz2
    \endverbatim
    </li>
    <li> Extracting this archive (by
@@ -287,7 +287,7 @@ OKlib/Experimentation/Benchmarks> tar -cjf SAT2011_AES-Challenge_OKlibrary.tar.b
    </li>
    <li> The package has md5sum
    \verbatim
-d0f0d35abee0d866c0f73fa762745df3  SAT2011_AES-Challenge_OKlibrary.tar.bz2
+030a5c8f2a1ed949747b1d9d4575e282  SAT2011_AES-Challenge_OKlibrary.tar.bz2
    \endverbatim
    </li>
    <li> Extracting this archive (by
