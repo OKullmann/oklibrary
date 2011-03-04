969d03e092fdf0292d794a638e3230b56b28ad47
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-04 08:52:49 +0000

    Editorial correction.

--- Structures/Sets/SetAlgorithms/plans/general.hpp
+++ Structures/Sets/SetAlgorithms/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 19.11.2006 (Swansea)
-/* Copyright 2006 - 2010 Oliver Kullmann
+/* Copyright 2006 - 2010, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -38,6 +38,7 @@ License, or any later version. */
 
 
   \todo Write demos.
+
 
   \todo Introduce subsumption hypergraph generator
   <ul>
