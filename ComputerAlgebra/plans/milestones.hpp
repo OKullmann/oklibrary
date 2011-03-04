8aea0e2c723c4a7395b6d1987c87c1e0b1e440b9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-04 11:47:36 +0000

    Editorial.

--- ComputerAlgebra/plans/milestones.hpp
+++ ComputerAlgebra/plans/milestones.hpp
@@ -22,6 +22,9 @@ License, or any later version. */
   \par ComputerAlgebra/Combinatorics version 0.0.4 : DONE
   (see ComputerAlgebra/Combinatorics/plans/milestones.hpp)
 
+  \par ComputerAlgebra/Hypergraphs version 0.0.8 : DONE
+  (see ComputerAlgebra/Hypergraphs/Lisp/plans/milestones.hpp)
+  
   \par
    In ComputerAlgebra/DataStructures/Lisp/plans/HashMaps.hpp the following 
    topics are handled:
@@ -32,9 +35,6 @@ License, or any later version. */
    following topics are handled:
     - Random invertible matrix
 
-  \par ComputerAlgebra/Hypergraphs version 0.0.8 : DONE
-  (see ComputerAlgebra/Hypergraphs/Lisp/plans/milestones.hpp)
-  
   \par
    In ComputerAlgebra/TestSystem/Lisp/plans/general.hpp the following topics
    are handled:
