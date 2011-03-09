9e231f4bec0decece63b89433dd2053b77db4170
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-09 07:26:20 +0000

    Todo on specifying "PLA" (Espresso).
    Ticket MG-16.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/BooleanFunctions.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/BooleanFunctions.hpp
@@ -129,6 +129,9 @@ ExternalSources/Installations/R> oklib --R
    <li> At the ExternalSources page we need an index-page for the various
    pieces of documentation we have. </li>
    <li> We need to investigate what is in espresso-book-examples.tar.gz. </li>
+   <li> We need to fully specify (in our system, using our language) what
+   "PLAs" are, and create documentation for that. Perhaps we also need that
+   at Maxima-level. </li>
   </ul>
 
 
