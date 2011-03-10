80f38f613aa8b7939b82d56efd928039c6e5a3ec
Matthew Gwynne <360678@swan.ac.uk>
2011-03-10 23:54:41 +0000

    *** Initial version 0.0.1 (Lisp/CryptoSystems/DataEncryptionStandard) ***

---
+++ ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/milestones.hpp
@@ -0,0 +1,31 @@
+// Matthew Gwynne, 10.3.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/milestones.hpp
+
+  \module_version Cryptology/Lisp/Cryptanalysis 0.0.1 (10.3.2011)
+
+
+  \par Version 0.0.2
+
+  \par
+   In ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/general.hpp
+   the following topics are handled:
+    - Add todos
+
+  \par
+   Update the following milestones.
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+   - 0.0.1 : 10.3.2011; initial version number (general plans file).
+
+*/
