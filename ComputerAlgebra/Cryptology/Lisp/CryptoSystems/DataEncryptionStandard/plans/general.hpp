bae57a9627484cdde84463c1e60e75163e934d32
Matthew Gwynne <360678@swan.ac.uk>
2011-03-10 23:35:19 +0000

    New module for the implementation of the Data Encryption Standard cryptosystem.

---
+++ ComputerAlgebra/Cryptology/Lisp/CryptoSystems/DataEncryptionStandard/plans/general.hpp
@@ -0,0 +1,24 @@
+// Matthew Gwynne, 10.3.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/Cryptology/Lisp/CryptoSystems/DataEncryptionStandard/plans/general.hpp
+  \brief Plans for the Data Encryption Standard crypto-system in Maxima/Lisp
+
+
+  \todo Add todos
+
+  
+  \todo Links
+  <ul>
+   <li> See 
+   Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/general.hpp .
+   </li>
+  </ul>
+
+*/
+
