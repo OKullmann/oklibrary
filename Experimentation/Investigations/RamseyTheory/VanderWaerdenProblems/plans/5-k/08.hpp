f6c75f609cd7af61ea83e5876f819646bc604e7d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-11 06:31:39 +0000

    Editorial corrections for vdw_2(^pd)(5,k), and dedicated file for k=8.

---
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/08.hpp
@@ -0,0 +1,78 @@
+// Oliver Kullmann, 11.3.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/08.hpp
+  \brief On investigations into vdw_2(5,8)
+
+
+  \todo Best complete solver for palindromic problems
+  <ul>
+   <li> t seems that from the conflict-driven solvers minisat-2.2.0 is best,
+   from the look-ahead solvers march_pl (followed by OKsolver_2002, then
+   satz215), and that overall minisat-2.2.0 is best. </li>
+  </ul>
+
+
+  \todo Best local-search solver for palindromic problems
+  <ul>
+   <li> Best ubcsat-algorithm:
+   \verbatim
+> PdVanderWaerdenCNF-O3-DNDEBUG 5 8 312
+
+> E=run_ubcsat("VanDerWaerden_pd_2-5-8_312.cnf",runs=100,cutoff=1000000)
+
+ag2wsat
+ 0  1  2
+57 36  7
+FlipsPerSecond = 208908
+paws
+ 0  1  2  3
+52 22 25  1
+FlipsPerSecond = 204942
+nov
+ 0  1  2  3
+43 20 28  9
+FlipsPerSecond = 169408
+novp_p
+ 0  1  2  3  4
+39 18 27 15  1
+FlipsPerSecond = 129391
+novpc
+ 0  1  2  3  4
+36 11 29 23  1
+FlipsPerSecond = 206143
+anovp
+ 0  1  2
+35 48 17
+FlipsPerSecond = 172125
+novp
+ 0  1  2  3
+34 16 35 15
+FlipsPerSecond = 169951
+anovpp
+ 0  1  2
+33 41 26
+FlipsPerSecond = 170963
+ddfw
+ 0  1  2
+29 59 12
+FlipsPerSecond = 89456
+dano
+ 0  1  2
+27 44 29
+FlipsPerSecond = 169269
+g2wsat
+ 0  1  2  3
+27 23 44  6
+FlipsPerSecond = 221298
+   \endverbatim
+   (segmentation faults with saps and sapsnr). So adaptg2wsat seems clearly
+   strongest. </li>
+  </ul>
+
+*/
--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/general.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/general.hpp
@@ -24,8 +24,8 @@ License, or any later version. */
 
   \todo Literature overview
   <ul>
-   <li> Apparently nothing is known about vdw_2(5,k) ? Could there also be
-   a conjecture that it's polynomially bounded? </li>
+   <li> Apparently nothing is known about vdw_2(5,k) ? </li>
+   <li> Our conjecture is that it's polynomially bounded, </li>
    <li> The known values:
    \verbatim
 create_list(vanderwaerden5k(k),k,1,1);
@@ -35,70 +35,20 @@ create_list(vanderwaerden5k(k),k,1,1);
   </ul>
 
 
-  \todo Best complete solver
+  \todo Best complete solver for palindromic problems
   <ul>
    <li> Considering k=8, it seems that from the conflict-driven solvers
    minisat-2.2.0 is best, from the look-ahead solvers march_pl (followed
    by OKsolver_2002, then satz215), and that overall minisat-2.2.0 is best.
+   See Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/08.hpp.
    </li>
   </ul>
 
 
-  \todo Best local-search solver
+  \todo Best local-search solver for palindromic problems
   <ul>
-   <li> Best ubcsat-algorithm:
-   \verbatim
-> PdVanderWaerdenCNF-O3-DNDEBUG 5 8 312
-
-> E=run_ubcsat("VanDerWaerden_pd_2-5-8_312.cnf",runs=100,cutoff=1000000)
-
-ag2wsat
- 0  1  2
-57 36  7
-FlipsPerSecond = 208908
-paws
- 0  1  2  3
-52 22 25  1
-FlipsPerSecond = 204942
-nov
- 0  1  2  3
-43 20 28  9
-FlipsPerSecond = 169408
-novp_p
- 0  1  2  3  4
-39 18 27 15  1
-FlipsPerSecond = 129391
-novpc
- 0  1  2  3  4
-36 11 29 23  1
-FlipsPerSecond = 206143
-anovp
- 0  1  2
-35 48 17
-FlipsPerSecond = 172125
-novp
- 0  1  2  3
-34 16 35 15
-FlipsPerSecond = 169951
-anovpp
- 0  1  2
-33 41 26
-FlipsPerSecond = 170963
-ddfw
- 0  1  2
-29 59 12
-FlipsPerSecond = 89456
-dano
- 0  1  2
-27 44 29
-FlipsPerSecond = 169269
-g2wsat
- 0  1  2  3
-27 23 44  6
-FlipsPerSecond = 221298
-   \endverbatim
-   (segmentation faults with saps and sapsnr).So adaptg2wsat seems clearly
-   strongest. </li>
+   <li> adaptg2wsat seems clearly strongest for k=8 (see
+   Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/08.hpp). </li>
   </ul>
 
 */
