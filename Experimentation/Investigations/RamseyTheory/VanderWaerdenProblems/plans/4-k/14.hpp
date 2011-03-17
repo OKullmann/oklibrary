91cd8d4603f73d400e1710c2075ff4664eff726d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-17 08:19:59 +0000

    vdw_2^pd(4,14) >= (617,682).

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/14.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/14.hpp
@@ -16,51 +16,56 @@ License, or any later version. */
   </ul>
 
 
-  \todo vanderwaerden_2(4,14) > 623
+  \todo vanderwaerden_2(4,14) > 681
   <ul>
-   <li> Start with the palindromic solution for n=623. </li>
+   <li> Start with the palindromic solution for n=681. </li>
   </ul>
 
 
-  \todo vdw_2^pd(4,14) >= (611,624)
+  \todo vdw_2^pd(4,14) >= (617,682)
   <ul>
    <li> Certificates:
     <ol>
-     <li> n=610:
+     <li> n=616:
      \verbatim
-1,15,24,31,38,40,42,46,48,49,
-52,53,54,57,63,67,69,72,84,86,
-88,89,95,98,102,107,121,125,128,134,
-135,137,139,150,151,154,156,166,169,170,
-171,174,175,177,181,183,185,192,199,208,
-209,222,231,232,234,236,240,242,243,246,
-248,253,257,261,263,267,268,271,272,274,
-283,289,292,296,305
+6,7,12,22,35,37,42,48,50,59,
+68,69,71,73,74,79,82,95,102,103,
+104,106,109,119,120,122,123,126,127,134,
+147,150,156,158,160,161,163,165,170,176,
+179,187,192,194,207,217,219,220,222,223,
+224,229,236,244,253,255,257,262,263,265,
+273,284,289,291,296,297,298,300,301,303,
+304
      \endverbatim
      </li>
-     <li> n=623:
+     <li> n=681:
      \verbatim
-7,14,18,19,24,34,37,45,50,52,
-57,58,59,61,64,65,75,77,78,82,
-87,94,105,113,115,116,118,120,123,125,
-131,142,149,154,158,159,161,171,172,175,
-177,178,179,184,186,191,199,202,208,210,
-217,218,231,239,244,246,252,253,256,258,
-259,268,271,272,274,275,276,283,296,299,
-305,307,309,310,312
+1,11,24,27,33,40,41,43,44,54,
+61,65,71,75,82,88,89,93,98,112,
+115,121,122,124,126,132,133,138,143,147,
+157,158,161,162,168,170,172,173,179,182,
+186,196,205,212,219,223,227,229,230,233,
+238,240,250,253,254,261,267,270,279,292,
+293,302,306,309,315,316,318,320,324,326,
+327,330,331,332,335,337,341
      \endverbatim
     </ol>
    </li>
    <li> We need to find the best algorithm from the ubcsat-1-2-0 suite. </li>
    <li> For now we consider adaptg2wsat as best (it was determined as best
    for vdw_2^pd(5,8) in VanderWaerdenProblems/plans/5-k/general.hpp. </li>
-   <li>  "RunPdVdWk1k2 4 14 adaptg2wsat 100 1000000" yields
+   <li> "RunPdVdWk1k2 4 14 adaptg2wsat 100 1000000" yields
    \verbatim
 Break point 1: 611
 Break point 2: 624
    \endverbatim
-   finding all solutions in the first round with the old solution.
    </li>
+   <li> "RunPdVdWk1k2 4 14 adaptg2wsat 100 10000000" yields
+   \verbatim
+Break point 1: 617
+Break point 2: 682
+   \endverbatim
+   finding all solutions within the first 31 runs. </li>
   </ul>
 
 */
