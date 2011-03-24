d7289e6b75a5d9c9aff2631d92c0ce33e11c580a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-03-24 10:15:38 +0000

    Started systematic investigations into shortest transversals and CNFs/DNFs.
    Ticket MG-41: todos "Minimum CNF/DNF representations" and "Overview".

---
+++ Experimentation/Investigations/BooleanFunctions/MinimumRepresentations/plans/general.hpp
@@ -0,0 +1,33 @@
+// Oliver Kullmann, 24.3.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/BooleanFunctions/MinimumRepresentations/plans/general.hpp
+  \brief On investigations into minimum representations of boolean functions
+
+
+  \todo Connections
+  <ul>
+   <li> See Investigations/Transversals/Bounded/plans/general.hpp for
+   investigations into our main method. </li>
+   <li> See
+   ComputerAlgebra/Satisfiability/Lisp/Resolution/PrimeImplicatesImplicants.mac
+   for methods at Maxima level for computing minimum CNF/DNF representations.
+   </li>
+  </ul>
+
+
+  \todo Minimum CNF/DNF representations
+  <ul>
+   <li> We need an overview and links to all investigations related to
+   computing minimum CNFs or DNFs of boolean functions. </li>
+   <li> We need an overview and links to all methods and tools. </li>
+   <li> Likely we need a dedicated submodule. </li>
+  </ul>
+
+*/
+
