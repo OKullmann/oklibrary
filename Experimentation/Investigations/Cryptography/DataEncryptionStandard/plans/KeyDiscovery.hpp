bac521e3f9c671dfaf921c9b9f51ac5a86560d11
Matthew Gwynne <360678@swan.ac.uk>
2011-03-28 17:40:11 +0100

    Added solver times for ArgoSat DES instances.
    minisat-2.2.0 solves 17 unknown bits in 31.77s.

--- Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery.hpp
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery.hpp
@@ -23,6 +23,23 @@ License, or any later version. */
    <li> Their benchmarks only consider 13 <= 64-p <= 34, d.h.,
    30 <= p <= 51, however we can consider the whole range. </li>
    <li> We consider then all translations, and also many solvers. </li>
+   <li> The OKsolver takes at least four hours on any instance (still
+   running). </li>
+   <li> Solving time (ranked best to worst) on gss-X-s100.cnf:
+    <ul>
+     <li> X = 13 : glucose (0.61s), precosat-570.1 (3.5s), minisat-2.2.0
+     (3.72s), cryptominisat (7s), precosat236 (10.2s). </li>
+     <li> X = 14 : precosat-570.1 (2s), minisat-2.2.0 (12.35s), cryptominisat
+     (22.33s), precosat236 (32s), glucose (58.53s). </li>
+     <li> X = 15 : minisat-2.2.0 (5.43s), cryptominisat (10.71s), 
+     precosat-570.1 (44.7s), precosat236 (53.8s), glucose (68.75). </li>
+     <li> X = 16 : minisat-2.2.0 (6.76s), precosat236 (32.1s), precosat-570.1 
+     (53.8s), cryptominisat (104.38s), glucose (133.32s) . </li>
+     <li> X = 17 : minisat-2.2.0 (31.77s), precosat-570.1 (70.8s), 
+     precosat236 (109.8s), cryptominisat (144.73s), glucose (896.53s).
+     </li>
+    </ul>
+   </li>
   </ul>
 
 
