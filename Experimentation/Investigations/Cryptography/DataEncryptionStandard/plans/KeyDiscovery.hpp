1ad864c570c260b4d062cd434a9bb40f007ddbf5
Matthew Gwynne <360678@swan.ac.uk>
2011-03-28 16:40:23 +0100

    Added key discovery plans file for DES investigations.

---
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery.hpp
@@ -0,0 +1,31 @@
+// Matthew Gwynne, 27.3.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery.hpp
+  \brief On investigations into the Data Encryption Standard key discovery
+
+
+  \todo Transferring the Argosat-desgen example
+  <ul>
+   <li> Exactly as with argosat-desgen-sat09.tar.bz2, we shall consider
+   key 15FBC08D31B0D521, plaintext 038E596D4841D03B and ciphertext
+   A2FB6032638EC79D, and providing the examples for all 0 <= p <= 64,
+   where the last p keybits are provided (as unit-clauses). </li>
+   <li> This can be easily achieved by using function des_key2fcl(key),
+   where the first 64-p entries of the 64-bit vector key' are replaced by
+   the value "und" (while by hexstr2binv(key) we obtain the key as boolean
+   vector). </li>
+   <li> Their benchmarks only consider 13 <= 64-p <= 34, d.h.,
+   30 <= p <= 51, however we can consider the whole range. </li>
+   <li> We consider then all translations, and also many solvers. </li>
+  </ul>
+
+
+  \todo Move into separate sub-module
+
+*/
