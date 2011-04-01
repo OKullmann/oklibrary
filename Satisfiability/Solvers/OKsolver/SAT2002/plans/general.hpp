da970f6c35daa6260aba609ef7c73bea03b66f7d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-04-01 23:55:04 +0100

    Improved plans on simple parallelisation for the OKsolver_2002.

--- Satisfiability/Solvers/OKsolver/SAT2002/plans/general.hpp
+++ Satisfiability/Solvers/OKsolver/SAT2002/plans/general.hpp
@@ -531,32 +531,61 @@ extern unsigned int Suchbaumtiefe, Ueberschreitung2, init2Klauseln;
    and schedule new jobs if possible (and needed). </li>
    <li> For monitoring-depth 0 one would get the OKsolver_2002 to act
    as preprocessor (applying r_2-reduction and autarky-reduction). </li>
-   <li> To handle a large number of jobs, it must also be possible to
-   just output a certain clause-set from the long list; for example
-   when using a depth D=13, then by specifying a number from 1 to 8192
-   one should get the corresponding clause-set; this actually then
-   didn't need to be output, but could just be processed. </li>
-   <li> However it is less intrusive to just use the above output facilities
-   to output the clause-set obtained and then to stop (for solving the
-   instance then the OKsolver (or any other solver) is called again). </li>
-   <li> A script would then just manage the distribution of the jobs
-   to the available machines. </li>
-   <li> The index (in the above case 1 <= i <= 8192) would be represented
-   in binary, and the bits would specify the decisions; if a decision would
-   be achieved on the way, then nothing needed to be done. </li>
-   <li> Simplest is just to choose the branches according to the path
-   given by i; if a decision is obtained earlier then there is no need
-   to go further. </li>
-   <li> So the simplest mode is just to follow a path given by a binary word,
-   and either output SAT, UNSAT or the (reduced) clause-set finally reached.
+   <li> Installing an additional parameter to just compute one node:
     <ol>
-     <li> Say the parameter would be "-P=word" (--path=word), where "word"
-     is a string of 1's and 2's (first branch or second branch). </li>
-     <li> Branchings with just one successor are ignored here. </li>
-     <li> If an output flag is set, then the final clause-set is output
-     to a file (if no decision was reached). </li>
-     <li> The program is exit then (using the standard exit, thus printing
-     the statistics line). </li>
+     <li> To handle a large number of jobs, it must also be possible to
+     just output a certain clause-set from the long list; for example
+     when using a depth D=13, then by specifying a number from 1 to 8192
+     one should get the corresponding clause-set; this actually then
+     didn't need to be output, but could just be processed. </li>
+     <li> However it is less intrusive to just use the above output facilities
+     to output the clause-set obtained and then to stop (for solving the
+     instance then the OKsolver (or any other solver) is called again). </li>
+     <li> A script would then just manage the distribution of the jobs
+     to the available machines. </li>
+     <li> The index (in the above case 1 <= i <= 8192) would be represented
+     in binary, and the bits would specify the decisions; if a decision would
+     be achieved on the way, then nothing needed to be done. </li>
+     <li> Simplest is just to choose the branches according to the path
+     given by i; if a decision is obtained earlier then there is no need
+     to go further. </li>
+     <li> So the simplest mode is just to follow a path given by a binary word,
+     and either output SAT, UNSAT or the (reduced) clause-set finally reached.
+      <ol>
+       <li> Say the parameter would be "-P=word" (--path=word), where "word"
+       is a string of 1's and 2's (first branch or second branch). </li>
+       <li> Or perhaps better "-P=i" for 0 <= i < 2^D, with 0 meaning "left",
+       and the higher the power-order the close to the root. </li>
+       <li> Or using the enumeration indices 1 <= i <= 2^D. </li>
+       <li> Whenever a decision for branching is needed, then this path is used
+       as oracle. And the computation is stopped the first time the depth
+       is reached. </li>
+       <li> Then one also needed to output the index of the possible next
+       branch (to continue). </li>
+       <li> Perhaps the list of all indices (with jumps!) should be
+       computed at the beginning, so that one doesn't need to process the
+       branches from left to right, but can use for example a random order.
+       </li>
+       <li> This output to file "filename_OKs_indexlist", with one index
+       per line. </li>
+       <li> Printing out the total number of indices (if it is 2^D, then we
+       just have all of them). </li>
+       <li> Branchings with just one successor are ignored here. </li>
+       <li> If an output flag is set, then the final clause-set is output
+       to a file (if no decision was reached). </li>
+       <li> Alternatively, not the final clause-set but the assignment leading
+       to it, in form of unit-clauses, is output. This would minimise the
+       communication. </li>
+       <li> Just at the given depth the current assignment needs to be output.
+       </li>
+       <li> Perhaps to a file, with a standardised name, say
+       "filename_OKs_i" for the index i. Should this output be in Dimacs
+       format, that is, including the parameter line? Should n then be the
+       attained maximum, or the original n? Let's say the original n. </li>
+       <li> The program is exit then (using the standard exit, thus printing
+       the statistics line). </li>
+      </ol>
+     </li>
     </ol>
    </li>
    <li> In order to see what really needs to be done, for a given depth
