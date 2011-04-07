179751213b3db752f1b15ff9e22ae8939baf6f7b
Matthew Gwynne <360678@swan.ac.uk>
2011-04-07 15:42:38 +0100

    Added todo on combining 6-to-1 bit DES Sboxes.

--- ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/general.hpp
+++ ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/general.hpp
@@ -9,6 +9,63 @@ License, or any later version. */
   \file ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/plans/general.hpp
   \brief Plans for the cryptanalysis of the Data Encryption Standard in Maxima/Lisp
 
+ 
+  \todo Combining 6-to-1 bit Sbox functions
+  <ul>
+   <li> Consider the 4 boolean 6 x 1 functions for each DES Sbox. </li>
+   <li> We have functions output_dessbox_bit_fulldnf_stdname and 
+   output_dessbox_bit_fullcnf_stdname to compute the these. </li>
+   <li> We can compute other representations of these functions
+   (minimum, canonical, r_1-base etc). </li>
+   <li> Whatever the representation, these clause-sets need to be
+   recombined to derive a new representation of each Sbox. </li>
+   <li> Therefore we need convenience functions to handle this
+   recombination. </li>
+   <li> An initial attempt:
+   \verbatim
+bit_sboxes2sbox(FF1,FF2,FF3,FF4) := block(
+  [num_aux_vars : 0, FF1_renamed, FF2_renamed, FF3_renamed, FF4_renamed],
+  FF1_renamed :
+    rename_fcl(
+      FF1,
+      append(
+        [1,2,3,4,5,6,7],
+        create_list(desaux_var(num_aux_vars+j),j,1,length(FF1[1])-7))),
+  num_aux_vars : num_aux_vars + length(FF1[1])-7,
+  FF2_renamed : 
+    rename_fcl(
+      FF2,
+      append(
+        [1,2,3,4,5,6,8],
+        create_list(desaux_var(num_aux_vars+j),j,1,length(FF2[1])-7))),
+  num_aux_vars : num_aux_vars + length(FF2[1])-7,
+  FF3_renamed :
+    rename_fcl(
+      FF3,
+      append(
+        [1,2,3,4,5,6,9],
+        create_list(desaux_var(num_aux_vars+j),j,1,length(FF3[1])-7))),
+  num_aux_vars : num_aux_vars + length(FF3[1])-7,
+  FF4_renamed :
+    rename_fcl(
+      FF4,
+      append(
+        [1,2,3,4,5,6,10],
+        create_list(desaux_var(num_aux_vars+j),j,1,length(FF4[1])-7))),
+  lreduce(
+    lambda([FF1,FF2],
+      [stable_unique(append(FF1[1],FF2[1])),
+       stable_unique(append(FF1[2],FF2[2]))]),
+    [FF1_renamed,FF2_renamed,FF3_renamed,FF4_renamed]))$
+   \endverbatim
+   </li>
+   <li> Is there some general function here? </li>
+   <li> Perhaps we could ask for the number of input, output and auxilliary
+   bits? </li>
+   <li> Then we assume the variables are in the order, input, output variable,
+   auxilliary variables. </li>
+  </ul>
+
 
   \todo Add variants with reduced number of rounds
   <ul>
