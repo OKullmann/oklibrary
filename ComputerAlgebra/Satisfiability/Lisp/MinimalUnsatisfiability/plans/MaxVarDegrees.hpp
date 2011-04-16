267edc5acd49107458454c754ddd75841d621f8c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-04-16 04:53:37 +0100

    Editorial updates (now using "max", not "small").

--- ComputerAlgebra/Satisfiability/Lisp/MinimalUnsatisfiability/plans/MaxVarDegrees.hpp
+++ ComputerAlgebra/Satisfiability/Lisp/MinimalUnsatisfiability/plans/MaxVarDegrees.hpp
@@ -6,17 +6,31 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*!
-  \file ComputerAlgebra/Satisfiability/Lisp/MinimalUnsatisfiability/plans/SmallVariableDegrees.hpp
-  \brief Plans related to the determination of the smallest possible variable degrees of uniform minimally unsatisfiable clause-sets
+  \file ComputerAlgebra/Satisfiability/Lisp/MinimalUnsatisfiability/plans/MaxVarDegrees.hpp
+  \brief Plans related to the determination of the maximal variable degrees of (typically uniform) minimally unsatisfiable clause-sets
 
 
   \todo Minimal variable-degree of uniform minimally unsatisfiable clause-sets
   <ul>
-   <li> Let minvardeg_umu(k) for k >= 0 be the minimal possible (maximal)
-   variable-degree of (minimally) unsatisfiable k-uniform boolean
-   clause-sets F. </li>
-   <li> We need to implement minvardeg_umu(k) so that it yields the currently
-   known lower and upper bounds.
+   <li> The basic notions:
+    <ol>
+     <li> We have max_variable_degree_cs. </li>
+     <li> This is to be minimised for given k over all uniform minimally
+     unsatisfiable clause-sets which are k-uniform. </li>
+     <li> The function yielding precise values and bounds shall be called
+     "maxvardegree_umu" ("umu" for "uniform minimally unsatisfiable"). </li>
+     <li> That is, uniformcsp(F) and min_unsat_bydef(F) and max_rank_cs(F)=k
+     => max_variable_degree_cs(F) >= maxvardegree_umu(k). </li>
+     <li> Better, maxvardegree_umu(k) should be a number, iff the precise
+     value is known, and otherwise a pair [lower_bound, upper_bound]. </li>
+     <li> DONE (we speak of "maxvardegree" now)
+     Let minvardeg_umu(k) for k >= 0 be the minimal possible (maximal)
+     variable-degree of (minimally) unsatisfiable k-uniform boolean
+     clause-sets F. </li>
+    </ol>
+   </li>
+   <li> We need to implement maxvardegree_umu(k) so that it yields the
+   currently known lower and upper bounds.
     <ol>
      <li> We need a table with the best known values. </li>
      <li> Then there are several functions for computing upper bounds, which
@@ -61,14 +75,14 @@ License, or any later version. */
    for the background of the general translations. </li>
    <li> Let m(k) be the minimal variable-degree s for a k-uniform clause-set
    F_v, which forces v to be false and where v has variable-degree at most s-1.
-   Then we have minvardeg_umu(k) <= m(k), as follows by the satisfiability-
+   Then we have maxvardegree_umu(k) <= m(k), as follows by the satisfiability-
    equivalence of the general transformation (pumping up binary clauses by
    such a helper clause-set). </li>
-   <li> Perhaps m(k) is easier to determine than minvardeg_umu(k). Or do we
-   have minvardeg_umu(k) = m(k) ??? </li>
+   <li> Perhaps m(k) is easier to determine than maxvardegree_umu(k). Or do we
+   have maxvardegree_umu(k) = m(k) ??? </li>
    <li> In [Tovey, 1984, DAM] such an F_v for k=3, s=4 is given, which is
    implemented as "tovey84" in
-   Satisfiability/Lisp/MinimalUnsatisfiability/SmallVariableDegrees.mac
+   Satisfiability/Lisp/MinimalUnsatisfiability/MinVariableDegrees.mac
    </li>
    <li> Due to m(3) <= 4 we get m(k) <= 2^(k-1) for all k >= 3, simply by
    expanding the above F_v to higher clause-length using inverse 2-subsumption
@@ -83,10 +97,10 @@ License, or any later version. */
    which we should implement. </li>
    <li> Then (page 527) the two special families from Sections 2, 3 are
    derived; again we should implement this. </li>
-   <li> Let minvardeg_umu_hs(k) be the minimal (maximal) variable-degree
+   <li> Let maxvardegree_umu_hs(k) be the minimal (maximal) variable-degree
    obtainable by this construction (in the article called "f_2(k) + 1"). </li>
    <li> At the end of Section 4 a simple algorithm is presented for computing
-   minvardeg_umu_hs(k), which we should implement. </li>
+   maxvardegree_umu_hs(k), which we should implement. </li>
    <li> Likely the formulas constructed are minimally unsatisfiable? </li>
    <li> What are their parameters (deficiency, number of variables)? </li>
    <li> How regular are these formulas? </li>
