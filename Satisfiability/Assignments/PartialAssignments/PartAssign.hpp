9485a262dc0964ac1db82ebf6e4301b0679314bf
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-01 20:35:33 +0100

    Editorial compactifications and updates.

--- Satisfiability/Assignments/PartialAssignments/PartAssign.hpp
+++ Satisfiability/Assignments/PartialAssignments/PartAssign.hpp
@@ -29,98 +29,68 @@ namespace PartAssignments {
   using namespace Clausesets;
 
   namespace Error {
-
     struct not_in_domain {};
     struct inconsistent_extension {};
-
   }
 
-  class Pass {
-
-  public :
-
+  struct Pass {
     Pass () {}
     Pass (Lit);
-    Pass (const Cl&);
-
+    Pass (const Cl&)
     bool is_empty() const;
-
     set_length n() const;
-
     std::set<Var> var() const;
-
     bool in_domain(Var) const;
     bool val(Var) const;
-
     Pass& composition_left(const Pass&);
     // Attention: the argument is composed from the *left*
-
     Pass& restrict(const std::set<Var>&);
-
     Cl clause() const;
-
-    Cls operator * (const Cls&) const;
-
+    Cls operator* (const Cls&) const;
     std::map<Var, bool> pa;
-
   };
 
   inline Pass::Pass(const Lit x) {
     pa = std::map<Var, bool>();
     pa[x.var()] = x.val();
   }
-
   inline Pass::Pass (const Cl& C) {
     pa = std::map<Var, bool>();
-    for (std::set<Lit>::const_iterator i = C.ls.begin(); i != C.ls.end(); i++)
+    for (std::set<Lit>::const_iterator i = C.ls.begin(); i != C.ls.end(); ++i)
       pa[i -> var()] = i -> val();
   }
-
-  inline bool Pass::is_empty() const {
-    return pa.empty();
-  }
-
+  inline bool Pass::is_empty() const { return pa.empty(); }
   inline std::set<Var> Pass::var() const {
     std::set<Var> V;
-    for (std::map<Var,bool>::const_iterator i = pa.begin(); i != pa.end(); i++)
+    for (std::map<Var,bool>::const_iterator i = pa.begin(); i != pa.end(); ++i)
       V.insert(i -> first);
     return V;
   }
-
-  inline set_length Pass::n() const {
-    return pa.size();
-  }
-
+  inline set_length Pass::n() const { return pa.size(); }
   inline bool Pass::in_domain(const Var v) const {
     return pa.find(v) != pa.end();
   }
-
   inline bool Pass::val(const Var v) const {
     std::map<Var,bool>::const_iterator p = pa.find(v);
-    if (p == pa.end())
-      throw Error::not_in_domain();
+    if (p == pa.end()) throw Error::not_in_domain();
     return p -> second;
   }
-
   inline Pass& Pass::composition_left(const Pass& phi) {
     pa.insert(phi.pa.begin(), phi.pa.end());
     return *this;
   }
-
   inline Pass& Pass::restrict(const std::set<Var>& V) {
-    for (std::set<Var>::const_iterator i = V.begin(); i != V.end(); i++)
+    for (std::set<Var>::const_iterator i = V.begin(); i != V.end(); ++i)
       pa.erase(*i);
     return *this;
   }
-
   inline Cl Pass::clause() const {
     Cl C;
     for (std::map<Var,bool>::const_iterator i = pa.begin(); i != pa.end(); i++)
       C.ls.insert(Lit(i -> first, i -> second));
     return C;
   }
-
-  inline Cls Pass::operator * (const Cls& F) const {
+  inline Cls Pass::operator *(const Cls& F) const {
     Cls G;
     for (std::set<Cl>::iterator i = F.cls.begin(); i != F.cls.end(); i++)
       if ((*i & this -> clause().comp()).is_empty())
