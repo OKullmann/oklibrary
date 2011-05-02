45137d7a77e482dd5d8eb210601e4d4075d7e8bb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-02 02:46:29 +0100

    Removed namespace-directives, and provided all definitions in-class.

--- Satisfiability/ProblemInstances/Clauses/Clause.hpp
+++ Satisfiability/ProblemInstances/Clauses/Clause.hpp
@@ -25,15 +25,12 @@ License, or any later version. */
 
 namespace Clauses {
 
-  using namespace Variables;
-  using namespace Literals;
-
   typedef size_t size_type; // should by "size_type"
   const size_type max_length = std::numeric_limits<size_type>::max();
 
-  inline const std::set<Var> operator +(const std::set<Var>& A, const std::set<Var>& B) {
-    std::set<Var> result;
-    std::insert_iterator< std::set<Var> > res_ins(result, result.begin());
+  inline const std::set<Variables::Var> operator +(const std::set<Variables::Var>& A, const std::set<Variables::Var>& B) {
+    std::set<Variables::Var> result;
+    std::insert_iterator<std::set<Variables::Var> > res_ins(result, result.begin());
     std::set_union(A.begin(), A.end(), B.begin(), B.end(), res_ins);
     return result;
   }
@@ -41,133 +38,121 @@ namespace Clauses {
   
   class Litset {
   public :
-    std::set<Lit> ls; // should be private
+    typedef Literals::Lit literal_type;
+    typedef std::set<literal_type> litset_t;
   public :
-    Litset () {}
-    Litset (const std::set<Lit>&);
-    bool tautological() const;
-    bool empty() const;
-    size_type size() const;
-    bool contains(Lit) const;
-    Litset comp() const;
-    std::set<Var> var() const;
-    Litset& add(Lit);
-    Litset& remove(Lit);
-    Litset& add_ls(const Litset&);
-    Litset& remove_ls(const Litset&);
-    bool operator == (const Litset&) const;
-    bool operator < (const Litset&) const;
-    const Litset operator + (const Litset&) const;
-    const Litset operator - (const Litset&) const;
-    const Litset operator & (const Litset&) const;
-  };
+    litset_t ls; // should be protected
+  public :
+    typedef Variables::Var var_type;
+    typedef litset_t::const_iterator iterator;
+    typedef iterator const_iterator;
+    typedef std::set<var_type> varset_type;
 
-  inline Litset::Litset(const std::set<Lit>& L) { ls = std::set<Lit>(L); }
-  inline bool Litset::tautological() const {
+    Litset() {}
+    Litset (const litset_t& L) : ls(L) {}
+
     // uses the special order of literals
-    std::set<Lit>::iterator a0 = ls.begin(), a = a0;
-    if (a0 == ls.end()) return false;
-    ++a;
-    while (a != ls.end()) {
-      if (a -> comp() == *a0) return true;
-      ++a; ++a0;
+    bool tautological() const {
+      iterator a0 = ls.begin(), a = a0;
+      const iterator ls_end = ls.end();
+      if (a0 == ls_end) return false;
+      ++a;
+      while (a != ls.end()) {
+        if (a->comp() == *a0) return true;
+        ++a; ++a0;
+      }
+      return false;
     }
-    return false;
-  }
-  inline bool Litset::empty() const { return ls.empty(); }
-  inline size_type Litset::size() const { return ls.size(); }
-  inline bool Litset::contains(const Lit x) const {
-    return ! (ls.find(x) == ls.end());
-  }
-  inline Lit comp(const Lit x) {
-    return x.comp();
-  }
-  inline Litset Litset::comp() const {
-    std::set<Lit> result;
-    std::insert_iterator< std::set<Lit> > res_ins(result, result.begin());
-    transform(ls.begin(), ls.end(), res_ins, Clauses::comp);
-    return Litset(result);
-  }
-  inline Var var(const Lit x) { return x.var(); }
-  inline std::set<Var> Litset::var() const {
-    std::set<Var> result;
-    std::insert_iterator< std::set<Var> > res_ins(result, result.begin());
-    transform(ls.begin(), ls.end(), res_ins, Clauses::var);
-    return result;
-  }
-  inline Litset& Litset::add(const Lit x) {
-    ls.insert(x);
-    return *this;
-  }
-  inline Litset& Litset::remove(const Lit x) {
-    ls.erase(x);
-    return *this;
-  }
-  inline Litset& Litset::add_ls(const Litset& L) {
-    ls.insert(L.ls.begin(), L.ls.end());
-    return *this;
-  }
-  inline Litset& Litset::remove_ls(const Litset& L) {
-    ls.erase(L.ls.begin(), L.ls.end());
-    return *this;
-  }
-  inline bool Litset::operator ==(const Litset& L) const { return ls == L.ls; }
-  inline bool Litset::operator <(const Litset& L) const { return ls < L.ls; }
-  inline const Litset Litset::operator + (const Litset& L) const {
-    std::set<Lit> result;
-    std::insert_iterator< std::set<Lit> > res_ins(result, result.begin());
-    std::set_union(ls.begin(), ls.end(), L.ls.begin(), L.ls.end(), res_ins);
-    return Litset(result);
-  }
-  inline const Litset Litset::operator -(const Litset& L) const {
-    std::set<Lit> result;
-    std::insert_iterator< std::set<Lit> > res_ins(result, result.begin());
-    std::set_difference(ls.begin(), ls.end(), L.ls.begin(), L.ls.end(), res_ins);
-    return Litset(result);
-  }
-  inline const Litset Litset::operator &(const Litset& L) const {
-    std::set<Lit> result;
-    std::insert_iterator< std::set<Lit> > res_ins(result, result.begin());
-    std::set_intersection(ls.begin(), ls.end(), L.ls.begin(), L.ls.end(), res_ins);
-    return Litset(result);
-  }
+
+    bool empty() const { return ls.empty(); }
+    size_type size() const { return ls.size(); }
+    bool contains(const literal_type x) const {
+      return ! (ls.find(x) == ls.end());
+    }
+
+    static literal_type comp_(const literal_type x) { return x.comp(); }
+    Litset comp() const {
+      litset_t result;
+      std::insert_iterator<litset_t> res_ins(result, result.begin());
+      transform(ls.begin(), ls.end(), res_ins, comp_);
+      return result;
+    }
+
+    static var_type var_(const literal_type x) { return x.var(); }
+    varset_type var() const {
+      varset_type result;
+      std::insert_iterator<varset_type> res_ins(result, result.begin());
+      transform(ls.begin(), ls.end(), res_ins, var_);
+      return result;
+    }
+
+    Litset& add(const literal_type x) { ls.insert(x); return *this; }
+    Litset& remove(const literal_type x) { ls.erase(x); return *this; }
+    Litset& add_ls(const Litset& L) {
+      ls.insert(L.ls.begin(), L.ls.end());
+      return *this;
+    }
+    Litset& remove_ls(const Litset& L) {
+      ls.erase(L.ls.begin(), L.ls.end());
+      return *this;
+    }
+
+    friend bool operator ==(const Litset& lhs, const Litset& rhs) {
+      return lhs.ls == rhs.ls;
+    }
+    friend bool operator <(const Litset& lhs, const Litset& rhs) {
+      return lhs.ls < rhs.ls;
+    }
+
+    friend Litset operator +(const Litset& lhs, const Litset& rhs) {
+      litset_t result;
+      std::insert_iterator<litset_t> res_ins(result, result.begin());
+      std::set_union(lhs.ls.begin(), lhs.ls.end(), rhs.ls.begin(), rhs.ls.end(), res_ins);
+      return result;
+    }
+    friend Litset operator -(const Litset& lhs, const Litset& rhs) {
+      litset_t result;
+      std::insert_iterator<litset_t> res_ins(result, result.begin());
+      std::set_difference(lhs.ls.begin(), lhs.ls.end(), rhs.ls.begin(), rhs.ls.end(), res_ins);
+      return result;
+    }
+    friend Litset operator &(const Litset& lhs, const Litset& rhs) {
+      litset_t result;
+      std::insert_iterator<litset_t> res_ins(result, result.begin());
+      std::set_intersection(lhs.ls.begin(), lhs.ls.end(), rhs.ls.begin(), rhs.ls.end(), res_ins);
+      return result;
+    }
+  };
 
 
   namespace Error {
     struct not_a_clause {};
   }
 
-
   struct Cl : Litset {
-    // for objects x of type Cl we must have ! x.tautological()
-    Cl () {};
-    Cl (const std::set<Lit>&);
-    Cl (const Litset&);
-    Cl& add(Lit);
-    const Cl operator + (const Cl&) const;
+    Cl() {}
+    Cl(const litset_t& L) : Litset(L) {
+      if (this->tautological()) throw Error::not_a_clause();
+    }
+    Cl(const Litset& L) : Litset(L) {
+      if (this->tautological()) throw Error::not_a_clause();
+    }
+
+    Cl& add(const literal_type x) {
+      if (this->contains(x.comp())) throw Error::not_a_clause();
+      this -> add(x);
+      return *this;
+    }
+    friend Cl operator +(const Cl& lhs, const Cl& rhs) {
+      Cl D = Litset(lhs) + Litset(rhs);
+      if (D.tautological()) throw Error::not_a_clause();
+      return D;
+    }
+
   };
 
-  inline Cl::Cl(const std::set<Lit>& L)
-    : Litset(L) {
-    if (this -> Litset::tautological()) throw Error::not_a_clause();
-  }
 
-  inline Cl::Cl(const Litset& L)
-    : Litset(L) {
-    if (this -> Litset::tautological()) throw Error::not_a_clause();
-  }
 
-  inline Cl& Cl::add(const Lit x) {
-    if (this -> contains(x.comp())) throw Error::not_a_clause();
-    this -> Litset::add(x);
-    return *this;
-  }
-
-  inline const Cl Cl::operator +(const Cl& C) const {
-    Cl D = Litset(*this) + Litset(C);
-    if (D.tautological()) throw Error::not_a_clause();
-    return D;
-  }
 
 }
 
