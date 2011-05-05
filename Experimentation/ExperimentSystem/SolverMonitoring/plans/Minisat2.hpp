f3dca8361297b10ae539d3b83ff07ac0d816f590
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-05 12:02:37 +0100

    Todos on improved handling of solver-outputs.
    Ticket MG-51: for improving the handling of filenames, together with
    writing the wrapper scripts.

--- Experimentation/ExperimentSystem/SolverMonitoring/plans/Minisat2.hpp
+++ Experimentation/ExperimentSystem/SolverMonitoring/plans/Minisat2.hpp
@@ -27,6 +27,19 @@ License, or any later version. */
   </ul>
 
 
+  \todo Improved handling of file-names
+  <ul>
+   <li> Typically, filenames contain an important parameter, like
+   "VanDerWaerden_pd_2-6-7_350.cnf_OUT" containing 350, the number of
+   vertices. </li>
+   <li> There should be a stronger version of read_minisat_outputs, which
+   allows to create a mask for extracting such a parameter, storing it as
+   "param" (an integer) in the dataframe. </li>
+   <li> The order of rows then should follow increasing parameter values. </li>
+   <li> This should be realised by the wrapper-script; see below. </li>
+  </ul>
+
+
   \todo Move read_minisat2_output functionality to wrapper script
   <ul>
    <li> See "Move read_picosat_output functionality to wrapper script"
--- Experimentation/ExperimentSystem/SolverMonitoring/plans/Picosat.hpp
+++ Experimentation/ExperimentSystem/SolverMonitoring/plans/Picosat.hpp
@@ -1,5 +1,5 @@
 // Matthew Gwynne, 22.9.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -16,6 +16,17 @@ License, or any later version. */
    should go to a wrapper script, such as "picosat-okl", which
    causes picosat to output it's results in a form readable by
    read.table in R. </li>
+   <li> Better however is to write a script, which examines a complete
+   directory of solver-outputs, and creates an R-readable file containing
+   all data, where this file is stored in the directory. </li>
+   <li> This is better since then we have the original output as well as
+   the extracted data. And it leaves the look-and-feel of the solver intact.
+   </li>
+   <li> Remark: with the Ubcsat-wrapper it's different, since there we only
+   need to supply default parameter (which can also be overwritten if needed).
+   </li>
+   <li> See "Improved handling of file-names" in
+   ExperimentSystem/SolverMonitoring/plans/Minisat2.hpp. </li>
   </ul>
 
 */
