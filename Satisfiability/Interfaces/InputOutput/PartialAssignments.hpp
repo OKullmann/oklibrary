7839d82e90930b4a12b3e212391e2691b3b5b004
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-11 01:48:26 +0100

    Started with component for input of partial assignments.

---
+++ Satisfiability/Interfaces/InputOutput/PartialAssignments.hpp
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 11.5.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file OKlib/Satisfiability/Interfaces/InputOutput/PartialAssignments.hpp
+  \brief Input and output of partial assignments
+
+*/
+
+
+#ifndef PARTIALASSIGNMENTS_hB32wQwqs12
+#define PARTIALASSIGNMENTS
+
+namespace OKlib {
+  namespace Satisfiability {
+    namespace Interfaces {
+      namespace InputOutput {
+
+        
+      }
+    }
+  }
+}
+
+#endif
