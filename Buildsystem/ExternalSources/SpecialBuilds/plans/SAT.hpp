ebdd3b7b018159f1cfcc8a37e984aa48e29d05d0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-14 21:40:50 +0100

    Partial update on Minisat installation information.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/SAT.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/SAT.hpp
@@ -168,6 +168,7 @@ c CPU time              : 0 s
   <ul>
    <li> Mention the patched makefile regarding minisat+, and the usage of the
    locally provided GMP. </li>
+   <li> Explain the different versions. </li>
    <li> See "Minisat" below. </li>
   </ul>
 
