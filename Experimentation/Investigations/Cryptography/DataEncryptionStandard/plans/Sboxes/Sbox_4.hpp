94ea98d74668d87cdc2af0425f65e1119e5bfbc0
Matthew Gwynne <360678@swan.ac.uk>
2011-05-18 10:01:25 +0100

    Added small CNF for DES Sbox four (mincl_rinf <= 69).

--- Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/Sboxes/Sbox_4.hpp
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/Sboxes/Sbox_4.hpp
@@ -31,6 +31,32 @@ License, or any later version. */
      bf2relation_fullcnf_fcs(des_sbox_bf(4),6). </li>
     </ol>
    </li>
+   <li> The minimum CNF representation has at most 69 clauses. See
+   "Using weighted MaxSAT to compute small CNFs". </li>
+  </ul>
+
+
+  \todo Using weighted MaxSAT to compute small CNFs (mincl_rinf <= 69)
+  <ul>
+   <li> Computing the weighted MaxSAT problem:
+   \verbatim
+shell> QuineMcCluskeySubsumptionHypergraph-n16-O3-DNDEBUG DES_Sbox_4_fullCNF.cnf > DES_Sbox_4_shg.cnf
+shell> cat DES_Sbox_4_shg.cnf | MinOnes2WeightedMaxSAT-O3-DNDEBUG > DES_Sbox_4_shg.wcnf
+   \endverbatim
+   </li>
+   <li> Running then:
+   \verbatim
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 69 -solve 1 -seed 3808694681 -i DES_Sbox_4_shg.wcnf -r model DES_Sbox_4_s69.ass;
+shell> cat DES_Sbox_4_fullCNF.cnf_primes | FilterDimacs DES_Sbox_4_s69.ass > DES_Sbox_4_s69.cnf
+shell> cat DES_Sbox_4_s69.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG n
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+10 68 382 0 382 1 1
+ length count
+5 28
+6 38
+7 2
+   \endverbatim
+   </li>
   </ul>
 
 
@@ -44,10 +70,11 @@ shell> RUcpGen-O3-DNDEBUG DES_Sbox_4_sortedpi.cnf > DES_Sbox_4_gen.cnf
 shell> RandomShuffleDimacs-O3-DNDEBUG 4 < DES_Sbox_4_gen.cnf | SortByClauseLengthDescending-O3-DNDEBUG | RUcpBase-O3-DNDEBUG > DES_Sbox_4_1base.cnf
 shell> cat DES_Sbox_4_1base.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG n
  n non_taut_c red_l taut_c orig_l comment_count finished_bool
-10 128 699 0 699 0 1
+10 69 387 0 387 1 1
  length count
-5 69
-6 59
+5 29
+6 38
+7 2
    \endverbatim
    </li>
   </ul>
