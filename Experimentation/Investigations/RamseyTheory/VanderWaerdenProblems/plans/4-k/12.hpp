9d64994faf6c5b9acc8c024d55ca73426ef08d91
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-20 10:14:37 +0100

    Started computing vdw_2^pd(4,12).

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/12.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/12.hpp
@@ -48,6 +48,94 @@ Memory used           : 276.00 MB
 12 3202
      \endverbatim
      </li>
+     <li> Now using SplittingViaOKsolver. </li>
+     <li> First considering n=388. </li>
+     <li> First finding the n-parameter:
+     \verbatim
+> SplittingViaOKsolver -D10 -SN VanDerWaerden_pd_2-4-12_388.cnf
+10 11 12 13
+47 27 13  4
+> SplittingViaOKsolver -D15 -SN VanDerWaerden_pd_2-4-12_388.cnf
+ 15  16  17  18  19  20  21
+310 159 114  59  18   2   3
+> SplittingViaOKsolver -D20 -SN VanDerWaerden_pd_2-4-12_388.cnf
+  20   21   22   23   24   25   26   27   28   29   30
+2029  879  673  453  234   90   35   18    4    2    2
+> SplittingViaOKsolver -D25 -SN VanDerWaerden_pd_2-4-12_388.cnf
+   25    26    27    28    29    30    31    32    33    34    35    36    37
+10850  4618  4097  3203  2125  1154   503   255   113    43    34    13     9
+   38    39    40
+    3     4     2
+ E=read.table("Data")
+> E[E$n==25,][1:10,]
+       i  n
+16177  1 25
+16178  3 25
+16179  4 25
+16180  6 25
+16181  8 25
+16182 13 25
+16183 15 25
+16184 16 25
+16185 19 25
+16186 21 25
+> cat VanDerWaerden_pd_2-4-12_388.cnf | ApplyPass-O3-DNDEBUG Instances/1 > I1.cnf
+> minisat-2.2.0 I1.cnf
+conflicts             : 857041         (27774 /sec)
+CPU time              : 30.8573 s
+UNSATISFIABLE
+> minisat-2.2.0 I21.cnf
+conflicts             : 286648         (29254 /sec)
+CPU time              : 9.79851 s
+UNSATISFIABLE
+
+> SplittingViaOKsolver -D30 -SN VanDerWaerden_pd_2-4-12_388.cnf
+> cd SplitViaOKsolver_D30SNVanDerWaerden_pd_2412_388cnf_2011-05-20-091001
+> more Md5sum
+316fb102dce8874b4512acaecfde9db3
+> more Statistics
+> E=read.table("Data")
+> summary(E$n)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+  30.00   30.00   31.00   31.99   33.00   70.00
+> table(E$n)
+   30    31    32    33    34    35    36    37    38    39    40    41    42
+44995 21408 19887 17229 12297  7661  4122  2074  1042   554   332   178   120
+   43    44    45    46    47    48    49    50    51    52    53    54    55
+   88    39    32    17     9     7    10    11     4     2     3     2     2
+   58    62    64    65    68    70
+    1     1     1     1     1     1
+> more Result
+s UNKNOWN
+c sat_status                            2
+c initial_maximal_clause_length         12
+c initial_number_of_variables           194
+c initial_number_of_clauses             15544
+c initial_number_of_literal_occurrences 87698
+c number_of_initial_unit-eliminations   0
+c reddiff_maximal_clause_length         0
+c reddiff_number_of_variables           0
+c reddiff_number_of_clauses             0
+c reddiff_number_of_literal_occurrences 0
+c number_of_2-clauses_after_reduction   65
+c running_time(sec)                     2063.5
+c number_of_nodes                       264275
+c number_of_single_nodes                0
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                9332
+c number_of_pure_literals               0
+c number_of_autarkies                   0
+c number_of_missed_single_nodes         0
+c max_tree_depth                        27
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 0
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             VanDerWaerden_pd_2-4-12_388.cnf
+c splitting_directory                   SplitViaOKsolver_D30SNVanDerWaerden_pd_2412_388cnf_2011-05-20-091001/Instances
+c splitting_cases                       132131
+     \endverbatim
+     So n=30 seems about right. </li>
     </ol>
    </li>
    <li> Do we have an easy-hard pattern based on parity? </li>
