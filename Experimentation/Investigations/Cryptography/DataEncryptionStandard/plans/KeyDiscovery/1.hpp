254d9750e247a3087124e9bbc1f1136975c074ac
Matthew Gwynne <360678@swan.ac.uk>
2011-05-25 16:59:00 +0100

    Added data on the one-round DES using the "minimum" translation.

--- Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/1.hpp
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/1.hpp
@@ -23,9 +23,13 @@ License, or any later version. */
      <li> canonical translation; most (but not all) solvers solve with no
      conflicts. See "Using the canonical translation". </li>
      <li> 1-base translation; most (but not all) solvers solve with no
-     conflicts. See "Using the 1-base translation". OKsolver needs some
+     conflicts. See "Using the 1-base translation". OKsolver_2002 needs some
      backtracking. </li>
+     <li> "minimum" translation; some (but not all) solvers solve with
+     no conflicts. See 'Using the "minimum" translation". OKsolver_2002
+     needs some backtracking. </li>
     </ul>
+    Ordered (roughly) by the number of conflicts needed to solve.
    </li>
   </ul>
 
@@ -46,7 +50,7 @@ F_std : standardise_fcs([F[1],append(F[2],P[2],C[2])])$
 output_fcs_v(
   sconcat("DES ArgoSat comparison over ",rounds," rounds"),
   F_std[1],
-  sconcat("des_argocomp_r",rounds,"_b",unknown_bits,".cnf"),
+  sconcat("des_argocomp_r",rounds,".cnf"),
   F_std[2])$
 print("DONE!");
    \endverbatim
@@ -160,7 +164,7 @@ F_std : standardise_fcs([F[1],append(F[2],P[2],C[2])])$
 output_fcs_v(
   sconcat("DES ArgoSat comparison over ",rounds," rounds"),
   F_std[1],
-  sconcat("des_argocomp_r",rounds,"_b",unknown_bits,".cnf"),
+  sconcat("des_argocomp_r",rounds,".cnf"),
   F_std[2])$
 print("DONE!");
    \endverbatim
@@ -185,6 +189,80 @@ c number_of_autarkies                   8
    \endverbatim
    In this case, everything follows completely by UCP.
    </li>
+  </ul>
+
+
+  \todo Using the "minimum" translation
+  <ul>
+   <li> Generating the "minimum" CNFs for the Sboxes:
+   \verbatim
+maxima> for i : 1 thru 8 do output_dessbox_fullcnf_stdname(i)$
+shell> for i in $(seq 1 8); do
+  QuineMcCluskeySubsumptionHypergraph-n16-O3-DNDEBUG DES_Sbox_${i}_fullCNF.cnf > DES_Sbox_${i}_shg.cnf;
+  cat DES_Sbox_${i}_shg.cnf | MinOnes2WeightedMaxSAT-O3-DNDEBUG > DES_Sbox_${i}_shg.wcnf;
+done
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 2444475534 -i DES_Sbox_1_shg.wcnf -r model DES_Sbox_1.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 2521057446 -i DES_Sbox_2_shg.wcnf -r model DES_Sbox_2.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 68 -solve 1 -seed 3544367510 -i DES_Sbox_3_shg.wcnf -r model DES_Sbox_3.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 69 -solve 1 -seed 3808694681 -i DES_Sbox_4_shg.wcnf -r model DES_Sbox_4.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 1876503362 -i DES_Sbox_5_shg.wcnf -r model DES_Sbox_5.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 66 -solve 1 -seed 68018538 -i DES_Sbox_6_shg.wcnf -r model DES_Sbox_6.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 1856244582 -i DES_Sbox_7_shg.wcnf -r model DES_Sbox_7.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 69 -solve 1 -seed 4223500633 -i DES_Sbox_8_shg.wcnf -r model DES_Sbox_8.ass;
+shell> for i in $(seq 1 8); do
+  cat DES_Sbox_${i}_fullCNF.cnf_primes | FilterDimacs DES_Sbox_${i}.ass > DES_Sbox_${i}_min.cnf;
+done
+   </li>
+   <li> The numbers of clauses in the 1-bases are 124, 129, 138, 128, 134,
+   136, 123, 152 respectively. </li>
+   <li> All the 1-bases used have clauses of sizes 5 and 6, except Sbox 4
+   which has clauses of size 5 and 6 as well as 2 of size 7. </li>
+   <li> Generating the instance:
+   \verbatim
+rounds : 1$
+sbox_fcl_l : create_list(read_fcl_f(sconcat("DES_Sbox_",i,"_min.cnf")), i, 1, 8)$
+P_hex : "038E596D4841D03B"$
+K_hex : "15FBC08D31B0D521"$
+C_hex : des_encryption_hex_gen(rounds, "038E596D4841D03B","15FBC08D31B0D521")$
+P : des_plain2fcl_gen(hexstr2binv(P_hex),rounds)$
+C : des_cipher2fcl_gen(hexstr2binv(C_hex),rounds)$
+F : des2fcl_gen(sbox_fcl_l,rounds)$
+F_std : standardise_fcs([F[1],append(F[2],P[2],C[2])])$
+output_fcs_v(
+  sconcat("DES ArgoSat comparison over ",rounds," rounds"),
+  F_std[1],
+  sconcat("des_argocomp_r",rounds,".cnf"),
+  F_std[2])$
+print("DONE!");
+   \endverbatim
+   </li>
+   <li> Some solvers solve without any conflicts/backtracks (precosat236,
+   precosat-570.1, minisat-2.2.0, minisat2, sat-grasp).
+   </li>
+   <li> Some solvers do take some conflicts/nodes (cryptominisat:1, picosat:3, glucose:2, satz215:1).
+   </li>
+   <li> march_pl yields:
+   \verbatim
+shell> march_pl des_argocomp_r1.cnf
+c main():: nodeCount: 17
+c main():: dead ends in main: 0
+   \endverbatim
+   Does "dead ends" mean number of backtracks?
+   </li>
+   <li> OKsolver_2002 now needs some decisions to solve:
+   \verbatim
+shell> OKsolver_2002-O3-DNDEBUG des_argocomp_r1.cnf
+c running_time(sec)                     0.0
+c number_of_nodes                       9
+c number_of_quasi_single_nodes          1
+c number_of_2-reductions                5
+c number_of_autarkies                   8
+c max_tree_depth                        7
+c number_of_1-autarkies                 22
+   \endverbatim
+   </li>
+   <li> Are we just lucky that some solvers happen to pick the right
+   variables? </li>
   </ul>
 
 */
