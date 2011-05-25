c67264e283370ecc9313b02ecbac33bf65ac5c8b
Matthew Gwynne <360678@swan.ac.uk>
2011-05-25 18:51:56 +0100

    Added data on 4-round DES using the canonical translation.
    Solved by all solvers in <330s.

--- Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/general.hpp
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/general.hpp
@@ -17,6 +17,9 @@ License, or any later version. */
      <li> 1-round DES, see
      Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/1.hpp.
      </li>
+     <li> 4-round DES, see
+     Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/4.hpp.
+     </li>
      <li> 16-round DES with some known key bits, see
      Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/KnownKeyBits.hpp.
      </li>
