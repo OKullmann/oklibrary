f64f155550f64edeb405999707eaed446b593e05
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-05-29 09:15:49 +0100

    Successfully applied SplittingViaOKsolver to vdw_2(3,15) = 218.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/15.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/15.hpp
@@ -90,6 +90,84 @@ satz215 VanDerWaerden_2-3-15_218.cnf 99407.850 66642611 33796681 10812633494 373
   </ul>
 
 
+  \todo Splitting via OKsolver
+  <ul>
+   <li> Starting with n=26:
+   \verbatim
+> SplittingViaOKsolver -D26 -SN VanDerWaerden_2-3-15_218.cnf
+  26   27   28   29   30   31   32   33   34   35   36   37   38   39   40   41
+1202  505  641  612  621  535  472  388  268  186   76   23   19   14   17   12
+  42   43   44
+  12   13    3
+> SplittingViaOKsolver -D30 -SN VanDerWaerden_2-3-15_218.cnf
+> cd more Md5sum
+f5c6e03a97f4e218cfa29389a9701569
+> more Statistics
+> E=read.table("Data")
+> summary(E$n)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+  30.00   31.00   34.00   33.99   36.00   52.00
+> table(E$n)
+  30   31   32   33   34   35   36   37   38   39   40   41   42   43   44   45
+2955  926 1023 1100 1131 1344 1276 1134  963  594  249   88   39   30   19   15
+  46   47   48   49   50   51   52
+  10    5   10    6    2    1    2
+> more Result
+s UNKNOWN
+c sat_status                            2
+c initial_maximal_clause_length         15
+c initial_number_of_variables           218
+c initial_number_of_clauses             13362
+c initial_number_of_literal_occurrences 59166
+c number_of_initial_unit-eliminations   0
+c reddiff_maximal_clause_length         0
+c reddiff_number_of_variables           0
+c reddiff_number_of_clauses             0
+c reddiff_number_of_literal_occurrences 0
+c number_of_2-clauses_after_reduction   0
+c running_time(sec)                     294.1
+c number_of_nodes                       25843
+c number_of_single_nodes                0
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                1023
+c number_of_pure_literals               0
+c number_of_autarkies                   0
+c number_of_missed_single_nodes         0
+c max_tree_depth                        26
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 0
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             VanDerWaerden_2-3-15_218.cnf
+c splitting_directory                   SplitViaOKsolver_D30SNVanDerWaerden_2315_218cnf_2011-05-29-010530/Instances
+c splitting_cases                       12922
+
+> cd Instances
+> OKP=~/OKplatform; I="../$(cat ../F)"; echo " i n t sat cfs dec rts r1 mem ptime stime cfl" > Stats; time tail -n +2 ../Data | while read C F N; do cat $I | ApplyPass-O3-DNDEBUG $F Temp.cnf; minisat-2.2.0 Temp.cnf >Temp.out 2>&1; S=$?; if [[ $S != 20 ]]; then echo -e "UNEXPECTED RETURN VALUE ${S}\!"; break; else echo -n "$C " >> Stats; awk -f ${OKP}/OKsystem/OKlib/Experimentation/ExperimentSystem/SolverMonitoring/ExtractMinisat.awk Temp.out >> Stats; echo -n "$C "; fi; done
+real    251m42.474s
+user    217m52.028s
+sys     2m21.823s
+
+# Monitoring in R via
+#> E=read.table("Stats",header=TRUE,colClasses=c(rep("integer",3),"numeric","integer",rep("numeric",8))); plot(E$t); cat(sprintf("%d: %.2fh, sum-cfs=%e, mean-t=%.3fs, mean-cfs=%.0f",length(E$t),sum(E$t)/60/60,sum(E$cfs),mean(E$t),mean(E$cfs)),"\n")
+12922: 3.40h, sum-cfs=3.806417e+08, mean-t=0.947s, mean-cfs=29457 
+   \endverbatim
+   Times on csltok partially false (reduced clock-frequence), but perhaps not
+   too badly. </li>
+   <li> Now we need to test how satz215 as sub-solver does.
+   \verbatim
+SplitViaOKsolver_D30SNVanDerWaerden_2315_218cnf_2011-05-29-010530> tail Data
+12913 12893 30
+> cat VanDerWaerden_2-3-15_218.cnf | ApplyPass-O3-DNDEBUG Instances/12893 I12893.cnf
+> satz215 I12893.cnf
+Aborted after 14m. </li>
+   \endverbatim
+   So it seems that the combination of OKsolver for splitting and
+   conflict-driven solver for solving is the winning combination. </li>
+   </li>
+  </ul>
+
+
   \todo Palindromic numbers
   <ul>
    <li> pdvanderwaerden([3,15])[2] = 205 = vanderwaerden([3,15])-13 . </li>
