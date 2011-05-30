f03a50f7d0f89afbb0d1e74d51170b2ffe213b0a
Matthew Gwynne <360678@swan.ac.uk>
2011-05-30 15:40:05 +0100

    Added plans file for investigations into AES where some key bits are known.

---
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/KnownKeyBits.hpp
@@ -0,0 +1,15 @@
+// Matthew Gwynne, 25.5.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/KnownKeyBits.hpp
+  \brief On investigations into the full Data Encryption Standard key discovery where some key bits are known
+
+
+  \todo Add todos
+
+*/
