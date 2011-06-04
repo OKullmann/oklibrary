adc93d8caa769b8e064ac58f7df52e8e1c484e82
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-06-04 12:44:59 +0100

    Introduced prioritisation of tickets.

---
+++ plans/tickets/general.hpp
@@ -0,0 +1,26 @@
+// Oliver Kullmann, 4.6.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file OKlib/plans/tickets/general.hpp
+  \brief Plans regarding the ticket system
+
+
+  \todo Ranking
+  <ul>
+   <li> The running numbers gives a rough indication on the order of
+   processing the tickets. </li>
+   <li> In the fourth column we can use precise priority-specifications:
+    <ol>
+     <li> "A" means urgent, must be done as the next step. </li>
+     <li> "B" means is also urgent, but might use a bit more time, since
+     also some planning is involved. </li>
+    </ol>
+   </li>
+  </ul>
+
+*/
