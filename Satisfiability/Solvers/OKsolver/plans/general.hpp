e8d1cf716271eb0a198654df101a3e00d967cb90
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-06-04 12:00:29 +0100

    Editorial update.

--- Satisfiability/Solvers/OKsolver/plans/general.hpp
+++ Satisfiability/Solvers/OKsolver/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 18.8.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -20,21 +20,27 @@ License, or any later version. */
   <ul>
    <li> Perhaps the main name is "OKsolver2002" for the old OKsolver,
    OKsolver2009 for the new version etc. </li>
-   <li> Yet we use "OKsolver_2002", but the underscore seems superfluous
-   (and creates problems when writing the name). </li>
+   <li> Naming the old OKsolver:
+    <ol>
+     <li> Yet we use "OKsolver_2002", but the underscore seems superfluous
+     (and creates problems when writing the name). </li>
+     <li> Also "OKsolver-2002" is used. Somehow this looks nicer? </li>
+    </ol>
+   </li>
    <li> A problem then is how to call the re-programmed original
    OKsolver? </li>
    <li> Perhaps this is only an internal version, and so the name doesn't
    matter so much. </li>
    <li> So once the plans for the new OKsolver (currently "OKsolver_2_0")
-   are completed, the year of it first appearance should be estimated (likely
-   2009), and the module "OKsolver2009" (or else) has to be created. </li>
+   are completed, the year of its first appearance should be estimated
+   (perhaps 2013), and the module "OKsolver2013" (or else) has to be created.
+   </li>
    <li> Perhaps then "SAT2002" should be renamed to "OKsolver2002" --- or
-   perhaps we just call the modules "2002", "2009", etc., according to
+   perhaps we just call the modules "2002", "2013", etc., according to
    our strategy to use the full path name? </li>
    <li> Then we get a bit of a problem with the namespaces and their aliases;
-   the modules could be called "Y2002", "Y2009", etc., with aliases
-   "Y02", "Y08". Solvers still "OKsolver2002" etc. </li>
+   the modules could be called "Y2002", "Y2013", etc., with aliases
+   "Y02", "Y13". Solvers still "OKsolver2002" etc. </li>
    <li> Should the versions appear in the name? Better not. </li>
    <li> The version of each OKsolver is the version of its module. </li>
   </ul>
