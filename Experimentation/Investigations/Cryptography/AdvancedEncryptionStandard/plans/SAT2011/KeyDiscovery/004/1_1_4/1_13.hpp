59168acbc963312f1490046b4d8fa193c81faf74
Matthew Gwynne <360678@swan.ac.uk>
2011-06-06 13:49:21 +0100

    Correction: using plaintext-ciphertext pair not plaintext-key pair.

--- Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/004/1_1_4/1_13.hpp
+++ Experimentation/Investigations/Cryptography/AdvancedEncryptionStandard/plans/SAT2011/KeyDiscovery/004/1_1_4/1_13.hpp
@@ -97,7 +97,7 @@ maxima> output_ss_random_pc_pair(seed,num_rounds,num_columns,num_rows,exp,final_
    \endverbatim
    and the merging the assignment with the translation:
    \verbatim
-shell> AppendDimacs-O3-DNDEBUG ssaes_r1_c1_rw1_e4_f0.cnf ssaes_pkpair_r1_c1_rw1_e4_f0_s1.cnf > r1_keyfind.cnf
+shell> AppendDimacs-O3-DNDEBUG ssaes_r1_c1_rw1_e4_f0.cnf ssaes_pcpair_r1_c1_rw1_e4_f0_s1.cnf > r1_keyfind.cnf
    \endverbatim
    </li>
    <li> OKsolver solves this with no decisions:
