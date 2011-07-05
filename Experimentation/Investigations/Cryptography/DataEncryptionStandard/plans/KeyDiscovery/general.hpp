c473cfebde1a756841e61391042ab29136fae116
Matthew Gwynne <360678@swan.ac.uk>
2011-07-05 20:23:31 +0100

    Added todo on adding specific information on translations in the DIMACS files.

--- Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/general.hpp
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/general.hpp
@@ -34,6 +34,33 @@ License, or any later version. */
   </ul>
 
 
+  \todo Add information on specific S-box translations to experiment instances
+  <ul>
+   <li> At present, if one encounters an experiment instance for a DES
+   experiment, then it is not clear exactly how it was produced. </li>
+   <li> The ambiguity occurs because, over time, smaller "minimum" and 1-base
+   representations for the S-boxes are found. </li>
+   <li> Based on these new representations, new translations are generated,
+   and experiments rerun. </li>
+   <li> This leaves one with several copies of the same translation file,
+   and the possibility of uncertainty about which is which. </li>
+   <li> All instance generation code for experiments should include
+   information, via the DIMACS comments of the instance, on the S-box
+   representations used. </li>
+   <li> For example, the seeds given by RandomRUcpBases to generate the
+   1-base for a DES S-box could be output as comment in the DIMACS FILE as:
+   \verbatim
+c DES S-box 1 1-base seeds: 1 (gen) 2 (base).
+   \endverbatim
+   </li>
+   <li> Even better, tools and scripts should be written which take
+   only parameters such as seeds from RandomRUcpBases or ubcsat etc and
+   output the CNFs. </li>
+   <li> Then the actual generation instructions could be added to the CNF
+   file as comments. </li>
+  </ul>
+
+
   \todo DONE Move into separate sub-module
 
 */
