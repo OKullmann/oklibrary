c80f924dc9d0cecb296b3a6f5cb356a36312c10c
Matthew Gwynne <360678@swan.ac.uk>
2011-08-02 13:55:56 +0100

    Adding todo on looking at constraint modelling languages.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
@@ -130,6 +130,7 @@ License, or any later version. */
    In Buildsystem/ExternalSources/SpecialBuilds/plans/CSP.hpp
    the following topics are handled:
     - Overview
+    - Constraint modelling languages
     - Sugar
     - Gecode
     - Minion
