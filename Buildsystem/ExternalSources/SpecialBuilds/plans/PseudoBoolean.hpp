e9666ae4ee34a33f37cc6ab6a514c9a59a3f682a
Matthew Gwynne <360678@swan.ac.uk>
2011-08-04 17:45:15 +0100

    Added todo on installing tools for PB->SAT translation.

--- Buildsystem/ExternalSources/SpecialBuilds/plans/PseudoBoolean.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/PseudoBoolean.hpp
@@ -20,6 +20,33 @@ License, or any later version. */
   </ul>
 
 
+  \todo %Tools for PB -> SAT translation
+  <ul>
+   <li> We need a list of all tools for the conversion of pseudo-boolean
+   problems to SAT. </li>
+   <li> The input format for the pseudo-boolean problems should be the
+   OPB format used at the pseudo-boolean competitions; see
+   http://www.cril.univ-artois.fr/PB11/format.pdf. </li>
+   <li> %Tools:
+    <ul>
+     <li> BoolVar allows generation of SAT instances from a pseudo-boolean
+     problem modelled in Java; see "BoolVar/PB". </li>
+     <li> minisat+ allows the translation of a pseudo-boolean problem to
+     SAT using the option "-cnf=":
+      <ul>
+       <li> minisat+ also offers the ability to customise how constraints
+       are translated (adders, sorting networks, bdds, "mixed" etc) using the
+       options -ca, -cs, -cb and -cm. </li>
+       <li> However, minisat+ doesn't currently support the new OPB format;
+       see "minisat+ input format". </li>
+      </ul>
+     </li>
+    </ul>
+   </li>
+   <li> All tools should be installed in the OKlibrary. </li>
+  </ul>
+
+
   \todo minisat+ input format
   <ul>
    <li> The minisat+ input format is the OPB format from
@@ -80,8 +107,25 @@ License, or any later version. */
   <ul>
    <li> http://arxiv.org/abs/1103.3954 describes a java library for translating
    pseudo-boolean constraints to CNF. </li>
-   <li> Hopefully it can be compiled by gcj, and then we can use the
-   applications created. </li>
+   <li> The website is at http://boolvar.sourceforge.net/. </li>
+   <li> BoolVar is a Java library with which one constructs a pseudo-boolean
+   problem in Java, and then translates this to either SAT or OPB (the
+   pseudo-boolean competition format). </li>
+   <li> The ".jar" file is a collection of class files packaged together.
+   It is already compiled. </li>
+   <li> BoolVar does not support the reading in of OPB files and output
+   of a SAT problem as an application. </li>
+   <li> To convert OPB files to SAT, we would have to write an OPB file
+   parser and then use this to construct a pseudo-boolean problem in
+   the BoolVar system. </li>
+   <li> The source is available at
+   http://sourceforge.net/projects/boolvar/files/ . </li>
+   <li> Hopefully the source can be compiled by gcj, and then
+   we can use the applications created. </li>
+   <li> See "Gcj" in
+   Buildsystem/ExternalSources/SpecialBuilds/plans/PseudoBoolean.hpp. </li>
+   <li> Also see "Supporting Java" in
+   Buildsystem/ExternalSources/SpecialBuilds/plans/general.hpp. </li>
   </ul>
 
 */
--- Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
+++ Buildsystem/ExternalSources/SpecialBuilds/plans/milestones.hpp
@@ -126,6 +126,7 @@ License, or any later version. */
   \par
    In Buildsystem/ExternalSources/SpecialBuilds/plans/PseudoBoolean.hpp
    the following topics are handled:
+    - %Tools for PB -> SAT translation
     - minisat+ input format
     - BoolVar/PB
 
