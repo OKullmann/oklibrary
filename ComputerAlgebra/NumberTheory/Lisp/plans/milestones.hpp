6072161ccf96a428cba19500c39249178c4d5db3
Matthew Gwynne <360678@swan.ac.uk>
2011-08-07 10:47:58 +0100

    *** Initial version 0.1.2 (ComputerAlgebra/NumberTheory/Lisp) ***

---
+++ ComputerAlgebra/NumberTheory/Lisp/plans/milestones.hpp
@@ -0,0 +1,60 @@
+// Matthew Gwynne, 5.8.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file ComputerAlgebra/NumberTheory/Lisp/plans/milestones.hpp
+
+  \module_version ComputerAlgebra/NumberTheory/Lisp 0.1.2 (5.8.2011)
+
+
+  \par Version 0.1.3
+
+  \par
+   In ComputerAlgebra/NumberTheory/Lisp/plans/Auxiliary.hpp the following
+   topics are handled:
+    - Enumerating prime numbers
+
+  \par
+   In ComputerAlgebra/NumberTheory/Lisp/plans/general.hpp the following topics
+   are handled:
+    - Create milestones : DONE
+
+
+  \par Version 0.1.4
+
+  \par
+   In ComputerAlgebra/NumberTheory/Lisp/plans/Auxiliary.hpp the following
+   topics are handled:
+    - Positional (polyadic) representations of numbers
+    - Floors of logarithms
+
+
+  \par Version 0.1.5
+
+  \par
+   In ComputerAlgebra/NumberTheory/Lisp/plans/Quadratics.hpp the following
+   topics are handled:
+    - count_quadintsol
+
+  \par
+   In ComputerAlgebra/NumberTheory/Lisp/plans/PrimeNumbers.hpp the following
+   topics are handled:
+    - count_primes
+
+
+  \par More milestones
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1.2   : 5.8.2011; functions for number representation conversion, prime number ranking and counting, quadratic roots and surreal numbers.
+
+*/
+
