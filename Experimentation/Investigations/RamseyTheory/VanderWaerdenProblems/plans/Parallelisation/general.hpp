ee506d097397d1e8b08b814652b298d9be412cef
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-08-21 10:40:32 +0100

    Updating the example for the use of SplittingViaOKsolver.
    Output is more extensive here, for educational purposes.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/Parallelisation/general.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/Parallelisation/general.hpp
@@ -85,12 +85,12 @@ sys	0m9.289s
    (user-time is sum over all threads) while with 32 threads after 10h of
    total running time still no result was obtained (this might be due to
    scheduling problems). </li>
-   <li> One needs to re-run these experiments when nothing else runs on thes
+   <li> One needs to re-run these experiments when nothing else runs on these
    machine. </li>
   </ul>
-  
-  
-  \todo Simple parallelisation via OKsolver-2002
+
+
+  \todo Parallelisation via OKsolver-2002
   <ul>
    <li> See Solvers/OKsolver/SAT2002/plans/SimpleParallelisation.hpp. </li>
    <li> This tool is ready now, via the helper script
@@ -100,21 +100,20 @@ sys	0m9.289s
    <li> See "vdw_2^pd(5,8) = (312,323)" in
    Investigations/RamseyTheory/VanderWaerdenProblems/plans/5-k/08.hpp for
    another example. </li>
-   <li> The above example, first using the "depth-interpretation" of the
-   D-parameter:
+   <li> The above example:
    \verbatim
-> SplittingViaOKsolver -D4 -SD VanDerWaerden_2-3-12_135.cnf
-> cd SplitViaOKsolver_D4VanDerWaerden_2312_135cnf_2011-05-15-073354
+> SplittingViaOKsolver -D8 VanDerWaerden_2-3-12_135.cnf
+> cd SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-08-21-101819/
 > more Md5sum
-3115296eddcf269616a3f2cce5355eaf
+5edbf75d79e5021292a7a6bd25a2363d
 > more Statistics
 > E=read.table("Data")
 > summary(E$n)
    Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-  4.000   4.000   6.000   7.438  11.000  18.000
+  8.000   8.000   9.000   9.755  11.000  13.000
 > table(E$n)
- 4  6  7 11 18
- 5  5  1  4  1
+ 8  9 10 11 12 13
+21  6  8  6  7  5
 > more Result
 s UNKNOWN
 c sat_status                            2
@@ -128,80 +127,63 @@ c reddiff_number_of_variables           0
 c reddiff_number_of_clauses             0
 c reddiff_number_of_literal_occurrences 0
 c number_of_2-clauses_after_reduction   0
-c running_time(sec)                     0.1
-c number_of_nodes                       31
+c running_time(sec)                     0.2
+c number_of_nodes                       105
 c number_of_single_nodes                0
 c number_of_quasi_single_nodes          0
 c number_of_2-reductions                0
 c number_of_pure_literals               0
 c number_of_autarkies                   0
 c number_of_missed_single_nodes         0
-c max_tree_depth                        4
+c max_tree_depth                        8
 c number_of_table_enlargements          0
 c number_of_1-autarkies                 0
 c number_of_new_2-clauses               0
 c maximal_number_of_added_2-clauses     0
 c file_name                             VanDerWaerden_2-3-12_135.cnf
-c splitting_directory                   SplitViaOKsolver_D4VanDerWaerden_2312_135cnf_2011-05-15-073354/Instances
-c splitting_cases                       16
-
-> cd Instances/
-> time for F in $(awk 'NR!=1 {print $2}' ../Data); do cat ../$(cat ../F) | ApplyPass-O3-DNDEBUG $F Temp.cnf; echo $F $(satz215 Temp.cnf | awk '/terminated/ {print $4}'); done; rm Temp.cnf satz215_timetable
-13 0.320
-7 0.960
-11 0.860
-14 0.830
-15 0.660
-3 3.570
-6 2.560
-8 3.690
-10 3.410
-12 2.910
-16 2.500
-1 39.490
-2 11.990
-4 12.030
-5 12.250
-9 10.490
-
-real    1m49.306s
-user    1m48.887s
-sys     0m0.144s
-     \endverbatim
-   </li>
-   <li> Now using the "n-interpretation":
-   \verbatim
-> SplittingViaOKsolver -D8 VanDerWaerden_2-3-12_135.cnf
-> cd SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-05-30-082907/
-> more Md5sum
-5edbf75d79e5021292a7a6bd25a2363d
-> more Statistics
-> E=read.table("Data")
-> summary(E$n)
-   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-  8.000   8.000   9.000   9.755  11.000  13.000
-> table(E$n)
- 8  9 10 11 12 13
-21  6  8  6  7  5
-> more Result
-c running_time(sec)                     0.2
-c number_of_nodes                       105
-c number_of_2-reductions                0
-c max_tree_depth                        8
+c splitting_directory                   SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-08-21-101819/Instances
 c splitting_cases                       53
 
-> cd Instances
-> OKP=~/OKplatform; I="../$(cat ../F)"; echo " i n t sat cfs dec rts r1 mem ptime stime cfl" > Stats; time tail -n +2 ../Data | while read C F N; do cat $I | ApplyPass-O3-DNDEBUG $F Temp.cnf; minisat-2.2.0 Temp.cnf >Temp.out 2>&1; S=$?; if [[ $S != 20 ]]; then echo -e "UNEXPECTED RETURN VALUE ${S}\!"; break; else echo -n "$C " >> Stats; awk -f ${OKP}/OKsystem/OKlib/Experimentation/ExperimentSystem/SolverMonitoring/ExtractMinisat.awk Temp.out >> Stats; echo -n "$C "; fi; done
-real    0m39.139s
-user    0m38.440s
-sys     0m0.293s
+> cd ..
+> ProcessSplitViaOKsolver SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-08-21-101819/
+> cd Process_SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-08-21-101819_2011-08-21-102046
+> more Result
+UNSATISFIABLE
+  PID TTY      STAT   TIME COMMAND
+25820 pts/48   S+     0:40 /bin/bash /home/kullmann/OKplatform/bin/ProcessSplitViaOKsolver SplitViaOKsolver_D8VanDerWaerden_2312_135cnf_2011-08-21-101819/
+# that is, the whole computation took 0m40s processor time
 
-# Monitoring in R via
-#> E=read.table("Stats",header=TRUE,colClasses=c(rep("integer",3),"numeric","integer",rep("numeric",8))); plot(E$t); cat(sprintf("%d: %.2fh, sum-cfs=%e, mean-t=%.3fs, mean-cfs=%.0f",length(E$t),sum(E$t)/60/60,sum(E$cfs),mean(E$t),mean(E$cfs)),"\n")
-53: 0.01h, sum-cfs=1.990552e+06, mean-t=0.708s, mean-cfs=37558
-
+> oklib --R
+> oklib_load_all()
+> E=read_processsplit_minisat()
+53: 38.38s, sum-cfs=1.990552e+06, mean-t=0.724s, mean-cfs=37558
+$t:
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+0.05799 0.18600 0.45190 0.72420 0.73690 7.79100
+sd= 1.109398
+     95%      96%      97%      98%      99%     100%
+1.759130 1.759650 1.887312 2.038092 4.805432 7.790820
+sum= 38.38314
+$cfs:
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+   3203   10780   25270   37560   37570  366200
+sd= 52672.37
+      95%       96%       97%       98%       99%      100%
+ 90058.80  90488.84  96915.20 104431.80 230403.76 366247.00
+sum= 1990552
+$t ~ $cfs:
+               Estimate  Std. Error  t value  Pr(>|t|)
+(Intercept) -6.6133e-02  7.9951e-03  -8.2717 5.476e-11 ***
+E$cfs        2.1044e-05  1.2437e-07 169.2024 < 2.2e-16 ***
+R-squared: 0.9982
+> plot(E$t)
+     \endverbatim
+   </li>
+   <li> One sees that likely a higher splitting-depth would yield a better
+   result:
+   \verbatim
 > SplittingViaOKsolver -D12 VanDerWaerden_2-3-12_135.cnf
-> cd SplitViaOKsolver_D12VanDerWaerden_2312_135cnf_2011-05-30-083254/
+> cd SplitViaOKsolver_D12VanDerWaerden_2312_135cnf_2011-08-21-103003/
 > more Md5sum
 96d44d832aa27f0abbc531c35baee49d
 > more Statistics
@@ -213,39 +195,57 @@ sys     0m0.293s
 12 13 14 15 16 17 18 19 20
 65 23 32 27 23 15  3  4  2
 > more Result
-c running_time(sec)                     0.8
+c running_time(sec)                     1.0
 c number_of_nodes                       387
 c number_of_2-reductions                2
 c max_tree_depth                        12
 c splitting_cases                       194
 
-> cd Instances
-> OKP=~/OKplatform; I="../$(cat ../F)"; echo " i n t sat cfs dec rts r1 mem ptime stime cfl" > Stats; time tail -n +2 ../Data | while read C F N; do cat $I | ApplyPass-O3-DNDEBUG $F Temp.cnf; minisat-2.2.0 Temp.cnf >Temp.out 2>&1; S=$?; if [[ $S != 20 ]]; then echo -e "UNEXPECTED RETURN VALUE ${S}\!"; break; else echo -n "$C " >> Stats; awk -f ${OKP}/OKsystem/OKlib/Experimentation/ExperimentSystem/SolverMonitoring/ExtractMinisat.awk Temp.out >> Stats; echo -n "$C "; fi; done
-real    0m28.275s
-user    0m26.884s
-sys     0m0.889s
-
-# Monitoring in R via
-#> E=read.table("Stats",header=TRUE,colClasses=c(rep("integer",3),"numeric","integer",rep("numeric",8))); plot(E$t); cat(sprintf("%d: %.2fh, sum-cfs=%e, mean-t=%.3fs, mean-cfs=%.0f",length(E$t),sum(E$t)/60/60,sum(E$cfs),mean(E$t),mean(E$cfs)),"\n")
-194: 0.01h, sum-cfs=1.389903e+06, mean-t=0.121s, mean-cfs=7164
+> cd ..
+> ProcessSplitViaOKsolver SplitViaOKsolver_D12VanDerWaerden_2312_135cnf_2011-08-21-103003/
+> cd Process_SplitViaOKsolver_D12VanDerWaerden_2312_135cnf_2011-08-21-103003_2011-08-21-103122/
+> more Result
+UNSATISFIABLE
+  PID TTY      STAT   TIME COMMAND
+28375 pts/48   S+     0:30 /bin/bash /home/kullmann/OKplatform/bin/ProcessSplitViaOKsolver SplitViaOKsolver_D12VanDerWaerden_2312_135cnf_2011-08-21-103003/
+> oklib --R
+> oklib_load_all()
+> E=read_processsplit_minisat()
+194: 24.3s, sum-cfs=1.389903e+06, mean-t=0.125s, mean-cfs=7164
+$t:
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+0.02300 0.07399 0.10600 0.12520 0.15550 0.39290
+sd= 0.07745311
+      95%       96%       97%       98%       99%      100%
+0.3080026 0.3152317 0.3344188 0.3510860 0.3685537 0.3929400
+sum= 24.29521
+$cfs:
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+   1112    4242    6070    7164    9059   22640
+sd= 4403.824
+     95%      96%      97%      98%      99%     100%
+17368.30 18059.92 19053.31 19735.08 20662.55 22643.00
+sum= 1389903
+$t ~ $cfs:
+               Estimate  Std. Error  t value Pr(>|t|)
+(Intercept) -2.4006e-04  9.8007e-04  -0.2449   0.8068
+E$cfs        1.7513e-05  1.1662e-07 150.1702   <2e-16 ***
+R-squared: 0.9916
    \endverbatim
    </li>
-   <li> One sees that the n-interpretation creates are much more evenly
-   splitting, enabling to split harder problems much further (which seems to
-   be essential). </li>
-   <li> It also seems that the combination of the OKsolver with minisat-2.2.0
+   <li> It seems that the combination of the OKsolver with minisat-2.2.0
    is a good combination: The OKsolver for splitting the hard problem, the
    conflict-driven solver for finishing off. </li>
   </ul>
-  
-  
+
+
   \todo Parallel Satz
   <ul>
    <li> See "Satz" in Buildsystem/ExternalSources/SpecialBuilds/plans/SAT.hpp.
    </li>
   </ul>
-  
-  
+
+
   \todo Using Open MPI
   <ul>
    <li> It seems that to run even the "simple parallelisation" provided e.g.
@@ -261,16 +261,16 @@ sys     0m0.889s
    SplittingViaOKsolver version 0.1.2. </li>
    <li> This involves some changes of the output formatting. </li>
   </ul>
-  
-  
+
+
   \todo Verify vdw_2(4,9) = 309 (conjectured)
   <ul>
    <li> See
    Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/09.hpp for the
    computations. </li>
   </ul>
-  
-  
+
+
   \todo Verify vdw_2(5,7) = 260 (conjectured)
   <ul>
    <li> See
@@ -460,5 +460,5 @@ sys     16m52.403s
    So the problem is very hard, and further splitting is needed, at least
    D=26. </li>
   </ul>
-  
+
 */
