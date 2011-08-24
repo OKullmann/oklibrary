a2753896ebd6b35822575873b6c9a79528e99d0b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-08-24 19:51:26 +0100

    Editorial.

--- Satisfiability/Solvers/OKsolver/SAT2002/Einlesen.c
+++ Satisfiability/Solvers/OKsolver/SAT2002/Einlesen.c
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 19.1.2001 (Toronto)
-/* Copyright 2001 - 2007, 2008 Oliver Kullmann
+/* Copyright 2001 - 2007, 2008, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -1588,10 +1588,8 @@ void AufraeumenEinlesen(void)
 /* ------------------------------------------------------------- */
 
 
-__inline__ static char *Symbol1(unsigned int v)
-{
-  if (VarTab1 != NULL)
-    v = VarTab1[v];
+__inline__ static char* Symbol1(unsigned int v) {
+  if (VarTab1 != NULL) v = VarTab1[v];
   return VarTab[v];
 }
 
