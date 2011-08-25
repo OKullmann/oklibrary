bb26e520f8ce83bff26747f7d36823df2e4fa71f
Matthew Gwynne <360678@swan.ac.uk>
2011-08-25 16:41:38 +0100

    Started 6-round DES experiments.
    All des(6) translations so far take > 8 days to solve with minisat-2.2.0.

---
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/6.hpp
@@ -0,0 +1,303 @@
+// Matthew Gwynne, 25.8.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/Cryptography/DataEncryptionStandard/plans/KeyDiscovery/6.hpp
+  \brief On investigations into the 6-round Data Encryption Standard key discovery
+
+
+  \todo Overview
+  <ul>
+   <li> We consider the 6 round DES given by the encryption function
+   des_encryption_gen in
+   ComputerAlgebra/Cryptology/Lisp/CryptoSystems/DataEncryptionStandard/Cipher.mac.
+   </li>
+   <li> The translation of one round DES to SAT is given at the Maxima level by
+   des_fcl_gen in
+   ComputerAlgebra/Cryptology/Lisp/Cryptanalysis/DataEncryptionStandard/GeneralisedConstraintTranslation.mac.
+   </li>
+   <li> The DES consists of certain rewiring of the bits, additions (XOR) and
+   the application of 8 S-boxes (substitution boxes) for each round. </li>
+   <li> We consider the DES S-boxes as 6x4 boolean functions,
+   given by des_sbox_bf in
+   ComputerAlgebra/Cryptology/Lisp/CryptoSystems/DataEncryptionStandard/Sboxes.mac.
+   </li>
+   <li> We should also consider the DES S-boxes as 4 6x1 functions.
+   See "Basic translation" in
+   Investigations/Cryptography/DataEncryptionStandard/plans/general.hpp. </li>
+   <li> We translate the DES by treating the additions and S-boxes as the
+   boolean functions, which we consider our units of translation. </li>
+   <li> The additions are translated by the set of their prime implicates.
+   </li>
+   <li> The S-boxes are translated using each of the following CNF
+   representations:
+    <ul>
+     <li> canonical(+) representation, see dualts_fcl and dualtsplus_fcl in
+     ComputerAlgebra/Satisfiability/Lisp/FiniteFunctions/TseitinTranslation.mac;
+     </li>
+     <li> 1-base translations, see
+     Investigations/Cryptography/DataEncryptionStandard/plans/Sboxes/general.hpp;
+     </li>
+     <li> minimum translations, see
+     Investigations/Cryptography/DataEncryptionStandard/plans/Sboxes/general.hpp;
+     </li>
+     <li> their prime implicates; </li>
+     <li> their canonical CNF representations. </li>
+    </ul>
+   All such translations apply to both the 6x4 S-box functions and
+   the 4 decomposed 6x1 functions.
+   </li>
+   <li> All translations so far takes > 8 days to solve. </li>
+  </ul>
+
+
+  \todo Using the 1-base translation for the S-boxes (6-to-4)
+  <ul>
+   <li> Translating the DES Sboxes, as 6-to-4 bit boolean functions, using
+   1-bases. </li>
+   <li> Generating the 1-bases:
+   \verbatim
+maxima> for i : 1 thru 8 do output_dessbox_fullcnf_stdname(i)$
+shell> gen_seed[1]=7;gen_seed[2]=71;gen_seed[3]=185;gen_seed[4]=346;gen_seed[5]=67;gen_seed[6]=327;gen_seed[7]=148;gen_seed[8]=167;
+shell> base_seed[1]=1;base_seed[2]=1;base_seed[3]=2;base_seed[4]=4;base_seed[5]=2;base_seed[6]=1;base_seed[7]=2;base_seed[8]=1;
+shell> for i in $(seq 1 8); do
+  QuineMcCluskey-n16-O3-DNDEBUG DES_Sbox_${i}_fullCNF.cnf > DES_Sbox_${i}_pi.cnf;
+  RandomShuffleDimacs-O3-DNDEBUG ${gen_seed[$i]} < DES_Sbox_${i}_pi.cnf | SortByClauseLength-O3-DNDEBUG > DES_Sbox_${i}_sortedpi.cnf;
+  RUcpGen-O3-DNDEBUG DES_Sbox_${i}_sortedpi.cnf > DES_Sbox_${i}_gen.cnf;
+  RandomShuffleDimacs-O3-DNDEBUG ${base_seed[$i]}  < DES_Sbox_${i}_gen.cnf | SortByClauseLengthDescending-O3-DNDEBUG | RUcpBase-O3-DNDEBUG > DES_Sbox_${i}_1base.cnf;
+done
+   \endverbatim
+   </li>
+   <li> The numbers of clauses in the 1-bases are 124, 129, 138, 128, 134,
+   136, 123, 152 respectively. </li>
+   <li> All the 1-bases used have clauses of sizes 5 and 6, except Sbox 4
+   which has clauses of size 5 and 6 as well as 2 of size 7. </li>
+   <li> Generating the 20 instances with random plaintext-ciphertext pairs:
+   \verbatim
+rounds : 6$
+sbox_fcl_l : create_list(read_fcl_f(sconcat("DES_Sbox_",i,"_1base.cnf")), i, 1, 8)$
+for seed : 1 thru 20 do block(
+  print(sconcat("Generating ", rounds, "-round DES with seed ", seed)),
+  set_random(make_random_state(seed)),
+  P_hex : lpad(int2hex(random(2**64)),"0",16),
+  K_hex : lpad(int2hex(random(2**64)),"0",16),
+  C_hex : des_encryption_hex_gen(rounds, P_hex,K_hex),
+  P : des_plain2fcl_gen(hexstr2binv(P_hex),rounds),
+  C : des_cipher2fcl_gen(hexstr2binv(C_hex),rounds),
+  F : des2fcl_gen(sbox_fcl_l,rounds),
+  Fs : standardise_fcl([F[1],append(F[2],P[2],C[2])]),
+  output_fcl_v(
+  sconcat(rounds, "-round DES instantiated with plaintext and ciphertext generated from seed ", seed, "; translated using the 1-base translation for the S-boxes (6-to-4)."),
+    Fs[1],
+    sconcat("des_6t4_1base_r",rounds,"_s",seed,".cnf"),
+    Fs[2]))$
+print("DONE!");
+   \endverbatim
+   </li>
+   <li> Statistics for all instances:
+   \verbatim
+shell> cat des_6t4_1base_r6_s1.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG n
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+800 8432 40550 0 40550 801 1
+ length count
+1 128
+3 1920
+5 3648
+6 2730
+7 6
+   \endverbatim
+   </li>
+   <li> S-box statistics (1-base translations):
+   \verbatim
+for F in sbox_fcl_l do print(ncl_list_fcl(F));
+[[5,84],[6,39],[7,1]]
+[[5,75],[6,54]]
+[[5,76],[6,62]]
+[[5,69],[6,59]]
+[[5,78],[6,56]]
+[[5,83],[6,53]]
+[[5,75],[6,48]]
+[[5,68],[6,84]]
+   \endverbatim
+   </li>
+   <li> We have the following number of clauses of the following sizes:
+    <ul>
+     <li> 128 unit-clauses (setting plaintext + ciphertext); </li>
+     <li> 1920 ternary clauses (80 * 6 = 480 binary additions); </li>
+     <li> 3648 clauses of length 5 (8 * 5 = 48 S-boxes); </li>
+     <li> 2730 clauses of length 6 (8 * 6 = 48 S-boxes); </li>
+     <li> 6 clauses of length seven (1 * 6 = 6 S-boxes). </li>
+    </ul>
+   </li>
+   <li> Running minisat-2.2.0 on these instances:
+   \verbatim
+shell> r=6;
+shell> RunMinisat des_6t4_1base_r${r}_s1.cnf;
+   \endverbatim
+   This is still running after 8 days. </li>
+  </ul>
+
+
+  \todo Using the "minimum" translation for the S-boxes (6-to-4)
+  <ul>
+   <li> Translating the DES Sboxes, as 6-to-4 bit boolean functions, using the
+   "minimum" (inf-based) representations. </li>
+   <li> Generating the "minimum" CNFs for the Sboxes:
+   \verbatim
+maxima> for i : 1 thru 8 do output_dessbox_fullcnf_stdname(i)$
+shell> for i in $(seq 1 8); do
+  QuineMcCluskeySubsumptionHypergraph-n16-O3-DNDEBUG DES_Sbox_${i}_fullCNF.cnf > DES_Sbox_${i}_shg.cnf;
+  cat DES_Sbox_${i}_shg.cnf | MinOnes2WeightedMaxSAT-O3-DNDEBUG > DES_Sbox_${i}_shg.wcnf;
+done
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 2444475534 -i DES_Sbox_1_shg.wcnf -r model DES_Sbox_1.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 2521057446 -i DES_Sbox_2_shg.wcnf -r model DES_Sbox_2.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 68 -solve 1 -seed 3544367510 -i DES_Sbox_3_shg.wcnf -r model DES_Sbox_3.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 69 -solve 1 -seed 3808694681 -i DES_Sbox_4_shg.wcnf -r model DES_Sbox_4.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 1876503362 -i DES_Sbox_5_shg.wcnf -r model DES_Sbox_5.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 66 -solve 1 -seed 68018538 -i DES_Sbox_6_shg.wcnf -r model DES_Sbox_6.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 67 -solve 1 -seed 1856244582 -i DES_Sbox_7_shg.wcnf -r model DES_Sbox_7.ass;
+shell> ubcsat-okl  -alg gsat -w -runs 100 -cutoff 400000 -wtarget 69 -solve 1 -seed 4223500633 -i DES_Sbox_8_shg.wcnf -r model DES_Sbox_8.ass;
+shell> for i in $(seq 1 8); do
+  cat DES_Sbox_${i}_fullCNF.cnf_primes | FilterDimacs DES_Sbox_${i}.ass > DES_Sbox_${i}_min.cnf;
+done
+   \endverbatim
+   </li>
+   <li> The numbers of clauses in the CNFs are 67, 67, 68, 69, 67, 66, 67, and
+   69 respectively. </li>
+   <li> Generating the instance for 20 random plaintext-ciphertext pairs:
+   \verbatim
+rounds : 6$
+sbox_fcl_l : create_list(read_fcl_f(sconcat("DES_Sbox_",i,"_min.cnf")), i, 1, 8)$
+for seed : 1 thru 20 do block(
+  print(sconcat("Generating ", rounds, "-round DES with seed ", seed)),
+  set_random(make_random_state(seed)),
+  P_hex : lpad(int2hex(random(2**64)),"0",16),
+  K_hex : lpad(int2hex(random(2**64)),"0",16),
+  C_hex : des_encryption_hex_gen(rounds, P_hex,K_hex),
+  P : des_plain2fcl_gen(hexstr2binv(P_hex),rounds),
+  C : des_cipher2fcl_gen(hexstr2binv(C_hex),rounds),
+  F : des2fcl_gen(sbox_fcl_l,rounds),
+  Fs : standardise_fcl([F[1],append(F[2],P[2],C[2])]),
+  output_fcl_v(
+  sconcat(rounds, "-round DES instantiated with plaintext and ciphertext generated from seed ", seed, "; translated using the 1-base translation for the S-boxes (6-to-4)."),
+    Fs[1],
+    sconcat("des_6t4_min_r",rounds,"_s",seed,".cnf"),
+    Fs[2]))$
+print("DONE!");
+   \endverbatim
+   </li>
+   <li> Statistics:
+   \verbatim
+shell> cat des_6t4_min_r6_s1.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG n
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+800 5288 24008 0 24008 801 1
+ length count
+1 128
+3 1920
+5 1392
+6 1776
+7 72
+   \endverbatim
+   </li>
+   <li> S-box statistics ("minimum" translation):
+   \verbatim
+for F in sbox_fcl_l do print(ncl_list_fcl(F));
+
+[[5,30],[6,35],[7,2]]
+[[5,33],[6,33],[7,1]]
+[[5,28],[6,38],[7,2]]
+[[5,29],[6,38],[7,2]]
+[[5,29],[6,36],[7,2]]
+[[5,28],[6,38]]
+[[5,29],[6,37],[7,1]]
+[[5,26],[6,41],[7,2]]
+  \endverbatim
+   </li>
+   <li> We have the following number of clauses of the following sizes:
+    <ul>
+     <li> 128 unit-clauses (setting plaintext + ciphertext); </li>
+     <li> 1920 ternary clauses (80 * 6 = 480 binary additions); </li>
+     <li> 1392 clauses of length 5 (8 * 6 = 48 S-boxes); </li>
+     <li> 1776 clauses of length 6 (8 * 5 = 48 S-boxes); </li>
+     <li> 72 clauses of length seven (7 * 6 = 42 S-boxes). </li>
+    </ul>
+   </li>
+   <li> Running minisat-2.2.0 on these instances:
+   \verbatim
+shell> r=6;
+shell> RunMinisat des_6t4_min_r${r}_s1.cnf;
+   \endverbatim
+   This is still running after 8 days. </li>
+  </ul>
+
+
+  \todo Using the canonical translation for the S-boxes (6-to-4)
+  <ul>
+   <li> Translating the DES Sboxes, as 6x4 boolean functions, using the
+   canonical representation. That is, each Sbox is represented with the
+   canonical representation given by dualts_fcl in
+   ComputerAlgebra/Satisfiability/Lisp/FiniteFunctions/TseitinTranslation.mac.
+   </li>
+   <li> Generating the instance:
+   \verbatim
+rounds : 6$
+sbox_fcl_l : create_list(dualts_fcl([listify(setn(10)), des_sbox_fulldnf_cl(i)]), i, 1, 8)$
+for seed : 1 thru 20 do block(
+  print(sconcat("Generating ", rounds, "-round DES with seed ", seed)),
+  set_random(make_random_state(seed)),
+  P_hex : lpad(int2hex(random(2**64)),"0",16),
+  K_hex : lpad(int2hex(random(2**64)),"0",16),
+  C_hex : des_encryption_hex_gen(rounds, P_hex,K_hex),
+  P : des_plain2fcl_gen(hexstr2binv(P_hex),rounds),
+  C : des_cipher2fcl_gen(hexstr2binv(C_hex),rounds),
+  F : des2fcl_gen(sbox_fcl_l,rounds),
+  Fs : standardise_fcl([F[1],append(F[2],P[2],C[2])]),
+  output_fcl_v(
+  sconcat(rounds, "-round DES instantiated with plaintext and ciphertext generated from seed ", seed, "; translated using the 1-base translation for the S-boxes (6-to-4)."),
+    Fs[1],
+    sconcat("des_6t4_canon_r",rounds,"_s",seed,".cnf"),
+    Fs[2]))$
+print("DONE!");
+   \endverbatim
+   </li>
+   <li> Statistics:
+   \verbatim
+shell> cat des_6t4_canon_r6_s1.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG n
+ n non_taut_c red_l taut_c orig_l comment_count finished_bool
+3872 35888 104192 0 104192 3873 1
+ length count
+1 128
+2 30720
+3 1920
+11 3072
+64 48
+   \endverbatim
+   </li>
+   <li> S-box statistics (canonical translation):
+   \verbatim
+ncl_list_fcl(dualts_fcl([listify(setn(10)), des_sbox_fulldnf_cl(1)]));
+[[2,640],[11,64],[64,1]]
+   \endverbatim
+   </li>
+   <li> We have the following number of clauses of the following sizes:
+    <ul>
+     <li> 128 unit-clauses (setting plaintext + ciphertext); </li>
+     <li> 30720 binary clauses (8 * 6 = 48 S-boxes); </li>
+     <li> 1920 ternary clauses (80 * 6 = 480 binary additions); </li>
+     <li> 3072 clauses of length eleven (8 * 6 = 48 S-boxes); </li>
+     <li> 48 clauses of length 64 (8 * 6 = 48 S-boxes). </li>
+    </ul>
+   </li>
+   <li> Running minisat-2.2.0 on these instances:
+   \verbatim
+shell> r=6;
+shell> RunMinisat des_6t4_canon_r${r}_s1.cnf;
+   \endverbatim
+   This is still running after 8 days. </li>
+  </ul>
+
+*/
