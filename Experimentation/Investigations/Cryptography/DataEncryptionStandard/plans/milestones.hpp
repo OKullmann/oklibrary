93f80d44c5119206e1c52d215d250f1b1410eed7
Matthew Gwynne <360678@swan.ac.uk>
2011-08-26 14:31:40 +0100

    *** Initial version 0.1.2 (Investigations/Cryptography/DataEncryptionStandard) ***

---
+++ Experimentation/Investigations/Cryptography/DataEncryptionStandard/plans/milestones.hpp
@@ -0,0 +1,68 @@
+// Matthew Gwynne, 26.8.2011 (Swansea)
+/* Copyright 2011 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Investigations/Cryptography/DataEncryptionStandard/plans/milestones.hpp
+
+  \module_version Investigations/Cryptography/DataEncryptionStandard/ 0.1.2 (26.8.2011)
+
+
+  \par Version 0.1.3 :
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/KeyDiscovery/general.hpp the
+   following topics are handled:
+    - Experiment scripts
+    - Add information on specific S-box translations to experiment instances
+    - Move into separate sub-module : DONE
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/general.hpp the
+   following topics are handled:
+    - Understanding the Massacci-Marraro translation
+
+
+  \par Version 0.1.4 :
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/general.hpp the
+   following topics are handled:
+    - Understanding DES
+    - Basic translation
+    - Encryption and decryption
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/Sboxes/general.hpp the
+   following topics are handled:
+    - Analysing the S-boxes
+
+
+  \par Version 0.1.5 :
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/general.hpp the
+   following topics are handled:
+    - More advanced treatments
+    - r_2-bases of a round
+
+  \par
+   In Cryptography/DataEncryptionStandard/plans/Sboxes/general.hpp the
+   following topics are handled:
+    - Analysing the 6-to-1 bit Sbox functions
+
+  \par
+   Add further milestones
+
+
+
+  -------------------------------------------------------------------------------------------------------------------------------------
+
+  \par Version history
+
+   - 0.1.2   : 26.8.2011; initial version number (investigations into full DES up to 5 rounds, and full DES with up to 32 key-bits missing).
+
+*/
