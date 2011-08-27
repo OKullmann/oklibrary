03dd994851677017ddb0727f037afbf1ddcf74bb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-08-27 09:37:18 +0100

    Corrected specification.

--- Satisfiability/Interfaces/InputOutput/AppendDimacs.cpp
+++ Satisfiability/Interfaces/InputOutput/AppendDimacs.cpp
@@ -10,7 +10,7 @@ License, or any later version. */
   \brief Application to select clauses from a clause-set
 
 
-  \bug Incomplete specification
+  \bug Incomplete specification : DONE
   <ul>
    <li> What does "maximum number of variables" mean?? </li>
    <li> It should be the maximal variable-*index*. </li>
@@ -26,11 +26,8 @@ License, or any later version. */
    outputs on standard output a Dimacs CNF containing the clauses from all
    of the given clause-lists, in the order the arguments given and in their
    original order in their respective inputs. </li>
-   <li> The number of variables given on the p-line of the output is
-   the maximum number of variables across the given clause-lists. </li>
-   <li> In particular, the assumption is that the variable-set of every
-   argument clause-list is a subset of the variable-set of the clause-list with
-   the most variables. </li>
+   <li> The maximal variable-index given on the p-line of the output is
+   the maximum of maxima variable-indices across the given clause-lists. </li>
    <li> The number of clauses given on the p-line of the output is the
    sum of the number of clauses for each clause-list given as an argument.
    </li>
