bfa6c2f03a0abd041b717c418af19ac71cf0ddbd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-08-27 10:29:59 +0100

    Update to new Statistics-interface.
    Not tested yet.

--- Satisfiability/Interfaces/InputOutput/MinOnes2WeightedMaxSAT.cpp
+++ Satisfiability/Interfaces/InputOutput/MinOnes2WeightedMaxSAT.cpp
@@ -117,10 +117,10 @@ namespace {
       adaptor_statistics.c(pc);
       if (pc < 0)
         throw OKlib::InputOutput::ParameterOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2WeightedMaxSATOutput::n:\n  number of clauses is a negative quantity = " + boost::lexical_cast<std::string>(pc));
-      out << "p wcnf " << adaptor_statistics.stat.parameter_n << " " << adaptor_statistics.stat.parameter_c + adaptor_statistics.stat.parameter_n << "\n";
+      out << "p wcnf " << adaptor_statistics.stat.pn() << " " << adaptor_statistics.stat.pc() + adaptor_statistics.stat.pn() << "\n";
     }
     void finish() {
-      const int_type n = adaptor_statistics.stat.parameter_n;
+      const int_type n = adaptor_statistics.stat.pn();
       for (int_type i = 1; i <= n; ++i) out << "1 " << -i << " 0\n";
       adaptor_statistics.finish();
     }
@@ -130,16 +130,16 @@ namespace {
     template <class ForwardRange>
     void clause(const ForwardRange& r, const int_type t) {
       adaptor_statistics.clause(r, t);
-      out << adaptor_statistics.stat.parameter_n + 1 << " ";
-      if (adaptor_statistics.stat.non_tautological_clauses_count > adaptor_statistics.stat.parameter_c)
-        throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2WeightedMaxSATOutput::clause:\n  number of non-tautological clauses exceeds specified total number of clauses = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_c));
+      out << adaptor_statistics.stat.pn() + 1 << " ";
+      if (adaptor_statistics.stat.c() > adaptor_statistics.stat.pc())
+        throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2WeightedMaxSATOutput::clause:\n  number of non-tautological clauses exceeds specified total number of clauses = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.pc()));
       typedef typename boost::range_const_iterator<ForwardRange>::type const_iterator;
       const const_iterator& end(boost::end(r));
       for (const_iterator i = boost::begin(r); i != end; ++i) {
         typedef typename boost::range_value<ForwardRange>::type value_type;
         const value_type& literal = *i;
-        if (std::abs(literal) > adaptor_statistics.stat.parameter_n)
-          throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2WeightedMaxSATOutput::clause:\n  variable index of literal = " + boost::lexical_cast<std::string>(literal) + " exceeds specified maximal index = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_n));
+        if (std::abs(literal) > adaptor_statistics.stat.pn())
+          throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2WeightedMaxSATOutput::clause:\n  variable index of literal = " + boost::lexical_cast<std::string>(literal) + " exceeds specified maximal index = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.pn()));
         out << literal << " ";
       }
       out << 0 << "\n";
