46e2eb9fa6377be5bae272e656af9d4306f82546
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-08-27 09:48:47 +0100

    CORRECTION: no namespaces are opened in application files.

--- Satisfiability/Interfaces/InputOutput/MinOnes2PseudoBoolean.cpp
+++ Satisfiability/Interfaces/InputOutput/MinOnes2PseudoBoolean.cpp
@@ -32,7 +32,7 @@ p cnf n c
 +1 x1 +1 x2 +1 x3 +1 x4 +1 ~x5 >= 1;
    \endverbatim
    </li>
-   <li> After the "* #variable ..." line, the application outputs the 
+   <li> After the "* #variable ..." line, the application outputs the
    optimisation line
    \verbatim
 min: +1 x1 ... +1 xn ;
@@ -69,131 +69,124 @@ min: +1 x1 ... +1 xn ;
 #include <OKlib/Satisfiability/Interfaces/InputOutput/Dimacs.hpp>
 #include <OKlib/Satisfiability/Interfaces/InputOutput/ClauseSetAdaptors.hpp>
 
-namespace OKlib {
-  namespace InputOutput {
-
-    // #####################################################
-
-    /*!
-      \class CLSAdaptorMinOnes2PseudoBoolean
-      \brief Adaptor for clause-sets for output in DIMACS format
-
-      Parameter n is considered as maximal possible variable index,
-      while parameter c is considered as upper bound on the number of clauses.
-
-      The role of the template parameters are as follows:
-      <ol>
-       <li> Int is the integral type for the Dimacs literals. </li>
-       <li> String is the string type for comments. </li>
-       <li> AdaptorStatistics is a CLSAdaptor which handles statistics. </li>
-      </ol>
-
-    */
-
-    template <typename Int = int, class String = std::string, class AdaptorStatistics = CLSAdaptorStatistics<Int, String> >
-    class CLSAdaptorMinOnes2PseudoBooleanOutput {
-
-    public:
-      typedef String string_type;
-
-    private:
-
-      typedef AdaptorStatistics adaptor_statistics_type;
-
-      std::ostream& out;
-      adaptor_statistics_type adaptor_statistics;
-      string_type comments;
-      bool p_line_read;
-
-    public :
-
-      typedef Int int_type;
-      typedef typename adaptor_statistics_type::statistics_type statistics_type;
-
-      CLSAdaptorMinOnes2PseudoBooleanOutput(std::ostream& out) : 
-        out(out), p_line_read(false) {
-        if (not out)
-          throw OKlib::InputOutput::OStreamError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::CLSAdaptorMinOnes2PseudoBooleanOutput(std::ostream&):\n  cannot open the output stream");
-      }
-
-      void comment(const string_type& s) {
-        adaptor_statistics.comment(s);
-        if (s.empty())
-          comments = comments + "*\n";
-        else
-          if (boost::algorithm::is_space()(s[0]))
-            comments = comments + "*" + s + "\n";
-          else
-            comments = comments + "* " + s + "\n";
-        if (p_line_read)
-          out << comments;
-      }
-      void n(const int_type pn) {
-        adaptor_statistics.n(pn);
-        if (pn < 0)
-          throw OKlib::InputOutput::ParameterOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::n:\n  maximal variable index is a negative quantity = " + boost::lexical_cast<std::string>(pn));
-      }
-      void c(const int_type pc) {
-        adaptor_statistics.c(pc);
-        if (pc < 0)
-          throw OKlib::InputOutput::ParameterOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::n:\n  number of clauses is a negative quantity = " + boost::lexical_cast<std::string>(pc));
-        out << "* #variable= " << adaptor_statistics.stat.parameter_n << " #constraint= " << adaptor_statistics.stat.parameter_c + adaptor_statistics.stat.parameter_n << "\n";
-        out << comments;
-        out << "min: ";
-        const int_type n = adaptor_statistics.stat.parameter_n;
-        for (int_type i = 1; i <= n; ++i) {
-          out << "+1 x" << i << " ";
-        }
-        out << ";\n";
-        p_line_read = true;
-      }
-      void finish() {
-        adaptor_statistics.finish();
-      }
-      void tautological_clause(const int_type t) {
-        adaptor_statistics.tautological_clause(t);
-      }
-      template <class ForwardRange>
-      void clause(const ForwardRange& r, const int_type t) {
-        adaptor_statistics.clause(r, t);
-        if (adaptor_statistics.stat.non_tautological_clauses_count > adaptor_statistics.stat.parameter_c)
-          throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::clause:\n  number of non-tautological clauses exceeds specified total number of clauses = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_c));
-        typedef typename boost::range_const_iterator<ForwardRange>::type const_iterator;
-        const const_iterator& end(boost::end(r));
-        for (const_iterator i = boost::begin(r); i != end; ++i) {
-          typedef typename boost::range_value<ForwardRange>::type value_type;
-          const value_type& literal = *i;
-          if (std::abs(literal) > adaptor_statistics.stat.parameter_n)
-            throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::clause:\n  variable index of literal = " + boost::lexical_cast<std::string>(literal) + " exceeds specified maximal index = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_n));
-          
-          if (literal > 0) 
-            out << " +1 x" << literal << " ";
-          else
-            out << " +1 ~x" << literal << " ";
-        }
-        out << " >= 1;\n";
-      }
-
-      const adaptor_statistics_type& stat() const {
-        return adaptor_statistics.stat;
-      }
-
-    };
-  }
-}
 
 namespace {
 
   const std::string program = "MinOnes2PseudoBoolean";
   const std::string err = "ERROR[" + program + "]: ";
 
-  const std::string version = "0.0.8";
+  const std::string version = "0.0.9";
+
+  /*!
+    \class CLSAdaptorMinOnes2PseudoBoolean
+    \brief Adaptor for clause-sets for output in DIMACS format
+
+    Parameter n is considered as maximal possible variable index,
+    while parameter c is considered as upper bound on the number of clauses.
+
+    The role of the template parameters are as follows:
+    <ol>
+     <li> Int is the integral type for the Dimacs literals. </li>
+     <li> String is the string type for comments. </li>
+     <li> AdaptorStatistics is a CLSAdaptor which handles statistics. </li>
+    </ol>
+
+  */
+
+  template <typename Int = int, class String = std::string, class AdaptorStatistics = OKlib::InputOutput::CLSAdaptorStatistics<Int, String> >
+  class CLSAdaptorMinOnes2PseudoBooleanOutput {
+
+  public:
+    typedef String string_type;
+
+  private:
+
+    typedef AdaptorStatistics adaptor_statistics_type;
+
+    std::ostream& out;
+    adaptor_statistics_type adaptor_statistics;
+    string_type comments;
+    bool p_line_read;
+
+  public :
+
+    typedef Int int_type;
+    typedef typename adaptor_statistics_type::statistics_type statistics_type;
+
+    CLSAdaptorMinOnes2PseudoBooleanOutput(std::ostream& out) :
+      out(out), p_line_read(false) {
+      if (not out)
+        throw OKlib::InputOutput::OStreamError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::CLSAdaptorMinOnes2PseudoBooleanOutput(std::ostream&):\n  cannot open the output stream");
+    }
+
+    void comment(const string_type& s) {
+      adaptor_statistics.comment(s);
+      if (s.empty())
+        comments = comments + "*\n";
+      else
+        if (boost::algorithm::is_space()(s[0]))
+          comments = comments + "*" + s + "\n";
+        else
+          comments = comments + "* " + s + "\n";
+      if (p_line_read)
+        out << comments;
+    }
+    void n(const int_type pn) {
+      adaptor_statistics.n(pn);
+      if (pn < 0)
+        throw OKlib::InputOutput::ParameterOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::n:\n  maximal variable index is a negative quantity = " + boost::lexical_cast<std::string>(pn));
+    }
+    void c(const int_type pc) {
+      adaptor_statistics.c(pc);
+      if (pc < 0)
+        throw OKlib::InputOutput::ParameterOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::n:\n  number of clauses is a negative quantity = " + boost::lexical_cast<std::string>(pc));
+      out << "* #variable= " << adaptor_statistics.stat.parameter_n << " #constraint= " << adaptor_statistics.stat.parameter_c + adaptor_statistics.stat.parameter_n << "\n";
+      out << comments;
+      out << "min: ";
+      const int_type n = adaptor_statistics.stat.parameter_n;
+      for (int_type i = 1; i <= n; ++i) {
+        out << "+1 x" << i << " ";
+      }
+      out << ";\n";
+      p_line_read = true;
+    }
+    void finish() {
+      adaptor_statistics.finish();
+    }
+    void tautological_clause(const int_type t) {
+      adaptor_statistics.tautological_clause(t);
+    }
+    template <class ForwardRange>
+    void clause(const ForwardRange& r, const int_type t) {
+      adaptor_statistics.clause(r, t);
+      if (adaptor_statistics.stat.non_tautological_clauses_count > adaptor_statistics.stat.parameter_c)
+        throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::clause:\n  number of non-tautological clauses exceeds specified total number of clauses = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_c));
+      typedef typename boost::range_const_iterator<ForwardRange>::type const_iterator;
+      const const_iterator& end(boost::end(r));
+      for (const_iterator i = boost::begin(r); i != end; ++i) {
+        typedef typename boost::range_value<ForwardRange>::type value_type;
+        const value_type& literal = *i;
+        if (std::abs(literal) > adaptor_statistics.stat.parameter_n)
+          throw OKlib::InputOutput::ClauseOutputError("OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput::clause:\n  variable index of literal = " + boost::lexical_cast<std::string>(literal) + " exceeds specified maximal index = " + boost::lexical_cast<std::string>(adaptor_statistics.stat.parameter_n));
+
+        if (literal > 0)
+          out << " +1 x" << literal << " ";
+        else
+          out << " +1 ~x" << literal << " ";
+      }
+      out << " >= 1;\n";
+    }
+
+    const adaptor_statistics_type& stat() const {
+      return adaptor_statistics.stat;
+    }
+
+  };
 
 }
 
 int main() {
-  typedef OKlib::InputOutput::CLSAdaptorMinOnes2PseudoBooleanOutput<> CLSAdaptor;
+  typedef CLSAdaptorMinOnes2PseudoBooleanOutput<> CLSAdaptor;
   CLSAdaptor output(std::cout);
   OKlib::InputOutput::StandardDIMACSInput<CLSAdaptor >(std::cin, output);
 }
-
