ba93ec6ce9ff090d98c31ea12cf5e76f7d1f54f1
Matthew Gwynne <360678@swan.ac.uk>
2011-08-29 11:47:08 +0100

    Added todo on standardising the names and order of solver data columns in extraction scripts.

--- Experimentation/ExperimentSystem/SolverMonitoring/plans/milestones.hpp
+++ Experimentation/ExperimentSystem/SolverMonitoring/plans/milestones.hpp
@@ -19,6 +19,7 @@ License, or any later version. */
     - Improve docus
     - Running experiments
     - Better summary statistics
+    - Standardising names and order of solver data columns
     - Understanding solver output
     - Extraction tools
 
