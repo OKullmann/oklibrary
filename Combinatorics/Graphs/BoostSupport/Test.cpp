6f13e511e3ade635726662ea8095af4ca7f58e53
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-04 12:23:42 +0100

    Some updates (mostly editorial).

--- Combinatorics/Graphs/BoostSupport/Test.cpp
+++ Combinatorics/Graphs/BoostSupport/Test.cpp
@@ -1,32 +1,30 @@
 // Oliver Kullmann, 16.6.2003 (Swansea)
-/* Copyright 2003 - 2007 Oliver Kullmann
+/* Copyright 2003 - 2007, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
-// #include <iostream>
-#include <cassert>
-// #include <utility>
+/*!
+  \file Graphs/BoostSupport/Test.cpp
+  \deprecated Old version of tests (incomplete)
+*/
 
 #include <boost/graph/adjacency_list.hpp>
-// #include <boost/graph/graph_utility.hpp>
-// #include <boost/graph/graph_traits.hpp>
 
 #include "Generators.hpp"
 #include "Tests_Generators.hpp"
 #include "Graphs.hpp"
 
-typedef boost::adjacency_list<boost::listS, boost::vecS, boost::undirectedS> simple_undirected_graph;
+namespace {
 
-int max_size_complete_graph = 10;
+  typedef boost::adjacency_list<boost::listS, boost::vecS, boost::undirectedS> simple_undirected_graph;
+
+  const int max_size_complete_graph = 10;
+
+}
 
 int main() {
-  try {
-    Tests_Generators::complete_graph<simple_undirected_graph>(max_size_complete_graph); 
-  }
-  catch(...) {
-
-  }
+  Tests_Generators::complete_graph<simple_undirected_graph>(max_size_complete_graph);
 }
 
