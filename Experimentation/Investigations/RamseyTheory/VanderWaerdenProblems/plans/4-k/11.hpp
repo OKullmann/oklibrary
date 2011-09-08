729a0f40caf71bf8b3db311459ce20bc3e2fc54c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-08 09:58:27 +0100

    vanderwaerden_2(4,11) > 358.

--- Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/11.hpp
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/4-k/11.hpp
@@ -19,7 +19,7 @@ License, or any later version. */
   </ul>
 
 
-  \todo vanderwaerden_2(4,11) > 347
+  \todo vanderwaerden_2(4,11) > 358
   <ul>
    <li> Start with the palindromic solution for n=347.
     <ol>
@@ -30,6 +30,35 @@ License, or any later version. */
    \verbatim
 > k1=4 k2=11 n0=12 alg="adaptg2wsat" runs=100 cutoff=100000000; nohup RunVdWk1k2 ${k1} ${k2} ${n0} "${alg}" ${runs} ${cutoff} &
    \endverbatim
+   n=358 found satisfiable (a solution was found in 37 runs), with certificate
+   \verbatim
+7,15,20,23,32,34,38,41,45,46,
+55,59,61,66,69,70,72,76,78,80,
+81,87,90,94,103,104,113,117,120,126,
+127,129,131,135,137,138,141,143,146,148,
+156,158,161,162,163,167,169,173,175,177,
+178,184,187,191,200,201,210,214,223,224,
+226,228,232,234,235,238,239,240,243,245,
+249,253,259,260,263,264,266,270,272,274,
+281,284,288,297,307,310,314,320,321,331,
+333,335,336,342,352,355,356
+   \endverbatim
+   For n=359
+   \verbatim
+ 1  2  3  4 10 11 12 13
+13 20  5  3  6 29 23  1
+100
+   \endverbatim
+   </li>
+   <li> n=359:
+    <ol>
+     <li> Starting with the solution for n=359:
+     \verbatim
+> k1=4 k2=11 n0=358 alg="adaptg2wsat" runs=400 cutoff=400000000 oldsol="VanDerWaerden_2-4-11_358.cnf_sol"; nohup RunVdWk1k2 ${k1} ${k2} ${n0} "${alg}" ${runs} ${cutoff} ${oldsol} &
+
+     \endverbatim
+     </li>
+    </ol>
    </li>
   </ul>
 
