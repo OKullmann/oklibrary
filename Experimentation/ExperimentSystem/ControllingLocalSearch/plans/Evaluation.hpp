c1d7ed99e81a990ce3c89c729bfdcc6e66f08f2c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-09-24 12:09:34 +0100

    Removed stale elements of todo.
    MG, please check: this belongs to ticket MG-88.

--- Experimentation/ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp
+++ Experimentation/ExperimentSystem/ControllingLocalSearch/plans/Evaluation.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 11.9.2009 (Swansea)
-/* Copyright 2009, 2010 Oliver Kullmann
+/* Copyright 2009, 2010, 2011 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -30,7 +30,7 @@ License, or any later version. */
 plot(E$alg,E$min)
      \endverbatim
      which plots per algorithm the boxplots of the min-distribution. </li>
-     <li> However it is useless of on the x-axis not all algorithms are
+     <li> However it is useless if on the x-axis not all algorithms are
      listed. </li>
      <li> And an option for cutoff_min is needed, which removes algorithms
      whose best min-value is at least the cutoff-value. </li>
@@ -60,37 +60,6 @@ plot(E$alg,E$min)
    might yield an improvement, while if not then the algorithm wanders
    randomly around without achieving something (so increasing the cutoff
    seems of little value). </li>
-   <li> DONE (function read_ubcsat does this ??? WHAT IS "THIS" ???)
-   Reading into R by
-   \verbatim
-> E = read.table("Data", colClasses = c("character", "factor", "integer", "integer", "integer", "character"))
-   \endverbatim
-   and simple evaluation by "summary(E)" or, better,
-   \verbatim
-> summary(E[,c("sat","min","osteps")])
-   \endverbatim
-   Also the simple
-   \verbatim
-> table(E$min)
-   \endverbatim
-   is interesting (perhaps actually most interesting(!); may be plotted by
-   "plot(table(E$min))"). </li>
-   <li> STATUS ??? how is this different from what ubcsat-okl does ???
-   Given a file Data produced by ubcsat-okl (with first line naming
-   the column headers), one could create a nicer formatting by e.g.
-   \verbatim
-awk 'NR == 1 {printf("%8s %8s %8s %8s %11s\n", $1,$2,$3,$4,$5)} NR != 1 && NF > 0 {printf("%5u %2u %8u %8u %8u %11u\n", $1,$2,$3,$4,$5,$6)}' Data
-   \endverbatim
-   producing e.g.
-   \verbatim
-     sat      min   osteps   msteps        seed
-    1  0       13     8525    20000  1352592974
-    2  0       16     5984    20000  3521402437
-
- 9999  0       15     4420    20000  4085996178
-10000  0       12     5802    20000  3176480652
-   \endverbatim
-   However this should be part of the extended ubcsat-okl (see above). </li>
   </ul>
 
 */
