a425da1645f593d4b05d460c9109bf510a271def
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-12-10 09:50:45 +0000

    Removed trailing whitespace; this should be done when working on a file.

--- System/LegalIssues/Header.s
+++ System/LegalIssues/Header.s
@@ -1,5 +1,5 @@
 # Copyright XXXX Oliver Kullmann
-# This file is part of the OKlibrary. OKlibrary is free software; you can redistribute 
+# This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 # it and/or modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation and included in this library; either version 3 of the 
+# the Free Software Foundation and included in this library; either version 3 of the
 # License, or any later version.
