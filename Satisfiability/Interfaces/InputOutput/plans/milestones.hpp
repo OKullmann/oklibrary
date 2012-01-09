73b112cf2170d8d12d064a398e375553977c6650
Matthew Gwynne <360678@swan.ac.uk>
2012-01-09 11:20:49 +0000

    Adding RenameDimacs C++ application.

--- Satisfiability/Interfaces/InputOutput/plans/milestones.hpp
+++ Satisfiability/Interfaces/InputOutput/plans/milestones.hpp
@@ -16,6 +16,7 @@ License, or any later version. */
   \par
    In Interfaces/InputOutput/plans/general.hpp the following
    topics are handled:
+    - Tests for RenameDimacs
     - Update output for application tests
     - Can not update applications tests for AppendDimacs : DONE
 
