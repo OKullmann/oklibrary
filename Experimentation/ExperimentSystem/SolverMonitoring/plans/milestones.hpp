2c0b8b3f28b7bc1568961bfcbe1ec036505b59c0
Matthew Gwynne <360678@swan.ac.uk>
2012-01-20 19:06:59 +0000

    Adding todo on testing solver extraction scripts.
    Also discussing the need for testing that solvers perform as we expect,
    so we can detect if and when these scripts need to be updated.

--- Experimentation/ExperimentSystem/SolverMonitoring/plans/milestones.hpp
+++ Experimentation/ExperimentSystem/SolverMonitoring/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Matthew Gwynne, 1.8.2011 (Swansea)
-/* Copyright 2011 Oliver Kullmann
+/* Copyright 2011, 2012 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -19,6 +19,7 @@ License, or any later version. */
     - Running experiments
     - Better summary statistics
     - Extraction tools
+    - Testing solver extraction scripts
 
   \par
    In ExperimentSystem/SolverMonitoring/plans/Minisat2.hpp the following
