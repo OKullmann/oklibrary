a77fe16176894fe8b12ad3b37d01ed06937232e9
Matthew Gwynne <360678@swan.ac.uk>
2012-02-19 10:09:13 +0000

    Adding SAT competition todos to the milestones.

--- Experimentation/Investigations/plans/milestones.hpp
+++ Experimentation/Investigations/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 1.1.2011 (Swansea)
-/* Copyright 2011 Oliver Kullmann
+/* Copyright 2011, 2012 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -18,6 +18,16 @@ License, or any later version. */
 
   \par Cryptography/DataEncryptionStandard version 0.1.3
   (see Cryptography/DataEncryptionStandard/plans/milestones.hpp)
+
+
+  \par
+   In Experimentation/Investigations/plans/SATCompetition.hpp the following
+   topics are handled:
+    - Update and completion of plans regarding SAT 2011
+    - Formats for SAT 2011
+    - Categories / "tracks" for SAT 2011
+    - Benchmarks for SAT 2011 : DONE
+    - Supporting documents : DONE
 
   \par
    In Experimentation/Investigations/plans/general.hpp the following topics
@@ -39,6 +49,12 @@ License, or any later version. */
 
 
   \par Version 0.3.2 :
+
+
+  \par
+   In Experimentation/Investigations/plans/SATCompetition.hpp the following
+   topics are handled:
+    - Benchmarks for SAT 2012
 
   \par
    In Experimentation/Investigations/plans/general.hpp the following topics
