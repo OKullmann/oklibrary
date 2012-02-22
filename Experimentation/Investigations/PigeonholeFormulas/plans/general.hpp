c7458ec138e1242168096a776524a5bf8e93fe09
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-02-22 07:41:33 +0000

    Completed first impression of SAT solvers on EPHP.

--- Experimentation/Investigations/PigeonholeFormulas/plans/general.hpp
+++ Experimentation/Investigations/PigeonholeFormulas/plans/general.hpp
@@ -633,7 +633,7 @@ output_res_php(3,35);
    <li> Computed by output_weak_php_unsat_ext_stdname(n). </li>
    <li> Performance of SAT solvers (n is the number of holes):
     <ol>
-     <li> cryptominisat (csltok with 2.4 GHz):
+     <li> First impression with cryptominisat (csltok with 2.0 GHz):
      \verbatim
 n=10
 18.65s
@@ -641,13 +641,20 @@ n=11
 38.64s
 n=12
 249.72s
-# somewhat better than PHP itself
+n=13
+886.28s
+n=14
+13187.05s
+# with n=14 worse than PHP itself
      \endverbatim
      </li>
      <li> The other solvers seem to perform worse (and also not better than
      their PHP-performance). Though one needs to see complete data. </li>
     </ol>
    </li>
+   <li> So apparently no solver can make use of the extension. This seems
+   understandable, given the complicated nature of the resolution proof for
+   the extension --- all clauses have to be used, in a special order. </li>
   </ul>
 
 */
