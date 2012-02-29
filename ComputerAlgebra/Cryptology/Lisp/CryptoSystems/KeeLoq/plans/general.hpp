52da4d54967ad0bfce12ad36bb17af997e86151e
Matthew Gwynne <360678@swan.ac.uk>
2012-02-29 09:37:19 +0000

    Adding todo on finding test vectors for KeeLoq.

--- ComputerAlgebra/Cryptology/Lisp/CryptoSystems/KeeLoq/plans/general.hpp
+++ ComputerAlgebra/Cryptology/Lisp/CryptoSystems/KeeLoq/plans/general.hpp
@@ -10,6 +10,26 @@ License, or any later version. */
   \brief Plans for the KeeLoq crypto-system in Maxima/Lisp
 
 
+  \todo Test vectors
+  <ul>
+   <li> We need test vectors for the testing of the KeeLoq implementation.
+   </li>
+   <li> Test vectors are not provided in
+   [Algebraic and Slide attacks on KeeLoq; Gregory Bard, Nicholas Courtois and
+   David Wagner], and the KeeLoq specification documents referenced do
+   not provide them either. </li>
+   <li> [A Practical Attack on KeeLoq; Aerts, Biham, De Moitié, Mulder,
+   Dunkelman, Indesteege, Keller, Preneel, Vandenbosch, Verbauwhede] mentions
+   the computation of test vectors from a circuit implementation of KeeLoq.
+   </li>
+   <li> MG has e-mailed Eli Biham asking for these test vectors. </li>
+   <li> There is a C implementation of KeeLoq at
+   http://cryptolib.com/ciphers/keeloq/KeeLoq.c, and so test vectors could be
+   derived from this, however, the correctness of this implementation isn't
+   clear. </li>
+  </ul>
+
+
   \todo Add todos
 
 
