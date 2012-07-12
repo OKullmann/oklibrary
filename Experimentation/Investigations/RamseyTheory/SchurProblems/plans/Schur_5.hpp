207829835ae14f2a72df167a0fa17d4ce539430a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-07-12 17:03:02 +0100

    Update of data for OKsolver on schur(5) =? 160.

--- Experimentation/Investigations/RamseyTheory/SchurProblems/plans/Schur_5.hpp
+++ Experimentation/Investigations/RamseyTheory/SchurProblems/plans/Schur_5.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 17.4.2009 (Swansea)
-/* Copyright 2009 Oliver Kullmann
+/* Copyright 2009, 2012 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,55 +10,119 @@ License, or any later version. */
   \brief On computing schur(5)
 
 
+  \todo Statistics
+  <ul>
+   <li> Creation:
+   \verbatim
+output_schur_stdname(5,160);
+   \endverbatim
+   </li>
+   <li> Counts:
+   \verbatim
+> cat Schur_5_160.cnf | ExtendedDimacsFullStatistics-O3-DNDEBUG
+     pn      pc      n    nmi       c        l     n0   n0mi      c0       l0  cmts
+    800   33760    800    800   33760    99600     NA     NA   33760    99600   801
+ length   count
+      2    2000
+      3   31600
+      5     160
+   \endverbatim
+   </li>
+  </ul>
+
+
   \todo OKsolver_2002
   <ul>
    <li> With a cluster and a few months it looks feasible without any
    special action:
    \verbatim
 > OKsolver_2002-O3-DNDEBUG -M -D20 Schur_5_160.cnf
+Name of file, monitoring depth and number of monitoring nodes:
+ Schur_5_160.cnf,   20,    1048576
+level: nodes processed, average nodes, predicted total nodes, time for monitoring node, average time, predicted remaining time, single nodes, autarkies, depth
 
-Name of file and number of monitoring nodes:
- Schur_5_160.cnf, 1048576
-  1:   387,   15.4, 16179512.2
-  2: 13720,  387.6, 203208398.3
-  3:  1568,  441.4, 154290526.5
-  4: 17781,  969.0, 254011324.1
-  5:  4987, 1102.6, 231232974.1
-  6:  6499, 1310.7, 229063611.7
-  7: 18361, 1819.8, 272591989.1
-  8: 40898, 2820.2, 369652987.8
-  9: 10636, 3088.2, 359799455.0
- 10: 24184, 3686.7, 386575875.8
- 11: 13620, 4012.0, 382437393.0
- 12: 35767, 4868.0, 425370957.9
- 13: 10667, 5114.3, 412514363.2
- 14:  7705, 5335.4, 399605480.3
- 15: 18446, 5912.8, 413326668.3
-
+...
+14:   7705  14770.00  1.55E+10   250.47s   429.83s    14y 106d 12h 50m 47s     0     0   58
+15:  18446  15015.07  1.57E+10   570.63s   439.22s    14y 220d 10h 39m 21s     0     0   58
+...
+39:   3382  16130.87  1.69E+10    93.10s   478.83s    15y 336d  1h 12m 16s     0     0   58
+40:   5527  15865.77  1.66E+10   144.01s   470.46s    15y 234d 11h  3m 12s     0     0   58
 s UNKNOWN
-c sat_status=2 initial_maximal_clause_length=5 initial_number_of_variables=800 initial_number_of_clauses=33760 initial_number_of_literal_occurrences=99600 running_time(s)=6116.4 number_of_nodes=231351 number_of_single_nodes=14 number_of_quasi_single_nodes=0 number_of_2-reductions=3696760 number_of_pure_literals=0 number_of_autarkies=0 number_of_missed_single_nodes=282 max_tree_depth=58 number_of_table_enlargements=0 reduced_maximal_clause_length=0 reduced_number_of_variables=0 reduced_number_of_clauses=0 reduced_number_of_literal_occurrences=0 number_of_1-autarkies=65040054 number_of_initial_unit-eliminations=0 number_of_new_2-clauses=0 maximal_number_of_added_2-clauses=0 initial_number_of_2-clauses=2000 file_name=Schur_5_160.cnf
+c sat_status                            2
+c initial_maximal_clause_length         5
+c initial_number_of_variables           800
+c initial_number_of_clauses             33760
+c initial_number_of_literal_occurrences 99600
+c number_of_initial_unit-eliminations   0
+c reddiff_maximal_clause_length         0
+c reddiff_number_of_variables           0
+c reddiff_number_of_clauses             0
+c reddiff_number_of_literal_occurrences 0
+c number_of_2-clauses_after_reduction   2000
+c running_time(sec)                     19635.3
+c number_of_nodes                       667953
+c number_of_single_nodes                18
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                10676278
+c number_of_pure_literals               0
+c number_of_autarkies                   1
+c number_of_missed_single_nodes         359
+c max_tree_depth                        58
+c proportion_searched                   3.908341e-05
+c proportion_single                     6.698131e-11
+c total_proportion                      3.908347571268678e-05
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 187851110
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             Schur_5_160.cnf
+
+19635.3 / 3.908347571268678e-05 ~ 15.9 years
    \endverbatim
    </li>
    <li> And with initial symmetry breaking (using
    output_schur_sb_stdname(5,160)) a factor of nearly 5 should be achievable:
    \verbatim
 > OKsolver_2002-O3-DNDEBUG -M -D20 Schur_sb_5_160.cnf
-232: 55174, 149176.8, 674088581.0
-s UNKNOWN
-c sat_status=2 initial_maximal_clause_length=5 initial_number_of_variables=800 initial_number_of_clauses=33764 initial_number_of_literal_occurrences=99604 running_time(s)=150682.3 number_of_nodes=5582755 number_of_single_nodes=261 number_of_quasi_single_nodes=0 number_of_2-reductions=91981749 number_of_pure_literals=0 number_of_autarkies=2 number_of_missed_single_nodes=6184 max_tree_depth=73 number_of_table_enlargements=0 reduced_maximal_clause_length=0 reduced_number_of_variables=6 reduced_number_of_clauses=814 reduced_number_of_literal_occurrences=2574 number_of_1-autarkies=1562724854 number_of_initial_unit-eliminations=6 number_of_new_2-clauses=0 maximal_number_of_added_2-clauses=0 initial_number_of_2-clauses=2137 file_name=Schur_sb_5_160.cnf
-233: 67478, 151039.1, 679574131.9
+Name of file, monitoring depth and number of monitoring nodes:
+ Schur_sb_5_160.cnf,   20,    1048576
+level: nodes processed, average nodes, predicted total nodes, time for monitoring node, average time, predicted remaining time, single nodes, autarkies, depth
 
-292: 43443, 215949.6, 775262129.9
-293: 31644, 216809.7, 775692533.3
+...
+   548:  22149  41402.39  4.34E+10   648.51s  1099.41s    36y 195d 19h  1m 23s     0     0   85
 s UNKNOWN
-c sat_status=2 initial_maximal_clause_length=5 initial_number_of_variables=800 initial_number_of_clauses=33764 initial_number_of_literal_occurrences=99604 running_time(s)=216907.2 number_of_nodes=8093092 number_of_single_nodes=529 number_of_quasi_single_nodes=0 number_of_2-reductions=132323230 number_of_pure_literals=0 number_of_autarkies=14 number_of_missed_single_nodes=11142 max_tree_depth=74 number_of_table_enlargements=0 reduced_maximal_clause_length=0 reduced_number_of_variables=6 reduced_number_of_clauses=814 reduced_number_of_literal_occurrences=2574 number_of_1-autarkies=2264984266 number_of_initial_unit-eliminations=6 number_of_new_2-clauses=0 maximal_number_of_added_2-clauses=0 initial_number_of_2-clauses=2137 file_name=Schur_sb_5_160.cnf
+c sat_status                            2
+c initial_maximal_clause_length         5
+c initial_number_of_variables           800
+c initial_number_of_clauses             33764
+c initial_number_of_literal_occurrences 99604
+c number_of_initial_unit-eliminations   6
+c reddiff_maximal_clause_length         0
+c reddiff_number_of_variables           6
+c reddiff_number_of_clauses             814
+c reddiff_number_of_literal_occurrences 2574
+c number_of_2-clauses_after_reduction   2137
+c running_time(sec)                     602715.8
+c number_of_nodes                       22697398
+c number_of_single_nodes                1279
+c number_of_quasi_single_nodes          0
+c number_of_2-reductions                367966941
+c number_of_pure_literals               0
+c number_of_autarkies                   320
+c number_of_missed_single_nodes         23646
+c max_tree_depth                        85
+c proportion_searched                   5.231135e-04
+c proportion_single                     4.278459e-10
+c total_proportion                      0.0005231138784438372
+c number_of_table_enlargements          0
+c number_of_1-autarkies                 6357718175
+c number_of_new_2-clauses               0
+c maximal_number_of_added_2-clauses     0
+c file_name                             Schur_sb_5_160.cnf
+
+602715.8 / 0.0005231138784438372 ~ 36.5 years
    \endverbatim
    </li>
-   <li> So it seems that on a rather modest machine with 1 processor around
-   8000 days are needed. Splitting the problem into 2^13 = 8192 jobs
-   this would need a day per job (see "Simple parallelisation" in
-   Solvers/OKsolver/SAT2002/plans/general.hpp), and given 100 processor,
-   in altogether 3 months it should be done. </li>
   </ul>
 
 
