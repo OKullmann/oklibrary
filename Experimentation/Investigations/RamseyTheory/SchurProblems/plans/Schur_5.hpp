a1b584cecc92bb9f872bf87e42142a4b77ec532d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-07-16 00:34:18 +0100

    schur(5) >= 161, and improvements of general schur-mechanism.

--- Experimentation/Investigations/RamseyTheory/SchurProblems/plans/Schur_5.hpp
+++ Experimentation/Investigations/RamseyTheory/SchurProblems/plans/Schur_5.hpp
@@ -9,6 +9,12 @@ License, or any later version. */
   \file Experimentation/Investigations/RamseyTheory/SchurProblems/plans/general.hpp
   \brief On computing schur(5)
 
+  The conjecture is schur(5) = 161.
+
+  (In [Ramsey Theory on the Integers; Landman, Robertson, 2003] we have the
+  conjecture "schur(5) = 160", but already 1994 it was shown schur(5) >= 161
+  (see below).)
+
 
   \todo Statistics
   <ul>
@@ -27,6 +33,26 @@ output_schur_stdname(5,160);
       3   31335
       5     160
    \endverbatim
+   </li>
+  </ul>
+
+
+  \todo Certificates
+  <ul>
+   <li> From [A Lower Bound for Schur Numbers and Multicolor Ramsey Numbers of
+   K_3; Exoo, 1994]:
+   \verbatim
+C : uncompresss_palindromic_subsets(160,[
+{4,5,15,16,22,28,29,39,40,41,42,48,49,59},
+{2,3,8,14,19,20,24,25,36,46,47,51,62,73},
+{7,9,11,12,13,17,27,31,32,33,35,37,53,56,57,61,79},
+{1,6,10,18,21,23,26,30,34,38,43,45,50,54,65,74},
+{44,52,55,58,60,63,64,66,67,68,69,70,71,72,75,76,77,78,80}
+]);
+certificate_schur_p(5,160,C);
+  true
+   \endverbatim
+   and thus schur(5) >= 161 (it seems that was overlooked in the literature!).
    </li>
   </ul>
 
@@ -190,7 +216,8 @@ BestSolution_Median = 1
 BestSolution_Min = 1
 BestSolution_Max = 1
    \endverbatim
-   so the conjecture schur(5)=160 seems justified. </li>
+   so the conjecture schur(5)=160 seems justified --- however this is false
+   (see above)! </li>
    <li> We need to determine the best Ubcsat-solver. </li>
   </ul>
 
