2b2e549695365cc663d3ef307816c8476e435e5c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-07-25 08:09:14 +0100

    Update on plans regarding palindromic versions of hypergraphs.

--- ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
+++ ComputerAlgebra/Hypergraphs/Lisp/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 30.11.2007 (Swansea)
-/* Copyright 2007, 2008, 2009, 2010, 2011 Oliver Kullmann
+/* Copyright 2007, 2008, 2009, 2010, 2011, 2012 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -99,6 +99,14 @@ statistics_hg(G) := statistics_fcs(G)$
    given hypergraph in the maxima system. </li>
    <li> See also "Representing edge and vertex labellings" in 
    ComputerAlgebra/Graphs/Lisp/plans/general.hpp . </li>
+  </ul>
+
+
+  \todo Palindromic versions
+  <ul>
+   <li> We have palindromise_vdw_ohg as well as schurtriples_pd_ohg and
+   wschurtriples_pd_ohg. </li>
+   <li> Perhaps a more general framework is possible? </li>
   </ul>
 
 */
