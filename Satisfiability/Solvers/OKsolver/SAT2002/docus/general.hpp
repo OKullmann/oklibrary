189ac75082feecf62357c345dd373c577fd5dccb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2013-05-21 22:47:26 +0100

    Updates on specification of the OKsolver.

--- Satisfiability/Solvers/OKsolver/SAT2002/docus/general.hpp
+++ Satisfiability/Solvers/OKsolver/SAT2002/docus/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 13.9.2007 (Swansea)
-/* Copyright 2007, 2008, 2009, 2011, 2012 Oliver Kullmann
+/* Copyright 2007, 2008, 2009, 2011, 2012, 2013 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -27,8 +27,16 @@ License, or any later version. */
 
   <h2> Specification </h2>
 
-  Procedural specifications at the Lisp/Maxima level are given in
-  ComputerAlgebra/Satisfiability/Lisp/Backtracking/OKsolver2002.mac.
+  <ul>
+   <li> See the report "Investigating the behaviour of a SAT solver on random
+   formulas" available at
+   http://cs.swan.ac.uk/~csoliver/papers.html#OKsolverReport2002 .</li>
+   <li> Procedural specifications at the Lisp/Maxima level are given in
+   ComputerAlgebra/Satisfiability/Lisp/Backtracking/OKsolver2002.mac. </li>
+   <li> A general theoretical underpinning is given in the SAT Handbook article
+   " Fundaments of Branching Heuristics" available at
+   http://cs.swan.ac.uk/~csoliver/papers.html#Handbook2009Fundaments . </li>
+  </ul>
 
 
   <h2> Macro options </h2>
