8c8dc552ffb5969094dab1b0015878d705f926b3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2013-06-05 04:57:51 +0100

    Started file for general observations regarding solvers on palindromic problems.

---
+++ Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/SolversPdVdw.hpp
@@ -0,0 +1,12 @@
+// Oliver Kullmann, 3.6.2013 (Swansea)
+/* Copyright 2013 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*!
+  \file Experimentation/Investigations/RamseyTheory/VanderWaerdenProblems/plans/3-k/SolversPdVdw.hpp
+  \brief On SAT solvers computing palindromic vdW-numbers pdvdw_2(3,k)
+
+*/
