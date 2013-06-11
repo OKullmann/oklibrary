012d8d25dd0fd54117dfa4dabe6ffa215e9136e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2013-06-12 00:43:55 +0100

    Corrected two errors connected to 64-bit clauses.
    Bit-shifts must work with the correct type, and the heuristics must
    work with doubles (the latter was also buggy for 32-bit clauses).
    
    Now MAX_CLAUSE_LENGTH=64 incurs roughly a 10% speed penalty, and thus
    default reset to 32.

--- Satisfiability/Solvers/TawSolver/tawSolver.cpp
+++ Satisfiability/Solvers/TawSolver/tawSolver.cpp
@@ -24,6 +24,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <limits>
 #include <vector>
 
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -33,12 +34,22 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <sys/resource.h>
 
 
-constexpr int MAX_CLAUSES = 300000;
+constexpr int MAX_CLAUSES = 100000;
 static_assert(MAX_CLAUSES > 0, "MAX_CLAUSES must be positive.");
-constexpr int MAX_VARS = 4096;
-typedef unsigned long Clause_content;
-constexpr int MAX_CLAUSE_LENGTH {std::numeric_limits<Clause_content>::digits};
-// If longer clauses are needed, replace Clause_content with bigger uint type.
+constexpr int MAX_VARS = 1000;
+static_assert(MAX_VARS > 0, "MAX_VARS must be positive.");
+
+#ifndef MAX_CLAUSE_LENGTH
+#define MAX_CLAUSE_LENGTH 32
+#endif
+constexpr int maximal_clause_length = MAX_CLAUSE_LENGTH;
+static_assert(maximal_clause_length==32 or maximal_clause_length==64,"Currently only MAX_CLAUSE_LENGTH=32,64 is possible.");
+#if MAX_CLAUSE_LENGTH == 32
+typedef uint32_t Clause_content;
+#else
+typedef uint64_t Clause_content;
+#endif
+static_assert(std::numeric_limits<Clause_content>::digits==maximal_clause_length,"Error with choice of type \"Clause_content\".");
 
 enum Error_codes {
   missing_file_error=1, file_reading_error=2, clause_length_error=3 };
@@ -65,8 +76,8 @@ constexpr int log2(const Clause_content n) {return (n <= 1)?0:1+log2(n/2);}
 inline constexpr Clause_content bp(const unsigned N, const unsigned i) {
   return (i < N-1) ? bp(N-1,i) * (1 + pow22(N-1)) : pow22(N) - pow22(N-1);
 }
-constexpr int N = log2(MAX_CLAUSE_LENGTH);
-static_assert(pow2(N) == (unsigned) MAX_CLAUSE_LENGTH, "Number of bits in \"Clause_content\" not a power of 2.");
+constexpr int N = log2(maximal_clause_length);
+static_assert(pow2(N) == (unsigned) maximal_clause_length, "Number of bits in \"Clause_content\" not a power of 2.");
 static_assert(N==5 or N==6, "Unexpected size of type \"Clause_content\".");
 const Clause_content b[6] {bp(N,0),bp(N,1),bp(N,2),bp(N,3),bp(N,4), (N==6)?bp(N,5):0}; // Unfortunately there is no reasonable way in C++ to just define b[N].
 inline int log2s(const Clause_content v) {
@@ -101,7 +112,7 @@ int n_changes[MAX_VARS][2];
 unsigned int n_clauses, r_clauses, n_init_clauses, n_vars, depth = 0;
 unsigned int max_clause_len = 0;
 
-int current_working_clause[MAX_CLAUSE_LENGTH], cwc_length;
+int current_working_clause[maximal_clause_length], cwc_length;
 
 int gucl_stack[MAX_VARS], n_gucl = 0;
 int contradictory_unit_clauses = false;
@@ -141,8 +152,8 @@ bool read_a_clause_from_file(FILE* const f) {
     if (fscanf(f, "%d", &x) == EOF) return false;
     if (x == 0) break;
     if (checker[abs(x)]==0) {
-      if (cwc_length >= MAX_CLAUSE_LENGTH) {
-        printf("Clauses can have at most %u elements.\n", MAX_CLAUSE_LENGTH);
+      if (cwc_length >= maximal_clause_length) {
+        printf("Clauses can have at most %u elements.\n", maximal_clause_length);
         exit(clause_length_error);
       }
       current_working_clause[cwc_length++] = x;
@@ -159,18 +170,18 @@ bool read_a_clause_from_file(FILE* const f) {
   return true;
 }
 
-void add_a_clause_to_formula(const int A[], const unsigned int n) {
+void add_a_clause_to_formula(const int A[], const unsigned n) {
   if (n == 0) return;
   clauses[n_clauses].number = n_clauses;
   clauses[n_clauses].status = true;
   clauses[n_clauses].length = n;
-  clauses[n_clauses].value = ((1<<n)-1);
+  clauses[n_clauses].value = ((Clause_content) 1 << n) - 1;
   clauses[n_clauses].c_ucl = 0;
   clauses[n_clauses].literals = (int*) malloc((n + 1) * sizeof(int));
 
   if (n>max_clause_len) max_clause_len = n;
 
-  for (unsigned int i=0; i<n; ++i) {
+  for (int i=0; i<(int)n; ++i) {
     const int p = abs(A[i]), q = A[i]>0 ? POS : NEG;
     vars[p][q].var_in_clauses = (int*) realloc(vars[p][q].var_in_clauses,
                                 (vars[p][q].n_occur+1) * sizeof(int));
@@ -209,8 +220,9 @@ void reduce(const int v) {
    if (max_size >= changes.size()) changes.resize(max_size);
    for (unsigned int i=0; i<occur_true; ++i) {
      const int m = vars[p][q].var_in_clauses[i];
-     if(!clauses[m].status) continue;
+     if(not clauses[m].status) continue;
      clauses[m].status = false;
+     assert(r_clauses >= 1);
      --r_clauses;
      changes[changes_index++].clause_number = m;
      ++n_changes[depth][POS];
@@ -224,9 +236,10 @@ void reduce(const int v) {
    for (unsigned int i=0; i<occur_false; ++i) {
      const int m = vars[p][q].var_in_clauses[i];
      if (!clauses[m].status) continue;
+     assert(clauses[m].length >= 2);
      --clauses[m].length;
      const int n = vars[p][q].var_in_clause_locs[i];
-     clauses[m].value -= 1 << n;
+     clauses[m].value -= (Clause_content) 1 << n;
 
      changes[changes_index++] = {m,n};
      ++n_changes[depth][NEG];
@@ -253,6 +266,7 @@ void reduce(const int v) {
 
 void reverse(const int v) {
   const int p = abs(v);
+  assert(depth >= 1);
   --depth;
   while (n_changes[depth][NEG]) {
     --n_changes[depth][NEG];
@@ -263,7 +277,7 @@ void reverse(const int v) {
       checker[abs(clauses[m].c_ucl)] = 0;
       clauses[m].c_ucl = 0;
     }
-    clauses[m].value += 1 << n;
+    clauses[m].value += (Clause_content) 1 << n;
   }
 
   while (n_changes[depth][POS]) {
@@ -279,26 +293,28 @@ void reverse(const int v) {
 inline int get_variable_2sjw() {
   const auto mlen = max_clause_len;
   const auto nvar = n_vars;
-  unsigned int max = 0;
+  double max = 0;
   int v = 0;
 
   for (unsigned int i=1; i<=nvar; ++i) {
-    const auto& vpos = vars[i][POS]; const auto& vneg = vars[i][NEG];
+    const auto& vpos = vars[i][POS];
+    const auto& vneg = vars[i][NEG];
     if (vpos.status or vneg.status) {
-      unsigned int pz = 0, nz = 0;
+      double pz = 0;
       {const auto pos_occur = vpos.n_occur;
        for(unsigned int k=0; k<pos_occur; ++k) {
          const unsigned int ell = vpos.var_in_clauses[k];
-         pz += clauses[ell].status << (mlen - clauses[ell].length);
+         pz += (Clause_content) clauses[ell].status << (mlen - clauses[ell].length);
        }
       }
+      double nz = 0;
       {const auto neg_occur = vneg.n_occur;
        for (unsigned int k=0; k<neg_occur; ++k) {
          const unsigned int ell = vneg.var_in_clauses[k];
-         nz += clauses[ell].status << (mlen - clauses[ell].length);
+         nz += (Clause_content) clauses[ell].status << (mlen - clauses[ell].length);
        }
       }
-      const unsigned int s = pz + nz;
+      const double s = pz + nz;
       if (s > max) { max = s; v = (pz >= nz) ? i : -i; }
     }
   }
@@ -333,6 +349,8 @@ bool dpll() {
   }
   if (!r_clauses) return true;
   const int v = get_variable_2sjw();
+  assert(v);
+  assert(depth < n_vars);
   out[depth] = v;
   reduce(v);
   if (dpll()) return true;
