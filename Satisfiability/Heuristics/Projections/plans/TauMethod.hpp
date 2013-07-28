eee14df1d549761ab3c1712949f076c8a1b49943
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2013-07-28 14:59:26 +0100

    Updates on general plans on using the tau-function.

--- Satisfiability/Heuristics/Projections/plans/TauMethod.hpp
+++ Satisfiability/Heuristics/Projections/plans/TauMethod.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 16.6.2007 (Swansea)
-/* Copyright 2007 Oliver Kullmann
+/* Copyright 2007, 2013 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -10,20 +10,33 @@ License, or any later version. */
   \brief Plans regarding the tau-projection
 
 
+  \todo Connections
+  <ul>
+   <li> See Satisfiability/Solvers/TawSolver/plans/general.hpp for experiments
+   and implementations with the tawSolver. </li>
+  </ul>
+
+
   \todo Efficient computation of the tau-function:
-  <ol>
-   <li> Two arguments. </li>
-   <li> At most three arguments. </li>
-   <li> At most k arguments (k constant; using metaprogramming). </li>
-   <li> Arbitrary many arguments. </li>
-  </ol>
-  Using tables should speed up the computation.
+  <ul>
+   <li> Various versions:
+    <ol>
+     <li> Two arguments. </li>
+     <li> At most three arguments. </li>
+     <li> At most k arguments (k constant; using metaprogramming). </li>
+     <li> Arbitrary many arguments. </li>
+    </ol>
+   </li>
+   <li> Using tables should speed up the computation. </li>
+  </ul>
 
 
   \todo Experimentation
   <ul>
    <li> More experimentation about the difference between tau with
    2 arguments and the product. </li>
+   <li> For the OKsolver we didn't get a decrease in node-count, but for the
+   tawSolver on the vdW-instances we get one. </li>
   </ul>
 
 
@@ -36,7 +49,8 @@ License, or any later version. */
 
   \todo Theory
   <ul>
-   <li> First collect all the tau-theory from the [Kullmann/Luckhardt]-papers. </li>
+   <li> First collect all the tau-theory from the
+   [Kullmann/Luckhardt]-papers. </li>
   </ul>
 
 */
