df79a96bc25a64adbd1ef7ebca6fb5f171a3c150
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2015-12-05 21:25:41 +0000

    Partial update of plans regarding tau-computations.

--- Satisfiability/Heuristics/Projections/plans/general.hpp
+++ Satisfiability/Heuristics/Projections/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 16.6.2007 (Swansea)
-/* Copyright 2007, 2008 Oliver Kullmann
+/* Copyright 2007, 2008, 2015 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,6 +8,13 @@ License, or any later version. */
 /*!
   \file Satisfiability/Heuristics/Projections/plans/general.hpp
   \brief Module for projections (making branching tuples comparable)
+
+
+  \todo Update
+  <ul>
+   <li> In OKlib/Satisfiability/Solvers/TawSolver there are various
+   implementations. </li>
+  </ul>
 
 
   \todo Incorporate the theory from OK's SAT-Handbook-article: planning
--- Satisfiability/Heuristics/Projections/plans/milestones.hpp
+++ Satisfiability/Heuristics/Projections/plans/milestones.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 24.1.2008 (Swansea)
-/* Copyright 2008 Oliver Kullmann
+/* Copyright 2008, 2015 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -16,6 +16,7 @@ License, or any later version. */
   \par
    In Satisfiability/Heuristics/Projections/plans/general.hpp the following
    topics are handled:
+    - Update
     - Incorporate the theory from OK's SAT-Handbook-article: planning
 
  
