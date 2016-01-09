b87aa17ce7fe138ce21e77eddb24b1b747c29214
PENGYUN JI <jipengyun@hotmail.com>
2016-01-09 16:18:38 +0000

    newest version of testing shell.

--- Satisfiability/Solvers/TawSolver/app_tests/test.sh
+++ Satisfiability/Solvers/TawSolver/app_tests/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#newest version of testing shell.
+#testing all veriations of TawSolver by the cases in app_tests/test_cases.
+#create directory dimacs_output to store the outputs when doing the execution of this test bash.
+#testing results are showing on console.
+
 bin_dir=..
 input_dir=.
 output_dir=output
@@ -10,18 +15,24 @@ list_test_cases="$(find test_cases/basic -name '*.cnf') $(find test_cases/full -
 for bin in ${bin_list}; do
 	echo "Test ${bin}"
 	for test in ${list_test_cases}; do
-		output=$(dirname ${test})/$(basename ${bin})_$(basename ${test} .cnf)
+		mkdir $(dirname ${test})/dimacs_output
+		output=$(dirname ${test})/dimacs_output/$(basename ${bin})_$(basename ${test} .cnf)
 		echo -e ${bin} ${test} ${output}
 		${bin} ${test} ${output}
-		template_file=$(dirname ${test})/dimacs_output/$(basename ${test} .cnf)
+		#template_file=$(dirname ${test})/dimacs_output/$(basename ${test} .cnf)
 		
-		output_text=$(cat ${output})
-		template_text=$(cat ${template_file})
-		if [[ "${output_text}" =~ ${template_text} ]]; then 
-			echo "The output is the same as template file"
-		else
-			echo "Error: the output is different from template file"
-		fi
+		#output_text=$(cat ${output})
+		#template_text=$(cat ${template_file})
+		#if [[ "${output_text}" =~ ${template_text} ]]; then 
+		#	echo "The output is the same as template file"
+		#else
+		#	echo "Error: the output is different from template file"
+			#echo "Output:"
+			#cat ${output}
+			#echo
+			#echo "Template:"
+			#cat ${template_file}
+		#fi
 		echo
 	done
 done
