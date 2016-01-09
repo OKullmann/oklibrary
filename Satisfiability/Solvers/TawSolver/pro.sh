9cdd5375365b996c8259a566924c83190c3afd0b
PENGYUN JI <jipengyun@hotmail.com>
2016-01-09 15:31:28 +0000

    testing the all four variations of TawSolver by test_cases.

--- Satisfiability/Solvers/TawSolver/pro.sh
+++ Satisfiability/Solvers/TawSolver/pro.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Running the test cases sepseparately by four variations of TawSolver(including TawSolver).
+#further modifications undertaking in app_tests folder.
+
 
 name=.cnf
 txt=.txt
