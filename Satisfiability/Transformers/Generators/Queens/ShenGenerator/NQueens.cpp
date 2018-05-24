d754b4aaef3795e79b74e723a6ed7b157f5b917d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-05-24 12:47:09 +0100

    Explained numbering of fields.

--- Satisfiability/Transformers/Generators/Queens/ShenGenerator/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/ShenGenerator/NQueens.cpp
@@ -34,6 +34,22 @@ the vertex-cover count of the N-Queens graph).
 now allows also the graph-versions (without ALO-clauses) for Rooks- and
 Bishops-versions; "S" stands for "SAT".
 
+The NxN fields of the chess board have coordinates (indices) as follows
+(using N=4):
+
+ (3,0) (3,1) (3,2) (3,3)
+ (2,0) (2,1) (2,2) (2,3)
+ (1,0) (1,1) (1,2) (1,3)
+ (0,0) (0,1) (0,2) (0,3)
+
+The number of variables is row-wise, starting at the bottom row (using
+again N=4):
+
+ 13 14 15 16
+ 9  10 11 12
+ 5  6  7  8
+ 1  2  3  4
+
 Examples:
 
 > ./qgen 8
