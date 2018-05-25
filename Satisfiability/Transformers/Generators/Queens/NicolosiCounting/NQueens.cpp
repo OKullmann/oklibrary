6d9a74d8d38faaa81e56bcdf99764836350c274e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-05-25 22:31:40 +0100

    Added a node count.

--- Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
@@ -33,8 +33,9 @@ SOFTWARE.
 namespace {
 typedef std::uint_fast64_t count_t;
 typedef std::uint32_t queen_t;
-count_t count = 0;
+count_t count = 0, nodes = 0;
 inline void backtracking(const queen_t mask, const queen_t pos, const queen_t r, const queen_t d1, const queen_t d2) noexcept {
+  ++nodes;
   for (queen_t c = ~pos & mask; c;) {
     const queen_t i = -c & c;
     const queen_t rpi = r + i;
@@ -50,17 +51,17 @@ inline void backtracking(const queen_t mask, const queen_t pos, const queen_t r,
 int main(const int argc, const char* const argv[]) {
   if (argc != 2) { std::cout << "Usage[qcount]: N\n"; return 0; }
   const unsigned long arg1 = std::stoul(argv[1]);
-  if (arg1 == 0) { std::cout << 1 << "\n"; return 0; }
+  if (arg1 == 0) { std::cout << 1 << " " << nodes << "\n"; return 0; }
   if (arg1 > 32) { std::cerr << " N <= 32 required.\n"; return 1; }
   const queen_t N = arg1;
   const queen_t mask = (1 << N) - 1;
   if (N % 2 == 0) {
     backtracking(mask, (1 << (N / 2)) - 1, 0, 0, 0);
-    std::cout << 2*count;
+    std::cout << 2*count << " " << nodes << "\n";
   } else {
     backtracking(mask, (1 << ((N / 2) + 1)) - 1, 0, 0, 0);
     const count_t count1 = count; count = 0;
     backtracking(mask, ~(1 << (N / 2)), 0, 0, 0);
-    std::cout << 2*count1 + count;
+    std::cout << 2*count1 + count << " " << nodes << "\n";
   }
 }
