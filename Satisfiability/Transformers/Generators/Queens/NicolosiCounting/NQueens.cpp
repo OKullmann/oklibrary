ae1cd7eed0a84ba4d5b013deb4d886b5bdcf31fc
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-05-27 22:34:26 +0100

    Completed.

--- Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
@@ -36,13 +36,20 @@ The basic idea is the same as explained at
 Output of the solution count and the number of nodes; e.g.
 
 > ./qcount 8
-92 983
+92 615
 
 That is, 92 solutions (nonattacking placements of 8 queens on the 8x8 board),
-using 983 nodes in the backtracking tree.
+using 615 nodes in the backtracking tree.
 
 For compilation the Makefile in the same directory as this file can be used.
 It produces also the debugging-version "qcount_debug".
+To set NMAX (maximal allowed value of N; default 64), use e.g.
+> CXXFLAGS="-DNMAX=32" make
+
+TODOS:
+
+1. Use signals (like the tawSolver), to show current counts.
+2. Use futures for paralelisation.
 
 */
 
@@ -50,17 +57,32 @@ It produces also the debugging-version "qcount_debug".
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <limits>
 
 namespace {
 
-typedef std::uint8_t input_t;
-typedef std::uint_fast64_t count_t;
-typedef std::uint_fast64_t queen_t; // for 64 < N <= 128, use queen_t = std::uint_fast128_t (and appropriate count_t)
+typedef std::uint8_t input_t; // type of N
+typedef std::uint_fast64_t count_t; // counting solutions
+
+// Definition of queen_t, the bits representing the N columns:
+#ifndef NMAX
+# define NMAX 64
+#endif
+constexpr input_t maxN = NMAX;
+static_assert(maxN == 32 or maxN == 64, "Max value for N: 32 or 64.");
+#if maxN == 32
+  typedef std::uint_fast32_t queen_t;
+#else
+ typedef std::uint_fast64_t queen_t;
+#endif
+static_assert(std::numeric_limits<queen_t>::digits >= maxN, "Problem with queen_t.");
+// For 64 < N <= 128, use queen_t = std::uint_fast128_t (and appropriate count_t).
+
+// Three helper functions for bit-operations:
 
 // N 1's from the right, the rest 0:
 constexpr queen_t setrightmostbits(const input_t N) noexcept {
-  // assert(N<=64);
-  return (queen_t(1) << N) - 1;
+  return (N>=maxN) ? queen_t(-1) : (queen_t(1) << N) - 1;
 }
 // 1 at position+1 (from right), the rest 0:
 constexpr queen_t one(const input_t position) noexcept {
@@ -71,18 +93,21 @@ inline constexpr queen_t keeprightmostbit(const queen_t x) noexcept {
   return -x & x;
 }
 
+// The recursive counting-function;
+// using bit-positions 0, ..., N-1 for the columns 1, ..., N:
+
 count_t count = 0, nodes = 0;
-queen_t all_columns;
+queen_t all_columns; // the first N bits 1, the rest 0
 input_t N;
+// Idea: size-many rows (from bottom) have been processed, now consider the
+// next row, and try to place the next queen in some column.
 inline void backtracking(queen_t avail,
   const queen_t columns, const queen_t fdiag, const queen_t fantid,
   input_t size) noexcept {
-  // avail: columns available for this invocation (only), via their column positions (bit-positions 0, ..., N-1)
+  // avail: columns available (set to 1) for this invocation (only)
   // columns: the current placement of queens
   // fdiag: forbidden columns due to diagonal constraints
   // fantid: forbidden columns due to antidiagonal constraints
-  // Idea: size-many rows (from bottom) have been processed, now consider the
-  // next row, and try to place the next queen in some column.
   assert(size == 0 or avail == (~(columns|fdiag|fantid) & all_columns));
   ++nodes;
   assert(avail);
@@ -104,7 +129,7 @@ int main(const int argc, const char* const argv[]) {
   if (argc != 2) { std::cout << "Usage[qcount]: N\n"; return 0; }
   const unsigned long arg1 = std::stoul(argv[1]);
   if (arg1 <= 1) { std::cout << 1 << " " << nodes << "\n"; return 0; }
-  if (arg1 > 64) { std::cerr << " N <= 64 required.\n"; return 1; }
+  if (arg1 > maxN) { std::cerr << " N <= " << int(maxN) << " required.\n"; return 1; }
   N = arg1;
   all_columns = setrightmostbits(N);
   // Using rotation-symmetry around vertical axis:
