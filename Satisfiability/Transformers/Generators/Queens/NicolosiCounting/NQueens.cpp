d105561faadc19ad2c1b58d94b982fc3d226dad2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-05-27 10:16:14 +0100

    Added asserts, and re-structured algorithm.
    This "positive version" ("avail" instead of "forbidden") seems more
    natural.

--- Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
@@ -42,12 +42,14 @@ That is, 92 solutions (nonattacking placements of 8 queens on the 8x8 board),
 using 983 nodes in the backtracking tree.
 
 For compilation the Makefile in the same directory as this file can be used.
+It produces also the debugging-version "qcount_debug".
 
 */
 
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <cassert>
 
 namespace {
 
@@ -62,49 +64,56 @@ constexpr queen_t setrightmostbits(const input_t N) noexcept {
   // assert(N<=64);
   return (queen_t(1) << N) - 1;
 }
-// 0 at position (from right), the rest 1:
-constexpr queen_t exactlyonezero(const input_t position) noexcept {
-  // assert(N<64);
-  return ~(queen_t(1) << position);
+// 1 at position+1 (from right), the rest 0:
+constexpr queen_t one(const input_t position) noexcept {
+  return queen_t(1) << position;
 }
 // Set all bits in x to 0 except of rightmost one (if exists):
 inline constexpr queen_t keeprightmostbit(const queen_t x) noexcept {
   return -x & x;
 }
 
-inline void backtracking(const queen_t all_columns, const queen_t forbidden, const queen_t columns, const queen_t fdiag, const queen_t fantid, const input_t size, const input_t N) noexcept {
-  // forbidden: columns forbidden for this invocation (only)
-  // columns: the current placement of queens, via their column positions (bit-positions 0, ..., N-1)
-  // fdiag: forbidden column positions due to diagonal constraints
-  // fantid: forbidden column positions due to antidiagonal constraints
-  // size-many rows (from bottom) have been processed, now consider the next
-  // row, and try to place a queen in some column.
+inline void backtracking(const queen_t all_columns, queen_t avail,
+  const queen_t columns, const queen_t fdiag, const queen_t fantid,
+  input_t size, const input_t N) noexcept {
+  // avail: columns available for this invocation (only), via their column positions (bit-positions 0, ..., N-1)
+  // columns: the current placement of queens
+  // fdiag: forbidden columns due to diagonal constraints
+  // fantid: forbidden columns due to antidiagonal constraints
+  // Idea: size-many rows (from bottom) have been processed, now consider the
+  // next row, and try to place the next queen in some column.
+  assert(size == 0 or avail == (~(columns|fdiag|fantid) & all_columns));
   ++nodes;
-  for (queen_t avail = ~forbidden & all_columns; avail != 0;) {
-    const queen_t next = keeprightmostbit(avail);
-    if (size+1 == N) {++count; return;} else {
-      const queen_t newcolumns = columns | next, newdiag = (fdiag|next) >> 1, newantid = (fantid|next) << 1;
-      backtracking(all_columns, newcolumns|newdiag|newantid, newcolumns, newdiag, newantid, size+1,N);
-      avail ^= next;
-    }
-  }
+  assert(avail);
+  ++size;
+  assert(size < N);
+  queen_t next = keeprightmostbit(avail);
+  assert(next);
+  do {
+    const queen_t newcolumns = columns|next,
+          newdiag = (fdiag|next) >> 1, newantid = (fantid|next) << 1,
+          newavail = ~(newcolumns|newdiag|newantid) & all_columns;
+    if (newavail)
+      if (size+1 == N) ++count; else
+      backtracking(all_columns, newavail, newcolumns, newdiag, newantid, size,N);
+  } while (next = keeprightmostbit(avail^=next));
 }
 }
 
 int main(const int argc, const char* const argv[]) {
   if (argc != 2) { std::cout << "Usage[qcount]: N\n"; return 0; }
   const unsigned long arg1 = std::stoul(argv[1]);
-  if (arg1 == 0) { std::cout << 1 << " " << nodes << "\n"; return 0; }
-  if (arg1 > 32) { std::cerr << " N <= 32 required.\n"; return 1; }
+  if (arg1 <= 1) { std::cout << 1 << " " << nodes << "\n"; return 0; }
+  if (arg1 > 32) { std::cerr << " N <= 32 required.\n"; return 1; } // for larger N, use larger queen_t
   const input_t N = arg1;
   const queen_t all_columns = setrightmostbits(N);
   if (N % 2 == 0) {
     backtracking(all_columns, setrightmostbits(N/2), 0, 0, 0, 0,N);
     std::cout << 2*count << " " << nodes << "\n";
   } else {
-    backtracking(all_columns, setrightmostbits(N/2+1), 0, 0, 0, 0,N);
+    backtracking(all_columns, setrightmostbits(N/2), 0, 0, 0, 0,N);
     const count_t half = count; count = 0;
-    backtracking(all_columns, exactlyonezero(N/2), 0, 0, 0, 0,N);
+    backtracking(all_columns, one(N/2), 0, 0, 0, 0,N);
     std::cout << 2*half + count << " " << nodes << "\n";
   }
 }
