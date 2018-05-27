0a61961a4ba795ec69a6df2af66f39c8e8868ddd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-05-27 01:20:46 +0100

    Provided documentation.
    It seems the algorithm can (and should) be improved.

--- Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueens.cpp
@@ -25,6 +25,10 @@ SOFTWARE.
   https://github.com/alenic/nqueens/commits/master
   ID f50264291093fe8529d143995ae9784f097d7ebf.
 
+The basic idea is the same as explained at
+
+  https://helloacm.com/n-queen-problem-in-back-tracing-bit-logics/
+
   Usage:
 
 > ./qcount N
@@ -46,18 +50,41 @@ For compilation the Makefile in the same directory as this file can be used.
 #include <string>
 
 namespace {
+
+typedef std::uint8_t input_t;
 typedef std::uint_fast64_t count_t;
-typedef std::uint32_t queen_t;
+typedef std::uint_fast64_t queen_t;
+
 count_t count = 0, nodes = 0;
-inline void backtracking(const queen_t mask, const queen_t pos, const queen_t r, const queen_t d1, const queen_t d2) noexcept {
+
+// N 1's from the right, the rest 0:
+constexpr queen_t setrightmostbits(const input_t N) noexcept {
+  // assert(N<=64);
+  return (queen_t(1) << N) - 1;
+}
+// 0 at position (from right), the rest 1:
+constexpr queen_t exactlyonezero(const input_t position) noexcept {
+  // assert(N<64);
+  return ~(queen_t(1) << position);
+}
+// Set all bits in x to 0 except of rightmost one (if exists):
+inline constexpr queen_t keeprightmostbit(const queen_t x) noexcept {
+  return -x & x;
+}
+
+inline void backtracking(const queen_t all_columns, const queen_t forbidden, const queen_t columns, const queen_t fdiag, const queen_t fantid) noexcept {
+  // forbidden: columns forbidden for this invocation (only)
+  // columns: the current placement of queens, via their column positions (bit-positions 0, ..., N-1)
+  // fdiag: forbidden column positions due to diagonal constraints
+  // fantid: forbidden column positions due to antidiagonal constraints
   ++nodes;
-  for (queen_t c = ~pos & mask; c != 0;) {
-    const queen_t i = -c & c;
-    const queen_t rpi = r + i;
-    if (rpi == mask) {++count; return;} else {
-      const queen_t newd1 = (d1+i) >> 1, newd2 = (d2+i) << 1;
-      backtracking(mask, rpi|newd1|newd2, rpi, newd1, newd2);
-      c -= i;
+  for (queen_t avail = ~forbidden & all_columns; avail != 0;) {
+    const queen_t next = keeprightmostbit(avail);
+    const queen_t newcolumns = columns | next;
+    if (newcolumns == all_columns) {++count; return;} else {
+      const queen_t newdiag = (fdiag|next) >> 1, newantid = (fantid|next) << 1;
+      backtracking(all_columns, newcolumns|newdiag|newantid, newcolumns, newdiag, newantid);
+      avail ^= next;
     }
   }
 }
@@ -68,15 +95,15 @@ int main(const int argc, const char* const argv[]) {
   const unsigned long arg1 = std::stoul(argv[1]);
   if (arg1 == 0) { std::cout << 1 << " " << nodes << "\n"; return 0; }
   if (arg1 > 32) { std::cerr << " N <= 32 required.\n"; return 1; }
-  const queen_t N = arg1;
-  const queen_t mask = (1 << N) - 1;
+  const input_t N = arg1;
+  const queen_t all_columns = setrightmostbits(N);
   if (N % 2 == 0) {
-    backtracking(mask, (1 << (N / 2)) - 1, 0, 0, 0);
+    backtracking(all_columns, setrightmostbits(N/2), 0, 0, 0);
     std::cout << 2*count << " " << nodes << "\n";
   } else {
-    backtracking(mask, (1 << ((N / 2) + 1)) - 1, 0, 0, 0);
-    const count_t count1 = count; count = 0;
-    backtracking(mask, ~(1 << (N / 2)), 0, 0, 0);
-    std::cout << 2*count1 + count << " " << nodes << "\n";
+    backtracking(all_columns, setrightmostbits(N/2+1), 0, 0, 0);
+    const count_t half = count; count = 0;
+    backtracking(all_columns, exactlyonezero(N/2), 0, 0, 0);
+    std::cout << 2*half + count << " " << nodes << "\n";
   }
 }
