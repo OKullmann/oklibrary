3e658a2f7aec20c589bed6aab41a61a27fcf6b4e
IRFANSHA <irfansha536@gmail.com>
2018-06-26 12:36:37 +0100

    Using future to parallelize the count

--- Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueenspar.cpp
+++ Satisfiability/Transformers/Generators/Queens/NicolosiCounting/NQueenspar.cpp
@@ -1,7 +1,8 @@
-// Oliver Kullmann 25.5.2018 (Swansea)
+// Irfansha Shaik 26.6.2018 (Swansea)
 /*
   Copyright Alessandro Nicolosi 2016, 2017
   Copyright Oliver Kullmann 2018
+  Copyright Irfansha Shaik 2018
 
 MIT License
 
@@ -22,8 +23,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
   Started as copy from
-  https://github.com/alenic/nqueens/commits/master
-  ID f50264291093fe8529d143995ae9784f097d7ebf.
+  https://github.com/OKullmann/oklibrary/commits/master/Satisfiability
+  ID 3d8d9e84ec1154cf6773dfc71b1c3cec5a1f0be4.
 
 The basic idea is the same as explained at
   https://helloacm.com/n-queen-problem-in-back-tracing-bit-logics/
@@ -33,62 +34,56 @@ and indeed the same as the program from 2002 by Jeff Somers
 Variable numrows there is our size, lsb is next, bitfield is avail, mask
 is all_columns. The array aQueenBitCol contains the values of our variable
 column, aQueenBitPosDiag of fdiag, aQueenBitNegDiag of fantid.
-An array is used there to simulate the recursion.
-The additional aQueenBitRes there just represents the solution for printing.
 
-  Version 1.0, 28.5.2018.
+Additionally future is used to calculate the count parallely.
+Each thread is given a different placement in the first row and counts are calculated parallely.
+
+  Version 0.6, 26.6.2018.
 
   Usage:
+>g++ --std=c++11 -pthread -pedantic -Wall -Wno-parentheses -Ofast -DNDEBUG   NQueenspar.cpp -o qcount_p
 
-> ./qcount N
+> ./qcount_p N
 
-Output of the solution count and the number of nodes; e.g.
+Output of the solution count; e.g.
 
-> ./qcount 8
-92 615
+> ./qcount_p 8
+92
 
-That is, 92 solutions (nonattacking placements of 8 queens on the 8x8 board),
-using 615 nodes in the backtracking tree.
+That is, 92 solutions (nonattacking placements of 8 queens on the 8x8 board).
 
-For compilation the Makefile in the same directory as this file can be used.
-It produces also the debugging-version "qcount_debug".
-To set NMAX (maximal allowed value of N; default 64), use e.g.
-> CXXFLAGS="-DNMAX=32" make
 
 For timing-output, the gnu-tool /usr/bin/time is recommended.
 For example, output to file Out, first current date and time, then the output
 of this program, then the command itself, then the time measurements,
 first user-time (most important), then total-time, then system-time,
 always appending to Out:
-> date >> Out; /usr/bin/time -ao Out -f "%C\n%U %e %S" ./qcount 16 >>Out
+> date >> Out; /usr/bin/time -ao Out -f "%C\n%U %e %S" ./qcount_p 16 >>Out
 > cat Out
-Mon May 28 21:07:52 BST 2018
-14772512 355451208
+Tue 26 Jun 11:11:23 BST 2018
+14772512
 ./qcount 16
-5.41 5.43 0.00
+3.66 3.66 0.00
 
 A simple experiment in Bash then, running N=0,...,20, and collecting the data
 in file NQ_out:
 
 > OUT="NQ_out"; for ((N=0; N<=20; ++N)); do echo "N=$N" | tee -a $OUT;
-   date >> $OUT; /usr/bin/time -ao $OUT -f "%C\n%U %e %S" ./qcount $N >> $OUT;
+   date >> $OUT; /usr/bin/time -ao $OUT -f "%C\n%U %e %S" ./qcount_p $N >> $OUT;
    done
 
-TODOS:
-
-1. Use signals (like the tawSolver), to show current counts.
-2. Use futures for parallelisation.
-3. Simulate the recursion by a stack (as done in Jeff Somers's original
-   program, whose runtime is very similar to the runtime of this program
-   (version 1.0)). OK doesn't expect a speed-up, but one can try.
-
 */
+
 
 #include <cstdint>
 #include <iostream>
 #include <string>
 #include <cassert>
 #include <limits>
+#include <vector>
+#include <future>
+
+using namespace std;
 
 namespace {
 
@@ -127,31 +122,29 @@ inline constexpr queen_t keeprightmostbit(const queen_t x) noexcept {
 // The recursive counting-function;
 // using bit-positions 0, ..., N-1 for the columns 1, ..., N:
 
-count_t count = 0, nodes = 0;
 queen_t all_columns; // the first N bits 1, the rest 0
 input_t N;
 // Idea: size-many rows (from bottom) have been processed, now consider the
 // next row, and try to place the next queen in some column.
-inline void backtracking(queen_t avail,
+inline count_t backtracking(queen_t avail,
   const queen_t columns, const queen_t fdiag, const queen_t fantid,
-  const input_t size) noexcept {
+  const input_t size, const int i, count_t& local_count) {
   // avail: columns available (set to 1) for this invocation (only)
   // columns: the current placement of queens
   // fdiag: forbidden columns due to diagonal constraints
   // fantid: forbidden columns due to antidiagonal constraints
   assert(size == 0 or avail == (~(columns|fdiag|fantid) & all_columns));
   //assert(std::bitset<maxN>(columns).count() == size);
-  ++nodes;
   assert(avail);
   const queen_t sdiag = fdiag>>1, santid = fantid<<1;
   const queen_t newavail0 = ~(columns | sdiag | santid) & all_columns;
-  if (not newavail0) return;
+  if (not newavail0) return local_count;
   queen_t next = keeprightmostbit(avail); // could be any bit, but that seems fastest
   const input_t sp1 = size+1; // due to the placement of next
   assert(sp1 < N);
   if (sp1+1 == N) {
     do
-      count += bool(newavail0 & ~(next | next>>1 | next<<1));
+      local_count += bool(newavail0 & ~(next | next>>1 | next<<1));
     while (next = keeprightmostbit(avail^=next));
   }
   else
@@ -159,26 +152,39 @@ inline void backtracking(queen_t avail,
           nextrs = next>>1, nextls = next<<1,
           newdiag = sdiag | nextrs, newantid = santid | nextls,
           newavail = newavail0 & ~(next | nextrs | nextls);
-      if (newavail) backtracking(newavail,newcolumns,newdiag,newantid,sp1);
+      if (newavail) backtracking(newavail,newcolumns,newdiag,newantid,sp1,i,local_count);
     } while (next = keeprightmostbit(avail^=next));
+return local_count;
 }
+
+inline count_t parallel(queen_t avail,
+  const queen_t columns, const queen_t fdiag, const queen_t fantid,
+  const input_t size, const int i) {
+  count_t local_count = 0;
+  return backtracking(avail,columns,fdiag,fantid,size,i,std::ref(local_count));
+  }
+
 }
 
 int main(const int argc, const char* const argv[]) {
   if (argc != 2) { std::cout << "Usage[qcount]: N\n"; return 0; }
+  count_t count = 0;
+  std::vector<std::future<count_t>> futures;
   const unsigned long arg1 = std::stoul(argv[1]);
-  if (arg1 <= 1) { std::cout << 1 << " " << nodes << "\n"; return 0; }
+  if (arg1 <= 1) { std::cout << 1 << "\n"; return 0; }
   if (arg1 > maxN) { std::cerr << " N <= " << int(maxN) << " required.\n"; return 1; }
   N = arg1;
   all_columns = setrightmostbits(N);
   // Using rotation-symmetry around vertical axis:
   if (N % 2 == 0) {
-    backtracking(setrightmostbits(N/2), 0, 0, 0, 0);
-    std::cout << 2*count << " " << nodes << "\n";
+    for(int i = 0; i < N/2; ++i) futures.push_back (async(parallel, one(i), 0, 0, 0, 0, i));
+    for(auto& e : futures) count += e.get();
+    std::cout << 2*count << "\n";
   } else {
-    backtracking(setrightmostbits(N/2), 0, 0, 0, 0);
-    const count_t half = count; count = 0;
-    backtracking(one(N/2), 0, 0, 0, 0);
-    std::cout << 2*half + count << " " << nodes << "\n";
+    for(int i = 0; i < N/2; ++i) futures.push_back (async(parallel, one(i), 0, 0, 0, 0, i));
+    for(auto& e : futures) count += e.get();
+    const count_t half = count;
+    count = parallel(one(N/2), 0, 0, 0, 0, N/2);
+    std::cout << 2*half + count << "\n";
   }
 }
