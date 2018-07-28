6f8db726e05f686d2560d37aabce62243e99601b
irfansha <irfansha536@gmail.com>
2018-07-28 11:15:25 +0100

    Improving v_unset function

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/NQueens.hpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/NQueens.hpp
@@ -116,9 +116,7 @@ namespace NQueens {
       }
 
     // Checks if the field v is unset:
-    bool v_unset(Var v) {
-      if (board[v.first][v.second] == unset) return 1;
-      else return 0; }
+    bool v_unset(Var v) return (board[v.first][v.second] == unset);
 
     // Updates the field to forbidden:
     void field_update(Var v) {
