d47a1069362fbbbbd0b5ad72b1a11165a3dff2be
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-08-10 14:14:11 +0100

    No unnecessary index obscurantism.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/NQueens.hpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/NQueens.hpp
@@ -374,25 +374,25 @@ namespace NQueens {
        https://github.com/OKullmann/oklibrary/commits/master/Satisfiability/Solvers/TawSolver
        ID a227f64a6c66a817e4b53fa4c1a1244d530a25c5
     */
-    Weight_vector weight_vector = {4.85,1,0.354,0.11,0.0694};
+    const Weight_vector weight_vector = {0,0, 4.85, 1, 0.354, 0.11, 0.0694};
     const Var_uint vec_size = weight_vector.size();
     const AmoAlo_board& F;
 
     GreedyAmo(const AmoAlo_board& F) : F(F) {}
 
     inline Weight_t long_cw(const Var_uint long_c) const noexcept {
-      return std::pow(1/1.46,long_c-vec_size)*weight_vector.back();
+      return std::pow(1/1.46, long_c-vec_size) * weight_vector.back();
     }
 
     Weight_t weight(const Var v) const noexcept {
-      const Weight_t amo_w = weight_vector[0]*F.amo_count(v);
-      const Var_uint alo_r_cl = F.r_rank()[v.first].o; // alo_r_cl is at-least-one row clause length.
-      const Var_uint alo_c_cl = F.c_rank()[v.second].o; // alo_c_cl is at-least-one column clause length.
+      const Weight_t amo_w = F.amo_count(v);
+      const Var_uint alo_r_cl = F.r_rank()[v.first].o;
+      const Var_uint alo_c_cl = F.c_rank()[v.second].o;
       assert(alo_r_cl >= 2);
       assert(alo_c_cl >= 2);
       const Weight_t
-        alo_r_w = (alo_r_cl > vec_size + 2) ? long_cw(alo_r_cl) : weight_vector[alo_r_cl-2],
-        alo_c_w = (alo_c_cl > vec_size + 2) ? 1 + long_cw(alo_c_cl) : weight_vector[alo_c_cl-2];
+        alo_r_w = (alo_r_cl > vec_size) ? long_cw(alo_r_cl) : weight_vector[alo_r_cl],
+        alo_c_w = (alo_c_cl > vec_size) ? 1 + long_cw(alo_c_cl) : weight_vector[alo_c_cl];
       return ((1 + amo_w) * ( 1 + alo_r_w + alo_c_w));
     }
 
