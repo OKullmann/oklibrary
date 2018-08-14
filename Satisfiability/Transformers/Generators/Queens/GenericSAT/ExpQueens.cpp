88dc71b7679701850a2c5cf0340e548d941e8945
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-08-14 12:50:12 +0100

    New program ExpQueens for experimentation.

---
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/ExpQueens.cpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 14.8.2018 (Swansea)
+
+#include <iostream>
+#include <string>
+
+#include "Backtracking.hpp"
+#include "NQueens.hpp"
+#include "InOut.hpp"
+
+namespace {
+
+const std::string version = "0.2";
+const std::string date = "14.8.2018";
+const std::string program = "ExpQueens"
+#ifndef NDEBUG
+  "_debug"
+#endif
+;
+const std::string error = "ERROR[" + program + "]: ";
+
+}
+
+int main(const int argc, const char* const argv[]) {
+
+  const ChessBoard::coord_t N = InOut::interprete(argc, argv, error);
+  NQueens::AmoAlo_board Fq(N);
+  Backtracking::CountSat<NQueens::AmoAlo_board, NQueens::TawHeuristics> B;
+  const auto rFq = B(Fq);
+  std::cout << rFq;
+
+}
