ae1e0d30349a8081834405aad3f3353e66a10135
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-08-14 12:28:36 +0100

    Input and output in a dedicated file.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/SimTaw.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/SimTaw.cpp
@@ -2,14 +2,14 @@
 
 #include <iostream>
 #include <string>
-#include <stdexcept>
 
 #include "Backtracking.hpp"
 #include "NQueens.hpp"
+#include "InOut.hpp"
 
 namespace {
 
-const std::string version = "0.3.4";
+const std::string version = "0.3.5";
 const std::string date = "14.8.2018";
 const std::string program = "SimTaw"
 #ifndef NDEBUG
@@ -18,56 +18,14 @@ const std::string program = "SimTaw"
 ;
 const std::string error = "ERROR[" + program + "]: ";
 
-enum class Error {
-  missing_argument=1,
-  conversion=2,
-  too_big=3,
-  too_small=4
-};
-/* Extracting the underlying code of enum-classes (scoped enums) */
-template <typename EC>
-inline constexpr int code(const EC e) noexcept {return static_cast<int>(e);}
-
-ChessBoard::coord_t interprete(const int argc, const char* const argv[]) noexcept {
-  if (argc == 1) {
-    std::cerr << error << "The argument N is needed.\n";
-    std::exit(code(Error::missing_argument));
-  }
-  const std::string arg1 = argv[1];
-  unsigned long N;
-  try { N = std::stoul(arg1); }
-  catch (const std::invalid_argument& e) {
-    std::cerr << error << "The argument \"" << arg1 << "\" is not a valid integer.\n";
-    std::exit(code(Error::conversion));
-  }
-  catch (const std::out_of_range& e) {
-    std::cerr << error << "The argument \"" << arg1 << "\" is too big for unsigned long.\n";
-    std::exit(code(Error::too_big));
-  }
-  if (N > ChessBoard::max_coord) {
-    std::cerr << error << "The argument can be at most " << ChessBoard::max_coord << ", but is " << arg1 << ".\n";
-    std::exit(code(Error::too_big));
-  }
-  if (N == 0) {
-    std::cerr << error << "The argument is 0.\n";
-    std::exit(code(Error::too_small));
-  }
-  return N;
-}
-
 }
 
 int main(const int argc, const char* const argv[]) {
 
-  typedef Backtracking::CountSat<NQueens::AmoAlo_board, NQueens::TawHeuristics> Backtracking_q;
-
-  const ChessBoard::coord_t N = interprete(argc, argv);
-
+  const ChessBoard::coord_t N = InOut::interprete(argc, argv, error);
   NQueens::AmoAlo_board Fq(N);
-
-  Backtracking_q B;
+  Backtracking::CountSat<NQueens::AmoAlo_board, NQueens::TawHeuristics> B;
   const auto rFq = B(Fq);
-
   std::cout << rFq.solutions << " " << rFq.nodes << "\n";
 
 }
