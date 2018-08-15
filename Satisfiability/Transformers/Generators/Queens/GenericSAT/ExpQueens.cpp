d2abd96bf0537a0a7ede9879c1ada67e740e1b2e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-08-15 20:49:53 +0100

    Added parameter "-v" for version-information.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/ExpQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/ExpQueens.cpp
@@ -3,14 +3,16 @@
 #include <iostream>
 #include <string>
 
+#include <cstdlib>
+
 #include "Backtracking.hpp"
 #include "NQueens.hpp"
 #include "InOut.hpp"
 
 namespace {
 
-const std::string version = "0.2.2";
-const std::string date = "14.8.2018";
+const std::string version = "0.2.3";
+const std::string date = "15.8.2018";
 const std::string program = "ExpQueens"
 #ifndef NDEBUG
   "_debug"
@@ -18,10 +20,35 @@ const std::string program = "ExpQueens"
 ;
 const std::string error = "ERROR[" + program + "]: ";
 
+void version_information() noexcept {
+  std::cout << program << ":\n"
+   " Version: " << version << "\n"
+   " Last change date: " << date << "\n"
+#ifdef NDEBUG
+   " Compiled with NDEBUG\n"
+#else
+   " Compiled without NDEBUG\n"
+#endif
+#ifdef __OPTIMIZE__
+   " Compiled with optimisation options\n"
+#else
+   " Compiled without optimisation options\n"
+#endif
+   " Compilation date: " __DATE__ " " __TIME__ "\n"
+#ifdef __GNUC__
+   " Compiler: g++, version " __VERSION__ "\n"
+#else
+   " Compiler not gcc\n"
+#endif
+  ;
+  std::exit(0);
+}
+
 }
 
 int main(const int argc, const char* const argv[]) {
 
+  if (argc > 1 and std::string(argv[1]) == "-v") version_information();
   const ChessBoard::coord_t N = InOut::interprete(argc, argv, error);
   NQueens::AmoAlo_board Fq(N);
   Backtracking::CountSat<NQueens::AmoAlo_board, NQueens::TawHeuristics> B;
