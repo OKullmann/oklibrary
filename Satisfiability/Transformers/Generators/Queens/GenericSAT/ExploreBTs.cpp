2cbf30b8ade27deb1f5e16b8f3ab539c8e8af241
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-01-08 16:50:38 +0000

    Todos expanded.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/ExploreBTs.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/ExploreBTs.cpp
@@ -53,6 +53,65 @@ TODOS:
             ln(b/a) (assuming b >= a), that is, ln(b)-ln(a).
         (2) Apparently due to the capping of the precision, this is shortcut
             at 11400 iterations.
+        (3) How is the relation for float (32 bits) and double (64 bits)?
+   (e) Some approximations of the error, perhaps in dependency of ln(b/a),
+       are needed.
+        (1) We should check whether we have quadratic convergence (and
+            what that means), as predicted by the general results for
+            Newton's method. Perhaps the convergence speed should be
+            measured.
+            https://en.wikipedia.org/wiki/Rate_of_convergence
+
+(2) Alternative methods
+   (a) When we start below the tau-value, we have strictly monotonically
+       increasing convergence. And above -- is it now strictly monotonically
+       decreasing?
+   (b) The Newton-Fourier method
+       https://en.wikipedia.org/wiki/Newton%27s_method#Newton–Fourier_method
+       yields lower and upper bounds at the same time, using a somewhat
+       changed second strand -- how does this fit?
+   (c) Via Halley's method
+       https://en.wikipedia.org/wiki/Halley%27s_method
+       we should obtain cubic convergence.
+
+(3) Better exproximation for ltau(1,a) for a >= 1.
+   (a) In principle, handling of this case is enough, via
+       ltau(a,b) = 1/a ltau(1,b/a) for a <= b.
+   (b) While having good lower bounds (for some a) would enable faster general
+       computations, which currently just uses the start value ltau(1,1)
+       (scaled).
+       On should start curve-fitting, to see the general shape (growth rate)
+       of f(x) = ltau(1,x), x >= 1.
+   (b) ltau(1,1) = ln(2), ltau(1,2) = ln(1+sqrt(5)) - ln(2).
+       At Maxima we see formulas via radicals for tau(1,3), tau(1,4), while
+       the method fails for integers a >= 5 -- is this provably?
+       tau(1,5): x^-1 + x^-5 = 1, <=> x^4 + 1 = x^5 <=> x^5 - x^4 - 1 = 0 ---
+       is it provable that this polynomial has no solutions via radicals?
+       Algorithms (implemented) should exist to decide this question.
+   (c) The general polynomial is x^a - x^(a-1) - 1 = 0. Having more information
+       on this for natural numbers a would be helpful.
+       The related reccurrences are
+       f(n) = f(n-1) + f(n-a). In Maxima, for a=5:
+       f5(n) := if n<5 then n else f5(n-1) + f5(n-5)
+       for n >= 0. It seems that this sequence is not in the OEIS.
+         create_list(f5(n),n,0,20);
+         [0,1,2,3,4,4,5,7,10,14,18,23,30,40,54,72,95,125,165,219,291]
+   (d) One would assume that considering e.g.
+         exp(-x) + exp(-5*x) = 1
+       wouldn't have analytical advantages (only numerical)?
+       All that should be raised at MathOverflow.
+   (e) The general bounds we have are,
+       using the general bounds
+         mtau(a,b) := ln(2) / ltau(a,b)
+         sqrt(a*b) <= mtau(a,b) <= (a+b)/2,
+       which yields
+         ltau(1,a) >= 2ln(2) / (a+1)
+         ltau(1,a) <= ln(2) / sqrt(a).
+       One would guess that the upper bound is closer to the truth?
+       Considering the quotients, that's only true for parameter a not too big.
+       For the curve-fitting as above, one would guess an approach
+         f(x) ~ alpha / (x+beta)^gamma
+       for 1/2 < gamma < 1, should yield a reasonable fit.
 
 */
 
