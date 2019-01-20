d17266c3eb205300d11219d0d41efbe0cbebc96f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-01-20 09:04:30 +0000

    Data update.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/MeasureTau.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/MeasureTau.cpp
@@ -20,12 +20,42 @@ T> time ./MeasureTau 64 1e1 1e1 1e7 1e10
 real    170m59.247s
 user    170m10.173s
 sys     0m18.904s
+> time ./MeasureTau 80 1e1 1e1 1e7 1e10
+75750.154509474586924
+real    582m19.229s
+user    580m10.964s
+sys     0m34.342s
 
-80 XXX
+Comparison:
+> time ./ExploreBTs +1e10
+real    521m9.398s
+user    521m35.421s
+sys     1m7.061s
+> time ./ExploreBTs 1e10
+real    1622m26.857s
+user    1615m34.799s
+sys     2m14.001s
+
 
 cs-wsok:
+> time ./MeasureTau 64 1e1 1e1 1e7 1e10
+75750.154509474588849
+real    171m14.834s
+user    171m8.581s
+sys     0m0.002s
 
-64 XXX
+XXX 80
+
+Comparison:
+> time ./ExploreBTs +1e10
+real    526m53.909s
+user    526m33.472s
+sys     0m0.919s
+> time ./ExploreBTs 1e10
+real    1384m48.796s
+user    1383m56.399s
+sys     0m0.703s
+
 
 csverify:
 $ time ./MeasureTau 64 1e1 1e1 1e7 1e10
@@ -39,6 +69,24 @@ real    516m20.281s
 user    516m20.301s
 sys     0m0.000s
 
+Testing whether the parameter-range plays a big role:
+$ time ./MeasureTau 64 1e9 1e9 1e16 1e10
+0.00010440971844115158531
+real    32m31.131s
+user    32m31.135s
+sys     0m0.000s
+
+Comparison:
+$ time ./ExploreBTs +1e10
+real    98m7.253s
+user    98m7.253s
+sys     0m0.000s
+$ time ./ExploreBTs 1e10
+real    1432m49.335s
+user    1432m49.322s
+sys     0m0.000s
+
+
 cs2-irfan536:
 $ time ./MeasureTau 64 1e1 1e1 1e7 1e10
 75750.154509474588849
@@ -47,6 +95,16 @@ user    38m41.103s
 sys     0m0.308s
 
 80 XXX
+
+Comparison:
+$ time ./ExploreBTs +1e10
+real	114m11.254s
+user	114m10.984s
+sys	0m0.128s
+$ time ./ExploreBTs 1e10
+real	1490m30.361s
+user	1490m15.300s
+sys	0m2.912s
 
 */
 
