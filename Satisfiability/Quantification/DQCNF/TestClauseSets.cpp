e0075a2205c4c1dfa67d0552e54ee5fd807677ba
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-02-23 11:42:17 +0000

    Started providing regression testing for module ClauseSets.

---
+++ Satisfiability/Quantification/DQCNF/TestClauseSets.cpp
@@ -0,0 +1,14 @@
+// Oliver Kullmann, 23.2.2019 (Swansea)
+/* Copyright 2019 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include "ClauseSets.hpp"
+
+#include <cassert>
+
+int main() {
+
+}
