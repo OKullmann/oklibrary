667ba535014c8af37b57cf5b7c71a43eec9f4455
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-03 13:38:17 +0000

    Todo for IS (missing test).

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/TestSimTaw.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/TestSimTaw.cpp
@@ -5,6 +5,15 @@ it and/or modify it under the terms of the GNU General Public License as publish
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
+/* TODOS:
+
+1. Test
+output(out, T, "OK", "Test");
+
+(the value of out needs to be checked).
+
+*/
+
 #include <sstream>
 #include <stdexcept>
 #include <numeric>
