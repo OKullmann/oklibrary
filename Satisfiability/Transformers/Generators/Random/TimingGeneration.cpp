e766a1e8ac1eecf83631da3256e11b7ded4db161
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-04 23:49:33 +0000

    Timing program for speed of generation.

---
+++ Satisfiability/Transformers/Generators/Random/TimingGeneration.cpp
@@ -0,0 +1,86 @@
+// Oliver Kullmann, 4.3.2019 (Swansea)
+/* Copyright 2019 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/* Timing of generation-function for Mersenne-Twister
+
+Example (annotation on next line)
+
+Random> ./TimingGeneration 3e9
+# number N of calls of the generator (this is the default value)
+TimingGeneration 0.1.0 4.3.2019 4e56d2ade745848a4bf12d947f9b3717b41c6bef
+# name, version, last change-date, git ID
+g++ 8.3.0 Mar  4 2019 23:34:05
+# compiler-version, compilation-date
+-Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
+# optimisation options
+3000000000 9259490176346246065
+# the argument plus the sum of all generated numbers (as unsigned 64bit)
+3e+09
+# N again, in float80-precision.
+
+
+Results:
+
+On csltok:
+
+
+On cs-wsok:
+
+
+On csverify:
+
+
+*/
+
+#include <iostream>
+
+#include <Numerics/FloatingPoint.hpp>
+
+#include "Numbers.hpp"
+
+namespace {
+
+  const std::string version = "0.1.0";
+  const std::string date = "4.3.2019";
+  const std::string program = "TimingGeneration";
+
+  const std::string compilation_date = __DATE__ " " __TIME__;
+  const std::string compiler_version =
+#ifdef __GNUC__
+   "g++ " __VERSION__
+#else
+   "Compiler not gcc"
+#endif
+;
+  const std::string git_id = GITID;
+  const std::string optimisation = OPTIMISATION;
+
+  using namespace RandGen;
+
+  constexpr gen_uint_t N_default = 3e9L;
+
+}
+
+int main(const int argc, const char* const argv[]) {
+
+  const gen_uint_t N = (argc == 1) ? N_default : FloatingPoint::toUInt(argv[1]);
+
+
+  gen_uint_t sum = 0;
+  randgen_t g;
+  for (gen_uint_t i = 0; i < N; ++i) sum += g();
+
+
+  std::cout << program << " " << version << " " << date << " " << git_id << "\n";
+  std::cout << compiler_version << " " << compilation_date << "\n";
+  std::cout << optimisation << "\n";
+
+  std::cout << N << " " << sum << "\n";
+
+  std::cout << FloatingPoint::float80(N) << "\n";
+
+}
