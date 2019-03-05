b8f01269556cbccc51c61176b7ee0bac68a1d7eb
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-05 01:48:14 +0000

    Timings for the improved implementation.

--- Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
+++ Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
@@ -41,6 +41,19 @@ sys     0m0.002s
 So roughly 201e6 generations per sec, which is 89% of the pure
 generation-speed.
 
+With improved bernoulli:
+Random> time ./TimingBernoulli12
+TimingBernoulli12 0.1.0 5.3.2019 c17a9772808f47938a193174dc5a96df1c72ddfa
+g++ 8.3.0 Mar  5 2019 01:24:23
+-Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
+3000000000 1499961918 0.499987306
+3e+09
+real    0m14.679s
+user    0m14.629s
+sys     0m0.010s
+
+Possibly a small speed-up.
+
 
 On cs-wsok:
 
@@ -57,6 +70,19 @@ sys     0m0.001s
 So roughly 288e6 generations per sec, which is 91% of pure
 generation-speed.
 
+With improved bernoulli:
+> time ./TimingBernoulli12
+TimingBernoulli12 0.1.1 5.3.2019 ff59e829cd50d1ddf533260f800a0702923c6113
+g++ 8.3.0 Mar  5 2019 01:28:24
+-Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
+3000000000 1499961918 0.499987306
+3e+09
+real    0m10.371s
+user    0m10.364s
+sys     0m0.001s
+
+No change apparently.
+
 
 On csverify:
 
@@ -72,6 +98,20 @@ sys     0m0.000s
 
 So roughly 526e6 generations per sec, which is 93% of pure
 generation-speed.
+
+With improved bernoulli:
+Random$ time ./TimingBernoulli12 
+TimingBernoulli12 0.1.1 5.3.2019 ff59e829cd50d1ddf533260f800a0702923c6113
+g++ 8.2.0 Mar  5 2019 01:31:17
+-Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
+3000000000 1499961918 0.499987306
+3e+09
+real    0m5.748s
+user    0m5.748s
+sys     0m0.000s
+
+Apparently also here no change; one would assume that the compiler
+performs the optimisation itself (but it can't hurt to fix it).
 
 */
 
