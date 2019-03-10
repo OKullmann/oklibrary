8ef1e2b7fe69b83e0b6fa3974d71ca7980c52ef8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-10 04:36:54 +0000

    Clean-up of timings.

--- Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
+++ Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
@@ -29,89 +29,51 @@ Results:
 On csltok:
 
 Random> time ./TimingBernoulli12
-TimingBernoulli12 0.1.0 5.3.2019 3003b64047b19a11966ad18d705b9492f4abe18f
-g++ 8.3.0 Mar  5 2019 00:47:25
+0.1.4 10.3.2019 TimingBernoulli12 55c8a21b600a40817a4b729b2948b86cee6b57e0
+g++ 8.3.0 Mar 10 2019 04:21:59
 -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
 3000000000 1499961918 0.499987306
 3e+09
-real    0m14.951s
-user    0m14.905s
-sys     0m0.002s
+real    0m14.500s
+user    0m14.460s
+sys     0m0.001s
 
-So roughly 201e6 generations per sec, which is 89% of the pure
+So roughly 207e6 generations per sec, which is 91.6% of the pure
 generation-speed.
-
-With improved bernoulli:
-Random> time ./TimingBernoulli12
-TimingBernoulli12 0.1.0 5.3.2019 c17a9772808f47938a193174dc5a96df1c72ddfa
-g++ 8.3.0 Mar  5 2019 01:24:23
--Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
-3000000000 1499961918 0.499987306
-3e+09
-real    0m14.679s
-user    0m14.629s
-sys     0m0.010s
-
-Possibly a small speed-up.
 
 
 On cs-wsok:
 
 Random> time ./TimingBernoulli12
-TimingBernoulli12 0.1.0 5.3.2019 3003b64047b19a11966ad18d705b9492f4abe18f
-g++ 8.3.0 Mar  5 2019 00:50:55
+0.1.4 10.3.2019 TimingBernoulli12 55c8a21b600a40817a4b729b2948b86cee6b57e0
+g++ 8.3.0 Mar 10 2019 04:22:40
 -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
 3000000000 1499961918 0.499987306
 3e+09
-real    0m10.375s
-user    0m10.366s
+real    0m10.314s
+user    0m10.305s
 sys     0m0.001s
 
-So roughly 288e6 generations per sec, which is 91% of pure
+So roughly 291e6 generations per sec, which is 92.1% of pure
 generation-speed.
 
-With improved bernoulli:
-> time ./TimingBernoulli12
-TimingBernoulli12 0.1.1 5.3.2019 ff59e829cd50d1ddf533260f800a0702923c6113
-g++ 8.3.0 Mar  5 2019 01:28:24
--Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
-3000000000 1499961918 0.499987306
-3e+09
-real    0m10.371s
-user    0m10.364s
-sys     0m0.001s
-
-No change apparently.
 
 
 On csverify:
 
 Random$ time ./TimingBernoulli12
-TimingBernoulli12 0.1.0 5.3.2019 3003b64047b19a11966ad18d705b9492f4abe18f
-g++ 8.2.0 Mar  5 2019 00:53:23
+0.1.4 10.3.2019 TimingBernoulli12 55c8a21b600a40817a4b729b2948b86cee6b57e0
+g++ 8.2.0 Mar 10 2019 04:24:12
 -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
 3000000000 1499961918 0.499987306
 3e+09
-real    0m5.727s
-user    0m5.727s
-sys     0m0.000s
+real    0m5.675s
+user    0m5.675s
+sys     0m0.000
 
-So roughly 526e6 generations per sec, which is 93% of pure
+So roughly 529e6 generations per sec, which is 93.4% of pure
 generation-speed.
 
-With improved bernoulli:
-Random$ time ./TimingBernoulli12 
-TimingBernoulli12 0.1.1 5.3.2019 ff59e829cd50d1ddf533260f800a0702923c6113
-g++ 8.2.0 Mar  5 2019 01:31:17
--Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only -fno-unsafe-math-optimizations -fno-associative-math -fno-reciprocal-math  -fno-signed-zeros -fno-math-errno -fno-trapping-math
-3000000000 1499961918 0.499987306
-3e+09
-real    0m5.748s
-user    0m5.748s
-sys     0m0.000s
-
-Apparently also here no change; one would assume that the compiler
-performs the optimisation itself (but it can't hurt to fix it).
 
 It is actually surprising that adding just one small test to the generation
 slows it down by 7%: the generation should be a much more involved computation,
