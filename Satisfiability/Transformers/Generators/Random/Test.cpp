1d449e267a5f3085b9d6cffc0655ee6280fb3c94
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-16 16:42:08 +0000

    Added tests for class Bernoulli and for UniformRange.

--- Satisfiability/Transformers/Generators/Random/Test.cpp
+++ Satisfiability/Transformers/Generators/Random/Test.cpp
@@ -151,6 +151,19 @@ int main() {
    }
   }
 
+  {randgen_t g;
+   UniformRange u1(g,1007);
+   assert(randgen_max - u1.last_regions == 492);
+   assert(not u1.p2);
+   UniformRange u2(g,10000000019ULL);
+   assert(randgen_max - u2.last_regions == 8660737959ULL);
+   assert(not u2.p2);
+   UniformRange u3(g,iexp2(38));
+   assert(u3.last_regions == randgen_max);
+   assert(u3.p2);
+   assert(u3.size_region == iexp2(64-38));
+  }
+
   {randgen_t g1, g2;
    for (gen_uint_t e = 0; e <= 5; ++e) {
      const gen_uint_t max = iexp2(e);
@@ -164,6 +177,28 @@ int main() {
          if (not u.trivial and b.s != Bernoulli2::S::nc) g2.discard(1);
        }
      }
+   }
+  }
+
+  {RandGen_t g1({0,2,4});
+   randgen_t g2(g1.extract());
+   assert(g1.extract() == g2);
+   for (gen_uint_t den = 1; den <= 20; ++den) {
+     UniformRange u(g2, den);
+     assert(den != 1 or u.trivial);
+     for (gen_uint_t nom = 0; nom <= den; ++nom) {
+       Bernoulli b(g1, {nom,den});
+       assert(nom != 0 or b.s == Bernoulli::S::c0);
+       assert(nom != den or b.s == Bernoulli::S::c1);
+       assert(nom != 1 or Bernoulli::constant(b.s) or (u.p2 == (b.s == Bernoulli::S::dy)));
+       assert(nom != 1 or Bernoulli::constant(b.s) or u.last_regions == b.last_valid);
+       assert(nom != 1 or Bernoulli::constant(b.s) or b.threshold == nom * u.size_region);
+       for (unsigned i = 0; i < 100'000; ++i) {
+         assert(b() == (u() < nom));
+         if (not u.trivial and (Bernoulli::constant(b.s))) g1.discard(1);
+       }
+     }
+     assert(g1.extract() == g2); // happens to be the case here that there are no failed generator calls
    }
   }
 
