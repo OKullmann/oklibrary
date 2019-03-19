68fdf1c961b2eb661412e5cb499ad1e796231ba2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-19 12:35:43 +0000

    Make- and test-enviroment for module ProgramOptions.

---
+++ Programming/ProgramOptions/Test.cpp
@@ -0,0 +1,16 @@
+// Oliver Kullmann, 19.3.2019 (Swansea)
+/* Copyright 2019 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <cassert>
+
+#include "Environment.hpp"
+
+int main() {
+  using namespace Environment;
+
+
+}
