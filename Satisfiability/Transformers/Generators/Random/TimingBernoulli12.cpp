ab7c0e5bddce380d43b683649f90df701d1a3a64
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-19 20:59:58 +0000

    Update of timings.
    A slowdown of 10%, just due to trivial additions.
    But it seems we can't do anything about that.

--- Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
+++ Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
@@ -12,11 +12,6 @@ Example (annotation on next line):
 Random> ./TimingBernoulli12 3e9 0
 # number N of calls of the generator and number of discards (default values),
 # plus sequence of 64-bit seed values (empty by default)
-TimingBernoulli12 0.3.0 17.3.2019 f611f6943f65c89076db4cf3ce929bd55108c340
-csltok.swansea.ac.uk 4788.21
-g++ 8.3.0 Mar_17_2019 17:24:19
---std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only
-# program information
 3000000000 0
 # the arguments (in integer-format)
 ()
@@ -28,10 +23,6 @@ g++ 8.3.0 Mar_17_2019 17:24:19
 
 With one seed-value (a 64-bit 0, yielding two 32-bit 0s):
 Random> ./TimingBernoulli12 3e9 0 0
-TimingBernoulli12 0.3.0 17.3.2019 f611f6943f65c89076db4cf3ce929bd55108c340
-csltok.swansea.ac.uk 4788.21
-g++ 8.3.0 Mar_17_2019 17:36:11
---std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only
 3000000000 0 0
 (0,0)
 1500008065 0.50000268833333333336
@@ -42,7 +33,24 @@ Results:
 
 On csltok:
 
-Random> time ./TimingBernoulli12
+Random> ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.3.2
+ last change:       19.3.2019
+ git-id:            0e33bd127e3ef9e78912f8c8a900413d59382c25
+machine name:       csltok.swansea.ac.uk
+ bogomips:          4788.21
+compiler version:   g++ 8.3.0
+ date:              Mar_19_2019 20:37:29
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
+()
+1499919941 0.49997331366666666666
+3e+09 0
+MIN + MAX user times:
+15.97 16.25
+
 TimingBernoulli12 0.2.3 15.3.2019 c3e4d5e6d6b055a2dbb378bc8479bae92d29b3bc
 csltok.swansea.ac.uk 4788.21
 g++ 8.3.0 Mar_15_2019 18:54:05
@@ -55,52 +63,68 @@ real    0m14.575s
 user    0m14.504s
 sys     0m0.033s
 
-So roughly 207e6 generations per sec, which is 91.6% of the pure
-generation-speed.
+Roughly 3e9 / 15.97 ~ 188e6 generations per sec, which is
+188 / 225 ~ 84% of pure generation speed.
+And this is 188 / 207 ~ 91% of the speed of the previous version,
+where just trivial changes took place -- the compilation is "all over
+the place".
 
 
 On cs-wsok:
 
-Random> time ./TimingBernoulli12
-TimingBernoulli12 0.2.3 15.3.2019 4c4213e5b304b64d8c3438be8c9da3d7d058ee34
-cs-wsok 5986.74
-g++ 8.3.0 Mar_15_2019 19:02:25
---std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only
-3000000000
+Random> ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.3.2
+ last change:       19.3.2019
+ git-id:            0e33bd127e3ef9e78912f8c8a900413d59382c25
+machine name:       cs-wsok
+ bogomips:          5986.74
+compiler version:   g++ 8.3.0
+ date:              Mar_19_2019 20:41:28
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
 ()
 1499919941 0.49997331366666666666
-3e+09
-real    0m10.310s
-user    0m10.304s
-sys     0m0.000s
+3e+09 0
+MIN + MAX user times:
+11.90 12.00
 
-So roughly 291e6 generations per sec, which is 92.1% of pure
-generation-speed.
+Roughly 3e9 / 11.9 ~ 252e6 generations per sec, which is
+252 / 317 ~ 79.5% of pure generation-speed.
+And this is 252 / 291 ~ 87% of the speed of the previous version,
 
 
 On csverify:
 
-Random$ time ./TimingBernoulli12
-TimingBernoulli12 0.2.3 15.3.2019 4c4213e5b304b64d8c3438be8c9da3d7d058ee34
-csverify 7183.87
-g++ 8.2.0 Mar_15_2019 19:16:29
---std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only
-3000000000
+Random$ ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.3.2
+ last change:       19.3.2019
+ git-id:            0e33bd127e3ef9e78912f8c8a900413d59382c25
+machine name:       csverify
+ bogomips:          7183.87
+compiler version:   g++ 8.2.0
+ date:              Mar_19_2019 20:42:57
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
 ()
 1499919941 0.49997331366666666666
-3e+09
-real    0m5.746s
-user    0m5.746s
-sys     0m0.000s
+3e+09 0
+MIN + MAX user times:
+6.10 6.39
 
-So roughly 522e6 generations per sec, which is 92% of pure
-generation-speed.
+Roughly 3e9 / 6.1 ~ 492e6 generations per sec, which is
+492 / 568 ~ 86.6% of pure generation-speed.
+And this is 492 / 522 ~ 94% of the speed of the previous version,
 
 
 It is actually surprising that adding just one small test to the generation
-slows it down by 7%: the generation should be a much more involved computation,
-and so the effect of adding this test should be smaller? Apparently the
-computation of g() is very fast, so that such a small addition matters.
+slows it down by ~ 10%: the generation should be a much more involved
+computation, and so the effect of adding this test should be smaller?
+Apparently the computation of g() is very fast, so that such a small addition
+matters. Or it is the compilation.
 
 */
 
