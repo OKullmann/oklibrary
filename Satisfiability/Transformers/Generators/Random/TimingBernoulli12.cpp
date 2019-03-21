4f64d56322f46a260d0711d3afef003f349bcca9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-21 19:52:47 +0000

    Update of numerical details.

--- Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
+++ Satisfiability/Transformers/Generators/Random/TimingBernoulli12.cpp
@@ -9,6 +9,17 @@ License, or any later version. */
 
 Example (annotation on next line):
 
+Random> ./TimingBernoulli12 -v
+program name:       TimingBernoulli12
+ version:           0.4.0
+ last change:       21.3.2019
+ git-id:            e130527c6a50f5016461088eeffdee7e29961895
+machine name:       csltok.swansea.ac.uk
+ bogomips:          4788.21
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 19:27:32
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+
 Random> ./TimingBernoulli12 3e9 0
 # number N of calls of the generator and number of discards (default values),
 # plus sequence of 64-bit seed values (empty by default)
@@ -16,16 +27,24 @@ Random> ./TimingBernoulli12 3e9 0
 # the arguments (in integer-format)
 ()
 # the list of 32-bit seeds (initialising the generator)
-1499919941 0.49997331366666666666
-# the count of results "true" and their relative frequency
+1499919941 0.49997331366666666666 0.0034629664979143074932
+# the count of results "true", their relative frequency, and the p-value
 3e+09 0
 # N and discards again, in float80-precision.
+
+Remark: The p-value is very low: the probability that with 3e9 fair coin-flips
+we obtain a deviation of the measured frequency f to the true probability of
+at least the given value, i.e.,
+  |f-0.5| >= 0.5 - 0.49997331366666666666
+holds, is 0.35%, which is a low probability, and one would reject that the
+given sequence is "random", due to having too few "true"-results.
+This 0/1-sequence is further evaluated in Distributions.hpp.
 
 With one seed-value (a 64-bit 0, yielding two 32-bit 0s):
 Random> ./TimingBernoulli12 3e9 0 0
 3000000000 0 0
 (0,0)
-1500008065 0.50000268833333333336
+1500008065 0.50000268833333333336 0.76838185202788818196
 3e+09 0
 
 
@@ -57,6 +76,27 @@ And this is 188 / 207 ~ 91% of the speed of the previous version,
 where just trivial changes took place -- the compilation is "all over
 the place".
 
+New version:
+andom> ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.4.0
+ last change:       21.3.2019
+ git-id:            e130527c6a50f5016461088eeffdee7e29961895
+machine name:       csltok.swansea.ac.uk
+ bogomips:          4788.21
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 19:27:32
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
+()
+1499919941 0.49997331366666666666 0.0034629664979143074932
+3e+09 0
+MIN + MAX user times:
+16.08 16.37
+
+It seems that csltok has quite a high runtime-variability.
+
 
 On cs-wsok:
 
@@ -82,6 +122,27 @@ Roughly 3e9 / 11.9 ~ 252e6 generations per sec, which is
 252 / 317 ~ 79.5% of pure generation-speed.
 And this is 252 / 291 ~ 87% of the speed of the previous version,
 
+New version:
+Random> ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.4.0
+ last change:       21.3.2019
+ git-id:            e130527c6a50f5016461088eeffdee7e29961895
+machine name:       cs-wsok
+ bogomips:          5986.74
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 19:42:15
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
+()
+1499919941 0.49997331366666666666 0.0034629664979143074932
+3e+09 0
+MIN + MAX user times:
+11.87 12.02
+
+Basically the same.
+
 
 On csverify:
 
@@ -106,6 +167,27 @@ MIN + MAX user times:
 Roughly 3e9 / 6.1 ~ 492e6 generations per sec, which is
 492 / 568 ~ 86.6% of pure generation-speed.
 And this is 492 / 522 ~ 94% of the speed of the previous version,
+
+New version:
+Random$ ./RunTime ./TimingBernoulli12
+program name:       TimingBernoulli12
+ version:           0.4.0
+ last change:       21.3.2019
+ git-id:            e130527c6a50f5016461088eeffdee7e29961895
+machine name:       csverify
+ bogomips:          7183.87
+compiler version:   g++ 8.2.0
+ date:              Mar_21_2019 19:45:42
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+3000000000 0
+()
+1499919941 0.49997331366666666666 0.0034629664979143074932
+3e+09 0
+MIN + MAX user times:
+6.13 6.42
+
+Basically the same.
 
 
 It is actually surprising that adding just one small test to the generation
