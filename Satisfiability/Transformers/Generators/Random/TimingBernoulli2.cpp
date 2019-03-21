c133fd9f56b2a03a283ddfca0b710824253d6ff8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-03-21 21:43:49 +0000

    Update of numerical data.

--- Satisfiability/Transformers/Generators/Random/TimingBernoulli2.cpp
+++ Satisfiability/Transformers/Generators/Random/TimingBernoulli2.cpp
@@ -9,6 +9,17 @@ License, or any later version. */
 
 Example (annotation on next line):
 
+andom> ./TimingBernoulli2 -v
+program name:       TimingBernoulli2
+ version:           0.3.0
+ last change:       21.3.2019
+ git-id:            efd3a73bdaecf88852ec2e5cbda2dfc5c225d460
+machine name:       csltok.swansea.ac.uk
+ bogomips:          4788.21
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 21:30:08
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+
 Random> ./TimingBernoulli2 1e9 3 1
 # number N of calls of the generator, exponent e of denominator, nominator x (default values)
 # plus sequence of 64-bit seed values (empty by default)
@@ -16,8 +27,8 @@ Random> ./TimingBernoulli2 1e9 3 1
 # the arguments (in integer-format)
 ()
 # the list of 32-bit seeds (initialising the generator)
-124997746 0.124997746
-#  the count of results "true" and their relative frequency
+124997746 0.124997746 0.82935909643158182823
+# the count of results "true", their relative frequency, and the p-value
 1e+09 0.125
 # N again, in float80-precision, and x/2^e.
 
@@ -48,6 +59,25 @@ Roughly 1e9 / 5.38 ~  186e6 generations per sec, 186 / 188 ~ 99%
 of the bernoulli-speed.
 This is 186 / 189 ~ 98% of the speed of the previous version.
 
+New version:
+andom> ./RunTime ./TimingBernoulli2
+program name:       TimingBernoulli2
+ version:           0.3.0
+ last change:       21.3.2019
+ git-id:            efd3a73bdaecf88852ec2e5cbda2dfc5c225d460
+machine name:       csltok.swansea.ac.uk
+ bogomips:          4788.21
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 21:30:08
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+1000000000 3 1
+()
+124997746 0.124997746 0.82935909643158182823
+1e+09 0.125
+MIN + MAX user times:
+5.42 5.54
+
 
 On cs-wsok:
 
@@ -69,21 +99,28 @@ Output program:
 MIN + MAX user times:
 4.14 4.18
 
-TimingBernoulli2 0.2.0 16.3.2019 954cfb6d774b9f562424d99234bbd1df6e75bc52
-cs-wsok 5986.74
-g++ 8.3.0 Mar_16_2019 21:22:22
---std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static -fno-finite-math-only
-1000000000 3 1
-()
-124997746 0.124997746
-1e+09 0.125
-real    0m3.951s
-user    0m3.947s
-sys     0m0.000s
-
 Roughly 1e9 / 4.14 ~  242e6 generations per sec, 242 / 252 ~ 96%
 of the bernoulli-speed.
 This is 242 / 253 ~ 96% of the speed of the previous version.
+
+New version:
+Random> ./RunTime ./TimingBernoulli2
+program name:       TimingBernoulli2
+ version:           0.3.0
+ last change:       21.3.2019
+ git-id:            efd3a73bdaecf88852ec2e5cbda2dfc5c225d460
+machine name:       cs-wsok
+ bogomips:          5986.74
+compiler version:   g++ 8.3.0
+ date:              Mar_21_2019 21:33:49
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+1000000000 3 1
+()
+124997746 0.124997746 0.82935909643158182823
+1e+09 0.125
+MIN + MAX user times:
+4.15 4.19
 
 
 On csverify:
@@ -109,6 +146,25 @@ MIN + MAX user times:
 Roughly 1e9 / 2.39 ~  418e6 generations per sec, 418 / 492 ~ 85%
 of the bernoulli-speed.
 A surprising drop.
+
+New version:
+Random$ ./RunTime ./TimingBernoulli2
+program name:       TimingBernoulli2
+ version:           0.3.0
+ last change:       21.3.2019
+ git-id:            efd3a73bdaecf88852ec2e5cbda2dfc5c225d460
+machine name:       csverify
+ bogomips:          7183.87
+compiler version:   g++ 8.2.0
+ date:              Mar_21_2019 21:35:46
+ options:           --std=c++17 -pedantic -Ofast -DNDEBUG -march=native -fwhole-program -static   -fno-finite-math-only
+Output program:
+1000000000 3 1
+()
+124997746 0.124997746 0.82935909643158182823
+1e+09 0.125
+MIN + MAX user times:
+2.39 2.45
 
 */
 
