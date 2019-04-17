d2a94a724f6211cef549757386ef3af51915e21b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-04-17 12:04:41 +0100

    Planning for choose k from n.

--- Satisfiability/Transformers/Generators/Random/Algorithms.hpp
+++ Satisfiability/Transformers/Generators/Random/Algorithms.hpp
@@ -15,7 +15,49 @@ TODOS:
 
   - Just giving n, this means "from 1,...,n".
   - Alternatively a vector v (length at least k) is given, from
-    which to choose.
+    which to choose. Though this could be handled by just choosing
+    the indices.
+  - So perhaps just starting with parameter n, which means {0, ..., n-1}.
+  - The output is most naturally a vector; perhaps we just fix gen_uint_t
+    as the index-type (of n). The output is sorted.
+  - There is a choice of including versus including: choosing 3 from 1000
+    the obvious choice is including, while choosing 997 from 1000 the
+    obvious choice is excluding. The splitting point in this example is
+    500 from 1000, where here the choice doesn't matter.
+
+    The point is here that the number of random choices is to be minimised.
+  - It seems natural to provide a RandGen_t via reference, for using the
+    service once.
+  - But there are also natural applications where k, n are fixed, and
+    where a stream of choices is to be done.
+  - The main loop is a choice of k numbers s_1,...,s_k obtaind from
+    UniformRange, for n, n-1, ..., n-k+1 (either for inclusion or exclusion).
+  - Then a helper-function transforms this vector into the result-vector.
+  - In the inclusion-case, the first number obtained is s_1, the second is
+    s_2 applied to {0,...,n-1} - {s_1}, and so on.
+
+    So for each s_i, one makes a binary search with the s_1, ..., s_{i-1},
+    and according to the position found one substracts from s_i.
+  - For a given vector from which to chose, one can just doing it as in
+    shuffle, just considering k steps. But in general n is much larger
+    than k, and so creating the vector would be too costly.
+
+    However one needs actually to store only the changed elements, which
+    could be done in a std::map. That is, the algorithm to transform
+    s_1, ..., s_k is as follows: the first number is s_1, and implicitly
+    s_1 is replaced by n-1. If s_2 != s1, then s_2 is the next number,
+    and we implicitly replace s_2 by n-2; otherwise the next number is n-1.
+    and s_1 is implicitly replaced by n-2. "Implicitly replaced" can just
+    mean using std::map m: Initially m is empty. The algorithm for s_i
+    determines whether s_i is in m: if no, then the chosen number is s_i,
+    otherwise it is m[s_i]. In both cases, then update m[s_i] = n-i.
+
+    One could use a round-counter and a static array for m, which would
+    guarantee constant-time lookup. (This doesn't yield the chosen elements
+    in sorted order.) However std::map should be sufficient.
+  - In the exclusion-case, one could select n-k elements, and then leave
+    them out in the result vector. This can be done by first override the
+    values at the chosen index with n, and then remove those elements = n.
 
 2. Add a further version of shuffle, accepting RandGen_t.
 
