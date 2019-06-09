2228d8a92aed21bacca1711569536d0cbac4b8a4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-06-09 15:49:13 +0100

    Started providing generators for random DQCNFs.

---
+++ Satisfiability/Transformers/Generators/Random/DQCNF.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 9.6.2019 (Swansea)
+/* Copyright 2019 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/* Random dependency-quantified CNF
+
+DESIGN:
+
+*/
+
+#ifndef DQCNF_Z7vpC0rD5U
+#define DQCNF_Z7vpC0rD5U
+
+#include "Distributions.hpp"
+#include "ClauseSets.hpp"
+
+namespace RandGen {
+
+}
+
+#endif
---
+++ Satisfiability/Transformers/Generators/Random/TestDQCNF.cpp
@@ -0,0 +1,31 @@
+// Oliver Kullmann, 9.6.2019 (Swansea)
+/* Copyright 2019 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <iostream>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "DQCNF.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "9.6.2019",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Random/TestDQCNF.cpp",
+        "GPL v3"};
+
+  using namespace RandGen;
+
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+}
