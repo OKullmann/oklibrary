c7431ccf75f67ccf406a482d694e7b009bbff713
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-08-09 22:20:41 +0100

    Update of documentation.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/Caching.hpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/Caching.hpp
@@ -10,14 +10,22 @@ License, or any later version. */
   - const maxN = 32
 
   - struct ClosedLines (members d,a,r,c)
+  - operators ==, !=, <, <<
+  - functor HashClosedLines
   - functions used_da, used_da_inverse, used_rc, used_rc_inverse to
     construct a ClosedLines-object from the ranks for a Rooks_board
   - used_lines wraps usage of used_da, used_rc
 
   - scoped enum CS (caching schemes)
+  - operator <<
+
+  -typedef Count_t
 
   - class FullCaching_map
-  - class FullSymCaching_map
+
+  - helper type-traits memman ("memory management")
+  - class template FullSymCaching<MAP>
+  - instantiations FullSymCaching_map, FullSymCaching_hash
 
 */
 
