30498abd497ca04c633daf1feae5a3293e155991
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-11-21 11:43:35 +0000

    First (untested) implementation of counting connected components.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/Solutions.hpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/Solutions.hpp
@@ -23,9 +23,12 @@ TODO:
 #include <limits>
 #include <array>
 #include <bitset>
-#include <utility>
+#include <algorithm>
+#include <vector>
 
 #include <cassert>
+
+#include "ChessBoard.hpp"
 
 namespace Solutions {
 
@@ -34,7 +37,7 @@ namespace Solutions {
 
   constexpr lines_t maxN = std::numeric_limits<lines_t>::max() / 2 - 1;
   constexpr lines_t N = 8;
-  static_assert(N <= maxN);
+  static_assert(N >= 1 and N <= maxN);
 
   // The solution type:
   typedef std::array<lines_t, N> solution_t;
@@ -66,6 +69,45 @@ namespace Solutions {
       else return false;
     }
     return true;
+  }
+
+  typedef std::vector<solution_t> solution_vector;
+  typedef std::vector<ChessBoard::Count_t> solution_ccs;
+
+  void dfs_visit(const ChessBoard::Count_t v, const solution_vector& V, std::vector<bool>& visited, const ChessBoard::Count_t current_cc, solution_ccs& res) noexcept {
+    assert(v < V.size());
+    assert(visited.size() == V.size());
+    assert(res.size() == V.size());
+    assert(not visited[v]);
+    visited[v] = true;
+    res[v] = current_cc;
+    using ChessBoard::Count_t;
+    for (lines_t i = 0; i < N-1; ++i) {
+      for (lines_t j = i+1; j < N; ++i) {
+        const solution_t S = flip(V[v], i, j);
+	if (valid_da(S)) {
+          const auto it = std::lower_bound(V.begin(), V.end(), S);
+	  assert(it != V.end());
+          const Count_t w = it - V.begin();
+	  if (visited[w]) continue;
+	  dfs_visit(w, V, visited, current_cc, res);
+	}
+      }
+    }
+  }
+
+  solution_ccs determine_1ccs(const solution_vector& V) {
+    assert(std::is_sorted(V.begin(), V.end()));
+    solution_ccs res(V.size());
+    using ChessBoard::Count_t;
+    Count_t current_cc = 0;
+    std::vector<bool> visited(V.size());
+    for (Count_t i = 0; i < V.size(); ++i) {
+      if (visited[i]) continue;
+      dfs_visit(i, V, visited, current_cc, res);
+      ++current_cc;
+    }
+    return res;
   }
 
 }
