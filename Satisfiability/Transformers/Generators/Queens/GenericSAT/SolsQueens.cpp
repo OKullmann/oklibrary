963cc5311253ec957c98c5862fd827bf19811b1f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2019-11-25 20:39:15 +0000

    Enabling choice of only k=1,2 for the k-neighbourhoods.
    Using the general tool from Environment for handling option-enum CK
    is a bit of an overkill here, but it's for practice.

--- Satisfiability/Transformers/Generators/Queens/GenericSAT/SolsQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/SolsQueens.cpp
@@ -45,7 +45,7 @@ TODOS:
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.2.4",
+        "0.2.5",
         "25.11.2019",
         __FILE__,
         "Oliver Kullmann",
@@ -57,11 +57,15 @@ bool show_usage(const int argc, const char* const argv[]) {
   if (argc != 2 or not Environment::is_help_string(argv[1])) return false;
   const std::string& program = proginfo.prg;
   std::cout << "USAGE:\n"
-  "> " << program << " [-v | --version]\n"
+  "> " << program << " (-v | --version)\n"
   " shows version information and exits.\n"
-  "> " << program << " [-h | --help]\n"
+  "> " << program << " (-h | --help)\n"
   " shows help information and exits.\n"
-  "Run make with arguments \"-B CPPFLAGS=-DNQUEENS=12\" for e.g. N=12.\n"
+  "> " << program << " [c1|c2|c12]\n"
+  " runs the program with the built-in N, computing 1/2-components, or both"
+  " (the default).\n"
+  "Run make with arguments \"-B CPPFLAGS=-DNQUEENS=12\" for compiling with e.g. N=12.\n"
+  "Use the tool \"RSolsQueens\" for integrated compilation and running.\n"
 ;
   return true;
 }
@@ -86,12 +90,37 @@ bool show_usage(const int argc, const char* const argv[]) {
     void unsat(const ActiveClauseSet&) noexcept {}
   };
 
+  // Which of k-neighbourhoods, k=1,2, to compute:
+  enum class CK { both12=0, only1=1, only2=2 };
+}
+namespace Environment {
+  template <>
+  struct RegistrationPolicies<CK> {
+   static constexpr int size = int(CK::only2) + 1;
+   static constexpr std::array<const char*, size> string
+      {"c12", "c1", "c2"};
+  };
+}
+namespace {
+  std::ostream& operator <<(std::ostream& out, const CK k) {
+    switch (k) {
+    case CK::both12 : return out << "both12";
+    case CK::only1 : return out << "only1";
+    case CK::only2 : return out << "only2";
+    default : return out << "UNKNOWN:" << int(k);}
+  }
+  typedef std::tuple<CK> options_t;
+  constexpr char sep = ',';
 }
 
 int main(const int argc, const char* const argv[]) {
 
   if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
   if (show_usage(argc, argv)) return 0;
+
+  Environment::Index index;
+  const options_t choices = (argc <= index) ? options_t{} : Environment::translate<options_t>()(argv[index++], sep);
+  const CK level = std::get<CK>(choices);
 
   std::cout << Environment::Wrap(proginfo, Environment::OP::dimacs);
   using Environment::DHW;
@@ -102,6 +131,7 @@ int main(const int argc, const char* const argv[]) {
   Environment::args_output(std::cout, argc, argv);
   std::cout << "\n"
             << DWW{"N"} << (unsigned)N << "\n"
+            << DWW{"computation"} << level << "\n"
 ; std::cout.flush();
 
   NQueens::AmoAlo_board Fq(N);
@@ -113,14 +143,18 @@ int main(const int argc, const char* const argv[]) {
   std::cout << num_solutions << std::endl;
   std::sort(B.L.V.begin(), B.L.V.end());
 
-  const auto f1 = frequencies(determine_ccs1<N>(B.L.V));
-  std::cout << f1 << std::endl;
-  assert(not f1.first.empty());
   using FloatingPoint::float80;
-  std::cout << float80((--f1.first.cend())->first) / num_solutions << "\n";
+  if (level != CK::only2) {
+    const auto f1 = frequencies(determine_ccs1<N>(B.L.V));
+    std::cout << f1 << std::endl;
+    assert(not f1.freq.empty());
+    std::cout << float80((--f1.freq.cend())->first) / num_solutions << "\n";
+  }
 
-  const auto f2 = frequencies(determine_ccs2<N>(B.L.V));
-  std::cout << f2 << "\n";
-  assert(not f2.first.empty());
-  std::cout << float80((--f2.first.cend())->first) / num_solutions << "\n";
+  if (level != CK::only1) {
+    const auto f2 = frequencies(determine_ccs2<N>(B.L.V));
+    std::cout << f2 << "\n";
+    assert(not f2.freq.empty());
+    std::cout << float80((--f2.freq.cend())->first) / num_solutions << "\n";
+  }
 }
