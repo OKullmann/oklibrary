ecdae7b6e8fe3da22ffd03c9490a6f24918efee5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-04-25 03:55:12 +0100

    Implemented ucp (for the rows).

--- Satisfiability/Transformers/Generators/Queens/SimpleBacktracking/Queens_RUCP_ct.cpp
+++ Satisfiability/Transformers/Generators/Queens/SimpleBacktracking/Queens_RUCP_ct.cpp
@@ -8,17 +8,17 @@ License, or any later version. */
 #include <bitset>
 #include <iostream>
 #include <string>
+#include <array>
 
 #include <ProgramOptions/Environment.hpp>
 
 #include <cstdlib>
-#include <cstdint>
 #include <cassert>
 
 namespace {
 
 const Environment::ProgramInfo proginfo{
-      "0.1.0",
+      "0.1.1",
       "25.4.2020",
       __FILE__,
       "Oliver Kullmann",
@@ -29,7 +29,7 @@ typedef std::size_t size_t;
 #ifndef NN
 # error "NN must be defined."
 #endif
-constexpr size_t n=NN;
+constexpr size_t N=NN;
 
 bool show_usage(const int argc, const char* const argv[]) {
   assert(argc >= 1);
@@ -37,8 +37,107 @@ bool show_usage(const int argc, const char* const argv[]) {
     return false;
   std::cout <<
     "> " << proginfo.prg << "\n"
-    " runs the program for built-in N = " << n << ".\n";
+    " runs the program for built-in N = " << N << ".\n";
   return true;
+}
+
+
+typedef std::bitset<N> row_t; // "true" means forbidden or occupied
+typedef std::array<row_t,N> board_t;
+
+
+typedef std::bitset<3*N> extrow_t;
+inline extrow_t embed(const row_t& r) noexcept {
+  extrow_t res;
+  for (size_t i = 0; i < N; ++i) res[N+i] = r[i];
+  return res;
+}
+inline row_t truncate(const extrow_t& er) noexcept {
+  row_t res;
+  for (size_t i = 0; i < N; ++i) res[i] = er[N+i];
+  return res;
+}
+inline void add(const row_t& r, extrow_t& er) noexcept {
+  er |= embed(r);
+}
+
+
+struct Board {
+  board_t b;
+  size_t i; // i <= N
+  bool falsified_;
+
+  bool falsified() const noexcept { return falsified_; }
+  bool satisfied() const noexcept { return not falsified_ and i == N; }
+};
+
+
+// Propagate the queen set in the current bottom-row:
+inline void ucp(Board& B) noexcept {
+  if (N <= 1) return;
+  assert(not B.falsified());
+  assert(not B.satisfied());
+  assert(B.i < N-1);
+  assert(B.b[B.i].count() == 1);
+
+  row_t units = B.b[B.i];
+  ++B.i;
+  extrow_t diag = embed(units), antidiag = diag;
+  bool found;
+  do {
+    // Upsweep:
+    found = false;
+    for (size_t j = B.i; j != N; ++j) {
+      diag >>= 1;
+      antidiag <<= 1;
+      if (B.b[j].none()) continue;
+      assert(B.b[j].count() < N);
+      const row_t new_row = B.b[j] | units | truncate(diag) | truncate(antidiag);
+      const auto count = new_row.count();
+      if (count == N) {
+        B.falsified_ = true;
+        return;
+      }
+      else if (count == N-1) {
+        const row_t new_unit = ~ new_row;
+        units |= new_unit;
+        add(new_unit, diag);
+        add(new_unit, antidiag);
+        B.b[j].reset();
+        found = true;
+      }
+      else B.b[j] = new_row;
+    }
+    if (not found) break;
+
+    // Downsweep:
+    found = false;
+    for (size_t j0 = N; j0 != B.i; --j0) {
+      const size_t j = j0-1;
+      diag <<= 1;
+      antidiag >>= 1;
+      if (B.b[j].none()) continue;
+      assert(B.b[j].count() < N);
+      const row_t new_row = B.b[j] | units | truncate(diag) | truncate(antidiag);
+      const auto count = new_row.count();
+      if (count == N) {
+        B.falsified_ = true;
+        return;
+      }
+      else if (count == N-1) {
+        const row_t new_unit = ~ new_row;
+        units |= new_unit;
+        add(new_unit, diag);
+        add(new_unit, antidiag);
+        B.b[j].reset();
+        found = true;
+      }
+      else B.b[j] = new_row;
+    }
+    while (B.i < N and B.b[B.i].none()) ++B.i;
+    if (B.i == N) return;
+    assert(B.i < N-1);
+  } while (found);
 }
 
 }
