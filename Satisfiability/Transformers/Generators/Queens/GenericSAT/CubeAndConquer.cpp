cabe84393e14f8fdc20b6fc93f36f58ab7d5e7b8
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-05-03 14:52:29 +0100

    Started investigations into initial symmetry-breaking for N-Queens.
    And started with C&C for N-Queens.

---
+++ Satisfiability/Transformers/Generators/Queens/GenericSAT/CubeAndConquer.cpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 3.5.2020 (Swansea)
+/* Copyright 2020 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/* Cube-and-Conquer for N-Queens
+
+  Three phases:
+
+  (I)   Initial symmetry breaking
+
+        See data/CubeAndConquer/InitialSymmetryBreaking
+
+  (II)  Cube-splitting
+
+  (III) Conquering
+
+*/
+
