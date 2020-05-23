91070f4fe5c30959193a7b8f777fc611c9c35212
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-05-23 14:57:19 +0100

    Data updates.

---
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n400/Overview.R
@@ -0,0 +1,5 @@
+# c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
+ c    pa0               pa1ma0            a0stm       a1stm       N
+830   XXX
+840   0.1266091         0.4351626         0.02057     4.775       182017
+850   0.07698376        0.3522032         0.01893     4.596       181259
---
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t6/k5/e3/n600/Overview.R
@@ -0,0 +1,3 @@
+# c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
+ c    pa0               pa1ma0            a0stm       a1stm       N
+1260  0.327044          0.3710692         4.424707    7145.39     159
