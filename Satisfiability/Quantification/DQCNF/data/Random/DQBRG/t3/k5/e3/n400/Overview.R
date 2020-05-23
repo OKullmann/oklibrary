27b3742d89677bba39ff80096c39a86a05940a28
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-05-24 00:12:50 +0100

    Data updates.

--- Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n400/Overview.R
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n400/Overview.R
@@ -1,5 +1,6 @@
 # c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
  c    pa0               pa1ma0            a0stm       a1stm       N
-830   XXX
+820   XXX
+830   0.2000703         0.491929          0.02180     4.76        182071
 840   0.1266091         0.4351626         0.02057     4.775       182017
 850   0.07698376        0.3522032         0.01893     4.596       181259
