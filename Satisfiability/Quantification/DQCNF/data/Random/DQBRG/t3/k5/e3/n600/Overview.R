6f51fa7c7b90670dcc8fbefe3884186c727e17ba
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-05-29 11:42:29 +0100

    Update on threshold-data.

--- Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n600/Overview.R
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n600/Overview.R
@@ -1,4 +1,6 @@
 # c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
 # Maximum: rho ~ ?
  c    pa0               pa1ma0            a0stm       a1stm       N
-1230  XXX
+1220  0.3057734         0.5939429         1.0691      808.87      11986
+1230  0.219347          0.6100799         1.000980    1026.640    7381
+1240  XXX
