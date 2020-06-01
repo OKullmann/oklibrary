e60bb503e777dce81c9dba58216251d761404064
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-01 06:51:13 +0100

    Update of license-statement.

--- Satisfiability/Transformers/Generators/Queens/SimpleBacktracking/NQueens.cpp
+++ Satisfiability/Transformers/Generators/Queens/SimpleBacktracking/NQueens.cpp
@@ -1,29 +1,11 @@
 // Oliver Kullmann 25.5.2018 (Swansea)
+/* Copyright 2018, 2019, 2020 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
 /*
-  Copyright Alessandro Nicolosi 2016, 2017
-  Copyright Oliver Kullmann 2018, 2019
-
-MIT License
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-  Started as copy from
-  https://github.com/alenic/nqueens/commits/master
-  ID f50264291093fe8529d143995ae9784f097d7ebf.
 
 The basic idea is the same as explained at
   https://helloacm.com/n-queen-problem-in-back-tracing-bit-logics/
