46be7ed48964d03d4bccaf46f4ae1fc57a38e968
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-06 01:05:39 +0100

    More overview.

---
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k3/e1-3/n800/Overview.R
@@ -0,0 +1,10 @@
+# c,pao,pa1ma0,a0stm,a1stm,N as before
+# punkma1   : probability hqs-unknown which are not a1-sat
+# punka1ma0 : probability hqs-unknown and a1-a0-sat
+# punka0    : probability hqs-unknown and a0-sat
+# paima1    : probability sat (a-infinity) which are not a1-sat
+# hqsa1stm  : mean runtime of hqs on a1-a0-satisfiable instances
+ c    pa0               pa1ma0            a0stm       a1stm       N           punkma1     punka1ma0         punka0      paima1            hqsa1stm
+1500  0.98722469        0.0006098768      2.1249      71.36       186923      NA          NA                NA          NA                NA
+1500  NA                NA                NA          NA          129569      0.002083832 0.0002392548      0.02292987  0                 1060.37
+1600  
