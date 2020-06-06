b38d7a7bb50c0eb71b283102ce6fddc7fcb29572
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-06 10:02:30 +0100

    Completed overview for t3,k3,e1-3.

--- Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k3/e1-3/n800/Overview.R
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k3/e1-3/n800/Overview.R
@@ -4,7 +4,11 @@
 # punka0    : probability hqs-unknown and a0-sat
 # paima1    : probability sat (a-infinity) which are not a1-sat
 # hqsa1stm  : mean runtime of hqs on a1-a0-satisfiable instances
- c    pa0               pa1ma0            a0stm       a1stm       N           punkma1     punka1ma0         punka0      paima1            hqsa1stm
-1500  0.98722469        0.0006098768      2.1249      71.36       186923      NA          NA                NA          NA                NA
-1500  NA                NA                NA          NA          129569      0.002083832 0.0002392548      0.02292987  0                 1060.37
-1600  
+ c    pa0               pa1ma0            a0stm       a1stm       N           punkma1           punka1ma0         punka0      paima1            hqsa1stm
+1500  0.98722469        0.0006098768      2.1249      71.36       186923      NA                NA                NA          NA                NA
+1500  NA                NA                NA          NA          129569      0.002083832       0.0002392548      0.02292987  0                 1060.37
+
+1600  0.3676729         0.003510004       38.840      62.206      5698        0.08494208        0.0026325026      0.1493506   0                 1561.4
+
+1700  0.004510894       0.0001162601      12.816      14.9366     43007       NA                NA                NA          NA                NA
+1700  NA                NA                NA          NA          38106       1.724138e-02      5.248517e-05      0.002519288 0                 1042.28
