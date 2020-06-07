d6622c8f1e92bf766d8f2aeea14c1b831e8d109a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-07 20:14:54 +0100

    Data update.
    Enough data for now (initial exploration over, and more focussed questions
    needed).

--- Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t9/k5/e3/n400/Overview.R
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t9/k5/e3/n400/Overview.R
@@ -1,6 +1,7 @@
+# DQBRG "a200 200" 1800 "c*1,2|2-2,3"
 # c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
-# Maximum: rho ~ ?
+# Maximum: rho ~ 840 / 200 = 4.2
  c    pa0               pa1ma0            a0stm       a1stm       N
-830   XXX
+830   0.5989729         0.2138148         0.08128     120.08      112546
 840   0.4590435         0.2362643         0.08773     119.39      98151
 850   0.3284921         0.2283843         0.08895     107.969     75119
