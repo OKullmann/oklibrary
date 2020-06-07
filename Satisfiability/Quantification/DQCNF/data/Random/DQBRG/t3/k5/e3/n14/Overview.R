deb55d833d9909af3faf85f5a261fc0eb7febd9a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-07 17:28:54 +0100

    20 easy instances for hqs (around 600s).

---
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n14/Overview.R
@@ -0,0 +1,5 @@
+# DQBRG "a14 14" 42 "c*1,2|2-2,3" ""
+# c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
+# Maximum: rho ~ 820 / 200 = 4.1.
+ c    pa0               pa1ma0            a0stm       a1stm       N
+57    0.790821          0.129879          0.001778    0.004060    98638
