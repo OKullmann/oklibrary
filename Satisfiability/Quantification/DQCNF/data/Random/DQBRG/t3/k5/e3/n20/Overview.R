ebc5c9caa61ba74c49167a45c8bfc694aaef4f93
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-06-07 17:54:25 +0100

    More data.

--- Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n20/Overview.R
+++ Satisfiability/Quantification/DQCNF/data/Random/DQBRG/t3/k5/e3/n20/Overview.R
@@ -1,5 +1,5 @@
-# DQBRG "a14 14" 42 "c*1,2|2-2,3" ""
+# DQBRG "a20 20" 60 "c*1,2|2-2,3" ""
 # c   table(E$a0s_res)[1]/nrow(E)   table(E$a1s_res)[1]/nrow(E)   a0s_time:mean     a1s_t:mean  nrow(E)
 # Maximum: rho ~ ?.
  c    pa0               pa1ma0            a0stm       a1stm       N
-57    0.790821          0.129879          0.001778    0.004060    98638
+82    0.7301722         0.1509118         0.001866    0.005745    127611
