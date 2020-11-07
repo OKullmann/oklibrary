166b631cfbdafb8abcc1a977a5de349aef23393f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-11-07 13:26:14 +0000

    Implemented wtau(a) in both variations.

--- Programming/Numerics/Tau.hpp
+++ Programming/Numerics/Tau.hpp
@@ -20,30 +20,56 @@ namespace Tau {
 
   namespace FP = FloatingPoint;
 
+  // Where the mean-lower-bound equals the Lambert-W-lower-bound:
+  constexpr FP::float80 tau_meaneqLW = 2.8811206627473383049862597L;
+
   inline constexpr FP::float80 wtau_ge1(const FP::float80 a) noexcept {
     assert(a >= 1);
     if (FP::isinf(a)) return FP::pinfinity;
     if (a == 1) return FP::Log2;
-    FP::float80 x0 = 0;
-
-    return x0;
+    FP::float80 ra = 1 /a, x0 =
+      a <= tau_meaneqLW ? FP::log(4) / (1 + ra) : FP::lambertW0_lb(a);
+    while (true) {
+      const FP::float80 A = FP::expm1(-x0), B = FP::exp(-ra * x0), N = A+B;
+      if (N <= 0) return x0;
+      const FP::float80 D = 1 / (FP::fma(ra, B, A) + 1);
+      assert(D > 0);
+      const FP::float80 x1 = FP::fma(N, D, x0);
+      assert(x1 >= x0);
+      if (x1 == x0) return x0;
+      x0 = x1;
+    }
   }
   static_assert(wtau_ge1(1) == FP::Log2);
   static_assert(wtau_ge1(FP::pinfinity) == FP::pinfinity);
-  //static_assert(wtau_ge1(2) == 2 * FP::log_golden_ratio);
+  static_assert(FP::abs(wtau_ge1(2) - 2 * FP::log_golden_ratio) < FP::epsilon);
   
 
   inline constexpr FP::float80 wtau_le1(const FP::float80 a) noexcept {
     assert(a <= 1);
     if (a == 0) return 0;
     if (a == 1) return FP::Log2;
-    FP::float80 x0 = 0;
-
-    return x0;
+    const FP::float80 ra = 1 / a;
+    FP::float80 x0 = FP::log(4) / (1 + ra);
+    while (true) {
+      const FP::float80 A = FP::expm1(-x0), B = FP::exp(-ra * x0), N = A+B;
+      if (N <= 0) return x0;
+      const FP::float80 D = 1 / (FP::fma(ra, B, A) + 1);
+      assert(D > 0);
+      const FP::float80 x1 = FP::fma(N, D, x0);
+      assert(x1 >= x0);
+      if (x1 == x0) return x0;
+      x0 = x1;
+    }
   }
   static_assert(wtau_le1(1) == FP::Log2);
   static_assert(wtau_le1(0) == 0);
-  //static_assert(wtau_le1(0.5L) == FP::log_golden_ratio);
+  static_assert(wtau_le1(0.5L) == FP::log_golden_ratio);
+  /* It holds
+     wtau_le1(x) = x * wtau_ge1(1/x) and wtau_ge1(x) = x * wtau_le1(1/x).
+  */
+  static_assert(wtau_le1(0.1L) == 0.1L * wtau_ge1(10));
+  static_assert(FP::abs(wtau_ge1(10L) - 10L * wtau_le1(0.1L)) < 2*FP::epsilon);
 
 }
 #endif
