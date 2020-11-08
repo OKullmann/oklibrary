71d4f1fa6337f6df0613bb0ed12e3de3600a73d7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-11-08 12:58:28 +0000

    Completed basic implementation for ExpSeq.

--- Satisfiability/Transformers/Generators/Random/Sequences.hpp
+++ Satisfiability/Transformers/Generators/Random/Sequences.hpp
@@ -14,11 +14,120 @@ License, or any later version. */
 #ifndef SEQUENCES_dZakTZB00i
 #define SEQUENCES_dZakTZB00i
 
+#include <ostream>
+
+#include <cassert>
+#include <cstdint>
+#include <cmath>
+
 #include "Numbers.hpp"
 
 namespace RandGen {
 
   struct ExpSeq {
+    typedef gen_uint_t size_t;
+    typedef std::uint8_t small_size_t;
+    const size_t E;
+    const small_size_t S;
+    const size_t s = std::pow(size_t(10), S);
+    constexpr static small_size_t max_S = 19;
+    static_assert(std::pow(size_t(10), max_S) < randgen_max);
+    const size_t N;
+    const bool ge1;
+
+    constexpr ExpSeq() = delete;
+    constexpr ExpSeq(const size_t E, const size_t S0, const size_t N, const bool ge1) noexcept : E(E), S(S0), N(N), ge1(ge1) {
+      assert(E >= 1);
+      assert(S0 >= 1);
+      assert(S0 <= max_S);
+      assert(N >= 1);
+    }
+
+    constexpr size_t main_size() const noexcept { return E * (s-1); }
+    constexpr size_t size() const noexcept { return main_size() * N; }
+
+    struct iterator_value {
+      const size_t i, s, j, val;
+      const bool ge1;
+    };
+    friend constexpr bool operator ==(const iterator_value& l, const iterator_value& r) {
+      return l.i==r.i && l.s==r.s && l.j==r.j && l.val==r.val && l.ge1==r.ge1;
+    }
+    friend std::ostream& operator <<(std::ostream& out, const iterator_value& v) {
+      out << v.i << " " << v.s << " " << v.j << " " << v.val << " " << v.ge1;
+      return out;
+    }
+
+  private :
+
+    struct iterator_t {
+
+      explicit iterator_t() = default;
+
+      iterator_t(const size_t E, const small_size_t S, const size_t s, const size_t N, const bool ge1) noexcept : E(E), S(S), s(s), N(N), ge1(ge1), g(new RandGen_t(transform({E,S,N,ge1}))), val((*g)()) {}
+
+      iterator_t(const iterator_t& it) noexcept : E(it.E), S(it.S), s(it.s), N(it.N), ge1(it.ge1), i(it.i), j(it.j), g(it.g ? new RandGen_t(*it.g):nullptr), val(it.val) {}
+      iterator_t& operator =(const iterator_t& it) noexcept {
+        E = it.E; S = it.S; s = it.s; N = it.N; ge1 = it.ge1;
+        i = it.i; j = it.j;
+        delete g;
+        g = it.g ? new RandGen_t(*it.g) : nullptr;
+        val = it.val;
+        return *this;
+      }
+
+      ~iterator_t() noexcept { delete g; }
+
+      iterator_value operator *() const noexcept {
+        return {i,s,j,val,ge1};
+      }
+      iterator_t& operator ++() noexcept {
+        assert(g);
+        if (++k == N) {
+          k = 0;
+          if (++j == s) {
+            j=1;
+            if (++i == E) {
+              delete g;
+              g = nullptr;
+              return *this;
+            }
+          }
+        }
+        val = (*g)();
+        return *this;
+      }
+
+      friend bool operator ==(const iterator_t& lhs, const iterator_t& rhs) noexcept {
+        return (lhs.g == nullptr and rhs.g == nullptr) or
+          (lhs.E==rhs.E&&lhs.S==rhs.S&&lhs.N==rhs.N&&lhs.ge1==rhs.ge1 and
+           lhs.i == rhs.i and lhs.j == rhs.j and lhs.g == rhs.g);
+      }
+      friend bool operator !=(const iterator_t& lhs, const iterator_t& rhs) noexcept {
+        return not (lhs == rhs);
+      }
+
+    private :
+
+      size_t E = 0;
+      small_size_t S = 0;
+      size_t s = 0;
+      size_t N = 0;
+      bool ge1 = 0;
+
+      size_t i = 0; // i < E
+      size_t j = 1; // j < s
+      size_t k = 0; // k < N
+
+      RandGen_t* g = nullptr;
+      gen_uint_t val = 0;
+
+    };
+
+  public :
+
+    iterator_t begin() const noexcept { return iterator_t(E,S,s,N,ge1); }
+    iterator_t end() const noexcept { return iterator_t(); }
 
   };
 
--- Satisfiability/Transformers/Generators/Random/TestSequences.cpp
+++ Satisfiability/Transformers/Generators/Random/TestSequences.cpp
@@ -14,7 +14,7 @@ License, or any later version. */
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.1",
+        "0.1.0",
         "8.11.2020",
         __FILE__,
         "Oliver Kullmann",
@@ -29,4 +29,64 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
 
+  {ExpSeq S(3,2,2,true);
+   gen_uint_t s = 0, c = 0;
+   for (auto i = S.begin(); i != S.end(); ++i, ++c) s += (*i).val;
+   assert(s == 17775932923502691692ULL);
+   assert(c == 3 * 99 * 2);
+   s = 0; c = 0;
+   for (auto i = S.begin(); i != S.end(); ++i, ++c) s += (*i).val;
+   assert(s == 17775932923502691692ULL);
+   assert(c == 3 * 99 * 2);
+   auto i_orig = S.begin();
+   {auto i0 = i_orig; auto i1(i0);
+    assert(i0 != S.end());
+    assert(i1 != S.end());
+    s = 0; c = 0;
+    for (auto i = i0; i != S.end(); ++i, ++c) s += (*i).val;
+    assert(s == 17775932923502691692ULL);
+    assert(c == 3 * 99 * 2);
+    s = 0; c = 0;
+    for (auto i = i1; i != S.end(); ++i, ++c) s += (*i).val;
+    assert(s == 17775932923502691692ULL);
+    assert(c == 3 * 99 * 2);
+    i0 = i1;
+    s = 0; c = 0;
+    for (; i0 != S.end(); ++i0, ++c) s += (*i0).val;
+    assert(s == 17775932923502691692ULL);
+    assert(c == 3 * 99 * 2);
+    assert(i0 == S.end());
+    s = 0; c = 0;
+    for (; i1 != S.end(); ++i1, ++c) s += (*i1).val;
+    assert(s == 17775932923502691692ULL);
+    assert(c == 3 * 99 * 2);
+    assert(i1 == S.end());
+   }
+   s = 0; c = 0;
+   for (; i_orig != S.end(); ++i_orig, ++c) s += (*i_orig).val;
+   assert(s == 17775932923502691692ULL);
+   assert(c == 3 * 99 * 2);
+   assert(i_orig == S.end());
+  }
+  {
+   for (gen_uint_t E = 1; E <= 4; ++E) {
+     for (unsigned S = 1; S <= 3; ++S) {
+       gen_uint_t s = std::pow(gen_uint_t(10), S);
+       for (gen_uint_t N = 1; N <= 100; N+=10) {
+         for (unsigned eps = 0; eps <= 1; ++eps) {
+           const ExpSeq es(E,S,N,eps);
+           assert(es.main_size() == E * (s-1));
+           assert(es.size() == E * (s-1) * N);
+           gen_uint_t count = 0;
+           for (const auto& x : es) {
+             ++count;
+             assert(x.s == s);
+             assert(x.ge1 == eps);
+           }
+           assert(count == es.size());
+         }
+       }
+     }
+   }
+  }
 }
