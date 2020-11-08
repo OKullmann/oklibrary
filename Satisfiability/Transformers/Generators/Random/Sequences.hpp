1e386c099d583facafc573a57f56e5294a48a165
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-11-08 08:20:15 +0000

    Started new module for random sequences.

---
+++ Satisfiability/Transformers/Generators/Random/Sequences.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 8.11.2020 (Swansea)
+/* Copyright 2020 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/* Random sequences
+
+
+
+*/
+
+#ifndef SEQUENCES_dZakTZB00i
+#define SEQUENCES_dZakTZB00i
+
+#include "Numbers.hpp"
+
+namespace RandGen {
+
+  struct ExpSeq {
+
+  };
+
+}
+
+#endif
---
+++ Satisfiability/Transformers/Generators/Random/TestSequences.cpp
@@ -0,0 +1,32 @@
+// Oliver Kullmann, 8.11.2020 (Swansea)
+/* Copyright 2020 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <iostream>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "Sequences.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "8.11.2020",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Random/TestSequences.cpp",
+        "GPL v3"};
+
+  using namespace RandGen;
+
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+
+}
