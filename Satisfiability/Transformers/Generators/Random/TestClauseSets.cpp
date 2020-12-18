b75e4f91ff75669865a194d540db4288d3b26d58
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-12-18 21:41:48 +0000

    Editorial update.

--- Satisfiability/Transformers/Generators/Random/TestClauseSets.cpp
+++ Satisfiability/Transformers/Generators/Random/TestClauseSets.cpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 18.4.2019 (Swansea)
-/* Copyright 2019 Oliver Kullmann
+/* Copyright 2019, 2020 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
