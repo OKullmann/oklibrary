9c03e3595f605db71dfc21f14d148bd41395d3ee
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2020-12-18 21:42:44 +0000

    Started with module for creating random latin squares.

---
+++ Satisfiability/Transformers/Generators/Random/LatinSquares.hpp
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 18.12.2020 (Swansea)
+/* Copyright 2020 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#ifndef LATINSQUARES_WPlhfGMfBs
+#define LATINSQUARES_WPlhfGMfBs
+
+namespace RandGen {
+
+}
+
+#endif
