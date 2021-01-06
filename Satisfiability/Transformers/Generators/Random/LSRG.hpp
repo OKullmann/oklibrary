9f0866deef21af1ef62dca7a19f9e8ceae2b6b5e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-01-06 21:47:25 +0000

    Testing generated ls's for uniform distribution.

--- Satisfiability/Transformers/Generators/Random/LSRG.hpp
+++ Satisfiability/Transformers/Generators/Random/LSRG.hpp
@@ -15,10 +15,15 @@ License, or any later version. */
 #define LSRG_r3HkyAjxRY
 
 #include <tuple>
+#include <map>
+#include <ostream>
 
 #include <ProgramOptions/Environment.hpp>
+#include <Numerics/FloatingPoint.hpp>
 
 #include "SeedOrganisation.hpp"
+#include "Tests.hpp"
+#include "Statistics.hpp"
 
 //Guaranteed to be included:
 #include "LatinSquares.hpp"
@@ -99,7 +104,7 @@ namespace LSRG {
 
   LS::ls_t random_ls(const LS::ls_dim_t N, const LS::Selection& sel,
                      const GenO go, const LS::StRLS so,
-                     const RG::vec_eseed_t seeds) {
+                     const RG::vec_eseed_t& seeds) {
     RG::RandGen_t g(seeds);
     return random_ls(N, sel, go, so, g);
   }
@@ -117,6 +122,67 @@ namespace LSRG {
             const GenO go = GenO{}, const LS::StRLS so = LS::StRLS{}) {
     return random_ls(N, seeds, LS::Selection(N), go, so);
   }
+
+
+  /* Analysing generators */
+
+  struct Count_ls {
+    typedef RG::gen_uint_t count_t;
+    typedef FloatingPoint::float80 stats_t;
+    typedef std::map<LS::ls_t, count_t> map_t;
+    typedef map_t::const_iterator it_t;
+    typedef RG::BasicStats<stats_t, stats_t> Statistics;
+
+    map_t M;
+    count_t count_diff = 0;
+    const LS::ls_dim_t N;
+    count_t count_all = 0;
+
+    const bool reduced;
+    const count_t total;
+    const stats_t p;
+
+    Count_ls(const LS::ls_dim_t N, const bool reduced) noexcept :
+      N(N), reduced(reduced),
+      total(reduced ? LS::c_all_reduced_ls[N] : LS::c_all_ls[N]),
+      p(stats_t(1) / total) {
+        assert(reduced ? N <= LS::max64_N_all_reduced_ls :
+               N <= LS::max64_N_all_ls);
+    }
+
+    void add(const LS::ls_t& L) {
+      auto& elem = M[L]; ++elem;
+      if (elem == 1) ++count_diff;
+      ++count_all;
+    }
+
+    stats_t p_value(const it_t it) const noexcept {
+      return RG::monobit(it->second, count_all, p);
+    }
+    std::pair<Statistics,Statistics> evaluation() const noexcept {
+      Statistics res1, res2;
+      for (it_t i = M.begin(); i != M.end(); ++i) {
+        res1 += stats_t(i->second) / count_all;
+        res2 += p_value(i);
+      }
+      assert(res1.N == count_diff and res2.N == count_diff);
+      return {res1, res2};
+    }
+
+    friend std::ostream& operator <<(std::ostream& out, const Count_ls& c) {
+      const auto eval = c.evaluation();
+      out <<
+        "N=" << c.N << ", reduced=" << c.reduced << ", L(N)=" << c.total << "\n"
+        "T=" << c.count_all << ", found=" << c.count_diff << "\n"
+        "exact prob=L(N)/T = " << c.p << "\n"
+        "frequency statistics:\n  ";
+      eval.first.simple_output(out);
+      out << "\np-value statistics:\n  ";
+      eval.second.simple_output(out);
+      return out << "\n";
+    }
+
+  };
 
 }
 
