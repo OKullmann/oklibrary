dd7c6ea5cbda98ceb2ce4b957c468ec4893800ae
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-01-08 10:25:07 +0000

    More complete statistics.

--- Satisfiability/Transformers/Generators/Random/Statistics.hpp
+++ Satisfiability/Transformers/Generators/Random/Statistics.hpp
@@ -19,10 +19,18 @@ License, or any later version. */
 #define STATISTICS_n7Ju3x1bcK
 
 #include <ostream>
+#include <vector>
+#include <algorithm>
+#include <ostream>
+#include <iomanip>
 
+#include <cassert>
 #include <cmath>
 
+#include <Numerics/FloatingPoint.hpp>
+
 #include "Numbers.hpp"
+#include "Tests.hpp"
 
 namespace RandGen {
 
@@ -81,6 +89,102 @@ namespace RandGen {
       return lhs.N == rhs.N and lhs.sum == rhs.sum and
         lhs.sum_sq == rhs.sum_sq and lhs.min_ == rhs.min_ and lhs.max_ == rhs.max_;
     }
+  };
+
+
+  template <typename OUT, class V>
+  OUT median(const V& v) noexcept {
+    assert(std::is_sorted(v.begin(), v.end()));
+    const auto N = v.size();
+    if (N == 0) return 0;
+    else if (N % 2 == 1) return v[(N-1)/2];
+    else return (OUT(v[N/2-1]) + OUT(v[N/2]))/ OUT(2);
+  }
+
+  template <typename IN, typename OUT>
+  struct StatsStore {
+    typedef IN input_t;
+    typedef OUT output_t;
+
+    typedef gen_uint_t count_t;
+    typedef std::vector<input_t> vec_t;
+    vec_t data;
+    count_t N = 0;
+    input_t sum = 0;
+    input_t min_ = std::numeric_limits<input_t>::max();
+    input_t max_ = std::numeric_limits<input_t>::lowest();
+    bool sorted = false;
+
+    StatsStore& operator +=(const input_t x) noexcept {
+      data.push_back(x); sorted=false;
+      ++N;
+      sum += x;
+      if (x < min_) min_ = x;
+      if (x > max_) max_ = x;
+      return *this;
+    }
+
+    input_t min() const noexcept { return min_; }
+    input_t max() const noexcept { return max_; }
+    output_t amean() const noexcept {
+      if (N == 0) return 0;
+      return output_t(sum) / output_t(N);
+    }
+
+    output_t sum_diffsquares() const noexcept {
+      if (N <= 1) return 0;
+      const output_t m = amean();
+      output_t sum = 0;
+      for (const input_t x : data) {
+        const output_t diff = output_t(x) - m;
+        sum += diff*diff;
+      }
+      return sum;
+    }
+    output_t var_population() const noexcept {
+      if (N <= 1) return 0;
+      return sum_diffsquares() / output_t(N);
+    }
+    output_t var_unbiased() const noexcept {
+      if (N <= 1) return 0;
+      return sum_diffsquares() / output_t(N-1);
+    }
+    output_t sd_population() const noexcept {
+      return std::sqrt(var_population());
+    }
+    output_t sd_corrected() const noexcept {
+      return std::sqrt(var_unbiased());
+    }
+
+    output_t median() noexcept {
+      if (not sorted) {
+        std::sort(data.begin(), data.end());
+        sorted = true;
+      }
+      return RandGen::median<output_t, vec_t>(data);
+    }
+
+    FloatingPoint::float80 ks() noexcept {
+      if (N == 0) return 0;
+      if (not sorted) {
+        std::sort(data.begin(), data.end());
+        sorted = true;
+      }
+      using FloatingPoint::float80;
+      return RandGen::ks_P(N, ks_D_value<std::vector<float80>>(data));
+    }
+
+    int width = 30;
+    void simple_output(std::ostream& out, const bool with_ks = false) {
+      const auto sd = sd_corrected();
+      const auto med = median();
+      const auto w = std::setw(width);
+      out << w << min() << w << amean() << w << max() << "\n"
+          << w << "median" << w << med << "\n"
+          << w << "sd" << w << sd << "\n";
+      if (with_ks) out << "ks:" << w << ks() << "\n";
+    }
+
   };
 
 }
