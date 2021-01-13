78cb8abfc95a069ee927ba230be8f23fe4400b21
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2021-01-13 05:58:44 -0800

    Start testing LSRG.

---
+++ Satisfiability/Transformers/Generators/Random/TestLSRG.cpp
@@ -0,0 +1,45 @@
+// Oliver Kullmann and Oleg Zaikin, 13.1.2021 (Swansea)
+/* Copyright 2021 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <cassert>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "LSRG.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "13.1.2021",
+        __FILE__,
+        "Oliver Kullmann and Oleg Zaikin",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Random/TestLSRG.cpp",
+        "GPL v3"};
+
+    using namespace LSRG;
+    using namespace LatinSquares;
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+
+  {const auto [L, seeds, basic_size] =
+     random_ls(1, "0", Selection(1), GenO::jm, StRLS::none);
+   assert(is_square(L));
+   assert(valid(L));
+   assert(L == ls_t{{0}});
+  }
+
+  {const auto [L, seeds, basic_size] =
+     random_ls(1, "1", Selection(1), GenO::jm, StRLS::none);
+   assert(is_square(L));
+   assert(valid(L));
+   assert(L == ls_t{{0}});
+  }
+}
