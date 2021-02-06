52a25e98f90ca368449a04f4041b67294021af2f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-02-06 02:21:24 +0000

    Testing ltau_64.

--- Programming/Numerics/ExperimentsTau.cpp
+++ Programming/Numerics/ExperimentsTau.cpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 8.11.2020 (Swansea)
-/* Copyright 2020 Oliver Kullmann
+/* Copyright 2020, 2021 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -18,8 +18,8 @@ License, or any later version. */
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.9.2",
-        "23.12.2020",
+        "0.10.0",
+        "6.2.2021",
         __FILE__,
         "Oliver Kullmann",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Numerics/ExperimentsTau.cpp",
@@ -44,19 +44,21 @@ int main(const int argc, const char* const argv[]) {
   using size_t = ExpSeq::size_t;
   using float64 = FloatingPoint::float64;
   {auto it = seq.begin();
-   mpfr_t tau; Tau_mpfr::dinit(tau);
+   mpfr_t ltau, m_fixed; Tau_mpfr::dinit(ltau); Tau_mpfr::dinit(m_fixed);
+   constexpr float64 fixed = 2.5;
    for (size_t i = 0; i < seq.main_size(); ++i) {
      BasicStats<float64, float64> stats_args;
      BasicStats<gen_uint_t, float64> stats_accur;
      for (size_t j = 0; j < seq.N; ++j, ++it) {
        const float64 x = seq.translate<float64>(*it);
        stats_args += x;
-       mpfr_set_ld(tau, x, Tau_mpfr::defrnd);
-       Tau_mpfr::wtau(tau);
-       const float64 prec_res = Tau_mpfr::to_float64(tau);
+       mpfr_set_d(ltau, x, Tau_mpfr::defrnd);
+       mpfr_set_d(m_fixed, fixed, Tau_mpfr::defrnd);
+       Tau_mpfr::ltau(ltau, m_fixed);
+       const float64 prec_res = Tau_mpfr::to_float64(ltau);
        switch (version) {
        default :
-         stats_accur += FloatingPoint::accuracy_64(prec_res, Tau::wtau_64(x));
+         stats_accur += FloatingPoint::accuracy_64(prec_res, Tau::ltau_64(x,fixed));
        }
      }
      using FloatingPoint::Wrap64;
