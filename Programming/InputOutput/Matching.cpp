e47f6732618f3a988f7cbcb03c0bfb05ebf3cd81
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-02-11 22:21:04 +0000

    Completed first version, with line-per-line matching.

--- Programming/InputOutput/Matching.cpp
+++ Programming/InputOutput/Matching.cpp
@@ -34,8 +34,13 @@ License, or any later version. */
      It seems that without that flag, the match must be unique -- perhaps
      this is actually what we need?
    - For the construction of the regular expression, std::regex::ECMAScript
-     together with std::regex::multiline seems appropriate (only the latter
-     needs to be given for the constructor).
+     together with std::regex_constants::multiline seems appropriate
+     (only the latter needs to be given for the constructor).
+     Currently with gcc "multiline" is not available, but since we are
+     splitting lines anyway (one regular expression for a line), that doesn't
+     matter. And it seems also when using one regular expression for the
+     whole file, then we won't use the beginning or end of the line, since
+     we (likely) match the whole line.
 
   \todo More/better output
    - The regular expressions themselves could be faulty, and the
@@ -45,8 +50,12 @@ License, or any later version. */
 
 */
 
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <regex>
+
+#include <cstdlib>
 
 #include <ProgramOptions/Environment.hpp>
 
@@ -69,12 +78,13 @@ namespace Matching {
     case MatO::lines : return out << "matching-lines";
     default : return out << "matching-file";}
   }
+
 }
 
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.2",
+        "0.1.0",
         "11.2.2021",
         __FILE__,
         "Oliver Kullmann",
@@ -82,6 +92,8 @@ namespace {
         "GPL v3"};
 
   using namespace Matching;
+
+  const std::string error = "ERROR[" + proginfo.prg + "]: ";
 
   bool show_usage(const int argc, const char* const argv[]) {
     if (not Environment::help_header(std::cout, argc, argv, proginfo))
@@ -95,9 +107,83 @@ namespace {
     return true;
   }
 
+  enum class Error {
+    pnumber = 1,
+    file_open = 2,
+    file_read = 3,
+    eof = 4,
+    empty_pattern_file = 5,
+    regular_expression = 6,
+    number_lines = 7,
+    mismatch = 8,
+  };
+
+  Environment::tokens_t split(const std::string& name) {
+    std::ifstream in(name);
+    if (not in) {
+      std::cerr << error << "File \"" << name << "\" not readable.\n";
+      std::exit(int(Error::file_open));
+    }
+    char c;
+    auto res = Environment::split(in, '\n', c);
+    if (in.bad()) {
+      std::cerr << error << "Reading error with file \"" << name << "\".\n";
+      std::exit(int(Error::file_read));
+    }
+    if (c != '\n') {
+      std::cerr << error << "File \"" << name << "\" does not finish with"
+        "\n end-of-line symbol, but with character-code " << int(c) << ".\n";
+      std::exit(int(Error::eof));
+    }
+    return res;
+  }
+
 }
 
 int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
   if (show_usage(argc, argv)) return 0;
+
+  if (argc != 4) {
+    std::cerr << error << "Exactly three input parameters are required:\n"
+      " - the file-name for the pattern,\n"
+      " - the file-name of the file to check,\n"
+      " - and the matching-opting.\n";
+    return int(Error::pnumber);
+  }
+
+  const std::string Pfile = argv[1];
+  const auto P_lines = split(Pfile);
+  typedef std::size_t size_t;
+  const size_t N = P_lines.size();
+  std::vector<std::regex> regv; regv.reserve(N);
+  for (size_t i = 0; i < N; ++i) {
+    try {
+      regv.emplace_back(P_lines[i]);
+    }
+    catch (const std::regex_error& e) {
+      std::cerr << error << "Regular expression error in file \"" <<
+        Pfile << "\", line " << i+1 << ":\n"
+        "expression: \"" << P_lines[i] << "\"\n"
+        "what: " << e.what() << "\n"
+        "code: " << e.code() << "\n";
+      return int(Error::regular_expression);
+    }
+  }
+
+  const std::string Cfile = argv[2];
+  const auto C_lines = split(Cfile);
+  if (C_lines.size() != N) {
+    std::cerr << error << "File \"" << Cfile << "\" has " << C_lines.size() <<
+      " lines, but the pattern-file has " << N << " lines.\n";
+    return int(Error::number_lines);
+  }
+  for (size_t i = 0; i < N; ++i) {
+    if (not std::regex_match(C_lines[i], regv[i])) {
+      std::cerr << error << "Mismatch in line " << i+1 << ":\n"
+        "Pattern: \"" << P_lines[i] << "\"\n"
+        "Given  : \"" << C_lines[i] << "\"\n";
+      return int(Error::mismatch);
+    }
+  }
 }
