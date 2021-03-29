6371dc08ff54f76ffd04aa7da5166e9b1deb3a1d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-03-29 20:35:08 +0100

    Editorial improvements.

--- Satisfiability/Solvers/Gecode/Examples/send-more-money.cpp
+++ Satisfiability/Solvers/Gecode/Examples/send-more-money.cpp
@@ -25,66 +25,63 @@
  *
  */
 
-/* OK:
-
-> sudo ldconfig
-> g++ send-more-money.cpp -lgecodesearch -lgecodeint -lgecodekernel -lgecodesupport -o send-more-money
-{9, 5, 6, 7, 1, 0, 8, 2}
-
-*/
-
+#include <iostream>
 
 #include <gecode/int.hh>
 #include <gecode/search.hh>
 
-using namespace Gecode;
+namespace GC = Gecode;
 
-class SendMoreMoney : public Space {
+class SendMoreMoney : public GC::Space {
 protected:
-  IntVarArray l;
+  GC::IntVarArray l;
+
 public:
-  SendMoreMoney(void) : l(*this, 8, 0, 9) {
-    IntVar s(l[0]), e(l[1]), n(l[2]), d(l[3]),
-           m(l[4]), o(l[5]), r(l[6]), y(l[7]);
-    // no leading zeros
-    rel(*this, s, IRT_NQ, 0);
-    rel(*this, m, IRT_NQ, 0);
-    // all letters distinct
-    distinct(*this, l);
-    // linear equation
-    IntArgs c(4+4+5); IntVarArgs x(4+4+5);
+  SendMoreMoney() : l(*this, 8, 0, 9) {
+    GC::IntVar s(l[0]), e(l[1]), n(l[2]), d(l[3]),
+               m(l[4]), o(l[5]), r(l[6]), y(l[7]);
+
+    // no leading zeros:
+    GC::rel(*this, s, GC::IRT_NQ, 0);
+    GC::rel(*this, m, GC::IRT_NQ, 0);
+
+    // all letters distinct:
+    GC::distinct(*this, l);
+
+    // linear equation:
+    GC::IntArgs c(4+4+5); GC::IntVarArgs x(4+4+5);
     c[0]=1000; c[1]=100; c[2]=10; c[3]=1;
     x[0]=s;    x[1]=e;   x[2]=n;  x[3]=d;
     c[4]=1000; c[5]=100; c[6]=10; c[7]=1;
     x[4]=m;    x[5]=o;   x[6]=r;  x[7]=e;
     c[8]=-10000; c[9]=-1000; c[10]=-100; c[11]=-10; c[12]=-1;
     x[8]=m;      x[9]=o;     x[10]=n;    x[11]=e;   x[12]=y;
-    linear(*this, c, x, IRT_EQ, 0);
+    GC::linear(*this, c, x, GC::IRT_EQ, 0);
+
     // post branching
-    branch(*this, l, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
+    GC::branch(*this, l, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
   }
+
   // search support
-  SendMoreMoney(SendMoreMoney& s) : Space(s) {
+  SendMoreMoney(SendMoreMoney& s) : GC::Space(s) {
     l.update(*this, s.l);
   }
-  virtual Space* copy(void) {
+  virtual GC::Space* copy() {
     return new SendMoreMoney(*this);
   }
+
   // print solution
-  void print(void) const {
+  void print() const {
     std::cout << l << std::endl;
   }
 };
 
-// main function
-int main(int argc, char* argv[]) {
-  // create model and search engine
-  SendMoreMoney* m = new SendMoreMoney;
-  DFS<SendMoreMoney> e(m);
+
+int main() {
+  SendMoreMoney* const m = new SendMoreMoney;
+  GC::DFS<SendMoreMoney> e(m);
   delete m;
-  // search and print all solutions
-  while (SendMoreMoney* s = e.next()) {
+  while (SendMoreMoney* const s = e.next()) {
     s->print(); delete s;
   }
-  return 0;
 }
