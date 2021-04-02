b2f63cd31d44af5af8b8d9f403a58df6d1cf593b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-04-02 14:40:49 +0100

    Nearly finished (up to the permutations).
    Also needed is the entry of the other main types (N <= 7).

--- Satisfiability/Transformers/Generators/EulerSquareGen.java
+++ Satisfiability/Transformers/Generators/EulerSquareGen.java
@@ -1,5 +1,5 @@
 class EulerSquareGen {
-  public final int Nmin = 3, Nmax = 7;
+  public static final int Nmin = 3, Nmax = 4;
   public final int N;
 
   // http://users.cecs.anu.edu.au/~bdm/data/latin.html
@@ -9,7 +9,10 @@ class EulerSquareGen {
     {},
     {},
     {},
-    {{{{0,2,1}, {2,1,0}, {1,0,2}},  {{0,2,1}, {1,0,2}, {2,1,0}}}}
+    {{{{0,2,1}, {2,1,0}, {1,0,2}},
+      {{0,2,1}, {1,0,2}, {2,1,0}}}},
+    {{{{0,3,1,2}, {2,1,3,0}, {3,0,2,1}, {1,2,0,3}},
+      {{0,3,2,1}, {3,0,1,2}, {2,1,0,3}, {1,2,3,0}}}},
    };
 
   public EulerSquareGen(final int N_) {
@@ -56,18 +59,22 @@ class EulerSquareGen {
 
   // the two ls's are res[0][i][j] and res[1][i][j]:
   int[][][] generate() {
-    final int[][][] res = new int[N][N][2];
+    int[][][] res = new int[N][N][2];
     // chosen randomly from fixed "prototypes":
     final int tmax = main_types[N].length;
     final int t = (int) (Math.random() * tmax);
     final int[][][] E = main_types[N][t];
     assert(is_euler(E, N));
+    // create a random permutation of E in the four possible "dimensions" of
+    // permuations XXX
+    // needs to create a random permutation
 
+    res = E;
     return res;
   }
 
   public static void main(String[] args) {
-    assert(main_types.length == 4);
+    assert(main_types.length == 5);
     assert(main_types[0].length == 0);
     assert(main_types[1].length == 0);
     assert(main_types[2].length == 0);
@@ -79,6 +86,13 @@ class EulerSquareGen {
     assert(main_types[3][0][1][0].length == 3);
 
     assert(is_euler(main_types[3][0], 3));
+    assert(is_euler(main_types[4][0], 4));
+
+    for (int N = Nmin; N <= Nmax; ++N) {
+      final EulerSquareGen gen = new EulerSquareGen(N);
+      for (long i = 0; i < 100; ++i)
+        assert is_euler(gen.generate(), N);
+    }
   }
   
 }
