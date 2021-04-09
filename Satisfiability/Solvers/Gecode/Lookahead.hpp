02df77ff26ff36c261218dded3cf7d48476f6e88
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-04-09 06:12:07 +0100

    Editorial correction.

--- Satisfiability/Solvers/Gecode/Lookahead.hpp
+++ Satisfiability/Solvers/Gecode/Lookahead.hpp
@@ -5,7 +5,10 @@ it and/or modify it under the terms of the GNU General Public License as publish
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
 
-/* Lookahead for the Gecode library
+/*
+
+    Lookahead for the Gecode library
+
 */
 
 #ifndef LOOKAHEAD_lNFKYYpHQ8
