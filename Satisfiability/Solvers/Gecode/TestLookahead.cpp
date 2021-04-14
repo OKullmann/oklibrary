7b5c4292ecaa2d34397f590e51bb65bb56396da4
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2021-04-14 17:08:53 +0800

    Test valid function.

--- Satisfiability/Solvers/Gecode/TestLookahead.cpp
+++ Satisfiability/Solvers/Gecode/TestLookahead.cpp
@@ -20,7 +20,7 @@ namespace LA = Lookahead;
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.1.2",
+        "0.1.3",
         "14.4.2021",
         __FILE__,
         "Oleg Zaikin and Oliver Kullmann",
@@ -35,6 +35,8 @@ int main(const int argc, const char* const argv[]) {
 
   {Trivial::Sum m(1, 0, 0);
    assert(m.size() == 1);
+   assert(m.valid(0));
+   assert(not m.valid(1));
    assert(m.mu0() == 0);
    assert(m.mu1() == 0);
    Trivial::Sum m2(m);
@@ -44,6 +46,9 @@ int main(const int argc, const char* const argv[]) {
 
   {Trivial::Sum m(2, 0, 2);
    assert(m.size() == 2);
+   assert(m.valid(0));
+   assert(m.valid(1));
+   assert(not m.valid(2));
    assert(m.mu0() == 4);
    assert(m.mu1() == 2*FloatingPoint::log2(3));
    Trivial::Sum m2(1, 0, 0);
@@ -63,6 +68,11 @@ int main(const int argc, const char* const argv[]) {
   }
 
   {Trivial::Sum* const m = new Trivial::Sum(3, 0, 2);
+   assert(m->size() == 3);
+   assert(m->valid(0));
+   assert(m->valid(1));
+   assert(m->valid(2));
+   assert(not m->valid(3));
    m->status();
    assert(Trivial::propagate(m, 0, 0) == 4);
    assert(Trivial::propagate(m, 0, 1) == 2);
