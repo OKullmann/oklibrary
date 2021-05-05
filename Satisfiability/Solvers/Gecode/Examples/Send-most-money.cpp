159c42ccc59d66ee5c56df6dccb580fc305cd76c
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2021-05-05 15:46:23 +0800

    Editorial.

--- Satisfiability/Solvers/Gecode/Examples/Send-most-money.cpp
+++ Satisfiability/Solvers/Gecode/Examples/Send-most-money.cpp
@@ -41,8 +41,8 @@
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "1.0.6",
-        "22.4.2021",
+        "1.0.7",
+        "5.5.2021",
         __FILE__,
         "Christian Schulte, Oliver Kullmann, and Oleg Zaikin",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Solvers/Gecode/Examples/Send-most-money.cpp",
@@ -54,16 +54,23 @@ namespace {
   count_t inner_nodes = 0, leaves = 0, solutions = 0;
 
   class SendMostMoney : public GC::Space {
-  protected:
-    GC::IntVarArray l;
+    GC::IntVarArray L;
 
   public:
-    SendMostMoney(void) : l(*this, 8, 0, 9) {
-      GC::IntVar s(l[0]), e(l[1]), n(l[2]), d(l[3]), m(l[4]),
-            o(l[5]), t(l[6]), y(l[7]);
+    SendMostMoney(void) : L(*this, 8, 0, 9) {
+
+      GC::IntVar
+        s(L[0]), e(L[1]), n(L[2]), d(L[3]),
+        m(L[4]), o(L[5]), t(L[6]), y(L[7]);
+
+      // no leading zeros:
       GC::rel(*this, s, GC::IRT_NQ, 0);
       GC::rel(*this, m, GC::IRT_NQ, 0);
-      GC::distinct(*this, l);
+
+      // all letters distinct:
+      GC::distinct(*this, L);
+
+      // linear equation:
       GC::IntArgs c(4+4+5); GC::IntVarArgs x(4+4+5);
       c[0]=1000; c[1]=100; c[2]=10; c[3]=1;
       x[0]=s;    x[1]=e;   x[2]=n;  x[3]=d;
@@ -72,18 +79,23 @@ namespace {
       c[8]=-10000; c[9]=-1000; c[10]=-100; c[11]=-10; c[12]=-1;
       x[8]=m;      x[9]=o;     x[10]=n;    x[11]=e;   x[12]=y;
       GC::linear(*this, c, x, GC::IRT_EQ, 0);
-      GC::branch(*this, l, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
+
+      // post branching:
+      GC::branch(*this, L, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
     }
+
     SendMostMoney(SendMostMoney& s) : GC::Space(s) {
-      l.update(*this, s.l);
+      L.update(*this, s.L);
     }
+
     virtual GC::Space* copy(void) {
        ++inner_nodes;
       return new SendMostMoney(*this);
     }
+
     void print(void) const {
       ++solutions;
-      std::cout << l << "\n";
+      std::cout << L << "\n";
     }
   };
 
