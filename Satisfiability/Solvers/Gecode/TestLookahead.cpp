48e600087db588cc768f43d2a2c6bd191f8cd1d9
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2021-05-07 23:49:54 +0800

    Editorial.

--- Satisfiability/Solvers/Gecode/TestLookahead.cpp
+++ Satisfiability/Solvers/Gecode/TestLookahead.cpp
@@ -30,7 +30,7 @@ namespace {
   typedef std::shared_ptr<Trivial::Sum> trivial_sum_ptr;
 
   const Environment::ProgramInfo proginfo{
-        "0.2.0",
+        "0.2.1",
         "7.5.2021",
         __FILE__,
         "Oleg Zaikin and Oliver Kullmann",
@@ -43,62 +43,27 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
 
-  {Trivial::Sum m(1, 0, 0);
-   assert(m.size() == 1);
-   assert(m.valid(0));
-   assert(not m.valid(1));
-   assert(m.mu0() == 0);
-   assert(m.mu1() == 0);
-   Trivial::Sum m2(1, 0, 0);
-   assert(m == m2);
-   Trivial::Sum m3(2, 0, 2);
-   assert(m != m3);
-  }
-
   {const trivial_sum_ptr m(new Trivial::Sum(1, 0, 0));
    assert(m->valid());
-   m->branching_min_var_size();
-   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
-   assert(stat.solutions == 1);
-  }
-
-  {Trivial::Sum m(2, 0, 2);
-   assert(m.size() == 2);
-   assert(m.valid(0));
-   assert(m.valid(1));
-   assert(not m.valid(2));
-   assert(m.mu0() == 4);
-   assert(m.mu1() == 2*FloatingPoint::log2(3));
-  }
-
-  {const trivial_sum_ptr m(new Trivial::Sum(2, 0, 2));
-   assert(m->valid());
-   m->branching_min_var_size();
-   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
-   assert(stat.solutions == 3);
-  }
-
-  {const trivial_sum_ptr m(new Trivial::Sum(1, 0, 0));
-   assert(m->valid());
-   m->branching_min_var_size();
-   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
-   assert(stat.solutions == 1);
    assert(m->size() == 1);
    assert(m->valid(0));
    assert(not m->valid(1));
+   m->branching_min_var_size();
+   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
+   assert(stat.solutions == 1);
    m->status();
    assert(m->la_measure(0, 0) == 0);
   }
 
   {const trivial_sum_ptr m(new Trivial::Sum(2, 0, 1));
    assert(m->valid());
-   m->branching_min_var_size();
-   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
-   assert(stat.solutions == 2);
    assert(m->size() == 2);
    assert(m->valid(0));
    assert(m->valid(1));
    assert(not m->valid(2));
+   m->branching_min_var_size();
+   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
+   assert(stat.solutions == 2);
    m->status();
    assert(m->la_measure(0, 0) == 0);
    assert(m->la_measure(0, 1) == 0);
@@ -106,16 +71,29 @@ int main(const int argc, const char* const argv[]) {
    assert(m->la_measure(1, 1) == 0);
   }
 
-  {const trivial_sum_ptr m(new Trivial::Sum(3, 0, 2));
+  {const trivial_sum_ptr m(new Trivial::Sum(2, 0, 2));
    assert(m->valid());
+   assert(m->size() == 2);
+   assert(m->valid(0));
+   assert(m->valid(1));
+   assert(not m->valid(2));
+   assert(m->mu0() == 4);
+   assert(m->mu1() == 2*FloatingPoint::log2(3));
    m->branching_min_var_size();
    LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
-   assert(stat.solutions == 6);
+   assert(stat.solutions == 3);
+  }
+
+  {const trivial_sum_ptr m(new Trivial::Sum(3, 0, 2));
+   assert(m->valid());
    assert(m->size() == 3);
    assert(m->valid(0));
    assert(m->valid(1));
    assert(m->valid(2));
    assert(not m->valid(3));
+   m->branching_min_var_size();
+   LA::SearchStat stat = LA::find_all_solutions<Trivial::Sum>(m);
+   assert(stat.solutions == 6);
    m->status();
    assert(m->la_measure(0, 0) == 4);
    assert(m->la_measure(0, 1) == 2);
