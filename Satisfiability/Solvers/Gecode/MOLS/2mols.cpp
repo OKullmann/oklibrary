c42ef144a5da1683ca708bbad1a852dcac0c8859
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2021-05-21 00:41:36 +0800

    Namespace abbreviations.

--- Satisfiability/Solvers/Gecode/MOLS/2mols.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/2mols.cpp
@@ -22,7 +22,7 @@
 1. DONE Write application-tests (OZ)
 
 2. Update coding standard (OZ)
-    - Use namespace-abbreviations.
+    - DONE Use namespace-abbreviations.
 
 3. Likely best for now to remove all verbosity. (OZ)
     - Verbosity in the main output has been removed.
@@ -48,7 +48,6 @@
 
 #include <iostream>
 #include <vector>
-#include <chrono>
 
 #include <math.h>
 
@@ -58,20 +57,19 @@
 
 #include <ProgramOptions/Environment.hpp>
 
-using namespace Gecode; // XXX to be removed
-using namespace std; // XXX to be removed
-
 #define BRANCH_XY 1
 
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.1.0",
+        "0.1.1",
         "20.5.2021",
         __FILE__,
         "Noah Rubin, Curtis Bright, Oliver Kullmann, and Oleg Zaikin",
         "https://github.com/OKullmann/OKlib-MOLS/blob/master/Satisfiability/Solvers/Gecode/MOLS/2mols.cpp",
         "GPL v3"};
+
+  namespace GC = Gecode;
 
 }
 
@@ -92,7 +90,7 @@ public:
     Type = 'N';
   }
   StdIntTracer_VariableIDs(char S) : Type(S) {}
-  void prune(const Space& home, const IntTraceRecorder& trace, const ViewTraceInfo& vti, int i, IntTraceDelta& delta) {
+  void prune(const GC::Space& home, const IntTraceRecorder& trace, const ViewTraceInfo& vti, int i, IntTraceDelta& delta) {
     os << "trace<Int>::prune(id:" << trace.id();
     if (trace.group().in())
       os << ",g:" << trace.group().id();
@@ -130,12 +128,12 @@ StdIntTracer_VariableIDs StdIntTracer_VariableIDs::Sz('Z');
 #endif
 
 // We define the model as a sub-space
-class TWO_MOLS : public Space {
+class TWO_MOLS : public GC::Space {
 
   // Store dimension and variables
 protected:
   int n;
-  IntVarArray x, y, z;
+  GC::IntVarArray x, y, z;
 
   // Provide constructor
 public:
@@ -144,18 +142,18 @@ public:
 
     if(sym_breaking) {
       // Declare domains for lexocographic ordering
-      vector<int> domain_lex(n);
+      std::vector<int> domain_lex(n);
       for (int i = 0; i < n; i++) domain_lex[i] = i;
 
       // Declare domains for Dominance Detection
-      vector<vector<int>> domain_constrained(n);
+      std::vector<std::vector<int>> domain_constrained(n);
 
       // Fix entry (0,1) to 2 in L2
       domain_constrained[1].push_back(2);
 
       // Fix remaining constrained domains for column 1 in L2
       for (int i = 2; i < n; i++) {
-        vector<int> domainTemp;
+        std::vector<int> domainTemp;
         if (i == (n - 2)) {
           domainTemp.push_back((n - 1));
           domain_constrained[i] = domainTemp;
@@ -180,7 +178,7 @@ public:
         dom(*this, y[i], domain_lex[i]);
         dom(*this, x[i * n], domain_lex[i]);
         if (i > 0) {
-          dom(*this, y[i * n], IntSet(IntArgs(domain_constrained[i])));
+          dom(*this, y[i * n], GC::IntSet(GC::IntArgs(domain_constrained[i])));
         }
       }
     }
@@ -199,7 +197,7 @@ public:
 
     // Latin property in rows of X
     for (int i = 0; i < n; i++) {
-      vector<IntVar> rows_x;
+      std::vector<GC::IntVar> rows_x;
       for (int j = 0; j < n; j++) {
         rows_x.push_back(x[i * n + j]);
       }
@@ -208,59 +206,59 @@ public:
 
     // Latin property in cols of X
     for (int i = 0; i < n; i++) {
-      vector<IntVar> cols_x;
+      std::vector<GC::IntVar> cols_x;
       for (int j = 0; j < n; j++) {
         cols_x.push_back(x[j * n + i]);
       }
-      distinct(*this, cols_x);
+      GC::distinct(*this, cols_x);
     }
 
     // Latin property in rows of Y
     for (int i = 0; i < n; i++) {
-      vector<IntVar> rows_y;
+      std::vector<GC::IntVar> rows_y;
       for (int j = 0; j < n; j++) {
         rows_y.push_back(y[i * n + j]);
       }
-      distinct(*this, rows_y);
+      GC::distinct(*this, rows_y);
     }
 
     // Latin property in cols of Y
     for (int i = 0; i < n; i++) {
-      vector<IntVar> cols_y;
+      std::vector<GC::IntVar> cols_y;
       for (int j = 0; j < n; j++) {
         cols_y.push_back(y[j * n + i]);
       }
-      distinct(*this, cols_y);
+      GC::distinct(*this, cols_y);
     }
 
     // Row uniqueness of Z
     for (int i = 0; i < n; i++) {
-      vector<IntVar> rows_z;
+      std::vector<GC::IntVar> rows_z;
       for (int j = 0; j < n; j++) {
         rows_z.push_back(z[i * n + j]);
       }
-      distinct(*this, rows_z);
+      GC::distinct(*this, rows_z);
     }
 
     // Column uniqueness of Z
     for (int i = 0; i < n; i++) {
-      vector<IntVar> cols_z;
+      std::vector<GC::IntVar> cols_z;
       for (int j = 0; j < n; j++) {
         cols_z.push_back(z[j * n + i]);
       }
-      distinct(*this, cols_z);
+      GC::distinct(*this, cols_z);
     }
 
     // Enforce element constraints on Z, X, Y
     for (int i = 0; i < n; i++)
       {
-        vector<IntVar> Zvec_i;
+        std::vector<GC::IntVar> Zvec_i;
         for (int j = 0; j < n; j++) {
           Zvec_i.push_back(z[i * n + j]);
 
         }
         for (int j = 0; j < n; j++) {
-          element(*this, IntVarArgs(Zvec_i), x[i * n + j], y[i * n + j]);
+          element(*this, GC::IntVarArgs(Zvec_i), x[i * n + j], y[i * n + j]);
         }
       }
 
@@ -269,29 +267,29 @@ public:
 
     // Branch strategy: select variable w/ smallest domain size --> select its minimum value:
 #if BRANCH_XY == 0
-    branch(*this, x, INT_VAR_NONE(), INT_VAL_MIN());
-    branch(*this, y, INT_VAR_NONE(), INT_VAL_MIN());
+    GC::branch(*this, x, GC::INT_VAR_NONE(), GC::INT_VAL_MIN());
+    GC::branch(*this, y, GC::INT_VAR_NONE(), GC::INT_VAL_MIN());
 #elif BRANCH_XY == 1
     for (int i = 0; i < x.size(); i++)
       {
-        branch(*this, x[i], INT_VAL_MIN());
-        branch(*this, y[i], INT_VAL_MIN());
+        GC::branch(*this, x[i], GC::INT_VAL_MIN());
+        GC::branch(*this, y[i], GC::INT_VAL_MIN());
       }
 #endif
-    branch(*this, z, INT_VAR_NONE(), INT_VAL_MIN());
+    GC::branch(*this, z, GC::INT_VAR_NONE(), GC::INT_VAL_MIN());
 
     // Apply tracers to search to print out variable domain reductions
 #if VERBOSE == 1
-    trace(*this, x, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sx);
-    trace(*this, y, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sy);
-    trace(*this, z, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sz);
+    GC::trace(*this, x, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sx);
+    GC::trace(*this, y, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sy);
+    GC::trace(*this, z, TE_COMMIT | TE_INIT | TE_DONE | TE_FAIL | TE_POST | TE_PROPAGATE | TE_PRUNE, StdIntTracer_VariableIDs::Sz);
 #endif
 
   }
 
 
   // Provide copy constructor for committing
-  TWO_MOLS(TWO_MOLS& T) : Space(T){
+  TWO_MOLS(TWO_MOLS& T) : GC::Space(T){
     x.update(*this, T.x);
     y.update(*this, T.y);
     this->n = T.n;
@@ -299,7 +297,7 @@ public:
 
 
   // Provide instruction for copy within search
-  virtual Space* copy(void) {
+  virtual GC::Space* copy(void) {
     copyCount++;
 #if VERBOSE == 1
     if (this->stable()) {
@@ -347,12 +345,12 @@ int main(int argc, char *argv[]) {
   }
   else {
     fix_entries = true;
-    string str;
-    cout << "Format: Enter order; newline; entries of first square; newline; entries of second square" << endl;
-    cout << "Use asterisks for undefined entries" << endl;
-    cin >> n;
-    getline(cin, str);
-    getline(cin, str);
+    std::string str;
+    std::cout << "Format: Enter order; newline; entries of first square; newline; entries of second square" << std::endl;
+    std::cout << "Use asterisks for undefined entries" << std::endl;
+    std::cin >> n;
+    getline(std::cin, str);
+    getline(std::cin, str);
     assert(str.empty());
 
     A_init = new int[n*n];
@@ -360,17 +358,17 @@ int main(int argc, char *argv[]) {
 
     for(int i = 0; i < n; i++) {
       for(int j = 0; j < n; j++) {
-        cin >> str;
+        std::cin >> str;
         A_init[i*n+j] = (str == "*") ? -1 : stoi(str);
       }
     }
-    getline(cin, str);
-    getline(cin, str);
+    getline(std::cin, str);
+    getline(std::cin, str);
     assert(str.empty());
 
     for(int i = 0; i < n; i++) {
       for(int j = 0; j < n; j++) {
-        cin >> str;
+        std::cin >> str;
         B_init[i*n+j] = (str == "*") ? -1 : stoi(str);
       }
     }
@@ -381,14 +379,14 @@ int main(int argc, char *argv[]) {
   TWO_MOLS *T = new TWO_MOLS(n);
 
   // Declare options - the Tracer will tell us when the search branches and on what value
-  Search::Options *O = new Search::Options();
+  GC::Search::Options *O = new GC::Search::Options();
   O->threads = 1;
 #if VERBOSE == 1
   O->tracer = &StdSearchTracer::def;
 #endif
 
   // Execute Depth-First Search on the space
-  DFS<TWO_MOLS> e(T, *O);
+  GC::DFS<TWO_MOLS> e(T, *O);
 
   // Free up old space
   delete T;
@@ -398,7 +396,7 @@ int main(int argc, char *argv[]) {
     S->Print();
   }
   else {
-    cout << "No solutions found\n";
+    std::cout << "No solutions found\n";
   }
 
   if(fix_entries) {
