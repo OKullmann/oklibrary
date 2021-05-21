3fa4bbb2a75086d296cc9810c8375219ba1e3050
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-05-21 16:42:49 +0100

    Extended todos, and various updates of coding-standard.

--- Satisfiability/Solvers/Gecode/MOLS/2mols.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/2mols.cpp
@@ -21,6 +21,10 @@
 
 1. Determine the precise meaning of the branchers:
     - Do different branchers create a common pool?
+    - It seems sensible here to use *one* basic vector of variables, which
+      contains *all* variables.
+    - Helper functions perform all the index-computations (so that the code is easy
+      to read).
 
 2. Update coding standard (OZ)
     - DONE Use namespace-abbreviations.
@@ -60,7 +64,7 @@
 #include <iostream>
 #include <vector>
 
-#include <math.h>
+#include <cmath>
 
 #include "gecode/driver.hh"
 #include "gecode/int.hh"
@@ -71,7 +75,7 @@
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.2.1",
+        "0.2.2",
         "21.5.2021",
         __FILE__,
         "Noah Rubin, Curtis Bright, Oliver Kullmann, and Oleg Zaikin",
@@ -85,28 +89,26 @@ namespace {
 int copyCount = 0;
 bool sym_breaking = false;
 bool fix_entries = false;
-int * A_init;
-int * B_init;
+int* A_init;
+int* B_init;
 
-// We define the model as a sub-space
 class TWO_MOLS : public GC::Space {
 
-  // Store dimension and variables
 protected:
   int n;
   GC::IntVarArray x, y, z;
 
   // Provide constructor
 public:
-  TWO_MOLS(int DIMENSION) : x(*this, (int)pow(DIMENSION, 2), 0, DIMENSION - 1),
-                            y(*this, (int)pow(DIMENSION, 2), 0, DIMENSION - 1),
-                            z(*this, (int)pow(DIMENSION, 2), 0, DIMENSION - 1) {
+  TWO_MOLS(int DIMENSION) : x(*this, (int)std::pow(DIMENSION, 2), 0, DIMENSION - 1),
+                            y(*this, (int)std::pow(DIMENSION, 2), 0, DIMENSION - 1),
+                            z(*this, (int)std::pow(DIMENSION, 2), 0, DIMENSION - 1) {
     n = DIMENSION;
 
     if (sym_breaking) {
       // Declare domains for lexicographic ordering
       std::vector<int> domain_lex(n);
-      for (int i = 0; i < n; i++) domain_lex[i] = i;
+      for (int i = 0; i < n; ++i) domain_lex[i] = i;
 
       // Declare domains for Dominance Detection
       std::vector<std::vector<int>> domain_constrained(n);
@@ -115,7 +117,7 @@ public:
       domain_constrained[1].push_back(2);
 
       // Fix remaining constrained domains for column 1 in L2
-      for (int i = 2; i < n; i++) {
+      for (int i = 2; i < n; ++i) {
         std::vector<int> domainTemp;
         if (i == (n - 2)) {
           domainTemp.push_back((n - 1));
@@ -124,19 +126,19 @@ public:
         }
         if (i == (n - 1)) {
           domainTemp.push_back(1);
-          for (int j = 3; j <= (n - 2); j++) {
+          for (int j = 3; j <= (n - 2); ++j) {
             domainTemp.push_back(j);
           }
           domain_constrained[i] = domainTemp;
           break;
         }
-        for (int j = 1; j <= i + 1; j++)
+        for (int j = 1; j <= i + 1; ++j)
           if (j != i && j != 2) domainTemp.push_back(j);
         domain_constrained[i] = domainTemp;
       }
 
       // Apply domain constraints to the first rows and columns
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i < n; ++i) {
         GC::dom(*this, x[i], domain_lex[i]);
         GC::dom(*this, y[i], domain_lex[i]);
         GC::dom(*this, x[i * n], domain_lex[i]);
@@ -147,9 +149,8 @@ public:
     }
 
     if (fix_entries) {
-      // Set initialized entries
-      for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+      for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
           if (A_init[i*n+j] >= 0)
             GC::dom(*this, x[i*n+j], A_init[i*n+j], A_init[i*n+j]);
           if (B_init[i*n+j] >= 0)
@@ -159,65 +160,59 @@ public:
     }
 
     // Latin property in rows of X
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> rows_x;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         rows_x.push_back(x[i * n + j]);
-      }
       GC::distinct(*this, rows_x);
     }
 
     // Latin property in cols of X
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> cols_x;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         cols_x.push_back(x[j * n + i]);
-      }
       GC::distinct(*this, cols_x);
     }
 
     // Latin property in rows of Y
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> rows_y;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         rows_y.push_back(y[i * n + j]);
-      }
       GC::distinct(*this, rows_y);
     }
 
     // Latin property in cols of Y
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> cols_y;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         cols_y.push_back(y[j * n + i]);
-      }
       GC::distinct(*this, cols_y);
     }
 
     // Row uniqueness of Z
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> rows_z;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         rows_z.push_back(z[i * n + j]);
-      }
       GC::distinct(*this, rows_z);
     }
 
     // Column uniqueness of Z
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> cols_z;
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < n; ++j)
         cols_z.push_back(z[j * n + i]);
-      }
       GC::distinct(*this, cols_z);
     }
 
     // Enforce element constraints on Z, X, Y
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       std::vector<GC::IntVar> Zvec_i;
-      for (int j = 0; j < n; j++)
+      for (int j = 0; j < n; ++j)
         Zvec_i.push_back(z[i * n + j]);
-      for (int j = 0; j < n; j++)
+      for (int j = 0; j < n; ++j)
         GC::element(*this, GC::IntVarArgs(Zvec_i), x[i * n + j], y[i * n + j]);
     }
 
@@ -229,8 +224,7 @@ public:
   }
 
 
-  // Provide copy constructor for committing
-  TWO_MOLS(TWO_MOLS& T) : GC::Space(T){
+  TWO_MOLS(TWO_MOLS& T) : GC::Space(T) {
     x.update(*this, T.x);
     y.update(*this, T.y);
     z.update(*this, T.z);
@@ -238,17 +232,15 @@ public:
   }
 
 
-  // Provide instruction for copy within search
-  virtual GC::Space* copy(void) {
+  virtual GC::Space* copy() {
     copyCount++;
     return new TWO_MOLS(*this);
   }
 
-  // Provide function to print solutions
   void Print() {
     assert(n > 0);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
         std::cout << x[i * n + j];
         if (j < n-1) std::cout << " ";
       }
@@ -257,8 +249,8 @@ public:
 
     std::cout << "\n";
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
         std::cout << y[i * n + j];
         if (j < n-1) std::cout << " ";
       }
@@ -270,14 +262,10 @@ public:
 };
 
 // Driver
-int main(int argc, char *argv[]) {
+int main(const int argc, const char* const argv[]) {
 
   if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
 
-  //std::cout << "Format: Enter order; newline; entries of first square; newline; entries of second square" << std::endl;
-  //std::cout << "Use asterisks for undefined entries" << std::endl;
-
-  // Parse dimension
   int n = 0;
   if (argc == 2) {
     n = atoi(argv[1]);
@@ -294,8 +282,8 @@ int main(int argc, char *argv[]) {
     A_init = new int[n*n];
     B_init = new int[n*n];
 
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
+    for(int i = 0; i < n; ++i) {
+      for(int j = 0; j < n; ++j) {
         std::cin >> str;
         A_init[i*n+j] = (str == "*") ? -1 : stoi(str);
       }
@@ -304,8 +292,8 @@ int main(int argc, char *argv[]) {
     getline(std::cin, str);
     assert(str.empty());
 
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
+    for(int i = 0; i < n; ++i) {
+      for(int j = 0; j < n; ++j) {
         std::cin >> str;
         B_init[i*n+j] = (str == "*") ? -1 : stoi(str);
       }
@@ -313,26 +301,19 @@ int main(int argc, char *argv[]) {
 
   }
 
-  // Declare new search space
-  TWO_MOLS *T = new TWO_MOLS(n);
+  TWO_MOLS* const T = new TWO_MOLS(n);
 
-  // Declare options
   GC::Search::Options *O = new GC::Search::Options();
   O->threads = 1;
 
-  // Execute Depth-First Search on the space
   GC::DFS<TWO_MOLS> e(T, *O);
 
-  // Free up old space
   delete T;
 
-  // Grab first solution from DFS or else we have no solutions
-  if (TWO_MOLS * S = e.next()) {
+  if (TWO_MOLS * S = e.next())
     S->Print();
-  }
-  else {
+  else
     std::cout << "No solutions found\n";
-  }
 
   if(fix_entries) {
     delete [] A_init;
