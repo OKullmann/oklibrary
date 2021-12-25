463ff479bae22a15d55dcfaea44d48cd70bd6c44
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2021-12-25 14:23:42 +0000

    Namespace-correction also here.
    Removed unused functions.

--- Satisfiability/Solvers/Gecode/MOLS/Euler_BBOpt.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/Euler_BBOpt.cpp
@@ -112,8 +112,11 @@ General remark: Only in very special case should global variables be used.
 #include "gecode/int.hh"
 #include "gecode/search.hh"
 
+#include "Euler.hpp"
 
-namespace Euler{
+namespace {
+
+  using namespace Euler;
 
   namespace GC = Gecode;
   namespace LA = Lookahead;
@@ -127,68 +130,6 @@ namespace Euler{
   constexpr LS::ls_dim_t N_default = 0;
   constexpr LS::ls_dim_t k_default = 2;
 
-  // Pgopagation level for Gecode constraints.
-  // def: default propagation (can be different for different
-  //      constraints).
-  // val: value propagation (the fastest and the simplest one).
-  // bnd: bound propagation (average speed and performance).
-  // dom: domain propagation (the slowest and the most effective).
-  enum class PropO {def=0, val=1, bnd=2, dom=3};
-  constexpr int PropOsize = 4;
-
-  enum class HeO {show=0, noshow=1};
-  constexpr int HeOsize = 2;
-  enum class StatO {show=0, noshow=1};
-  constexpr int StatOsize = 2;
-  enum class SolO {show=0, noshow=1};
-  constexpr int SolOsize = 2;
-}
-namespace Environment {
-  template <> struct RegistrationPolicies<Euler::PropO> {
-    static constexpr int size = Euler::PropOsize;
-    static constexpr std::array<const char*, size> string {"def", "val", "bnd", "dom"};
-  };
-  template <> struct RegistrationPolicies<Euler::HeO> {
-    static constexpr int size = Euler::HeOsize;
-    static constexpr std::array<const char*, size> string {"+head", "-head"};
-  };
-  template <> struct RegistrationPolicies<Euler::StatO> {
-    static constexpr int size = Euler::StatOsize;
-    static constexpr std::array<const char*, size> string {"+stat", "-stat"};
-  };
-  template <> struct RegistrationPolicies<Euler::SolO> {
-    static constexpr int size = Euler::SolOsize;
-    static constexpr std::array<const char*, size> string {"+sol", "-sol"};
-  };
-}
-namespace Euler {
-  constexpr char sep = ',';
-
-  typedef std::tuple<PropO> gecode_option_t;
-  std::ostream& operator <<(std::ostream& out, const PropO m) {
-    switch (m) {
-    case PropO::val : return out << "values-prop";
-    case PropO::bnd : return out << "bounds-prop";
-    case PropO::dom : return out << "domain-prop";
-    default : return out << "default-prop";}
-  }
-
-  typedef std::tuple<HeO, StatO, SolO> output_option_t;
-  std::ostream& operator <<(std::ostream& out, const HeO m) {
-    switch (m) {
-    case HeO::show : return out << "show-header";
-    default : return out << "noshow-header";}
-  }
-  std::ostream& operator <<(std::ostream& out, const StatO m) {
-    switch (m) {
-    case StatO::show : return out << "show-statistics";
-    default : return out << "noshow-statistics";}
-  }
-  std::ostream& operator <<(std::ostream& out, const SolO m) {
-    switch (m) {
-    case SolO::show : return out << "show-solutions";
-    default : return out << "noshow-solutions";}
-  }
 
   LS::ls_dim_t read_N(const std::string& s, const std::string& error) noexcept {
     if (s.empty()) return N_default;
@@ -223,18 +164,6 @@ namespace Euler {
     return read_k(s, error);
   }
 
-  // Read weights needed to calculate distances in lookahead.
-  Optimisation::vec_t read_weights(const std::string& s) noexcept {
-    if (s.empty()) return {};
-    Optimisation::vec_t wghts;
-    std::stringstream sstr(s);
-    for (Optimisation::x_t i; sstr >> i;) {
-        wghts.push_back(i);
-        if (sstr.peek() == ',') sstr.ignore();
-    }
-    return wghts;
-  }
-
   gecode_intvec_t read_partial_ls(const LS::ls_dim_t N) noexcept {
     assert(N > 0);
     const LS::ls_dim_t size = N*N;
@@ -253,12 +182,6 @@ namespace Euler {
       partial_ls[i] = (ls_s[i] == "*") ? -1 : std::stoi(ls_s[i]);
     }
     return partial_ls;
-  }
-
-  LS::ls_dim_t given_cells(const gecode_intvec_t ls_partial) {
-    LS::ls_dim_t res = 0;
-    for (auto x : ls_partial) res += x==-1 ? 0 : 1;
-    return res;
   }
 
   class TWO_MOLS : public GC::Space {
@@ -441,7 +364,7 @@ namespace Euler {
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.2.1",
+        "0.2.2",
         "25.12.2021",
         __FILE__,
         "Oliver Kullmann and Oleg Zaikin",
@@ -472,9 +395,9 @@ namespace {
      // Finding an Euler square:
     LS::ls_dim_t N;
     LA::option_t alg_options;
-    Euler::gecode_option_t gecode_options;
-    Euler::gecode_intvec_t ls1_partial;
-    Euler::gecode_intvec_t ls2_partial;
+    gecode_option_t gecode_options;
+    gecode_intvec_t ls1_partial;
+    gecode_intvec_t ls2_partial;
 
     void init(const int argc, const char* const argv[]) noexcept {
       Environment::Index index;
@@ -484,20 +407,20 @@ namespace {
       // Read gecode options:
       gecode_options = argc <= index ? Euler::gecode_option_t{Euler::PropO::dom} :
         Environment::translate<Euler::gecode_option_t>()(argv[index++], LA::sep);
-      N = Euler::read_N(error);
-      [[maybe_unused]] const auto k = Euler::read_k(error);
+      N = read_N(error);
+      [[maybe_unused]] const auto k = read_k(error);
       assert(N > 0 and k > 0);
-      ls1_partial = Euler::read_partial_ls(N);
-      ls2_partial = Euler::read_partial_ls(N);
+      ls1_partial = read_partial_ls(N);
+      ls2_partial = read_partial_ls(N);
       assert(not ls1_partial.empty() and not ls2_partial.empty());
     }
 
     Optimisation::y_t func(const Optimisation::vec_t& v) noexcept {
       assert(not v.empty());
       assert(v.size() == N-2);
-      const std::shared_ptr<Euler::TWO_MOLS> p(new Euler::TWO_MOLS(N,
+      const std::shared_ptr<TWO_MOLS> p(new TWO_MOLS(N,
         alg_options, gecode_options, ls1_partial, ls2_partial, v));
-      Statistics::SearchStat stat = LA::solve<Euler::TWO_MOLS>(p);
+      Statistics::SearchStat stat = LA::solve<TWO_MOLS>(p);
       assert(p.use_count() == 1);
 for (const auto x : v) std::cerr << x << " ";
 std::cerr << stat << "\n";
