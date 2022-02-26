ae6880db4dc0166a2d0bfa207a73cba4767eba94
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-02-26 17:35:42 +0000

    Provided generator for "crowns".

--- Satisfiability/Transformers/Generators/Bicliques/Generators.hpp
+++ Satisfiability/Transformers/Generators/Bicliques/Generators.hpp
@@ -23,6 +23,7 @@ License, or any later version. */
 #include <ProgramOptions/Environment.hpp>
 #include <Numerics/NumInOut.hpp>
 #include <Numerics/Conversions.hpp>
+#include <Numerics/FloatingPoint.hpp>
 
 #include "Graphs.hpp"
 
@@ -33,14 +34,14 @@ namespace Generators {
   typedef AdjMapStr::size_t size_t;
 
 
-  enum class Types { clique=0, biclique=1 };
+  enum class Types { clique=0, biclique=1, crown=2 };
 }
 namespace Environment {
   template <>
   struct RegistrationPolicies<Generators::Types> {
-    static constexpr int size = int(Generators::Types::biclique)+1;
+    static constexpr int size = int(Generators::Types::crown)+1;
     static constexpr std::array<const char*, size> string
-    {"clique", "biclique"};
+    {"clique", "biclique", "crown"};
   };
 }
 namespace Generators {
@@ -48,13 +49,20 @@ namespace Generators {
     switch (t) {
     case Types::clique : return out << "clique";
     case Types::biclique : return out << "biclique";
+    case Types::crown : return out << "crown";
     default : return out << "Types::UNKNOWN";
     }
   }
 
 
+  size_t bcc_clique(const size_t n) {
+    if (n == 0) return 0;
+    else return FloatingPoint::ceil(FloatingPoint::log2(n));
+  }
   AdjMapStr clique(const size_t n) {
     AdjMapStr G(Graphs::GT::und);
+    G.comments.push_back("clique(" + std::to_string(n) + ")");
+    G.comments.push_back("bcc=" + std::to_string(bcc_clique(n)));
     if (n == 0) return G;
     std::vector<std::string> V; V.reserve(n);
     for (size_t i = 0; i < n; ++i) V.push_back(std::to_string(i+1));
@@ -62,13 +70,18 @@ namespace Generators {
     assert(res.first == n and res.second == (n*(n-1)) / 2);
     return G;
   }
-  size_t bcc_clique(const size_t n) {
-    if (n == 0) return 0;
-    else return FloatingPoint::ceil(FloatingPoint::log2(n));
-  }
 
-  AdjMapStr biclique(const size_t n, const size_t m) {
+  size_t bcc_biclique(const size_t n, const size_t m) {
+    if (n == 0 or m == 0) return 0;
+    else return 1;
+  }
+  AdjMapStr biclique(const size_t n, const size_t m, const bool with_c = true) {
     AdjMapStr G(Graphs::GT::und);
+    if (with_c) {
+      G.comments.push_back("biclique(" + std::to_string(n) + "," +
+                           std::to_string(m) + ")");
+      G.comments.push_back("bcc=" + std::to_string(bcc_biclique(n,m)));
+    }
     if (n == 0 and m == 0) return G;
     std::vector<std::string> V1; V1.reserve(n);
     for (size_t i = 0; i < n; ++i) V1.push_back("l" + std::to_string(i+1));
@@ -78,9 +91,23 @@ namespace Generators {
     assert(res.first == n+m and res.second == n*m);
     return G;
   }
-  size_t bcc_biclique(const size_t n, const size_t m) {
-    if (n == 0 or m == 0) return 0;
-    else return 1;
+
+  size_t bcc_crown(const size_t n) {
+    return FloatingPoint::inv_fcbinomial_coeff(n);
+  }
+  AdjMapStr crown(const size_t n) {
+    AdjMapStr G = biclique(n,n,false);
+    G.comments.push_back("crown(" + std::to_string(n) + ")");
+    G.comments.push_back("bcc=" + std::to_string(bcc_crown(n)));
+    std::vector<std::pair<std::string,std::string>> matching;
+    matching.reserve(n);
+    for (size_t i =0; i < n; ++i)
+      matching.emplace_back("l" + std::to_string(i+1),
+                            "r" + std::to_string(i+1));
+    [[maybe_unused]] const size_t res = G.remove_edgesr(matching);
+    assert(res == n);
+    assert(G.n() == 2*n and G.m() == n * (n-1));
+    return G;
   }
 
 
@@ -107,6 +134,12 @@ namespace Generators {
       const size_t N{FloatingPoint::toUInt(argv[2])};
       const size_t M{FloatingPoint::toUInt(argv[3])};
       return biclique(N,M);
+    }
+    case Types::crown : {
+      if (argc < 3)
+        throw std::invalid_argument("Generators::create:clique: argc=2");
+      const size_t N{FloatingPoint::toUInt(argv[2])};
+      return crown(N);
     }
     default : assert(0);
       throw std::range_error("Generators::create: UNKNOWN t="
