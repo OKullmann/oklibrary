9cdcf9ba6c60c7e3c087ad325018973bd297069a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-02-26 00:40:44 +0000

    Started with collection of generators.

---
+++ Satisfiability/Transformers/Generators/Bicliques/Generators.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 26.2.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+  Graph generators
+
+*/
+
+#ifndef GENERATORS_1twuS7HUpM
+#define GENERATORS_1twuS7HUpM
+
+namespace Generators {
+
+}
+
+#endif
---
+++ Satisfiability/Transformers/Generators/Bicliques/TestGenerators.cpp
@@ -0,0 +1,38 @@
+// Oliver Kullmann, 26.2.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <iostream>
+
+#include <cassert>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "Generators.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "26.2.2022",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/TestGenerators.cpp",
+        "GPL v3"};
+
+  using namespace Generators;
+
+  template <class X>
+  constexpr bool eqp(const X& lhs, const X& rhs) noexcept {
+    return lhs == rhs;
+  }
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+
+}
