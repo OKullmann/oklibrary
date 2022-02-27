3c58763e2f3ccae2b7ec382610d8eab04b779ca7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-02-27 12:52:27 +0000

    Started with new components for conflict-graphs.

---
+++ Satisfiability/Transformers/Generators/Bicliques/ConflictGraphs.hpp
@@ -0,0 +1,21 @@
+// Oliver Kullmann, 27.2.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+  Conflict graphs of clause-sets etc.
+
+*/
+
+#ifndef CONFLICTGRAPHS_ipM62kMu5w
+#define CONFLICTGRAPHS_ipM62kMu5w
+
+namespace ConflictGraphs {
+
+}
+
+#endif
