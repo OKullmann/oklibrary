1dc13f31df41c9da1065e81fc2910fa363e755d9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-02-28 10:56:18 +0000

    Application to transform CNF to its conflict-graph.

---
+++ Satisfiability/Transformers/Generators/Bicliques/CNF2cg.cpp
@@ -0,0 +1,51 @@
+// Oliver Kullmann, 28.2.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/* 
+
+*/
+
+#include <iostream>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "ConflictGraphs.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.1.0",
+        "28.2.2022",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/CNF2cg.cpp",
+        "GPL v3"};
+
+  using namespace ConflictGraphs;
+
+  const std::string error = "ERROR[" + proginfo.prg + "]: ";
+
+  bool show_usage(const int argc, const char* const argv[]) {
+    if (not Environment::help_header(std::cout, argc, argv, proginfo))
+      return false;
+    std::cout <<
+    "> " << proginfo.prg << "\n\n"
+    " reads a Dimacs-file from standard input, and prints the conflict-graph to standard output.\n"
+;
+    return true;
+  }
+
+}
+
+int main(const int argc, const char* const argv[]) {
+
+  if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
+  if (show_usage(argc, argv)) return 0;
+
+  std::cout << conflictgraph_bydef(read_strict_Dimacs(std::cin));
+
+}
