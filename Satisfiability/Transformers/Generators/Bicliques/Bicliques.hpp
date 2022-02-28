2f949b4fc38282506d32149bb653ef76b7e1a360
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-02-28 11:59:14 +0000

    Started module on bicliques.

---
+++ Satisfiability/Transformers/Generators/Bicliques/Bicliques.hpp
@@ -0,0 +1,23 @@
+// Oliver Kullmann, 28.2.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+  Bicliques on their own, and coverings and partitions with them
+
+*/
+
+#ifndef BICLIQUES_zlDSHv4COU
+#define BICLIQUES_zlDSHv4COU
+
+#include <cassert>
+
+namespace Bicliques {
+
+}
+
+#endif
