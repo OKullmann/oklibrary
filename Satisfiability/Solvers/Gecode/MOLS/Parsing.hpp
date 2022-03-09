faa1822d8e5845999294d0c69659bba476bcb5b3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-09 20:15:34 +0000

    Update of syntax-plans.
    Mostly complete; implementation can start.

--- Satisfiability/Solvers/Gecode/MOLS/Parsing.hpp
+++ Satisfiability/Solvers/Gecode/MOLS/Parsing.hpp
@@ -10,90 +10,67 @@ License, or any later version. */
   Parsing of the problem specification
 
 
-0. Design for general k
-    - N is constant.
-    - We have primary squares A, B, C, ... (any identifier).
-        intro A B C
-    - Then we have secondary squares given by some unary operation on the
-      primary squares:
-       - We have t(A), at(A) (transposed and antitransposed) --- for these we
-         likely do not introduce new variables.
-         We could use "tA" (forbidding that an identifier starts with "t").
-       - The question is here where t(A) can be used; likely we want a very
-         restricted usage.
-         diag(tA) is equivalent to diag(A), and the same for antidiag and
-         at.
-         So t(A) and at(A) are only used in lists for orthogonality?
+1. Example:
 
-         QUESTION: But perhaps it is actually easier to formally treat t(A)
-         and at(A) as new squares, and then to actually eliminate them
-         when posting the constraints?
-       - Other types of "conjugates" likely need the introduction of new
-         squares X = conj(A); expressed like
-           conj A X
-         introducing X.
-       - A more specific syntax is needed:
-           conj231 A A231
-       - For the latter it is relevant that we can express the relation easily
-         (likely via the elementship-constraint)>
-    - Then we have conditions on those primary and secondary squares:
-     - diag(A), antidiag(X), written
-         diag A
-         antidiag X
-     - ortho A, X, t(B) ..., written
-         ortho A X tB
-       (here for the formulation we use pairwise the witness-squares; only
-       here can we use say tB, atB).
-     - = A X .
 
-1. Handle special types of Latin squares (unary conditions):
-    - Diagonal Latin square: in the main diagonal and antidiagonal all
-      elements are distinct.
-      We want two special conditions:
-        diag A
-        antidiag A
-    - Constant diagonal Latin square: in the main diagonal all elements are
-      equal to each other, similar for elements of the main antidiagonal.
-      These ls's are called "unipotent" (and similarly "antiunipotent").
-      Conditions:
-        uni A
-        antiuni A
-    - Idempotent Latin square: A(i,i) = i.
-        idem A
-      Similarly "row-reduced", "column-reduced":
-        rred A
-        cred A
-    - Symmetric Latin square: it is equal to its transpose.
-        symm A
-        antisymm A
+squares A B c10 # comment; k=3
 
-        QUESTION: it seems that for N>=3 one can not have both at the same
-        time? But perhaps easiest to allow such combinations.
-    - Later (does not seem to be realisable):
-      Semisymmetric Latin square: it is equal to (at least) three of its
+# empty lines possible (always optional)
+
+A        rls cls symm # like the simple format for graphs
+at B     cls diag # these are conditions for at(B)
+c231 A   idem rred # these variants will be introduced (basic encoding)
+A        uni cls # as with programming languages, space-symbols are collapsed
+
+= c213 c10  c321 A
+= B B # we allow that (in the basic encoding)
+
+ortho A c213 B at A
+ortho A # perhaps this we disallow?
+
+
+The order above is irrelvant except of that "squares" acts like a declaration
+in programming languages (several declarations possible, only affecting
+following lines; redeclarations yield errors).
+
+The search for three MOLS is formulated as:
+
+squares A B C
+A rls cls
+B rls cls
+C rls cls
+ortho A B C
+
+
+A self-orthogonal ls:
+
+squares A
+ortho A c213 A
+ortho A at A
+
+
+Totally symmetric Latin square: all six conjugates are equal:
+
+squares A
+A symm
+= A c312 A
+= A c321 A
+= A c213 A
+= A c231 A
+= A c132 A
+
+
+QUESTION: it seems that for N>=3 one can not have both diag and antidiag
+at the same time? But easiest to allow such combinations (and such tests
+can be useful for performance evaluation).
+
+
+1. Conditions not included:
+    - Semisymmetric Latin square: it is equal to (at least) three of its
       conjugates, which must necessarily include the (1, 2, 3), (3, 1, 2) and
       (2, 3, 1)-conjugates.
-    - Totally symmetric Latin square: all six conjugates are equal:
-      can be expressed by
-       conj321 A A321
-       ,,.
-       symm A
-       = A A321
-       ...
 
-
-2. Handle special types of orthogonality.
-    - For example the search for 3 MOLS is expressed by
-        intro A B C
-        ortho A B C
-    - Self-orthogonal Latin square: it is orthogonal to its transposition:
-        intro A
-        ortho A t(A)
-    - Doubly self-orthogonal Latin square: it is orthogonal to both its
-      transposed and antitransposed:
-        intro A
-        ortho A t(A)
-        ortho A at(A).
+      Due to the "or" we don't handle that.
 
 */
 
