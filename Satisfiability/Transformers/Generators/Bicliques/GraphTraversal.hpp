a39dadfc11f056e155c14f11cddcb60c413cdc29
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-12 12:32:59 +0000

    Computation of connected components of a graph.

--- Satisfiability/Transformers/Generators/Bicliques/GraphTraversal.hpp
+++ Satisfiability/Transformers/Generators/Bicliques/GraphTraversal.hpp
@@ -10,7 +10,12 @@ License, or any later version. */
 
 */
 
+#include <vector>
+#include <algorithm>
+#include <stack>
+
 #include <cassert>
+#include <cstdint>
 
 #include "Graphs.hpp"
 
@@ -18,6 +23,52 @@ License, or any later version. */
 #define GRAPHTRAVERSAL_ipqWptxVwS
 
 namespace GraphTraversal {
+
+  namespace GR = Graphs;
+
+  typedef std::uint64_t size_t;
+  constexpr size_t max_size = 18446744073709551615UL; // 2^64-1
+
+  struct CCbyIndices {
+    typedef std::vector<size_t> indvec_t;
+    indvec_t cv; // mapping every vertex-index to its cc-index
+    size_t numcc; // number of connected components
+
+    explicit CCbyIndices(const size_t n = 0, const size_t cc = 0)
+      : cv(n), numcc(cc) {}
+
+    bool operator ==(const CCbyIndices&) const noexcept = default;
+  };
+  // The elements of cv are exactly the numbers in [1, ..., numcc]:
+  inline bool valid(const CCbyIndices& cc) {
+    const size_t C = cc.numcc;
+    if (C == 0) return cc.cv.empty();
+    std::vector<size_t> found(C);
+    for (const size_t i : cc.cv)
+      if (i == 0 or i > C) return false;
+      else ++found[i-1];
+    return std::ranges::find(found, 0) == found.end();
+  }
+
+  CCbyIndices cc_by_dfs(const GR::AdjVecUInt& G) {
+    const size_t n = G.n();
+    CCbyIndices res(n);
+    using id_t = GR::AdjVecUInt::id_t;
+    for (id_t v = 0; v < n; ++v) {
+      if (res.cv[v] != 0) continue;
+      res.cv[v] = ++res.numcc;
+      std::stack<id_t> S;
+      for (const id_t w : G.neighbours(v)) if (res.cv[w] == 0) S.push(w);
+      while (not S.empty()) {
+        const id_t v = S.top(); S.pop();
+        if (res.cv[v] != 0) continue;
+        res.cv[v] = res.numcc;
+        for (const id_t w : G.neighbours(v)) if (res.cv[w] == 0) S.push(w);
+      }
+    }
+    assert(valid(res));
+    return res;
+  }
 
 }
 
--- Satisfiability/Transformers/Generators/Bicliques/TestGraphTraversal.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/TestGraphTraversal.cpp
@@ -6,17 +6,19 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 #include <iostream>
+#include <algorithm>
 
 #include <cassert>
 
 #include <ProgramOptions/Environment.hpp>
 
 #include "GraphTraversal.hpp"
+#include "Generators.hpp"
 
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.1",
+        "0.1.0",
         "12.3.2022",
         __FILE__,
         "Oliver Kullmann",
@@ -34,5 +36,61 @@ namespace {
 int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
+
+  {assert(valid(CCbyIndices()));
+   assert(not valid(CCbyIndices(0,1)));
+   assert(not valid(CCbyIndices(1,1)));
+   CCbyIndices cc(1,1);
+   assert(not valid(cc));
+   cc.cv[0] = 1;
+   assert(valid(cc));
+  }
+
+  {for (size_t n = 0; n <= 10; ++n) {
+     const CCbyIndices res = cc_by_dfs(GR::AdjVecUInt(GR::GT::und, n));
+     assert(valid(res));
+     assert(res.numcc == n);
+     assert(res.cv.size() == n);
+   }
+  }
+  {for (size_t n = 0; n <= 10; ++n) {
+     const CCbyIndices res = cc_by_dfs(GR::AdjVecUInt(GR::GT::dir, n));
+     assert(valid(res));
+     assert(res.numcc == n);
+     assert(res.cv.size() == n);
+   }
+  }
+  {for (size_t n = 0; n <= 10; ++n) {
+      const CCbyIndices res = cc_by_dfs(GR::AdjVecUInt(Generators::clique(n)));
+      assert(valid(res));
+      assert(res.numcc == Generators::numcc_clique(n));
+      assert(res.cv.size() == n);
+    }
+  }
+  {for (size_t n = 0; n <= 5; ++n)
+     for (size_t m = 0; m <= 5; ++m) {
+       const CCbyIndices res =
+         cc_by_dfs(GR::AdjVecUInt(Generators::biclique(n,m)));
+       assert(valid(res));
+       assert(res.numcc == Generators::numcc_biclique(n,m));
+       assert(res.cv.size() == n+m);
+     }
+  }
+  {for (size_t n = 0; n <= 5; ++n)
+     for (size_t m = 0; m <= 5; ++m) {
+       const CCbyIndices res =
+         cc_by_dfs(GR::AdjVecUInt(Generators::grid(n,m)));
+       assert(valid(res));
+       assert(res.numcc == Generators::numcc_grid(n,m));
+       assert(res.cv.size() == n*m);
+     }
+  }
+  {for (size_t n = 0; n <= 10; ++n) {
+     const CCbyIndices res = cc_by_dfs(GR::AdjVecUInt(Generators::crown(n)));
+     assert(valid(res));
+     assert(res.numcc == Generators::numcc_crown(n));
+     assert(res.cv.size() == 2*n);
+   }
+  }
 
 }
