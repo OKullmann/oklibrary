6cb96dbd160092f84314f1069dd86e0aceebecc5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-12 09:25:45 +0000

    New module on graph traversal (BFS, DFS, etc.).

---
+++ Satisfiability/Transformers/Generators/Bicliques/GraphTraversal.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 12.3.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+  Graph traversal (BFS, DFS, etc.), for various purposes
+
+*/
+
+#include <cassert>
+
+#include "Graphs.hpp"
+
+#ifndef GRAPHTRAVERSAL_ipqWptxVwS
+#define GRAPHTRAVERSAL_ipqWptxVwS
+
+namespace GraphTraversal {
+
+}
+
+#endif
