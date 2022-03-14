985c0e1c452b4343d82df65cf6af5efa6cdffb19
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-14 12:23:54 +0000

    New module for the encoding.

---
+++ Satisfiability/Solvers/Gecode/MOLS/Encoding.hpp
@@ -0,0 +1,22 @@
+// Oliver Kullmann, 12.3.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+  Encoding the LS-MOLS-conditions, for Gecode
+
+*/
+
+#ifndef ENCODING_HqEmYk6s0p
+#define ENCODING_HqEmYk6s0p
+
+#include "Conditions.hpp"
+
+namespace Encoding {
+
+}
+
+#endif
