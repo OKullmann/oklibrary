1812323b47440cacf16ebee700b6c00de88f260d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-17 22:55:52 +0000

    Started module for Gecode-mols-solvers.

---
+++ Satisfiability/Solvers/Gecode/MOLS/Solvers.hpp
@@ -0,0 +1,20 @@
+// Oliver Kullmann, 17.3.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+  Solvers for the LS-MOLS-conditions, for Gecode
+
+*/
+
+#ifndef SOLVERS_PNeIRm1Ic7
+#define SOLVERS_PNeIRm1Ic7
+
+namespace Solvers {
+
+}
+
+#endif
