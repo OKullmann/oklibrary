852f700b1efc44c568928359cb614e5aba9109d6
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-21 09:25:56 +0000

    Started module for partial squares.

---
+++ Satisfiability/Solvers/Gecode/MOLS/PartialSquares.hpp
@@ -0,0 +1,83 @@
+// Oliver Kullmann, 21.3.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+  Partial squares
+
+
+TODOS:
+
+1. Clarify the relations to Random/LatinSquares.hpp
+ - Especially Random::ls_ip_t is similar to PSquares.
+ - Though the context is different: in Random/LatinSquares.hpp one is
+   concerned with direct computation with latin squares, while here
+   we are concerned with representing conditions on (arbitrary) squares.
+
+*/
+
+#ifndef PARTIALSQUARES_xfWDmQELnQ
+#define PARTIALSQUARES_xfWDmQELnQ
+
+#include <vector>
+#include <algorithm>
+
+#include "Conditions.hpp"
+
+namespace PartialSquares {
+
+  namespace CD = Conditions;
+
+  typedef CD::size_t size_t;
+
+  typedef std::vector<bool> cell_t;
+  typedef std::vector<cell_t> prow_t;
+  typedef std::vector<prow_t> psquare_t;
+
+  bool valid(const prow_t& pr, const size_t N) noexcept {
+    return pr.size() == N and
+      std::ranges::all_of(pr, [&N](const cell_t& c){return c.size()==N;});
+  }
+  bool valid(const prow_t& pr) noexcept {
+    return valid(pr, pr.size());
+  }
+  bool valid(const psquare_t& ps, const size_t N) noexcept {
+    return ps.size() == N and
+      std::ranges::all_of(ps, [&N](const prow_t& r){return valid(r,N);});
+  }
+  bool valid(const psquare_t& ps) noexcept {
+    return valid(ps, ps.size());
+  }
+
+  prow_t empty_prow(const size_t N) {
+    const cell_t c(N);
+    return prow_t(N, c);
+  }
+  psquare_t empty_psquare(const size_t N) {
+    const prow_t r = empty_prow(N);
+    return psquare_t(N, r);
+  }
+
+  struct PSquare {
+    psquare_t ps;
+    CD::Square s;
+    const size_t N;
+    PSquare(const size_t N, const CD::Square s) :
+      ps(empty_psquare(N)), s(s), N(N) {}
+
+    bool operator ==(const PSquare&) const = default;
+    auto operator <=>(const PSquare&) const = default;
+  };
+
+  bool valid(const PSquare& ps) {
+    return valid(ps.ps, ps.N);
+  }
+
+}
+
+#endif
+
+
---
+++ Satisfiability/Solvers/Gecode/MOLS/TestPartialSquares.cpp
@@ -0,0 +1,38 @@
+// Oliver Kullmann, 21.3.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <iostream>
+
+#include <cassert>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "PartialSquares.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "21.3.2022",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/OKlib-MOLS/blob/master/Satisfiability/Solvers/Gecode/MOLS/TestPartialSquares.cpp",
+        "GPL v3"};
+
+  using namespace PartialSquares;
+
+  template <class X>
+  constexpr bool eqp(const X& lhs, const X& rhs) noexcept {
+    return lhs == rhs;
+  }
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+
+}
