307a3ce65523e83cd0aa26d8d15c387b13c5a5fa
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-03-27 08:46:33 +0100

    Provided options for handling Gecode-branching-heuristics.

--- Satisfiability/Solvers/Gecode/MOLS/Options.hpp
+++ Satisfiability/Solvers/Gecode/MOLS/Options.hpp
@@ -16,6 +16,8 @@ License, or any later version. */
 
 #include <array>
 #include <ostream>
+#include <exception>
+#include <string>
 
 #include <gecode/int.hh>
 
@@ -26,19 +28,91 @@ namespace Options {
   namespace GC = Gecode;
 
   // Propagation levels for Gecode-constraints:
-  // dom: domain propagation (the strongest).
-  // def: default propagation (can be different for different
-  //      constraints).
-  // val: values propagation (the weakest).
-  // bnd: bounds propagation.
-  enum class PropO {dom=0, def=1, val=2, bnd=3};
-  constexpr int PropOsize = int(PropO::bnd)+1;
+  enum class PropO {
+    dom=0, // domain propagation (strongest)
+    def=1, // default propagation (different for different constraints)
+    val=2, // values propagation (weakest)
+    bnd=3}; // bounds propagation
+  constexpr int PropOsize = int(PropO::bnd) + 1;
+  GC::IntPropLevel prop_level(const PropO po) noexcept {
+    switch (po) {
+    case PropO::dom: return GC::IPL_DOM;
+    case PropO::def: return GC::IPL_DEF;
+    case PropO::val: return GC::IPL_VAL;
+    case PropO::bnd: return GC::IPL_BND;
+    default : return GC::IPL_DOM;}
+  }
+
+  // Variable-selection for Gecode-branching ("branching-heuristic variables"):
+  enum class BHV {
+    first = 1, // first open
+    mindeg = 2, // smallest degree
+    maxdeg = 3, // largest degree
+    mindom = 4, // smallest domain
+    maxdom = 5, // largest domain
+    mindegdom = 6, // smallest degree/domain
+    maxdegdom = 7, // largest degree/domain
+    minminreg = 8, // smallest minimum-regret
+    maxminreg = 9, // largest minimum-regret
+    minmaxreg = 10, // smallest maximum-regret
+    maxmaxreg = 11 // largest maximum-regret
+  };
+  constexpr int BHVsize = int(BHV::maxmaxreg) + 1;
+  GC::IntVarBranch var_branch(const BHV bvar) {
+    switch (bvar) {
+    case BHV::first: return GC::INT_VAR_NONE();
+    case BHV::mindeg: return GC::INT_VAR_DEGREE_MIN();
+    case BHV::maxdeg: return GC::INT_VAR_DEGREE_MAX();
+    case BHV::mindom: return GC::INT_VAR_SIZE_MIN();
+    case BHV::maxdom: return GC::INT_VAR_SIZE_MAX();
+    case BHV::mindegdom: return GC::INT_VAR_DEGREE_SIZE_MIN();
+    case BHV::maxdegdom: return GC::INT_VAR_DEGREE_SIZE_MAX();
+    case BHV::minminreg: return GC::INT_VAR_REGRET_MIN_MIN();
+    case BHV::maxminreg: return GC::INT_VAR_REGRET_MIN_MAX();
+    case BHV::minmaxreg: return GC::INT_VAR_REGRET_MAX_MIN();
+    case BHV::maxmaxreg: return GC::INT_VAR_REGRET_MAX_MAX();
+    default : throw std::runtime_error("Options::var_branch: UNKNOWN bvar="
+                                       + std::to_string(int(bvar)));
+    }
+  }
+
+  // Values-section for Gecode-branching (also determining the branching-type
+  // "binary" vs "enumerativ"); for us "branching-heuristic order":
+  enum class BHO {
+    binvalmin = 1, // INT_VAL_MIN()
+    binvalmax = 2, // INT_VAL_MAX()
+    enumvalmin = 3, // INT_VALUES_MIN()
+    enumvalmax = 4 // INT_VALUES_MAX()
+  };
+  constexpr int BHOsize = int(BHO::enumvalmax) + 1;
+  GC::IntValBranch val_branch(const BHO bord) {
+    switch (bord) {
+    case BHO::binvalmin: return GC::INT_VAL_MIN();
+    case BHO::binvalmax: return GC::INT_VAL_MAX();
+    case BHO::enumvalmin: return GC::INT_VALUES_MIN();
+    case BHO::enumvalmax: return GC::INT_VALUES_MAX();
+    default : throw std::runtime_error("Options::val_branch: UNKNOWN bord="
+                                       + std::to_string(int(bord)));
+    }
+  }
+
 }
 namespace Environment {
   template <> struct RegistrationPolicies<Options::PropO> {
     static constexpr int size = Options::PropOsize;
     static constexpr std::array<const char*, size>
       string {"dom", "def", "val", "bnd"};
+  };
+  template <> struct RegistrationPolicies<Options::BHV> {
+    static constexpr int size = Options::BHVsize;
+    static constexpr std::array<const char*, size>
+    string {"first", "mindeg", "maxdeg", "mindom", "maxdom", "mindegdom",
+        "maxdegdom", "minminreg", "maxminreg", "minmaxreg", "maxmaxreg"};
+  };
+  template <> struct RegistrationPolicies<Options::BHO> {
+    static constexpr int size = Options::BHOsize;
+    static constexpr std::array<const char*, size>
+      string {"bvalmin", "bvalmax", "evalmin", "evalmax"};
   };
 }
 namespace Options {
@@ -49,16 +123,30 @@ namespace Options {
     case PropO::def : return out << "default-prop";
     case PropO::val : return out << "values-prop";
     case PropO::bnd : return out << "bounds-prop";
-    default : return out << "Options::PropP: UNKNOWN=" << int(m);}
+    default : return out << "Options::PropO: UNKNOWN=" << int(m);}
   }
-
-  GC::IntPropLevel prop_level(const PropO po) noexcept {
-    switch (po) {
-    case PropO::dom: return GC::IPL_DOM;
-    case PropO::def: return GC::IPL_DEF;
-    case PropO::val: return GC::IPL_VAL;
-    case PropO::bnd: return GC::IPL_BND;
-    default : return GC::IPL_DOM;}
+  std::ostream& operator <<(std::ostream& out, const BHV bvar) {
+    switch (bvar) {
+    case BHV::first: return out << "first-var";
+    case BHV::mindeg: return out << "min-deg-var";
+    case BHV::maxdeg: return out << "max-deg-var";
+    case BHV::mindom: return out << "min-dom-var";
+    case BHV::maxdom: return out << "max-dom-var";
+    case BHV::mindegdom: return out << "min-deg/dom-var";
+    case BHV::maxdegdom: return out << "max-deg/dom-var";
+    case BHV::minminreg: return out << "min-min-reg-var";
+    case BHV::maxminreg: return out << "max-min-reg-var";
+    case BHV::minmaxreg: return out << "min-max-reg-var";
+    case BHV::maxmaxreg: return out << "max-max-reg-var";
+    default : return out << "Options::BHV: UNKNOWN=" << int(bvar);}
+  }
+  std::ostream& operator <<(std::ostream& out, const BHO bord) {
+    switch (bord) {
+    case BHO::binvalmin: return out << "bin-branch-min";
+    case BHO::binvalmax: return out << "bin-branch-max";
+    case BHO::enumvalmin: return out << "enum-branch-min";
+    case BHO::enumvalmax: return out << "enum-branch-max";
+    default : return out << "Options::BHO: UNKNOWN=" << int(bord);}
   }
 
 }
