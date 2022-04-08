11518c6d845b475995905674d7617f6caf9ee616
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-04-08 23:09:29 +0100

    Started providing verification-functions.

--- Satisfiability/Solvers/Gecode/MOLS/TestVerification.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/TestVerification.cpp
@@ -16,7 +16,7 @@ License, or any later version. */
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.1",
+        "0.1.0",
         "8.4.2022",
         __FILE__,
         "Oliver Kullmann",
@@ -31,4 +31,28 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
 
+  {assert(orthogonal({},{}));
+   assert(orthogonal({{0}},{{0}}));
+   assert(orthogonal({{0,1},{1,0}},{{0,1},{0,1}}));
+   assert(not orthogonal({{0,1},{1,0}},{{1,0},{0,1}}));
+  }
+
+  {const ls_t A = {
+      {0,1,2,3,4,5,6},
+      {2,3,6,5,0,4,1},
+      {4,2,5,0,1,6,3},
+      {6,4,1,2,3,0,5},
+      {1,5,3,4,6,2,0},
+      {3,0,4,6,5,1,2},
+      {5,6,0,1,2,3,4}};
+    const ls_t tA = {
+      {0,2,4,6,1,3,5},
+      {1,3,2,4,5,0,6},
+      {2,6,5,1,3,4,0},
+      {3,5,0,2,4,6,1},
+      {4,0,1,3,6,5,2},
+      {5,4,6,0,2,1,3},
+      {6,1,3,5,0,2,4}};
+    assert(orthogonal(A,tA));
+  }
 }
--- Satisfiability/Solvers/Gecode/MOLS/Verification.hpp
+++ Satisfiability/Solvers/Gecode/MOLS/Verification.hpp
@@ -13,7 +13,40 @@ License, or any later version. */
 #ifndef VERIFICATION_CewvYixXoa
 #define VERIFICATION_CewvYixXoa
 
+#include <vector>
+#include <map>
+#include <utility>
+
+#include <Transformers/Generators/Random/LatinSquares.hpp>
+
+#include "Conditions.hpp"
+
 namespace Verification {
+
+  namespace RG = RandGen;
+  namespace CD = Conditions;
+
+  typedef Conditions::size_t size_t;
+
+  typedef std::vector<size_t> ls_row_t;
+  typedef std::vector<ls_row_t> ls_t;
+
+  bool orthogonal(const ls_t& ls1, const ls_t& ls2) {
+    const size_t N = ls1.size();
+    if (ls2.size() != N) return false;
+    typedef std::pair<size_t,size_t> p_t;
+    typedef std::map<p_t, size_t> m_t;
+    m_t m;
+    for (size_t i = 0; i < N; ++i) {
+      const ls_row_t& row1 = ls1[i];
+      if (row1.size() != N) return false;
+      const ls_row_t& row2 = ls2[i];
+      if (row2.size() != N) return false;
+      for (size_t j = 0; j < N; ++j)
+        if (++m[{row1[j], row2[j]}] == 2) return false;
+    }
+    return true;
+  }
 
 }
 
