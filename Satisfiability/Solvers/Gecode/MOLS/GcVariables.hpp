8dcb358573cd95f0369d4bf58104becf3f1e123a
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-05-11 15:14:07 +0100

    Selectively committed; such a file should always come with a testfile.

---
+++ Satisfiability/Solvers/Gecode/MOLS/GcVariables.hpp
@@ -0,0 +1,37 @@
+// Oleg Zaikin, 11.5.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+  Helper functions for dealing with Gecode variables.
+
+TODOS:
+
+*/
+
+#ifndef GCVARIABLES_FQM6cOCEcZ
+#define GCVARIABLES_FQM6cOCEcZ
+
+#include <vector>
+
+#include <gecode/int.hh>
+#include <gecode/search.hh>
+
+namespace GcVariables {
+
+  namespace GC = Gecode;
+
+  bool valid(const GC::IntVarArray& x) const noexcept {
+    return x.size() > 0;
+  }
+  bool valid(const GC::IntVarArray& x, const int i) const noexcept {
+    assert(valid(x) and i>=0);
+    return i<x.size();
+  }
+
+}
+
+#endif
