2298fe80d49d24af841ee4b8b1956da256f3a2ad
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2022-05-13 07:54:40 +0800

    Trivial brancher for testing.
    The brancher is posted in GenericMoldNB that is inherited from GenericMols0. It is checked that not branching is really performed in the brancher.

--- Satisfiability/Solvers/Gecode/MOLS/TestLookaheadReduction.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/TestLookaheadReduction.cpp
@@ -12,9 +12,9 @@ Testing of look-ahead reduction for the Gecode library.
 TODOS:
 
 -1.Testing lareduction is URGENTLY needed:
-  - DONE The space provided should be just GenericMols0.
-  - More precisely, a class say "GenericMolsNB" should be derived from
-    GenericMols0, with "NB" for "no branching", which posts a brancher
+  - DONE The space provided should be just GenericMolsNB.
+  - DONE More precisely, a class say "GenericMolsNB" should be derived from
+    GenericMolsNB, with "NB" for "no branching", which posts a brancher
     which immediately throws an exception -- this class is only to be
     used for testing.
 
@@ -29,7 +29,8 @@ TODOS:
 0. Tests should mostly used enumeration-modes:
   - So that also the satisfying assignments can be tested.
 
-2. Use a trivial brancher in unit tests.
+1. DONE (GenericMoldNB class with the trivial brancher is used now)
+   Use a trivial brancher in unit tests.
   - Since in Gecode branching must be posted to apply the status() function
     of a Space, a trivial customised brancher should be used to be sure
     that no branching is really done.
@@ -63,8 +64,8 @@ TODOS:
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.2.0",
-        "11.5.2022",
+        "0.2.1",
+        "12.5.2022",
         __FILE__,
         "Oleg Zaikin and Oliver Kullmann",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Solvers/Gecode/MOLS/TestLookaheadReduction.cpp",
@@ -81,6 +82,64 @@ namespace {
 
   namespace GC = Gecode;
 
+  // A trivial customised brancher that performs no branching.
+  // It is needed for testing propagation, since Gecode::Space::status()
+  // after propagation finds a brancher which status() function
+  // returns true. Here status() returns true if there is at least
+  // one unassigned variable.
+  class VoidBrancher : public GC::Brancher {
+    IntViewArray x;
+    mutable int firstunassign;
+  public:
+    VoidBrancher(const GC::Home home, const IntViewArray& x) :
+      GC::Brancher(home), x(x), firstunassign(0) {}
+    VoidBrancher(GC::Space& home, VoidBrancher& b)
+      : GC::Brancher(home,b), firstunassign(b.firstunassign) {
+      x.update(home, b.x);
+    }
+    static void post(GC::Home home, const IntViewArray& x) {
+      new (home) VoidBrancher(home, x);
+    }
+    virtual GC::Brancher* copy(GC::Space& home) {
+      return new (home) VoidBrancher(home, *this);
+    }
+    virtual bool status(const GC::Space&) const noexcept {
+      for (auto i = firstunassign; i < x.size(); ++i)
+        if (not x[i].assigned()) { firstunassign = i; return true; }
+      return false;
+    }
+    struct ModChoice : public GC::Choice {
+      ModChoice(const VoidBrancher& b)
+        : GC::Choice(b, 0) {}
+    };
+    // No branching is done, so neither choice() nor commit() are called:
+    virtual GC::Choice* choice(GC::Space&) {
+      assert(false);
+      return new ModChoice(*this);
+    }
+    virtual GC::Choice* choice(const GC::Space&, GC::Archive&) {
+      assert(false);
+      return new ModChoice(*this);
+    }
+    virtual GC::ExecStatus commit(GC::Space&, const GC::Choice&,
+                                  const unsigned) {
+      assert(false);
+      return GC::ES_OK;
+    }
+  };
+
+  inline void post_void_branching(GC::Home home, const GC::IntVarArgs& V)
+    noexcept {
+    const IntViewArray y(home, V);
+    VoidBrancher::post(home, y);
+  }
+
+  struct GenericMolsNB : GenericMols0 {
+    GenericMolsNB(const EncCond& enc) : GenericMols0(enc) {
+      post_void_branching(*this, V);
+    }
+  };
+
 }
 
 int main(const int argc, const char* const argv[]) {
@@ -94,15 +153,14 @@ int main(const int argc, const char* const argv[]) {
    const PSquares ps = PSquares(2, in_ps);
    const GC::IntPropLevel pl = GC::IPL_VAL;
    const EncCond enc(ac, ps, pl);
-   GenericMols0* const m = new GenericMols0(enc);
-   GC::branch(*m, m->V, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
+   GenericMolsNB* const m = new GenericMolsNB(enc);
    assert(m->status() == Gecode::SS_BRANCH);
    assert(m->V.size() == 4);
    assert(assignedvars(m->V) == 0);
    assert(sumdomsizes(m->V) == 8);
    // Post X[0] == 0:
-   std::unique_ptr<GenericMols0> ch =
-     child_node<GenericMols0>(m, 0, 0, pl, true);
+   std::unique_ptr<GenericMolsNB> ch =
+     child_node<GenericMolsNB>(m, 0, 0, pl, true);
    assert(ch->status() == Gecode::SS_BRANCH);
    assert(ch->V.size() == m->V.size());
    assert(assignedvars(ch->V) == 1);
@@ -110,8 +168,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[0] == 0:
    assert(assignedval(ch->V, 0) == 0);
    // Post X[0] != 0, so X[0] == 1:
-   std::unique_ptr<GenericMols0> ch2 =
-     child_node<GenericMols0>(m, 0, 0, pl, false);
+   std::unique_ptr<GenericMolsNB> ch2 =
+     child_node<GenericMolsNB>(m, 0, 0, pl, false);
    assert(ch2->status() == Gecode::SS_BRANCH);
    assert(ch2->V.size() == m->V.size());
    assert(assignedvars(ch2->V) == 1);
@@ -136,16 +194,14 @@ int main(const int argc, const char* const argv[]) {
    const PSquares ps = PSquares(2, in_ps);
    const GC::IntPropLevel pl = GC::IPL_VAL;
    const EncCond enc(ac, ps, pl);
-   GenericMols0* const m = new GenericMols0(enc);
-   GC::branch(*m, m->V, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
-   //GC::branch(*m, [](GC::Space&)->void{;}); // XXX ???
+   GenericMolsNB* const m = new GenericMolsNB(enc);
    assert(m->status() == Gecode::SS_BRANCH);
    assert(m->V.size() == 4);
    assert(assignedvars(m->V) == 0);
    assert(sumdomsizes(m->V) == 8);
    // Post X[0] == 0:
-   std::unique_ptr<GenericMols0> ch =
-     child_node<GenericMols0>(m, 0, 0, pl, true);
+   std::unique_ptr<GenericMolsNB> ch =
+     child_node<GenericMolsNB>(m, 0, 0, pl, true);
    assert(ch->status() == Gecode::SS_SOLVED);
    assert(ch->V.size() == m->V.size());
    assert(assignedvars(ch->V) == 4);
@@ -153,8 +209,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[0] == 0:
    assert(assignedval(ch->V,0) == 0);
    // Post X[0] != 0, so X[0] == 1:
-   std::unique_ptr<GenericMols0> ch2 =
-     child_node<GenericMols0>(m, 0, 0, pl, false);
+   std::unique_ptr<GenericMolsNB> ch2 =
+     child_node<GenericMolsNB>(m, 0, 0, pl, false);
    assert(ch2->status() == Gecode::SS_SOLVED);
    assert(ch2->V.size() == m->V.size());
    assert(assignedvars(ch2->V) == 4);
@@ -170,7 +226,7 @@ int main(const int argc, const char* const argv[]) {
    assert(assignedvars(m->V) == 0);
    assert(sumdomsizes(m->V) == 8);
    ReductionStatistics stat =
-     lareduction<GenericMols0>(m, RT::enumerate_solutions, GC::IPL_VAL,
+     lareduction<GenericMolsNB>(m, RT::enumerate_solutions, GC::IPL_VAL,
        LAR::eag_npr);
    assert(stat.props() == 1);
    assert(stat.elimvals() == 2);
@@ -190,15 +246,14 @@ int main(const int argc, const char* const argv[]) {
    const PSquares ps = PSquares(3, in_ps);
    const GC::IntPropLevel pl = GC::IPL_VAL;
    const EncCond enc(ac, ps, pl);
-   GenericMols0* const m = new GenericMols0(enc);
-   GC::branch(*m, m->V, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
+   GenericMolsNB* const m = new GenericMolsNB(enc);
    assert(m->status() == Gecode::SS_BRANCH);
    assert(m->V.size() == 9);
    assert(assignedvars(m->V) == 0);
    assert(sumdomsizes(m->V) == 27);
    // Post X[0] == 0:
-   std::unique_ptr<GenericMols0> ch =
-     child_node<GenericMols0>(m, 0, 0, pl, true);
+   std::unique_ptr<GenericMolsNB> ch =
+     child_node<GenericMolsNB>(m, 0, 0, pl, true);
    assert(ch->status() == Gecode::SS_BRANCH);
    assert(ch->V.size() == m->V.size());
    assert(assignedvars(ch->V) == 1);
@@ -206,8 +261,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[0] == 0:
    assert(assignedval(ch->V,0) == 0);
    // Post X[0] != 0:
-   std::unique_ptr<GenericMols0> ch2 =
-     child_node<GenericMols0>(m, 0, 0, pl, false);
+   std::unique_ptr<GenericMolsNB> ch2 =
+     child_node<GenericMolsNB>(m, 0, 0, pl, false);
    assert(ch2->status() == Gecode::SS_BRANCH);
    assert(ch2->V.size() == m->V.size());
    assert(assignedvars(ch2->V) == 0);
@@ -236,8 +291,7 @@ int main(const int argc, const char* const argv[]) {
    const PSquares ps = PSquares(3, in_ps);
    const GC::IntPropLevel pl = GC::IPL_VAL;
    const EncCond enc(ac, ps, pl);
-   GenericMols0* const m = new GenericMols0(enc);
-   GC::branch(*m, m->V, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
+   GenericMolsNB* const m = new GenericMolsNB(enc);
    assert(m->status() == Gecode::SS_BRANCH);
    assert(m->V.size() == 9);
    assert(assignedvars(m->V) == 1);
@@ -245,8 +299,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[0] == 0:
    assert(assignedval(m->V,0) == 0);
    // Post X[0] == 0:
-   std::unique_ptr<GenericMols0> ch =
-     child_node<GenericMols0>(m, 0, 0, pl, true);
+   std::unique_ptr<GenericMolsNB> ch =
+     child_node<GenericMolsNB>(m, 0, 0, pl, true);
    assert(ch->status() == Gecode::SS_BRANCH);
    assert(ch->V.size() == m->V.size());
    assert(assignedvars(ch->V) == 1);
@@ -254,8 +308,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[0] == 0:
    assert(assignedval(ch->V,0) == 0);
    // Post X[0] != 0:
-   std::unique_ptr<GenericMols0> ch2 =
-     child_node<GenericMols0>(m, 0, 0, pl, false);
+   std::unique_ptr<GenericMolsNB> ch2 =
+     child_node<GenericMolsNB>(m, 0, 0, pl, false);
    assert(ch2->status() == Gecode::SS_FAILED);
    assert(ch2->V.size() == m->V.size());
    assert(assignedvars(ch2->V) == 1);
@@ -284,9 +338,7 @@ int main(const int argc, const char* const argv[]) {
    const PSquares ps = PSquares(3, in_ps);
    const GC::IntPropLevel pl = GC::IPL_VAL;
    const EncCond enc(ac, ps, pl);
-   GenericMols0* const m = new GenericMols0(enc);
-   GC::branch(*m, m->V, GC::INT_VAR_SIZE_MIN(), GC::INT_VAL_MIN());
-   // XXX what is GC::branch doing? it seems inconsistent with GBO::asc ?
+   GenericMolsNB* const m = new GenericMolsNB(enc);
    assert(m->status() == Gecode::SS_BRANCH);
    assert(m->V.size() == 9);
    assert(assignedvars(m->V) == 1);
@@ -294,8 +346,8 @@ int main(const int argc, const char* const argv[]) {
    // Check X[4] == 1:
    assert(assignedval(m->V,4) == 1);
    // Post X[0] == 0:
-   std::unique_ptr<GenericMols0> ch =
-     child_node<GenericMols0>(m, 0, 0, pl, true);
+   std::unique_ptr<GenericMolsNB> ch =
+     child_node<GenericMolsNB>(m, 0, 0, pl, true);
    assert(ch->status() == Gecode::SS_SOLVED);
    assert(ch->V.size() == m->V.size());
    assert(assignedvars(ch->V) == 9);
@@ -304,8 +356,8 @@ int main(const int argc, const char* const argv[]) {
    assert(assignedval(ch->V,0) == 0);
    assert(assignedval(ch->V,4) == 1);
    // Post X[0] != 0:
-   std::unique_ptr<GenericMols0> ch2 =
-     child_node<GenericMols0>(m, 0, 0, pl, false);
+   std::unique_ptr<GenericMolsNB> ch2 =
+     child_node<GenericMolsNB>(m, 0, 0, pl, false);
    assert(ch2->status() == Gecode::SS_BRANCH);
    assert(ch2->V.size() == m->V.size());
    assert(assignedvars(ch2->V) == 1);
