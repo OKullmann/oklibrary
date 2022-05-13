fd424fae2a1965cc8ea051800fca9b217b4cf762
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2022-05-13 15:30:20 +0100

    Removed deprecated functions. Editorial.
    valid() functions are not needed because it is more natural to check the correctness directly. Also redundant headers were removed.

--- Satisfiability/Solvers/Gecode/MOLS/TestGcVariables.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/TestGcVariables.cpp
@@ -22,32 +22,24 @@ BUGS:
 
 #include <cassert>
 
+#include <gecode/int.hh>
+#include <gecode/search.hh>
+
 #include <ProgramOptions/Environment.hpp>
 
 #include "GcVariables.hpp"
-#include "Conditions.hpp"
-#include "Parsing.hpp"
-#include "PartialSquares.hpp"
-#include "Encoding.hpp"
-#include "Options.hpp"
-#include "Constraints.hpp"
 
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.1",
-        "11.5.2022",
+        "0.0.2",
+        "13.5.2022",
         __FILE__,
         "Oleg Zaikin",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Solvers/Gecode/MOLS/TestGcVariables.cpp",
         "GPL v3"};
 
   using namespace GcVariables;
-  using namespace Conditions;
-  using namespace Parsing;
-  using namespace PartialSquares;
-  using namespace Encoding;
-  using namespace Constraints;
 
   namespace GC = Gecode;
 }
@@ -56,22 +48,4 @@ namespace {
 int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
-
-  {// An empty square or order 2, four variables, each with domain {0,1}:
-   std::istringstream in_cond("squares A\n");
-   std::istringstream in_ps("");
-   const AConditions ac = ReadAC()(in_cond);
-   const PSquares ps = PSquares(2, in_ps);
-   const EncCond enc(ac, ps, GC::IPL_VAL);
-   GenericMols0* const m = new GenericMols0(enc);
-   assert(valid(m->V));
-   assert(m->V.size() == 4);
-   assert(valid(m->V,0));
-   assert(valid(m->V,1));
-   assert(valid(m->V,2));
-   assert(valid(m->V,3));
-   assert(not valid(m->V,4));
-   delete m;
-  }
-
 }
