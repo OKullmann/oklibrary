23bdf0ba1a4180419d7416f33351927c4e5cb9ea
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2022-05-23 14:52:36 +0100

    Started Cases.
    Database of test cases for lookahead reduction and solving.

---
+++ Satisfiability/Solvers/Gecode/MOLS/Cases.hpp
@@ -0,0 +1,42 @@
+// Oleg Zaikin, 23.5.2022 (Swansea)
+/* Copyright 2022 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+Test cases for lookahead-reduction and solvers.
+
+BUG:
+
+TODOS:
+
+0. Add cases needed for testing lookahead-reduction.
+  - Square.
+  - Trivial (empty) Latin square.
+
+*/
+
+#include <cassert>
+
+#ifndef CASES_tqVXGkU1YS
+#define CASES_tqVXGkU1YS
+
+#include <gecode/int.hh>
+#include <gecode/search.hh>
+
+namespace Cases {
+
+  struct Square {
+    // XXX
+  };
+
+  struct TrivialLatinSquare {
+    // XXX
+  };
+
+};
+
+#endif
