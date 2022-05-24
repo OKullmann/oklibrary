5f377211e47ae00d0613117aa4329fb1ba0e9952
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2022-05-24 15:39:14 +0100

    Propagation level loops for child_node() tests.

--- Satisfiability/Solvers/Gecode/MOLS/TestLookaheadReduction.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/TestLookaheadReduction.cpp
@@ -79,7 +79,7 @@ TODOS:
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.5.1",
+        "0.5.2",
         "24.5.2022",
         __FILE__,
         "Oleg Zaikin and Oliver Kullmann",
@@ -132,108 +132,120 @@ int main(const int argc, const char* const argv[]) {
 
   {const CS::TrivialLatinSquare<2> A;
    const std::unique_ptr<CS::GenericMolsNB> m = A.space();
-   const auto pl = GC::IPL_VAL;
-   const auto ch = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, true);
-   assert(eqp(values(ch->V, 0), {0}));
-   assert(eqp(values(ch->V, 1), {0,1}));
-   assert(eqp(values(ch->V, 2), {0,1}));
-   assert(eqp(values(ch->V, 3), {0,1}));
-   const auto st = ch->status();
-   assert(st == Gecode::SS_SOLVED);
-   const auto ch2 = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, false);
-   assert(eqp(values(ch2->V, 0), {1}));
-   assert(eqp(values(ch2->V, 1), {0,1}));
-   assert(eqp(values(ch2->V, 2), {0,1}));
-   assert(eqp(values(ch2->V, 3), {0,1}));
-   const auto st2 = ch2->status();
-   assert(st2 == Gecode::SS_SOLVED);
+   for (const PropO po : {PropO::dom, PropO::def, PropO::val, PropO::bnd}) {
+     const auto ch =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), true);
+     assert(eqp(values(ch->V, 0), {0}));
+     assert(eqp(values(ch->V, 1), {0,1}));
+     assert(eqp(values(ch->V, 2), {0,1}));
+     assert(eqp(values(ch->V, 3), {0,1}));
+     const auto st = ch->status();
+     assert(st == Gecode::SS_SOLVED);
+     const auto ch2 =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), false);
+     assert(eqp(values(ch2->V, 0), {1}));
+     assert(eqp(values(ch2->V, 1), {0,1}));
+     assert(eqp(values(ch2->V, 2), {0,1}));
+     assert(eqp(values(ch2->V, 3), {0,1}));
+     const auto st2 = ch2->status();
+     assert(st2 == Gecode::SS_SOLVED);
+   }
   }
 
   {const CS::Square<3> A;
    const std::unique_ptr<CS::GenericMolsNB> m = A.space();
-   const auto pl = GC::IPL_VAL;
-   const auto ch = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, true);
-   assert(eqp(values(ch->V, 0), {0}));
-   assert(eqp(values(ch->V, 1), {0,1,2}));
-   assert(eqp(values(ch->V, 2), {0,1,2}));
-   assert(eqp(values(ch->V, 3), {0,1,2}));
-   assert(eqp(values(ch->V, 4), {0,1,2}));
-   assert(eqp(values(ch->V, 5), {0,1,2}));
-   assert(eqp(values(ch->V, 6), {0,1,2}));
-   assert(eqp(values(ch->V, 7), {0,1,2}));
-   assert(eqp(values(ch->V, 8), {0,1,2}));
-   const auto st = ch->status();
-   assert(st == Gecode::SS_BRANCH);
-   const auto ch2 = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, false);
-   assert(eqp(values(ch2->V, 0), {1,2}));
-   assert(eqp(values(ch2->V, 1), {0,1,2}));
-   assert(eqp(values(ch2->V, 2), {0,1,2}));
-   assert(eqp(values(ch2->V, 3), {0,1,2}));
-   assert(eqp(values(ch2->V, 4), {0,1,2}));
-   assert(eqp(values(ch2->V, 5), {0,1,2}));
-   assert(eqp(values(ch2->V, 6), {0,1,2}));
-   assert(eqp(values(ch2->V, 7), {0,1,2}));
-   assert(eqp(values(ch2->V, 8), {0,1,2}));
-   const auto st2 = ch2->status();
-   assert(st2 == Gecode::SS_BRANCH);
+   for (const PropO po : {PropO::dom, PropO::def, PropO::val, PropO::bnd}) {
+     const auto ch =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), true);
+     assert(eqp(values(ch->V, 0), {0}));
+     assert(eqp(values(ch->V, 1), {0,1,2}));
+     assert(eqp(values(ch->V, 2), {0,1,2}));
+     assert(eqp(values(ch->V, 3), {0,1,2}));
+     assert(eqp(values(ch->V, 4), {0,1,2}));
+     assert(eqp(values(ch->V, 5), {0,1,2}));
+     assert(eqp(values(ch->V, 6), {0,1,2}));
+     assert(eqp(values(ch->V, 7), {0,1,2}));
+     assert(eqp(values(ch->V, 8), {0,1,2}));
+     const auto st = ch->status();
+     assert(st == Gecode::SS_BRANCH);
+     const auto ch2 =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), false);
+     assert(eqp(values(ch2->V, 0), {1,2}));
+     assert(eqp(values(ch2->V, 1), {0,1,2}));
+     assert(eqp(values(ch2->V, 2), {0,1,2}));
+     assert(eqp(values(ch2->V, 3), {0,1,2}));
+     assert(eqp(values(ch2->V, 4), {0,1,2}));
+     assert(eqp(values(ch2->V, 5), {0,1,2}));
+     assert(eqp(values(ch2->V, 6), {0,1,2}));
+     assert(eqp(values(ch2->V, 7), {0,1,2}));
+     assert(eqp(values(ch2->V, 8), {0,1,2}));
+     const auto st2 = ch2->status();
+     assert(st2 == Gecode::SS_BRANCH);
+   }
   }
 
   {const CS::Square<3> A("A\n0 * *\n* * *\n* * *\n");
    const std::unique_ptr<CS::GenericMolsNB> m = A.space();
-   const auto pl = GC::IPL_VAL;
-   const auto ch = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, true);
-   assert(eqp(values(ch->V, 0), {0}));
-   assert(eqp(values(ch->V, 1), {0,1,2}));
-   assert(eqp(values(ch->V, 2), {0,1,2}));
-   assert(eqp(values(ch->V, 3), {0,1,2}));
-   assert(eqp(values(ch->V, 4), {0,1,2}));
-   assert(eqp(values(ch->V, 5), {0,1,2}));
-   assert(eqp(values(ch->V, 6), {0,1,2}));
-   assert(eqp(values(ch->V, 7), {0,1,2}));
-   assert(eqp(values(ch->V, 8), {0,1,2}));
-   const auto st = ch->status();
-   assert(st == Gecode::SS_BRANCH);
-   const auto ch2 = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, false);
-   assert(eqp(values(ch2->V, 0), {0}));
-   assert(eqp(values(ch2->V, 1), {0,1,2}));
-   assert(eqp(values(ch2->V, 2), {0,1,2}));
-   assert(eqp(values(ch2->V, 3), {0,1,2}));
-   assert(eqp(values(ch2->V, 4), {0,1,2}));
-   assert(eqp(values(ch2->V, 5), {0,1,2}));
-   assert(eqp(values(ch2->V, 6), {0,1,2}));
-   assert(eqp(values(ch2->V, 7), {0,1,2}));
-   assert(eqp(values(ch2->V, 8), {0,1,2}));
-   const auto st2 = ch2->status();
-   assert(st2 == Gecode::SS_FAILED);
+   for (const PropO po : {PropO::dom, PropO::def, PropO::val, PropO::bnd}) {
+     const auto ch =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), true);
+     assert(eqp(values(ch->V, 0), {0}));
+     assert(eqp(values(ch->V, 1), {0,1,2}));
+     assert(eqp(values(ch->V, 2), {0,1,2}));
+     assert(eqp(values(ch->V, 3), {0,1,2}));
+     assert(eqp(values(ch->V, 4), {0,1,2}));
+     assert(eqp(values(ch->V, 5), {0,1,2}));
+     assert(eqp(values(ch->V, 6), {0,1,2}));
+     assert(eqp(values(ch->V, 7), {0,1,2}));
+     assert(eqp(values(ch->V, 8), {0,1,2}));
+     const auto st = ch->status();
+     assert(st == Gecode::SS_BRANCH);
+     const auto ch2 =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), false);
+     assert(eqp(values(ch2->V, 0), {0}));
+     assert(eqp(values(ch2->V, 1), {0,1,2}));
+     assert(eqp(values(ch2->V, 2), {0,1,2}));
+     assert(eqp(values(ch2->V, 3), {0,1,2}));
+     assert(eqp(values(ch2->V, 4), {0,1,2}));
+     assert(eqp(values(ch2->V, 5), {0,1,2}));
+     assert(eqp(values(ch2->V, 6), {0,1,2}));
+     assert(eqp(values(ch2->V, 7), {0,1,2}));
+     assert(eqp(values(ch2->V, 8), {0,1,2}));
+     const auto st2 = ch2->status();
+     assert(st2 == Gecode::SS_FAILED);
+   }
   }
 
   {const CS::Square<3> A("A\n* * *\n* 1 *\n* * *\n");
    const std::unique_ptr<CS::GenericMolsNB> m = A.space();
-   const auto pl = GC::IPL_VAL;
-   const auto ch = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, true);
-   assert(eqp(values(ch->V, 0), {0}));
-   assert(eqp(values(ch->V, 1), {0,1,2}));
-   assert(eqp(values(ch->V, 2), {0,1,2}));
-   assert(eqp(values(ch->V, 3), {0,1,2}));
-   assert(eqp(values(ch->V, 4), {1}));
-   assert(eqp(values(ch->V, 5), {0,1,2}));
-   assert(eqp(values(ch->V, 6), {0,1,2}));
-   assert(eqp(values(ch->V, 7), {0,1,2}));
-   assert(eqp(values(ch->V, 8), {0,1,2}));
-   const auto st = ch->status();
-   assert(st == Gecode::SS_BRANCH);
-   const auto ch2 = child_node<CS::GenericMolsNB>(m.get(), 0, 0, pl, false);
-   assert(eqp(values(ch2->V, 0), {1,2}));
-   assert(eqp(values(ch2->V, 1), {0,1,2}));
-   assert(eqp(values(ch2->V, 2), {0,1,2}));
-   assert(eqp(values(ch2->V, 3), {0,1,2}));
-   assert(eqp(values(ch2->V, 4), {1}));
-   assert(eqp(values(ch2->V, 5), {0,1,2}));
-   assert(eqp(values(ch2->V, 6), {0,1,2}));
-   assert(eqp(values(ch2->V, 7), {0,1,2}));
-   assert(eqp(values(ch2->V, 8), {0,1,2}));
-   const auto st2 = ch2->status();
-   assert(st2 == Gecode::SS_BRANCH);
+   for (const PropO po : {PropO::dom, PropO::def, PropO::val, PropO::bnd}) {
+     const auto ch =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), true);
+     assert(eqp(values(ch->V, 0), {0}));
+     assert(eqp(values(ch->V, 1), {0,1,2}));
+     assert(eqp(values(ch->V, 2), {0,1,2}));
+     assert(eqp(values(ch->V, 3), {0,1,2}));
+     assert(eqp(values(ch->V, 4), {1}));
+     assert(eqp(values(ch->V, 5), {0,1,2}));
+     assert(eqp(values(ch->V, 6), {0,1,2}));
+     assert(eqp(values(ch->V, 7), {0,1,2}));
+     assert(eqp(values(ch->V, 8), {0,1,2}));
+     const auto st = ch->status();
+     assert(st == Gecode::SS_BRANCH);
+     const auto ch2 =
+       child_node<CS::GenericMolsNB>(m.get(), 0, 0, prop_level(po), false);
+     assert(eqp(values(ch2->V, 0), {1,2}));
+     assert(eqp(values(ch2->V, 1), {0,1,2}));
+     assert(eqp(values(ch2->V, 2), {0,1,2}));
+     assert(eqp(values(ch2->V, 3), {0,1,2}));
+     assert(eqp(values(ch2->V, 4), {1}));
+     assert(eqp(values(ch2->V, 5), {0,1,2}));
+     assert(eqp(values(ch2->V, 6), {0,1,2}));
+     assert(eqp(values(ch2->V, 7), {0,1,2}));
+     assert(eqp(values(ch2->V, 8), {0,1,2}));
+     const auto st2 = ch2->status();
+     assert(st2 == Gecode::SS_BRANCH);
+   }
   }
 
   {const CS::Square<2> A;
