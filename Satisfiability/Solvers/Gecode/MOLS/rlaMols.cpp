7cdf8298ec93f70aaac6d7700f706b747539592b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2022-06-09 20:15:01 +0100

    More thoughs on output of counts.

--- Satisfiability/Solvers/Gecode/MOLS/rlaMols.cpp
+++ Satisfiability/Solvers/Gecode/MOLS/rlaMols.cpp
@@ -29,6 +29,11 @@ Compare also with todos in gcMols.
    - On the one hand, it is informative to see how many solutions per node
      are obtained.
    - On the other hand, one also wants to see the total count.
+   - One could have an output-option for switching between the two
+     possibilities.
+   - Or one always outputs the total count T, and outputs the local count L
+     additionally iff it is > 1, and the form
+       T:L.
 
 -1. Better time-information
    - The reduction-time is sum of user-times for all running threads.
