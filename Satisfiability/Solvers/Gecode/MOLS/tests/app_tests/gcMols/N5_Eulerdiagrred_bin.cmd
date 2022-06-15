8a3ec67953d893939f75c0cee41c15cf043191e3
Oleg Zaikin <o.s.zaikin@swansea.ac.uk>
2022-06-15 08:12:59 +0800

    Enabled app tests.
    More flexible regular expressions according to the discussion.

--- Satisfiability/Solvers/Gecode/MOLS/tests/app_tests/gcMols/N5_Eulerdiagrred_bin.cmd
+++ Satisfiability/Solvers/Gecode/MOLS/tests/app_tests/gcMols/N5_Eulerdiagrred_bin.cmd
@@ -1,1 +1,1 @@
-5 "@squares A B aux\nrred A B\nls A B aux\nrprod A aux B\ndiag A B\nantidiag A B\n" "" satd dom bin mindom desc 1 1
+5 "@squares A B aux\nrred A B\nls A B aux\nrprod A aux B\ndiag A B\nantidiag A B\n" "" satd dom bin mindom desc 1 1 ""
