7c5d9a189a63036d30c6154c85d73869836c5837
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2023-03-26 10:44:33 +0100

    Unit-tests (forgotten with penultimate commit).

---
+++ Satisfiability/Transformers/Generators/Bicliques/TestAlgorithms.cpp
@@ -0,0 +1,69 @@
+// Oliver Kullmann, 26.3.2022 (Swansea)
+/* Copyright 2023 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+#include <cassert>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include <Transformers/Generators/Random/Numbers.hpp>
+#include <Transformers/Generators/Random/Distributions.hpp>
+
+#include "Algorithms.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.6",
+        "25.3.2023",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/TestAlgorithms.cpp",
+        "GPL v3"};
+
+  using namespace Algorithms;
+
+  template <class X>
+  constexpr bool eqp(const X& lhs, const X& rhs) noexcept {
+    return lhs == rhs;
+  }
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv))
+  return 0;
+
+  {typedef std::vector<int> v_t;
+   assert(empty_intersection(v_t{}, v_t{}));
+   assert(empty_intersection(v_t{1}, v_t{}));
+   assert(empty_intersection(v_t{}, v_t{1}));
+   assert(not empty_intersection(v_t{1}, v_t{1}));
+   assert(not empty_intersection(v_t{1}, v_t{0,1,2}));
+   assert(not empty_intersection(v_t{0,1,2}, v_t{1}));
+   const auto empty_int = [](const v_t& v1, const v_t& v2){
+     v_t res;
+     std::ranges::set_intersection(v1, v2, std::back_inserter(res));
+     return res.empty();
+   };
+   RandGen::RandGen_t g({77});
+   const auto size_dist = RandGen::UniformRange(g, 10);
+   const auto val1_dist = RandGen::UniformRange(g, 10);
+   const auto val2_dist = RandGen::UniformRange(g, 10, 5);
+   for (unsigned i = 0; i < 10000; ++i) {
+     const auto N1 = size_dist(); const auto N2 = size_dist();
+     v_t v1; v1.reserve(N1); v_t v2; v2.reserve(N2);
+     for (unsigned j = 0; j < N1; ++j) v1.push_back(val1_dist());
+     for (unsigned j = 0; j < N2; ++j) v2.push_back(val2_dist());
+     assert(empty_int(v1, v2) == empty_intersection(v1, v2));
+   }
+  }
+
+}
