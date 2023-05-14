6700c6a578f369761100704c864e220bab90ddc0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2023-05-14 11:23:44 +0100

    Now one can run the algorithms a given number of times.

--- Satisfiability/Transformers/Generators/Bicliques/MaxIndependentGreedy.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/MaxIndependentGreedy.cpp
@@ -14,63 +14,80 @@ EXAMPLES:
 
 For a biclique with two equal sides, the maximum independent sets are
 exactly the two sides, and the simple greedy approach finds them:
-Bicliques> ./GraphGen biclique 6 6 | ./MaxIndependentGreedy_debug "" 0
+Bicliques> ./GraphGen biclique 6 6 | ./MaxIndependentGreedy_debug "" 0 ""
 # "./MaxIndependentGreedy_debug" "" "0"
 # comments-result 1 1
 # num_e-seeds 1
 # e-seeds 0
-# V 12
-# E 36
-# result-size 6
+# T 1
+#  V 12
+#  E 36
+#  result-size 6
 l5 l6 l3 l1 l4 l2
-Bicliques> ./GraphGen biclique 6 6 | ./MaxIndependentGreedy_debug -com 4
+Bicliques> ./GraphGen biclique 6 6 | ./MaxIndependentGreedy_debug -com 4 ""
 r4 r5 r2 r1 r6 r3
 
 For the biclique-compatability-graphs of grids, it seems this simple
 lower bound for bcc is easily exact when just using a relatively
 small number of runs:
 
-Bicliques> time ./GraphGen grid 100 100 | ./Graph2BCcompGraph | ./MaxIndependentGreedy -sol 18
+Bicliques> time ./GraphGen grid 100 100 | ./Graph2BCcompGraph | ./MaxIndependentGreedy -sol 18 ""
 # "./MaxIndependentGreedy" "-sol" "18"
 # comments-result 1 0
 # num_e-seeds 1
 # e-seeds 18
-# V 19800
-# E 78406
-# result-size 4999
+# T 1
+#  V 19800
+#  E 78406
+#  result-size 4999
 real	0m2.920s
 user	0m2.934s
 sys	0m0.021s
-Bicliques> time ./GraphGen grid 200 200 | ./Graph2BCcompGraph | ./MaxIndependentGreedy -sol 18
+Bicliques> time ./GraphGen grid 200 200 | ./Graph2BCcompGraph | ./MaxIndependentGreedy -sol 18 ""
 # "./MaxIndependentGreedy" "-sol" "18"
 # comments-result 1 0
 # num_e-seeds 1
 # e-seeds 18
-# V 79600
-# E 316806
-# result-size 19999
+# T 1
+#  V 79600
+#  E 316806
+#  result-size 19999
 real	0m31.440s
 user	0m31.551s
 sys	0m0.041s
 
+Bicliques> time ./GraphGen grid 300 300 | ./Graph2BCcompGraph | ./MaxIndependentGreedy -sol "" 1000
+# "./MaxIndependentGreedy" "-sol" "" "1000"
+# comments-result 1 0
+# num_e-seeds 0
+# T 1000
+#  V 179400
+#  E 715206
+#  result-sizes 1000 : 44851 44929.1 44999; 47.382
+#  seed 18
+real	3m57.414s
+user	3m57.635s
+sys	0m0.121s
 
 */
 
 #include <iostream>
+#include <tuple>
 
 #include <ProgramOptions/Environment.hpp>
 #include <Transformers/Generators/Random/Numbers.hpp>
+#include <Numerics/NumInOut.hpp>
+#include <Numerics/Statistics.hpp>
 
 #include "Graphs.hpp"
-#include "Bicliques2SAT.hpp"
 
 #include "BCC2SAT.hpp"
 
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.2.0",
-        "13.5.2023",
+        "0.3.0",
+        "14.5.2023",
         __FILE__,
         "Oliver Kullmann",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/MaxIndependentGreedy.cpp",
@@ -87,14 +104,43 @@ namespace {
     if (not Environment::help_header(std::cout, argc, argv, proginfo))
       return false;
     std::cout <<
-    "> " << proginfo.prg << " format-options seeds \n\n"
+    "> " << proginfo.prg << " format-options seeds T \n\n"
     " format-options : " << Environment::WRP<DC>{} << "\n"
     "                : " << Environment::WRP<BC>{} << "\n"
-    " seeds          : " << "sequence, can contain \"t\" or \"r\"" << "\n\n"
+    " seeds          : " << "sequence, can contain \"t\" or \"r\"\n"
+    " T              : " << "number of trials, natural number >= 0, default 1"
+                         << "\n\n"
     " reads a graph from standard input, and prints a maximal vertex-set to standard output:\n\n"
-    "  - Arguments \"\" (the empty string) yield the default-values.\n\n"
+    "  - Arguments \"\" (the empty string) yield the default-values.\n"
+    "  - T can be an arbitrary 80-bit floating-point (rounding is applied).\n\n"
 ;
     return true;
+  }
+
+  typedef GenStats::BasicStats<FloatingPoint::float80, FloatingPoint::float80> stats_t;
+  std::tuple<AdjVecUInt::list_t, stats_t, id_t>
+  perform_trials(const AdjVecUInt& G,
+                 RandGen::vec_eseed_t seeds,
+                 const id_t T) {
+    assert(T != 0);
+    if (T == 1)
+      return {maximal_independent_greedy_simplest(G, seeds), {}, {}};
+    else {
+      AdjVecUInt::list_t res;
+      stats_t S;
+      id_t opti = 0;
+      seeds.push_back(opti);
+      for (id_t i = 0; i < T; ++i) {
+        seeds.back() = i;
+        AdjVecUInt::list_t I = maximal_independent_greedy_simplest(G, seeds);
+        S += I.size();
+        if (I.size() > res.size()) {
+          res = std::move(I);
+          opti = i;
+        }
+      }
+      return {res,S,opti};
+    }
   }
 
 }
@@ -104,16 +150,19 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
   if (show_usage(argc, argv)) return 0;
 
-  if (argc != 3) {
+  if (argc != 4) {
     std::cerr << error <<
-      "Exactly two arguments (form-opt, seeds)"
+      "Exactly two arguments (form-opt, seeds, T)"
       " needed, but " << argc-1 << " provided.\n";
-    return int(Error::missing_parameters);
+    return int(argc > 4 ? Error::too_many_parameters :
+                          Error::missing_parameters);
   }
 
   const format2_options_t formopt =
     Environment::translate<format2_options_t>()(argv[1], sep);
   const RandGen::vec_eseed_t seeds = RandGen::extract_seeds(argv[2]);
+  const std::string Tstring = argv[3];
+  const id_t T = Tstring.empty() ? 1 : FloatingPoint::toUInt(argv[3]);
 
   const bool with_comments = std::get<DC>(formopt) == DC::with;
   const bool with_result = std::get<BC>(formopt) == BC::with;
@@ -124,25 +173,32 @@ int main(const int argc, const char* const argv[]) {
               << " " << with_result << "\n"
               << comment << "num_e-seeds " << seeds.size() << "\n";
     if (not seeds.empty())
-      std::cout << comment << "e-seeds " << RandGen::ESW{seeds} << std::endl;
+      std::cout << comment << "e-seeds " << RandGen::ESW{seeds} << "\n";
+    std::cout << comment << "T " << T << std::endl;
   }
 
   const auto G = make_AdjVecUInt(std::cin, GT::und);
 
   if (with_comments) {
-    std::cout << comment << "V " << G.n() << "\n"
-              << comment << "E " << G.m() << std::endl;
+    std::cout << comment << " V " << G.n() << "\n"
+              << comment << " E " << G.m() << std::endl;
   }
 
-  const auto I = maximal_independent_greedy_simplest(G, seeds);
+  const auto [I,S,opti] = perform_trials(G, seeds, T);
+  if (T == 0) return 0;
 
   if (with_comments) {
-    std::cout << comment << "result-size " << I.size() << "\n";
+    if (T == 1)
+      std::cout << comment << " result-size " << I.size() << "\n";
+    else
+      std::cout << comment << " result-sizes " << S << "\n"
+                << comment << " seed " << opti << "\n";
   }
+  std::cout.flush();
 
   if (with_result) {
     G.output(std::cout, I);
-    std::cout << std::endl;
+    std::cout << "\n";
   }
 
 }
