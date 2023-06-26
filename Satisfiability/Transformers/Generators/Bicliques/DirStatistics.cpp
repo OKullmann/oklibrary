8d0c99aaf06bf0bce806093411c28ef85609967b
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2023-06-26 22:09:33 +0100

    Started program computing statistics for QBF2BCC.

---
+++ Satisfiability/Transformers/Generators/Bicliques/DirStatistics.cpp
@@ -0,0 +1,59 @@
+// Oliver Kullmann, 26.6.2023 (Swansea)
+/* Copyright 2023 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+*/
+
+#include <iostream>
+
+#include <ProgramOptions/Environment.hpp>
+
+#include "DirStatistics.hpp"
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.4",
+        "26.6.2023",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/DirStatistics.cpp",
+        "GPL v3"};
+
+  using namespace DirStatistics;
+
+  const std::string error = "ERROR[" + proginfo.prg + "]: ";
+
+  bool show_usage(const int argc, const char* const argv[]) {
+    if (not Environment::help_header(std::cout, argc, argv, proginfo))
+      return false;
+    std::cout <<
+    "> " << proginfo.prg
+         << " dirname\n\n"
+    " computes statistics for the corpus QBF2BCC.\n\n"
+;
+    return true;
+  }
+
+}
+
+int main(const int argc, const char* const argv[]) {
+
+  if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
+  if (show_usage(argc, argv)) return 0;
+
+  if (argc != 2) {
+    std::cerr << error <<
+      "Exactly one argument (dirname)"
+      " needed, but " << argc-1 << " provided.\n";
+    return int(Error::missing_parameters);
+  }
+
+  const std::string dirname = argv[1];
+
+}
---
+++ Satisfiability/Transformers/Generators/Bicliques/DirStatistics.hpp
@@ -0,0 +1,27 @@
+// Oliver Kullmann, 26.6.2023 (Swansea)
+/* Copyright 2023 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+  General definitions for processing the corpus QBF2BCC
+
+*/
+
+#ifndef DIRSTATISTICS_gDRJRMWsir
+#define DIRSTATISTICS_gDRJRMWsir
+
+#include <filesystem>
+
+namespace DirStatistics {
+
+  enum class Error {
+    missing_parameters = 1,
+  };
+
+}
+
+#endif
