b10a19fbe4b7df7b77c89dbaac8b40fb2cd174f2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2023-11-06 10:32:52 +0000

    Optionally storing the isomorphism.

--- Satisfiability/Transformers/Generators/Bicliques/TotalPermutation.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/TotalPermutation.cpp
@@ -21,18 +21,31 @@ p cnf 5 3
 -4 -2 3 -5 0
 4 0
 
+With showing the mapping old-variable -> new literal:
+Bicliques> echo -e "p cnf 5 3\n1 2 -3 0\n2 3 -5 -1 0\n-2 0\n" | ./TotalPermutation OUT
+Bicliques> cat OUT
+1 5
+2 -4
+3 -2
+4 1
+5 -3
+
 Bicliques> echo -e "p cnf 6 3\n1 2 -3 0\n2 3 -5 -1 0\n-2 0\n" | ./TotalPermutation
 c 6 3 17204549642661511693 5128273459892383319
 p cnf 6 3
 -4 -2 -1 0
 1 0
 -6 -1 4 2 0
+Bicliques> echo -e "p cnf 6 3\n1 2 -3 0\n2 3 -5 -1 0\n-2 0\n" | ./TotalPermutation OUT
+1 -2
+2 -1
+3 4
+4 -3
+5 6
+6 5
 
 
 TODOS:
-
-1. If an argument is given, then this is the filename showing the
-   isomorphism.
 
 2. This should go to Generators/Random.
     - Then generalise to non-strict inputs.
@@ -55,7 +68,7 @@ TODOS:
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.9",
+        "0.1.0",
         "6.11.2023",
         __FILE__,
         "Oliver Kullmann",
@@ -71,11 +84,17 @@ namespace {
       return false;
     std::cout <<
     "> " << proginfo.prg
-         << "\n\n"
+         << " [assignment-file]\n\n"
     " reads a strict Dimacs-file from standard input, and prints the"
     " permuted CNF to standard output.\n\n"
 ;
     return true;
+  }
+
+
+  std::string assignment_file(const int argc, const char* const argv[]) {
+    if (argc >= 2) return argv[1];
+    else return "";
   }
 
   RandGen::vec_eseed_t seeds(const DimacsClauseList& F) noexcept {
@@ -140,12 +159,14 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
   if (show_usage(argc, argv)) return 0;
 
-  if (argc != 1) {
+  if (argc != 1 and argc != 2) {
     std::cerr << error <<
-      "No arguments needed, but " << argc-1 << " provided.\n";
+      "At most one arguments needed ([assignment-file]), but "
+              << argc-1 << " provided.\n";
     return 1;
   }
 
+  const std::string assignmentfile = assignment_file(argc, argv);
   DimacsClauseList F = read_strict_Dimacs(std::cin);
   const auto s = seeds(F);
   RandGen::RandGen_t g(s);
@@ -154,4 +175,20 @@ int main(const int argc, const char* const argv[]) {
   const varvec_t V = random_variables(g, F.first.n);
   const signvec_t S = random_signs(g, F.first.n);
   output(std::cout, F, s, g, V, S);
+  if (not assignmentfile.empty()) {
+    std::ofstream out(assignmentfile);
+    for (RandGen::gen_uint_t i = 0; i < F.first.n; ++i) {
+      const RandGen::gen_uint_t oldv = i+1, v = V[i];
+      out << oldv << " ";
+      assert(S[i] == 1 or S[i] == -1);
+      if (S[i] == -1) out << "-";
+      out << v << "\n";
+    }
+    out.close();
+    if (not out) {
+      std::cerr << error <<
+        "Writing problem to file \"" << assignmentfile << "\".\n";
+      return 1;
+    }
+  }
 }
