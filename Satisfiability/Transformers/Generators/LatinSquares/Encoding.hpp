c706f12e4aac92131675daf6c205c262d6eae963
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-02-12 20:53:04 +0000

    Editorial.

--- Satisfiability/Transformers/Generators/LatinSquares/Encoding.hpp
+++ Satisfiability/Transformers/Generators/LatinSquares/Encoding.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 4.8.2020 (Swansea)
-/* Copyright 2020 Oliver Kullmann
+/* Copyright 2020, 2024 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -77,7 +77,11 @@ namespace Encoding {
     mutable Statistics::var_t next = nvc.n0;
   public :
 
-    constexpr VarEncoding(const Statistics::Param ps, const Options::SymP s, const Options::EAloP e, const Options::EulP eul, const Options::PrimeP prim) noexcept : N(ps.N), k(ps.k), nvc(numvarscls(ps,s,e,eul,prim)), symopt(s), ealoopt(e), eulopt(eul), primopt(prim) {}
+    constexpr VarEncoding(const Statistics::Param ps, const Options::SymP s,
+                          const Options::EAloP e, const Options::EulP eul,
+                          const Options::PrimeP prim) noexcept :
+    N(ps.N), k(ps.k), nvc(numvarscls(ps,s,e,eul,prim)), symopt(s), ealoopt(e),
+    eulopt(eul), primopt(prim) {}
 
     void nls(std::ostream& out) const {
       if (symopt == Options::SymP::full)
@@ -154,7 +158,11 @@ namespace Encoding {
       return ++next;
     }
 
-    constexpr Statistics::var_t operator()(const Statistics::dim_t i, const Statistics::dim_t j, const Statistics::dim_t eps, const Statistics::dim_t p) const noexcept {
+    constexpr Statistics::var_t operator()(const Statistics::dim_t i,
+                                           const Statistics::dim_t j,
+                                           const Statistics::dim_t eps,
+                                           const Statistics::dim_t p)
+      const noexcept {
       assert(i < N);
       assert(j < N);
       assert(eps < N);
@@ -174,8 +182,10 @@ namespace Encoding {
           assert(eps != i);
           assert(eps != j);
           const Statistics::var_t n_prev_lines = (i-1) * ((N-2)*(N-2) + (N-1));
-          const Statistics::var_t n_prev_cells = i>=j ? (j-1)*(N-2) : (j-2)*(N-2) + (N-1);
-          const Statistics::var_t v = 1 + n_prev_lines + n_prev_cells + eps_adj(i,j,eps);
+          const Statistics::var_t n_prev_cells = i>=j ? (j-1)*(N-2) :
+            (j-2)*(N-2) + (N-1);
+          const Statistics::var_t v = 1 + n_prev_lines + n_prev_cells +
+            eps_adj(i,j,eps);
           assert(v <= nvc.nls);
           return v;
         }
@@ -186,15 +196,19 @@ namespace Encoding {
           const Statistics::var_t n_prev_ls = nvc.nbls1 + (p-1) * nvc.nbls2;
           const Statistics::var_t n_prev_lines = (i-1) * (N*(N-1) - 1);
           const Statistics::var_t n_prev_cells = j * (N-1) - (j==0 ? 0 : 1);
-          const Statistics::var_t v = 1 + n_prev_ls + n_prev_lines + n_prev_cells +
-            (j==0 ? eps_adj(i,j,eps) : eps_adj(j,eps));
+          const Statistics::var_t v = 1 + n_prev_ls + n_prev_lines +
+            n_prev_cells + (j==0 ? eps_adj(i,j,eps) : eps_adj(j,eps));
           assert(v <= nvc.nls);
           return v;
         }
       }
     }
 
-    constexpr Statistics::var_t operator()(const Statistics::dim_t i, const Statistics::dim_t j, const ValPair eps, const IndexEuler pq) const noexcept {
+    constexpr Statistics::var_t operator()(const Statistics::dim_t i,
+                                           const Statistics::dim_t j,
+                                           const ValPair eps,
+                                           const IndexEuler pq)
+      const noexcept {
       assert(i < N);
       assert(j < N);
       assert(eps.x < N);
@@ -206,7 +220,8 @@ namespace Encoding {
         const Statistics::var_t n_prev_es = index(pq) * nvc.nbes1;
         const Statistics::var_t n_prev_lines = i * N3;
         const Statistics::var_t n_prev_cells = j * N2;
-        const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines + n_prev_cells + index(eps,N);
+        const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines +
+          n_prev_cells + index(eps,N);
         assert(nvc.nls < v and v <= nvc.n0);
         return v;
       }
@@ -220,11 +235,14 @@ namespace Encoding {
         const Statistics::var_t p = pq.p, q = pq.q;
         if (p >= 1) {
           assert(j != 0 or (eps.x != i and eps.y != i));
-          const Statistics::var_t n_prev_es = q * nvc.nbes1 + (index(pq)-q) * nvc.nbes2;
-          const Statistics::var_t n_prev_lines = (i-1) * N * (N-1) * (N-2) - (i-1) * 2 * (N-2);
-          const Statistics::var_t n_prev_cells = j==0 ? 0  : j * (N-1) * (N-2) - 2 * (N-2);
-          const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines + n_prev_cells
-            + (j==0 ? index_adj2(i,j,eps) : index_adj(j, eps));
+          const Statistics::var_t n_prev_es = q * nvc.nbes1 +
+            (index(pq)-q) * nvc.nbes2;
+          const Statistics::var_t n_prev_lines = (i-1) * N * (N-1) * (N-2) -
+            (i-1) * 2 * (N-2);
+          const Statistics::var_t n_prev_cells = j==0 ? 0  :
+            j * (N-1) * (N-2) - 2 * (N-2);
+          const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines +
+            n_prev_cells + (j==0 ? index_adj2(i,j,eps) : index_adj(j, eps));
           assert(nvc.nls < v and v <= nvc.n0);
           return v;
         }
@@ -235,11 +253,14 @@ namespace Encoding {
             return operator()(i,0,eps.y,pq.q);
           }
           assert(eps.x != i);
-          const Statistics::var_t n_prev_es = (q-1) * nvc.nbes1 + (index(pq)-(q-1)) * nvc.nbes2;
-          const Statistics::var_t n_prev_lines = (i-1) * ((N-2)*(N-2)*(N-2) + (N-1)*(N-2));
+          const Statistics::var_t n_prev_es = (q-1) * nvc.nbes1 +
+            (index(pq)-(q-1)) * nvc.nbes2;
+          const Statistics::var_t n_prev_lines = (i-1) * ((N-2)*(N-2)*(N-2) +
+                                                          (N-1)*(N-2));
           const Statistics::var_t n_prev_cells = j<=i ? (j-1) * (N-2) * (N-2) :
-                                           (N-1)*(N-2) + (j-2) * (N-2) * (N-2);
-          const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines + n_prev_cells + index_adj(i, j, eps);
+            (N-1)*(N-2) + (j-2) * (N-2) * (N-2);
+          const Statistics::var_t v = 1+nvc.nls + n_prev_es + n_prev_lines +
+            n_prev_cells + index_adj(i, j, eps);
           assert(nvc.nls < v and v <= nvc.n0);
           return v;
         }
@@ -247,7 +268,10 @@ namespace Encoding {
     }
 
     // Using var_t for the arguments to avoid implicit conversions:
-    static constexpr Statistics::var_t eps_adj(Statistics::var_t i, Statistics::var_t j, const Statistics::var_t eps) noexcept {
+    static constexpr Statistics::var_t eps_adj(Statistics::var_t i,
+                                               Statistics::var_t j,
+                                               const Statistics::var_t eps)
+      noexcept {
       if (i == j) {
         if (eps < i) return eps;
         else return eps-1;
@@ -257,12 +281,15 @@ namespace Encoding {
       else if (eps > i) return eps-1;
       else return eps;
     }
-    static constexpr Statistics::var_t eps_adj(const Statistics::var_t j, const Statistics::var_t eps) noexcept {
+    static constexpr Statistics::var_t eps_adj(const Statistics::var_t j,
+                                               const Statistics::var_t eps)
+      noexcept {
       if (eps > j) return eps-1;
       else return eps;
     }
 
-    constexpr Statistics::var_t index_adj(const Statistics::var_t j, const ValPair eps) const noexcept {
+    constexpr Statistics::var_t index_adj(const Statistics::var_t j,
+                                          const ValPair eps) const noexcept {
       Statistics::var_t res = index(eps, N);
       if (eps.x < j) {
         res -= eps.x;
@@ -280,7 +307,9 @@ namespace Encoding {
       }
       return res;
     }
-    constexpr Statistics::var_t index_adj(const Statistics::var_t i, const Statistics::var_t j, const ValPair eps) const noexcept {
+    constexpr Statistics::var_t index_adj(const Statistics::var_t i,
+                                          const Statistics::var_t j,
+                                          const ValPair eps) const noexcept {
       /* The relevant constraints are: x, y != j, x != i, x != y.
           For i!=j there are (N-2)^2 variables in total, namely N-2 rows
             (minus i,j), each with N-2 elements (minus j, minus diagonal).
@@ -291,7 +320,9 @@ namespace Encoding {
       const Statistics::var_t yadj = eps_adj(eps.x,j, eps.y);
       return xadj * (N-2) + yadj;
     }
-    constexpr Statistics::var_t index_adj2(const Statistics::var_t i, const Statistics::var_t j, const ValPair eps) const noexcept {
+    constexpr Statistics::var_t index_adj2(const Statistics::var_t i,
+                                           const Statistics::var_t j,
+                                           const ValPair eps) const noexcept {
       assert(i != 0 and j == 0);
       /* The relevant constraints are: x,y >= 1, x,y != i; x != y. */
       return index_adj(i,j,eps) - (eps.x-1) - (eps.y > i ? 1 : 0);
