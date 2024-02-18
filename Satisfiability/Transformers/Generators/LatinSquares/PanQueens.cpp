d7925bb49cda5b2858161a5e4fa7b48aa6d9b939
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-02-18 00:02:26 +0000

    Started SAT-translations of Pan-/Queens-diagonal problems.

---
+++ Satisfiability/Transformers/Generators/LatinSquares/PanQueens.cpp
@@ -0,0 +1,47 @@
+// Oliver Kullmann, 17.2.2024 (Swansea)
+/* Copyright 2024 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+/*
+
+*/
+
+#include <iostream>
+
+#include <ProgramOptions/Environment.hpp>
+
+namespace {
+
+  const Environment::ProgramInfo proginfo{
+        "0.0.1",
+        "17.2.2024",
+        __FILE__,
+        "Oliver Kullmann",
+        "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/LatinSquares/PanQueens.cpp",
+        "GPL v3"};
+
+  const std::string error = "ERROR[" + proginfo.prg + "]: ";
+
+  bool show_usage(const int argc, const char* const argv[]) {
+    if (not Environment::help_header(std::cout, argc, argv, proginfo))
+      return false;
+    std::cout <<
+    "> " << proginfo.prg <<
+      " N\n\n"
+      " - N            : \";\"-separated list of \"a[,b][,c]\"-sequences\n\n"
+      "Here\n"
+      "  - \n\n"
+;
+    return true;
+  }
+
+}
+
+int main(const int argc, const char* const argv[]) {
+  if (Environment::version_output(std::cout, proginfo, argc, argv)) return 0;
+  if (show_usage(argc, argv)) return 0;
+
+}
