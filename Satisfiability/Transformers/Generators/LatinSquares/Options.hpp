c061d626bae7a8243a58a845974f50933c8093b7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-02-18 02:29:18 +0000

    Editorial.

--- Satisfiability/Transformers/Generators/LatinSquares/Options.hpp
+++ Satisfiability/Transformers/Generators/LatinSquares/Options.hpp
@@ -1,9 +1,14 @@
 // Oliver Kullmann, 3.8.2020 (Swansea)
-/* Copyright 2020 Oliver Kullmann
+/* Copyright 2020, 2024 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
 License, or any later version. */
+
+/*
+  See docus/Representation/Options for documentation.
+
+*/
 
 #ifndef OPTIONS_3R4etWCIqM
 #define OPTIONS_3R4etWCIqM
