1b0df4992949a3f5da87e7627a196c859a662ff5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-02-19 09:05:34 +0000

    Started encoding of pan-/queens-diagonal problems.

---
+++ Satisfiability/Transformers/Generators/LatinSquares/PQEncoding.hpp
@@ -0,0 +1,24 @@
+// Oliver Kullmann, 18.2.2024 (Swansea)
+/* Copyright 2024 Oliver Kullmann
+This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation and included in this library; either version 3 of the
+License, or any later version. */
+
+#ifndef PQENCODING_eK42F6xZuv
+#define PQENCODING_eK42F6xZuv
+
+#include "Statistics.hpp"
+
+namespace PQEncoding {
+
+  using dim_t = Statistics::dim_t;
+  using var_t = Statistics::var_t;
+
+  struct cell_t {
+    var_t i, j;
+  };
+
+}
+
+#endif
--- Satisfiability/Transformers/Generators/LatinSquares/PanQueens.cpp
+++ Satisfiability/Transformers/Generators/LatinSquares/PanQueens.cpp
@@ -14,6 +14,8 @@ License, or any later version. */
 #include <ProgramOptions/Environment.hpp>
 
 #include "PQOptions.hpp"
+#include "AloAmo.hpp"
+#include "PQEncoding.hpp"
 
 namespace {
 
