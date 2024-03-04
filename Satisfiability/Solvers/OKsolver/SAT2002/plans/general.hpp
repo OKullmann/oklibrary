07b051d0f4ff5b2a9612ed466c555b8518c71e95
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-03-04 01:49:00 +0000

    Problems with reading from standard input.

--- Satisfiability/Solvers/OKsolver/SAT2002/plans/general.hpp
+++ Satisfiability/Solvers/OKsolver/SAT2002/plans/general.hpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 17.8.2007 (Swansea)
-/* Copyright 2007, 2008, 2009, 2010, 2011, 2023 Oliver Kullmann
+/* Copyright 2007, 2008, 2009, 2010, 2011, 2023, 2024 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -8,6 +8,16 @@ License, or any later version. */
 /*!
   \file Solvers/OKsolver/SAT2002/plans/general.hpp
   \brief Plans on the maintenance of the code for the old OKsolver
+
+
+  \todo BUG with standard input
+LatinSquares> N=23; ./CPandiagonal $N "" | OKsolver2002 -O cin
+Segmentation fault (core dumped)
+Ebenfalls
+LatinSquares> N=23; ./CPandiagonal $N "" | OKsolver2002_NTP -O -MAXN=529 -MAXK=280417 cin
+Segmentation fault (core dumped)
+Saving in a file and then processing works.
+
 
 
   \todo For option -P (only preprocessing), enable output of processed input
