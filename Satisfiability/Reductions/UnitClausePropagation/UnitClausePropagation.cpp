98e8e1770f337342b55c165f7089314cc4cbf0a7
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-03-25 21:35:24 +0000

    Indentation updated.

--- Satisfiability/Reductions/UnitClausePropagation/UnitClausePropagation.cpp
+++ Satisfiability/Reductions/UnitClausePropagation/UnitClausePropagation.cpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 13.12.2009 (Swansea)
-/* Copyright 2009, 2021 Oliver Kullmann
+/* Copyright 2009, 2021, 2024 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -48,18 +48,22 @@ License, or any later version. */
 namespace {
 
   typedef LIT_TYPE default_Lit;
-  typedef OKlib::Satisfiability::ProblemInstances::Clauses::RClausesAsVectors<default_Lit> default_Clauses;
+  typedef OKlib::Satisfiability::ProblemInstances::Clauses::RClausesAsVectors<default_Lit>
+  default_Clauses;
   // The default values of template parameters should be the same as for
   // OKlib::Satisfiability::Reductions::UnitClausePropagation::CLSAdaptorUcp.
 
   template <class Clauses = default_Clauses>
   struct read_ucp_output {
     typedef typename Clauses::value_type literal_type;
-    typedef OKlib::Satisfiability::Reductions::UnitClausePropagation::CLSAdaptorUcp<literal_type,Clauses> adaptor_type;
+    typedef OKlib::Satisfiability::Reductions::UnitClausePropagation::CLSAdaptorUcp<literal_type,Clauses>
+    adaptor_type;
 
     read_ucp_output(std::istream& in, std::ostream& out) {
       adaptor_type A;
-      typedef OKlib::InputOutput::StandardDIMACSInput<adaptor_type, OKlib::InputOutput::LiteralReadingStrict, default_Lit> dimacs_input_type;
+      typedef OKlib::InputOutput::StandardDIMACSInput<adaptor_type,
+        OKlib::InputOutput::LiteralReadingStrict, default_Lit>
+        dimacs_input_type;
       dimacs_input_type(in, A);
       typedef typename adaptor_type::int_type int_type;
       typedef std::vector<int_type> cl_t;
@@ -69,13 +73,16 @@ namespace {
       switch (A.perform_ucp()) {
       case falsified :
         F.push_back(cl_t());
-        OKlib::InputOutput::List2DIMACSOutput(F,out,A.orig_comment()+A.add_comment());
+        OKlib::InputOutput::List2DIMACSOutput(F,out,
+            A.orig_comment()+A.add_comment());
         break;
       case satisfied :
-        OKlib::InputOutput::List2DIMACSOutput(F,out,A.orig_comment()+A.add_comment());
+        OKlib::InputOutput::List2DIMACSOutput(F,out,
+            A.orig_comment()+A.add_comment());
         break;
       case open :
-        OKlib::InputOutput::List2DIMACSOutput(A,out,A.orig_comment()+A.add_comment());
+        OKlib::InputOutput::List2DIMACSOutput(A,out,
+            A.orig_comment()+A.add_comment());
         break;
       }
     }
@@ -88,6 +95,8 @@ int main(const int argc, const char* const argv[]) {
   if (argc == 1 or argv[1] != arg_set)
     read_ucp_output<>(std::cin, std::cout);
   else
-    read_ucp_output<OKlib::Satisfiability::ProblemInstances::Clauses::RClausesAsSets<default_Lit> >(std::cin, std::cout);
+    read_ucp_output
+      <OKlib::Satisfiability::ProblemInstances::Clauses::RClausesAsSets<default_Lit> >
+      (std::cin, std::cout);
   
 }
