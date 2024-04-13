2994846e69ac4885ea34af602ec6f865c150cf78
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2024-04-13 09:27:30 +0100

    Provided AC for options regarding additional constraints.
    Currently only "non-cyclic".

--- Satisfiability/Transformers/Generators/LatinSquares/ECOptions.hpp
+++ Satisfiability/Transformers/Generators/LatinSquares/ECOptions.hpp
@@ -7,7 +7,7 @@ License, or any later version. */
 
 /*
 
-  - enum class 
+  - enum class AC (none, noncyclic)
 
 */
 
@@ -17,6 +17,31 @@ License, or any later version. */
 #include <ProgramOptions/Environment.hpp>
 
 namespace ECOptions {
+
+  // Additional constraints:
+  enum class AC {
+    none = 0,
+    noncyclic = 1
+  };
+  constexpr int ACsize = int(AC::noncyclic) + 1;
+}
+
+namespace Environment {
+  template <> struct RegistrationPolicies<ECOptions::AC> {
+    static constexpr const char* sname = "ac";
+    static constexpr int size = ECOptions::ACsize;
+    static constexpr std::array<const char*, size>
+      string {"0", "nc"};
+    static constexpr std::array<const char*, size>
+      estring {"none", "noncyclic"};
+  };
+}
+
+namespace ECOptions{
+
+  std::ostream& operator <<(std::ostream& out, const AC ac) {
+    return out << Environment::W2(ac);
+  }
 
 }
 
