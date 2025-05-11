4cf8d1191ffae2df00fda0b20469afb3e931e1a2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2025-05-11 22:33:59 +0100

    Started providing enumeration-facilities.

--- Satisfiability/Transformers/Generators/Bicliques/Combinatorics.hpp
+++ Satisfiability/Transformers/Generators/Bicliques/Combinatorics.hpp
@@ -12,17 +12,77 @@ License, or any later version. */
    - General/Combinatorics.hpp contains components for enumerating k-subsets
    - Combinatorics/Enumeration/plans/Subsets.hpp contains plans.
 
+
+TODOS:
+
+1. Is AntiDiagonal::sum exact for all inputs?
+
 */
 
 #ifndef COMBINATORICS_h9dRdvvDx1
 #define COMBINATORICS_h9dRdvvDx1
 
+#include <utility>
+
 #include <cassert>
 
 #include <Numerics/NumTypes.hpp>
+#include <Numerics/NumBasicFunctions.hpp>
 
 namespace Combinatorics {
 
+  using UInt_t = FloatingPoint::UInt_t;
+
+  // Types of pairs {u,v} :
+  enum class PaTy { full, // just u, v >= 0
+                    fullneq, // u # v
+                    sorted, // u <= v
+                    sortedneq // u < v
+                  };
+
+  // Equal to Graphs::AdjVecUInt::edge_t :
+  typedef std::pair<UInt_t, UInt_t> pair_t;
+
+  /*
+    Anti-diagonal enumeration (unranking):
+    Sorting the pairs (u,v), u,v in NN_0
+     - first by sum s=u+v (the antidiagonal)
+     - second by ascending u.
+  */
+
+  struct AntiDiagonal {
+    const PaTy pt;
+    using float80 = FloatingPoint::float80;
+
+    // The antidiagonal of i (u+v for the corresponding pair):
+    constexpr UInt_t sum(const UInt_t i) const noexcept {
+      using enum PaTy;
+      switch (pt) {
+      case full : return (FloatingPoint::sqrt(float80(i)*8 + 1) - 1) / 2;
+        // https://oeis.org/A003056
+      case fullneq : return FloatingPoint::sqrt(float80(i)*2 + 1);
+        // https://oeis.org/A103128, but there starting with i=1
+      case sorted : return FloatingPoint::sqrt(float80(i)*4 + 1) - 1;
+        // https://oeis.org/A055086
+      case sortedneq : return FloatingPoint::sqrt(float80(i)*4 + 1);
+        // https://oeis.org/A000267
+      default : assert(false); return 0;
+      }
+    }
+  };
+
+  static_assert(AntiDiagonal{PaTy::full}.sum(0) == 0);
+  static_assert(AntiDiagonal{PaTy::full}.sum(65535) == 361);
+  static_assert(AntiDiagonal{PaTy::full}.sum(-1) == 6074000999UL);
+  static_assert(AntiDiagonal{PaTy::fullneq}.sum(0) == 1);
+  static_assert(AntiDiagonal{PaTy::fullneq}.sum(65535) == 362);
+  static_assert(AntiDiagonal{PaTy::fullneq}.sum(-1) == 6074000999UL);
+  static_assert(AntiDiagonal{PaTy::sorted}.sum(0) == 0);
+  static_assert(AntiDiagonal{PaTy::sorted}.sum(65535) == 510);
+  static_assert(AntiDiagonal{PaTy::sorted}.sum(-1) == 8589934590UL);
+  static_assert(AntiDiagonal{PaTy::sortedneq}.sum(0) == 1);
+  static_assert(AntiDiagonal{PaTy::sortedneq}.sum(65535) == 511);
+  static_assert(AntiDiagonal{PaTy::sortedneq}.sum(-1) == 8589934591UL);
 }
 
 #endif
--- Satisfiability/Transformers/Generators/Bicliques/TestCombinatorics.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/TestCombinatorics.cpp
@@ -6,6 +6,8 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 #include <iostream>
+#include <ranges>
+#include <vector>
 
 #include <cassert>
 
@@ -16,7 +18,7 @@ License, or any later version. */
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.0.1",
+        "0.0.5",
         "11.5.2025",
         __FILE__,
         "Oliver Kullmann",
@@ -29,10 +31,26 @@ namespace {
   constexpr bool eqp(const X& lhs, const X& rhs) noexcept {
     return lhs == rhs;
   }
+
+  template<typename F>
+  auto makelist(const F f, const UInt_t start, const UInt_t end) {
+    return std::views::iota(start, end + 1) |
+      std::views::transform(f) | std::ranges::to<std::vector>();
+  }
 }
 
 int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
+
+  {assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::full}.sum(i);}, 0, 30),
+          {0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7}));
+   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::fullneq}.sum(i);}, 0, 30),
+          {1,1,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7}));
+   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::sorted}.sum(i);}, 0, 30),
+          {0,1,2,2,3,3,4,4,4,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,8,9,9,9,9,9,10}));
+   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::sortedneq}.sum(i);}, 0, 30),
+          {1,2,3,3,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,9,10,10,10,10,10,11}));
+  }
 
 }
