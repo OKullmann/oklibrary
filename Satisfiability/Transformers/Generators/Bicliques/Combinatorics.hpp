4b2edcd83e633c94c093bc2bcc664cc375961add
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2025-05-12 13:19:14 +0100

    Nearly completed first enumeration-class.

--- Satisfiability/Transformers/Generators/Bicliques/Combinatorics.hpp
+++ Satisfiability/Transformers/Generators/Bicliques/Combinatorics.hpp
@@ -54,7 +54,18 @@ namespace Combinatorics {
     const PaTy pt;
     using float80 = FloatingPoint::float80;
 
-    // The antidiagonal of i (u+v for the corresponding pair):
+    // The number of (consecutive) indices with sum s:
+    constexpr UInt_t length(const UInt_t s) const noexcept {
+      using enum PaTy;
+      switch (pt) {
+      case full : return s + 1;
+      case fullneq : return s + 1 - (s % 2 == 0);
+      case sorted : return s/2 + 1;
+      case sortedneq : return s%2==0 ? s/2 : (s+1)/2;
+      default : assert(false); return 0; }
+    }
+
+    // The antidiagonal of i (that is, u+v for the pair corresponding to i):
     constexpr UInt_t sum(const UInt_t i) const noexcept {
       using enum PaTy;
       switch (pt) {
@@ -66,8 +77,30 @@ namespace Combinatorics {
         // https://oeis.org/A055086
       case sortedneq : return FloatingPoint::sqrt(float80(i)*4 + 1);
         // https://oeis.org/A000267
-      default : assert(false); return 0;
-      }
+      default : assert(false); return 0; }
+    }
+
+    // The first i such that sum(i) >= n, first trivially computed: */
+    constexpr UInt_t mintriv(const UInt_t n) const noexcept {
+      UInt_t summation = 0;
+      for (UInt_t s = 0; s < sum(n); ++s) summation += length(s);
+      return summation;
+    }
+    constexpr UInt_t min(const UInt_t n) const noexcept {
+      using enum PaTy;
+      const UInt_t s = sum(n);
+      switch (pt) {
+      case full : return s%2==0 ? (s/2) * (s+1) : ((s+1)/2) * s;
+      case fullneq : return 0; // ???
+      case sorted : return 0; // ???
+      case sortedneq : { const float80 h = s;
+          return FloatingPoint::ceil((h/4) * h - float80(1)/4); }
+      default : assert(false); return 0; }
+    }
+
+    constexpr pair_t operator()(const UInt_t i) const noexcept {
+      const UInt_t s = sum(i), m = min(i);
+      return {i - m, (s+m) - i};
     }
   };
 
@@ -83,6 +116,16 @@ namespace Combinatorics {
   static_assert(AntiDiagonal{PaTy::sortedneq}.sum(0) == 1);
   static_assert(AntiDiagonal{PaTy::sortedneq}.sum(65535) == 511);
   static_assert(AntiDiagonal{PaTy::sortedneq}.sum(-1) == 8589934591UL);
+
+  static_assert(AntiDiagonal{PaTy::sortedneq}.min(-1) == 18446744069414584320UL);
+
+  static_assert(AntiDiagonal{PaTy::full}(0) == pair_t{0,0});
+  static_assert(AntiDiagonal{PaTy::full}(65535) == pair_t{194,167});
+  static_assert(AntiDiagonal{PaTy::full}(-1) == pair_t{2746052115UL,3327948884UL});
+
+  static_assert(AntiDiagonal{PaTy::sortedneq}(0) == pair_t{0,1});
+  static_assert(AntiDiagonal{PaTy::sortedneq}(65535) == pair_t{255,256});
+  static_assert(AntiDiagonal{PaTy::sortedneq}(-1) == pair_t{4294967295UL,4294967296UL});
 }
 
 #endif
--- Satisfiability/Transformers/Generators/Bicliques/TestCombinatorics.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/TestCombinatorics.cpp
@@ -33,7 +33,7 @@ namespace {
   }
 
   template<typename F>
-  auto makelist(const F f, const UInt_t start, const UInt_t end) {
+  constexpr auto makelist(const F f, const UInt_t start, const UInt_t end) {
     return std::views::iota(start, end + 1) |
       std::views::transform(f) | std::ranges::to<std::vector>();
   }
@@ -43,14 +43,32 @@ int main(const int argc, const char* const argv[]) {
   if (Environment::version_output(std::cout, proginfo, argc, argv))
   return 0;
 
-  {assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::full}.sum(i);}, 0, 30),
+  {static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::full}.sum(i);}, 0, 30),
           {0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7}));
-   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::fullneq}.sum(i);}, 0, 30),
+   static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::fullneq}.sum(i);}, 0, 30),
           {1,1,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7}));
-   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::sorted}.sum(i);}, 0, 30),
+   static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::sorted}.sum(i);}, 0, 30),
           {0,1,2,2,3,3,4,4,4,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,8,9,9,9,9,9,10}));
-   assert(eqp(makelist([](UInt_t i){return AntiDiagonal{PaTy::sortedneq}.sum(i);}, 0, 30),
+   static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::sortedneq}.sum(i);}, 0, 30),
           {1,2,3,3,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,9,10,10,10,10,10,11}));
+  }
+  {using enum PaTy;
+   for (const PaTy pt : {full,  sortedneq}) {
+     const AntiDiagonal A{pt};
+     for (UInt_t n = 0; n <= 100; ++n) {
+       const UInt_t min = A.mintriv(n);
+       assert(min == A.min(n));
+       const UInt_t S = A.sum(n);
+       assert(A.sum(min) == S);
+       if (min > 0) assert(A.sum(min-1) < S);
+     }
+   }
+  }
+  {static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::full}(i);}, 0, 20),
+          {{0,0},{0,1},{1,0},{0,2},{1,1},{2,0},{0,3},{1,2},{2,1},{3,0},{0,4},{1,3},{2,2},{3,1},{4,0},{0,5},{1,4},{2,3},{3,2},{4,1},{5,0}}));
+
+   static_assert(eqp(makelist([](auto i){return AntiDiagonal{PaTy::sortedneq}(i);}, 0, 20),
+          {{0,1},{0,2},{0,3},{1,2},{0,4},{1,3},{0,5},{1,4},{2,3},{0,6},{1,5},{2,4},{0,7},{1,6},{2,5},{3,4},{0,8},{1,7},{2,6},{3,5},{0,9}}));
   }
 
 }
