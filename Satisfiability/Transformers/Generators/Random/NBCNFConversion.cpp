f9130078120abdb06aa48dfbb1cc36e514289341
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2025-05-25 14:08:59 +0100

    Improved documentation, and improved descriptive comments.

--- Satisfiability/Transformers/Generators/Random/NBCNFConversion.cpp
+++ Satisfiability/Transformers/Generators/Random/NBCNFConversion.cpp
@@ -6,17 +6,28 @@ the Free Software Foundation and included in this library; either version 3 of t
 License, or any later version. */
 
 /*
-  Reading a NB-CNF in NOBOCONF format, and printing it after various
-  modifications
+  Reading a NB-CNF in NOBOCONF format, and printing it (same format)
+  after various modifications.
 
 
 EXAMPLES:
 
 Random> echo -e "C here is starts\n1:2 2:4 0 first clause\n 4:2  0:1 0 second clause\nC comments everywhere" | ./NBCNFConversion
+C 0110
 n 5
 c 2
 1:2 2:4 0
 4:2 0:1 0
+
+The first comment shows (0 means false):
+ - sorted clauses?
+ - no (consecutive) duplicated literals in clauses?
+ - no (consecutive) clashes in clauses?
+ - sorted clause-list?
+This is refers to the original input.
+
+If clauses are sorted, then duplicated or clashing literals must occur
+consecutively, if they occur.
 
 Same as standardisation-level 0:
 Random> echo -e "C here is starts\n1:2 2:4 0 first clause\n 4:2  0:1 0 second clause\nC comments everywhere" | ./NBCNFConversion 0
@@ -27,19 +38,21 @@ The three standardisation-levels are:
             sorting of clauses, lexicographically
   level 2: additionally sorting of the clause-list, antilexicographically.
 
-THus level 1 standardises all clauses, but does not sort the whole clause-list:
+Thus level 1 standardises all clauses, but does not sort the whole clause-list:
 Random> echo -e "C here is starts\n2:4 1:2 5:4 0 first clause\n 4:2  0:1 0 second clause\nC comments everywhere" | ./NBCNFConversion 1
+C 0110
 C 0 0
 n 6
 c 2
 1:2 2:4 5:4 0
 0:1 4:2 0
 
-The first comment shows the number of eliminated clauses and the total number
+The second comment shows the number of eliminated clauses and the total number
 of eliminated literal-occurrences.
 
 Showing the application of n-lines:
 Random> echo -e "C here is starts\n1:2 2:4 1:2 5:4 0 first clause, with repetitions\n 4:2  0:1 0 second clause\nC comments everywhere\n5:1 5:17 0 tautology\nn 10 with formal variables" | ./NBCNFConversion 1
+C 0100
 C 1 3
 n 10
 c 2
@@ -49,6 +62,7 @@ c 2
 Now full standardisation, and the application of c-lines:
 
 Random> echo -e "C here is starts\nc 4\n1:2 2:4 1:2 5:4 0 first clause, with repetitions\n 4:2  0:1 0 second clause\nC comments everywhere\n20:0 20:1 0 tautology\n2:3 0 unit\nn 10 with formal variables (no effect here)" | ./NBCNFConversion 2
+C 0100
 C 1 1
 n 21
 c 3
@@ -63,6 +77,8 @@ We see that n and c include the tautological clauses (even if they get removed).
 #include <iostream>
 #include <exception>
 
+#include <cassert>
+
 #include <ProgramOptions/Environment.hpp>
 
 #include "GenLit.hpp"
@@ -72,7 +88,7 @@ We see that n and c include the tautological clauses (even if they get removed).
 namespace {
 
   const Environment::ProgramInfo proginfo{
-    "0.1.0",
+    "0.1.1",
     "25.5.2025",
     __FILE__,
     "Oliver Kullmann",
@@ -114,6 +130,11 @@ int main(const int argc, const char* const argv[]) {
     std::cerr << error << "SYNTAX ERROR:\n" << e.what() << "\n";
     return 1;
   }
+  assert(F.valid());
+  std::cout << comchar << " " << F.clauses_sorted()
+            << not F.has_consecutive_duplicates() << F.no_consecutive_clashes()
+            << F. clauselist_sorted() << std::endl;
+
   if (std_level > 0) {
     const auto stats = std_level == 1 ? F.standardise() : F.fully_standardise();
     std::cout << comchar << " ";
