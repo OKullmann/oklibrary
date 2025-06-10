f451681242bb585d9e6e2aec21d3db18297db6bd
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2025-06-10 18:49:41 +0100

    Update of version-number after various internal changes.

--- Satisfiability/Transformers/Generators/Bicliques/GraphGen.cpp
+++ Satisfiability/Transformers/Generators/Bicliques/GraphGen.cpp
@@ -1,5 +1,5 @@
 // Oliver Kullmann, 25.2.2022 (Swansea)
-/* Copyright 2022, 2023 Oliver Kullmann
+/* Copyright 2022, 2023, 2025 Oliver Kullmann
 This file is part of the OKlibrary. OKlibrary is free software; you can redistribute
 it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation and included in this library; either version 3 of the
@@ -71,8 +71,8 @@ See plans/general.txt.
 namespace {
 
   const Environment::ProgramInfo proginfo{
-        "0.3.0",
-        "8.3.2022",
+        "0.3.1",
+        "9.6.2025",
         __FILE__,
         "Oliver Kullmann",
         "https://github.com/OKullmann/oklibrary/blob/master/Satisfiability/Transformers/Generators/Bicliques/GraphGen.cpp",
